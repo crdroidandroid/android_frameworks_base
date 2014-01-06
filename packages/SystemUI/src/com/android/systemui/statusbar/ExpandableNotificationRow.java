6152f2ad88eea462c638c7334a4de633f678cf84
Jmz <jmztaylor@gmail.com>
2014-01-06 15:30:15 -0500

    Force Expanded Notifications [1/2]
    This will force expanded notifications for all apps that support expanded notifications
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
@@ -38,6 +38,8 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 import android.util.ArraySet;
 import android.util.AttributeSet;
@@ -1983,7 +1985,12 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
     }
 
     public boolean isUserExpanded() {
-        return mUserExpanded;
+        if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FORCE_EXPANDED_NOTIFICATIONS, 0, UserHandle.USER_CURRENT) != 1) {
+            return mUserExpanded;
+        } else {
+            return true;
+        }
     }
 
     /**
