07cb0fe0a49ca746aed087a2ae995c819b0bfce0
AndroidRul3z <ferrettidario@gmail.com>
2014-11-09 18:34:05 +0100

    Turn off some debugs
    Change-Id: I36bd0034f6a0d7e674e97152ac35ec1961718527

--- packages/SystemUI/src/com/android/systemui/navigationbar/buttons/DeadZone.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/buttons/DeadZone.java
@@ -43,7 +43,7 @@ public class DeadZone {
     public static final int HORIZONTAL = 0;  // Consume taps along the top edge.
     public static final int VERTICAL = 1;  // Consume taps along the left edge.
 
-    private static final boolean CHATTY = true; // print to logcat when we eat a click
+    private static final boolean CHATTY = false; // print to logcat when we eat a click
     private final NavigationBarController mNavBarController;
     private final NavigationBarView mNavigationBarView;
 
