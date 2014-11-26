dc9d5d4a378ddf727b10abf03e3d302c94bbb34b
cristianomatos <cristianobmatos@gmail.com>
2014-11-26 12:04:44 -0200

    [Base]: recents panel clear all button tweaks
    - Show or hide recents button
    - Place the button in top right or left
    
    @cristianomatos

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3081,6 +3081,18 @@ public final class Settings {
                 "mute_annoying_notifications_threshold";
 
         /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
