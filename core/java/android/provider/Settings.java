ca43bde7732fd0e0c56df7620400f5e54efa2986
faust93 <monumentum@gmail.com>
2014-12-02 12:26:57 +0300

    Wakelock Blocker [1/2]
    Change-Id: I658e8ad54dc34735e920cf2e942cab15aa3eaa2e
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3953,6 +3953,18 @@ public final class Settings {
         public static final String STATUS_BAR_CRDROID_LOGO = "status_bar_crdroid_logo";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Disable ads (HFM)
          * @hide
          */
