521dc1bf5a058f3d29d08a74bfad92cbb93f470f
Lars Greiss <kufikugel@googlemail.com>
2014-12-04 12:55:59 +0100

    Frameworks: Actually start to use HEADS_UP_REQUESTED extra
    Prepairing slim heads up feature, we do this here to allow apps to
    send a heads up notification no matter what.
    
    Google introduced this flag but is not using it in the actual API
    
    Take HEADS_UP_REQUESTED into account to indicate that this notification
    is a good heads up candidate.
    
    How to use it you can see in email or mms app
    
    Change-Id: I4a29404122ae12ab9b95e1d223911078a360b44e

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2309,12 +2309,14 @@ public abstract class BaseStatusBar extends SystemUI implements
         boolean isHighPriority = sbn.getScore() >= INTERRUPTION_THRESHOLD;
         boolean isFullscreen = notification.fullScreenIntent != null;
         boolean hasTicker = mHeadsUpTicker && !TextUtils.isEmpty(notification.tickerText);
-        boolean isAllowed = notification.extras.getInt(Notification.EXTRA_AS_HEADS_UP,
-                Notification.HEADS_UP_ALLOWED) != Notification.HEADS_UP_NEVER;
+        int asHeadsUp = notification.extras.getInt(Notification.EXTRA_AS_HEADS_UP,
+                Notification.HEADS_UP_ALLOWED);
+        boolean isAllowed = asHeadsUp != Notification.HEADS_UP_NEVER;
         boolean accessibilityForcesLaunch = isFullscreen
                 && mAccessibilityManager.isTouchExplorationEnabled();
 
-        boolean interrupt = (isFullscreen || (isHighPriority && (isNoisy || hasTicker)))
+        boolean interrupt = (isFullscreen || (isHighPriority && (isNoisy || hasTicker))
+                || asHeadsUp == Notification.HEADS_UP_REQUESTED)
                 && isAllowed
                 && !accessibilityForcesLaunch
                 && mPowerManager.isScreenOn()
