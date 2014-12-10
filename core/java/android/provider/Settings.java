ce5e33d2695c9831568bf409c129fc3993ebeb25
Lars Greiss <kufikugel@googlemail.com>
2014-12-10 20:51:07 -0200

    Frameworks: Keyguard - Add user configurable shortcuts

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2092,6 +2092,21 @@ public final class Settings {
         public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
 
         /**
+         * Defines the shortcuts to be shown on lockscreen
+         * Usage is like this: target:icon|target:icon|target:icon
+         * if :icon is not set, default application icon will be used
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
+        /**
+         * Whether shorcuts open with normal or longpress
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS_LONGPRESS =
+                "lockscreen_shortcuts_longpress";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated Use {@link Global#SHOW_PROCESSES} instead
