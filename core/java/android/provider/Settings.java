c71e4bb4ea16702e944b180468015bbd1b10155e
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    - @neobuddy89: Fix multiuser usage.
    
    From: Michael Ifland <msifland@gmail.com>
    Date: Mon, 21 Sep 2015 02:23:37 -0600
    Subject: [PATCH] Animations: Add option 'Random' to System Animations
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4330,6 +4330,25 @@ public final class Settings {
          */
         public static final String FP_SUCCESS_VIBRATE = "fp_success_vibrate";
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
