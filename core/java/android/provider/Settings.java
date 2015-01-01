6ce8c462695056d92a2ab067a1744875498ce603
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    Quick settings: Add Haptic Feedback to tiles [1/2]
    mydongistiny: updated for oreo
    neobuddy89: rewrite using TunerAPI
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9384,6 +9384,12 @@ public final class Settings {
         public static final String LOCKSCREEN_ALBUMART_FILTER = "lockscreen_albumart_filter";
 
         /**
+         * Whether to vibrate when quick settings tile is pressed.
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_VIBRATE = "quick_settings_vibrate";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
