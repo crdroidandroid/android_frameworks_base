373dfe36825aecd4ff73f055727d5a87e31b8311
maxwen <max.weninger@gmail.com>
2015-02-19 15:15:49 -0800

    Base: Add OmniSwitch as option for default recents [1/2]
    Change-Id: I6e7e1a493ab4f96abc13439466b63ee9ad1b8e5f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3527,6 +3527,12 @@ public final class Settings {
         public static final String TOAST_ANIMATION = "toast_animation";
 
         /**
+         * Alternative recent apps integration using OmniSwitch
+         * @hide
+         */
+        public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
