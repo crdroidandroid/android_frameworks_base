56146a44688a8fd8357da6c9c78c39bf525c27f3
Danny Baumann <dannybaumann@web.de>
2015-03-03 10:43:28 +0100

    Allow sending vendor- or device-specific commands to the camera HAL.
    Change-Id: I2aaa9e526b6f1a35d45e96b6d23e3db972d82733
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -1048,6 +1048,18 @@ static int32_t android_hardware_Camera_getAudioRestriction(
     return ret;
 }
 
+static void android_hardware_Camera_sendVendorCommand(JNIEnv *env, jobject thiz,
+        jint cmd, jint arg1, jint arg2)
+{
+    ALOGV("sendVendorCommand");
+    sp<Camera> camera = get_native_camera(env, thiz, NULL);
+    if (camera == 0) return;
+
+    if (camera->sendCommand(cmd, arg1, arg2) != NO_ERROR) {
+        jniThrowRuntimeException(env, "sending vendor command failed");
+    }
+}
+
 //-------------------------------------------------
 
 static const JNINativeMethod camMethods[] = {
@@ -1088,6 +1100,7 @@ static const JNINativeMethod camMethods[] = {
          (void *)android_hardware_Camera_enableFocusMoveCallback},
         {"setAudioRestriction", "(I)V", (void *)android_hardware_Camera_setAudioRestriction},
         {"getAudioRestriction", "()I", (void *)android_hardware_Camera_getAudioRestriction},
+        { "_sendVendorCommand", "(III)V", (void *)android_hardware_Camera_sendVendorCommand },
 };
 
 struct field {
