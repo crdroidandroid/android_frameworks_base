fac81352f80c3d429ec1643bdfe13ec85d01bace
Meticulus <theonejohnnyd@gmail.com>
2015-03-24 15:14:17 +0100

    Live Volume Steps [1/2]
    -Volume steps changes are instant, no need to reboot!
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
@@ -776,7 +776,8 @@ public class VolumeDialog implements TunerService.Tunable {
 
         // update slider max
         final int max = ss.levelMax * 100;
-        if (max != row.slider.getMax()) {
+        final boolean maxChanged = max != row.slider.getMax();
+        if (maxChanged) {
             row.slider.setMax(max);
         }
 
@@ -848,7 +849,7 @@ public class VolumeDialog implements TunerService.Tunable {
         final boolean enableSlider = !zenMuted;
         final int vlevel = row.ss.muted && (isRingVibrate || !isRingStream && !zenMuted) ? 0
                 : row.ss.level;
-        updateVolumeRowSliderH(row, enableSlider, vlevel);
+        updateVolumeRowSliderH(row, enableSlider, vlevel, maxChanged);
     }
 
     private void updateVolumeRowHeaderVisibleH(VolumeRow row) {
@@ -872,7 +873,7 @@ public class VolumeDialog implements TunerService.Tunable {
         row.slider.setThumbTintList(tint);
     }
 
-    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel) {
+    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel, boolean maxChanged) {
         row.slider.setEnabled(enable);
         updateVolumeRowSliderTintH(row, row.stream == mActiveStream);
         if (row.tracking) {
@@ -896,7 +897,7 @@ public class VolumeDialog implements TunerService.Tunable {
             }
         }
         final int newProgress = vlevel * 100;
-        if (progress != newProgress) {
+        if (progress != newProgress || maxChanged) {
             if (mShowing && rowVisible) {
                 // animate!
                 if (row.anim != null && row.anim.isRunning()
