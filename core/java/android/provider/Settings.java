768313aa01d3ca879d4744a808d5278eff8dcb7a
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    Add membar to recents options [1/2]
    Change-Id: Ieaf86a2360cc993bb20f971b25c2b8c49a746f64
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4166,6 +4166,12 @@ public final class Settings {
         public static final String RECENTS_DISMISS_ICON = "recents_dismiss_icon";
 
         /**
+          * show the membar in recents
+          *  @hide
+          */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
