1d546e97eec8df1da96263a390a4fa13e7134a62
Vijay Kumar Tumati <vtumati@codeaurora.org>
2015-04-22 12:50:02 +0530

    Camera: Longshot with Burst Functionality.
    New Longshot stop command is sent after receiving all the required YUV
    callbacks or releasing the shutter before reaching Max number.
    
    Change-Id: I264d94cde624b5f2ead88236bdff8d6db41e12e6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -520,6 +520,21 @@ static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jbool
     }
 }
 
+static void android_hardware_Camera_stopLongshot(JNIEnv *env, jobject thiz)
+{
+    ALOGV("stopLongshot");
+    JNICameraContext* context;
+    status_t rc;
+    sp<Camera> camera = get_native_camera(env, thiz, &context);
+    if (camera == 0) return;
+
+    rc = camera->sendCommand(CAMERA_CMD_STOP_LONGSHOT, 0, 0);
+
+    if (rc != NO_ERROR) {
+       jniThrowException(env, "java/lang/RuntimeException", "enabling longshot mode failed");
+    }
+}
+
 static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
  {
    ALOGV("sendHistogramData" );
@@ -1179,9 +1194,12 @@ static const JNINativeMethod camMethods[] = {
   { "native_sendHistogramData",
     "()V",
      (void *)android_hardware_Camera_sendHistogramData },
- { "native_setLongshot",
-     "(Z)V",
-      (void *)android_hardware_Camera_setLongshot },
+  { "native_setLongshot",
+    "(Z)V",
+     (void *)android_hardware_Camera_setLongshot },
+  { "native_stopLongshot",
+    "()V",
+     (void *)android_hardware_Camera_stopLongshot },
   { "native_setParameters",
     "(Ljava/lang/String;)V",
     (void *)android_hardware_Camera_setParameters },
