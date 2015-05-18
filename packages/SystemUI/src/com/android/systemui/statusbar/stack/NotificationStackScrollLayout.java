051b58d59e3b08998cbc05675824fe361fff70a5
tobitege <tobiasteschner@googlemail.com>
2015-05-18 10:43:39 +0200

    Fix NPE in updateEmptyShadeView() upon screen rotation
    FATAL EXCEPTION: main
    Process: com.android.systemui, PID: 19252
    
    java.lang.NullPointerException: Attempt to invoke virtual method 'boolean com.android.systemui.statusbar.EmptyShadeView.willBeGone()' on a null object reference
      at com.android.systemui.statusbar.stack.NotificationStackScrollLayout.updateEmptyShadeView(NotificationStackScrollLayout.java:2291)
      at com.android.systemui.statusbar.phone.NotificationPanelView.updateEmptyShadeView(NotificationPanelView.java:2058)
      at com.android.systemui.statusbar.phone.NotificationPanelView.updateQsState(NotificationPanelView.java:1181)
      at com.android.systemui.statusbar.phone.NotificationPanelView.setQsScrimEnabled(NotificationPanelView.java:2065)
      at com.android.systemui.statusbar.phone.PhoneStatusBar$11.onVerticalChanged(PhoneStatusBar.java:1079)
    
    Change-Id: I010b1a23301385f7a2e9e4863d9e2d1ebcde1825
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -3432,6 +3432,7 @@ public class NotificationStackScrollLayout extends ViewGroup
     }
 
     public void updateEmptyShadeView(boolean visible) {
+        if (mEmptyShadeView == null) return;
         int oldVisibility = mEmptyShadeView.willBeGone() ? GONE : mEmptyShadeView.getVisibility();
         int newVisibility = visible ? VISIBLE : GONE;
         if (oldVisibility != newVisibility) {
