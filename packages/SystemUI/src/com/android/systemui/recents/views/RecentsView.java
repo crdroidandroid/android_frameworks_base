6afcbecf3a98af3b41287550879dc99668853f21
cristianomatos <cristianobmatos@gmail.com>
2015-05-26 23:20:44 -0300

    Adjustments in recents clear all icon
    Remove alignment of clear all icon with task view, because the position is to far from fingers, and also add right margins when in left position.
    
    This commir auto reverts this two: https://github.com/crdroidandroid/android_frameworks_base/commit/91c7ce9d1a48eded092aad03bc09a19643d8e08b and https://github.com/crdroidandroid/android_frameworks_base/commit/4725e899158828b3e28e7148759e75713bc2f581
    
    Change-Id: Ice41a2069035ca84d8f1af177c568678dd3dbd34

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -315,8 +315,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
         // Get the search bar bounds and measure the search bar layout
-        Rect searchBarSpaceBounds = new Rect();
         if (mSearchBar != null) {
+            Rect searchBarSpaceBounds = new Rect();
             mConfig.getSearchBarBounds(width, height, mConfig.systemInsets.top, searchBarSpaceBounds);
             mSearchBar.measure(
                     MeasureSpec.makeMeasureSpec(searchBarSpaceBounds.width(), MeasureSpec.EXACTLY),
@@ -330,7 +330,6 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         // Measure each TaskStackView with the full width and height of the window since the 
         // transition view is a child of that stack view
         int childCount = getChildCount();
-        int taskViewWidth = 0;
         for (int i = 0; i < childCount; i++) {
             View child = getChildAt(i);
             if (child != mSearchBar && child.getVisibility() != GONE) {
@@ -338,14 +337,6 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
                 // Set the insets to be the top/left inset + search bounds
                 tsv.setStackInsetRect(taskStackBounds);
                 tsv.measure(widthMeasureSpec, heightMeasureSpec);
-
-                // Retrieve the max width of the task views
-                int taskViewChildCount = tsv.getChildCount();
-                for (int j = 0; j < taskViewChildCount; j++) {
-                    View taskViewChild = tsv.getChildAt(j);
-                    taskViewWidth = Math.max(taskViewChild.getMeasuredWidth(), taskViewWidth);
-                }
-
             }
         }
 
@@ -361,22 +352,13 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             switch (clearRecentsLocation) {
                 case Constants.DebugFlags.App.RECENTS_CLEAR_ALL_TOP_LEFT:
                     params.gravity = Gravity.TOP | Gravity.LEFT;
+                    params.leftMargin = width - taskStackBounds.right;
                     break;
                 case Constants.DebugFlags.App.RECENTS_CLEAR_ALL_TOP_RIGHT:
                 default:
                     params.gravity = Gravity.TOP | Gravity.RIGHT;
-                    break;
-            }
-            if (mSearchBar != null && (searchBarSpaceBounds.width() > taskViewWidth)) {
-                // Adjust to the search bar
-                params.rightMargin = width - searchBarSpaceBounds.right;
-            } else {
-                // Adjust to task views
-                params.rightMargin = (width / 2) - (taskViewWidth / 2);
-
-                // If very close to the screen edge, align to it
-                if (params.rightMargin < mClearRecents.getWidth())
                     params.rightMargin = width - taskStackBounds.right;
+                    break;
             }
             mClearRecents.setLayoutParams(params);
         } else {
