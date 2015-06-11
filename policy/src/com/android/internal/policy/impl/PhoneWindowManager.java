67039d193f5023bc870136b0518617a752ebde42
cristianomatos <cristianobmatos@gmail.com>
2015-06-11 11:19:39 -0300

    Leave home key wake working even if hw keys are disabled
    - If a user enable "Wake up device" in home button options and further disable hw keys, home key will have your wake functionality preserved and working
    
    Change-Id: Id9a50e6dbb8eeac7759acc7b0612e3d221795f25
    Plus: some code comments corrections

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3114,8 +3114,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // timeout.
         if (keyCode == KeyEvent.KEYCODE_HOME) {
 
+            // Disable home key if hw keys is set to off
             if (scanCode != 0 && !hasHwKeysEnabled()) {
-                Log.i(TAG, "Ignoring Home Key: we have navbar on");
+                Log.i(TAG, "Ignoring Home Key: we have hw keys disabled");
                 return 0;
             }
 
@@ -3213,9 +3214,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return -1;
         } else if (keyCode == KeyEvent.KEYCODE_MENU) {
 
-            // Disable menu key if navbar is set to on
+            // Disable menu key if hw keys is set to off
             if (scanCode != 0 && !hasHwKeysEnabled()) {
-                Log.i(TAG, "Ignoring Menu Key: we have navbar on");
+                Log.i(TAG, "Ignoring Menu Key: we have hw keys disabled");
                 return 0;
             }
 
@@ -3279,9 +3280,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return -1;
         } else if (keyCode == KeyEvent.KEYCODE_SEARCH) {
 
-            // Disable search key if navbar is set to on
+            // Disable search key if hw keys is set to off
             if (scanCode != 0 && !hasHwKeysEnabled()) {
-                Log.i(TAG, "Ignoring Search Key: we have navbar on");
+                Log.i(TAG, "Ignoring Search Key: we have hw keys disabled");
                 return 0;
             }
 
@@ -3300,9 +3301,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return 0;
         } else if (keyCode == KeyEvent.KEYCODE_APP_SWITCH) {
 
-            // Disable app switch key if navbar is set to on
+            // Disable app switch key if hw keys is set to off
             if (scanCode != 0 && !hasHwKeysEnabled()) {
-                Log.i(TAG, "Ignoring App Switch Key: we have navbar on");
+                Log.i(TAG, "Ignoring App Switch Key: we have hw keys disabled");
                 return 0;
             }
 
@@ -3340,9 +3341,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return -1;
         } else if (keyCode == KeyEvent.KEYCODE_ASSIST) {
 
-            // Disable assist key if navbar is set to on
+            // Disable assist key if hw keys is set to off
             if (scanCode != 0 && !hasHwKeysEnabled()) {
-                Log.i(TAG, "Ignoring Assist Key: we have navbar on");
+                Log.i(TAG, "Ignoring Assist Key: we have hw keys disabled");
                 return 0;
             }
 
@@ -3438,9 +3439,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return -1;
         } else if (keyCode == KeyEvent.KEYCODE_BACK) {
 
-            // Disable back key if navbar is set to on
+            // Disable back key if navbar hw keys is set to off
             if (scanCode != 0 && !hasHwKeysEnabled()) {
-                Log.i(TAG, "Ignoring Back Key: we have navbar on");
+                Log.i(TAG, "Ignoring Back Key: we have hw keys disabled");
                 return 0;
             }
 
@@ -5523,30 +5524,30 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                                                 isKeyguardShowingAndNotOccluded() :
                                                 mKeyguardDelegate.isShowing()));
 
-        // If we have nav bar on let's disable all hw keys actions
+        // Disable all hw keys actions but let home key wake on if it's enabled
         if (!hasHwKeysEnabled()) {
-            if (scanCode != 0 && keyCode == KeyEvent.KEYCODE_HOME) {
-                Log.i(TAG, "Ignoring Home Key: we have navbar on");
+            if (scanCode != 0 && keyCode == KeyEvent.KEYCODE_HOME && !mHomeWakeScreen) {
+                Log.i(TAG, "Ignoring Home Key: we have hw keys and also home key wake disabled");
                 return 0;
             }
             if (scanCode != 0 && keyCode == KeyEvent.KEYCODE_MENU) {
-                Log.i(TAG, "Ignoring Menu Key: we have navbar on");
+                Log.i(TAG, "Ignoring Menu Key: we have hw keys disabled");
                 return 0;
             }
             if (scanCode != 0 && keyCode == KeyEvent.KEYCODE_BACK) {
-                Log.i(TAG, "Ignoring Back Key: we have navbar on");
+                Log.i(TAG, "Ignoring Back Key: we have hw keys disabled");
                 return 0;
             }
             if (scanCode != 0 && keyCode == KeyEvent.KEYCODE_SEARCH) {
-                Log.i(TAG, "Ignoring Search Key: we have navbar on");
+                Log.i(TAG, "Ignoring Search Key: we have hw keys disabled");
                 return 0;
             }
             if (scanCode != 0 && keyCode == KeyEvent.KEYCODE_APP_SWITCH) {
-                Log.i(TAG, "Ignoring App Switch Key: we have navbar on");
+                Log.i(TAG, "Ignoring App Switch Key: we have hw keys disabled");
                 return 0;
             }
             if (scanCode != 0 && keyCode == KeyEvent.KEYCODE_ASSIST) {
-                Log.i(TAG, "Ignoring Assist Key: we have navbar on");
+                Log.i(TAG, "Ignoring Assist Key: we have hw keys disabled");
                 return 0;
             }
         }
