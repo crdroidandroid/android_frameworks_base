8783ba0a57142f186752d52915f1bbc4e49ccc0e
Shuhao Wu <shuhao@shuhaowu.com>
2015-06-22 20:21:14 -0400

    Allow toggling animations off [1/2]
    @neobuddyy89: Rewrite code with below changes
    * Adapt for Android O
    * Move settings to Global with other animation settings
    * Fixed disabling logic
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12363,6 +12363,12 @@ public final class Settings {
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
         /**
+         * Disabled Transition animations of the android system
+         * @hide
+         */
+        public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
