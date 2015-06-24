89aa51e4a3af0bfb67f9b35073e25fcb2e497fea
Daniel Koman <dankoman30@gmail.com>
2015-06-23 21:16:42 -0300

    [1/3] Frameworks: Proximity speaker
    Patchset: add time delay preference
    Change-Id: I8846e1d54b7f26f4322b91fc1ba20f3e5da465ec

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2551,6 +2551,26 @@ public final class Settings {
         public static final String RINGTONE = "ringtone";
 
         /**
+         * Whether the proximity sensor will adjust call to speaker
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
+
+        /**
+         * Time delay to activate speaker after proximity sensor triggered
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
+
+        /**
+         * Whether the proximity sensor will adjust call to speaker,
+         * only while in call (not while ringing on outgoing call)
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY =
+                "proximity_auto_speaker_incall_only";
+
+        /**
          * Persistent store for the SIM-2 ringtone URI.
          * <p>
          * If you need to play SIM-2 ringtone at any given time, it is recommended
