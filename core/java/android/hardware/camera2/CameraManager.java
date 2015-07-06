e102e9f70bf02fa39d1ddb20a3eff8532bccca24
Shuzhen Wang <shuzhenw@codeaurora.org>
2015-07-06 15:20:24 -0700

    Camera: Extend face detection
    This change is the combination of below commits, plus removing
    ExtendedFace class from frameworks/base.
    
      # This is the 1st commit message:
      Add framework support for extended face detect
      CRs-Fixed: 587195
      Change-Id: I2a1c477ee6d11f2b9764966abbb85ede9100fe91
    
      # This is the 2nd commit message:
      Camera: Extended face features added
      - QCFace renamed to ExtendedFace
      CRs-Fixed: 587195
      Change-Id: I7c7e2073f283522c2f0bd0fa5e7b96a74ae18011
    
      # This is the 3rd commit message:
      Camera: QCFace AOST copyrights fixed
      AOST files copyright data must not be updated to 2014
      CRs-Fixed: 587195
      Change-Id: I0cd603da674152c9f47350e571ec3e230b61f8dd
    
    Updated these changes with change in package name of extendedFace
    com.qualcomm.qti.camera.ExtendedFace
    
    Change-Id: I5d099efe1721e5d26af51006dae4e5f3ea3292d6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -66,6 +66,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.ServiceSpecificException;
 import android.os.SystemProperties;
+import android.text.TextUtils;
 import android.util.ArrayMap;
 import android.util.ArraySet;
 import android.util.Log;
