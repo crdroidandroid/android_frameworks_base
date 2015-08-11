cf432fb3bcb28ac2308b85fe7a3295b31b1a3ca9
Preetam Singh Ranawat <apranawat@codeaurora.org>
2015-08-11 10:32:57 -0700

    audio: add support for extended formats
    Add support to playback and record extended formats.
    
    audio: add support for extended audio encoding
    - EVRC, QCELP ,LPCM, and multichannel encoding.
    Change-Id: I9542ec9dec1872237940846952d28bef3ce24285
    
    audio: add support for extended audio encoding
    - update the constants for extended audio file types.
    Change-Id: I3e87347d58d8c1d9b281185ed13fdaaa4d95f988
    
    audio: add DSD as a valid file format
    Change-Id: I7d8ba4ce2e8b5185e0e2db8f92957112614e8d44
    
    Change-Id: Ie7df424da1bd526ca15a9a0c69d192133579c152
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/jni/android_media_AudioFormat.h
+++ core/jni/android_media_AudioFormat.h
@@ -48,6 +48,13 @@
 #define ENCODING_DTS_UHD 27
 #define ENCODING_DRA 28
 
+#define ENCODING_AMR_NB     100
+#define ENCODING_AMR_WB     101
+#define ENCODING_EVRC       102
+#define ENCODING_EVRC_B     103
+#define ENCODING_EVRC_WB    104
+#define ENCODING_EVRC_NW    105
+
 #define ENCODING_INVALID    0
 #define ENCODING_DEFAULT    1
 
@@ -94,6 +101,18 @@ static inline audio_format_t audioFormatToNative(int audioFormat)
         return AUDIO_FORMAT_AC4;
     case ENCODING_E_AC3_JOC:
         return AUDIO_FORMAT_E_AC3_JOC;
+    case ENCODING_AMR_NB:
+        return AUDIO_FORMAT_AMR_NB;
+    case ENCODING_AMR_WB:
+        return AUDIO_FORMAT_AMR_WB;
+    case ENCODING_EVRC:
+        return AUDIO_FORMAT_EVRC;
+    case ENCODING_EVRC_B:
+        return AUDIO_FORMAT_EVRCB;
+    case ENCODING_EVRC_WB:
+        return AUDIO_FORMAT_EVRCWB;
+    case ENCODING_EVRC_NW:
+        return AUDIO_FORMAT_EVRCNW;
     case ENCODING_DEFAULT:
         return AUDIO_FORMAT_DEFAULT;
     case ENCODING_DOLBY_MAT:
@@ -189,6 +208,18 @@ static inline int audioFormatFromNative(audio_format_t nativeFormat)
         return ENCODING_DTS_UHD;
     case AUDIO_FORMAT_DRA:
         return ENCODING_DRA;
+    case AUDIO_FORMAT_AMR_NB:
+        return ENCODING_AMR_NB;
+    case AUDIO_FORMAT_AMR_WB:
+        return ENCODING_AMR_WB;
+    case AUDIO_FORMAT_EVRC:
+        return ENCODING_EVRC;
+    case AUDIO_FORMAT_EVRCB:
+        return ENCODING_EVRC_B;
+    case AUDIO_FORMAT_EVRCWB:
+        return ENCODING_EVRC_WB;
+    case AUDIO_FORMAT_EVRCNW:
+        return ENCODING_EVRC_NW;
     case AUDIO_FORMAT_DEFAULT:
         return ENCODING_DEFAULT;
     default:
