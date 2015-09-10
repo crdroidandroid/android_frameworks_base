a88b5a397406dab6b78313008898d04ec92b16d0
Alexander Martinz <eviscerationls@gmail.com>
2015-09-09 18:07:01 -0700

    fw_base: On-The-Go Mode (1/3)
    BenzoEdit:
    Cleaned up for M now N
    
    AICPEdit:
    Adapted to Los base and power menu on O
    Includes
     * 3ef04d9737eb92af4b27af6d5cd28144775db557
    	GlobalActions: OnTheGo: Change drawable and fix string
    	by mydongistiny <jaysonedson@gmail.com>
     * 01dfffa2518cd4e57bdc02e741eb24ea3ca5e899
    	SystemUI: OnTheGo: Fix derp that started screenrecord
    	by mydongistiny <jaysonedson@gmail.com>
     * fd91b431015cf5823dcd5d6b04b5aceb0185b07d
    	SystemUI: OnTheGo: Add notification channel parts
    	by mydongistiny <jaysonedson@gmail.com>
    
    Change-Id: If3db29575cefd4a46ae387a5e3f7464e62bd3edd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4896,6 +4896,29 @@ public final class Settings {
         public static final String LOCKSCREEN_CLOCK_SELECTION = "lockscreen_clock_selection";
 
         /**
+         * The alpha value of the On-The-Go overlay.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
+
+        /**
+         * Whether the service should restart itself or not.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_SERVICE_RESTART = "on_the_go_service_restart";
+
+        /**
+         * The camera instance to use.
+         * 0 = Rear Camera
+         * 1 = Front Camera
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
