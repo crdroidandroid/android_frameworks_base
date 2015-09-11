1bf199dc1899391cc2e5b649098682640c1c5723
Roman Birg <roman@cyngn.com>
2015-09-11 14:08:50 -0700

    SystemUI: improve circle battery
    While plugged in, the circle animation would cause the statusbar to
    keep redrawing itself repeatedly, using up unnecessary cpu cycles,
    even when idle.
    
    - Removed circle animation in status bar
    - Left animations on keyguard/notificaiton header
    - Set layer type to hardware when animating for optimized quick drawing
    - Cleaned up some logic and unnecessary locking
    - Removed extra work done in each draw
    - override onDraw instead of draw to let the View system decide when we
      should draw
    
    Change-Id: If0624ab6e3723f4522ebbd9ce583b142c1cb4606
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1319,6 +1319,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery));
         batteryMeterView.setBatteryStateRegistar(mBatteryController);
         batteryMeterView.setBatteryController(mBatteryController);
+        batteryMeterView.setAnimationsEnabled(false);
         ((BatteryLevelTextView) mStatusBarView.findViewById(R.id.battery_level_text))
                 .setBatteryStateRegistar(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
