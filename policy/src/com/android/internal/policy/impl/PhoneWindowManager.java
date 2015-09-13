0591c736c6de4927b91b9e8ca48f0b9c27cafecc
Jorge Ruesga <jorge@ruesga.com>
2015-09-13 21:12:51 +0200

    base: fix window bounds for left hand navigationbar
    Currently in left-hand navigationbar mode all drawerlayouts have an inconsistent presentation
    because the actual window size calculation code only takes into consideration the content
    view. This problem is not present in the rest of the navigation modes because of the nature
    of the position of the navigation bar. This patch fix the display and content layouts calculation.
    To repro just set navigation mode to hand-left and access an app with a drawerlayout (xe:
    the email app) in landscape mode.
    
    Change-Id: I1394f39f4f5e7e6ac50b9558657685e86547acf3
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4178,7 +4178,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     int right = overscanLeft + mNavigationBarWidthForRotation[displayRotation];
                     mTmpNavigationFrame.set(0, 0, right, displayHeight);
                     mStableLeft = mStableFullscreenLeft = mTmpNavigationFrame.right;
-                    mStableFullscreenLeft = mTmpNavigationFrame.right;
                     if (transientNavBarShowing) {
                         mNavigationBarController.setBarShowingLw(true);
                     } else if (navVisible) {
@@ -4426,7 +4425,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    private void applyStableConstraints(int sysui, int fl, Rect r) {
+    private void applyStableConstraints(int sysui, int fl, Rect r, Rect d) {
+        if (mNavigationBarLeftInLandscape) {
+            d.left = r.left;
+            r.left = 0;
+        }
+
         if ((sysui & View.SYSTEM_UI_FLAG_LAYOUT_STABLE) != 0) {
             // If app is requesting a stable layout, don't let the
             // content insets go below the stable values.
@@ -4666,7 +4670,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         cf.right = mRestrictedScreenLeft + mRestrictedScreenWidth;
                         cf.bottom = mRestrictedScreenTop + mRestrictedScreenHeight;
                     }
-                    applyStableConstraints(sysUiFl, fl, cf);
+                    applyStableConstraints(sysUiFl, fl, cf, df);
                     if (adjust != SOFT_INPUT_ADJUST_NOTHING) {
                         vf.left = mCurLeft;
                         vf.top = mCurTop;
@@ -4780,7 +4784,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             + mRestrictedScreenHeight;
                 }
 
-                applyStableConstraints(sysUiFl, fl, cf);
+                applyStableConstraints(sysUiFl, fl, cf, df);
 
                 if (adjust != SOFT_INPUT_ADJUST_NOTHING) {
                     vf.left = mCurLeft;
