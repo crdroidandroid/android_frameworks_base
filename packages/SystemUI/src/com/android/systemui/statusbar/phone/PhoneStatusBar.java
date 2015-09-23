abbb8a8fc9f2c3e7ce809778614e059f3c592a4e
d34d <clark@cyngn.com>
2015-09-23 11:57:48 -0700

    Themes: Call recreateTheme when theme changed
    In order to pick up changes in styles and attributes it is necessary
    to call recreateTheme() when a theme change occurs.
    
    Change-Id: I7cd22ec09914c467710fbe76672e975b30c556b3
    TICKET: CYNGNOS-1126

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4214,7 +4214,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         final boolean updateStatusBar = shouldUpdateStatusbar(mCurrentTheme, newTheme);
         if (newTheme != null) mCurrentTheme = (ThemeConfig) newTheme.clone();
         if (updateStatusBar) {
-            mContext.recreateTheme();
             recreateStatusBar();
             attachPieContainer(isPieEnabled());
 
