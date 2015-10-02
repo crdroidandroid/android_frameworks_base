371abb9ae7fe0d5a5c426de52b138aa2a1fe3d84
cristianomatos <cristianobmatos@gmail.com>
2015-10-02 06:08:41 -0300

    Revert "Changes in Heads up qs tile"
    This reverts commit c893ae35a0d5d06a371ecf876637088c36ac24cf.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
@@ -19,15 +19,28 @@ package com.android.systemui.qs.tiles;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
+import android.provider.Settings.Global;
 
+import com.android.systemui.qs.GlobalSetting;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.R;
 
 /** Quick settings tile: Heads up **/
 public class HeadsUpTile extends QSTile<QSTile.BooleanState> {
 
+    private static final Intent NOTIFICATION_SETTINGS = new Intent("android.settings.NOTIFICATION_MANAGER");
+
+    private final GlobalSetting mSetting;
+
     public HeadsUpTile(Host host) {
         super(host);
+
+        mSetting = new GlobalSetting(mContext, mHandler, Global.HEADS_UP_NOTIFICATIONS_ENABLED) {
+            @Override
+            protected void handleValueChanged(int value) {
+                handleRefreshState(value);
+            }
+        };
     }
 
     @Override
@@ -37,36 +50,29 @@ public class HeadsUpTile extends QSTile<QSTile.BooleanState> {
 
     @Override
     protected void handleClick() {
-        setEnabled();
+        setEnabled(!mState.value);
         refreshState();
     }
 
     @Override
     protected void handleLongClick() {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.setClassName("com.android.settings",
-            "com.android.settings.Settings$HeadsUpSettingsActivity");
-        mHost.startSettingsActivity(intent);
+        mHost.startSettingsActivity(NOTIFICATION_SETTINGS);
     }
 
-    private boolean getUserHeadsUpState() {
-         return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HEADS_UP_USER_ENABLED,
-                Settings.System.HEADS_UP_USER_ON) != 0;
-    }
-
-    private void setEnabled() {
-        Settings.System.putInt(mContext.getContentResolver(),
-                Settings.System.HEADS_UP_USER_ENABLED,
-                getUserHeadsUpState() ? 0 : 1);
+    private void setEnabled(boolean enabled) {
+        Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
+                enabled ? 1 : 0);
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        state.value = getUserHeadsUpState();
+        final int value = arg instanceof Integer ? (Integer)arg : mSetting.getValue();
+        final boolean headsUp = value != 0;
+        state.value = headsUp;
         state.visible = true;
         state.label = mContext.getString(R.string.quick_settings_heads_up_label);
-        if (getUserHeadsUpState()) {
+        if (headsUp) {
             state.icon = ResourceIcon.get(R.drawable.ic_qs_heads_up_on);
             state.contentDescription =  mContext.getString(
                     R.string.accessibility_quick_settings_heads_up_on);
