2eff1baac15407696c48ec4b9ca0a768f7534352
maxwen <max.weninger@gmail.com>
2015-10-27 18:30:27 +0000

    Add Wakelock Blocker [1/3]
    Forward ported to N
    
    Change-Id: I3565a15d53a7a20124ba2a9ae3ec3a761d8721d5
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3736,6 +3736,18 @@ public final class Settings {
          */
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
