6079168c11581f86e5c62999d1c8353e89938f36
Jmz <jmztaylor@gmail.com>
2015-10-29 23:16:34 +0000

    Allow disabling of FC dialogs [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4911,6 +4911,12 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
