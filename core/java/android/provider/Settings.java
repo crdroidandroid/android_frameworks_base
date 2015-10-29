b38aa301dd7ff02c9b561255fdad78a5d5b32c47
Jmz <jmztaylor@gmail.com>
2015-10-29 22:56:21 +0000

    Disable Immersive Messages [1/2]
    Based off Xposed module by GermainZ
    https://github.com/GermainZ/ImmersiveModeDontPanic
    
    - Ported to M since Google decided to move shit around
    
    Change-Id: Ie5559d8bb50374b3a13afb6efd91fdfb9a77d179
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3629,6 +3629,12 @@ public final class Settings {
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
+         * Disable Immersive Message
+         * @hide
+         */
+        public static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
