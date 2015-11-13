6b855701c4e812a3da1a2208103edefa73809280
bigrushdog <randall.rushing@gmail.com>
2015-11-13 00:50:14 -0800

    Statusbar: Gracefully adapt 3Minit Battery hook for AOSP
    3Minit Battery is a free or paid third party statusbar
    battery icon solution. This commit builds upon the
    initial SystemUI hook source release by Gary Harrington.
    
    We are able to significantly optimize the original code
    because we are not constrained by the "one-size-fits-all"
    conditions the original code must meet. At the same time,
    we fully maintain the integrity of the UX intended by
    the original code.
    
    Of note is the introduction of a controller class. This
    class takes over settings and state management as well
    as handles receiver events. The controller class also
    monitors package changes and updates feature visibility
    accordingly if a needed resource is added or removed.
    
    Upon initialization, if no feature resources are
    detected, all resources are freed.
    
    ** Changes for N **
    
    There is no longer a batter view in the statusbar header
    so don't pass as constructor arg and adapt in controller
    class.
    
    Change-Id: I0254bfa0becfbf6e83e9d0547abda8a77047ab1e
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -209,6 +209,8 @@ import com.android.systemui.statusbar.policy.KeyguardUserSwitcher;
 import com.android.systemui.statusbar.policy.LocationControllerImpl;
 import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.MinitBattery;
+import com.android.systemui.statusbar.policy.MinitBatteryController;
+import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NetworkControllerImpl;
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.PreviewInflater;
@@ -419,6 +421,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     protected LockscreenWallpaper mLockscreenWallpaper;
     SuControllerImpl mSuController;
     WeatherControllerImpl mWeatherController;
+    MinitBatteryController mMinitBatteryController;
 
     int mNaturalBarHeight = -1;
 
@@ -1185,6 +1188,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             });
         }
+
+        mMinitBatteryController = new MinitBatteryController(mContext, mStatusBarView, mKeyguardStatusBar);
+        mPackageMonitor.addListener(mMinitBatteryController);
 
         // User info. Trigger first load.
         mKeyguardStatusBar.setUserInfoController(mUserInfoController);
@@ -4549,6 +4555,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mNavigationController.destroy();
         }
         mPackageMonitor.removeListener(mNavigationController);
+        mPackageMonitor.removeListener(mMinitBatteryController);
         mPackageMonitor.unregister();
 
         if (mHandlerThread != null) {
@@ -4557,6 +4564,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mContext.unregisterReceiver(mBroadcastReceiver);
         mContext.unregisterReceiver(mDemoReceiver);
+        mContext.unregisterReceiver(mDUReceiver);
         mAssistManager.destroy();
 
         final SignalClusterView signalCluster =
