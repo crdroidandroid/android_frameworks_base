93d4e6169ac603b487a6548e5e683c3aa9873a2b
maxwen <max.weninger@gmail.com>
2015-11-17 00:45:25 +0100

    base: silent mode is back
    BenzoEdit: Fixed for 8.1
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -462,14 +462,16 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
                             if (hasVibrator) {
                                 mController.setRingerMode(AudioManager.RINGER_MODE_VIBRATE, false);
                             } else {
-                                final boolean wasZero = row.ss.level == 0;
-                                mController.setStreamVolume(stream,
-                                        wasZero ? row.lastAudibleLevel : 0);
+                                mController.setRingerMode(AudioManager.RINGER_MODE_SILENT, false);
                             }
+                        } else if (mState.ringerModeInternal == AudioManager.RINGER_MODE_VIBRATE) {
+                            mController.setRingerMode(AudioManager.RINGER_MODE_SILENT, false);
                         } else {
                             mController.setRingerMode(AudioManager.RINGER_MODE_NORMAL, false);
                             if (row.ss.level == 0) {
                                 mController.setStreamVolume(stream, 1);
+                            } else {
+                                mController.setStreamVolume(stream, row.lastAudibleLevel);
                             }
                         }
                     } else {
@@ -814,7 +816,8 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
         row.icon.setAlpha(iconEnabled ? 1 : 0.5f);
         final int iconRes =
                 isRingVibrate ? R.drawable.ic_volume_ringer_vibrate
-                : isRingSilent || zenMuted ? row.cachedIconRes
+                : isRingSilent ? R.drawable.ic_volume_ringer_mute
+                : zenMuted ? row.cachedIconRes
                 : ss.routedToBluetooth ?
                         (ss.muted ? R.drawable.ic_volume_media_bt_mute
                                 : R.drawable.ic_volume_media_bt)
@@ -881,7 +884,7 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
 
         // update slider
         final boolean enableSlider = !zenMuted;
-        final int vlevel = row.ss.muted && (!isRingStream && !zenMuted) ? 0
+        final int vlevel = row.ss.muted && (isRingSilent || !isRingStream && !zenMuted) ? 0
                 : row.ss.level;
         updateVolumeRowSliderH(row, enableSlider, vlevel, maxChanged);
     }
