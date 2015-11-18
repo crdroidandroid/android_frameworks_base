bf12c010f499606668844ea565cfcd2531934385
Jmz <jmztaylor@gmail.com>
2015-11-18 16:28:03 +0100

    Allow disabling of FC dialogs [1/2]
    Change-Id: I18f81e3d3f0873c7742809d1a8c9d217c87e9531

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3617,6 +3617,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
