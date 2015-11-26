b2a7c1daf222c00e1a701c5170dc54e003b2d603
cristianomatos <cristianobmatos@gmail.com>
2015-11-26 15:54:08 -0200

    Revert "Base: AdBlocker [1/3]"
    This reverts commit aa44b0c8ab28badabf7caa51efd5a3266ad10265.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3495,12 +3495,6 @@ public final class Settings {
         public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
 
         /**
-         * Disable ads (HFM)
-         * @hide
-         */
-        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
