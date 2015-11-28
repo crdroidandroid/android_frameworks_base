c85cc14865254d022cab21dbd0fe0ab23019dad9
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2015-11-28 16:16:32 +0100

    Fix and work around aliasing rule violations
    Change-Id: Ic23aaf092bda5edda4ea15ae15e81dca9d5d02be
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/hwui/Android.mk
+++ libs/hwui/Android.mk
@@ -112,7 +112,7 @@ hwui_test_common_src_files := \
 hwui_cflags := \
     -DEGL_EGLEXT_PROTOTYPES -DGL_GLEXT_PROTOTYPES \
     -DATRACE_TAG=ATRACE_TAG_VIEW -DLOG_TAG=\"OpenGLRenderer\" \
-    -Wall -Wno-unused-parameter -Wunreachable-code -Werror
+    -Wall -Wno-unused-parameter -Wunreachable-code -Werror -Wno-error=strict-aliasing
 
 ifeq ($(TARGET_USES_HWC2),true)
     hwui_cflags += -DUSE_HWC2
