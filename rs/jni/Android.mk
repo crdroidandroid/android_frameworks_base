7bcda4c778ec949484199d66721f077be497eb44
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2015-11-28 16:16:32 +0100

    Fix and work around aliasing rule violations
    Change-Id: Ic23aaf092bda5edda4ea15ae15e81dca9d5d02be
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rs/jni/Android.mk
+++ rs/jni/Android.mk
@@ -30,6 +30,7 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_CFLAGS += -Wno-unused-parameter -std=c++11
 LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code
+LOCAL_CFLAGS += -fno-strict-aliasing
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(addprefix $(rs_generated_include_dir)/,rsgApiFuncDecl.h)
 LOCAL_MODULE:= librs_jni
