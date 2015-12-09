eece7d7d2bbb00dc9dadfb7ea92dca87940869a0
David Viteri <davidteri91@gmail.com>
2015-12-09 21:45:00 +0100

    Base: remove Duplicate method onAttachedToWindow() in type RecentsView
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -446,6 +446,10 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
     @Override
     protected void onAttachedToWindow () {
         super.onAttachedToWindow();
+        mMemText = (TextView) ((View)getParent()).findViewById(R.id.recents_memory_text);
+        mMemBar = (ProgressBar) ((View)getParent()).findViewById(R.id.recents_memory_bar);
+        updateMemoryStatus();
+
         mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
         mClearRecents.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -494,15 +498,6 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         } catch (IOException e) {
         }
         return memory / 1048576;
-    }
-    
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        mMemText = (TextView) ((View)getParent()).findViewById(R.id.recents_memory_text);
-        mMemBar = (ProgressBar) ((View)getParent()).findViewById(R.id.recents_memory_bar);
-
-        updateMemoryStatus();
     }
 
     /**
