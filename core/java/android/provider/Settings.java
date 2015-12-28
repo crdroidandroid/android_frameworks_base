90d30419415c5e9a047e4270e75c84e902c64478
Alex Cruz <mazdarider23@gmail.com>
2015-12-28 13:42:21 -0500

    Add Power Menu animations [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4482,6 +4482,13 @@ public final class Settings {
         public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
 
         /**
+         * Whether to display animation when prompting the power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ANIMATION = "power_menu_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
