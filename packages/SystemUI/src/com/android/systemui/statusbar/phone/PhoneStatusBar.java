cac7681aec7d1a6da997757de9f3ca33ad407e74
beanstown106 <nbenis106@gmail.com>
2016-01-02 04:33:46 -0500

    Max Lockscreen Notification count [1/2]
    when items like lockscreen weather and slim lockscreen shortcuts are enabled the default 5 notifications makes things janky as hell, this allows you to lower the max notifications to play nice with the added features in the lockscreen
    
    xanaxdroid fixed it up for N
    
    Change-Id: I7374d51648d6bdf8c7098772a2a9322ade289312
    [neo: Use Tuner API.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -309,6 +309,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             "cmsystem:" + CMSettings.System.NAVBAR_LEFT_IN_LANDSCAPE;
     private static final String NAVIGATION_BAR_SHOW =
             "system:" + Settings.System.NAVIGATION_BAR_SHOW;
+    private static final String LOCKSCREEN_MAX_NOTIF_CONFIG =
+            "system:" + Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG;
 
     static {
         boolean onlyCoreApps;
@@ -358,6 +360,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private boolean mHasNavigationBar = false;
     private boolean mNavigationBarAttached = false;
+
+    private int mMaxKeyguardNotifConfig;
+    private boolean mCustomMaxKeyguard;
 
     Display mDisplay;
     Point mCurrentDisplaySize = new Point();
@@ -824,7 +829,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 SCREEN_BRIGHTNESS_MODE,
                 NAVBAR_LEFT_IN_LANDSCAPE,
                 STATUS_BAR_BRIGHTNESS_CONTROL,
-                NAVIGATION_BAR_SHOW);
+                NAVIGATION_BAR_SHOW,
+                LOCKSCREEN_MAX_NOTIF_CONFIG);
 
         // Lastly, call to the icon policy to install/update all the icons.
         mIconPolicy = new PhoneStatusBarPolicy(mContext, mIconController, mCastController,
@@ -4732,13 +4738,20 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     @Override
     protected int getMaxKeyguardNotifications(boolean recompute) {
-        if (recompute) {
-            mMaxKeyguardNotifications = Math.max(1,
-                    mNotificationPanel.computeMaxKeyguardNotifications(
-                            mMaxAllowedKeyguardNotifications));
-            return mMaxKeyguardNotifications;
+        mCustomMaxKeyguard = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.LOCK_SCREEN_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT) == 1;
+        if (mCustomMaxKeyguard) {
+            return mMaxKeyguardNotifConfig;
+        } else {
+           if (recompute) {
+               mMaxKeyguardNotifications = Math.max(1,
+                       mNotificationPanel.computeMaxKeyguardNotifications(
+                               mMaxAllowedKeyguardNotifications));
+               return mMaxKeyguardNotifications;
+           } else {
+               return mMaxKeyguardNotifications;
+           }
         }
-        return mMaxKeyguardNotifications;
     }
 
     public int getMaxKeyguardNotifications() {
@@ -5353,6 +5366,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 break;
             case NAVIGATION_BAR_SHOW:
                 updateNavigationBarVisibility();
+                break;
+            case LOCKSCREEN_MAX_NOTIF_CONFIG:
+                mMaxKeyguardNotifConfig =
+                        newValue == null ? 5 : Integer.parseInt(newValue);
                 break;
             default:
                 break;
