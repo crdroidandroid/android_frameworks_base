66338fcf3febf3aae0acd74e9f5fba5b2e357087
Lars Greiss <kufikugel@googlemail.com>
2016-01-02 19:19:48 +0100

    fb: add back all keyboard IME features back (1/2)
    - ime switcher notification - thanks cm
    - slims fullscreen disable on landscape
    - slims force autorotation on IME
    - volume button control for navigate in the message - thanks cm
    - slims force to show always enter key
    
    mydongistiny: fixed up for oreo
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -1006,6 +1006,15 @@ public class StatusBarManagerService extends IStatusBarService.Stub {
         enforceStatusBar();
     }
 
+    @Override
+    public void setAutoRotate(boolean enabled) {
+        if (mBar != null) {
+            try {
+                mBar.setAutoRotate(enabled);
+            } catch (RemoteException ex) {}
+        }
+    }
+
     private void enforceStatusBar() {
         mContext.enforceCallingOrSelfPermission(android.Manifest.permission.STATUS_BAR,
                 "StatusBarManagerService");
