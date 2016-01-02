85b286d315e82a42c20b1dc3b5dedfc6481dbc75
Lars Greiss <kufikugel@googlemail.com>
2016-01-02 19:19:48 +0100

    fb: add back all keyboard IME features back (1/2)
    - ime switcher notification - thanks cm
    - slims fullscreen disable on landscape
    - slims force autorotation on IME
    - volume button control for navigate in the message - thanks cm
    - slims force to show always enter key
    
    mydongistiny: fixed up for oreo
    Change-Id: I71e93c39620cc171e658b2d7e26491bac97c651d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -119,6 +119,7 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_SHOW_IN_DISPLAY_FINGERPRINT_VIEW = 48 << MSG_SHIFT;
     private static final int MSG_HIDE_IN_DISPLAY_FINGERPRINT_VIEW = 49 << MSG_SHIFT;
     private static final int MSG_SET_BLOCKED_GESTURAL_NAVIGATION = 50 << MSG_SHIFT;
+    private static final int MSG_SET_AUTOROTATE_STATUS         = 51 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -283,6 +284,7 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         default void showInDisplayFingerprintView() { }
         default void hideInDisplayFingerprintView() { }
         default void setBlockedGesturalNavigation(boolean blocked) {}
+        default void setAutoRotate(boolean enabled) { }
 
         /**
          * @see IStatusBar#onDisplayReady(int)
@@ -854,6 +856,15 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         }
     }
 
+    @Override
+    public void setAutoRotate(boolean enabled) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SET_AUTOROTATE_STATUS);
+            mHandler.obtainMessage(MSG_SET_AUTOROTATE_STATUS,
+                enabled ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -1132,6 +1143,11 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                 case MSG_SET_BLOCKED_GESTURAL_NAVIGATION:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).setBlockedGesturalNavigation((Boolean) msg.obj);
+                    }
+                    break;
+                case MSG_SET_AUTOROTATE_STATUS:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).setAutoRotate(msg.arg1 != 0);
                     }
                     break;
             }
