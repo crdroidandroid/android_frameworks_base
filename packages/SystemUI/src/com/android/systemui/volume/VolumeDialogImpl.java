4b649627523de342777f4335506a64f407a7e4e8
Randall Rushing <randall.rushing@gmail.com>
2016-01-10 12:09:58 -0800

    Themes: Allow volume slider and thumb to work with custom images.
    * forward-port to Android Pie
    * forward-port to Android Oreo
    
    * Credit Bryan Owens for giving guidance on what was needed.
    
    Change-Id: I862008d1b433666333a4c77904b44a05e70472a2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -383,6 +383,8 @@ public class VolumeDialogImpl implements VolumeDialog {
         }
         row.dndIcon = row.view.findViewById(R.id.dnd_icon);
         row.slider =  row.view.findViewById(R.id.volume_row_slider);
+        row.slider.setProgressTintMode(PorterDuff.Mode.SRC_ATOP);
+        row.slider.setThumbTintMode(PorterDuff.Mode.SRC_ATOP);
         row.slider.setOnSeekBarChangeListener(new VolumeSeekBarChangeListener(row));
         row.anim = null;
 
