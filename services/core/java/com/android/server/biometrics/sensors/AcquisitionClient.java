77be0db5ee5243daeb77978909c95e94af4343b1
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    Change-Id: Ib2444f91cdba9973dc3142bba8cf6d349e983c8b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/sensors/AcquisitionClient.java
+++ services/core/java/com/android/server/biometrics/sensors/AcquisitionClient.java
@@ -27,6 +27,8 @@ import android.os.SystemClock;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Slog;
 
 import com.android.server.biometrics.log.BiometricContext;
@@ -197,7 +199,9 @@ public abstract class AcquisitionClient<T> extends HalClientMonitor<T> implement
 
     protected final void vibrateSuccess() {
         Vibrator vibrator = getContext().getSystemService(Vibrator.class);
-        if (vibrator != null && mShouldVibrate) {
+        boolean shouldVib = Settings.System.getIntForUser(getContext().getContentResolver(),
+            Settings.System.FP_SUCCESS_VIBRATE, 1, UserHandle.USER_CURRENT) == 1;
+        if (vibrator != null && mShouldVibrate && shouldVib) {
             vibrator.vibrate(Process.myUid(),
                     getContext().getOpPackageName(),
                     SUCCESS_VIBRATION_EFFECT,
