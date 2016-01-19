5836bcae2fe29673e95060a8b9ebed6d16898e3e
Damien Vagner <damienx.vagner@intel.com>
2016-01-19 13:09:54 +0100

    Fix java crash under DhcpClient.java
    Avoid calling method getTransactionId() on a null pointer.
    
    Change-Id: I330982293ba005679c2ad338e779153b790ee983
    Signed-off-by: Damien Vagner <damienx.vagner@intel.com>
    Signed-off-by: Zhiquan Liu <zhiquan.liu@intel.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/net/java/android/net/dhcp/DhcpClient.java
+++ services/net/java/android/net/dhcp/DhcpClient.java
@@ -632,6 +632,8 @@ public class DhcpClient extends StateMachine {
     }
 
     public boolean isValidPacket(DhcpPacket packet) {
+        if (packet == null)
+            return false;
         // TODO: check checksum.
         int xid = packet.getTransactionId();
         if (xid != mTransactionId) {
