8c705f5090f74cfd784cd5dff3f408aae0fe5859
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow/nougat By: BeansTown106
    this was basically a rewrite as everything about it changed
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Fri, 15 Apr 2022 21:20:04 +0530
        Subject: HeadsUp: Change heads up timeout to seconds
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BaseHeadsUpManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BaseHeadsUpManager.java
@@ -25,6 +25,8 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.ArrayMap;
 import android.view.accessibility.AccessibilityManager;
 
@@ -66,6 +68,7 @@ public abstract class BaseHeadsUpManager extends AlertingNotificationManager imp
     private final AccessibilityManagerWrapper mAccessibilityMgr;
 
     private final UiEventLogger mUiEventLogger;
+    private final int mDecayDefault;
 
     /**
      * Enum entry for notification peek logged from this class.
@@ -99,7 +102,10 @@ public abstract class BaseHeadsUpManager extends AlertingNotificationManager imp
         mMinimumDisplayTime = resources.getInteger(R.integer.heads_up_notification_minimum_time);
         mStickyForSomeTimeAutoDismissTime = resources.getInteger(
                 R.integer.sticky_heads_up_notification_time);
-        mAutoDismissTime = resources.getInteger(R.integer.heads_up_notification_decay);
+        mDecayDefault = resources.getInteger(R.integer.heads_up_notification_decay) / 1000;
+        mAutoDismissTime = Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT,
+                mDecayDefault, UserHandle.USER_CURRENT) * 1000;
         mTouchAcceptanceDelay = resources.getInteger(R.integer.touch_acceptance_delay);
         mSnoozedPackages = new ArrayMap<>();
         int defaultSnoozeLengthMs =
@@ -116,11 +122,17 @@ public abstract class BaseHeadsUpManager extends AlertingNotificationManager imp
                     mSnoozeLengthMs = packageSnoozeLengthMs;
                     mLogger.logSnoozeLengthChange(packageSnoozeLengthMs);
                 }
+                mAutoDismissTime = Settings.System.getIntForUser(
+                    context.getContentResolver(), Settings.System.HEADS_UP_TIMEOUT,
+                    mDecayDefault, UserHandle.USER_CURRENT) * 1000;
             }
         };
         globalSettings.registerContentObserver(
                 globalSettings.getUriFor(SETTING_HEADS_UP_SNOOZE_LENGTH_MS),
                 /* notifyForDescendants = */ false,
+                settingsObserver);
+        context.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.HEADS_UP_TIMEOUT), false,
                 settingsObserver);
     }
 
