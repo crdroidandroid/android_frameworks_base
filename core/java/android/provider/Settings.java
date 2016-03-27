eb0111ab173f61aaca2fe3b0108f4995838ecc6f
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    base: HeadsUp add timeout option [1/2]
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: Ia8afb6494a3feb1f4f2ff6c13e8b6444c38a7506
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4878,6 +4878,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+        * Heads up timeout configuration
+        * @hide
+        */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
