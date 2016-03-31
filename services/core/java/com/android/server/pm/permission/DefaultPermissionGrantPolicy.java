998d8cba08fed651cdcb9e9cfc63adf8fa2a51fc
mydongistiny <jaysonedson@gmail.com>
2016-03-30 17:06:26 -0700

    Fix permissons for ContactsProvider2
    Fix this error:
    
    W ActivityManager: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS
    E GooglePlusContactsSync: Failed to clear out contacts
    E GooglePlusContactsSync: java.lang.SecurityException: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS
    
    Change-Id: I3544ccb29382ddb693720e9ddead658a77e31847
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -943,6 +943,12 @@ public final class DefaultPermissionGrantPolicy {
             grantRuntimePermissions(fiPackage, LOCATION_PERMISSIONS, userId);
             grantRuntimePermissions(fiPackage, SMS_PERMISSIONS, userId);
         }
+        // ContactsProvider2
+        PackageParser.Package conpro2Package = getDefaultProviderAuthorityPackage("com.android.providers.contacts.ContactsProvider2", userId);
+        if (conpro2Package != null) {
+            grantRuntimePermissions(conpro2Package, CONTACTS_PERMISSIONS, true, userId);
+            grantRuntimePermissions(conpro2Package, STORAGE_PERMISSIONS, true, userId);
+        }
 
         if (mPermissionGrantedCallback != null) {
             mPermissionGrantedCallback.onDefaultRuntimePermissionsGranted(userId);
