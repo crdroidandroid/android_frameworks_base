292156392494168073cfd7dc8946d6add527d3f3
Kazuki Nakayama <kazuki.x.nakayama@sonymobile.com>
2016-06-29 14:36:49 +0900

    Avoid ANR caused by synchronized() of appDiedLocked()
    Symptom:
    SystemUI is crashed by ANR.
    
    Root cause:
    1) The application is crashed.
    2) ActivityManagerService handles it using AppDeathRecipient.
    3) AppDeathRecipient calls synchronized() for a long time.
    4) SystemUI calls the method locked by synchronized on Main thread.
    5) Main thread is blocked. and SystemUI is crashed by ANR.
    
    Solution:
    updateOomAdjLocked() is moved to Handler thread
    from ActivityManagerService#appDiedLocked().
    
    Bug: 36043366
    
    Change-Id: I9c11b5facc7e0668b1c7dda11dcfcc4f3d4ffb42
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1705,6 +1705,8 @@ public class ActivityManagerService extends IActivityManager.Stub
     static ServiceThread sKillThread = null;
     static KillHandler sKillHandler = null;
 
+    static final int UPDATE_OOMADJ_MSG = 99999;
+
     CompatModeDialog mCompatModeDialog;
     UnsupportedDisplaySizeDialog mUnsupportedDisplaySizeDialog;
     long mLastMemUsageReportTime = 0;
@@ -2501,6 +2503,11 @@ public class ActivityManagerService extends IActivityManager.Stub
                             }
                         }
                     }
+                }
+            } break;
+            case UPDATE_OOMADJ_MSG: {
+                synchronized (ActivityManagerService.this) {
+                    updateOomAdjLocked();
                 }
             } break;
             }
@@ -5531,7 +5538,8 @@ public class ActivityManagerService extends IActivityManager.Stub
             handleAppDiedLocked(app, false, true);
 
             if (doOomAdj) {
-                updateOomAdjLocked();
+                mHandler.removeMessages(UPDATE_OOMADJ_MSG);
+                mHandler.sendEmptyMessage(UPDATE_OOMADJ_MSG);
             }
             if (doLowMem) {
                 doLowMemReportIfNeededLocked(app);
