4b6938f93e8adf561c7f008769bf5f6873d1eeed
Sauhard Pande <sauhardp@codeaurora.org>
2016-07-21 18:23:21 -0700

    camera: Support exposing aux camera to apps
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Paul Keith <javelinanddart@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Change-Id: I15910154c6df205e6d4e00bfad30a00c9e3d5bee

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -42,6 +42,7 @@ import android.content.AttributionSourceState;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.hardware.Camera;
 import android.hardware.CameraExtensionSessionStats;
 import android.hardware.CameraStatus;
 import android.hardware.ICameraService;
@@ -2504,7 +2505,9 @@ public final class CameraManager {
                     Thread.currentThread().getId(), mDeviceStatus.size()));
             try {
                 List<String> cameraIds = new ArrayList<>();
+                boolean exposeAuxCamera = Camera.shouldExposeAuxCamera();
                 for (int i = 0; i < mDeviceStatus.size(); i++) {
+                    if (!exposeAuxCamera && i == 2) break;
                     int status = mDeviceStatus.valueAt(i);
                     DeviceCameraInfo info = mDeviceStatus.keyAt(i);
                     if (status == ICameraServiceListener.STATUS_NOT_PRESENT
@@ -3086,6 +3089,11 @@ public final class CameraManager {
         }
 
         private void onStatusChangedLocked(int status, DeviceCameraInfo info) {
+            if (!Camera.shouldExposeAuxCamera() && Integer.parseInt(info.mCameraId) >= 2) {
+                Log.w(TAG, String.format("Ignoring status update of camera %d", info.mDeviceId));
+                return;
+            }
+
             if (DEBUG) {
                 Log.v(TAG,
                         String.format("Camera id %s has status changed to 0x%x for device %d",
