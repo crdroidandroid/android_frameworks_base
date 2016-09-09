1a294002502f6d60fb11121f1c4a805a93e09adb
Alex <mazdarider23@gmail.com>
2016-09-09 01:09:13 -0400

    Immersive Recents [1/2]
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3842,6 +3842,16 @@ public final class Settings {
         public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
 
         /**
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
+         */
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
