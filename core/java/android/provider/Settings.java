2e63572c62aeb4014318ed8fb3adbe20db58110e
Pranav Vashi <neobuddy89@gmail.com>
2016-09-20 20:00:17 +0530

    base: add CPU info overlay [1/2]
    Change-Id: I8e2fd81edd8499eea2b1a6de8c042938387b33ef
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10348,6 +10348,12 @@ public final class Settings {
                 "storage_settings_clobber_threshold";
 
         /**
+         * Control whether the process CPU info meter should be shown.
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
