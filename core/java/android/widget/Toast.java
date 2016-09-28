59c5b041533bb125e7daf720dfda84c70576379a
Marcel Dopita <m@rcel.cz>
2016-09-28 19:55:13 +0200

    Show icons of only background apps in Toasts
    As implemented and discussed in I82bf23664eea134f3b1f89ad5a99f6be73906ba8,
    this patch disables the icons in Tosts for already foreground apps. It's
    useful, no questions about that but please be aware of:
    
    - Icons in Toasts are also displayed in "tooltips" of ActionBar (when you
    press and hold an action). I think that goes agains material design as the
    ActionBar component now doesn't even show app icon.
    - Toasts from Android OS contain the Green robot. (IMHO: It was never intended
    for system Toast to have an icon)
    - Toasts are intended "to be as unobtrusive as possible" and this kinds of
    break it. (From javadoc)
    - Icons should stay as workaround for finding that badly designed/intrusive
    app because: "Your app should not create a dialog or toast if it is not
    currently on screen." (From notifications design paterns)
    - Not that relevant but not all users welcome this aesthetic:
    https://www.reddit.com/r/cyanogenmod/comments/3oazh8/does_the_icon_in_the_toast_notification_bother/
    https://www.reddit.com/r/cyanogenmod/comments/3jg5c2/icons_on_toasts/
    etc.
    
    Change-Id: If748297bd422c7ddbf061da735b8e4ed0a41b425
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/widget/Toast.java
+++ core/java/android/widget/Toast.java
@@ -18,6 +18,7 @@ package android.widget;
 
 import android.annotation.IntDef;
 import android.annotation.StringRes;
+import android.app.ActivityManager;
 import android.app.INotificationManager;
 import android.app.ITransientNotification;
 import android.content.Context;
@@ -439,14 +440,18 @@ public class Toast {
                 if ((Settings.System.getInt(context.getContentResolver(),
                         Settings.System.TOAST_ICON, 1) == 1)) {
                     if (appIcon != null) {
-                        PackageManager pm = context.getPackageManager();
-                        Drawable icon = null;
-                        try {
-                            icon = pm.getApplicationIcon(packageName);
-                        } catch (PackageManager.NameNotFoundException e) {
-                            // nothing to do
+                        ActivityManager am =
+                                (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+                        if (!am.isPackageInForeground(packageName)) {
+                            PackageManager pm = context.getPackageManager();
+                            Drawable icon = null;
+                            try {
+                                icon = pm.getApplicationIcon(packageName);
+                            } catch (PackageManager.NameNotFoundException e) {
+                                // nothing to do
+                            }
+                            appIcon.setImageDrawable(icon);
                         }
-                        appIcon.setImageDrawable(icon);
                     }
                 }
                 mWM = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
