04a6d3bd81196dc96d7cfa14964470a20c1bf9dc
Alex Cruz <mazdarider23@gmail.com>
2016-09-30 21:45:43 +0200

    Add Sync QS Tile
    * 3rd tile without CM's implementation. It's pretty easy after all. Credits to the original author of the commits below and zephiK.
    * Adapted for marshmallow by KreAch3R
    * SystemUI Tuner configurable
    
    This is a combination of 2 commits.
    
    QS: add Sync tile (1/2)
    
    QS: Sync tile cleanup (1/2)
    
    * (Get inline with cm)
    * Add vector drawables (thanks to cm)
    
    fix sync
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -55,6 +55,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
+import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -438,6 +439,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(this);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);
         else if (tileSpec.equals("adb_network")) return new AdbOverNetworkTile(this);
+        else if (tileSpec.equals("sync")) return new SyncTile(this);
         else if (tileSpec.equals(NightModeTile.NIGHT_MODE_SPEC))
             return new NightModeTile(this);
         // Intent tiles.
