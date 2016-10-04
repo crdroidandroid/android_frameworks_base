ffd7c23f506347f03e178b33fc2ddcbe90e342e7
xyyx <xyyx@mail.ru>
2016-10-04 13:38:38 +0800

    Add BT easy toggle [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3785,6 +3785,13 @@ public final class Settings {
         public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
 
         /**
+         * Enable single click to turn WiFi on or off.
+         *
+         * @hide
+         */
+        public static final String QS_BT_EASY_TOGGLE = "qs_bt_easy_toggle";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
