93ce1124a805c8c07d46343cf19f34bf72cac278
Jmz <jmztaylor@gmail.com>
2016-10-06 21:37:04 +0200

    Add support to disable immersive messages [1/2]
    Based off Xposed module by GermainZ
    https://github.com/GermainZ/ImmersiveModeDontPanic
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/ImmersiveModeConfirmation.java
+++ services/core/java/com/android/server/policy/ImmersiveModeConfirmation.java
@@ -379,13 +379,18 @@ public class ImmersiveModeConfirmation {
 
         @Override
         public void handleMessage(Message msg) {
-            switch(msg.what) {
-                case SHOW:
-                    handleShow();
-                    break;
-                case HIDE:
-                    handleHide();
-                    break;
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                     Settings.System.DISABLE_IMMERSIVE_MESSAGE, 0) != 1) {
+                switch(msg.what) {
+                    case SHOW:
+                        handleShow();
+                        break;
+                    case HIDE:
+                        handleHide();
+                        break;
+                }
+            } else {
+                handleHide();
             }
         }
     }
