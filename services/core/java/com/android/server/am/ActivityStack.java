23293f5fb6c2476bd4d245742380436144ce535c
lei.peng <lei.peng@spreadtrum.com>
2016-10-08 17:58:37 +0800

    Modify the case in adjustFocus when the mFocusedActivity is null
    Starting activity failed and the mFocusedActvity become null, consider this condition when adjust focus.
    Because in this condition, the focus mismatch between ActivityManagerService#mFocusedActivity and InputDispatcher(inputfilnger)#mFocusedApplication, ANR may occur.
    
    Test: monkey test 48h with no ANR
    
    Change-Id: Id1a2bf0d6ca6530e34443d359fbe2aa6c11b878c
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -3218,7 +3218,7 @@ final class ActivityStack {
     }
 
     private void adjustFocusedActivityLocked(ActivityRecord r, String reason) {
-        if (!mStackSupervisor.isFocusedStack(this) || mService.mFocusedActivity != r) {
+        if (!mStackSupervisor.isFocusedStack(this) || (mService.mFocusedActivity != r && mService.mFocusedActivity != null)) {
             return;
         }
 
