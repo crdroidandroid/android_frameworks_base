ba4fc3f161127283351ced8b092f0c067266b074
Pranav Vashi <neobuddy89@gmail.com>
2016-10-09 17:30:16 +0530

    base: Fix easy toggles [1/2]
    Change-Id: I0968df359508848b2abcb3e029bd1d6b202edd38
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3784,26 +3784,6 @@ public final class Settings {
         public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
 
         /**
-         * Enable single click to turn WiFi on or off.
-         *
-         * @hide
-         */
-        public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
-
-        /**
-         * Enable single click to turn WiFi on or off.
-         *
-         * @hide
-         */
-        public static final String QS_BT_EASY_TOGGLE = "qs_bt_easy_toggle";
-
-        /**
-          * Whether to use one tap tile action to enable or disable data 
-          * @hide
-          */
-        public static final String QS_DATA_ADVANCED = "qs_data_advanced";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6673,6 +6653,26 @@ public final class Settings {
          * @hide
          */
         public static final String WEB_ACTION_ENABLED = "web_action_enabled";
+
+        /**
+         * Enable single click to turn WiFi on or off.
+         *
+         * @hide
+         */
+        public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
+
+        /**
+         * Enable single click to turn WiFi on or off.
+         *
+         * @hide
+         */
+        public static final String QS_BT_EASY_TOGGLE = "qs_bt_easy_toggle";
+
+        /**
+          * Whether to use one tap tile action to enable or disable data 
+          * @hide
+          */
+        public static final String QS_DATA_ADVANCED = "qs_data_advanced";
 
         /**
          * This are the settings to be backed up.
