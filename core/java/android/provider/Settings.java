c6fba8eaa9639f42bede5d4cbf6fa05db3bbced0
Pranav Vashi <neobuddy89@gmail.com>
2016-10-12 01:44:12 +0530

    Customizable lockscreen shortcuts [1/2]
    * Credits to original authors and slim ROMs.
    
    Change-Id: Ia6d3b96ee56a47e8e147578e9de97d20eb17462c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3820,6 +3820,21 @@ public final class Settings {
          */
         public static final String HIDE_LOCKSCREEN_ALARM = "hide_lockscreen_alarm";
 
+       /**
+         * Defines the shortcuts to be shown on lockscreen
+         * Usage is like this: target:icon|target:icon|target:icon
+         * if :icon is not set, default application icon will be used
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
+        /**
+         * Whether shorcuts open with normal or longpress
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS_LONGPRESS =
+                "lockscreen_shortcuts_longpress";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
