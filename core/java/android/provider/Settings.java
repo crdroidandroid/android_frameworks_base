3aadb0c68926748b8ed08ae677fd45f0b99138f4
UtkarshGupta <utkarsh.eminem@gmail.com>
2016-10-11 09:53:13 +0200

    Fw_base - Battery light: 100% charged level (1/2)
    Change-Id: I37d3ba83b6246b549f6be1096798fccd0c949f11
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4603,6 +4603,12 @@ public final class Settings {
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
         /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
