b58c1e7c6501b43eb18afcd360a0aece73725eb4
Kazuki Nakayama <kazuki.x.nakayama@sonymobile.com>
2016-10-19 15:58:52 +0900

    Avoid deadlock when installing app
    Symptom:
    System crash is occurred by deadlock
    
    Root cause:
    ActivityManagerService's APIs usually hold a lock of itself instance.
    
    In this case, four threads tried to take the same lock and it caused
    deadlock.
    Following three objects are in a three-cornered deadlocked.
    - ActivityManagerService's instance
    - PackageInstallerSession.mLock
    - PackageManagerService.mPackages
    
    Solution:
    Call checkUidPermission before taking a lock of ActivityManagerService's
    instance.
    
    Bug: 32425801
    
    Change-Id: I71ce57b1b8f3e0e9ba64b94a7b1f210b702efb6a
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -17356,8 +17356,10 @@ public final class ActivityManagerService extends ActivityManagerNative
     public List<ActivityManager.RunningServiceInfo> getServices(int maxNum,
             int flags) {
         enforceNotIsolatedCaller("getServices");
+
+        boolean granted = ActiveServices.checkIntractAcrossUsersFullPermission();
         synchronized (this) {
-            return mServices.getRunningServiceInfoLocked(maxNum, flags);
+            return mServices.getRunningServiceInfoLocked(maxNum, flags, granted);
         }
     }
 
