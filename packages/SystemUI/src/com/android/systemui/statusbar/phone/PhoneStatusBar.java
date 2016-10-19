0939d464332dea274f590b035000567e31d33ce0
bigrushdog <randall.rushing@gmail.com>
2016-10-18 23:51:44 -0700

    DUI: Initialize package monitor class
    We use our package monitor class to let listeners know about
    package install events. Our instance was declared in DUI bringup
    but wasn't initialized.
    
    Change-Id: Ib3af977ac6fd6ea80404a9c00696263cd155c423
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -804,6 +804,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationController == null) {
             mNavigationController = new NavigationController(mContext, mContext.getResources(), this);
         }
+        mPackageMonitor = new DUPackageMonitor();
+        mPackageMonitor.register(mContext, mHandler);
+        mPackageMonitor.addListener(mNavigationController);
 
         super.start(); // calls createAndAddWindows()
 
@@ -4241,6 +4244,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mWindowManager.removeViewImmediate(mNavigationController.getBar().getBaseView());
             mNavigationController.destroy();
         }
+        mPackageMonitor.removeListener(mNavigationController);
+        mPackageMonitor.unregister();
+
         if (mHandlerThread != null) {
             mHandlerThread.quitSafely();
             mHandlerThread = null;
