95e8bcde34c2334acd150fd379b9a000f4a02f5f
Pranav Vashi <neobuddy89@gmail.com>
2016-10-25 02:13:57 +0530

    base: Fix compilation without SystemUI Tests
    Change-Id: I5bb6f77642f778d92b51698518b554077486ef58
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -71,4 +71,6 @@ include $(BUILD_PACKAGE)
 
 ifeq ($(EXCLUDE_SYSTEMUI_TESTS),)
     include $(call all-makefiles-under,$(LOCAL_PATH))
+else
+    include frameworks/base/packages/SystemUI/libs/Android.mk
 endif
