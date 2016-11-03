0c4f2b5ef8478c0d94a316af83584522b45261dd
Qiushi.Han <qiushi.han@spreadtrum.com>
2016-11-03 11:46:15 +0800

    ANR related to split-screen
    In some cases, for example, when an activity with a dialog leaves
    split-screen mode, the input focus is not changed to the recreated
    window, tap Back key, anr is coming.
    
    To avoid this, update window focus one more time.
    
    Google issue:
    https://code.google.com/p/android/issues/detail?id=226906
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -2478,6 +2478,7 @@ public class WindowManagerService extends IWindowManager.Stub
                 // animation.
                 win.mAnimatingExit = true;
                 win.mReplacingRemoveRequested = true;
+                updateFocusedWindowLocked(UPDATE_FOCUS_WILL_PLACE_SURFACES, true);
                 Binder.restoreCallingIdentity(origId);
                 return;
             }
