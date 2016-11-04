96372da2ef20ebb9d78ac6a73b6862d706fdef0f
Alex Cruz <mazdarider23@gmail.com>
2016-11-04 06:16:47 +0100

    Optional screenshot type [1/2]
    @neobuddy89: Only when using volume keys to take screenshot.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5006,6 +5006,12 @@ public final class Settings {
         public static final String SCREENRECORD_LOW_QUALITY = "screenrecord_low_quality";
 
         /**
+         * Whether to use partial screenshot when using volume keys
+         * @hide
+         */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
