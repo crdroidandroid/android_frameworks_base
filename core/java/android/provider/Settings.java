bd1de29e2bae6929f57c00a0a3741e1de690e575
Alex Cruz <mazdarider23@gmail.com>
2016-11-04 06:16:47 +0100

    Optional screenshot type [1/2]
    @neobuddy89: Only when using volume keys to take screenshot.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4313,6 +4313,12 @@ public final class Settings {
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
+         * Whether to use partial screenshot when using volume keys
+         * @hide
+         */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
