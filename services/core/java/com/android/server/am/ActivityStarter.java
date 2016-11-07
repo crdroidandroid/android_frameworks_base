04d115cf1f4dd15071ff1b723fc4b8d1e68bde60
Ryuki Nakamura <ryuki.x.nakamura@sonymobile.com>
2016-11-07 11:17:35 +0900

    Avoid NPE when restarting task all activities of which will be cleared
    Symptom:
    Crash occurred in ActivityManager when restarting task with
    FLAG_ACTIVITY_CLEAR_TOP and FLAG_ACTIVITY_RESET_TASK_IF_NEEDED flags.
    
    Root cause:
    Before M, reset task was being performed before clear task.
    Reset task returns an activity which is top of its task after reset
    task.
    The returned activity is used as launching source when all activities of
    their task were cleared by clear task.
    
    Since N, reset task became to be performed AFTER clear task.
    In the case of this order, reset task returns null when all activities
    of their task were cleared by clear task.
    As a result, NPE occurred by trying to use the returned activity as
    launching source.
    
    Solution:
    Reuse top activity of its task as launching source when task is empty by
    clear task and reset task.
    
    Bug: 33529635
    
    Change-Id: I58382f9f009cea7f45069f385e9d8797c907ed50
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityStarter.java
+++ services/core/java/com/android/server/am/ActivityStarter.java
@@ -1130,7 +1130,12 @@ class ActivityStarter {
 
             sendPowerHintForLaunchStartIfNeeded(false /* forceSend */);
 
+            final ActivityRecord intentActivity = mReusedActivity;
             mReusedActivity = setTargetStackAndMoveToFrontIfNeeded(mReusedActivity);
+            if (mReusedActivity == null && ((mLaunchFlags & FLAG_ACTIVITY_CLEAR_TOP) != 0
+                    || mLaunchSingleInstance || mLaunchSingleTask)) {
+                mReusedActivity = intentActivity;
+            }
 
             if ((mStartFlags & START_FLAG_ONLY_IF_NEEDED) != 0) {
                 // We don't need to start a new activity, and the client said not to do anything
