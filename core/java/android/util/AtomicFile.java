3528c53241bb1c5486190bbc37868bcd256c945c
songjinshi <songjinshi@xiaomi.com>
2016-11-08 16:31:01 +0800

    Rename the backup file to base file when backup exists.
    Restore the backup file as base file.
    
    Change-Id: I9e2caf8ec97384f08a7b9c632ab927175ed973d2
    Signed-off-by: songjinshi <songjinshi@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/util/AtomicFile.java
+++ core/java/android/util/AtomicFile.java
@@ -95,6 +95,7 @@ public class AtomicFile {
                 }
             } else {
                 mBaseName.delete();
+                mBackupName.renameTo(mBaseName);
             }
         }
         FileOutputStream str = null;
