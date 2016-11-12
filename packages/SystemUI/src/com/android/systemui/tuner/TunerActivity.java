595f4ac2fa30638bbfd4c80d9814377335b1fc0d
Pranav Vashi <neobuddy89@gmail.com>
2016-11-12 13:06:28 +0530

    base: Clean up systemUI tuner
    * Stay aligned with CM.
    
    Change-Id: I97f3df8e5f04950c0669eabb6e7ef31f0d150422
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -33,8 +33,6 @@ public class TunerActivity extends SettingsDrawerActivity implements
 
     private static final String TAG_TUNER = "tuner";
 
-    private String mInitialTitle;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -53,13 +51,6 @@ public class TunerActivity extends SettingsDrawerActivity implements
 
             getFragmentManager().beginTransaction().replace(R.id.content_frame,
                     fragment, TAG_TUNER).commit();
-
-            mInitialTitle = String.valueOf(getActionBar().getTitle());
-
-            String extra = getIntent().getStringExtra(TAG_TUNER);
-            if (extra != null) {
-                startPreferenceScreen((PreferenceFragment)fragment, extra, false);
-            }
         }
     }
 
@@ -67,8 +58,6 @@ public class TunerActivity extends SettingsDrawerActivity implements
     public void onBackPressed() {
         if (!getFragmentManager().popBackStackImmediate()) {
             super.onBackPressed();
-        } else {
-            getActionBar().setTitle(mInitialTitle);
         }
     }
 
@@ -87,7 +76,7 @@ public class TunerActivity extends SettingsDrawerActivity implements
             Class<?> cls = Class.forName(pref.getFragment());
             Fragment fragment = (Fragment) cls.newInstance();
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
-            getActionBar().setTitle(pref.getTitle());
+            setTitle(pref.getTitle());
             transaction.replace(R.id.content_frame, fragment);
             transaction.addToBackStack("PreferenceFragment");
             transaction.commit();
@@ -125,7 +114,6 @@ public class TunerActivity extends SettingsDrawerActivity implements
             PreferenceScreen p = (PreferenceScreen) ((PreferenceFragment) getTargetFragment())
                     .getPreferenceScreen().findPreference(rootKey);
             setPreferenceScreen(p);
-            getActivity().getActionBar().setTitle(p.getTitle());
         }
     }
 }
