371fae113813ac4ac9edf30d79e5939220313f94
Pranav Vashi <neobuddy89@gmail.com>
2016-11-20 19:29:35 +0530

    systemui: Respect doze settings before starting service
    Change-Id: Ib0cdc6cd7d631b239f602688b869a8ca94585483
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeService.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeService.java
@@ -35,6 +35,7 @@ import android.os.Handler;
 import android.os.PowerManager;
 import android.os.SystemClock;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.service.dreams.DreamService;
 import android.util.Log;
 import android.view.Display;
@@ -157,7 +158,12 @@ public class DozeService extends DreamService {
     public void onDreamingStarted() {
         super.onDreamingStarted();
 
-        if (mHost == null) {
+        boolean dozeEnabled = Settings.Secure.getInt(
+                getContentResolver(), Settings.Secure.DOZE_ENABLED,
+                mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0) != 0;
+
+        if (mHost == null || !dozeEnabled) {
             finish();
             return;
         }
