e6387a236b7f163dcd15a390c47a2915309fb01c
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-11-25 21:54:23 -0600

    SystemUI: Add back the Pixel Navbar
    - Shoutout to bigrushdog for providing a decompiled form used
      to verify the smali and as a guideline for the more obsure
      smali sections. It was a big help.
    - Tagging myself here to see adoption rate. -USA-RedDragon
    
    Squashed with:
    - Fix landscape pixel animation view -USA-RedDragon
    - Cleanup pixel navbar code -BeansTown106
    - Pixel navbar: remove this. and OpaLayout references, cleanup -KreAch3R
    - Pixel navbar: allow disabling based on config -KreAch3R
    - fwb: Pixel Navbar: Hide opa when not pressed -Myself5
    - systemui: opalayout: fix up build error with latest source -mydongistiny
    - Pixel Navbar animation toggle [1/2] -BeansTown106
    - Reload Pixel Home Animation without reboot -Joe Maples
    - OpaLayout: Misc code fixes and change halo dimens - Rashed Abdel-Tawab
    - OpaLayout: Hide halo when swipe up on home enabled - mydongistiny
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4177,6 +4177,12 @@ public final class Settings {
         public static final String SCREENSHOT_SOUND = "screenshot_sound";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
