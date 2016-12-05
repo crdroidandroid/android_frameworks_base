f0d7b4aa83dbd579f1da98ccef4ca1ff92d4332e
Qiushi.Han <qiushi.han@spreadtrum.com>
2016-12-05 15:07:48 +0800

    Delay move input method windows when exiting.
    If we have a dialog with the IME shown, when leaving split-screen mode,
    the IME could not find the right window because of early called
    WMS.findDesiredInputMethodWindowIndexLocked, bad user experience.
    
    To fix this, delay calling WMS.moveInputMethodWindowsIfNeededLocked().
    
    Google issue:
    https://code.google.com/p/android/issues/detail?id=229485
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -2580,10 +2580,6 @@ public class WindowManagerService extends IWindowManager.Stub
 
         win.mRemoved = true;
 
-        if (mInputMethodTarget == win) {
-            moveInputMethodWindowsIfNeededLocked(false);
-        }
-
         if (false) {
             RuntimeException e = new RuntimeException("here");
             e.fillInStackTrace();
@@ -2597,6 +2593,10 @@ public class WindowManagerService extends IWindowManager.Stub
         }
         mPolicy.removeWindowLw(win);
         win.removeLocked();
+
+        if (mInputMethodTarget == win) {
+            moveInputMethodWindowsIfNeededLocked(false);
+        }
 
         if (DEBUG_ADD_REMOVE) Slog.v(TAG_WM, "removeWindowInnerLocked: " + win);
         mWindowMap.remove(win.mClient.asBinder());
