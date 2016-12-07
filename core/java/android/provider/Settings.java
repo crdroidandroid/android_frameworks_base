2bdfe6bcea2f111e35fb834efa71dde1baca20d5
cristianomatos <cristianobmatos@gmail.com>
2016-12-07 14:50:41 +0800

    Add Clear All Button to Recents [1/2]
    @neobuddy89: Code clean up and fixes
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4803,6 +4803,18 @@ public final class Settings {
         public static final String BLUR_QUICKSETTINGS_PERCENTAGE = "blur_quicksettings_percentage";
 
         /**
+         * Show clear all recents button
+         * @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * Location of the clear all recents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
