4c1d04584f73aef881556ab7e77414064e30beae
cristianomatos <cristianobmatos@gmail.com>
2016-12-07 14:50:41 +0800

    Add Clear All Button to Recents [1/2]
    @neobuddy89: Code clean up and fixes
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
@@ -83,6 +83,10 @@ public class RecentsConfiguration {
 
     private final Context mAppContext;
 
+    public int fabEnterAnimDuration;
+    public int fabEnterAnimDelay;
+    public int fabExitAnimDuration;
+
     public RecentsConfiguration(Context context) {
         // Load only resources that can not change after the first load either through developer
         // settings or via multi window
@@ -99,6 +103,13 @@ public class RecentsConfiguration {
         smallestWidth = ssp.getDeviceSmallestWidth();
         isLargeScreen = smallestWidth >= (int) (screenDensity * LARGE_SCREEN_MIN_DP);
         isXLargeScreen = smallestWidth >= (int) (screenDensity * XLARGE_SCREEN_MIN_DP);
+
+        fabEnterAnimDuration =
+                res.getInteger(R.integer.recents_animate_fab_enter_duration);
+        fabEnterAnimDelay =
+                res.getInteger(R.integer.recents_animate_fab_enter_delay);
+        fabExitAnimDuration =
+                res.getInteger(R.integer.recents_animate_fab_exit_duration);
     }
 
     /**
