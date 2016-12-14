acadada6cd6aed4216d8d414fe7f62dc7a70ea96
Varun Date <date.varun123@gmail.com>
2016-12-14 14:41:39 +0530

    base: Add Dynamic Navbar capability [1/2]
    @neobuddy89: Updated for Oreo
    
    Before: https://i.imgur.com/cri4liL.png
    After: https://i.imgur.com/9krQSEL.png
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4263,6 +4263,12 @@ public final class Settings {
         public static final String SLIM_RECENTS_BLACKLIST_VALUES = "slim_recents_blacklist_values";
 
         /**
+         * Dynamic Navbar Switch
+         * @hide
+         */
+        public static final String NAVBAR_DYNAMIC = "navbar_dynamic";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
