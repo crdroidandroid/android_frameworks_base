1382f29aa583a89ffdff720432a72a4140b9b09d
baek.kyoungmin <baek.kyoungmin@lge.com>
2016-12-14 09:11:12 +0900

    send cancel cmd to hal in Lockout mode.
    1. Pixel cannot fp scroll gesture in Lockout mode until user place
       his enrolled fingerprint. Because lockout mode does not send
       stop cmd to hal. This case makes polling is still alive which
       makes user can not do fp scroll.
    
    2. We might think about a solution that fp hal can count when auth fail occured.
       However, when FingerprintService gets bindercall (resetTimeout)from
       App(Included GMS), it has to send lockOut reset occured to hal.
       Otherwise, fp failCount in fp hal and FingerprintService would not be
       synchronized.
    
    Test: sending cancel & auth almost on the same time.
    Change-Id: I8a0d53d01492a6eec08150c74cab1edd0403c9a6
    Signed-off-by: Kyoungmin Baek <baek.kyoungmin@lge.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/fingerprint/AuthenticationClient.java
+++ services/core/java/com/android/server/fingerprint/AuthenticationClient.java
@@ -81,6 +81,7 @@ public abstract class AuthenticationClient extends ClientMonitor {
             // allow system-defined limit of number of attempts before giving up
             boolean inLockoutMode =  handleFailedAttempt();
             // send lockout event in case driver doesn't enforce it.
+            stop(true);
             if (inLockoutMode) {
                 try {
                     Slog.w(TAG, "Forcing lockout (fp driver code should do this!)");
