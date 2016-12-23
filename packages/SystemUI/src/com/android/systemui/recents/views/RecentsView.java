288ad66c91253a948bb9ffbd7827c7abbf84a0e7
Michael W <baddaemon87@gmail.com>
2016-12-23 13:33:45 +0100

    SystemUI: Recents: Allow closing empty view via tapping
    * Same as https://review.cyanogenmod.org/#/c/97718/ on new code base
    
    Change-Id: I7fa7e48c38db503ea152cd64dd98acbb2943f15f
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -74,6 +74,7 @@ import com.android.systemui.recents.events.activity.HideStackActionButtonEvent;
 import com.android.systemui.recents.events.activity.LaunchTaskEvent;
 import com.android.systemui.recents.events.activity.MultiWindowStateChangedEvent;
 import com.android.systemui.recents.events.activity.ShowStackActionButtonEvent;
+import com.android.systemui.recents.events.activity.ToggleRecentsEvent;
 import com.android.systemui.recents.events.ui.AllTaskViewsDismissedEvent;
 import com.android.systemui.recents.events.ui.DismissAllTaskViewsEvent;
 import com.android.systemui.recents.events.ui.DraggingInRecentsEndedEvent;
@@ -331,6 +332,12 @@ public class RecentsView extends FrameLayout {
         if (mFloatingButton != null) {
             mFloatingButton.setVisibility(View.GONE);
         }
+        setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventBus.getDefault().send(new ToggleRecentsEvent());
+            }
+        });
     }
 
     /**
@@ -346,6 +353,7 @@ public class RecentsView extends FrameLayout {
         if (mFloatingButton != null) {
             mFloatingButton.setVisibility(View.VISIBLE);
         }
+        setOnClickListener(null);
     }
 
     public void startFABanimation() {
@@ -569,7 +577,11 @@ public class RecentsView extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        return mTouchHandler.onTouchEvent(ev);
+        if (mTouchHandler.onTouchEvent(ev)) {
+            return true;
+        } else {
+            return super.onTouchEvent(ev);
+        }
     }
 
     @Override
