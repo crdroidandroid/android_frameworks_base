426d99af7428f858f355471997928374865fe51d
Pranav Vashi <neobuddy89@gmail.com>
2017-01-01 22:55:18 +0530

    Revert "Fix thread safety issue"
    This reverts commit d86058347e8160a3088720bb3ac999a3865eefac.

--- core/java/android/app/SharedPreferencesImpl.java
+++ core/java/android/app/SharedPreferencesImpl.java
@@ -46,7 +46,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ConcurrentHashMap;
+
 import libcore.io.IoUtils;
 
 final class SharedPreferencesImpl implements SharedPreferences {
@@ -61,7 +61,7 @@ final class SharedPreferencesImpl implements SharedPreferences {
     private final File mBackupFile;
     private final int mMode;
 
-    private ConcurrentHashMap<String, Object> mMap;     // guarded by 'this'
+    private Map<String, Object> mMap;     // guarded by 'this'
     private int mDiskWritesInFlight = 0;  // guarded by 'this'
     private boolean mLoaded = false;      // guarded by 'this'
     private long mStatTimestamp;          // guarded by 'this'
@@ -108,7 +108,7 @@ final class SharedPreferencesImpl implements SharedPreferences {
             Log.w(TAG, "Attempt to read preferences file " + mFile + " without permission");
         }
 
-        ConcurrentHashMap map = null;
+        Map map = null;
         StructStat stat = null;
         try {
             stat = Os.stat(mFile.getPath());
@@ -135,7 +135,7 @@ final class SharedPreferencesImpl implements SharedPreferences {
                 mStatTimestamp = stat.st_mtime;
                 mStatSize = stat.st_size;
             } else {
-                mMap = new ConcurrentHashMap<>();
+                mMap = new HashMap<>();
             }
             notifyAll();
         }
@@ -214,7 +214,7 @@ final class SharedPreferencesImpl implements SharedPreferences {
         synchronized (this) {
             awaitLoadedLocked();
             //noinspection unchecked
-            return new ConcurrentHashMap<String, Object>(mMap);
+            return new HashMap<String, Object>(mMap);
         }
     }
 
@@ -399,7 +399,7 @@ final class SharedPreferencesImpl implements SharedPreferences {
                     // in-flight write owns it.  Clone it before
                     // modifying it.
                     // noinspection unchecked
-                    mMap = new ConcurrentHashMap<String, Object>(mMap);
+                    mMap = new HashMap<String, Object>(mMap);
                 }
                 mcr.mapToWriteToDisk = mMap;
                 mDiskWritesInFlight++;
