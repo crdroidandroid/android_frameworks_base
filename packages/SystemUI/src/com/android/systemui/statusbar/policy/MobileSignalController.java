debcc262ad55bf8d1f6801396ec441a860a1a104
xyyx <xyyx@mail.ru>
2017-01-02 19:53:36 +0530

    Ability to toggle VoLTE icon in statusbar [1/2]
    Extracted by xyyx
    Adapted by neobuddy89
    
    Original commits:
    codeaurora-unofficial/platform-frameworks-base@8c968e8
    AOSPA/android_frameworks_base@4dabbbe
    
    Change-Id: Iaac49349a75eb38b325b151d320cb06e60146a63
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -49,7 +49,7 @@ import com.android.systemui.tuner.TunerServiceImpl;
 import java.io.PrintWriter;
 import java.util.BitSet;
 import java.util.Objects;
-
+import java.util.List;
 
 public class MobileSignalController extends SignalController<
         MobileSignalController.MobileState, MobileSignalController.MobileIconGroup> implements TunerService.Tunable {
@@ -340,8 +340,26 @@ public class MobileSignalController extends SignalController<
         int typeIcon = showDataIcon ? icons.mDataType : 0;
         callback.setMobileDataIndicators(statusIcon, qsIcon, typeIcon, qsTypeIcon,
                 activityIn, activityOut, dataContentDescription, description, icons.mIsWide,
-                mSubscriptionInfo.getSubscriptionId(), mCurrentState.roaming);
+                mSubscriptionInfo.getSubscriptionId(), mCurrentState.roaming, isMobileIms());
     }
+
+    private boolean isMobileIms() {
+
+        List<SubscriptionInfo> subInfos = SubscriptionManager.from(mContext)
+                        .getActiveSubscriptionInfoList();
+        if (subInfos != null) {
+            for (SubscriptionInfo subInfo: subInfos) {
+                int subId = subInfo.getSubscriptionId();
+                if (mPhone != null
+                        && mPhone.isImsRegisteredForSubscriber(subId)) {
+                    return true;
+                }
+            }
+        } else {
+            Log.e(mTag, "Invalid SubscriptionInfo");
+        }
+        return false;
+      }
 
     @Override
     protected MobileState cleanState() {
