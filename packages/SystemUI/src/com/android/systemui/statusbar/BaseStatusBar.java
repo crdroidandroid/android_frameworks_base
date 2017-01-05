4ffd3df290f5fd02e53acb8d74e3276670e519ef
dongwan0605.kim <dongwan0605.kim@lge.com>
2017-01-05 10:01:11 +0900

    Fix guts not expanding when QSPanel is expanded
    When exposing notification guts, we notified height changed event
    without expandable view(row). Then, when QSPanel is expanded,
    NotificationPanelView did not update its height and guts was not
    expanded properly due to lack of height.
    So fix it by notifying height changed event with expandable view
    when expose notification guts.
    Also, do not request animation when panel is collapsed.
    
    Test: Expand QSPanel > Long press on the collapsed notification >
          Verify guts expanded properly
    
    Change-Id: Id7abd858800036fd6ff11c003c079c84c7ea54ef
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -144,8 +144,7 @@ import static android.service.notification.NotificationListenerService.Ranking.i
 public abstract class BaseStatusBar extends SystemUI implements
         CommandQueue.Callbacks, ActivatableNotificationView.OnActivatedListener,
         ExpandableNotificationRow.ExpansionLogger, NotificationData.Environment,
-        ExpandableNotificationRow.OnExpandClickListener,
-        OnGutsClosedListener {
+        ExpandableNotificationRow.OnExpandClickListener {
     public static final String TAG = "StatusBar";
     public static final boolean DEBUG = false;
     public static final boolean MULTIUSER_DEBUG = false;
@@ -1212,7 +1211,12 @@ public abstract class BaseStatusBar extends SystemUI implements
         PackageManager pmUser = getPackageManagerForUser(mContext, sbn.getUser().getIdentifier());
         row.setTag(sbn.getPackageName());
         final NotificationGuts guts = row.getGuts();
-        guts.setClosedListener(this);
+        guts.setClosedListener((NotificationGuts g) -> {
+            if (!row.isRemoved()) {
+                mStackScroller.onHeightChanged(row, !isPanelFullyCollapsed() /* needsAnimation */);
+            }
+            mNotificationGutsExposed = null;
+        });
         final String pkg = sbn.getPackageName();
         String appname = pkg;
         Drawable pkgicon = null;
@@ -1354,7 +1358,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                         guts.setExposed(true /* exposed */,
                                 mState == StatusBarState.KEYGUARD /* needsFalsingProtection */);
                         row.closeRemoteInput();
-                        mStackScroller.onHeightChanged(null, true /* needsAnimation */);
+                        mStackScroller.onHeightChanged(row, true /* needsAnimation */);
                         mNotificationGutsExposed = guts;
                     }
                 });
@@ -1385,12 +1389,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         if (resetGear) {
             mStackScroller.resetExposedGearView(animate, true /* force */);
         }
-    }
-
-    @Override
-    public void onGutsClosed(NotificationGuts guts) {
-        mStackScroller.onHeightChanged(null, true /* needsAnimation */);
-        mNotificationGutsExposed = null;
     }
 
     @Override
