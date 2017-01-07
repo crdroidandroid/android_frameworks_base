6506a9a10a0cd499810795e988268fcd37599c81
Pranav Vashi <neobuddy89@gmail.com>
2017-01-07 20:44:11 +0530

    base: Improve secure lockscreen access with disabled QS
    * Swipe shortcuts will be hidden.
    * Statusbar pull down will be restricted even with on-screen
      notifications.
    
    Change-Id: I3c3a68f6ad4cf4d7586086269f40abc327f74990
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -73,6 +73,7 @@ import com.android.systemui.cm.LockscreenShortcutsHelper.Shortcuts;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.KeyguardAffordanceView;
 import com.android.systemui.statusbar.KeyguardIndicationController;
+import com.android.systemui.statusbar.phone.NotificationPanelView;
 import com.android.systemui.statusbar.policy.AccessibilityController;
 import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.PreviewInflater;
@@ -241,28 +242,29 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     }
 
     private void updateCustomShortcuts() {
-        updateLeftAffordanceIcon();
-        updateRightAffordanceIcon();
-        inflateCameraPreview();
+        updateLeftAffordance();
+        updateRightAffordance();
     }
 
     private void updateRightAffordanceIcon() {
         Drawable drawable;
         String contentDescription;
         boolean shouldGrayScale = false;
+        boolean visible = mUserSetupComplete && !NotificationPanelView.isQSEventBlocked();
         if (isTargetCustom(Shortcuts.RIGHT_SHORTCUT)) {
             drawable = mShortcutHelper.getDrawableForTarget(Shortcuts.RIGHT_SHORTCUT);
             shouldGrayScale = true;
             contentDescription = mShortcutHelper.getFriendlyNameForUri(Shortcuts.RIGHT_SHORTCUT);
+            visible |= !mShortcutHelper.isTargetEmpty(Shortcuts.RIGHT_SHORTCUT);
         } else {
             drawable = mContext.getDrawable(R.drawable.ic_camera_alt_24dp);
             contentDescription = mContext.getString(R.string.accessibility_camera_button);
         }
+        mCameraImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
         mCameraImageView.setImageDrawable(drawable);
         mCameraImageView.setContentDescription(contentDescription);
         mCameraImageView.setDefaultFilter(shouldGrayScale ? mGrayScaleFilter : null);
         updateCameraVisibility();
-        updateLeftButtonVisibility();
     }
 
     private void initAccessibility() {
@@ -332,8 +334,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
     public void setUserSetupComplete(boolean userSetupComplete) {
         mUserSetupComplete = userSetupComplete;
-        updateCameraVisibility();
-        updateLeftButtonVisibility();
+        updateRightAffordanceIcon();
         updateLeftAffordanceIcon();
     }
 
@@ -358,7 +359,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         if (mLeftAffordanceView == null) {
             return;
         }
-        boolean visible = mUserSetupComplete;
+        boolean visible = mUserSetupComplete && !NotificationPanelView.isQSEventBlocked();
         if (visible) {
             if (isTargetCustom(Shortcuts.LEFT_SHORTCUT)) {
                 visible = !mShortcutHelper.isTargetEmpty(Shortcuts.LEFT_SHORTCUT);
@@ -374,7 +375,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             // Things are not set up yet; reply hazy, ask again later
             return;
         }
-        boolean visible = mUserSetupComplete;
+        boolean visible = mUserSetupComplete && !NotificationPanelView.isQSEventBlocked();
         if (visible) {
             if (isTargetCustom(Shortcuts.RIGHT_SHORTCUT)) {
                 visible = !mShortcutHelper.isTargetEmpty(Shortcuts.RIGHT_SHORTCUT);
@@ -391,7 +392,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         Drawable drawable;
         String contentDescription;
         boolean shouldGrayScale = false;
-        boolean visible = mUserSetupComplete;
+        boolean visible = mUserSetupComplete && !NotificationPanelView.isQSEventBlocked();
         if (mShortcutHelper.isTargetCustom(Shortcuts.LEFT_SHORTCUT)) {
             drawable = mShortcutHelper.getDrawableForTarget(Shortcuts.LEFT_SHORTCUT);
             shouldGrayScale = true;
@@ -827,9 +828,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
         @Override
         public void onUserUnlocked() {
-            inflateCameraPreview();
-            updateCameraVisibility();
             updateLeftAffordance();
+            updateRightAffordance();
         }
     };
 
@@ -848,9 +848,14 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         updateLeftPreview();
     }
 
+    public void updateRightAffordance() {
+        updateRightAffordanceIcon();
+        inflateCameraPreview();
+    }
+
     public void onKeyguardShowingChanged() {
         updateLeftAffordance();
-        inflateCameraPreview();
+        updateRightAffordance();
     }
 
     private String getIndexHint(LockscreenShortcutsHelper.Shortcuts shortcut) {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -181,7 +181,7 @@ public class NotificationPanelView extends PanelView implements
     private boolean mQsExpanded;
     private boolean mQsExpandedWhenExpandingStarted;
     private boolean mQsFullyExpanded;
-    private boolean mKeyguardOrShadeShowing;
+    public static boolean mKeyguardOrShadeShowing;
     public static boolean mKeyguardShowing;
     public static boolean mHeadsUpShowing;
     private boolean mDozing;
@@ -258,9 +258,9 @@ public class NotificationPanelView extends PanelView implements
     private boolean mLaunchingAffordance;
     private FalsingManager mFalsingManager;
     private String mLastCameraLaunchSource = KeyguardBottomAreaView.CAMERA_LAUNCH_SOURCE_AFFORDANCE;
-    private LockPatternUtils mLockPatternUtils;
+    public static LockPatternUtils mLockPatternUtils;
 
-    private boolean mStatusBarAllowedOnSecureKeyguard;
+    public static boolean mStatusBarAllowedOnSecureKeyguard = true;
 
     private Runnable mHeadsUpExistenceChangedRunnable = new Runnable() {
         @Override
@@ -864,7 +864,7 @@ public class NotificationPanelView extends PanelView implements
         requestLayout();
     }
 
-    private boolean isQSEventBlocked() {
+    public static boolean isQSEventBlocked() {
         return mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser())
             && !mStatusBarAllowedOnSecureKeyguard && mKeyguardOrShadeShowing;
     }
@@ -2880,7 +2880,7 @@ public class NotificationPanelView extends PanelView implements
                 break;
             case LOCK_ENABLE_QS:
                 mStatusBarAllowedOnSecureKeyguard =
-                        newValue == null ? 1 : Integer.parseInt(newValue);
+                        newValue == null || Integer.parseInt(newValue) == 1;
                 break;
             default:
                 break;
