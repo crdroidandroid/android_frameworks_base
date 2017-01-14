2777786321c3d6f6335288d8ec0c0d25a474df2a
ezio84 <brabus84@gmail.com>
2017-01-14 13:57:25 +0100

    DUI: Fix navbar edit logspam
    01-14 13:51:31.585  5276  6478 E ActivityManager: Sending non-protected broadcast intent_navbar_edit from system 23657:com.android.settings/1000 pkg com.android.settings
    01-14 13:51:31.585  5276  6478 E ActivityManager: java.lang.Throwable
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.checkBroadcastFromSystem(ActivityManagerService.java:17868)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.broadcastIntentLocked(ActivityManagerService.java:18349)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.broadcastIntent(ActivityManagerService.java:18531)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at android.app.ActivityManagerNative.onTransact(ActivityManagerNative.java:500)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:2799)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at android.os.Binder.execTransact(Binder.java:565)
    
    thanks @KreAch3R
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4594,7 +4594,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                         || actionName.startsWith("com.android.server.sip.SipWakeupTimer")
                         || actionName.startsWith("com.android.internal.telephony.data-reconnect")
                         || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
-                        || actionName.startsWith("eu.chainfire.supersu.NativeAccess")) {
+                        || actionName.startsWith("eu.chainfire.supersu.NativeAccess")
+                        || actionName.startsWith("intent_navbar_edit")) {
                     return true;
                 }
             }
