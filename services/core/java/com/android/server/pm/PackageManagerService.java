6ea012487baa1cb12dc056a633c3404e10f1c8eb
bigrushdog <randall.rushing@gmail.com>
2017-01-14 23:33:38 -0800

    Add a protected broadcast for Masquerade events
    Parse extras to get event details. Docs will be available
    in Masquerade source code
    
    Change-Id: I24ca3d11438bb830ce97af8b0e935c0700b394e6
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4601,7 +4601,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                         || actionName.startsWith("com.android.internal.telephony.data-reconnect")
                         || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
                         || actionName.startsWith("eu.chainfire.supersu.NativeAccess")
-                        || actionName.startsWith("intent_navbar_edit")) {
+                        || actionName.startsWith("intent_navbar_edit")
+                        || actionName.startsWith("masquerade.substratum.STATUS_CHANGED")) {
                     return true;
                 }
             }
