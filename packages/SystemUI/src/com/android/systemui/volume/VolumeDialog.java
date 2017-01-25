2a8d03175732ffe4345556ace9b2e151af5f2cc2
maxwen <max.weninger@gmail.com>
2017-01-25 19:03:43 +0100

    base: volume dialog: hide headers on all streams if requested
    also on dynamic ones e.g. cast
    
    Change-Id: I79e116345dfce9fde029def86aa62b391f912e8d
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
@@ -876,8 +876,7 @@ public class VolumeDialog implements TunerService.Tunable {
     }
 
     private void updateVolumeRowHeaderVisibleH(VolumeRow row) {
-        final boolean dynamic = row.ss != null && row.ss.dynamic;
-        final boolean showHeaders = mExpanded && (mShowHeaders || dynamic);
+        final boolean showHeaders = mExpanded && mShowHeaders;
         if (row.cachedShowHeaders != showHeaders) {
             row.cachedShowHeaders = showHeaders;
             Util.setVisOrGone(row.header, showHeaders);
