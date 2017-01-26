19dda6b5f8740467d1b26db9f2e5b47b43de28bf
Pranav Vashi <neobuddy89@gmail.com>
2017-01-27 00:45:38 +0530

    Add more custom ambient display settings [1/2]
    Change-Id: I6f583723415b693f31e964ced6fed665703d78fc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4645,6 +4645,18 @@ public final class Settings {
         public static final String DOZE_SCREEN_BRIGHTNESS = "doze_screen_brightness";
 
         /**
+         * Vibrate on pulsing while pickup
+         * @hide
+         */
+        public static final String DOZE_VIBRATE_ON_PICKUP = "doze_vibrate_on_pickup";
+
+        /**
+         * Check proximity sensor before pulsing
+         * @hide
+         */
+        public static final String DOZE_PROXIMITY_CHECK_BEFORE_PULSE = "doze_check_proximity_before_pulse";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
