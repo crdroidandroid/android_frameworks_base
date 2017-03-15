a4b446c8069fa2a6c000a23b41f61e429f122732
Pranav Vashi <neobuddy89@gmail.com>
2017-03-15 07:36:19 +0530

    SystemUI: Make roaming indicator optional [1/2]
    * Roaming indicator on signal icon is useless most of the times
      since a carrier might not charge differently to user if in same
      country.
    * So, allow user to hide roaming indicator.
    * This commit will only allow to hide indicator not roaming warnings.
    
    [someone5678]
    * Adapt to SystemUI mobile connections pipeline
    
    Change-Id: I5d103555459a5e771b072a98258b8b14f6fd8479
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6014,6 +6014,12 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Whether to display roaming indicator for roaming signal connection
+         * @hide
+         */
+        public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
