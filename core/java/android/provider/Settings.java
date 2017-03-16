874bc7a786ea70f2c82ca1e15c45968930ec0bc8
maxwen <max.weninger@gmail.com>
2017-03-16 20:50:58 +0800

    Allow disabling QS access on secured lockscreen [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13404,6 +13404,12 @@ public final class Settings {
         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
         /**
+         * This preference enables showing the quick settings panel on LockScreen.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_QS = "lockscreen_enable_qs";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
