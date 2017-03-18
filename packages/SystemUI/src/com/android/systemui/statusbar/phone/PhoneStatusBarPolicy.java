f86d50545b9fa16153733b56939bf20150e9b8c2
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    Add ability to toggle bluetooth battery level [1/2]
    Change-Id: I1b7dbc58c9db25261292dae09491c2318ed87df5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -87,6 +87,8 @@ import com.android.systemui.statusbar.policy.RotationLockController;
 import com.android.systemui.statusbar.policy.RotationLockController.RotationLockControllerCallback;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.ZenModeController;
+import com.android.systemui.tuner.TunerService;
+import com.android.systemui.tuner.TunerServiceImpl;
 import com.android.systemui.util.NotificationChannels;
 
 import java.util.List;
@@ -99,7 +101,7 @@ import java.util.Locale;
  */
 public class PhoneStatusBarPolicy implements Callback, Callbacks,
         RotationLockControllerCallback, Listener, LocationChangeCallback,
-        ZenModeController.Callback, DeviceProvisionedListener, KeyguardMonitor.Callback {
+        ZenModeController.Callback, DeviceProvisionedListener, KeyguardMonitor.Callback, TunerService.Tunable {
     private static final String TAG = "PhoneStatusBarPolicy";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
 
@@ -150,6 +152,11 @@ public class PhoneStatusBarPolicy implements Callback, Callbacks,
 
     private BluetoothController mBluetooth;
     private AlarmManager.AlarmClockInfo mNextAlarm;
+
+    private boolean mShowBluetoothBattery;
+
+    private static final String BLUETOOTH_SHOW_BATTERY =
+            "system:" + Settings.System.BLUETOOTH_SHOW_BATTERY;
 
     public PhoneStatusBarPolicy(Context context, StatusBarIconController iconController) {
         mContext = context;
@@ -264,6 +271,22 @@ public class PhoneStatusBarPolicy implements Callback, Callbacks,
             mDockedStackExists = exists;
             updateForegroundInstantApps();
         });
+
+        Dependency.get(TunerService.class).addTunable(this,
+                BLUETOOTH_SHOW_BATTERY);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case BLUETOOTH_SHOW_BATTERY:
+                mShowBluetoothBattery =
+                        newValue == null || Integer.parseInt(newValue) != 0;
+                updateBluetooth();
+                break;
+            default:
+                break;
+        }
     }
 
     public void destroy() {
@@ -438,7 +461,9 @@ public class PhoneStatusBarPolicy implements Callback, Callbacks,
         if (mBluetooth != null) {
             if (mBluetooth.isBluetoothConnected()) {
                 int batteryLevel = mBluetooth.getConnectedDevices().get(0).getBatteryLevel();
-                if (batteryLevel >= 88) {
+                if (!mShowBluetoothBattery) {
+                    iconId = R.drawable.stat_sys_data_bluetooth_connected;
+                } else if (batteryLevel >= 88) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_5;
                 } else if (batteryLevel >= 63) {
                     iconId = R.drawable.stat_sys_data_bluetooth_connected_battery_4;
