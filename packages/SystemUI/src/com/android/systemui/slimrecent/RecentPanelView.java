5f39b26526cfde0a0650113ec2300cff08ea10a9
ezio84 <brabus84@gmail.com>
2017-03-23 13:54:47 +0100

    Slim Recents: more fixes, cleanup and improvements
    -code cleanup
    -set max cache size to 1/6 max maxMemory
    -we use drawables for app icons, let's handle drawables also into the cache
    without bitmap to drawable conversions
    -add back mCardAdapter.notifyItemChanged(index) for app icon and thumbs loading,
    (those drawable are loaded in separated async tasks so sometimes they are ready
    after the card has been shown).
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -1159,18 +1159,18 @@ public class RecentPanelView {
 
             final ExpandableCard ec = card;
 
-            final Bitmap appIcon =
+            final Drawable appIcon =
                     CacheController.getInstance(mContext).getBitmapFromMemCache(task.identifier);
             if (appIcon != null) {
-                ec.appIcon = new BitmapDrawable(mContext.getResources(), appIcon);
-                //mCardAdapter.notifyItemChanged(index);
+                ec.appIcon = appIcon;
+                mCardAdapter.notifyItemChanged(index);
             } else {
                 AppIconLoader.getInstance(mContext).loadAppIcon(task.resolveInfo,
                         task.identifier, new AppIconLoader.IconCallback() {
                             @Override
                             public void onDrawableLoaded(Drawable drawable) {
                                 ec.appIcon = drawable;
-                                //mCardAdapter.notifyItemChanged(index);
+                                mCardAdapter.notifyItemChanged(index);
                             }
                 }, mScaleFactor);
             }
@@ -1178,7 +1178,7 @@ public class RecentPanelView {
                 @Override
                 public void onBitmapLoaded(Bitmap bitmap) {
                     ec.screenshot = bitmap;
-                    //mCardAdapter.notifyItemChanged(index);
+                    mCardAdapter.notifyItemChanged(index);
                 }
             }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, task.persistentTaskId);
             card.cardClickListener = new View.OnClickListener() {
