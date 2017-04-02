dcc328c915af65245a10f1c4e56413d0d365fa59
Pranav Vashi <neobuddy89@gmail.com>
2017-04-02 11:59:34 +0530

    base: Add permissions for OmniJaws
    Change-Id: Ib6d7c49f784ab600d9e16d34f61be986dd2b6b7e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -703,6 +703,13 @@ final class DefaultPermissionGrantPolicy {
                 grantRuntimePermissionsLPw(fmpackage, STORAGE_PERMISSIONS, userId);
             }
 
+            // OmniJaws
+            PackageParser.Package omnijawspackage = getSystemPackageLPr(
+                    "org.omnirom.omnijaws");
+            if (omnijawspackage != null && doesPackageSupportRuntimePermissions(omnijawspackage)) {
+                grantRuntimePermissionsLPw(omnijawspackage, LOCATION_PERMISSIONS, userId);
+            }
+
             mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
         }
     }
