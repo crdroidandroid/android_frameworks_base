c6950720aa8f1f930f1986ee954f91554d17097a
Pranav Vashi <neobuddy89@gmail.com>
2017-04-08 20:45:42 +0530

    Allow tuning ambient display with sensors [1/3]
    Squashed:
       From: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
       Date: Fri, 7 Feb 2020 23:39:42 +0200
       Subject: [PATCH] Allow devices to set proximity sensor type for ambient  display [1/2]
    
       true - wake-up
       false - non wake-up
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -52,6 +52,9 @@ public class AmbientDisplayConfiguration {
                 || wakeLockScreenGestureEnabled(user)
                 || wakeDisplayGestureEnabled(user)
                 || pickupGestureEnabled(user)
+                || tiltGestureEnabled(user)
+                || handwaveGestureEnabled(user)
+                || pocketGestureEnabled(user)
                 || tapGestureEnabled(user)
                 || doubleTapGestureEnabled(user)
                 || quickPickupSensorEnabled(user)
@@ -78,6 +81,35 @@ public class AmbientDisplayConfiguration {
     /** {@hide} */
     public boolean dozePickupSensorAvailable() {
         return mContext.getResources().getBoolean(R.bool.config_dozePulsePickup);
+    }
+
+
+    /** {@hide} */
+    public boolean tiltGestureEnabled(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_TILT_GESTURE, user)
+                && dozeTiltSensorAvailable();
+    }
+
+    /** {@hide} */
+    public boolean dozeTiltSensorAvailable() {
+        return mContext.getResources().getBoolean(R.bool.config_dozePulseTilt);
+    }
+
+    /** {@hide} */
+    public boolean handwaveGestureEnabled(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_HANDWAVE_GESTURE, user)
+                && dozeProximitySensorAvailable();
+    }
+
+    /** {@hide} */
+    public boolean pocketGestureEnabled(int user) {
+        return boolSettingDefaultOff(Settings.Secure.DOZE_POCKET_GESTURE, user)
+                && dozeProximitySensorAvailable();
+    }
+
+    /** {@hide} */
+    public boolean dozeProximitySensorAvailable() {
+        return mContext.getResources().getBoolean(R.bool.config_dozePulseProximity);
     }
 
     /** {@hide} */
