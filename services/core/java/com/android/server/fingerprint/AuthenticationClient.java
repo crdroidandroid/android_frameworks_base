209d32f24743994f034527a6dbd24cfa4e712bc2
Pranav Vashi <neobuddy89@gmail.com>
2017-04-28 07:09:58 +0530

    Revert "send cancel cmd to hal in Lockout mode."
    * Breaks fingerprint authentication. Fix suggested by by @rohitjaiswal
    
    This reverts commit 161cf03a7cfb7710e98cf400212a9a12a7291688.

--- services/core/java/com/android/server/fingerprint/AuthenticationClient.java
+++ services/core/java/com/android/server/fingerprint/AuthenticationClient.java
@@ -81,7 +81,6 @@ public abstract class AuthenticationClient extends ClientMonitor {
             // allow system-defined limit of number of attempts before giving up
             boolean inLockoutMode =  handleFailedAttempt();
             // send lockout event in case driver doesn't enforce it.
-            stop(true);
             if (inLockoutMode) {
                 try {
                     Slog.w(TAG, "Forcing lockout (fp driver code should do this!)");
