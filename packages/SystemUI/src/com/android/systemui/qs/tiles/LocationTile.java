8d6b6594d0406d1b1bec268b4f321188c89c91d1
Pranav Vashi <neobuddy89@gmail.com>
2017-05-01 13:44:11 +0530

    Revert "Location tile: add high accuracy state icon"
    This reverts commit 70e8e2af25d227a70991e3ec4d208a90420876d4.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
@@ -188,7 +188,7 @@ public class LocationTile extends QSTile<QSTile.State> {
             case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
                 state.contentDescription = mContext.getString(R.string.accessibility_quick_settings_location_high_accuracy);
                 state.label = mContext.getString(R.string.quick_settings_location_high_accuracy_label);
-                state.icon = ResourceIcon.get(R.drawable.ic_qs_location_high_accuracy);
+                state.icon = mEnable;
                 break;
             default:
                 state.contentDescription = mContext.getString(R.string.accessibility_quick_settings_location_off);
