0f303c7a008e5257feb5484f3719ce000be60184
Pranav Vashi <neobuddy89@gmail.com>
2017-05-14 08:55:06 +0530

    Custom statusbar logo customizations [1/2]
    * Original placement logic and custom logo patches
      by LorDClockaN and varund7726 respectively.
    * Refresh and resize various icons.
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 7 Jul 2024 18:03:28 +0530
        Subject: SystemUI: Fix multiuser usage for custom logo
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -26,6 +26,7 @@ import androidx.annotation.NonNull;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.widget.ViewClippingUtil;
+import com.android.systemui.crdroid.logo.LogoImage;
 import com.android.systemui.dagger.qualifiers.RootView;
 import com.android.systemui.flags.FeatureFlagsClassic;
 import com.android.systemui.plugins.DarkIconDispatcher;
@@ -94,6 +95,8 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
 
     private final Optional<View> mOperatorNameViewOptional;
 
+    private final LogoImage mLeftLogo;
+
     @VisibleForTesting
     float mExpandedHeight;
     @VisibleForTesting
@@ -154,6 +157,7 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
         mOperatorNameViewOptional = operatorNameViewOptional;
         mDarkIconDispatcher = darkIconDispatcher;
         mClockController = new ClockController(statusBarView.getContext(), statusBarView);
+        mLeftLogo = statusBarView.findViewById(R.id.statusbar_logo);
 
         mView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
@@ -277,7 +281,11 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
                     hide(clockView, View.INVISIBLE);
                 }
                 mOperatorNameViewOptional.ifPresent(view -> hide(view, View.INVISIBLE));
+                if (mLeftLogo.getVisibility() != View.GONE)
+                    mLeftLogo.setVisibility(View.INVISIBLE);
             } else {
+                if (mLeftLogo.getVisibility() != View.GONE)
+                    mLeftLogo.setVisibility(View.VISIBLE);
                 if (isClock) {
                     show(clockView);
                 }
