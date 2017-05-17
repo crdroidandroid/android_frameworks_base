d4361877a626eee51c78a66aacce1fb30f130dc4
Ken Narita <Ken.Narita@sony.com>
2017-05-17 16:12:40 +0900

    media: Parse Exif OffsetTime in MediaScanner
    MediaScanner parses Exif OffsetTime value to use UTC time
    for DATE_TAKEN value.
    
    Bug: 62856173
    Change-Id: I797d6095600516bea3f1d44bcc64daf00887d03d
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/java/android/media/MediaScanner.java
+++ media/java/android/media/MediaScanner.java
@@ -959,17 +959,25 @@ public class MediaScanner implements AutoCloseable {
                         values.put(Images.Media.LONGITUDE, latlng[1]);
                     }
 
-                    long time = exif.getGpsDateTime();
-                    if (time != -1) {
-                        values.put(Images.Media.DATE_TAKEN, time);
+                    long[] datetime = exif.getDateTime();
+
+                    long localtime = datetime[0];
+                    long utctime = datetime[1];
+                    if (utctime != -1) {
+                        values.put(Images.Media.DATE_TAKEN, utctime);
                     } else {
-                        // If no time zone information is available, we should consider using
-                        // EXIF local time as taken time if the difference between file time
-                        // and EXIF local time is not less than 1 Day, otherwise MediaProvider
-                        // will use file time as taken time.
-                        time = exif.getDateTime();
-                        if (time != -1 && Math.abs(mLastModified * 1000 - time) >= 86400000) {
-                            values.put(Images.Media.DATE_TAKEN, time);
+                        utctime = exif.getGpsDateTime();
+                        if (utctime != -1) {
+                            values.put(Images.Media.DATE_TAKEN, utctime);
+                        } else {
+                            // If no time zone information is available, we should consider using
+                            // EXIF local time as taken time if the difference between file time
+                            // and EXIF local time is not less than 1 Day, otherwise MediaProvider
+                            // will use file time as taken time.
+                            if (localtime != -1
+                                    && Math.abs(mLastModified * 1000 - localtime) >= 86400000) {
+                                values.put(Images.Media.DATE_TAKEN, localtime);
+                            }
                         }
                     }
 
