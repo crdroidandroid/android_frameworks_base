9140e6bd659a44272dcf3ae1c7d31a0c474e2880
ezio84 <brabus84@gmail.com>
2017-06-05 14:11:04 +0200

    Slim Recents: allow to disable round corners for cards [1/3]
    the user might prefer square cards
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4642,6 +4642,12 @@ public final class Settings {
         public static final String SLIM_RECENTS_ICON_PACK = "slim_recents_icon_pack";
 
         /**
+         * Wheter to use corner radius for Slim Recents cards
+         * @hide
+         */
+        public static final String SLIM_RECENTS_CORNER_RADIUS = "slim_recents_corner_radius";
+
+        /**
          * Dynamic Navbar Switch
          * @hide
          */
