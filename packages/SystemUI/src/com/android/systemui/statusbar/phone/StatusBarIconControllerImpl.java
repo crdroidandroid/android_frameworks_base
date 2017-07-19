8a249fd6dfb52193192e0fb051946cd933055774
SagarMakhar <sagarmakhar@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Introduce dynamic VoLTE & VoWiFi icons
    @neobuddy89:
    * Clean up unrequired derps. Adapted for A14.
    * Disabled by default.
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I374d8630e766a9508df182974748441fe340e6e9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconControllerImpl.java
@@ -38,6 +38,7 @@ import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.dump.DumpManager;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.StatusIconDisplayable;
+import com.android.systemui.statusbar.connectivity.ImsIconState;
 import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy.BluetoothIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.pipeline.StatusBarPipelineFlags;
@@ -173,6 +174,23 @@ public class StatusBarIconControllerImpl implements Tunable,
             for (StatusBarIconHolder holder : iconsForSlot) {
                 setIcon(item.getName(), holder);
             }
+        }
+    }
+
+    @Override
+    public void setImsIcon(String slot, ImsIconState state) {
+        if (state == null) {
+            removeIcon(slot, 0);
+            return;
+        }
+
+        StatusBarIconHolder holder = mStatusBarIconList.getIconHolder(slot, 0);
+        if (holder == null) {
+            holder = StatusBarIconHolder.fromImsIconState(state);
+            setIcon(slot, holder);
+        } else {
+            holder.setImsState(state);
+            handleSet(slot, holder);
         }
     }
 
