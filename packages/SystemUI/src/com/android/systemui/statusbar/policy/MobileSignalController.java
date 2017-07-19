9348dc76d14389ddda79ec62eae1ae43d76fd117
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    Allow using 4G icon instead LTE [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -76,9 +76,12 @@ public class MobileSignalController extends SignalController<
     private Config mConfig;
 
     private boolean mRoamingIconAllowed;
+    private boolean mShow4gForLte;
 
     private static final String ROAMING_INDICATOR_ICON =
             "system:" + Settings.System.ROAMING_INDICATOR_ICON;
+    private static final String SHOW_FOURG_ICON =
+            "system:" + Settings.System.SHOW_FOURG_ICON;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -118,6 +121,7 @@ public class MobileSignalController extends SignalController<
         };
 
         Dependency.get(TunerService.class).addTunable(this, ROAMING_INDICATOR_ICON);
+        Dependency.get(TunerService.class).addTunable(this, SHOW_FOURG_ICON);
     }
 
     @Override
@@ -127,6 +131,11 @@ public class MobileSignalController extends SignalController<
                      mRoamingIconAllowed =
                         newValue == null || Integer.parseInt(newValue) != 0;
                      updateTelephony();
+                break;
+            case SHOW_FOURG_ICON:
+                     mShow4gForLte =
+                        newValue != null && Integer.parseInt(newValue) != 0;
+                     mapIconSets();
                 break;
             default:
                 break;
@@ -238,7 +247,7 @@ public class MobileSignalController extends SignalController<
         mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_HSPA, hGroup);
         mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_HSPAP, hPlusGroup);
 
-        if (mConfig.show4gForLte) {
+        if (mShow4gForLte) {
             mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_LTE, TelephonyIcons.FOUR_G);
             if (mConfig.hideLtePlus) {
                 mNetworkToIconLookup.put(TelephonyManager.NETWORK_TYPE_LTE_CA,
