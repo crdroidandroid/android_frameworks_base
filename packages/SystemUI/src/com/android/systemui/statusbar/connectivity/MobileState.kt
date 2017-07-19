241b3e897b3f782f36dd7abf0c219737a7e83b41
SagarMakhar <sagarmakhar@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Introduce dynamic VoLTE & VoWiFi icons
    @neobuddy89: Clean up unrequired derps. Adapted for A13 QPR1.
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileState.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileState.kt
@@ -41,7 +41,10 @@ internal class MobileState(
     @JvmField var roaming: Boolean = false,
     @JvmField var dataState: Int = TelephonyManager.DATA_DISCONNECTED,
     // Tracks the on/off state of the defaultDataSubscription
-    @JvmField var defaultDataOff: Boolean = false
+    @JvmField var defaultDataOff: Boolean = false,
+    @JvmField var imsRegistered: Boolean = false,
+    @JvmField var voiceCapable: Boolean = false,
+    @JvmField var videoCapable: Boolean = false
 ) : ConnectivityState() {
 
     @JvmField var telephonyDisplayInfo = TelephonyDisplayInfo(TelephonyManager.NETWORK_TYPE_UNKNOWN,
@@ -86,6 +89,9 @@ internal class MobileState(
         roaming = o.roaming
         dataState = o.dataState
         defaultDataOff = o.defaultDataOff
+        imsRegistered = o.imsRegistered;
+        voiceCapable = o.voiceCapable;
+        videoCapable = o.videoCapable;
 
         telephonyDisplayInfo = o.telephonyDisplayInfo
         serviceState = o.serviceState
@@ -125,6 +131,10 @@ internal class MobileState(
         return serviceState != null && serviceState!!.roaming
     }
 
+    fun getDataNetworkType(): Int {
+        return serviceState?.getDataNetworkType() ?: 0
+    }
+
     fun setFromMobileStatus(mobileStatus: MobileStatus) {
         activityIn = mobileStatus.activityIn
         activityOut = mobileStatus.activityOut
@@ -151,6 +161,9 @@ internal class MobileState(
         builder.append("userSetup=$userSetup,")
         builder.append("dataState=$dataState,")
         builder.append("defaultDataOff=$defaultDataOff,")
+        builder.append("imsRegistered=$imsRegistered,")
+        builder.append("voiceCapable=$voiceCapable,")
+        builder.append("videoCapable=$videoCapable,")
 
         // Computed properties
         builder.append("showQuickSettingsRatIcon=${showQuickSettingsRatIcon()},")
@@ -227,6 +240,9 @@ internal class MobileState(
         if (roaming != other.roaming) return false
         if (dataState != other.dataState) return false
         if (defaultDataOff != other.defaultDataOff) return false
+        if (imsRegistered != other.imsRegistered) return false
+        if (voiceCapable != other.voiceCapable) return false
+        if (videoCapable != other.videoCapable) return false
         if (telephonyDisplayInfo != other.telephonyDisplayInfo) return false
         if (serviceState != other.serviceState) return false
         if (signalStrength != other.signalStrength) return false
