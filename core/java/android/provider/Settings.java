955e4ae08df188cab0e1a00962cc7865fe48a8bc
Ivan Iskandar <ivan@prjkt.io>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>
    Signed-off-by: Nicholas Chum <nicholas@prjkt.io>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8479,6 +8479,13 @@ public final class Settings {
         public static final String EDGE_GESTURES_BACK_SCREEN_PERCENT = "edge_gestures_back_screen_percent";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
