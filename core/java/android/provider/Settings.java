bf8803348ab577085076079921d3e44407578841
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    FWB: Sensor block per-package switch (1/2)
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6791,6 +6791,13 @@ public final class Settings {
         public static final String FP_ERROR_VIBRATE = "fp_error_vibrate";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        @Readable
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
