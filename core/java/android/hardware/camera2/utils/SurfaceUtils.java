2c5692e95e9946abfd6f09f63c9dc22a94590cb1
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2017-08-08 16:27:19 +0530

    camera: Skip HFR checks for privileged apps.
    Issue: HAL3 supports HFR only if its greater than or equal
           to 120fps.
    
    Fix: Skip the checks to allow HFR 60 and 90 fps in HAL3 for
         specific apps.
         The following property need to be set to allow these
         HFR configurations :
         adb shell setprop persist.camera.privapp.list <pack1,pack2,etc>
    
    Change-Id: I5f3bc94bea60dbe03284de39cd4280f67df8b015
    CRs-Fixed: 2258892
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/camera2/utils/SurfaceUtils.java
+++ core/java/android/hardware/camera2/utils/SurfaceUtils.java
@@ -30,6 +30,10 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import android.app.ActivityThread;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+
 /**
  * Various Surface utilities.
  */
@@ -182,7 +186,13 @@ public class SurfaceUtils {
                     + " the size must be 1 or 2");
         }
 
+        if (isPrivilegedApp()) {
+            //skip checks for privileged apps
+            return;
+        }
+
         List<Size> highSpeedSizes = null;
+
         if (fpsRange == null) {
             highSpeedSizes = Arrays.asList(config.getHighSpeedVideoSizes());
         } else {
@@ -234,4 +244,20 @@ public class SurfaceUtils {
         }
     }
 
+    private static boolean isPrivilegedApp() {
+        String packageName = ActivityThread.currentOpPackageName();
+        String packageList = SystemProperties.get("persist.camera.privapp.list");
+
+        if (packageList.length() > 0) {
+            TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+            splitter.setString(packageList);
+            for (String str : splitter) {
+                if (packageName.equals(str)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
