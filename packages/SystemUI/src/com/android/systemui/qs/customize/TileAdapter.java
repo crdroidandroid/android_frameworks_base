1b2d2cfff47c77a9c4d8decc004189e5d38bf9f0
Andrzej Ressel <jereksel@gmail.com>
2017-08-09 00:11:16 +0200

    base: SystemUI: Allow user to add/remove QS with one click
    Change-Id: I9250fe5fd6a3edf567f1f4d30fd86838f5ef4610
    
    Restore one-click to add/remove tiles after r20 merge
    
    Google now added the possibility to double click to move tiles
    for accessibility purposes but i actually prefer the oneclick way
    
    [jhonboy121]: use a lambda expression for click listener
    
    Change-Id: Ia44fc4e2ddc77c9cf918c2b8fea38a4e9a3c8252
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -109,8 +109,8 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
     private int mAccessibilityAction = ACTION_NONE;
     private int mAccessibilityFromIndex;
     private final UiEventLogger mUiEventLogger;
-    private final AccessibilityDelegateCompat mAccessibilityDelegate;
-    @Nullable
+    //@Nullable
+    //private final AccessibilityDelegateCompat mAccessibilityDelegate;
     private RecyclerView mRecyclerView;
     private int mNumColumns;
 
@@ -127,7 +127,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         mMarginDecoration = new MarginTileDecoration();
         mMinNumTiles = context.getResources().getInteger(R.integer.quick_settings_min_num_tiles);
         mNumColumns = context.getResources().getInteger(NUM_COLUMNS_ID);
-        mAccessibilityDelegate = new TileAdapterDelegate();
+        //mAccessibilityDelegate = new TileAdapterDelegate();
         mSizeLookup.setSpanIndexCacheEnabled(true);
     }
 
@@ -383,13 +383,24 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         // Don't show the side view for third party tiles, as we don't have the actual state.
         tileView.setShowSideView(position < mEditIndex || info.isSystem);
         holder.mTileView.setSelected(true);
-        holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+        /*holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
         holder.mTileView.setClickable(true);
         holder.mTileView.setOnClickListener(null);
         holder.mTileView.setFocusable(true);
-        holder.mTileView.setFocusableInTouchMode(true);
+        holder.mTileView.setFocusableInTouchMode(true);*/
 
-        if (mAccessibilityAction != ACTION_NONE) {
+        holder.mTileView.setOnClickListener(v -> {
+            final int layoutPosition = holder.getLayoutPosition();
+            if (layoutPosition < mEditIndex) {
+                if (canRemoveTiles()) {
+                    move(layoutPosition, mEditIndex);
+                }
+            } else {
+                move(layoutPosition, mEditIndex);
+            }
+        });
+
+        /*if (mAccessibilityAction != ACTION_NONE) {
             holder.mTileView.setClickable(selectable);
             holder.mTileView.setFocusable(selectable);
             holder.mTileView.setFocusableInTouchMode(selectable);
@@ -411,7 +422,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         }
         if (position == mFocusIndex) {
             focusOnHolder(holder);
-        }
+        }*/
     }
 
     private void focusOnHolder(Holder holder) {
@@ -572,7 +583,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                 mTileView = (QSTileViewImpl) ((FrameLayout) itemView).getChildAt(0);
                 mTileView.getIcon().disableAnimation();
                 mTileView.setTag(this);
-                ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
+                //ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
             }
         }
 
