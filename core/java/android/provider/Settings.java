fc3d1c80ab8eb7cb83a70ab421a084c8b8b08af4
maxwen <max.weninger@gmail.com>
2017-09-05 20:36:26 +0200

    SystemUI: add CPU info overlay
    Change-Id: I3c75a9c2837c9293822a33a95380cc1edb7f2b1a
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14055,6 +14055,11 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
