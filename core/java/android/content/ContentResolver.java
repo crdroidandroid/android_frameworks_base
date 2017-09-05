53837362abd517938c66cb3c2fa8b5c16192392a
aiyongbiao <aiyongbiao@xiaomi.com>
2017-09-05 16:18:15 +0800

    ContentResolver: fix native crash by limit observer's count
    The number of registerContentObserver for each applications can call
    should be limited, so that third-party application cannot easily
    make android system reboot.
    
    https://issuetracker.google.com/issues/65355068
    
    Test: Run app-debug.apk uploaded in issuetracker
    
    Change-Id: I105e3117287afe7b1ecef70c0e15ae0288012247
    Signed-off-by: aiyongbiao <aiyongbiao@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/content/ContentResolver.java
+++ core/java/android/content/ContentResolver.java
@@ -69,6 +69,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This class provides applications access to the content model.
@@ -1920,9 +1921,15 @@ public abstract class ContentResolver {
     /** @hide - designated user version */
     public final void registerContentObserver(Uri uri, boolean notifyForDescendents,
             ContentObserver observer, @UserIdInt int userHandle) {
+        if (observerCount.get() >= MAX_OBSERVER_COUNT) {
+            throw new IllegalStateException("registerContentObserver failed, " +
+                    "the number of observers has exceeded the maximum " + MAX_OBSERVER_COUNT);
+        }
+
         try {
             getContentService().registerContentObserver(uri, notifyForDescendents,
                     observer.getContentObserver(), userHandle, mTargetSdkVersion);
+            observerCount.incrementAndGet();
         } catch (RemoteException e) {
         }
     }
@@ -1940,6 +1947,7 @@ public abstract class ContentResolver {
             if (contentObserver != null) {
                 getContentService().unregisterContentObserver(
                         contentObserver);
+                observerCount.decrementAndGet();
             }
         } catch (RemoteException e) {
         }
@@ -2990,6 +2998,10 @@ public abstract class ContentResolver {
     final int mTargetSdkVersion;
 
     private static final String TAG = "ContentResolver";
+
+    private static final int MAX_OBSERVER_COUNT = 1024;
+
+    private final AtomicInteger observerCount = new AtomicInteger(0);
 
     /** @hide */
     public int resolveUserId(Uri uri) {
