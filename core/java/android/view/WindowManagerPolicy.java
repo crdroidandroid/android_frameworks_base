69d7093cdb68fa36d731ac573bc309c00fd38fed
bigrushdog <randall.rushing@gmail.com>
2017-09-06 20:46:51 +0200

    Add api to take screenshots
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -66,6 +66,7 @@ import android.annotation.Nullable;
 import android.annotation.SystemApi;
 import android.app.ActivityManager.StackId;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.CompatibilityInfo;
 import android.content.res.Configuration;
@@ -1553,6 +1554,11 @@ public interface WindowManagerPolicy {
      * Specifies whether there is an on-screen navigation bar separate from the status bar.
      */
     public boolean hasNavigationBar();
+
+    /**
+     * Send some ActionHandler commands to WindowManager.
+     */
+    public void sendCustomAction(Intent intent);
 
     /**
      * Lock the device now.
