6a25cd7bc540e10f3f9084dadc464a792e129a88
ezio84 <brabus84@gmail.com>
2017-09-06 23:59:02 +0200

    Allow to disable qs footer warnings [1/2]
    When using apps like AdGuard that create a vpn, a warning is displayed
    in the QS panel footer, so the user knows the device net is being monitored
    by an external app.
    The warning takes space and the user might want to avoid it if he already
    knows that the app is running (also because there's a statusbar icon
    for the same purpose)
    
    @neobuddy89: Adapt TunerService directly to QSSecurityFooter
    
    Change-Id: I909cacc67aa44f62489068d3903b7a24e94ea18a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5501,6 +5501,12 @@ public final class Settings {
          */
         public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
 
+       /**
+        * Whether to show QS panel footer warnings like for active VPN
+        * @hide
+        */
+        public static final String QS_FOOTER_WARNINGS = "qs_footer_warnings";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
