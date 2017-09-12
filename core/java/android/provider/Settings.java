454a1ccdcad5b91abfefd1e9b0f03b3d0ff3b719
Christian Oder <myself5@carbonrom.org>
2017-09-12 15:56:04 +0800

    fwb: Add ability to permanently hide apps from recents [1/3]
    This is based off XposedAppSettings, but modified to fit our needs, and
    a little bit of: https://github.com/CarbonROM/android_frameworks_base/commit/d9973ba0ef48d27688df1e6b33a9777b67b24142
    
    Squashed with fwb: fix HIDE_FROM_RECENTS_LIST provider entry
    
    Change-Id: I859f2c9561fc7fc77c57d8dad5f1ba6a8033e21b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5072,6 +5072,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+        * List of Apps hidden from recents
+        * @hide
+        */
+        public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
