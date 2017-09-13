34f2648b79971e4faf17ddc09cd4b7e6c0e12253
ezio84 <brabus84@gmail.com>
2017-09-13 20:49:29 +0200

    Enable notifications led light by default [1/2]
    def_notification_pulse bool is already set to true
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I70968cb2731db07fee8253c1c6c37fe1d03446ae

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1665,7 +1665,7 @@ public class NotificationManagerService extends SystemService {
             ContentResolver resolver = getContext().getContentResolver();
             if (uri == null || NOTIFICATION_LIGHT_PULSE_URI.equals(uri)) {
                 boolean pulseEnabled = Settings.System.getIntForUser(resolver,
-                            Settings.System.NOTIFICATION_LIGHT_PULSE, 0, UserHandle.USER_CURRENT)
+                            Settings.System.NOTIFICATION_LIGHT_PULSE, 1, UserHandle.USER_CURRENT)
                         != 0;
                 if (mNotificationPulseEnabled != pulseEnabled) {
                     mNotificationPulseEnabled = pulseEnabled;
