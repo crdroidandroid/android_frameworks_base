39982a0aae75f99dc46d61fd14cdbb68a443db13
Carsten Hauge <carsten.hauge@sonymobile.com>
2017-09-21 22:45:21 +0200

    Vibrate on plug [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4300,6 +4300,12 @@ public final class Settings {
          */
         public static final String QS_COLUMNS_LANDSCAPE = "qs_columns_landscape";
 
+       /**
+         * Whether to vibrate on power connection or disconnection
+         * @hide
+         */
+        public static final String VIBRATION_ON_CHARGE_STATE_CHANGED = "vibration_on_charge_state_changed";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
