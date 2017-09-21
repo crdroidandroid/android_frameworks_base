8cac393d329a5b01746fcc53482a7bea6ab94213
maxwen <max.weninger@gmail.com>
2017-09-21 18:45:39 +0200

    Hide tiles title: longpress will launch detail view
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -253,5 +253,10 @@ public class QuickQSPanel extends QSPanel {
                         R.id.expand_indicator);
             }
         }
+
+        @Override
+        public boolean isShowTitles() {
+            return false;
+        }
     }
 }
