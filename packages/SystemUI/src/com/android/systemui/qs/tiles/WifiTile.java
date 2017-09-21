846f0e29a1561ea7e294911fdf7557e08906f8e1
maxwen <max.weninger@gmail.com>
2017-09-21 18:45:39 +0200

    Hide tiles title: longpress will launch detail view
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
@@ -103,6 +103,11 @@ public class WifiTile extends QSTileImpl<SignalState> {
     }
 
     @Override
+    public boolean isDualTarget() {
+        return true;
+    }
+
+    @Override
     public QSIconView createTileView(Context context) {
         return new AlphaControlledSignalTileView(context);
     }
