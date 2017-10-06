814935be1c9bf459fac607260d587602002cefe9
ezio84 <brabus84@gmail.com>
2017-10-06 12:54:22 +0200

    Stock Recents icon pack support [1/2]
    adapted IconPackHelper from CM trebuchet (https://review.lineageos.org/#/c/136600/)
    with some OmniSwitch improvements;
    splitted the whole dialog part and moved it into Settings;
    
    @neobuddy89: Use Tuner API.
    
    Change-Id: I65cc0360c908801243f37d05212eb864fa08a5e1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4172,6 +4172,12 @@ public final class Settings {
         public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
 
         /**
+         * Whether to use icon pack for Recents
+         * @hide
+         */
+        public static final String RECENTS_ICON_PACK = "recents_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
