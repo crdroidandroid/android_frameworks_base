4dcd119808c84527dc445037c73fb04862ea4725
Pranav Vashi <neobuddy89@gmail.com>
2017-10-13 00:43:01 +0530

    PhoneWindowManager: Do not panic if immersive mode active
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/WindowManagerPolicyControl.java
+++ core/java/android/view/WindowManagerPolicyControl.java
@@ -276,6 +276,10 @@ public class WindowManagerPolicyControl {
         }
     }
 
+    public static boolean isImmersiveFiltersActive() {
+        return sImmersiveStatusFilter != null || sImmersiveNavigationFilter != null;
+    }
+
     public static void dump(String prefix, PrintWriter pw) {
         dump("sImmersiveStatusFilter", sImmersiveStatusFilter, prefix, pw);
         dump("sImmersiveNavigationFilter", sImmersiveNavigationFilter, prefix, pw);
