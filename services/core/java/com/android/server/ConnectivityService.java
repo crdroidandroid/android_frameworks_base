9419f07f651c9c56890c636bc4443b17b41f822f
Pranav Vashi <neobuddy89@gmail.com>
2017-11-12 16:39:41 +0530

    Revert "ConnectivityService: safer locking"
    This reverts commit 1648b12ca50d8ab0d4985863396cf36b1036f73d.

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -1346,16 +1346,13 @@ public class ConnectivityService extends IConnectivityManager.Stub
     @Override
     public LinkProperties getLinkProperties(Network network) {
         enforceAccessPermission();
-        return getLinkProperties(getNetworkAgentInfoForNetwork(network));
-    }
-
-    private LinkProperties getLinkProperties(NetworkAgentInfo nai) {
-        if (nai == null) {
-            return null;
+        NetworkAgentInfo nai = getNetworkAgentInfoForNetwork(network);
+        if (nai != null) {
+            synchronized (nai) {
+                return new LinkProperties(nai.linkProperties);
+            }
         }
-        synchronized (nai) {
-            return new LinkProperties(nai.linkProperties);
-        }
+        return null;
     }
 
     private NetworkCapabilities getNetworkCapabilitiesInternal(NetworkAgentInfo nai) {
@@ -3227,8 +3224,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         enforceAccessPermission();
         enforceInternetPermission();
 
-        // TODO: execute this logic on ConnectivityService handler.
-        final NetworkAgentInfo nai;
+        NetworkAgentInfo nai;
         if (network == null) {
             nai = getDefaultNetwork();
         } else {
@@ -3239,24 +3235,21 @@ public class ConnectivityService extends IConnectivityManager.Stub
             return;
         }
         // Revalidate if the app report does not match our current validated state.
-        if (hasConnectivity == nai.lastValidated) {
-            return;
-        }
+        if (hasConnectivity == nai.lastValidated) return;
         final int uid = Binder.getCallingUid();
         if (DBG) {
             log("reportNetworkConnectivity(" + nai.network.netId + ", " + hasConnectivity +
                     ") by " + uid);
         }
-        // Validating a network that has not yet connected could result in a call to
-        // rematchNetworkAndRequests() which is not meant to work on such networks.
-        if (!nai.everConnected) {
-            return;
+        synchronized (nai) {
+            // Validating a network that has not yet connected could result in a call to
+            // rematchNetworkAndRequests() which is not meant to work on such networks.
+            if (!nai.everConnected) return;
+
+            if (isNetworkWithLinkPropertiesBlocked(nai.linkProperties, uid, false)) return;
+
+            nai.networkMonitor.sendMessage(NetworkMonitor.CMD_FORCE_REEVALUATION, uid);
         }
-        LinkProperties lp = getLinkProperties(nai);
-        if (isNetworkWithLinkPropertiesBlocked(lp, uid, false)) {
-            return;
-        }
-        nai.networkMonitor.sendMessage(NetworkMonitor.CMD_FORCE_REEVALUATION, uid);
     }
 
     private ProxyInfo getDefaultProxy() {
