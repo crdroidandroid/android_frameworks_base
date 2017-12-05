cb4b70d426552ebae793c49af1361c2f204b1ae7
Pranav Vashi <neobuddy89@gmail.com>
2017-12-05 23:35:18 +0530

    SystemUI: Add case to quick pull down status bar anywhere
    * Also disable this by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
@@ -544,6 +544,9 @@ public class QuickSettingsController {
             case 2: // Left side pulldown
                 showQsOverride = mQs.getView().isLayoutRtl() ? w - region < x : x < region;
                 break;
+            case 3: // pull down anywhere
+                showQsOverride = true;
+                break;
         }
         showQsOverride &= mBarState == StatusBarState.SHADE;
 
