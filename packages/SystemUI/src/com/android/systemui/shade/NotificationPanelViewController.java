bc4c505e43b6488808e090d30e8b4a5359eaba4d
Pranav Vashi <neobuddy89@gmail.com>
2017-12-05 23:35:18 +0530

    SystemUI: Add case to quick pull down status bar anywhere
    * Also disable this by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2537,6 +2537,9 @@ public final class NotificationPanelViewController implements Dumpable {
             case 2: // Left side pulldown
                 showQsOverride = mView.isLayoutRtl() ? w - region < x : x < region;
                 break;
+            case 3: // pull down anywhere
+                showQsOverride = true;
+                break;
         }
         showQsOverride &= mBarState == StatusBarState.SHADE;
 
