1b1560cee08dcb3abbf8150fe2def8153fa0352f
Pranav Vashi <neobuddy89@gmail.com>
2017-12-05 23:35:18 +0530

    Disable quick QS pull down by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4990,7 +4990,7 @@ public class NotificationPanelViewController extends PanelViewController {
         @Override
         public void onTuningChanged(String key, String newValue) {
             if (STATUS_BAR_QUICK_QS_PULLDOWN.equals(key)) {
-                mOneFingerQuickSettingsIntercept = TunerService.parseInteger(newValue, 1);
+                mOneFingerQuickSettingsIntercept = TunerService.parseInteger(newValue, 0);
             } else if (DOUBLE_TAP_SLEEP_GESTURE.equals(key)) {
                 mDoubleTapToSleepEnabled = TunerService.parseIntegerSwitch(newValue, true);
             }
