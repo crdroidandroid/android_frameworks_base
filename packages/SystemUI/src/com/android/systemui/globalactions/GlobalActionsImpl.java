9d762c877f73e9bb8da4604b7af834067928c19f
maxwen <max.weninger@gmail.com>
2017-12-10 09:30:50 +0100

    Powermenu: use new gradient dialog also for recovery/bootloader action
    adapted to our code
    
    original commit: https://gerrit.omnirom.org/#/c/27327/
    
    Change-Id: I3a05ca554927dacff7e10d901de5947569a08d07
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -21,6 +21,7 @@ import android.app.WallpaperManager;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.os.PowerManager;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -96,7 +97,15 @@ public class GlobalActionsImpl implements GlobalActions {
         bar.getIndeterminateDrawable().setTint(color);
         TextView message = d.findViewById(R.id.text1);
         message.setTextColor(color);
-        if (isReboot) message.setText(R.string.reboot_to_reset_message);
+        if (reason != null) {
+            if (PowerManager.REBOOT_BOOTLOADER.equals(reason)) {
+                message.setText(com.android.internal.R.string.reboot_to_bootloader_message);
+            } else if (PowerManager.REBOOT_RECOVERY.equals(reason)) {
+                message.setText(com.android.internal.R.string.reboot_to_recovery_message);
+            }
+        } else if (isReboot) {
+            message.setText(R.string.reboot_to_reset_message);
+        }
 
         Point displaySize = new Point();
         mContext.getDisplay().getRealSize(displaySize);
