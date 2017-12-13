243563b8f7ef8f98de739955deba133e25e14ad8
Pranav Vashi <neobuddy89@gmail.com>
2017-12-14 01:38:41 +0530

    Add ability to toggle bluetooth battery level in QS [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -48,24 +48,47 @@ import com.android.systemui.qs.QSDetailItems.Item;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.statusbar.policy.BluetoothController;
+import com.android.systemui.tuner.TunerService;
+import com.android.systemui.tuner.TunerServiceImpl;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 /** Quick settings tile: Bluetooth **/
-public class BluetoothTile extends QSTileImpl<BooleanState> {
+public class BluetoothTile extends QSTileImpl<BooleanState> implements TunerService.Tunable {
     private static final Intent BLUETOOTH_SETTINGS = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
 
     private final BluetoothController mController;
     private final BluetoothDetailAdapter mDetailAdapter;
     private final ActivityStarter mActivityStarter;
 
+    private boolean mShowBluetoothBattery;
+
+    private static final String BLUETOOTH_QS_SHOW_BATTERY =
+            "system:" + Settings.System.BLUETOOTH_QS_SHOW_BATTERY;
+
     public BluetoothTile(QSHost host) {
         super(host);
         mController = Dependency.get(BluetoothController.class);
         mActivityStarter = Dependency.get(ActivityStarter.class);
         mDetailAdapter = (BluetoothDetailAdapter) createDetailAdapter();
+
+        Dependency.get(TunerService.class).addTunable(this,
+                BLUETOOTH_QS_SHOW_BATTERY);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case BLUETOOTH_QS_SHOW_BATTERY:
+                mShowBluetoothBattery =
+                        newValue == null || Integer.parseInt(newValue) != 0;
+                refreshState();
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -199,7 +222,7 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
             CachedBluetoothDevice lastDevice = connectedDevices.get(0);
             final int batteryLevel = lastDevice.getBatteryLevel();
 
-            if (batteryLevel != BluetoothDevice.BATTERY_LEVEL_UNKNOWN) {
+            if (mShowBluetoothBattery && batteryLevel != BluetoothDevice.BATTERY_LEVEL_UNKNOWN) {
                 return mContext.getString(
                         R.string.quick_settings_bluetooth_secondary_label_battery_level,
                         Utils.formatPercentage(batteryLevel));
