59bc143c3a112dd23d6bde21fca372b7b7f741f7
ezio84 <brabus84@gmail.com>
2017-12-26 15:31:19 +0100

    Power menu advanced reboot toggle: new icons
    base from flaticon.com
    
    also don't add the emergency button (e.g. with some indians carriers)
    
    Change-Id: Ia2ee761817ef985741275cda92f83abb74e31cdd
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -349,7 +349,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         mRestartAdvancedAction = new ToggleRestartAdvancedAction(
                 com.android.systemui.R.drawable.ic_restart_advanced,
-                com.android.systemui.R.drawable.ic_restart_advanced,
+                com.android.systemui.R.drawable.ic_restart_bootloader,
                 com.android.systemui.R.string.global_action_restart_advanced,
                 com.android.systemui.R.string.global_action_restart_recovery,
                 com.android.systemui.R.string.global_action_restart_bootloader,
@@ -411,9 +411,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             addedKeys.add(actionKey);
         }
 
-        if (mEmergencyAffordanceManager.needsEmergencyAffordance()) {
+        /*if (mEmergencyAffordanceManager.needsEmergencyAffordance()) {
             mItems.add(getEmergencyAction());
-        }
+        }*/
 
         mAdapter = new MyAdapter();
 
