b4503da6a96ac97224bf21da65a5186fdbd171d1
ezio84 <brabus84@gmail.com>
2018-01-05 22:21:18 +0100

    Flashlight toggle api improvements
    being sure mCameraId is initialized.
    
    Change-Id: Ide72c7143c0e04715ca5404cd7f8e02333ac455b
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3743,10 +3743,11 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     @Override
     public void toggleFlashlight() {
-        if (mFlashlightController != null
-                && mFlashlightController.hasFlashlight()
-                && mFlashlightController.isAvailable()) {
-            mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
+        if (mFlashlightController != null) {
+            mFlashlightController.initFlashLight();
+            if (mFlashlightController.hasFlashlight() && mFlashlightController.isAvailable()) {
+                mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
+            }
         }
     }
 
