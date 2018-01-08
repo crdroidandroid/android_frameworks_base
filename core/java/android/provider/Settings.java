0c7e7e489b6dcc6235a48fe95cd67c5c3486d6dc
maxwen <max.weninger@gmail.com>
2018-01-08 02:52:58 +0100

    base: persist screenshot type in settings
    save state set in qs tile into settings
    
    Change-Id: Ieca6148862386cef01767d43938bb803aab282cb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4189,6 +4189,13 @@ public final class Settings {
         public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
 
         /**
+         * 0 - fullscreen
+         * 1 - partial
+         * @hide
+         */
+        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
