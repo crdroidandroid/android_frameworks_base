b694bfefd83f0fb6c9e254f5de6d4204bbae37f6
beanstown106 <nbenis106@gmail.com>
2018-01-09 18:38:59 +0100

    Fingerprint authentication vibration [1/2]
    @neobuddy89: Use old string declarations.
    
    -------
    
    - Bring it to o8.1 by LorDClockaN
    
    Change-Id: I7fc6f7fff7002b39645996bc6791e3a06d501247
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4499,6 +4499,12 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Whether to enable or disable vibration on succesful fingerprint auth
+         * @hide
+         */
+        public static final String FP_SUCCESS_VIBRATE = "fp_success_vibrate";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
