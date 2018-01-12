4970397d0dba029be4434f23e9582344c890460b
bigrushdog <randall.rushing@gmail.com>
2018-01-12 12:35:28 -0800

    Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    Adapted to 10 by eyosen
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5557,6 +5557,12 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
