9024669be9bccbdf5d5f8acc9a8a5c5b67f293cd
Weijie Wang <weijiew@codeaurora.org>
2018-01-12 13:16:33 +0800

    SystemUI: No Signal bar displays for a moment in UI after Bootup
    No signal bar should be not displayed when airplaneMode is enabled
    
    Change-Id: I776ac57f1a811f98625c6169a714126f4658482e
    CRs-Fixed: 2155823
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -593,7 +593,7 @@ public class SignalClusterView extends LinearLayout implements NetworkController
             mWifiSignalSpacer.setVisibility(View.GONE);
         }
 
-        if (mNoSimsVisible) {
+        if (mNoSimsVisible && !mIsAirplaneMode) {
             mIconLogger.onIconShown(SLOT_MOBILE);
             mNoSimsCombo.setVisibility(View.VISIBLE);
             if (!Objects.equals(mSimDetected, mNoSimsCombo.getTag())) {
