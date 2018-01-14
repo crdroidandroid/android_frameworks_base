56b13cb80b57ff4c54bc56c0dbfe4ab0f12b5c72
Pranav Vashi <neobuddy89@gmail.com>
2018-01-14 10:04:03 +0530

    base: Add accidental touch prevention for assist key
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4166,6 +4166,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean homeKey = keyCode == KeyEvent.KEYCODE_HOME;
         final boolean menuKey = keyCode == KeyEvent.KEYCODE_MENU;
         final boolean backKey = keyCode == KeyEvent.KEYCODE_BACK;
+        final boolean assistKey = keyCode == KeyEvent.KEYCODE_ASSIST;
         final boolean navBarKey = source == InputDevice.SOURCE_NAVIGATION_BAR;
 
         // If screen is off then we treat the case where the keyguard is open but hidden
@@ -4184,7 +4185,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (mANBIEnabled && mANBIHandler != null && mANBIHandler.isScreenTouched()
-                && !navBarKey && (appSwitchKey || homeKey || menuKey || backKey)) {
+                && !navBarKey && (appSwitchKey || homeKey || menuKey || backKey || assistKey)) {
             return 0;
         }
 
