374580a6104bafb28f50b6e9dae4f15c126621e1
ezio84 <brabus84@gmail.com>
2018-01-18 23:25:14 +0100

    Ambient display music ticker [1/2]
    Trigger an Ambient Display notification, even if Ambient
    Display is disabled, so we can know playing track title/artist.
    This is handy when the player starts playing a new track or even
    when we use volume rocker skip track feature when screen is off.
    A new track will force an ambient notification but with a clean
    layout without clock and other elements, but track info in the middle
    of the screen.
    
    Double tapping on track info allows to skip the track (even for
    remote Spotify/KdeConnect or other similar apps).
    
    We use provided Doze services to skip this when the proximity
    sensor is covered or system Ambient events are pending.
    
    If the feature is disabled, track info is still shown by default
    on keyguard and ambient when these modes are called by the system
    or the user.
    
    [@neobuddy89: Squash includes all fixes by ezio, disabled by default]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
+++ packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -2,27 +2,45 @@ package com.android.systemui.ambientmusic;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.media.MediaMetadata;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.text.TextUtils.TruncateAt;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.systemui.AutoReinflateContainer;
-import com.android.systemui.doze.DozeReceiver;
 import com.android.systemui.R;
+import com.android.systemui.doze.DozeLog;
+import com.android.systemui.statusbar.phone.DozeParameters;
 import com.android.systemui.statusbar.phone.StatusBar;
 
 import com.android.systemui.ambientmusic.AmbientIndicationInflateListener;
 
-public class AmbientIndicationContainer extends AutoReinflateContainer implements DozeReceiver {
+import java.util.concurrent.TimeUnit;
+
+public class AmbientIndicationContainer extends AutoReinflateContainer {
     private View mAmbientIndication;
-    private boolean mDozing;
     private ImageView mIcon;
     private CharSequence mIndication;
     private StatusBar mStatusBar;
     private TextView mText;
+    private TextView mTrackLenght;
     private Context mContext;
+    private MediaMetadata mMediaMetaData;
+    private String mMediaText;
+    private boolean mForcedMediaDoze;
+    private Handler mHandler;
+    private boolean mInfoAvailable;
+    private String mInfoToSet;
+    private String mLengthInfo;
+    private boolean mDozing;
+    private String mLastInfo;
 
     public AmbientIndicationContainer(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -30,36 +48,133 @@ public class AmbientIndicationContainer extends AutoReinflateContainer implement
     }
 
     public void hideIndication() {
-        setIndication(null);
+        setIndication(null, null);
     }
 
-    public void initializeView(StatusBar statusBar) {
+    public void initializeView(StatusBar statusBar, Handler handler) {
         mStatusBar = statusBar;
         addInflateListener(new AmbientIndicationInflateListener(this));
+        mHandler = handler;
     }
 
     public void updateAmbientIndicationView(View view) {
         mAmbientIndication = findViewById(R.id.ambient_indication);
         mText = (TextView)findViewById(R.id.ambient_indication_text);
+        mTrackLenght = (TextView)findViewById(R.id.ambient_indication_track_lenght);
         mIcon = (ImageView)findViewById(R.id.ambient_indication_icon);
-        setIndication(mIndication);
+        setIndication(mMediaMetaData, mMediaText);
     }
 
-    @Override
     public void setDozing(boolean dozing) {
-        mDozing = dozing;
-    }
+        if (dozing == mDozing) return;
 
-    public void setIndication(CharSequence charSequence) {
-        mText.setText(charSequence);
-        mIndication = charSequence;
-        mAmbientIndication.setClickable(false);
-        boolean infoAvaillable = TextUtils.isEmpty((CharSequence)charSequence);
-        if (infoAvaillable) {
-            mAmbientIndication.setVisibility(View.INVISIBLE);
-        } else {
+        mDozing = dozing;
+        setTickerMarquee(dozing, false);
+        if (dozing && mInfoAvailable) {
+            mText.setText(mInfoToSet);
+            mLastInfo = mInfoToSet;
+            mTrackLenght.setText(mLengthInfo);
             mAmbientIndication.setVisibility(View.VISIBLE);
+            updatePosition();
+        } else {
+            setCleanLayout(-1);
+            mAmbientIndication.setVisibility(View.INVISIBLE);
+            mText.setText(null);
+            mTrackLenght.setText(null);
         }
     }
-}
 
+    private void setTickerMarquee(boolean enable, boolean extendPulseOnNewTrack) {
+        if (enable) {
+            setTickerMarquee(false, false);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mText.setEllipsize(TruncateAt.MARQUEE);
+                    mText.setMarqueeRepeatLimit(2);
+                    mText.setSelected(true);
+                    if (extendPulseOnNewTrack && mStatusBar.isPulsing()) {
+                        mStatusBar.getDozeScrimController().extendPulseForMusicTicker();
+                    }
+                }
+            }, 1600);
+        } else {
+            mText.setEllipsize(null);
+            mText.setSelected(false);
+        }
+    }
+
+    public void setOnPulseEvent(int reason, boolean pulsing) {
+        setCleanLayout(reason);
+        setTickerMarquee(pulsing,
+                reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION);
+    }
+
+    public void setCleanLayout(int reason) {
+        mForcedMediaDoze =
+                reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION;
+        updatePosition();
+    }
+
+    public void updatePosition() {
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) this.getLayoutParams();
+        lp.gravity = mForcedMediaDoze ? Gravity.CENTER : Gravity.BOTTOM;
+        this.setLayoutParams(lp);
+    }
+
+    public void setIndication(MediaMetadata mediaMetaData, String notificationText) {
+        CharSequence charSequence = null;
+        mLengthInfo = null;
+        mInfoToSet = null;
+        if (mediaMetaData != null) {
+            CharSequence artist = mediaMetaData.getText(MediaMetadata.METADATA_KEY_ARTIST);
+            CharSequence album = mediaMetaData.getText(MediaMetadata.METADATA_KEY_ALBUM);
+            CharSequence title = mediaMetaData.getText(MediaMetadata.METADATA_KEY_TITLE);
+            long duration = mediaMetaData.getLong(MediaMetadata.METADATA_KEY_DURATION);
+            if (artist != null && album != null && title != null) {
+                /* considering we are in Ambient mode here, it's not worth it to show
+                    too many infos, so let's skip album name to keep a smaller text */
+                charSequence = artist.toString() /*+ " - " + album.toString()*/ + " - " + title.toString();
+                if (duration != 0) {
+                    mLengthInfo = String.format("%02d:%02d",
+                            TimeUnit.MILLISECONDS.toMinutes(duration),
+                            TimeUnit.MILLISECONDS.toSeconds(duration) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))).toString();
+                }
+            }
+        }
+        if (mDozing) {
+            // if we are already showing an Ambient Notification with track info,
+            // stop the current scrolling and start it delayed again for the next song
+            setTickerMarquee(true, true);
+        }
+
+        if (!TextUtils.isEmpty(charSequence)) {
+            mInfoToSet = charSequence.toString();
+        } else if (!TextUtils.isEmpty(notificationText)) {
+            mInfoToSet = notificationText;
+            mLengthInfo = null;
+        }
+
+        mInfoAvailable = mInfoToSet != null;
+        if (mInfoAvailable) {
+            mMediaMetaData = mediaMetaData;
+            mMediaText = notificationText;
+            boolean isAnotherTrack = mInfoAvailable
+                    && (TextUtils.isEmpty(mLastInfo) || (!TextUtils.isEmpty(mLastInfo) && !mLastInfo.equals(mInfoToSet)));
+            if (!DozeParameters.getInstance(mContext).getAlwaysOn() && mStatusBar != null && isAnotherTrack) {
+                mStatusBar.triggerAmbientForMedia();
+            }
+            if (mDozing) {
+                mLastInfo = mInfoToSet;
+            }
+        }
+        mText.setText(mInfoToSet);
+        mTrackLenght.setText(mLengthInfo);
+        mAmbientIndication.setVisibility(mDozing && mInfoAvailable ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    public View getIndication() {
+        return mAmbientIndication;
+    }
+}
