89e4c18bb83698425bc50eea35fc124791da424d
ezio84 <brabus84@gmail.com>
2018-01-18 23:25:14 +0100

    Ambient display music ticker [1/2]
    Trigger an Ambient Display notification, even if Ambient
    Display is disabled, so we can know playing track title/artist.
    This is handy when the player starts playing a new track or even
    when we use volume rocker skip track feature when screen is off.
    A new track will force an ambient notification but with a clean
    layout without clock and other elements, but track info in the middle
    of the screen.
    
    Double tapping on track info allows to skip the track (even for
    remote Spotify/KdeConnect or other similar apps).
    
    We use provided Doze services to skip this when the proximity
    sensor is covered or system Ambient events are pending.
    
    If the feature is disabled, track info is still shown by default
    on keyguard and ambient when these modes are called by the system
    or the user.
    
    [@neobuddy89: Squash includes all fixes by ezio, disabled by default]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -46,6 +46,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.ViewClippingUtil;
 import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
+import com.android.systemui.doze.DozeLog;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.util.wakelock.KeepAwakeAnimationListener;
 
@@ -78,6 +79,8 @@ public class KeyguardStatusView extends GridLayout implements
     private int mTextColor;
     private float mWidgetPadding;
     private int mLastLayoutHeight;
+
+    private boolean mForcedMediaDoze;
 
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
 
@@ -459,9 +462,19 @@ public class KeyguardStatusView extends GridLayout implements
         updateDozeVisibleViews();
     }
 
+    public void setCleanLayout(int reason) {
+        mForcedMediaDoze =
+                reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION;
+        updateDozeVisibleViews();
+    }
+
     private void updateDozeVisibleViews() {
         for (View child : mVisibleInDoze) {
-            child.setAlpha(mDarkAmount == 1 && mPulsing ? 0.8f : 1);
+            if (!mForcedMediaDoze) {
+                child.setAlpha(mDarkAmount == 1 && mPulsing ? 0.8f : 1);
+            } else {
+                child.setAlpha(mDarkAmount == 1 ? 0 : 1);
+            }
         }
     }
 
