c6266d203e0361830f8bba913a02b07bd0f8070e
ezio84 <brabus84@gmail.com>
2018-01-24 12:16:44 +0100

    DUI Pulse: add Automatic albumart base color mode [1/3]
    the dui part will also be sure that pulse color has
    an acceptable contrast against white and black navbar
    
    Also expose media notification colors values for future improvements
    
    Change-Id: I1bd6b70f2f5ad1f0e6decdac311b1c8e51c9149d
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -465,6 +465,23 @@ public class Notification implements Parcelable
     public int defaults;
 
     /**
+     * @hide
+     */
+    public int backgroundColor;
+    /**
+     * @hide
+     */
+    public int foregroundColor;
+    /**
+     * @hide
+     */
+    public int primaryTextColor;
+    /**
+     * @hide
+     */
+    public int secondaryTextColor;
+
+    /**
      * Bit to be bitwise-ored into the {@link #flags} field that should be
      * set if you want the LED on for this notification.
      * <ul>
@@ -1969,6 +1986,11 @@ public class Notification implements Parcelable
         }
 
         mGroupAlertBehavior = parcel.readInt();
+
+        backgroundColor = parcel.readInt();
+        foregroundColor = parcel.readInt();
+        primaryTextColor = parcel.readInt();
+        secondaryTextColor = parcel.readInt();
     }
 
     @Override
@@ -2087,6 +2109,11 @@ public class Notification implements Parcelable
         if (!heavy) {
             that.lightenPayload(); // will clean out extras
         }
+
+        that.backgroundColor = this.backgroundColor;
+        that.foregroundColor = this.foregroundColor;
+        that.primaryTextColor = this.primaryTextColor;
+        that.secondaryTextColor = this.secondaryTextColor;
     }
 
     /**
@@ -2362,6 +2389,11 @@ public class Notification implements Parcelable
         }
 
         parcel.writeInt(mGroupAlertBehavior);
+
+        parcel.writeInt(backgroundColor);
+        parcel.writeInt(foregroundColor);
+        parcel.writeInt(primaryTextColor);
+        parcel.writeInt(secondaryTextColor);
     }
 
     /**
@@ -4054,6 +4086,11 @@ public class Notification implements Parcelable
                 mActionBarColor = NotificationColorUtil.resolveActionBarColor(mContext,
                         backgroundColor);
             }
+
+            mN.backgroundColor = backgroundColor;
+            mN.foregroundColor = mForegroundColor;
+            mN.primaryTextColor = mPrimaryTextColor;
+            mN.secondaryTextColor = mSecondaryTextColor;
         }
 
         private void updateBackgroundColor(RemoteViews contentView) {
