70c6ff417280029db586726965de10cfd5d98019
Prema Chand Alugu <premaca@gmail.com>
2018-01-31 23:51:19 +0530

    AbsListView: Fix widget couldn't find any view fc
    * Use current context Main Looper to inflate
    
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView: updateAppWidget couldn't find any view, using error view
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView: android.view.InflateException: Binary XML file line #50: Binary XML file line #50: Error inflating class ListView
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView: Caused by: android.view.InflateException: Binary XML file line #50: Error inflating class ListView
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView: Caused by: java.lang.RuntimeException: Can't create handler inside thread that has not called Looper.prepare()
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at android.os.Handler.<init>(Handler.java:204)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at android.os.Handler.<init>(Handler.java:118)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at android.widget.AbsListView$1.<init>(AbsListView.java:3934)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at android.widget.AbsListView.<init>(AbsListView.java:3934)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at android.widget.ListView.<init>(ListView.java:227)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at android.widget.ListView.<init>(ListView.java:223)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at o.bjg.<init>(SourceFile:46)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at o.bjg.<init>(SourceFile:42)
    01-31 22:32:33.091  2802  2802 W AppWidgetHostView:     at o.bix.eN(SourceFile:77)
    
    Change-Id: Iff692f8b4991507c550ad0ca57268d47719f7956
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -31,6 +31,7 @@ import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -3981,7 +3982,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         }
     }
 
-    private final Handler mInverse = new Handler() {
+    private final Handler mInverse = new Handler(mContext.getMainLooper()) {
         public void handleMessage(Message msg) {
             mIsTap = !mIsTap;
         }
