adfff487aaf3d5d2294ba6d925043046ac1f742d
Alex Cruz <du.alexcruz@gmail.com>
2018-01-30 23:45:42 -0500

    Remove call to expand QS panel while pressing the Airplane QS tile
    Currently if you bring down the QS panel a quarter of the way, just enough where you
    can see the QS tiles and press on the Airplane QS tile it expands the QS panel.
    
    This is not stock behavior.
    
    The call was added in the commit below but I don't believe that it was intended to ever
    be there because it doesn't make sense. If you're on a secured lockscreen and bring down
    the QS panel, press the QS tile and unlock your device, the QS panel is already down. There
    is really no need to call it again.
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/9774af33cd85ef976f9b0e4f32c8ab5f1d57480a
    
    Change-Id: I4679d0a637dec60df2d49033e267de35cae4d758
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AirplaneModeTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AirplaneModeTile.java
@@ -83,7 +83,6 @@ public class AirplaneModeTile extends QSTileImpl<BooleanState> {
         }
         if (mKeyguardMonitor.isSecure() && !mKeyguardMonitor.canSkipBouncer()) {
             mActivityStarter.postQSRunnableDismissingKeyguard(() -> {
-                mHost.openPanels();
                 setEnabled(!mState.value);
             });
             return;
