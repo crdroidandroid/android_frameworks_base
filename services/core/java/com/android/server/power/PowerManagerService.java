d9a9607fe00968126bf7a53a8b269d25645eba80
xyyx <xyyx@mail.ru>
2018-02-01 21:36:45 +0800

    policy: introduce pocket lock
    * Block touch screen and keys inputs when device is in pocket.
    * Safe-door to disable pocket lock within long press power button
    if sensors do not behave correctly after
    PocketManager.isDeviceInPocket()
    returns true.
    * Window uses TYPE_SYSTEM_ERROR flag with ensure high
    priority over the lockscreen and other high priority windows.
    * Do not illuminate buttons when pocket lock is showing.
    * Disable fingerprint events and gestures when in pocket
    * Be friendly towards Keypress Boost
    
    Contributors:
    Carlo Savignano <carlosavignano@aospa.co>
    Chris Lahaye <dev@chrislahaye.com>
    Anas Karbila <anaskarbila@aospa.co>
    Alex Naidis <alex.naidis@linux.com>
    Park Ju Hyung <qkrwngud825@gmail.com>
    
    Ticket: NOUGAT-15
    
    Updated for 8.0 by Sirhc95
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -66,6 +66,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.WorkSource;
 import android.os.WorkSource.WorkChain;
+import android.pocket.PocketManager;
 import android.provider.Settings;
 import android.provider.Settings.Global;
 import android.provider.Settings.SettingNotFoundException;
@@ -267,6 +268,7 @@ public final class PowerManagerService extends SystemService
     private boolean mButtonOn;
     private boolean mButtonLightOnKeypressOnly;
 
+    private int mEvent;
     private final Object mLock = LockGuard.installNewLock(LockGuard.INDEX_POWER);
 
     // A bitfield that indicates what parts of the power state have
@@ -2124,6 +2126,9 @@ public final class PowerManagerService extends SystemService
                 final long screenDimDuration = getScreenDimDurationLocked(screenOffTimeout);
                 final boolean userInactiveOverride = mUserInactiveOverrideFromWindowManager;
                 final long nextProfileTimeout = getNextProfileTimeoutLocked(now);
+                final PocketManager pocketManager = (PocketManager) mContext.getSystemService(Context.POCKET_SERVICE);
+                final boolean isDeviceInPocket = pocketManager != null && pocketManager.isDeviceInPocket();
+                final boolean buttonPressed = mEvent == PowerManager.USER_ACTIVITY_EVENT_BUTTON;
 
                 mUserActivitySummary = 0;
                 if (mLastUserActivityTime >= mLastWakeTime) {
@@ -2147,10 +2152,10 @@ public final class PowerManagerService extends SystemService
                                 mButtonOn = false;
                             } else {
                                 if ((!mButtonLightOnKeypressOnly || mButtonPressed) &&
-                                        !mProximityPositive) {
+                                        !mProximityPositive && !isDeviceInPocket) {
                                     mButtonsLight.setBrightness(buttonBrightness);
                                     mButtonPressed = false;
-                                    if (buttonBrightness != 0 && mButtonTimeout != 0) {
+                                    if (buttonBrightness != 0 && mButtonTimeout != 0 && buttonPressed) {
                                         mButtonOn = true;
                                         if (now + mButtonTimeout < nextTimeout) {
                                             nextTimeout = now + mButtonTimeout;
