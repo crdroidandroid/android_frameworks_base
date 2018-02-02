42f0c97e9abdd85683ff618e7501ebd4750a1253
Pranav Vashi <neobuddy89@gmail.com>
2018-02-02 20:26:26 +0530

    Ambient ticker: Play nice with AOD
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -100,8 +100,9 @@ public class DozeTriggers implements DozeMachine.Part {
     private void onNotificationForced() {
         if (DozeMachine.DEBUG) Log.d(TAG, "requestNotificationPulse");
         mNotificationPulseTime = SystemClock.elapsedRealtime();
-        if (!mConfig.pulseOnNotificationAvailable() && !mConfig.canForceDozeNotifications()) return;
-        requestPulse(DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION, false /* performedProxCheck */);
+        if ((!mConfig.pulseOnNotificationAvailable() && !mConfig.canForceDozeNotifications()) ||
+            mConfig.alwaysOnEnabled(UserHandle.USER_CURRENT)) return;
+        requestPulse(DozeLog.PULSE_REASON_NOTIFICATION, false /* performedProxCheck */);
         DozeLog.traceNotificationPulse(mContext);
     }
 
