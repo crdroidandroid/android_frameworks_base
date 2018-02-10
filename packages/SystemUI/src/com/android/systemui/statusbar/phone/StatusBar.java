e3fb97600b0f748d7b1cb76cb3d6cb217ed5eb96
Ivan Iskandar <ivan@prjkt.io>
2018-02-11 02:12:50 +0700

    Reload more assets on overlay change
    @neobuddy89: Ported to P.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1336,6 +1336,12 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onOverlayChanged();
         }
+        mGutsManager.onOverlayChanged();
+        mStackScroller.onOverlayChanged();
+        mNotificationShelf.onOverlayChanged();
+        mNotificationPanel.onOverlayChanged();
+        Dependency.get(DarkIconDispatcher.class).onOverlayChanged(mContext);
+        reevaluateStyles();
     }
 
     private void inflateEmptyShadeView() {
