violet-reborn/android_frameworks_base-1

Clean up of expanded desktop policy and flags
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/view/WindowManager.java
@@ -1393,19 +1393,19 @@ public interface WindowManager extends ViewManager {
         /**
          * {@hide}
          */
-        public static final int PRIVATE_FLAG_STATUS_HIDE_FORCED = 0x01000000;
+        public static final int PRIVATE_FLAG_STATUS_HIDE_FORCED = 0x00800000;
 
         /**
          * {@hide}
          */
-        public static final int PRIVATE_FLAG_NAV_HIDE_FORCED = 0x02000000;
+        public static final int PRIVATE_FLAG_NAV_HIDE_FORCED = 0x01000000;
 
         /**
          * The window had not set FULLSCREEN flag so don't handle it as fullscreen in layoutWindowLw
          *
          * {@hide}
          */
-        public static final int PRIVATE_FLAG_WAS_NOT_FULLSCREEN = 0x03000000;
+        public static final int PRIVATE_FLAG_WAS_NOT_FULLSCREEN = 0x02000000;
 
         /**
          * Control flags that are private to the platform.

crdroidandroid/android_frameworks_base

core/java/android/view/WindowManager.java
@@ -1442,19 +1442,19 @@ public interface WindowManager extends ViewManager {
         /**
          * {@hide}
          */
-        public static final int PRIVATE_FLAG_STATUS_HIDE_FORCED = 0x01000000;
+        public static final int PRIVATE_FLAG_STATUS_HIDE_FORCED = 0x05000000;
 
         /**
          * {@hide}
          */
-        public static final int PRIVATE_FLAG_NAV_HIDE_FORCED = 0x02000000;
+        public static final int PRIVATE_FLAG_NAV_HIDE_FORCED = 0x06000000;
 
         /**
          * The window had not set FULLSCREEN flag so don't handle it as fullscreen in layoutWindowLw
          *
          * {@hide}
          */
-        public static final int PRIVATE_FLAG_WAS_NOT_FULLSCREEN = 0x03000000;
+        public static final int PRIVATE_FLAG_WAS_NOT_FULLSCREEN = 0x07000000;
 
         /**
          * Control flags that are private to the platform.