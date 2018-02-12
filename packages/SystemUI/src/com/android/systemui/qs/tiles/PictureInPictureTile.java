sanchitrohilla/abc_frameworks_base

PiP tile: add toast if pip mode is not avail
Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8

packages/SystemUI/src/com/android/systemui/qs/tiles/PictureInPictureTile.java
@@ -15,10 +15,12 @@
 
 package com.android.systemui.qs.tiles;
 
+import android.content.pm.ActivityInfo;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.service.quicksettings.Tile;
+import android.widget.Toast;
 
 import com.android.internal.util.abc.AbcUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -50,6 +52,12 @@ public class PictureInPictureTile extends QSTileImpl<BooleanState> {
     @Override
     public void handleClick() {
         mHost.collapsePanels();
+        ActivityInfo ai = AbcUtils.getRunningActivityInfo(mContext);
+        if (ai != null && !ai.supportsPictureInPicture()) {
+            Toast.makeText(mContext, mContext.getString(
+                    R.string.quick_settings_pip_tile_app_na), Toast.LENGTH_LONG).show();
+            return;
+        }
         AbcUtils.sendKeycode(171);
     }
 

crdroidandroid/android_frameworks_base

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/PictureInPictureTile.java
@@ -15,10 +15,12 @@
 
 package com.android.systemui.qs.tiles;
 
+import android.content.pm.ActivityInfo;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.service.quicksettings.Tile;
+import android.widget.Toast;
 
 import com.android.internal.util.crdroid.Utils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -50,6 +52,12 @@ public class PictureInPictureTile extends QSTileImpl<BooleanState> {
     @Override
     public void handleClick() {
         mHost.collapsePanels();
+        ActivityInfo ai = Utils.getRunningActivityInfo(mContext);
+        if (ai != null && !ai.supportsPictureInPicture()) {
+            Toast.makeText(mContext, mContext.getString(
+                    R.string.quick_settings_pip_tile_app_na), Toast.LENGTH_LONG).show();
+            return;
+        }
         Utils.sendKeycode(171);
     }
 