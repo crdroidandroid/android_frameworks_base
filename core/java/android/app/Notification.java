6fa4e7c48ff99180c3e18d60d2843cd96eb49c2f
Marko Man <darkobas@gmail.com>
2018-03-06 20:13:49 -0500

    Add support for theming notifications
    @maxwen Fix notification text when using dark themes
    
    Change-Id: Iea927eb3d95a9e9d07f07f61b2b420c95056dd5c
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4866,7 +4866,8 @@ public class Notification implements Parcelable
 
         private CharSequence processLegacyText(CharSequence charSequence, boolean ambient) {
             boolean isAlreadyLightText = isLegacy() || textColorsNeedInversion();
-            boolean wantLightText = ambient;
+            boolean wantLightText = ambient || mContext.getResources().getBoolean(
+                    R.bool.config_useDarkBgNotificationIconTextTinting);
             if (isAlreadyLightText != wantLightText) {
                 return getColorUtil().invertCharSequenceColors(charSequence);
             } else {
@@ -4946,10 +4947,8 @@ public class Notification implements Parcelable
                 ensureColors();
                 color = mSecondaryTextColor;
             } else {
-                boolean isDark = mInNightMode || mContext.getResources()
-                        .getBoolean(R.bool.config_useDarkBgNotificationIconTextTinting);
                 color = NotificationColorUtil.resolveContrastColor(mContext, mN.color,
-                        background, isDark);
+                        background, mInNightMode);
             }
             if (Color.alpha(color) < 255) {
                 // alpha doesn't go well for color filters, so let's blend it manually
@@ -6314,7 +6313,7 @@ public class Notification implements Parcelable
                 sb.append(bidi.unicodeWrap(m.mSender),
                         makeFontColorSpan(colorize
                                 ? builder.getPrimaryTextColor()
-                                : builder.getSenderTextColor()),
+                                : mBuilder.resolveContrastColor()),
                         0 /* flags */);
             }
             CharSequence text = m.mText == null ? "" : m.mText;
