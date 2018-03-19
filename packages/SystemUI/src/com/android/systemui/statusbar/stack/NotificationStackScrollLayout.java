a46f6e3b64db8ae91604b5e23f82ba800def7eec
wonyoung <won_0.byun@samsung.com>
2018-03-19 16:52:59 +0900

    SystemUI: modify overscroll bug in NotificationStackScrollLayout
    After overscroll, notifiation panel is seen as being cut.
    Initialize overscroll when you unlock
    
    Test: open notificaiton panel at shade lock state
    Test: Use overscroll to raise notificationStackscrollLayout and hide it behind quickpanel.
    Test: At the same time, Swipe touch in empty space at the bottom.
    Test: Then, notificationStackscrollLayout goes up. Also, when the panel is opened, it is cut and visible.
    
    Change-Id: I6167ae33557f24e3b19de97576181792d9c23fd1
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -4289,6 +4289,9 @@ public class NotificationStackScrollLayout extends ViewGroup
     }
 
     public void setStatusBarState(int statusBarState) {
+        if (mStatusBarState != statusBarState) {
+            endDrag();
+        }
         mStatusBarState = statusBarState;
         mAmbientState.setStatusBarState(statusBarState);
     }
