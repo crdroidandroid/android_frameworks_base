d85cfa59fd4f9021698a5462b36474e53b8d0671
Preeti Nagar <pnagar@codeaurora.org>
2018-04-03 16:34:25 +0530

    SEEMP: framework instrumentation and AppProtect features
    - Changes for Android-P porting
    - Instrument framework files to log events of interest for QSSP.
    - Fix the String out of boundary.
    - Add optional package verifier to PackageManagerService
      - In additional to the required and sufficient package verifiers,
        add support for an optional verifier in PackageManager. The
        optional verifier will act as a required verifier if a required
        verifier is not found in the system. It will act as a secondary
        verifier if a required verifier is present.
    - Add auth framework for outgoing SMS messages.
      - Add the APIs required to register an authorization agent in order
        to allow/reject outgoing SMS messages and the core service that
        implements the sms security model.
    - Do not connect to seempdw socket in zygote
      - Do not connect to seempdw socket in zygote. The socket
        initialization is skipped if the parent process id is 1, which is
        always the case for zygote.
    - Moved instrumentation point to PackageInstaller and some
      minor fixes.
    - Add action for optional package verifier along other legacy
      actions to handle tightened StrictMode policy in Android P.
    
    Change-Id: I38a8ace9943fae0ad2ac7bc2932f3deb790359a6
    CRs-Fixed: 2219016
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/accounts/AccountManagerService.java
+++ services/core/java/com/android/server/accounts/AccountManagerService.java
@@ -1473,6 +1473,7 @@ public class AccountManagerService
 
     @Override
     public String getPassword(Account account) {
+        android.util.SeempLog.record(14);
         int callingUid = Binder.getCallingUid();
         if (Log.isLoggable(TAG, Log.VERBOSE)) {
             Log.v(TAG, "getPassword: " + account
@@ -1553,6 +1554,7 @@ public class AccountManagerService
 
     @Override
     public String getUserData(Account account, String key) {
+        android.util.SeempLog.record(15);
         final int callingUid = Binder.getCallingUid();
         if (Log.isLoggable(TAG, Log.VERBOSE)) {
             String msg = String.format("getUserData( account: %s, key: %s, callerUid: %s, pid: %s",
@@ -2132,6 +2134,7 @@ public class AccountManagerService
     @Override
     public void removeAccount(IAccountManagerResponse response, Account account,
             boolean expectActivityLaunch) {
+        android.util.SeempLog.record(17);
         removeAccountAsUser(
                 response,
                 account,
@@ -2573,6 +2576,7 @@ public class AccountManagerService
 
     @Override
     public void setPassword(Account account, String password) {
+        android.util.SeempLog.record(18);
         final int callingUid = Binder.getCallingUid();
         if (Log.isLoggable(TAG, Log.VERBOSE)) {
             Log.v(TAG, "setAuthToken: " + account
@@ -2639,6 +2643,7 @@ public class AccountManagerService
 
     @Override
     public void clearPassword(Account account) {
+        android.util.SeempLog.record(19);
         final int callingUid = Binder.getCallingUid();
         if (Log.isLoggable(TAG, Log.VERBOSE)) {
             Log.v(TAG, "clearPassword: " + account
@@ -2665,6 +2670,7 @@ public class AccountManagerService
 
     @Override
     public void setUserData(Account account, String key, String value) {
+        android.util.SeempLog.record(20);
         final int callingUid = Binder.getCallingUid();
         if (Log.isLoggable(TAG, Log.VERBOSE)) {
             Log.v(TAG, "setUserData: " + account
@@ -3157,6 +3163,7 @@ public class AccountManagerService
     public void addAccount(final IAccountManagerResponse response, final String accountType,
             final String authTokenType, final String[] requiredFeatures,
             final boolean expectActivityLaunch, final Bundle optionsIn) {
+        android.util.SeempLog.record(16);
         Bundle.setDefusable(optionsIn, true);
         if (Log.isLoggable(TAG, Log.VERBOSE)) {
             Log.v(TAG, "addAccount: accountType " + accountType
@@ -3910,6 +3917,7 @@ public class AccountManagerService
     @Override
     public void editProperties(IAccountManagerResponse response, final String accountType,
             final boolean expectActivityLaunch) {
+        android.util.SeempLog.record(21);
         final int callingUid = Binder.getCallingUid();
         if (Log.isLoggable(TAG, Log.VERBOSE)) {
             Log.v(TAG, "editProperties: accountType " + accountType
