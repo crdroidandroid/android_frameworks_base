ea331003ec877ee93192c48887750fb8baae9f46
Nico60 <ndhouailly@gmail.com>
2018-04-11 21:03:36 +0200

    base: QS panel opacity [1/2]
    @neobuddy89: Fixes and simplify.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5081,6 +5081,12 @@ public final class Settings {
                 "status_bar_file_header_image";
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
