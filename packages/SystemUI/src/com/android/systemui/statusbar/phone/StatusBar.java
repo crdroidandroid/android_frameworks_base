9bd4409b50584c81887feb06e92503a00699a7d1
ezio84 <brabus84@gmail.com>
2018-04-14 14:52:34 +0200

    Cleanup SlimRecents media info/colors code [2/2]
    Change-Id: Ic7767aea5d50ea0ff0a1178bd124feada9efac21
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -822,9 +822,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 if (isAmbientContainerAvailable()) {
                     ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata);
                 }
-                if (mSlimRecents != null) {
-                    mSlimRecents.setMediaInfo(mMediaMetadata);
-                }
                 isMediaPlaying = true;
                 // NotificationInflater calls async MediaNotificationProcessoron to create notification
                 // colors and when finished will trigger AsyncInflationFinished for all registered callbacks
@@ -2085,7 +2082,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 if (icon != null) {
                     drawable = icon.loadDrawable(mContext);
                 }
-                mSlimRecents.setMediaColors(n.isColorizedMedia(), colors, drawable);
+                mSlimRecents.setMediaColors(n.isColorizedMedia(), colors, drawable, mMediaMetadata);
             }
         }
     }
