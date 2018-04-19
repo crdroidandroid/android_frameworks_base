998e99ff83f0f470a2f6eacf8681b9c73769266e
Pranav Vashi <neobuddy89@gmail.com>
2018-04-19 16:47:25 +0530

    Switch to stock behavior if ambient display toggle is disabled
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -79,7 +79,7 @@ public class KeyguardStatusView extends GridLayout implements
     private TextClock mClockView;
     private TextView mOwnerInfo;
     private ViewGroup mClockContainer;
-    //private ChargingView mBatteryDoze;
+    private ChargingView mBatteryDoze;
     private View mKeyguardStatusArea;
     private Runnable mPendingMarqueeStart;
     private Handler mHandler;
@@ -106,6 +106,7 @@ public class KeyguardStatusView extends GridLayout implements
     private boolean mShowWeather;
     private boolean mShowConditionIcon;
     private boolean mShowLocation;
+    private boolean mShowAmbientBattery;
 
     private SettingsObserver mSettingsObserver;
 
@@ -192,9 +193,9 @@ public class KeyguardStatusView extends GridLayout implements
         mClockView = findViewById(R.id.clock_view);
         mClockView.setShowCurrentUserTime(true);
         mOwnerInfo = findViewById(R.id.owner_info);
-        //mBatteryDoze = findViewById(R.id.battery_doze);
+        mBatteryDoze = findViewById(R.id.battery_doze);
         mKeyguardStatusArea = findViewById(R.id.keyguard_status_area);
-        mVisibleInDoze = new View[]{/*mBatteryDoze, */mClockView, mKeyguardStatusArea};
+        mVisibleInDoze = new View[]{mBatteryDoze, mClockView, mKeyguardStatusArea};
         mTextColor = mClockView.getCurrentTextColor();
         mDateTextColor = mDateView.getCurrentTextColor();
         mAlarmTextColor = mAlarmStatusView.getCurrentTextColor();
@@ -547,7 +548,12 @@ public class KeyguardStatusView extends GridLayout implements
         }
 
         updateDozeVisibleViews();
-        //mBatteryDoze.setDark(dark);
+
+        if (mShowAmbientBattery) {
+            mBatteryDoze.setAlpha(dark ? 0 : 1);
+        } else {
+            mBatteryDoze.setDark(dark);
+        }
         mClockView.setTextColor(ColorUtils.blendARGB(mTextColor, Color.WHITE, darkAmount));
         mDateView.setTextColor(ColorUtils.blendARGB(mDateTextColor, Color.WHITE, darkAmount));
         int blendedAlarmColor = ColorUtils.blendARGB(mAlarmTextColor, Color.WHITE, darkAmount);
@@ -589,6 +595,8 @@ public class KeyguardStatusView extends GridLayout implements
                   Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION), false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                   Settings.System.OMNIJAWS_WEATHER_ICON_PACK), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.AMBIENT_BATTERY_PERCENT), false, this, UserHandle.USER_ALL);
 
             mShowWeather = Settings.System.getIntForUser(resolver,
                   Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0, UserHandle.USER_CURRENT) == 1;
@@ -596,6 +604,8 @@ public class KeyguardStatusView extends GridLayout implements
                   Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, 1, UserHandle.USER_CURRENT) == 1;
             mShowLocation = Settings.System.getIntForUser(resolver,
                   Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1, UserHandle.USER_CURRENT) == 1;
+            mShowAmbientBattery = Settings.System.getIntForUser(resolver,
+                  Settings.System.AMBIENT_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) == 1;
             queryAndUpdateWeather();
         }
 
@@ -626,6 +636,10 @@ public class KeyguardStatusView extends GridLayout implements
             } else if (uri.equals(Settings.System.getUriFor(
                    Settings.System.OMNIJAWS_WEATHER_ICON_PACK))) {
                 updateWeather();
+            } else if (uri.equals(Settings.System.getUriFor(
+                   Settings.System.AMBIENT_BATTERY_PERCENT))) {
+                mShowAmbientBattery = Settings.System.getIntForUser(resolver,
+                    Settings.System.AMBIENT_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) == 1;
             }
         }
     }
