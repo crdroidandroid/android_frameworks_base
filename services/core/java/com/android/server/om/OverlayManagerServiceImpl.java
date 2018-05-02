a09d7ab6a60fda94aad6b41ffd3b99075b3e4ac5
Mårten Kongstad <marten.kongstad@sony.com>
2018-05-02 10:06:34 +0200

    OMS: handle target or overlay package disabled
    Update what happens when the package manager disables a package:
    
      - if a target is disabled, update its overlays to STATE_MISSING_TARGET
    
      - if an overlay is disabled, update its state to the new state
        STATE_OVERLAY_NOT_AVAILABLE
    
    In both cases, OverlayInfo.isEnabled will return false.
    
    Note: if the package manager only disables a single component, nothing
    happens to the overlays: OMS only looks at ApplicationInfo.enabled, not
    the individual ComponentInfo.enabled fields.
    
    Test: atest OverlayHostTests
    Change-Id: I8c417acc0175739002a0350eed1913c32cc73337
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -21,6 +21,7 @@ import static android.content.om.OverlayInfo.STATE_ENABLED;
 import static android.content.om.OverlayInfo.STATE_ENABLED_STATIC;
 import static android.content.om.OverlayInfo.STATE_MISSING_TARGET;
 import static android.content.om.OverlayInfo.STATE_NO_IDMAP;
+import static android.content.om.OverlayInfo.STATE_OVERLAY_NOT_AVAILABLE;
 import static android.content.om.OverlayInfo.STATE_OVERLAY_UPGRADING;
 import static android.content.om.OverlayInfo.STATE_TARGET_UPGRADING;
 
@@ -682,6 +683,14 @@ final class OverlayManagerServiceImpl {
 
         if (targetPackage == null) {
             return STATE_MISSING_TARGET;
+        }
+
+        if (!targetPackage.applicationInfo.enabled) {
+            return STATE_MISSING_TARGET;
+        }
+
+        if (!overlayPackage.applicationInfo.enabled) {
+            return STATE_OVERLAY_NOT_AVAILABLE;
         }
 
         if (!mIdmapManager.idmapExists(overlayPackage, userId)) {
