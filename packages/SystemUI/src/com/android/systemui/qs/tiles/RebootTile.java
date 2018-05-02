b2fc90bec42e7639311ac6ff1363122c31b20b71
Pranav Vashi <neobuddy89@gmail.com>
2018-05-03 03:45:46 +0530

    Extend reboot tile to add power off
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -30,7 +30,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl;
 
 public class RebootTile extends QSTileImpl<BooleanState> {
 
-    private boolean mRebootToRecovery = false;
+    private int mRebootToRecovery = 0;
 
     public RebootTile(QSHost host) {
         super(host);
@@ -43,7 +43,13 @@ public class RebootTile extends QSTileImpl<BooleanState> {
 
     @Override
     public void handleClick() {
-        mRebootToRecovery = !mRebootToRecovery;
+        if (mRebootToRecovery == 0) {
+            mRebootToRecovery = 1;
+        } else if (mRebootToRecovery == 1) {
+            mRebootToRecovery = 2;
+        } else {
+            mRebootToRecovery = 0;
+        }
         refreshState();
     }
 
@@ -55,8 +61,10 @@ public class RebootTile extends QSTileImpl<BooleanState> {
             public void run() {
                 PowerManager pm =
                     (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-                if (mRebootToRecovery) {
+                if (mRebootToRecovery == 1) {
                     pm.rebootCustom(PowerManager.REBOOT_RECOVERY);
+                } else if (mRebootToRecovery == 2) {
+                    pm.shutdown(false, pm.SHUTDOWN_USER_REQUESTED, false);
                 } else {
                     pm.reboot("");
                 }
@@ -81,9 +89,12 @@ public class RebootTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (mRebootToRecovery) {
+        if (mRebootToRecovery == 1) {
             state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
             state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
+        } else if (mRebootToRecovery == 2) {
+            state.label = mContext.getString(R.string.quick_settings_poweroff_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_poweroff);
         } else {
             state.label = mContext.getString(R.string.quick_settings_reboot_label);
             state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
