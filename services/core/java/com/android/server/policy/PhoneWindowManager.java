6db571f1599f2e725d3d8e5682ad7d966baf67eb
Pranav Vashi <neobuddy89@gmail.com>
2018-05-05 22:30:41 +0530

    Add device key action to kill app [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -652,7 +652,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mVolumeAnswerCall;
 
     boolean mKillAppLongpressBack;
-    int mBackKillTimeout;
+    int mKillTimeout;
 
     int mPointerLocationMode = 0; // guarded by mLock
 
@@ -1963,7 +1963,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mHandler.post(mScreenrecordRunnable);
     }
 
-    Runnable mBackLongPress = new Runnable() {
+    Runnable mCloseApp = new Runnable() {
         public void run() {
             if (unpinActivity(false)) {
                 return;
@@ -2159,6 +2159,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case SPLIT_SCREEN:
                 toggleSplitScreen();
                 break;
+            case CLOSE_APP:
+                closeApp();
+                break;
             default:
                 break;
         }
@@ -2339,7 +2342,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mDeviceHardwareKeys = mContext.getResources().getInteger(
                 org.lineageos.platform.internal.R.integer.config_deviceHardwareKeys);
-        mBackKillTimeout = mContext.getResources().getInteger(
+        mKillTimeout = mContext.getResources().getInteger(
                 org.lineageos.platform.internal.R.integer.config_backKillTimeout);
 
         updateKeyAssignments();
@@ -3968,7 +3971,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (keyCode == KeyEvent.KEYCODE_BACK && !down) {
-            mHandler.removeCallbacks(mBackLongPress);
+            mHandler.removeCallbacks(mCloseApp);
         }
 
         // First we always handle the home key here, so applications
@@ -4347,7 +4350,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         } else if (keyCode == KeyEvent.KEYCODE_BACK) {
             if (mKillAppLongpressBack || unpinActivity(true)) {
                 if (down && repeatCount == 0) {
-                    mHandler.postDelayed(mBackLongPress, mBackKillTimeout);
+                    closeApp();
                 }
             }
         }
@@ -4826,6 +4829,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (statusbar != null) {
             statusbar.toggleSplitScreen();
         }
+    }
+
+   private void closeApp() {
+        mHandler.postDelayed(mCloseApp, mKillTimeout);
     }
 
     @Override
