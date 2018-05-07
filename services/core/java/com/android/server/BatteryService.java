bb862772fa86fcf7eb0cdc97e53ce97f11e8dde9
Razziell <razziell@ya.ru>
2018-05-07 20:41:48 +0300

    Battery light: 100% charged level (1/3)
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -1601,6 +1601,7 @@ public final class BatteryService extends SystemService {
         private final int mBatteryLowARGB;
         private final int mBatteryMediumARGB;
         private final int mBatteryFullARGB;
+        private final int mBatteryReallyFullARGB;
         private final int mBatteryLedOn;
         private final int mBatteryLedOff;
         private final int mBatteryLowBehavior;
@@ -1614,6 +1615,8 @@ public final class BatteryService extends SystemService {
                     com.android.internal.R.integer.config_notificationsBatteryMediumARGB);
             mBatteryFullARGB = context.getResources().getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryFullARGB);
+            mBatteryReallyFullARGB = context.getResources().getInteger(
+                    com.android.internal.R.integer.config_notificationsBatteryReallyFullARGB);
             mBatteryLedOn = context.getResources().getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryLedOn);
             mBatteryLedOff = context.getResources().getInteger(
