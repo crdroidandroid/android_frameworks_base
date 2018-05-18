441e903d099cd162d8489b20c2106e654775a238
Alex <alex@dirtyunicorns.com>
2018-05-17 20:55:33 -0500

    Introduce lockscreen clock/date styles [1/2]
    Inspired by OnePlus and their recent options to switch lockscreen
    clock styles on the OP6, I took it upon myself to do the same with our ROM.
    
    This isn't as fancy as OnePlus' because is just using a list preference instead
    of adding in all the eye candy but it gets the job done!
    
    Below is a run down of what took place...
    
    - Mirrored the analog clock class to avoid issues with any apps that may
    or may not be utilizing. Google deprecated this class back in 2015 but there's
    still a chance that something may go wrong some where. Better safe than sorry.
    
    https://github.com/DirtyUnicorns/android_frameworks_base/blob/o8x/core/java/android/widget/AnalogClock.java
    
    - Added the analog clock to the keyguard and made it optional
    - Manipulated the clockview format for both 12 and 24 hour to achieved the 'sammy' style
    - Made the sammy style optional and created a switch case for all 3.
    - Set padding, singeline and other layout changes programmatically to minimize the jank
    - Increased the date widget size to 20sp becuase it just looks better
    - Reworked/cleaned up some existing code (Huge thanks to @ezio84)
    - Created bold sammy by using html formatting, based off the sammy one
    - Created bold digital by using html formatting, based off the digital one
    - Created some date styles to match clock styles
    - Vectorized the analog clock images so we can theme
    - Modified Analog clock class to fix ambient display issues (@ezio84)
    - Added DeadPool analog clock because Fuck you, that's why! lol
    
    For right now there's six clock styles and 3 date styles:
    
    Clock styles---
    - Default
    - Digital (bold)
    - Analog
    - Analog (deadpool)
    - Sammy
    - Sammy (bold)
    
    Date styles----
    - Default
    - Semi transparent box
    - Semi transparent box (round corners)
    
    Added -
    * Fixes and default bold date style by @ezio84
    * SpideyAnalog clock by @ishubhamsingh and @@KrapK
    * Fine tuning by @neobuddy89
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ core/java/android/widget/SpideyAnalogClock.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2006 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.text.format.Time;
+import android.util.AttributeSet;
+import android.widget.RemoteViews.RemoteView;
+
+/**
+ * This widget display an analogic clock with two hands for hours and
+ * minutes.
+ *
+ * @attr ref android.R.styleable#SpideyAnalogClock_dial
+ * @attr ref android.R.styleable#SpideyAnalogClock_hand_hour
+ * @attr ref android.R.styleable#SpideyAnalogClock_hand_minute
+ * @deprecated This widget is no longer supported.
+ * @hide
+ */
+@RemoteView
+@Deprecated
+public class SpideyAnalogClock extends android.widget.CustomAnalogClock {
+
+    private boolean mChanged;
+
+    public SpideyAnalogClock(Context context) {
+        this(context, null);
+    }
+
+    public SpideyAnalogClock(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SpideyAnalogClock(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SpideyAnalogClock(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        final Resources r = context.getResources();
+        final TypedArray a = context.obtainStyledAttributes(
+                attrs, com.android.internal.R.styleable.SpideyAnalogClock, defStyleAttr, defStyleRes);
+
+        mDial = a.getDrawable(com.android.internal.R.styleable.SpideyAnalogClock_spidey_dial);
+        if (mDial == null) {
+            mDial = context.getDrawable(com.android.internal.R.drawable.spidey_clock_dial);
+            mDialAmbient = context.getDrawable(com.android.internal.R.drawable.spidey_clock_dial_ambient);
+        }
+
+        mHourHand = a.getDrawable(com.android.internal.R.styleable.SpideyAnalogClock_spidey_hand_hour);
+        if (mHourHand == null) {
+            mHourHand = context.getDrawable(com.android.internal.R.drawable.spidey_clock_hand_hour);
+        }
+
+        mMinuteHand = a.getDrawable(com.android.internal.R.styleable.SpideyAnalogClock_spidey_hand_minute);
+        if (mMinuteHand == null) {
+            mMinuteHand = context.getDrawable(com.android.internal.R.drawable.spidey_clock_hand_minute);
+        }
+
+        a.recycle();
+
+        mCalendar = new Time();
+
+        mDialWidth = mDial.getIntrinsicWidth();
+        mDialHeight = mDial.getIntrinsicHeight();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        boolean changed = mChanged;
+        if (changed) {
+            mChanged = false;
+        }
+
+        int availableWidth = mRight - mLeft;
+        int availableHeight = mBottom - mTop;
+
+        int x = availableWidth / 2;
+        int y = availableHeight / 2;
+
+        final Drawable dial = mIsAmbientDisplay ? mDialAmbient : mDial;
+        int w = dial.getIntrinsicWidth();
+        int h = dial.getIntrinsicHeight();
+
+        boolean scaled = false;
+
+        if (availableWidth < w || availableHeight < h) {
+            scaled = true;
+            float scale = Math.min((float) availableWidth / (float) w,
+                    (float) availableHeight / (float) h);
+            canvas.save();
+            canvas.scale(scale, scale, x, y);
+        }
+
+        if (changed) {
+            dial.setBounds(x - (w / 2), y - (h / 2), x + (w / 2), y + (h / 2));
+        }
+        dial.draw(canvas);
+
+        canvas.save();
+        canvas.rotate(mHour / 12.0f * 360.0f, x, y);
+        final Drawable hourHand = mHourHand;
+        if (changed) {
+            w = hourHand.getIntrinsicWidth();
+            h = hourHand.getIntrinsicHeight();
+            hourHand.setBounds(x - (w / 2), y - (h / 2), x + (w / 2), y + (h / 2));
+        }
+        if (mIsAmbientDisplay) {
+            hourHand.setTint(Color.GRAY);
+        } else {
+            hourHand.setTintList(null);
+        }
+        hourHand.draw(canvas);
+        canvas.restore();
+
+        canvas.save();
+        canvas.rotate(mMinutes / 60.0f * 360.0f, x, y);
+
+        final Drawable minuteHand = mMinuteHand;
+        if (changed) {
+            w = minuteHand.getIntrinsicWidth();
+            h = minuteHand.getIntrinsicHeight();
+            minuteHand.setBounds(x - (w / 2), y - (h / 2), x + (w / 2), y + (h / 2));
+        }
+        if (mIsAmbientDisplay) {
+            minuteHand.setTint(Color.GRAY);
+        } else {
+            minuteHand.setTintList(null);
+        }
+        minuteHand.draw(canvas);
+        canvas.restore();
+
+        if (scaled) {
+            canvas.restore();
+        }
+    }
+}
