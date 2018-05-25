41705b7332d3fa5351ceed7f09f83c259f60c1b0
eddy-sh.chen <eddy-sh.chen@mstarsemi.com>
2018-05-25 16:43:08 +0800

    services: Do not call finishKeyguardDrawn with PWM's mlock
    Otherwise it may cause a deadlock between
    PWM's mlock and WMS's mWindowMap.
    Just post the work to the policy handler instead.
    
    Bug: 80279029
    Change-Id: Idae333687cf5c88e1893ee216c278b9e6c17dd98
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7559,7 +7559,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             } else {
                 if (DEBUG_WAKEUP) Slog.d(TAG,
                         "null mKeyguardDelegate: setting mKeyguardDrawComplete.");
-                finishKeyguardDrawn();
+                mHandler.sendEmptyMessage(MSG_KEYGUARD_DRAWN_COMPLETE);
             }
         }
     }
