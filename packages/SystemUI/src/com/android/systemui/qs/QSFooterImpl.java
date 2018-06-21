aab3e3fed97cfb1b082f2086198924770dfce256
maxwen <max.weninger@gmail.com>
2018-06-21 13:46:22 +0200

    base: SystemUI: split calendar from clock in qs footer
    pressing alarm -> clock
    pressing date -> calendar
    
    Change-Id: I09598eca6a3c60ff81a852f10c0acc65360eb7aa
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -28,8 +28,10 @@ import android.content.res.Resources;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
+import android.net.Uri;
 import android.os.UserManager;
 import android.provider.AlarmClock;
+import android.provider.CalendarContract;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.util.AttributeSet;
@@ -123,6 +125,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
 
         mDateTimeGroup = findViewById(id.date_time_alarm_group);
         mDate = findViewById(R.id.date);
+        mDate.setOnClickListener(this);
 
         mExpandIndicator = findViewById(R.id.expand_indicator);
         mSettingsButton = findViewById(R.id.settings_button);
@@ -135,7 +138,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
 
         mAlarmStatusCollapsed = findViewById(R.id.alarm_status_collapsed);
         mAlarmStatus = findViewById(R.id.alarm_status);
-        mDateTimeGroup.setOnClickListener(this);
+        mAlarmStatus.setOnClickListener(this);
 
         mMultiUserSwitch = findViewById(R.id.multi_user_switch);
         mMultiUserAvatar = mMultiUserSwitch.findViewById(R.id.multi_user_avatar);
@@ -364,7 +367,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
                     mExpanded ? MetricsProto.MetricsEvent.ACTION_QS_EXPANDED_SETTINGS_LAUNCH
                             : MetricsProto.MetricsEvent.ACTION_QS_COLLAPSED_SETTINGS_LAUNCH);
                 startSettingsActivity();
-        } else if (v == mDateTimeGroup) {
+        } else if (v == mAlarmStatus) {
             Dependency.get(MetricsLogger.class).action(ACTION_QS_DATE,
                     mNextAlarm != null);
             if (mNextAlarm != null && mNextAlarm.getShowIntent() != null) {
@@ -374,6 +377,12 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
                 mActivityStarter.postStartActivityDismissingKeyguard(new Intent(
                         AlarmClock.ACTION_SHOW_ALARMS), 0);
             }
+        } else if (v == mDate) {
+            Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+            builder.appendPath("time");
+            builder.appendPath(Long.toString(System.currentTimeMillis()));
+            Intent todayIntent = new Intent(Intent.ACTION_VIEW, builder.build());
+            mActivityStarter.postStartActivityDismissingKeyguard(todayIntent, 0);
         } else if (v == mRunningServicesButton) {
             MetricsLogger.action(mContext,
                     mExpanded ? MetricsProto.MetricsEvent.ACTION_QS_EXPANDED_SETTINGS_LAUNCH
