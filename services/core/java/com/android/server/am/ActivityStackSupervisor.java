30820dd565ef704fbd00a74165845e98152df723
qiushi.han <qiushi.han@spreadtrum.com>
2018-06-29 13:53:25 +0800

    Remove unused calling for better performance
    getWindowContainerBounds will be called many times, trying to hold
    WMS lock, leading to bad performance, but no one will use this taskBounds.
    
    Test: make services
    Change-Id: I8719a06c5a71b1a33b579dbd0e7863550265d975
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -4093,7 +4093,6 @@ public class ActivityStackSupervisor extends ConfigurationContainer implements D
         final int numTasks = tasks.size();
         int[] taskIds = new int[numTasks];
         String[] taskNames = new String[numTasks];
-        Rect[] taskBounds = new Rect[numTasks];
         int[] taskUserIds = new int[numTasks];
         for (int i = 0; i < numTasks; ++i) {
             final TaskRecord task = tasks.get(i);
@@ -4102,13 +4101,10 @@ public class ActivityStackSupervisor extends ConfigurationContainer implements D
                     : task.realActivity != null ? task.realActivity.flattenToString()
                     : task.getTopActivity() != null ? task.getTopActivity().packageName
                     : "unknown";
-            taskBounds[i] = new Rect();
-            task.getWindowContainerBounds(taskBounds[i]);
             taskUserIds[i] = task.userId;
         }
         info.taskIds = taskIds;
         info.taskNames = taskNames;
-        info.taskBounds = taskBounds;
         info.taskUserIds = taskUserIds;
 
         final ActivityRecord top = stack.topRunningActivityLocked();
