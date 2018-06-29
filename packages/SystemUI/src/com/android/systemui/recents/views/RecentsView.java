f1a11196313062cec56c0a5c82100c37fc2ed84d
Murat Kozan <muratkozan350@gmail.com>
2018-06-29 07:22:52 +0300

    Make Recents "Free Ram" text translatable
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -570,7 +570,7 @@ public class RecentsView extends FrameLayout implements TunerService.Tunable {
         mAm.getMemoryInfo(memInfo);
         int available = (int)(memInfo.availMem / 1048576L);
         int max = (int)(memInfo.totalMem / 1048576L);
-        mMemText.setText("Free RAM: " + String.valueOf(available) + "MB");
+        mMemText.setText(String.format(getResources().getString(R.string.recents_free_ram),available));
         mMemBar.setMax(max);
         mMemBar.setProgress(available);
     }
