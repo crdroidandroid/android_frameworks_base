102e7b8cad83bd745ea814e6fc51c83564ea099d
Pranav Vashi <neobuddy89@gmail.com>
2018-08-04 23:07:56 +0530

    Add separate tunable for clock seconds
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -70,7 +70,8 @@ public class TunerServiceImpl extends TunerService {
     // Things that use the tunable infrastructure but are now real user settings and
     // shouldn't be reset with tuner settings.
     private static final String[] RESET_BLACKLIST = new String[] {
-            Clock.CLOCK_STYLE,
+            Clock.STATUS_BAR_AM_PM,
+            Clock.STATUS_BAR_CLOCK_SECONDS,
             ClockController.STATUS_BAR_CLOCK,
             NavigationBarView.NAVIGATION_BAR_MENU_ARROW_KEYS,
             NotificationPanelView.DOUBLE_TAP_SLEEP_GESTURE,
