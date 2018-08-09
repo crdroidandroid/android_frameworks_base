7dd0aee0a5be209f7c8de55622190427734233fa
Pranav Vashi <neobuddy89@gmail.com>
2018-08-09 08:59:17 +0530

    Clean up back animation view for edge gesture
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8544,12 +8544,6 @@ public final class Settings {
         public static final String EDGE_GESTURES_BACK_SHOW_UI_FEEDBACK = "edge_gestures_back_show_ui_feedback";
 
         /**
-         * Use black arrow theme instead of the white version.
-         * @hide
-         */
-        public static final String EDGE_GESTURES_BACK_USE_BLACK_ARROW = "edge_gestures_back_use_black_arrow";
-
-        /**
          * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
          * The value is boolean (1 or 0).
          * @hide
