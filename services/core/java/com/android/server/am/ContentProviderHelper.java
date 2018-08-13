66534d5a94d05154bc65f18acf423d11570de686
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-08-13 12:19:51 -0400

    Shell: Don't show bugreport on DocumentsUI
    Change-Id: I3dde24312f416b1ae2d365fea683aacd8970d923
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ContentProviderHelper.java
+++ services/core/java/com/android/server/am/ContentProviderHelper.java
@@ -1356,7 +1356,6 @@ public class ContentProviderHelper {
         mService.mConstants.start(mService.mContext.getContentResolver());
         mService.mCoreSettingsObserver = new CoreSettingsObserver(mService);
         mService.mActivityTaskManager.installSystemProviders();
-        new DevelopmentSettingsObserver(); // init to observe developer settings enable/disable
         SettingsToPropertiesMapper.start(mService.mContext.getContentResolver());
         mService.mOomAdjuster.initSettings();
 
