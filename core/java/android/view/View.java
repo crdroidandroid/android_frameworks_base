252974e61508586e86fddeb4fb897c37eff59f78
guyunjian <guyunjian@xiaomi.com>
2018-08-28 11:22:09 +0800

    DragSurface: destroy surfacecontrol in view site
    SurfaceControl should be destroyed after perfromDrag. It will not longer be used.
    Otherwise it will be destroyed after gc.
    
    Change-Id: I57bc60fd38e41e7e10301f94b70f00bbb32de5e4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -26413,6 +26413,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                 surface.destroy();
             }
             session.kill();
+            surface.destroy();
         }
     }
 
