d88123f54b594d753e290b54226fc3b252776647
maxwen <max.weninger@gmail.com>
2018-08-30 12:25:45 +0200

    base: add back weather parts [1/3]
    * @neobuddy89: Updated for Android10
    
    Includes below changes:
    
        From: LorDClockaN <lordclockan@gmail.com>
        Date: Mon, 6 Mar 2017 22:32:58 +0100
        Subject: [PATCH 5/9] WeatherTile: Start Google Weather on LongClick
    
        Change-Id: I049e026bcdcdea34ed61a07e6de953d91c6e57c1
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: SpiritCroc <spiritcroc@gmail.com>
        Date: Wed, 8 Mar 2017 20:26:35 +0100
        Subject: [PATCH 6/9] Weather tile longclick: alternative weather apps when
         google weather not installed
    
        Change-Id: I2ea721a53d63dbd80edcc7a29e791f89c8cc0859
        Signed-off-by: Shubham Singh <coolsks94@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: El Dainosor <eldainosor@gmail.com>
        Date: Thu, 22 Mar 2018 00:11:04 +0000
        Subject: [PATCH 7/9] Weather tile: Use the alternate apps first
    
        This will make the weather tile, on long press, start the alternative weather apps instead of shoveling google weather no matter what
        Thanks ishubhamsingh for the help with the code part
    
        Change-Id: I92839d79ec6e472fc619fee2dfb9fd4d27605708
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: El Dainosor <eldainosor@gmail.com>
        Date: Sat, 18 Jan 2020 21:58:09 +0100
        Subject: [PATCH 9/9] WeatherTile: Send weather info to a Secondary Label
    
        Instead of making the actual status the title, let's show it as
        secondary text, else it's a bad, ugly, meanie QS tile.
    
        Change-Id: Ifc4c5f12e6759d016ce77a35c6bdf0d8df2db8ce
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5252,6 +5252,11 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
 
         /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
