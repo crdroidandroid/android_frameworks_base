d31852509259eb38c602662296189ddd9eadecd3
George G <kreach3r@gmail.com>
2018-09-02 15:11:50 +0300

    SubstratumService: Refactor & cleanup according to AOSP conventions
    Change-Id: I588ae0c6ea89d05c824b94c7560d1f8a69423b48
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- data/etc/Android.mk
+++ data/etc/Android.mk
@@ -48,16 +48,9 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+########################
 include $(CLEAR_VARS)
-LOCAL_MODULE := projekt.substratum.theme.xml
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := substratum_theme.xml
+LOCAL_MODULE := substratum-theme-feature.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
--- packages/SubstratumHelperService/src/projekt/substratum/helper/SubstratumHelperService.java
+++ packages/SubstratumHelperService/src/projekt/substratum/helper/SubstratumHelperService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package android.substratum.service;
+package projekt.substratum.helper;
 
 import android.app.Service;
 import android.content.Intent;
@@ -30,7 +30,7 @@ import com.android.internal.substratum.ISubstratumHelperService;
 import java.io.File;
 
 public class SubstratumHelperService extends Service {
-    private static final String TAG = "SubstratumService";
+    private static final String TAG = "SubstratumHelperService";
 
     private final File EXTERNAL_CACHE_DIR =
             new File(Environment.getExternalStorageDirectory(), ".substratum");
