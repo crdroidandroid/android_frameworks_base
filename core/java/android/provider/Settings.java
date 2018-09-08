9ca1b43e7bbc2ee2924d4ed82c348b0941214380
Pranav Vashi <neobuddy89@gmail.com>
2018-09-08 22:32:30 +0530

    Separate double tap to sleep on lockscreen [1/2]
    * Based on commits by ezio84 and maxwen:
    https://github.com/ezio84/abc_frameworks_base/commit/4247a869d62323ccf70034a50efa2b0b4ee638c5
    https://github.com/ezio84/abc_frameworks_base/commit/4467b77e6f70d2c80fc3c94fb9034c70c691a923
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4145,6 +4145,13 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN =
+                "double_tap_sleep_lockscreen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
