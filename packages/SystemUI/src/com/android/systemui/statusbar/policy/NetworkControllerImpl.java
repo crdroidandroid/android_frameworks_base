7e45ad3463f7dce1c40e4cb6680a29867e20bb87
Weijie Wang <weijiew@codeaurora.org>
2018-09-11 18:02:21 +0800

    SystemUI: Refactor the feature of volte icon
    1. Move volte state to MobileState
    2. Notify UI to update only when it is necessary
    
    Change-Id: I80572f7db7a3839b7261dceb625b358039799959
    CRs-Fixed: 2318167
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -1198,6 +1198,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         boolean hspaDataDistinguishable;
         boolean inflateSignalStrengths = false;
         boolean alwaysShowDataRatIcon = false;
+        boolean showVolteIcon;
 
         static Config readConfig(Context context) {
             Config config = new Config();
@@ -1228,6 +1229,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
                         CarrierConfigManager.KEY_HIDE_LTE_PLUS_DATA_ICON_BOOL);
             }
 
+            config.showVolteIcon = res.getBoolean(R.bool.config_display_volte);
             return config;
         }
     }
