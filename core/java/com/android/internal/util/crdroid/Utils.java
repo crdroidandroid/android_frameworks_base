1059d0517a06b8f4e466e74182833c408a31186f
PMS22 <prathams99@rediff.com>
2018-10-16 02:25:57 +0530

    CompassTile: Add a device supports compass check
    - Previously it showed up on devices even without compass support
      making it a bloat
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/Utils.java
+++ core/java/com/android/internal/util/crdroid/Utils.java
@@ -25,6 +25,8 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
@@ -343,5 +345,11 @@ public class Utils {
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 break;
         }
+    }
+
+    public static boolean deviceHasCompass(Context ctx) {
+        SensorManager sm = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
+        return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+                && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
     }
 }
