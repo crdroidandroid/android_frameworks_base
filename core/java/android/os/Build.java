c4fa2bbe7e9fd56c07e642e177edd76053b21e0b
Marko Man <darkobas@gmail.com>
2018-10-17 10:22:17 +0200

    base: allow device to override *caugh*caugh* prop
    Change-Id: Ib51a5e5ae87b004053dcf1c01d8aa6cf3ef63b65
    (cherry picked from commit 1382f28e52eb33e5da9f45bc97f854671f1efe34)

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -276,6 +276,13 @@ public class Build {
                 "ro.build.version.security_patch", "");
 
         /**
+         * The user-visible crDroid security patch level.
+         * @hide
+         */
+        public static final String SECURITY_PATCH_CRDROID = SystemProperties.get(
+                "ro.crdroid.version.security_patch", "");
+
+        /**
          * The user-visible SDK version of the framework in its raw String
          * representation; use {@link #SDK_INT} instead.
          *
