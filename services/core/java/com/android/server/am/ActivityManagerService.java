5330b517cd7592abe7261a80d97a8f850539e229
wangyougang <wangyougang@xiaomi.com>
2018-10-19 18:59:54 +0800

    Refusing to enter PIP mode when activity destroyed
    Test: manual -adds fingerprint unlocking, calls enterPictureInPictureMode
    in the activity that can be displayed on the lock screen interface,
    and then call finish
    
    Change-Id: I63506bb3f9010077e5a6576e574761831439c1ee
    Signed-off-by: wangyougang <wangyougang@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -8977,6 +8977,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                 }
 
                 final Runnable enterPipRunnable = () -> {
+                    if (r.finishing || r.getTask() == null) return;
                     // Only update the saved args from the args that are set
                     r.pictureInPictureArgs.copyOnlySet(params);
                     final float aspectRatio = r.pictureInPictureArgs.getAspectRatio();
