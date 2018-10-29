81044f2aabe94d6094c71a70d0552dc28b6ccccd
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-29 17:32:21 -0300

    SystemUI: Implement weather on lockscreen like Pixels
    This use OmniJaws implementations as base, very thanks to Omni team
    
    Icons extracted from Lawnchair, thanks to deletescape and paphonb
    
    WeatherClient is included on vendor/pixelstyle
    
    Our edits:
    
    * Cleanup code
    * Adapt to our weather provider
    * Slice-ify
    * Add required permissions
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ core/java/com/android/internal/util/crdroid/weather/WeatherClient.java
@@ -0,0 +1,329 @@
+/*
+ * Copyright (C) 2018 The OmniROM Project
+ *                    The PixelExperience Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.android.internal.util.crdroid.weather;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Process;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import com.android.internal.R;
+
+public class WeatherClient {
+
+    public static final String SERVICE_PACKAGE = "com.crdroid.weather.client";
+    private static final String SERVICE_PACKAGE_PERMISSION = SERVICE_PACKAGE + ".READ_WEATHER";
+    public static final Uri WEATHER_URI = Uri.parse("content://com.crdroid.weather.client.provider/weather");
+    public static final int WEATHER_UPDATE_SUCCESS = 0; // Success
+    public static final int WEATHER_UPDATE_RUNNING = 1; // Update running
+    public static final int WEATHER_UPDATE_ERROR = 2; // Error
+    private static final String TAG = "WeatherClient";
+    private static final boolean DEBUG = false;
+    private static final String COLUMN_STATUS = "status";
+    private static final String COLUMN_CONDITIONS = "conditions";
+    private static final String COLUMN_TEMPERATURE_METRIC = "temperatureMetric";
+    private static final String COLUMN_TEMPERATURE_IMPERIAL = "temperatureImperial";
+    private static final String[] PROJECTION_DEFAULT_WEATHER = new String[]{
+            COLUMN_STATUS,
+            COLUMN_CONDITIONS,
+            COLUMN_TEMPERATURE_METRIC,
+            COLUMN_TEMPERATURE_IMPERIAL
+    };
+
+    private static final int WEATHER_UPDATE_INTERVAL = 60 * 10 * 1000; // 10 minutes
+    private String updateIntentAction;
+    private PendingIntent pendingWeatherUpdate;
+    private WeatherInfo mWeatherInfo = new WeatherInfo();
+    private Context mContext;
+    private List<WeatherObserver> mObserver;
+    private boolean isRunning;
+    private boolean isScreenOn = true;
+    private long lastUpdated;
+    private long scheduledAlarmTime = 0;
+    private AlarmManager alarmManager;
+    private BroadcastReceiver weatherReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent == null) {
+                return;
+            }
+            if (DEBUG) Log.d(TAG, "Received intent: " + intent.getAction());
+            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+                onScreenOff();
+            } else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
+                onScreenOn();
+            } else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) || updateIntentAction.equals(intent.getAction())) {
+                updateWeatherAndNotify();
+            } else if (Intent.ACTION_TIME_CHANGED.equals(intent.getAction()) || Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
+                resetScheduledAlarm();
+                updateWeatherAndNotify();
+            }
+        }
+    };
+
+    public WeatherClient(Context context) {
+        mContext = context;
+        mContext.enforceCallingOrSelfPermission(SERVICE_PACKAGE_PERMISSION, "Missing or invalid weather permission: " + SERVICE_PACKAGE_PERMISSION);
+        updateIntentAction = "updateIntentAction_" + Integer.toString(getRandomInt());
+        pendingWeatherUpdate = PendingIntent.getBroadcast(mContext, getRandomInt(), new Intent(updateIntentAction), 0);
+        mObserver = new ArrayList<>();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_BOOT_COMPLETED);
+        filter.addAction(Intent.ACTION_TIME_CHANGED);
+        filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        filter.addAction(updateIntentAction);
+        filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+        mContext.registerReceiver(weatherReceiver, filter);
+    }
+
+    public static boolean isAvailable(Context context) {
+        final PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(SERVICE_PACKAGE, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(SERVICE_PACKAGE);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                    enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    private int getRandomInt() {
+        Random r = new Random();
+        return r.nextInt((20000000 - 10000000) + 1) + 10000000;
+    }
+
+    private void updateWeatherAndNotify() {
+        if (isRunning){
+            return;
+        }
+        isRunning = true;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                updateWeatherData();
+                for (WeatherObserver observer : mObserver) {
+                    try {
+                        observer.onWeatherUpdated(mWeatherInfo);
+                    } catch (Exception ignored) {
+                    }
+                }
+                lastUpdated = System.currentTimeMillis();
+                resetScheduledAlarm();
+            }
+        });
+        thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+    }
+
+    private boolean needsUpdate() {
+        boolean lastUpdatedExpired = System.currentTimeMillis() - lastUpdated > WEATHER_UPDATE_INTERVAL;
+        return mWeatherInfo.getStatus() != WEATHER_UPDATE_SUCCESS || lastUpdatedExpired;
+    }
+
+    private void onScreenOn() {
+        if (isScreenOn){
+            return;
+        }
+        if (DEBUG) Log.d(TAG, "onScreenOn");
+        isScreenOn = true;
+        if (!isRunning) {
+            if (needsUpdate()) {
+                if (DEBUG) Log.d(TAG, "Needs update, triggering updateWeatherAndNotify");
+                updateWeatherAndNotify();
+            } else {
+                if (DEBUG) Log.d(TAG, "Scheduling update");
+                scheduleWeatherUpdateAlarm();
+            }
+        }
+    }
+
+    private void onScreenOff() {
+        if (DEBUG) Log.d(TAG, "onScreenOff");
+        isScreenOn = false;
+        cancelWeatherUpdateAlarm();
+    }
+
+    private void resetScheduledAlarm(){
+        scheduledAlarmTime = 0;
+        scheduleWeatherUpdateAlarm();
+    }
+
+    private void scheduleWeatherUpdateAlarm() {
+        if (!isScreenOn) {
+            return;
+        }
+        if (System.currentTimeMillis() >= scheduledAlarmTime){
+            scheduledAlarmTime = System.currentTimeMillis() + WEATHER_UPDATE_INTERVAL;
+        }
+        alarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingWeatherUpdate);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, scheduledAlarmTime, pendingWeatherUpdate);
+        if (DEBUG) Log.d(TAG, "Update scheduled");
+    }
+
+    private void cancelWeatherUpdateAlarm() {
+        alarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingWeatherUpdate);
+        if (DEBUG) Log.d(TAG, "Update scheduling canceled");
+    }
+
+    private void updateWeatherData() {
+        if (!isAvailable(mContext)) {
+            isRunning = false;
+            return;
+        }
+        isRunning = true;
+        Cursor c = mContext.getContentResolver().query(WEATHER_URI, PROJECTION_DEFAULT_WEATHER,
+                null, null, null);
+        if (c != null) {
+            try {
+                int count = c.getCount();
+                if (count > 0) {
+                    for (int i = 0; i < count; i++) {
+                        c.moveToPosition(i);
+                        if (i == 0) {
+                            mWeatherInfo.status = c.getInt(0);
+                            mWeatherInfo.conditions = c.getString(1);
+                            mWeatherInfo.temperatureMetric = c.getInt(2);
+                            mWeatherInfo.temperatureImperial = c.getInt(3);
+                        }
+                    }
+                }
+            } finally {
+                c.close();
+            }
+        } else {
+            mWeatherInfo.status = WEATHER_UPDATE_ERROR;
+        }
+        if (DEBUG) Log.d(TAG, mWeatherInfo.toString());
+        isRunning = false;
+    }
+
+    public void addObserver(final WeatherObserver observer) {
+        mObserver.add(observer);
+        if (isRunning) {
+            return;
+        }
+        isRunning = true;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                updateWeatherData();
+                try {
+                    observer.onWeatherUpdated(mWeatherInfo);
+                } catch (Exception ignored) {
+                }
+            }
+        });
+        thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+    }
+
+    public void removeObserver(WeatherObserver observer) {
+        mObserver.remove(observer);
+    }
+
+    public void destroy(){
+        mContext.unregisterReceiver(weatherReceiver);
+        mObserver = new ArrayList<>();
+    }
+
+    public interface WeatherObserver {
+        void onWeatherUpdated(WeatherInfo info);
+    }
+
+    public class WeatherInfo {
+
+        int status = WEATHER_UPDATE_ERROR;
+        String conditions = "";
+        int temperatureMetric = 0;
+        int temperatureImperial = 0;
+
+        public WeatherInfo() {
+        }
+
+        public int getTemperature(boolean metric) {
+            return metric ? this.temperatureMetric : this.temperatureImperial;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public String getConditions() {
+            return this.conditions;
+        }
+
+        public int getWeatherConditionImage() {
+            Map<String, Integer> conditions = new HashMap<>();
+            conditions.put("partly-cloudy", R.drawable.weather_partly_cloudy);
+            conditions.put("partly-cloudy-night", R.drawable.weather_partly_cloudy_night);
+            conditions.put("mostly-cloudy", R.drawable.weather_mostly_cloudy);
+            conditions.put("mostly-cloudy-night", R.drawable.weather_mostly_cloudy_night);
+            conditions.put("cloudy", R.drawable.weather_cloudy);
+            conditions.put("clear-night", R.drawable.weather_clear_night);
+            conditions.put("mostly-clear-night", R.drawable.weather_mostly_clear_night);
+            conditions.put("sunny", R.drawable.weather_sunny);
+            conditions.put("mostly-sunny", R.drawable.weather_mostly_sunny);
+            conditions.put("scattered-showers", R.drawable.weather_scattered_showers);
+            conditions.put("scattered-showers-night", R.drawable.weather_scattered_showers_night);
+            conditions.put("rain", R.drawable.weather_rain);
+            conditions.put("windy", R.drawable.weather_windy);
+            conditions.put("snow", R.drawable.weather_snow);
+            conditions.put("scattered-thunderstorms", R.drawable.weather_isolated_scattered_thunderstorms);
+            conditions.put("scattered-thunderstorms-night", R.drawable.weather_isolated_scattered_thunderstorms_night);
+            conditions.put("isolated-thunderstorms", R.drawable.weather_isolated_scattered_thunderstorms);
+            conditions.put("isolated-thunderstorms-night", R.drawable.weather_isolated_scattered_thunderstorms_night);
+            conditions.put("thunderstorms", R.drawable.weather_thunderstorms);
+            conditions.put("foggy", R.drawable.weather_foggy);
+            for (String condition : conditions.keySet()) {
+                if (getConditions().equals(condition)) {
+                    return conditions.get(condition);
+                }
+            }
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return "WeatherInfo: " +
+                    "status=" + getStatus() + "," +
+                    "conditions=" + getConditions() + "," +
+                    "temperatureMetric=" + getTemperature(true) + "," +
+                    "temperatureImperial=" + getTemperature(false);
+        }
+    }
+}
--- packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardSliceView.java
@@ -85,6 +85,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
 
     private LiveData<Slice> mLiveData;
     private int mIconSize;
+    private int mWeatherIconSize;
     /**
      * Runnable called whenever the view contents change.
      */
@@ -195,14 +196,18 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
             SliceItem item = subItems.get(i);
             RowContent rc = new RowContent(getContext(), item, true /* showStartItem */);
             final Uri itemTag = item.getSlice().getUri();
+            final boolean isWeatherSlice = itemTag.toString().equals(KeyguardSliceProvider.KEYGUARD_WEATHER_URI);
             // Try to reuse the view if already exists in the layout
             KeyguardSliceButton button = mRow.findViewWithTag(itemTag);
             if (button == null) {
                 button = new KeyguardSliceButton(mContext);
+                button.setShouldTintDrawable(!isWeatherSlice);
                 button.setTextColor(blendedColor);
                 button.setTag(itemTag);
                 final int viewIndex = i - (mHasHeader ? 1 : 0);
                 mRow.addView(button, viewIndex);
+            }else{
+                button.setShouldTintDrawable(!isWeatherSlice);
             }
 
             PendingIntent pendingIntent = null;
@@ -221,8 +226,8 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
             if (icon != null) {
                 iconDrawable = icon.getIcon().loadDrawable(mContext);
                 final int width = (int) (iconDrawable.getIntrinsicWidth()
-                        / (float) iconDrawable.getIntrinsicHeight() * mIconSize);
-                iconDrawable.setBounds(0, 0, Math.max(width, 1), mIconSize);
+                        / (float) iconDrawable.getIntrinsicHeight() * (isWeatherSlice ? mWeatherIconSize : mIconSize));
+                iconDrawable.setBounds(0, 0, Math.max(width, 1), (isWeatherSlice ? mWeatherIconSize : mIconSize));
             }
             button.setCompoundDrawables(iconDrawable, null, null, null);
             button.setOnClickListener(this);
@@ -385,6 +390,7 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     @Override
     public void onDensityOrFontScaleChanged() {
         mIconSize = mContext.getResources().getDimensionPixelSize(R.dimen.widget_icon_size);
+        mWeatherIconSize = mContext.getResources().getDimensionPixelSize(R.dimen.weather_icon_size);
     }
 
     public void refresh() {
@@ -486,11 +492,16 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
     static class KeyguardSliceButton extends Button implements
             ConfigurationController.ConfigurationListener {
 
+        private boolean shouldTintDrawable = true;
         public KeyguardSliceButton(Context context) {
             super(context, null /* attrs */, 0 /* styleAttr */,
                     com.android.keyguard.R.style.TextAppearance_Keyguard_Secondary);
             onDensityOrFontScaleChanged();
             setEllipsize(TruncateAt.END);
+        }
+
+        public void setShouldTintDrawable(boolean shouldTintDrawable){
+            this.shouldTintDrawable = shouldTintDrawable;
         }
 
         @Override
@@ -542,6 +553,9 @@ public class KeyguardSliceView extends LinearLayout implements View.OnClickListe
         }
 
         private void updateDrawableColors() {
+            if (!shouldTintDrawable){
+                return;
+            }
             final int color = getCurrentTextColor();
             for (Drawable drawable : getCompoundDrawables()) {
                 if (drawable != null) {
