4d538386ef7031f08938b48d54e468f21004b691
Pranav Vashi <neobuddy89@gmail.com>
2018-11-01 23:52:21 +0530

    Add device key action to take screenshot [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1947,6 +1947,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case TORCH:
                 Utils.toggleCameraFlash();
                 break;
+            case SCREENSHOT:
+                takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN);
+                break;
             default:
                 break;
         }
