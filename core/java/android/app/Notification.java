937401b1ced292dbada5d5d4e0c954e028c261d1
SpiritCroc <dev@spiritcroc.de>
2018-11-06 09:07:53 +0100

    Themes: Remove color spans when using config_useDarkBgNotificationIconTinting
    Some notifications use color spans while assuming a dark background,
    leading to unreadable text, so better remove them.
    
    Change-Id: I4cfcb0b3566c1a36aada64810ddd9f4ec6b2eea1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4461,6 +4461,11 @@ public class Notification implements Parcelable
         private CharSequence processTextSpans(CharSequence text) {
             if (hasForegroundColor()) {
                 return NotificationColorUtil.clearColorSpans(text);
+            } else if (mContext.getResources()
+                .getBoolean(R.bool.config_useDarkBgNotificationIconTinting)) {
+                // Some notifications have color spans, assuming a dark background,
+                // so let's remove them
+                return NotificationColorUtil.clearColorSpans(text);
             }
             return text;
         }
