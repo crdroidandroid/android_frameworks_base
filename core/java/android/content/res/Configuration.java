dfd96aeeb9e42b181b6e747e3198c878e2700481
bigrushdog <randall.rushing@gmail.com>
2018-11-21 20:03:32 +0100

    bringup: Fontservice
    Pie Bringup by @ZeNiXxX
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/content/res/Configuration.java
+++ core/java/android/content/res/Configuration.java
@@ -1222,6 +1222,7 @@ public final class Configuration implements Parcelable, Comparable<Configuration
         int changed = 0;
         if (delta.fontScale > 0 && fontScale != delta.fontScale) {
             changed |= ActivityInfo.CONFIG_FONT_SCALE;
+	        changed |= ActivityInfo.CONFIG_THEME_FONT;
             fontScale = delta.fontScale;
         }
         if (delta.mcc != 0 && mcc != delta.mcc) {
@@ -1382,7 +1383,8 @@ public final class Configuration implements Parcelable, Comparable<Configuration
         }
         if (delta.assetsSeq != ASSETS_SEQ_UNDEFINED && delta.assetsSeq != assetsSeq) {
             changed |= ActivityInfo.CONFIG_ASSETS_PATHS;
-            assetsSeq = delta.assetsSeq;
+            changed |= ActivityInfo.CONFIG_THEME_FONT;
+	        assetsSeq = delta.assetsSeq;
         }
         if (delta.seq != 0) {
             seq = delta.seq;
@@ -1450,7 +1452,8 @@ public final class Configuration implements Parcelable, Comparable<Configuration
         int changed = 0;
         if ((compareUndefined || delta.fontScale > 0) && fontScale != delta.fontScale) {
             changed |= ActivityInfo.CONFIG_FONT_SCALE;
-        }
+            changed |= ActivityInfo.CONFIG_THEME_FONT;
+	    }
         if ((compareUndefined || delta.mcc != 0) && mcc != delta.mcc) {
             changed |= ActivityInfo.CONFIG_MCC;
         }
@@ -1539,7 +1542,8 @@ public final class Configuration implements Parcelable, Comparable<Configuration
         if ((compareUndefined || delta.assetsSeq != ASSETS_SEQ_UNDEFINED)
                 && assetsSeq != delta.assetsSeq) {
             changed |= ActivityInfo.CONFIG_ASSETS_PATHS;
-        }
+            changed |= ActivityInfo.CONFIG_THEME_FONT;
+	    }
 
         // WindowConfiguration differences aren't considered public...
         if (!publicOnly
@@ -1567,7 +1571,7 @@ public final class Configuration implements Parcelable, Comparable<Configuration
         // CONFIG_ASSETS_PATHS and CONFIG_FONT_SCALE are higher level configuration changes that
         // all resources are subject to change with.
         interestingChanges = interestingChanges | ActivityInfo.CONFIG_ASSETS_PATHS
-                | ActivityInfo.CONFIG_FONT_SCALE;
+                | ActivityInfo.CONFIG_FONT_SCALE | ActivityInfo.CONFIG_THEME_FONT;
         return (configChanges & interestingChanges) != 0;
     }
 
