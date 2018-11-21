b4afb0e401dac1e41ec490b9573e80b4170210de
LorDClockaN <davor@losinj.com>
2018-11-21 15:49:47 +0100

    Volume dialog timeout [1/2]
    @neobuddy89:
    * Rewrite to avoid called getInt everytime changing volume! Really?
    * Removed validators - handle range in the settings.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -155,6 +155,8 @@ public class VolumeDialogImpl implements VolumeDialog,
 
     private static final String VOLUME_PANEL_ON_LEFT =
             "lineagesecure:" + LineageSettings.Secure.VOLUME_PANEL_ON_LEFT;
+    public static final String VOLUME_DIALOG_TIMEOUT =
+            "system:" + Settings.System.VOLUME_DIALOG_TIMEOUT;
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
     private static final int UPDATE_ANIMATION_DURATION = 80;
@@ -291,6 +293,8 @@ public class VolumeDialogImpl implements VolumeDialog,
     // Volume panel expand state
     private boolean mExpanded;
 
+    private int mTimeOutDesired, mTimeOut;
+
     public VolumeDialogImpl(
             Context context,
             VolumeDialogController volumeDialogController,
@@ -340,6 +344,7 @@ public class VolumeDialogImpl implements VolumeDialog,
         if (!mShowActiveStreamOnly) {
             mTunerService.addTunable(mTunable, VOLUME_PANEL_ON_LEFT);
         }
+        mTunerService.addTunable(mTunable, VOLUME_DIALOG_TIMEOUT);
 
         initDimens();
     }
@@ -744,6 +749,9 @@ public class VolumeDialogImpl implements VolumeDialog,
                         mControllerCallbackH.onConfigurationChanged();
                     });
                 }
+            } else if (VOLUME_DIALOG_TIMEOUT.equals(key)) {
+                mTimeOutDesired = TunerService.parseInteger(newValue, 3);
+                mTimeOut = mTimeOutDesired * 1000;
             }
         }
     };
@@ -1575,8 +1583,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                     AccessibilityManager.FLAG_CONTENT_TEXT
                             | AccessibilityManager.FLAG_CONTENT_CONTROLS);
         }
-        return mAccessibilityMgr.getRecommendedTimeoutMillis(DIALOG_TIMEOUT_MILLIS,
-                AccessibilityManager.FLAG_CONTENT_CONTROLS);
+        return mTimeOut;
     }
 
     protected void dismissH(int reason) {
