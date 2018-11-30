bafa9a82b9541096a8934250a2d7ee036bcf0473
Pranav Vashi <neobuddy89@gmail.com>
2018-12-01 00:08:47 +0530

    PebbleBerry: Support black overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4699,6 +4699,12 @@ public final class Settings {
         public static final String BERRY_THEME_OVERRIDE = "berry_theme_override";
 
         /**
+         * Switch between different dark styles
+         * @hide
+         */
+        public static final String BERRY_DARK_STYLE = "berry_dark_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
