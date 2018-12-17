03a31f0c42198b6cc1a6cca708373e4604fe2f66
a.derendyaev <a.derendyaev@magdv.com>
2018-12-17 16:28:44 +0700

    Extended audio panel [1/2]
    Change-Id: Ieb85137ee73f7ed032b9bbdd4087be2c72a4d1f5
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4724,6 +4724,24 @@ public final class Settings {
          */
         public static final String FONT_PACKAGES = "font_packages";
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
