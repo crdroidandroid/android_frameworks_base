955128829e689ce3841928877f2b929e80a63682
LorDClockaN <lordclockan@gmail.com>
2018-12-19 19:55:34 -0600

    base: Add device key action to Clear all notifications [1/3]

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1966,6 +1966,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case VOLUME_PANEL:
                 Utils.toggleVolumePanel(mContext);
                 break;
+            case CLEAR_ALL_NOTIFICATIONS:
+                Utils.clearAllNotifications();
+                break;
             default:
                 break;
         }
