b3f9c530ac77402c4f58787892d30870038453ad
Anushek Prasal <anushekprasal@gmail.com>
2018-12-30 00:21:27 +0530

    PebbleBerry: Switch styles [1/3]
    Based on the code of Fruity pebbles and QS tile picker.
    
    Modified by @KillerDroid96 and @neobuddy89
    
    Change-Id: Ic86637a0ea6374034ddd9625aab49101b03957b6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -328,6 +328,8 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             "system:" + Settings.System.BERRY_QS_TILE_STYLE;
     private static final String BERRY_QS_HEADER_STYLE =
             "system:" + Settings.System.BERRY_QS_HEADER_STYLE;
+    private static final String BERRY_SWITCH_STYLE =
+            "system:" + Settings.System.BERRY_SWITCH_STYLE;
 
     private static final String BANNER_ACTION_CANCEL =
             "com.android.systemui.statusbar.banner_action_cancel";
@@ -514,6 +516,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
     private int mNotiStyle;
     private int mQSTileStyle;
     private int mQSHeaderStyle;
+    private int mSwitchStyle;
     private boolean mPowerSave;
 
     /**
@@ -788,6 +791,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         tunerService.addTunable(this, BERRY_NOTIFICATION_STYLE);
         tunerService.addTunable(this, BERRY_QS_TILE_STYLE);
         tunerService.addTunable(this, BERRY_QS_HEADER_STYLE);
+        tunerService.addTunable(this, BERRY_SWITCH_STYLE);
 
         mDisplayManager = mContext.getSystemService(DisplayManager.class);
 
@@ -4419,6 +4423,12 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         });
     }
 
+    private void updateSwitchStyle() {
+        mUiOffloadThread.submit(() -> {
+            ThemeAccentUtils.setSwitchStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), mSwitchStyle);
+        });
+    }
+
     private void updateDozingState() {
         Trace.traceCounter(Trace.TRACE_TAG_APP, "dozing", mDozing ? 1 : 0);
         Trace.beginSection("StatusBar#updateDozingState");
@@ -6269,6 +6279,14 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 if (mQSHeaderStyle != qsHeaderStyle) {
                     mQSHeaderStyle = qsHeaderStyle;
                     updateQSHeaderStyle();
+                }
+                break;
+            case BERRY_SWITCH_STYLE:
+                int switchStyle =
+                        newValue == null ? 0 : Integer.parseInt(newValue);
+                if (mSwitchStyle != switchStyle) {
+                    mSwitchStyle = switchStyle;
+                    updateSwitchStyle();
                 }
                 break;
             default:
