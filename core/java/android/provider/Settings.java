ac2102339bdb62d4aa6afab2c058f5bb368c4510
Alex Cruz <du.alexcruz@gmail.com>
2018-12-31 03:36:06 -0600

    Add custom analog clocks for lockscreen [1/2]
    Contributions by ezio84 and SKULSHADY
    Spidey clock by ishubhamsingh
    Dot clock and Spectrum clock by mohancm
    
    @neobuddy89:
    Removed layout rules modification
    Added "hidden" as one of the clock selection
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4892,6 +4892,12 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Select which lockscreen clock style to display
+         * @hide
+         */
+        public static final String LOCKSCREEN_CLOCK_SELECTION = "lockscreen_clock_selection";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
