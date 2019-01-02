7a343ce8b37560e24bb9d3d10722c4d89c48aff4
liulvping <liulvping@xiaomi.com>
2019-01-02 18:46:52 +0800

    fix deadlock of binder thread and ActivityManager
    Traces:
    "ActivityManager" prio=5 tid=12 Blocked
      ...
      at ActivityManagerService.updateCpuStatsNow(ActivityManagerService.java:3107)
      - waiting to lock <0x0037ca7c> (aandroid.util.SparseArray) held by thread 135
      - locked <0x0dfe4378> (a com.android.internal.os.ProcessCpuTracker)
      - locked <0x064e7a29> (a com.android.internal.os.BatteryStatsImpl)
      at AppErrors.appNotResponding(AppErrors.java:983)
      at ActivityManagerService$17.run(ActivityManagerService.java:13416)
      at android.os.Handler.handleCallback(Handler.java:793)
      at android.os.Handler.dispatchMessage(Handler.java:98)
      at android.os.Looper.loop(Looper.java:173)
      at android.os.HandlerThread.run(HandlerThread.java:65)
      at com.android.server.ServiceThread.run(ServiceThread.java:46)
    
    "Binder:1328_15" prio=5 tid=135 Blocked
      ...
      at BatteryStatsService.noteEvent(BatteryStatsService.java:422)
      - waiting to lock <0x064e7a29> (acom.android.internal.os.BatteryStatsImpl) held by thread 12
      at ActivityManagerService.updateProcessForegroundLocked(ActivityManagerService.java:23108)
      at ActivityManagerService.importanceTokenDied(ActivityManagerService.java:8177)
      - locked <0x0ef100c6> (a ActivityManagerService)
      - locked <0x0037ca7c> (a android.util.SparseArray)
      at ActivityManagerService$13.binderDied(ActivityManagerService.java:8209)
      at android.os.BinderProxy.sendDeathNotice(Binder.java:842)
    
    Test: manul
    
    Change-Id: Ibf4cbf01c537509207e3c9031031c6e9aec73585
    Signed-off-by: liulvping <liulvping@xiaomi.com>
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -8836,6 +8836,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     void importanceTokenDied(ImportanceToken token) {
         synchronized (ActivityManagerService.this) {
+            ProcessRecord pr = null;
             synchronized (mPidsSelfLocked) {
                 ImportanceToken cur
                     = mImportantProcesses.get(token.pid);
@@ -8843,13 +8844,13 @@ public class ActivityManagerService extends IActivityManager.Stub
                     return;
                 }
                 mImportantProcesses.remove(token.pid);
-                ProcessRecord pr = mPidsSelfLocked.get(token.pid);
+                pr = mPidsSelfLocked.get(token.pid);
                 if (pr == null) {
                     return;
                 }
                 pr.forcingToImportant = null;
-                updateProcessForegroundLocked(pr, false, false);
             }
+            updateProcessForegroundLocked(pr, false, false);
             updateOomAdjLocked();
         }
     }
