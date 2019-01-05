820365c4a6016713708c28320cbd15f1e269715b
neobuddy89 <neobuddy89@gmail.com>
2019-01-05 22:33:08 +0530

    TunerServiceImpl: Remove unavailable settings from blacklist
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -73,7 +73,6 @@ public class TunerServiceImpl extends TunerService {
             NotificationPanelView.STATUS_BAR_QUICK_QS_PULLDOWN,
             QSTileHost.TILES_SETTING,
             Settings.Secure.DOZE_ALWAYS_ON,
-            StatusBar.BERRY_GLOBAL_STYLE,
             StatusBar.FORCE_SHOW_NAVBAR,
             StatusBar.SCREEN_BRIGHTNESS_MODE,
             StatusBar.STATUS_BAR_BRIGHTNESS_CONTROL,
