42a203746187e199495c42a75a290c2da461c64c
ezio84 <brabus84@gmail.com>
2019-01-15 23:49:45 +0100

    Ambient music Now Playing: more improvements
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
+++ packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -123,13 +123,17 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
     }
 
     public void setNowPlayingIndication(String trackInfo) {
-        // don't trigger this if we are already showing local/remote session track info
         setIndication(null, trackInfo, true);
     }
 
     public void setIndication(MediaMetadata mediaMetaData, String notificationText, boolean nowPlaying) {
-        // never override local music ticker
+        // never override local music ticker but be sure to delete Now Playing info when needed
+        if (nowPlaying && notificationText == null) {
+            mMediaText = null;
+            mNpInfoAvailable = false;
+        }
         if (nowPlaying && mInfoAvailable) return;
+
         CharSequence charSequence = null;
         mInfoToSet = null;
         Typeface tf = Typeface.create(FONT_FAMILY, Typeface.NORMAL);
