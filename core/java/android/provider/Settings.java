f8ecd0b6b8a78db3a6a10f0768f4af4a96fe73f9
neobuddy89 <neobuddy89@gmail.com>
2019-01-16 08:04:37 +0530

    PebbleBerry: Introduce Tile Styles - a QS tile style picker [1/3]
    This gives users the chance to change the look of their QS tiles. Being based on the same methods used by Omni's system theming and DU's Fruity Pebbles, it seems to work quite nicely with them.
    
    Originally implemented by calebcabob
    Code Ref: https://github.com/AquariOS/frameworks_base/commit/7d82a97d78c96a13fee931e634633f6ace6bac82
    
    Re-write from scratch.
    
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4730,6 +4730,12 @@ public final class Settings {
         public static final String BERRY_NOTIFICATION_STYLE = "berry_notification_style";
 
         /**
+         * Switch between different qs tile styles
+         * @hide
+         */
+        public static final String BERRY_QS_TILE_STYLE = "berry_qs_tile_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -324,6 +324,8 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
             "system:" + Settings.System.BERRY_DARK_STYLE;
     private static final String BERRY_NOTIFICATION_STYLE =
             "system:" + Settings.System.BERRY_NOTIFICATION_STYLE;
+    private static final String BERRY_QS_TILE_STYLE =
+            "system:" + Settings.System.BERRY_QS_TILE_STYLE;
 
     private static final String BANNER_ACTION_CANCEL =
             "com.android.systemui.statusbar.banner_action_cancel";
@@ -508,6 +510,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
     private int mThemeOverride;
     private int mDarkStyle;
     private int mNotiStyle;
+    private int mQSTileStyle;
     private boolean mPowerSave;
 
     /**
@@ -780,6 +783,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         tunerService.addTunable(this, BERRY_THEME_OVERRIDE);
         tunerService.addTunable(this, BERRY_DARK_STYLE);
         tunerService.addTunable(this, BERRY_NOTIFICATION_STYLE);
+        tunerService.addTunable(this, BERRY_QS_TILE_STYLE);
 
         mDisplayManager = mContext.getSystemService(DisplayManager.class);
 
@@ -4399,6 +4403,12 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         });
     }
 
+    private void updateQSTileStyle() {
+        mUiOffloadThread.submit(() -> {
+            ThemeAccentUtils.setQSTileStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), mQSTileStyle);
+        });
+    }
+
     private void updateDozingState() {
         Trace.traceCounter(Trace.TRACE_TAG_APP, "dozing", mDozing ? 1 : 0);
         Trace.beginSection("StatusBar#updateDozingState");
@@ -6233,6 +6243,14 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 if (mNotiStyle != notiStyle) {
                     mNotiStyle = notiStyle;
                     updateTheme();
+                }
+                break;
+            case BERRY_QS_TILE_STYLE:
+                int qsTileStyle =
+                        newValue == null ? 0 : Integer.parseInt(newValue);
+                if (mQSTileStyle != qsTileStyle) {
+                    mQSTileStyle = qsTileStyle;
+                    updateQSTileStyle();
                 }
                 break;
             default:
