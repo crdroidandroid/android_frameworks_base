5672609c025b8aceacac7d82fc2a73bae55b7ca8
Ethan_Hu <huyuxin3818@gmail.com>
2019-01-19 17:49:41 +0800

    AccountManagerService: Fix ConnectionRecord Leak
    Bug:123084947
    Test: Stop AccountAuthenticator Process Many Times.
    Change-Id: I6cd2ae667f91426fe01a3e2673737d2072f15486
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/accounts/AccountManagerService.java
+++ services/core/java/com/android/server/accounts/AccountManagerService.java
@@ -4876,7 +4876,7 @@ public class AccountManagerService
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-            mAuthenticator = null;
+            unbind();
             IAccountManagerResponse response = getResponseAndClose();
             if (response != null) {
                 try {
