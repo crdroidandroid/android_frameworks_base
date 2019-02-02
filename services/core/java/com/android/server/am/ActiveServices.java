b39868afb8508992fe213fe6e9df27e8cb65d4af
Wei Wang <wangwei.mark@gmail.com>
2019-02-02 16:40:35 +0800

    am: Avoid scheduling service restart twice
    Sometimes when an app is killed by low memory killer
    due to memory pressure and there is a service in it,
    activity manager service will schedule a delay to
    restart the service. The delay for first time of app
    death is one second, and it will be quadrupled if the
    death happen again in one minute.
    Activity manager service start a service in
    ActiveServices.realStartServiceLocked() method.
    It calls all service start methods in a try{} block,
    and check if the service has been created successfully
    in its finally{} block. If not, it will schedule a delay
    to restart it. But a special case is the service app was
    killed during service starting. Then it will go into
    the DeadObjectException catch{} block first and it calls
    ActivityManagerService.appDiedLocked() to clean up
    the process record. During the clean up, it will
    schedule the service restart also. After that it schedule
    the restart for the second time in the finally{} block.
    So the restart delay will always be quadrupled in this case.
    
    To solve the problem, we add a check in finally{} block.
    If the service is already scheduled to restart, we will not
    schedule it again.
    
    We meet a scenario as followings.
    We have a metric service in the system. Sometimes it was
    killed by low memory killer due to memory pressure.
    Then activity manager service will schedule to restart it
    after one second. During restarting, it was killed again
    and it was scheduled to restart in 16 seconds.
    
    Test: make & flash & run stress test & check log message
    Change-Id: I5e79a8a114ead2dcaf3292c61a47859b71a45bff
    Signed-off-by: Wei Wang <wangwei.mark@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -2561,7 +2561,7 @@ public final class ActiveServices {
                 }
 
                 // Retry.
-                if (!inDestroying) {
+                if (!inDestroying && !mRestartingServices.contains(r)) {
                     scheduleServiceRestartLocked(r, false);
                 }
             }
