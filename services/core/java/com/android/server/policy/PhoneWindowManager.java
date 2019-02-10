0bae03173deccfa182ba1bfc264bcd2994ca55fd
Pranav Vashi <neobuddy89@gmail.com>
2019-02-10 13:06:59 +0530

    Disable edge gesture service by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3376,7 +3376,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     LineageSettings.System.ACCELEROMETER_ROTATION_ANGLES, -1);
 
             final boolean useEdgeService = Settings.System.getIntForUser(resolver,
-                    Settings.System.USE_EDGE_SERVICE_FOR_GESTURES, 1, UserHandle.USER_CURRENT) == 1;
+                    Settings.System.USE_EDGE_SERVICE_FOR_GESTURES, 0, UserHandle.USER_CURRENT) == 1;
             if (useEdgeService ^ mUsingEdgeGestureServiceForGestures && mSystemReady) {
                 if (!mUsingEdgeGestureServiceForGestures && useEdgeService) {
                     mUsingEdgeGestureServiceForGestures = true;
