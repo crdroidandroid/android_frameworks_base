b6d5665a2bd275948a672eb2e5a8027f49961529
Pranav Vashi <neobuddy89@gmail.com>
2019-02-19 13:31:26 +0530

    Do not apply date customization to QS clock [1/2]
    * Date customization to QS clock is not required since QS panel shows date.
    * Also, add separate switch to toggle QS clock.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4818,6 +4818,12 @@ public final class Settings {
         public static final String USE_EDGE_SERVICE_FOR_GESTURES = "edge_service_for_gestures";
 
         /**
+         * Whether to display QS clock
+         * @hide
+         */
+        public static final String SHOW_QS_CLOCK = "show_qs_clock";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
