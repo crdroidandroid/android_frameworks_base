3cff42ccc35daa54c09797cb541797a9c7e8887c
Giuseppe Maggio <jertlokk@gmail.com>
2019-02-20 14:00:31 +0100

    GlobalActions: avoid multiple power dialog animations
    Remove the onTouchListener after one dismiss call is done, this avoids
    touch spams hence multiple animation initialisations.
    
    Change-Id: I08f7681338f6a56926499c7c943e6e461d90ef13
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1782,6 +1782,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
         @Override
         public void dismiss() {
+            // Remove outside touch listener to avoid multiple dismiss calls.
+            mHardwareLayout.setOutsideTouchListener(null);
             mHardwareLayout.setTranslationX(0);
             mHardwareLayout.setAlpha(1);
             mHardwareLayout.animate()
