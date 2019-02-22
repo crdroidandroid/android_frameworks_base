2195b85de2f5e212d0fa59bab8b69f4f8ce8a039
Pranav Vashi <neobuddy89@gmail.com>
2019-02-23 04:20:47 +0530

    Do not apply date customization to QS clock [1/2]
    * Add separate switch to disable QS clock.
    * Add mQsHeader boolean to identify QS clock. Credits: ezio84.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -77,6 +77,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     private static final String VISIBLE_BY_USER = "visible_by_user";
     private static final String SHOW_SECONDS = "show_seconds";
     private static final String VISIBILITY = "visibility";
+    private static final String QSHEADER = "qsheader";
 
     private final CurrentUserTracker mCurrentUserTracker;
     private final CommandQueue mCommandQueue;
@@ -121,6 +122,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
     private String mClockDateFormat = null;
     private boolean mClockAutoHide;
     private int mHideDuration = HIDE_DURATION, mShowDuration = SHOW_DURATION;
+    private boolean mQsHeader;
 
     public static final String STATUS_BAR_CLOCK_SECONDS =
             "system:" + Settings.System.STATUS_BAR_CLOCK_SECONDS;
@@ -190,6 +192,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         bundle.putBoolean(VISIBLE_BY_USER, mClockVisibleByUser);
         bundle.putBoolean(SHOW_SECONDS, mShowSeconds);
         bundle.putInt(VISIBILITY, getVisibility());
+        bundle.putBoolean(QSHEADER, mQsHeader);
 
         return bundle;
     }
@@ -213,6 +216,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         if (bundle.containsKey(VISIBILITY)) {
             super.setVisibility(bundle.getInt(VISIBILITY));
         }
+        mQsHeader = bundle.getBoolean(QSHEADER, false);
     }
 
     @Override
@@ -332,6 +336,10 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         super.setVisibility(visibility);
     }
 
+    public void setQsHeader() {
+        mQsHeader = true;
+    }
+
     public void setClockVisibleByUser(boolean visible) {
         mClockVisibleByUser = visible;
         updateClockVisibility();
@@ -355,7 +363,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
             // Do nothing
         }
         setVisibility(visibility);
-        if (mClockAutoHide && visible && mScreenOn) {
+        if (!mQsHeader && mClockAutoHide && visible && mScreenOn) {
             autoHideHandler.postDelayed(()->autoHideClock(), mShowDuration * 1000);
         }
     }
@@ -547,7 +555,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
         String timeResult = sdf.format(mCalendar.getTime());
         String dateResult = "";
 
-        if (mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
+        if (!mQsHeader && mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
             Date now = new Date();
 
             if (mClockDateFormat == null || mClockDateFormat.isEmpty()) {
