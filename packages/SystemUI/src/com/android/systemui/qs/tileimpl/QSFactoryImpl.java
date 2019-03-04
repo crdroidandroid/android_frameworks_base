ba85c4d5707ee8abcccb001fbc08b2a24f769dbf
bigrushdog <randall.rushing@gmail.com>
2019-03-03 18:49:41 -0800

    Introduce FontTile
    In order to accomodate this feature, minor changes were
    required to the font service. File permissions on the
    font preview files were made more accomodating to allow
    other UID's besides system to read them. This should have
    been done in the initial font service bring-up, but
    wasn't noticed until it became a issue.
    
    In addition, we made the ACCESS_FONT_MANAGER permission
    normal to avoid needing users to wipe their devices
    to use this feature. A wipe would be necessary from
    adding a system level permission to SystemUI manifest.
    The service api isn't in the SDK so i don't foresee
    any issues.
    
    Change-Id: I8b781d11950aaa1e01b7fe47efcaad494bfd3cf5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.FontTile;
 import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
@@ -178,6 +179,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new RebootTile(mHost);
             case "gaming":
                 return new GamingModeTile(mHost);
+            case "font":
+                return new FontTile(mHost);
         }
 
         // Intent tiles.
