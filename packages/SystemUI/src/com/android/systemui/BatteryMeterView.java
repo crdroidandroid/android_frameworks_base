8adb1dacab38dc8dc48ed596f36f355a5f648e37
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-07 19:29:58 -0300

    BatteryMeterView: Fix logcat spam
    This fixes a logcat spam when keyguard is showing:
    
    03-07 19:19:10.233  2567  2567 W View    : requestLayout() improperly called by android.widget.ImageView{a12edf1 V.ED..... ......ID 0,37-23,73} during layout: running second layout pass
    03-07 19:19:10.234  2567  2567 W View    : requestLayout() improperly called by com.android.systemui.BatteryMeterView{981a9db V.E...... ......ID 58,0-81,110 #7f0a0077 app:id/battery} during second layout pass: posting in next frame
    03-07 19:19:10.238  2567  2567 W View    : requestLayout() improperly called by android.widget.ImageView{a12edf1 V.ED..... ......ID 0,37-23,73} during second layout pass: posting in next frame
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -391,7 +391,7 @@ public class BatteryMeterView extends LinearLayout implements
         scaledLayoutParams.setMargins(0, 0, 0, marginBottom);
 
         if (mBatteryIconView != null) {
-            mBatteryIconView.setLayoutParams(scaledLayoutParams);
+            mBatteryIconView.post(() -> mBatteryIconView.setLayoutParams(scaledLayoutParams));
         }
         if (mBatteryPercentView != null) {
             FontSizeUtils.updateFontSize(mBatteryPercentView, R.dimen.qs_time_expanded_size);
