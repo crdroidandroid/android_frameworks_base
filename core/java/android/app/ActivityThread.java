72feb6e423c5fbbb4495a8a7955a1bf811f20e68
katao <katao@bytedance.com>
2019-03-12 18:57:20 +0800

    Fixes crash/race condition when destroyActivity
    some UI stress (or sleep on UI thread) causes a crash
    from DESTROY_TIMEOUT_MSG.
    The steps that crash the app are:
    1. build two activities ,such as A and B;
    2. A start B;
    3. B(oncreate) start A use FLAG_ACTIVITY_CLEAR_TOP;
    4. B(oncreate) stall UI thread (Thread.sleep, heavy task)
    5. crash tring to handleLaunchActivity or handleResumeActivity
    
    This bug was reported to Google Issue Tracker.
    Link: https://issuetracker.google.com/issues/128469605
    Test: manual - write an app to jump between two activities, then
    Thread.sleep();
    Change-Id: I4027dfc0cf3878c12964cabee54ad82bc108d79c
    Signed-off-by: katao <katao@bytedance.com>
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -3810,7 +3810,7 @@ public final class ActivityThread extends ClientTransactionHandler
             return;
         }
         Configuration[] configurations = r.activity.getResources().getSizeConfigurations();
-        if (configurations == null) {
+        if (configurations == null || r.activity.mFinished) {
             return;
         }
         r.mSizeConfigurations = new SizeConfigurationBuckets(configurations);
