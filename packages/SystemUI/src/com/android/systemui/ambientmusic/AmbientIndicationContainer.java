9a52e6ebdd79f25e2493423b56934c79cb44f713
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-03-16 13:03:00 -0300

    Improve ambient play layout on keyguard
    Kanged from Pixel 3, Android Q beta
    
    **DU Edits**
    
    - Fix formatting in animation and drawable xmls
    - Reuse resources, no need to create another dimen
    
    Change-Id: Ia93f020693ea8b995597b197c93dfd2d1929c21e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
+++ packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -1,9 +1,8 @@
 package com.android.systemui.ambientmusic;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.text.TextUtils.TruncateAt;
@@ -11,8 +10,6 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.systemui.AutoReinflateContainer;
@@ -24,11 +21,13 @@ import com.android.systemui.statusbar.phone.StatusBar;
 
 import com.android.systemui.ambientmusic.AmbientIndicationInflateListener;
 
+import java.util.Locale;
+
 public class AmbientIndicationContainer extends AutoReinflateContainer {
     private View mAmbientIndication;
-    private ImageView mIcon;
     private CharSequence mIndication;
     private StatusBar mStatusBar;
+    private AnimatedVectorDrawable mAnimatedIcon;
     private TextView mText;
     private Context mContext;
     private String mMediaText;
@@ -47,10 +46,16 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
     public AmbientIndicationContainer(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         mContext = context;
+        final int iconSize = mContext.getResources().getDimensionPixelSize(
+                R.dimen.notification_menu_icon_padding);
+        mAnimatedIcon = (AnimatedVectorDrawable) mContext.getDrawable(
+                R.drawable.audioanim_animation).getConstantState().newDrawable();
+        mAnimatedIcon.setBounds(0, 0, iconSize, iconSize);
     }
 
     public void hideIndication() {
         setIndication(null, false);
+        mAnimatedIcon.stop();
     }
 
     public void initializeView(StatusBar statusBar, Handler handler) {
@@ -62,7 +67,6 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
     public void updateAmbientIndicationView(View view) {
         mAmbientIndication = findViewById(R.id.ambient_indication);
         mText = (TextView)findViewById(R.id.ambient_indication_text);
-        mIcon = (ImageView)findViewById(R.id.ambient_indication_icon);
         setIndication(mMediaText, false);
     }
 
@@ -91,8 +95,9 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
         // not for local media players if they are showing a lockscreen media notification
         final NotificationLockscreenUserManager lockscreenManager =
                 mStatusBar.getNotificationLockscreenUserManager();
-        boolean filtered = lockscreenManager.shouldHideNotifications(lockscreenManager.getCurrentUserId())
-                || lockscreenManager.shouldHideNotifications(mStatusBar.getMediaManager().getMediaNotificationKey());
+        boolean filtered = lockscreenManager.shouldHideNotifications(
+                lockscreenManager.getCurrentUserId()) || lockscreenManager.shouldHideNotifications(
+                        mStatusBar.getMediaManager().getMediaNotificationKey());
         return mKeyguard
                 && ((mDozing && (mInfoAvailable || mNpInfoAvailable))
                 || (!mDozing && mNpInfoAvailable && !mInfoAvailable)
@@ -108,7 +113,12 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
                 public void run() {
                     mText.setEllipsize(TruncateAt.MARQUEE);
                     mText.setMarqueeRepeatLimit(2);
+                    boolean rtl = TextUtils.getLayoutDirectionFromLocale(
+                            Locale.getDefault()) == View.LAYOUT_DIRECTION_RTL;
+                    mText.setCompoundDrawables(rtl ? null : mAnimatedIcon, null, rtl ?
+                            mAnimatedIcon : null, null);
                     mText.setSelected(true);
+                    mAnimatedIcon.start();
                     if (extendPulseOnNewTrack && mStatusBar.isPulsing()) {
                         mStatusBar.getDozeScrimController().extendPulseForMusicTicker();
                     }
@@ -117,6 +127,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
         } else {
             mText.setEllipsize(null);
             mText.setSelected(false);
+            mAnimatedIcon.stop();
         }
     }
 
@@ -169,8 +180,10 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
         if (mInfoAvailable || mNpInfoAvailable) {
             mMediaText = notificationText;
             boolean isAnotherTrack = (mInfoAvailable || mNpInfoAvailable)
-                    && (TextUtils.isEmpty(mLastInfo) || (!TextUtils.isEmpty(mLastInfo) && !mLastInfo.equals(mInfoToSet)));
-            if (!DozeParameters.getInstance(mContext).getAlwaysOn() && mStatusBar != null && isAnotherTrack) {
+                    && (TextUtils.isEmpty(mLastInfo) || (!TextUtils.isEmpty(mLastInfo)
+                    && !mLastInfo.equals(mInfoToSet)));
+            if (!DozeParameters.getInstance(mContext).getAlwaysOn() && mStatusBar != null
+                    && isAnotherTrack) {
                 mStatusBar.triggerAmbientForMedia();
             }
             if (mKeyguard) {
