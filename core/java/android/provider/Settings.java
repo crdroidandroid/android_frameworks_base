a5547b5653a555b2d7960f6cb60a60f4236cb4e9
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    Also includes:
    
      From: DennySPB <dennyspb@gmail.com>
      Date: Fri, 17 May 2019 09:49:23 +0300
      Subject: [PATCH] SmartCharging: Rework and add more customizations
    
      From: Pranav Vashi <neobuddy89@gmail.com>
      Date: Thu, 16 Jan 2020 01:30:38 +0530
      Subject: [PATCH] Add config to make Smart Charging available on device
    
      From: xNombre <kartapolska@gmail.com>
      Date: Thu, 19 Aug 2021 23:25:28 +0200
      Subject: [PATCH] SmartCharging: Make sure resume level is lower than suspend level
    
        Braindead settings fragment or end-user (you can choose) can set
        resume level higher than suspend level. This is obviously wrong.
    
        This causes Android to write suspend and resume commands very
        frequently and quickly, this can cause phone crashes due to
        improper locking in the kernel. Such issue was reported to me.
    
        I am definitely not fixing the setting tile so let's come up with
        an easier approach - not resuming charging if level is mistakenly
        set. This won't kill shitty qcom kernel any more.
    
        Bonus: simplified if condition
    
      Signed-off-by: xNombre <kartapolska@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6152,6 +6152,26 @@ public final class Settings {
         public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_RESET_STATS = "smart_charging_reset_stats";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
