0e69141674dd1446365f59319623275b1ba48a0a
Pranav Vashi <neobuddy89@gmail.com>
2019-04-09 00:18:01 +0530

    Lockscreen Visualizer: Add pulse magic
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
@@ -1,18 +1,19 @@
 /*
-* Copyright (C) 2015 The CyanogenMod Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2018-2019 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.android.systemui.statusbar;
 
@@ -26,24 +27,37 @@ import android.graphics.Paint;
 import android.media.audiofx.Visualizer;
 import android.os.AsyncTask;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
-import com.android.internal.graphics.palette.Palette;
 import com.android.systemui.Dependency;
 import com.android.systemui.tuner.TunerService;
-
-import lineageos.providers.LineageSettings;
+import com.android.internal.graphics.palette.Palette;
+import com.android.internal.util.crdroid.ColorAnimator;
 
 public class VisualizerView extends View
-        implements Palette.PaletteAsyncListener, TunerService.Tunable {
+        implements Palette.PaletteAsyncListener, TunerService.Tunable,
+        ColorAnimator.ColorAnimationListener {
 
     private static final String TAG = VisualizerView.class.getSimpleName();
     private static final boolean DEBUG = false;
 
     private static final String LOCKSCREEN_VISUALIZER_ENABLED =
-            "lineagesecure:" + LineageSettings.Secure.LOCKSCREEN_VISUALIZER_ENABLED;
+            Settings.Secure.LOCKSCREEN_VISUALIZER_ENABLED;
+    private static final String LOCKSCREEN_VISUALIZER_AUTOCOLOR =
+            Settings.Secure.LOCKSCREEN_VISUALIZER_AUTOCOLOR;
+    private static final String LOCKSCREEN_LAVALAMP_ENABLED =
+            Settings.Secure.LOCKSCREEN_LAVALAMP_ENABLED;
+    private static final String LOCKSCREEN_LAVALAMP_SPEED =
+            Settings.Secure.LOCKSCREEN_LAVALAMP_SPEED;
+    private static final String LOCKSCREEN_SOLID_UNITS_COUNT =
+            Settings.Secure.LOCKSCREEN_SOLID_UNITS_COUNT;
+    private static final String LOCKSCREEN_SOLID_FUDGE_FACTOR =
+            Settings.Secure.LOCKSCREEN_SOLID_FUDGE_FACTOR;
+    private static final String LOCKSCREEN_SOLID_UNITS_OPACITY =
+            Settings.Secure.LOCKSCREEN_SOLID_UNITS_OPACITY;
 
     private Paint mPaint;
     private Visualizer mVisualizer;
@@ -53,16 +67,26 @@ public class VisualizerView extends View
     private float[] mFFTPoints;
 
     private int mStatusBarState;
-    private boolean mVisualizerEnabled;
-    private boolean mVisible;
-    private boolean mPlaying;
-    private boolean mPowerSaveMode;
-    private boolean mDisplaying; // the state we're animating to
-    private boolean mDozing;
-    private boolean mOccluded;
+    private boolean mVisualizerEnabled = false;
+    private boolean mVisible = false;
+    private boolean mPlaying = false;
+    private boolean mPowerSaveMode = false;
+    private boolean mDisplaying = false; // the state we're animating to
+    private boolean mDozing = false;
+    private boolean mOccluded = false;
 
     private int mColor;
     private Bitmap mCurrentBitmap;
+
+    private ColorAnimator mLavaLamp;
+    private boolean mAutoColor;
+    private boolean mLavaLampEnabled;
+    private int mLavaLampSpeed;
+    private boolean shouldAnimate;
+    private int mUnits = 32;
+    private float mDbFuzzFactor = 16f;
+    private int mWidth, mHeight;
+    private int mOpacity = 140;
 
     private Visualizer.OnDataCaptureListener mVisualizerListener =
             new Visualizer.OnDataCaptureListener() {
@@ -76,7 +100,7 @@ public class VisualizerView extends View
 
         @Override
         public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
-            for (int i = 0; i < 32; i++) {
+            for (int i = 0; i < mUnits; i++) {
                 mValueAnimators[i].cancel();
                 rfk = fft[i * 2 + 2];
                 ifk = fft[i * 2 + 3];
@@ -84,7 +108,7 @@ public class VisualizerView extends View
                 dbValue = magnitude > 0 ? (int) (10 * Math.log10(magnitude)) : 0;
 
                 mValueAnimators[i].setFloatValues(mFFTPoints[i * 4 + 1],
-                        mFFTPoints[3] - (dbValue * 16f));
+                        mFFTPoints[3] - (dbValue * mDbFuzzFactor));
                 mValueAnimators[i].start();
             }
         }
@@ -98,7 +122,10 @@ public class VisualizerView extends View
             }
 
             try {
-                mVisualizer = new Visualizer(0);
+                if (mVisualizer == null) {
+                    mVisualizer = new Visualizer(0);
+                    shouldAnimate = true;
+                }
             } catch (Exception e) {
                 Log.e(TAG, "error initializing visualizer", e);
                 return;
@@ -129,24 +156,27 @@ public class VisualizerView extends View
             if (DEBUG) {
                 Log.w(TAG, "+++ mUnlinkVisualizer run(), mVisualizer: " + mVisualizer);
             }
+
             if (mVisualizer != null) {
                 mVisualizer.setEnabled(false);
                 mVisualizer.release();
                 mVisualizer = null;
             }
+            shouldAnimate = false;
+
+            if (!mAutoColor && !mLavaLampEnabled) {
+                if (mCurrentBitmap != null) {
+                    setBitmap(null);
+                } else {
+                    setColor(Color.TRANSPARENT);
+                }
+            }
+
             if (DEBUG) {
-                Log.w(TAG, "--- mUnlinkVisualizer run()");
+                Log.w(TAG, "--- mUninkVisualizer run()");
             }
         }
     };
-
-    public VisualizerView(Context context) {
-        this(context, null, 0);
-    }
-
-    public VisualizerView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
 
     public VisualizerView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -155,11 +185,119 @@ public class VisualizerView extends View
 
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        mPaint.setColor(mColor);
 
-        mFFTPoints = new float[128];
-        mValueAnimators = new ValueAnimator[32];
-        for (int i = 0; i < 32; i++) {
+        mFFTPoints = new float[mUnits * 4];
+
+        mLavaLamp = new ColorAnimator();
+        mLavaLamp.setColorAnimatorListener(this);
+
+        loadValueAnimators();
+    }
+
+    public VisualizerView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public VisualizerView(Context context) {
+        this(context, null, 0);
+    }
+
+    private void updateViewVisibility() {
+        final int curVis = getVisibility();
+        final int newVis = mVisible && mStatusBarState != StatusBarState.SHADE
+                && mVisualizerEnabled ? View.VISIBLE : View.GONE;
+        if (curVis != newVis) {
+            setVisibility(newVis);
+            checkStateChanged();
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        final TunerService tunerService = Dependency.get(TunerService.class);
+        tunerService.addTunable(this, LOCKSCREEN_VISUALIZER_ENABLED);
+        tunerService.addTunable(this, LOCKSCREEN_VISUALIZER_AUTOCOLOR);
+        tunerService.addTunable(this, LOCKSCREEN_LAVALAMP_ENABLED);
+        tunerService.addTunable(this, LOCKSCREEN_LAVALAMP_SPEED);
+        tunerService.addTunable(this, LOCKSCREEN_SOLID_UNITS_COUNT);
+        tunerService.addTunable(this, LOCKSCREEN_SOLID_FUDGE_FACTOR);
+        tunerService.addTunable(this, LOCKSCREEN_SOLID_UNITS_OPACITY);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mLavaLamp.stop();
+        Dependency.get(TunerService.class).removeTunable(this);
+        mCurrentBitmap = null;
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case LOCKSCREEN_VISUALIZER_ENABLED:
+                mVisualizerEnabled =
+                        TunerService.parseIntegerSwitch(newValue, true);
+                break;
+            case LOCKSCREEN_VISUALIZER_AUTOCOLOR:
+                mAutoColor =
+                        TunerService.parseIntegerSwitch(newValue, false);
+                if (mCurrentBitmap != null && mAutoColor && !mLavaLampEnabled) {
+                    Palette.generateAsync(mCurrentBitmap, this);
+                } else if (mCurrentBitmap != null) {
+                    setBitmap(null);
+                } else {
+                    setColor(Color.TRANSPARENT);
+                }
+                break;
+            case LOCKSCREEN_LAVALAMP_ENABLED:
+                mLavaLampEnabled =
+                        TunerService.parseIntegerSwitch(newValue, true);
+                break;
+            case LOCKSCREEN_LAVALAMP_SPEED:
+                mLavaLampSpeed = 10000;
+                try {
+                    mLavaLampSpeed = Integer.valueOf(newValue);
+                } catch (NumberFormatException ex) {}
+                mLavaLamp.setAnimationTime(mLavaLampSpeed);
+                break;
+            case LOCKSCREEN_SOLID_UNITS_COUNT:
+                int oldUnits = mUnits;
+                mUnits = 32;
+                try {
+                    mUnits = Integer.valueOf(newValue);
+                } catch (NumberFormatException ex) {}
+                if (mUnits != oldUnits) {
+                    mFFTPoints = new float[mUnits * 4];
+                    onSizeChanged(0, 0, 0, 0);
+                }
+                break;
+            case LOCKSCREEN_SOLID_FUDGE_FACTOR:
+                mDbFuzzFactor = 16f;
+                try {
+                    mDbFuzzFactor = Integer.valueOf(newValue);
+                } catch (NumberFormatException ex) {}
+                break;
+            case LOCKSCREEN_SOLID_UNITS_OPACITY:
+                mOpacity = 140;
+                try {
+                    mOpacity = Integer.valueOf(newValue);
+                } catch (NumberFormatException ex) {}
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadValueAnimators() {
+        if (mValueAnimators != null) {
+            for (int i = 0; i < mValueAnimators.length; i++) {
+                mValueAnimators[i].cancel();
+            }
+        }
+        mValueAnimators = new ValueAnimator[mUnits];
+        for (int i = 0; i < mUnits; i++) {
             final int j = i * 4 + 1;
             mValueAnimators[i] = new ValueAnimator();
             mValueAnimators[i].setDuration(128);
@@ -173,52 +311,29 @@ public class VisualizerView extends View
         }
     }
 
-    private void updateViewVisibility() {
-        final int curVis = getVisibility();
-        final int newVis = mStatusBarState != StatusBarState.SHADE &&
-                mVisualizerEnabled ? View.VISIBLE : View.GONE;
-        if (curVis != newVis) {
-            setVisibility(newVis);
-            checkStateChanged();
-        }
-    }
+    private void setPortraitPoints() {
+        float units = Float.valueOf(mUnits);
+        float barUnit = mWidth / units;
+        float barWidth = barUnit * 8f / 9f;
+        barUnit = barWidth + (barUnit - barWidth) * units / (units - 1);
+        mPaint.setStrokeWidth(barWidth);
 
-    @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        Dependency.get(TunerService.class).addTunable(this, LOCKSCREEN_VISUALIZER_ENABLED);
-    }
-
-    @Override
-    protected void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-        Dependency.get(TunerService.class).removeTunable(this);
-        mCurrentBitmap = null;
-    }
-
-    @Override
-    public void onTuningChanged(String key, String newValue) {
-        if (LOCKSCREEN_VISUALIZER_ENABLED.equals(key)) {
-            mVisualizerEnabled = newValue == null || Integer.parseInt(newValue) != 0;
-            checkStateChanged();
-            updateViewVisibility();
+        for (int i = 0; i < mUnits; i++) {
+            mFFTPoints[i * 4] = mFFTPoints[i * 4 + 2] = i * barUnit + (barWidth / 2);
+            mFFTPoints[i * 4 + 1] = mHeight;
+            mFFTPoints[i * 4 + 3] = mHeight;
         }
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
+        if (w > 0) mWidth = w;
+        if (h > 0) mHeight = h;
 
-        float barUnit = w / 32f;
-        float barWidth = barUnit * 8f / 9f;
-        barUnit = barWidth + (barUnit - barWidth) * 32f / 31f;
-        mPaint.setStrokeWidth(barWidth);
+        super.onSizeChanged(mWidth, mHeight, oldw, oldh);
 
-        for (int i = 0; i < 32; i++) {
-            mFFTPoints[i * 4] = mFFTPoints[i * 4 + 2] = i * barUnit + (barWidth / 2);
-            mFFTPoints[i * 4 + 1] = h;
-            mFFTPoints[i * 4 + 3] = h;
-        }
+        loadValueAnimators();
+        setPortraitPoints();
     }
 
     @Override
@@ -236,18 +351,25 @@ public class VisualizerView extends View
     }
 
     @Override
-    public void onGenerated(Palette palette) {
-        int color = Color.TRANSPARENT;
+    public void onColorChanged(ColorAnimator colorAnimator, int color) {
+        if (mLavaLampEnabled)
+            setColor(color);
+    }
 
-        color = palette.getVibrantColor(color);
-        if (color == Color.TRANSPARENT) {
-            color = palette.getLightVibrantColor(color);
-            if (color == Color.TRANSPARENT) {
-                color = palette.getDarkVibrantColor(color);
-            }
+    @Override
+    public void onStartAnimation(ColorAnimator colorAnimator, int firstColor) {
+    }
+
+    @Override
+    public void onStopAnimation(ColorAnimator colorAnimator, int lastColor) {
+    }
+
+    public void setVisible(boolean visible) {
+        if (DEBUG) {
+            Log.i(TAG, "setVisible() called with visible = [" + visible + "]");
         }
-
-        setColor(color);
+        mVisible = visible;
+        updateViewVisibility();
     }
 
     public void setDozing(boolean dozing) {
@@ -256,16 +378,6 @@ public class VisualizerView extends View
                 Log.i(TAG, "setDozing() called with dozing = [" + dozing + "]");
             }
             mDozing = dozing;
-            checkStateChanged();
-        }
-    }
-
-    public void setOccluded(boolean occluded) {
-        if (mOccluded != occluded) {
-            if (DEBUG) {
-                Log.i(TAG, "setOccluded() called with occluded = [" + occluded + "]");
-            }
-            mOccluded = occluded;
             checkStateChanged();
         }
     }
@@ -290,12 +402,12 @@ public class VisualizerView extends View
         }
     }
 
-    public void setVisible(boolean visible) {
-        if (mVisible != visible) {
+    public void setOccluded(boolean occluded) {
+        if (mOccluded != occluded) {
             if (DEBUG) {
-                Log.i(TAG, "setVisible() called with visible = [" + visible + "]");
+                Log.i(TAG, "setOccluded() called with occluded = [" + occluded + "]");
             }
-            mVisible = visible;
+            mOccluded = occluded;
             checkStateChanged();
         }
     }
@@ -308,15 +420,31 @@ public class VisualizerView extends View
     }
 
     public void setBitmap(Bitmap bitmap) {
-        if (mCurrentBitmap == bitmap) {
+        if (mCurrentBitmap == bitmap)
             return;
-        }
+
         mCurrentBitmap = bitmap;
-        if (bitmap != null) {
-            Palette.generateAsync(bitmap, this);
-        } else {
+
+        if (mCurrentBitmap == null) {
             setColor(Color.TRANSPARENT);
+        } else if (mAutoColor && !mLavaLampEnabled) {
+            Palette.generateAsync(mCurrentBitmap, this);
         }
+    }
+
+    @Override
+    public void onGenerated(Palette palette) {
+        int color = Color.TRANSPARENT;
+
+        color = palette.getVibrantColor(color);
+        if (color == Color.TRANSPARENT) {
+            color = palette.getLightVibrantColor(color);
+            if (color == Color.TRANSPARENT) {
+                color = palette.getDarkVibrantColor(color);
+            }
+        }
+
+        setColor(color);
     }
 
     private void setColor(int color) {
@@ -324,12 +452,14 @@ public class VisualizerView extends View
             color = Color.WHITE;
         }
 
-        color = Color.argb(140, Color.red(color), Color.green(color), Color.blue(color));
+        color = Color.argb(mOpacity, Color.red(color), Color.green(color), Color.blue(color));
 
         if (mColor != color) {
             mColor = color;
 
-            if (mVisualizer != null) {
+            if (mVisualizer != null && shouldAnimate) {
+                shouldAnimate = false;
+
                 if (mVisualizerColorAnimator != null) {
                     mVisualizerColorAnimator.cancel();
                 }
@@ -339,14 +469,14 @@ public class VisualizerView extends View
                 mVisualizerColorAnimator.setStartDelay(600);
                 mVisualizerColorAnimator.setDuration(1200);
                 mVisualizerColorAnimator.start();
-            } else {
-                mPaint.setColor(mColor);
             }
+            mPaint.setColor(mColor);
         }
     }
 
     private void checkStateChanged() {
-        if (getVisibility() == View.VISIBLE && mVisible && mPlaying && !mDozing && !mPowerSaveMode
+        boolean isVisible = getVisibility() == View.VISIBLE;
+        if (isVisible && mPlaying && !mDozing && !mPowerSaveMode
                 && mVisualizerEnabled && !mOccluded) {
             if (!mDisplaying) {
                 mDisplaying = true;
@@ -355,11 +485,13 @@ public class VisualizerView extends View
                         .alpha(1f)
                         .withEndAction(null)
                         .setDuration(800);
+                if (mLavaLampEnabled) mLavaLamp.start();
             }
         } else {
             if (mDisplaying) {
                 mDisplaying = false;
-                if (mVisible) {
+                mLavaLamp.stop();
+                if (isVisible) {
                     animate()
                             .alpha(0f)
                             .withEndAction(mAsyncUnlinkVisualizer)
