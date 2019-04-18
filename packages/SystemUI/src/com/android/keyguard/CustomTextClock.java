9c377a174ea30860d2e208e122491b734636ca96
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-04-18 02:45:21 +0000

    base: CustomTextClock: Remove methods moved in utility classes
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I4d3b912a989f7716a4d75a748f86be584b3355f8

--- packages/SystemUI/src/com/android/keyguard/CustomTextClock.java
+++ packages/SystemUI/src/com/android/keyguard/CustomTextClock.java
@@ -242,17 +242,5 @@ public class CustomTextClock extends TextView {
         return NumString;
     }
 
-    private boolean langExEval (String langVal) {
-        return (ArrayUtils.contains(langExceptions, langVal) ? true : false);
-    }
-
-    private Bitmap drawEmpty() {
-        Bitmap convertedBitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(convertedBitmap);
-        Paint paint = new Paint();
-        paint.setColor(Color.YELLOW);
-        canvas.drawPaint(paint);
-        return convertedBitmap;
-    }
 }
 
