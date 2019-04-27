d77245494d7330bef8f83ec355e8335eb2eb7a4d
Pranav Vashi <neobuddy89@gmail.com>
2019-04-27 10:59:00 +0530

    Revert "Improve foreground task kill process"
    This reverts commit 06c2b972113d0ba17c87d5781ce5b3173aef71d4.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2291,8 +2291,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 return;
             }
 
-            if (ActionHandler.killProcess(mContext)) {
+            if (ActionUtils.killForegroundApp(mContext, mCurrentUserId)) {
                 performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false);
+                Toast.makeText(mContext,
+                        org.lineageos.platform.internal.R.string.app_killed_message,
+                        Toast.LENGTH_SHORT).show();
             }
         }
     };
