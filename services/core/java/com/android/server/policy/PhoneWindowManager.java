e0faddf279cd084dd69cf2bffa5f003c13ba5f97
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-02 13:55:33 -0300

    pocket: Don't show immersive mode confirmation when UI showing
    Change-Id: If7f181c9c3c8d14a91195fc98804329665adb019
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -8585,6 +8585,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      */
     private void handleDevicePocketStateChanged() {
         final boolean interactive = mPowerManager.isInteractive();
+        mImmersiveModeConfirmation.onDevicePocketStateChanged(mIsDeviceInPocket);
         if (mIsDeviceInPocket) {
             showPocketLock(interactive);
         } else {
