478a107ccdc0927c8d92ace9039000d0f2418e3c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-08 11:52:38 -0300

    SwipeToScreenshot: Import MIUI implementation
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    Change-Id: Iffdff674ba3419126df22150b47d1cb765b000d7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -608,7 +608,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mSwapCapacitiveKeys = false;
     ANBIHandler mANBIHandler;
     private boolean mANBIEnabled;
-    private OPGesturesListener mOPGestures;
+    private SwipeToScreenshotListener mSwipeToScreenshot;
     private boolean haveEnableGesture = false;
 
     // Tracks user-customisable behavior for certain key events
@@ -2970,13 +2970,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             boolean threeFingerGesture = Settings.System.getIntForUser(resolver,
                     Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
-            if (mOPGestures != null) {
+            if (mSwipeToScreenshot != null) {
                 if (haveEnableGesture != threeFingerGesture) {
                     haveEnableGesture = threeFingerGesture;
                     if (haveEnableGesture) {
-                        mWindowManagerFuncs.registerPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
+                        mWindowManagerFuncs.registerPointerEventListener(mSwipeToScreenshot, DEFAULT_DISPLAY);
                     } else {
-                        mWindowManagerFuncs.unregisterPointerEventListener(mOPGestures, DEFAULT_DISPLAY);
+                        mWindowManagerFuncs.unregisterPointerEventListener(mSwipeToScreenshot, DEFAULT_DISPLAY);
                     }
                 }
             }
@@ -6010,7 +6010,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mANBIHandler = new ANBIHandler(mContext);
 
-        mOPGestures = new OPGesturesListener(mContext, new OPGesturesListener.Callbacks() {
+        mSwipeToScreenshot = new SwipeToScreenshotListener(mContext, new SwipeToScreenshotListener.Callbacks() {
             @Override
             public void onSwipeThreeFinger() {
                 interceptScreenshotChord(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0 /*pressDelay*/);
