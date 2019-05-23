0c1dbf1be0b25330148f0eef8c8460cf73651382
soma, kawata <soma.kawata@sony.com>
2019-05-23 09:30:40 +0900

    Fix NullPointerException in ConnectivityService
    [Problem]
    NullPointerException occurs in ConnectivityService when boot up.
    
    [Root Cause]
    When ConnectivityService starts up, isTetheringSupported may access
    UserManager that has not been created yet. Therefore,
    NullPointerException occurs.
    
    [Solution]
    Changed to create UserManager before makeTethering().
    
    Bug: 134351774
    Test: ConnectivityServiceTest
    Change-Id: I299ecfa6af7295c1f3922f37d90eb5d3ab0d0286
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -863,6 +863,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
         mTestMode = mSystemProperties.get("cm.test.mode").equals("true")
                 && mSystemProperties.get("ro.build.type").equals("eng");
 
+        mUserManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
+
         mTethering = makeTethering();
 
         mPermissionMonitor = new PermissionMonitor(mContext, mNetd);
@@ -893,8 +895,6 @@ public class ConnectivityService extends IConnectivityManager.Stub
         mDataConnectionStats.startMonitoring();
 
         mPacManager = new PacManager(mContext, mHandler, EVENT_PROXY_HAS_CHANGED);
-
-        mUserManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
 
         mKeepaliveTracker = new KeepaliveTracker(mHandler);
         mNotifier = new NetworkNotificationManager(mContext, mTelephonyManager,
