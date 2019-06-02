53da896799f85f27ac2337c9ba5cf96c8a8bebf4
Pranav Vashi <neobuddy89@gmail.com>
2019-06-02 11:08:47 +0530

    Revert "QS: LocationTile: make it cycle modes"
    This reverts commit 95ae9456231732f9c0aa368e874b2d17f81e9441.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/LocationControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/LocationControllerImpl.java
@@ -22,7 +22,6 @@ import android.app.ActivityManager;
 import android.app.AppOpsManager;
 import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -98,29 +97,18 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
      *
      * @return true if attempt to change setting was successful.
      */
-    public boolean setLocationEnabled(int mode) {
+    public boolean setLocationEnabled(boolean enabled) {
         // QuickSettings always runs as the owner, so specifically set the settings
         // for the current foreground user.
         int currentUserId = ActivityManager.getCurrentUser();
         if (isUserLocationRestricted(currentUserId)) {
             return false;
         }
-        final ContentResolver cr = mContext.getContentResolver();
         // When enabling location, a user consent dialog will pop up, and the
         // setting won't be fully enabled until the user accepts the agreement.
-        Settings.Secure.putIntForUser(cr, Settings.Secure.LOCATION_MODE, mode,
-                currentUserId);
+        updateLocationEnabled(mContext, enabled, currentUserId,
+                Settings.Secure.LOCATION_CHANGER_QUICK_SETTINGS);
         return true;
-    }
-
-    /**
-     * Returns the current location mode.
-     */
-    public int getCurrentMode() {
-        int currentUserId = ActivityManager.getCurrentUser();
-        final ContentResolver cr = mContext.getContentResolver();
-         return Settings.Secure.getIntForUser(cr, Settings.Secure.LOCATION_MODE,
-                Settings.Secure.LOCATION_MODE_OFF, currentUserId);
     }
 
     /**
