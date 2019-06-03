d12663c16f0f13d81be81d896ee0ec1c82dc50e5
Joey <joey@lineageos.org>
2019-06-03 14:51:00 +0200

    PebbleBerry: sync dark mode with AOSP night mode too
    LiveDisplay night mode may not be available on all devices
    
    Change-Id: I6882bc871881e3a2385b698831aa80bd70b830fd
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -149,6 +149,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.app.ColorDisplayController;
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -661,6 +662,8 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
     private boolean mNavbarVisible;
 
     private BitmapDrawable altDrawable;
+
+    private ColorDisplayController mColorDisplayController;
 
     private BroadcastReceiver mWallpaperChangedReceiver = new BroadcastReceiver() {
         @Override
@@ -1209,6 +1212,9 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 DozeParameters.getInstance(context));
 
         mVisualizerView = (VisualizerView) mStatusBarWindow.findViewById(R.id.visualizerview);
+
+        mColorDisplayController = new ColorDisplayController(mContext,
+                ActivityManager.getCurrentUser());
 
         // Other icons
         mVolumeComponent = getComponent(VolumeComponent.class);
@@ -4466,6 +4472,20 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         return false;
     }
 
+    private boolean isAospNightModeOn() {
+        // SystemUI is initialized before ColorDisplayService, so the service may not
+        // be ready when this is called the first time
+        if (!mColorDisplayController.isAvailable(mContext)) {
+            return false;
+        }
+        try {
+            return mColorDisplayController.isActivated();
+        } catch (NullPointerException e) {
+            Log.w(TAG, e.getMessage());
+        }
+        return false;
+    }
+
     /**
      * Switches theme from light to dark and vice-versa.
      */
@@ -4482,7 +4502,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
         // 0 = auto, 1 = time-based, 2 = light, 3 = dark
         switch (mThemeOverride) {
             case 1:
-                useDarkTheme = isLiveDisplayNightModeOn();
+                useDarkTheme = isLiveDisplayNightModeOn() || isAospNightModeOn();
                 break;
             case 2:
                 useDarkTheme = false;
