713884b2ee80da64ebb8a47fbe9698b750f1da9e
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    Add toggle to disable charging animation [1/2]
    @neobuddy89: Use same toggle for wireless charging animation too.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5691,6 +5691,12 @@ public final class Settings {
         public static final String FP_ERROR_VIBRATE = "fp_error_vibrate";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
