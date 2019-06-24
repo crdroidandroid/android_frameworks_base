58adc15235370b4d7f70cd28634a2eb4dad5810f
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-24 21:27:35 +0530

    Add Cloudflare DNS as a private DNS provider
    TODO: Add tests
    
    Change-Id: I7e8a320d47e7c5ddbcb9acfaf23032ae92d5d70d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/net/ConnectivityManager.java
+++ core/java/android/net/ConnectivityManager.java
@@ -799,11 +799,28 @@ public class ConnectivityManager {
     /**
      * @hide
      */
+    public static final String PRIVATE_DNS_MODE_CLOUDFLARE = "cloudflare";
+    /**
+     * @hide
+     */
     public static final String PRIVATE_DNS_MODE_OPPORTUNISTIC = "opportunistic";
     /**
      * @hide
      */
     public static final String PRIVATE_DNS_MODE_PROVIDER_HOSTNAME = "hostname";
+    /**
+     * @hide
+     * Alternative: 1dot1dot1dot1.cloudflare-dns.com
+     */
+    public static final String PRIVATE_DNS_SPECIFIER_CLOUDFLARE = "one.one.one.one";
+    /**
+     * @hide
+     */
+    public static final String DNS_CLOUDFLARE_IP1 = "1.1.1.1";
+    /**
+     * @hide
+     */
+    public static final String DNS_CLOUDFLARE_IP2 = "1.0.0.1";
     /**
      * The default Private DNS mode.
      *
