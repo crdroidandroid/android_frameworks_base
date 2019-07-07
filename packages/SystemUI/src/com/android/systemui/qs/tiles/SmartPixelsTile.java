8d48ba8e017bcc18ca4c560b63ea5533827e465a
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 11:56:12 +0530

    Fix long click intent for Smart Pixels tile [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
@@ -35,11 +35,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.R;
 
 public class SmartPixelsTile extends QSTileImpl<BooleanState> {
-    private static final ComponentName SMART_PIXELS_SETTING_COMPONENT = new ComponentName(
-            "com.android.settings", "com.android.settings.Settings$SmartPixelsActivity");
-
-    private static final Intent SMART_PIXELS_SETTINGS =
-            new Intent().setComponent(SMART_PIXELS_SETTING_COMPONENT);
+    private static final Intent SMART_PIXELS_SETTINGS = new Intent("android.settings.SMART_PIXELS_SETTINGS");
 
     private boolean mSmartPixelsEnable;
     private boolean mSmartPixelsOnPowerSave;
