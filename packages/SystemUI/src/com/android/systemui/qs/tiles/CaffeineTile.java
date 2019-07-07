a103512add44f5f8afb0481af69149bced32ffcc
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 12:00:19 +0530

    Disable long click for Caffeine tile
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -64,7 +64,9 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     public BooleanState newTileState() {
-        return new BooleanState();
+        BooleanState state = new BooleanState();
+        state.handlesLongClick = false;
+        return state;
     }
 
     @Override
