680d14da3a0b2872e1b21ccbabf7f98e9fddc096
ezio84 <brabus84@gmail.com>
2019-07-26 12:45:22 +0300

    Add less boring heads up option [SQUASHED]
    Q: don't skip the new Bubbles (the circular popping up notifications
    like the Dialer one when a call is active and you press the home button)
    Reference: https://github.com/ezio84/abc_frameworks_base/commit/c3d6f7d386286a4e55a3e0644782827705db7168#diff-c38ee79f64d8dc57e7757863d741be46
    
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    neobuddy89:
    * Added clock app too in whitelist.
    * Adapted and squashed for A12.
    
    Stallix:
    * Adapted logging for A13.
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Wed, 14 Dec 2022 08:22:21 +0530
        Subject: Slightly optimize less boring apps check for heads up
    
        * We do not want to keep checking packages everytime
          when this option is disabled or device is dozing.
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Thu, 9 Mar 2023 12:06:30 +0800
        Subject: Less boring heads up: Don't skip heads up for most messaging apps
    
        From: Rushab Shah <rushabshah32@gmail.com>
        Date: Sun, 30 Apr 2023 00:22:42 +0530
        Subject: LessBoring: fixup LessBoring preventing bubbles
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
@@ -367,7 +367,9 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 Handler.createAsync(Looper.myLooper()));
 
         mNotificationInterruptStateProvider =
-                new TestableNotificationInterruptStateProviderImpl(mContext.getContentResolver(),
+                new TestableNotificationInterruptStateProviderImpl(
+                        mContext,
+                        mContext.getContentResolver(),
                         mPowerManager,
                         mAmbientDisplayConfiguration,
                         mStatusBarStateController,
@@ -1307,6 +1309,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
             NotificationInterruptStateProviderImpl {
 
         TestableNotificationInterruptStateProviderImpl(
+                Context context,
                 ContentResolver contentResolver,
                 PowerManager powerManager,
                 AmbientDisplayConfiguration ambientDisplayConfiguration,
@@ -1321,6 +1324,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 UiEventLogger uiEventLogger,
                 UserTracker userTracker) {
             super(
+                    context,
                     contentResolver,
                     powerManager,
                     ambientDisplayConfiguration,
