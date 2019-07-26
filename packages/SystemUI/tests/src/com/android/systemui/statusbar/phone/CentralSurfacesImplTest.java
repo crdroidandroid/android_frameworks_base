c129b7d078500ce7b6963d3e4c84006d0ccb9af2
ezio84 <brabus84@gmail.com>
2019-07-26 12:45:22 +0300

    Less boring heads up option [1/2]
    Q: don't skip the new Bubbles (the circular popping up notifications
    like the Dialer one when a call is active and you press the home button)
    Reference: https://github.com/ezio84/abc_frameworks_base/commit/c3d6f7d386286a4e55a3e0644782827705db7168#diff-c38ee79f64d8dc57e7757863d741be46
    
    Allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    neobuddy89:
    * Added clock app too in whitelist.
    * Adapted and squashed for A12.
    
    Stallix:
    * Adapted logging for A13.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/CentralSurfacesImplTest.java
@@ -338,7 +338,9 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 Handler.createAsync(Looper.myLooper()));
 
         mNotificationInterruptStateProvider =
-                new TestableNotificationInterruptStateProviderImpl(mContext.getContentResolver(),
+                new TestableNotificationInterruptStateProviderImpl(
+                        mContext,
+                        mContext.getContentResolver(),
                         mPowerManager,
                         mAmbientDisplayConfiguration,
                         mStatusBarStateController,
@@ -1347,6 +1349,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
             NotificationInterruptStateProviderImpl {
 
         TestableNotificationInterruptStateProviderImpl(
+                Context context,
                 ContentResolver contentResolver,
                 PowerManager powerManager,
                 AmbientDisplayConfiguration ambientDisplayConfiguration,
@@ -1361,6 +1364,7 @@ public class CentralSurfacesImplTest extends SysuiTestCase {
                 UiEventLogger uiEventLogger,
                 UserTracker userTracker) {
             super(
+                    context,
                     contentResolver,
                     powerManager,
                     ambientDisplayConfiguration,
