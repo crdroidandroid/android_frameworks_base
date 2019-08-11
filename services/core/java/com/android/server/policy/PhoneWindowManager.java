834e21a54014188c5f5477ca61198047fe9f05ca
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-10 22:06:22 -0300

    base: Port extended screenshot function from OOS
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -213,6 +213,7 @@ import android.widget.Toast;
 
 import com.android.internal.R;
 import com.android.internal.accessibility.AccessibilityShortcutController;
+import com.android.internal.custom.longshot.ILongScreenshotManager;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.os.DeviceKeyHandler;
@@ -1741,14 +1742,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         @Override
         public void run() {
             if(!mPocketLockShowing) {
-                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType);
+                boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
+                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
             }
         }
     }
 
     private final ScreenshotRunnable mScreenshotRunnable = new ScreenshotRunnable();
 
-    private void takeScreenshot(int screenshotType) {
+    @Override
+    public void takeScreenshot(int screenshotType) {
         takeScreenshotDelayed(screenshotType, 0);
     }
 
@@ -1782,6 +1785,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     void showGlobalActionsInternal() {
+        stopLongshot();
         if (mGlobalActions == null) {
             mGlobalActions = new GlobalActions(mContext, mWindowManagerFuncs);
         }
@@ -1790,6 +1794,24 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // since it took two seconds of long press to bring this up,
         // poke the wake lock so they have some time to see the dialog.
         mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
+    }
+
+    private void stopLongshot() {
+        ILongScreenshotManager shot = ILongScreenshotManager.Stub.asInterface(ServiceManager.getService(Context.LONGSCREENSHOT_SERVICE));
+        if (shot != null) {
+            try {
+                if (shot.isLongshotMode()) {
+                    shot.stopLongshot();
+                }
+            } catch (RemoteException e) {
+                Slog.d(TAG, e.toString());
+            }
+        }
+    }
+
+    @Override
+    public void stopLongshotConnection() {
+        mDefaultDisplayPolicy.stopLongshotConnection();
     }
 
     boolean isDeviceProvisioned() {
