36cd10bad2803e404701608bd29154a85d0d08af
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-21 23:33:42 -0300

    fwb: Add support for internal audio recording
    Change-Id: I953c07d9a511b53ddb79f6a7ce5b47ca0c079acb
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/plugin/src/com/android/systemui/plugins/VolumeDialogController.java
+++ packages/SystemUI/plugin/src/com/android/systemui/plugins/VolumeDialogController.java
@@ -105,6 +105,7 @@ public interface VolumeDialogController {
         public boolean disallowMedia;
         public boolean disallowSystem;
         public boolean disallowRinger;
+        public boolean routedToSubmixAndEarphone;
 
         public State copy() {
             final State rt = new State();
@@ -123,6 +124,7 @@ public interface VolumeDialogController {
             rt.disallowMedia = disallowMedia;
             rt.disallowSystem = disallowSystem;
             rt.disallowRinger = disallowRinger;
+            rt.routedToSubmixAndEarphone = routedToSubmixAndEarphone;
             return rt;
         }
 
@@ -156,6 +158,7 @@ public interface VolumeDialogController {
             sep(sb, indent); sb.append("disallowMedia:").append(disallowMedia);
             sep(sb, indent); sb.append("disallowSystem:").append(disallowSystem);
             sep(sb, indent); sb.append("disallowRinger:").append(disallowRinger);
+            sep(sb, indent); sb.append("routedToSubmixAndEarphone:").append(routedToSubmixAndEarphone);
             if (indent > 0) sep(sb, indent);
             return sb.append('}').toString();
         }
