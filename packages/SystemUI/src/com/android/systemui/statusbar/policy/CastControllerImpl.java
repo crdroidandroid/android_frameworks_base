32311d2455c31b53006ca7bf70a1c6e860bc7f37
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-02 02:49:15 -0300

    SystemUI: Whitelist our recorder on CastController
    Change-Id: Ifeec0d416d0ae30e01fbf659c6a48e1c650cf284
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/CastControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/CastControllerImpl.java
@@ -47,6 +47,7 @@ import java.util.UUID;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import com.android.internal.util.custom.recorder.RecorderConstants;
 
 /** Platform implementation of the cast controller. **/
 @Singleton
@@ -177,6 +178,10 @@ public class CastControllerImpl implements CastController {
         }
 
         synchronized (mProjectionLock) {
+            if (mProjection != null && mProjection.getPackageName().equals(
+                    RecorderConstants.APP_PACKAGE_NAME)){
+                mProjection = null;
+            }
             if (mProjection != null) {
                 final CastDevice device = new CastDevice();
                 device.id = mProjection.getPackageName();
