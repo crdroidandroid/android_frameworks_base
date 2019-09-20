34851cdfee9d45536e1411fdce7e95d54093a5b5
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-20 17:47:19 +0530

    Fix Fi permissions
    For Nexus 6 users, turn Fi permissions on by default (on first boot)
    
    [AgentFabulous - POSP]
    - Adapt for android-10.0.0_r2
    - Use grantSystemFixedPermissionsToSystemPackage to simplify syntax
    
    Date: Fri, 20 Sep 2019 17:47:19 +0530
    Subject: [PATCH] Fix permissons for ContactsProvider2
    
    Fix this error:
    
    W ActivityManager: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS
    E GooglePlusContactsSync: Failed to clear out contacts
    E GooglePlusContactsSync: java.lang.SecurityException: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS
    
    [AgentFabulous - POSP]
    - Adapt for android-10.0.0_r2
    - Use grantSystemFixedPermissionsToSystemPackage to simplify syntax
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -884,6 +884,15 @@ public final class DefaultPermissionGrantPolicy {
         // Google Play Store
         grantSystemFixedPermissionsToSystemPackage(pm, "com.android.vending", userId, CONTACTS_PERMISSIONS,
                 PHONE_PERMISSIONS, ALWAYS_LOCATION_PERMISSIONS, SMS_PERMISSIONS, STORAGE_PERMISSIONS);
+
+        // Project Fi
+        grantSystemFixedPermissionsToSystemPackage(pm, "com.google.android.apps.tycho", userId, CONTACTS_PERMISSIONS,
+                PHONE_PERMISSIONS, MICROPHONE_PERMISSIONS, ALWAYS_LOCATION_PERMISSIONS, SMS_PERMISSIONS);
+
+        // ContactsProvider2
+        grantSystemFixedPermissionsToSystemPackage(pm,
+                getDefaultProviderAuthorityPackage("com.android.providers.contacts.ContactsProvider2", userId), userId,
+                CONTACTS_PERMISSIONS, STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
