5ead06c3aea36ac1a4502e0db9f8695eee4fcac8
darkobas <darkobas@gmail.com>
2019-10-07 20:31:48 +0200

    base: Add Doze-on-charge customization [1/2]
    @idoybh edits: Adapted to A11's settings backup
    [jhonboy121]: adapt to A12 changes in DozeParameters. Also in the og commit DOZE_ALWAYS_ON was being read from System namespace instead of Secure, fixed it here. Make use of SettingsProxy utlity classes for settings / getting values
    
    Change-Id: I831583fde68de15788e3d7ecab55d864726d140f
    
    Commit message #2:
    
    base: check whether device is charging when alwaysOnChargingEnabled is called
    
    * Using settings for this is quite redundant. And if you toggle aod on charge after plugging in then aod won't turn on unless you unplug and plug again (since DOZE_ON_CHARGE_NOW is set only if aod on charge is enabled and device is plugged in"
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Commit message #3:
    
    base: fix deadlock between activity manager and power manager
    
    * Using the battery manager intent to query plugged in status was the root cause of deadlock,
      so inside power manager, user mIsPowered and setting value instead of using the intent based power status
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]: adapted to A13
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14239,6 +14239,12 @@ public final class Settings {
         public static final String SHOW_CLIPBOARD_OVERLAY = "show_clipboard_overlay";
 
         /**
+         * Whether to enable DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE = "doze_on_charge";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
