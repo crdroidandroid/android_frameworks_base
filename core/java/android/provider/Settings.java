e7c5b865b0b85ed57295d9a0d6282b4fdcec0ef7
Pranav Vashi <neobuddy89@gmail.com>
2019-10-08 22:59:24 +0530

    Add separate tunables for navbar layout customization
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12687,6 +12687,18 @@ public final class Settings {
         public static final String GAME_OVERLAY = "game_overlay";
 
         /**
+         * Select from different navigation bar layouts
+         * @hide
+         */
+        public static final String NAVBAR_LAYOUT_VIEWS = "navbar_layout_views";
+
+        /**
+         * Inverse navigation bar layout
+         * @hide
+         */
+        public static final String NAVBAR_INVERSE_LAYOUT = "navbar_inverse_layout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
