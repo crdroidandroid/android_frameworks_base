411a8ef29fcad675c0e1222ebeb418382b702f37
Anushek Prasal <anushekprasal@gmail.com>
2019-10-12 01:31:48 +0530

    base: Grant storage permission to ThemePicker
    @neobuddy89
    - Adapt for android-10.0.0_r2
    - Use grantSystemFixedPermissionsToSystemPackage to simplify syntax
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -905,6 +905,9 @@ public final class DefaultPermissionGrantPolicy {
         // Google dialer
         grantSystemFixedPermissionsToSystemPackage(pm, "com.google.android.dialer", userId, PHONE_PERMISSIONS,
                 CONTACTS_PERMISSIONS, SMS_PERMISSIONS);
+
+        // ThemePicker
+        grantSystemFixedPermissionsToSystemPackage(pm, "com.android.wallpaper", userId, STORAGE_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
