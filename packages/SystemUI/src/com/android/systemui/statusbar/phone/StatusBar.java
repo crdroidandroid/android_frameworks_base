ad5fccc9a0873437d35ed47d8ef0517621283039
Pranav Vashi <neobuddy89@gmail.com>
2019-10-21 02:47:43 +0530

    PebbleBerry: Support initial dark themes
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3529,9 +3529,11 @@ public class StatusBar extends SystemUI implements DemoMode,
                 break;
         }
 
-        boolean isDark = mUiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES;
-
-        if (mUiModeManager != null && isDark != useDarkTheme) {
+        if (mUiModeManager != null && isUsingDarkSystemTheme() != useDarkTheme) {
+            mUiOffloadThread.submit(() -> {
+                ThemeAccentUtils.setSystemTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId(),
+                                            useDarkTheme);
+            });
             if (useDarkTheme) {
                 mUiOffloadThread.submit(() -> {
                     mUiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
@@ -3554,6 +3556,11 @@ public class StatusBar extends SystemUI implements DemoMode,
             Dependency.get(ConfigurationController.class).notifyThemeChanged();
         }
         updateCorners();
+    }
+
+    // Check for the dark system theme
+    public boolean isUsingDarkSystemTheme() {
+        return ThemeAccentUtils.isUsingDarkTheme(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
     private void updateCorners() {
