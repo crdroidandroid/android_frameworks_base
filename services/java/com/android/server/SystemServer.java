3055e6a420b29b65dd066832227cfd3ce3845430
Chris Lahaye <dev@chrislahaye.com>
2019-10-29 17:37:17 +0100

    pocket: introduce pocket bridge
    This service communicates pocket state to the pocket judge kernel driver.
    It maintains the pocket state by binding to the pocket service.
    
    Ticket: NOUGAT-49
    
    Change-Id: Iee77f767f23d3f77a0d1d871f9305f3b3c6d6630
    Signed-off-by: Chris Lahaye <dev@chrislahaye.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -135,6 +135,7 @@ import com.android.server.os.DeviceIdentifiersPolicyService;
 import com.android.server.os.SchedulingPolicyService;
 import com.android.server.people.PeopleService;
 import com.android.server.pocket.PocketService;
+import com.android.server.pocket.PocketBridgeService;
 import com.android.server.pm.BackgroundDexOptService;
 import com.android.server.pm.CrossProfileAppsService;
 import com.android.server.pm.DataLoaderManagerService;
@@ -2019,13 +2020,21 @@ public final class SystemServer {
             mSystemServiceManager.startService(CrossProfileAppsService.class);
             t.traceEnd();
 
-            traceBeginAndSlog("StartPocketService");
+            t.traceBegin("StartPocketService");
             mSystemServiceManager.startService(PocketService.class);
             t.traceEnd();
 
             t.traceBegin("StartPeopleService");
             mSystemServiceManager.startService(PeopleService.class);
             t.traceEnd();
+
+            if (!context.getResources().getString(
+                    com.android.internal.R.string.config_pocketBridgeSysfsInpocket).isEmpty()) {
+                t.traceBegin("StartPocketBridgeService");
+                mSystemServiceManager.startService(PocketBridgeService.class);
+                t.traceEnd();
+            }
+
         }
 
         if (!isWatch) {
