733964e8ec1d35b838a1ced2a6712a2a733b0bed
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-10-29 17:40:41 +0100

    Pocket lock improvements
    * Fix auto hide not working correctly
    * Restore systemui visibility when hidden
    
    ZeNiXxX
    * remove faceunlock logic and adapt for android 10
    
    Change-Id: I61b38798d01775b252af88abb01dd2423b3a7638
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -424,6 +424,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         }
     };
 
+    public boolean isPocketLockVisible(){
+        return mPocketManager.isPocketLockVisible();
+    }
+
     private final IBiometricEnabledOnKeyguardCallback mBiometricEnabledCallback =
             new IBiometricEnabledOnKeyguardCallback.Stub() {
                 @Override
