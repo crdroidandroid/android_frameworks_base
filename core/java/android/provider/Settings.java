de444a6f16219041837b72550bd43b5fb9e968aa
ezio84 <brabus84@gmail.com>
2019-11-02 17:46:14 +0100

    SystemUI: Allow to pulse on new tracks
    * @neobuddy89: Forward Port to Android 11/12/13/14/16
    
    neobuddy89:
    * Remove now playing package usage. Rely on metadata changes.
    * Improve doze pulse broadcast usage.
    * Add minor fixes and NPE guards.
    * Use BG executor for smooth UX.
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14227,6 +14227,12 @@ public final class Settings {
         public static final String DOZE_ON_CHARGE = "doze_on_charge";
 
         /**
+         * Whether to pulse ambient on new music tracks
+         * @hide
+         */
+        public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
