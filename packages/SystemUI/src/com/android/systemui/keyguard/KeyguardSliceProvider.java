2a186ade0b45d80780b14cafc222f8eba4b5a198
ezio84 <brabus84@gmail.com>
2019-11-02 17:46:14 +0100

    SystemUI: Allow to pulse on new tracks
    * @neobuddy89: Forward Port to Android 11/12/13/14/16
    
    neobuddy89:
    * Remove now playing package usage. Rely on metadata changes.
    * Improve doze pulse broadcast usage.
    * Add minor fixes and NPE guards.
    * Use BG executor for smooth UX.
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -32,7 +32,9 @@ import android.media.MediaMetadata;
 import android.media.session.PlaybackState;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.notification.ZenModeConfig;
 import android.text.TextUtils;
@@ -97,6 +99,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
             "content://com.android.systemui.keyguard/media";
     public static final String KEYGUARD_ACTION_URI =
             "content://com.android.systemui.keyguard/action";
+    private static final String PULSE_ACTION = "com.android.systemui.doze.pulse";
 
     /**
      * Only show alarms that will ring within N hours.
@@ -106,6 +109,8 @@ public class KeyguardSliceProvider extends SliceProvider implements
 
     private static final Object sInstanceLock = new Object();
     private static KeyguardSliceProvider sInstance;
+
+    private static final long MIN_PULSE_INTERVAL_MS = 3000;
 
     protected final Uri mSliceUri;
     protected final Uri mHeaderUri;
@@ -151,6 +156,9 @@ public class KeyguardSliceProvider extends SliceProvider implements
     protected boolean mDozing;
     private int mStatusBarState;
     private boolean mMediaIsVisible;
+    private boolean mPulseOnNewTracks;
+    private long mLastPulseUptimeMs = 0L;
+
     private ContentProviderContextAvailableCallback mContextAvailableCallback;
     @Inject
     WakeLockLogger mWakeLockLogger;
@@ -328,7 +336,9 @@ public class KeyguardSliceProvider extends SliceProvider implements
 
     @Override
     public boolean onCreateSliceProvider() {
-        mContextAvailableCallback.onContextAvailable(getContext());
+        if (mContextAvailableCallback != null) {
+            mContextAvailableCallback.onContextAvailable(getContext());
+        }
         if (mMediaManager == null) {
             Log.e(TAG, "Dagger injection failed, cannot start. See any above warnings with string: "
                     + "\"No injector for class\"");
@@ -406,7 +416,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
         }
 
         long limit = System.currentTimeMillis() + TimeUnit.HOURS.toMillis(hours);
-        return mNextAlarmInfo.getTriggerTime() <= limit;
+        return alarmClockInfo.getTriggerTime() <= limit;
     }
 
     /**
@@ -531,6 +541,27 @@ public class KeyguardSliceProvider extends SliceProvider implements
         mMediaArtist = artist;
         mMediaIsVisible = nextVisible;
         notifyChange();
+        if (mPulseOnNewTracks && mMediaIsVisible
+                && !mDozeParameters.getAlwaysOn() && mDozing
+                && !TextUtils.isEmpty(mMediaTitle)) {
+            final long now = SystemClock.uptimeMillis();
+            if (now - mLastPulseUptimeMs >= MIN_PULSE_INTERVAL_MS) {
+                mLastPulseUptimeMs = now;
+                mBgHandler.post(() -> {
+                    try {
+                        Intent intent = new Intent(PULSE_ACTION)
+                                .addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+                        getContext().sendBroadcastAsUser(intent, UserHandle.CURRENT);
+                    } catch (Exception e) {
+                        Log.e(TAG, "Error on sendBroadcastAsUser()", e);
+                    }
+                });
+            }
+        }
+    }
+
+    public void setPulseOnNewTracks(boolean enabled) {
+        mPulseOnNewTracks = enabled;
     }
 
     protected void notifyChange() {
