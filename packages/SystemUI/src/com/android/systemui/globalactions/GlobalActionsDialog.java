38f9d09925a5aa2e5cb25b3c216571d3249142eb
Pranav Vashi <neobuddy89@gmail.com>
2019-11-10 16:59:08 +0530

    GlobalActionsDialog: Fix corner cases of NPE
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1280,7 +1280,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         public boolean onLongClickItem(int position) {
             final Action action = mAdapter.getItem(position);
             if (action instanceof LongPressAction) {
-                mDialog.dismiss();
+                if (mDialog != null) mDialog.dismiss();
                 return ((LongPressAction) action).onLongPress();
             }
             return false;
@@ -1290,7 +1290,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         public void onClickItem(int position) {
             Action item = mAdapter.getItem(position);
             if (!(item instanceof SilentModeTriStateAction)) {
-                mDialog.dismiss();
+                if (mDialog != null) mDialog.dismiss();
             }
             item.onPress();
         }
