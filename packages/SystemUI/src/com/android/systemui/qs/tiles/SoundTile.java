145e4a58925ec03702f0bdc392ea569ed939ed2d
Pranav Vashi <neobuddy89@gmail.com>
2019-11-10 11:51:58 +0530

    SoundTile: Make default state inactive
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -139,6 +139,7 @@ public class SoundTile extends QSTileImpl<BooleanState> {
             default:
                 break;
         }
+        state.state = Tile.STATE_INACTIVE;
     }
 
     @Override
