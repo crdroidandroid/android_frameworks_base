cbead8dbb0ee0cdd37ca7ffc7c4ea6749070427b
Christian Oder <myself5@carbonrom.org>
2019-11-26 23:35:01 +0100

    SystemUI: Introduce DataSwitchTile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    @neobuddy89: Updated for Android 14 and squashed below -
    
        From: DennySPB <dennyspb@gmail.com>
        Date: Thu, 5 Dec 2019 12:40:17 +0300
        Subject: DataSwitchTile: collapse notification panel onClick
    
        Change-Id: I37a064c910ea05493b41bdd0123ca3a6aca1f25d
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: micky387 <mickaelsaibi@free.fr>
        Date: Wed, 11 Mar 2020 13:36:51 +0100
        Subject: DataSwitchTile: dont show toast on click
    
        and add drawable for No SIM
    
        Change-Id: Ib9d26630879420216e67de1a1e6fc82c5c2ace5e
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: DennySPb <dennyspb@gmail.com>
        Date: Tue, 17 Nov 2020 11:39:43 +0300
        Subject: SystemUI: Show carrier name of opposite slot in DataSwitch tile label
    
        make it more user friendly
    
        Change-Id: I53094db21fe21b1a4bd9ee76fd28661622eb5e26
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Wed, 14 Jul 2021 01:43:13 +0900
        Subject: DataSwitchTile: Improve the code
    
        * Get rid of deprecated AsyncTask
        * Removed unused imports
        * Finalize global vars
        * Some other formatting and minor stuff
    
        Change-Id: Id3e39c98dac6200301d15bf752f866f47875633d
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Mon, 7 Feb 2022 15:06:47 +0530
        Subject: DataSwitchTile: Fix issue when subId is non-binary
    
        Fixes: https://github.com/crdroidandroid/android_frameworks_base/issues/770
    
        Other changes:
        1. Collapse QS panel after switching sim.
        2. Change drawable to reflect opposite sim number.
        3. Move move opposite sim label to secondary level.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: ShevT <evgeny.shishkov@gmail.com>
        Date: Sat, 12 Feb 2022 00:02:04 +0300
        Subject: DataSwitchTile: Resolve initial tile state
    
         After loading the system, we do not see on the tile the telecom
         operator to which we will switch. Instead, we see "On". And so on
         until we switch the network. If you pull out one SIM, then the name
         of the operator to which we could switch to will "freeze" on the tile.
    
         Let's fix this.
    
        @neobuddy89: Improvise logic and rewrite.
    
        Change-Id: I7a4996b48e014825c5452c1259454e9b7c70ce82
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Joe Maples <joe@maples.dev>
        Date: Sat, 29 Feb 2020 16:38:14 -0500
        Subject: DataSwitchTile: Use Mobile Data panel
    
        Change-Id: I9f818eea1ab906bb1721e0aab4cbb138187e507b
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Hernán Castañón Álvarez <herna@paranoidandroid.co>
        Date: Thu, 4 Jun 2020 19:08:06 +0000
        Subject: [PATCH 0533/1200] DataSwitchTile: Update SIMs QS icons
    
        * Designed and made by Andrew Fluck.
    
        Change-Id: I1b83c360029382a8a1d0598758cddf1634f7d489
        Co-authored-by: Andrew Fluck <andrew@aospa.co>
        Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sat, 25 Mar 2023 14:16:57 +0530
        Subject: DataSwitchTile: Show active sim as tile current state
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.CPUInfoTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
+import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.FPSInfoTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.OnTheGoTile
@@ -77,6 +78,12 @@ interface LineageModule {
     @IntoMap
     @StringKey(CompassTile.TILE_SPEC)
     fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
+
+    /** Inject DataSwitchTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DataSwitchTile.TILE_SPEC)
+    fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
 
     /** Inject FPSInfoTile into tileMap in QSModule */
     @Binds
