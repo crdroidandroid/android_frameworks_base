e36f5b4a7910381bf23c907372d81cf605ad5978
Anushek Prasal <anushekprasal@gmail.com>
2019-12-01 03:25:39 +0530

    base: Allow to use old style icons as default [1/2]
    * Some devices don't have much statusbar space due to a big notch.
      Let's add a bool for them to set the old style icons as default.
      Users can later disable it if they want.
    
    @neobuddy89: Code optimization.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -219,6 +219,7 @@ public interface StatusBarIconController {
         protected DemoStatusIcons mDemoStatusIcons;
 
         private boolean mOldStyleType;
+        private boolean mConfigUseOldMobileType;
 
         private static final String USE_OLD_MOBILETYPE =
             "system:" + Settings.System.USE_OLD_MOBILETYPE;
@@ -228,6 +229,8 @@ public interface StatusBarIconController {
             mContext = group.getContext();
             mIconSize = mContext.getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.status_bar_icon_size);
+            mConfigUseOldMobileType = mContext.getResources().
+                    getBoolean(com.android.internal.R.bool.config_useOldMobileIcons);
 
             DisableStateTracker tracker =
                     new DisableStateTracker(DISABLE_NONE, DISABLE2_SYSTEM_ICONS, commandQueue);
@@ -459,7 +462,7 @@ public interface StatusBarIconController {
             switch (key) {
                 case USE_OLD_MOBILETYPE:
                     mOldStyleType =
-                        TunerService.parseIntegerSwitch(newValue, false);
+                        TunerService.parseIntegerSwitch(newValue, mConfigUseOldMobileType);
                     updateOldStyleMobileDataIcons();
                     break;
                 default:
