f3d1796c4cf705ff7eff50f28533d28d456f3217
Pranav Vashi <neobuddy89@gmail.com>
2019-12-01 21:28:27 +0530

    Add toggle for emergency affordance in power menu [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14462,6 +14462,11 @@ public final class Settings {
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
         /**
+         * @hide
+         */
+        public static final String POWER_MENU_EMERGENCY_AFFORDANCE = "power_menu_emergency_affordance";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
