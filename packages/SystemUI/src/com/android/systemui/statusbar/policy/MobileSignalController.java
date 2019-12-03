677c7394f1eb0bda56bdf635aa8a80a531dba90a
Anushek Prasal <anushekprasal@gmail.com>
2019-12-03 16:42:45 +0530

    Notify listeners when toggling old mobile type settings
    * For instant update.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -106,6 +106,8 @@ public class MobileSignalController extends SignalController<
             "system:" + Settings.System.SHOW_FOURG_ICON;
     private static final String DATA_DISABLED_ICON =
             "system:" + Settings.System.DATA_DISABLED_ICON;
+    private static final String USE_OLD_MOBILETYPE =
+            "system:" + Settings.System.USE_OLD_MOBILETYPE;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -158,6 +160,7 @@ public class MobileSignalController extends SignalController<
         Dependency.get(TunerService.class).addTunable(this, ROAMING_INDICATOR_ICON);
         Dependency.get(TunerService.class).addTunable(this, SHOW_FOURG_ICON);
         Dependency.get(TunerService.class).addTunable(this, DATA_DISABLED_ICON);
+        Dependency.get(TunerService.class).addTunable(this, USE_OLD_MOBILETYPE);
     }
 
     @Override
@@ -183,6 +186,9 @@ public class MobileSignalController extends SignalController<
                         TunerService.parseIntegerSwitch(newValue, true);
                      updateTelephony();                
                 break; 
+            case USE_OLD_MOBILETYPE:
+                notifyListeners();
+                break;
             default:
                 break;
         }
