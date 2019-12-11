f25e5cf31e6c0fa0cc7affa03789496d17a41780
Lucchetto <lucchetto.tie@live.com>
2019-12-11 19:48:13 +0100

    Lockscreen charging info: show decimal in battery temperature
    Change-Id: I10bbe4df988387c401802475697452796e0afb6d
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -128,7 +128,7 @@ public class KeyguardIndicationController implements StateListener,
     private int mBatteryLevel;
     private int mChargingCurrent;
     private double mChargingVoltage;
-    private int mTemperature;
+    private float mTemperature;
     private String mMessageToShowOnScreenOn;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
