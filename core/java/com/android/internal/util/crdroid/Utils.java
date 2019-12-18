0f6b579e8836ef0ed68d5725b39fbc3c0eaa4773
Alex <du.alexcruz@gmail.com>
2019-12-18 10:26:50 +0100

    Utils: Add toggleVolumePanel action
    Ref Commit: https://github.com/DirtyUnicorns/android_frameworks_base/commit/296209c52247802eb836302a3516f0a8f06b7249#diff-d4316cfc617c4372899ff3b1fd7c6e73
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/Utils.java
+++ core/java/com/android/internal/util/crdroid/Utils.java
@@ -32,6 +32,7 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.input.InputManager;
+import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.os.Handler;
 import android.os.Looper;
@@ -331,5 +332,10 @@ public class Utils {
         }
 
         return null;
+    }
+
+    public static void toggleVolumePanel(Context context) {
+        AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        am.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
     }
 }
