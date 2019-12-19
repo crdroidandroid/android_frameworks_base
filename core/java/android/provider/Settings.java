1ebfe2e4334fd09c2bc3a8d4033c146f2f688f73
Ido Ben-Hur <idoybh2@gmail.com>
2019-12-19 15:27:43 +0200

    SystemUI: Allow changing duration and repeats count of ambient edge light [1/2]
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ib2dcdfad3dc0e69155b72a5be0228550a61e7607

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5505,6 +5505,16 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_COLOR_AUTOMATIC = "ambient_notification_light_color_automatic";
 
         /**
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
+
+        /**
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/omni/NotificationLightsView.java
+++ packages/SystemUI/src/com/android/systemui/omni/NotificationLightsView.java
@@ -88,13 +88,19 @@ public class NotificationLightsView extends RelativeLayout {
     }
 
     public void animateNotificationWithColor(int color) {
+        int duration = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_LIGHT_DURATION, 2,
+                UserHandle.USER_CURRENT) * 1000;
+        int repeat = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_LIGHT_REPEAT_COUNT, 0,
+                UserHandle.USER_CURRENT);
         ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
         ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
         leftView.setColorFilter(color);
         rightView.setColorFilter(color);
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
-        mLightAnimator.setDuration(2000);
-        mLightAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        mLightAnimator.setDuration(duration);
+        mLightAnimator.setRepeatCount(repeat == 0 ? ValueAnimator.INFINITE : repeat);
         mLightAnimator.setRepeatMode(ValueAnimator.RESTART);
         mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
