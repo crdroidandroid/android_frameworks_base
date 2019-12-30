fef0f093be1eec080fa58f84c5ce013dcbf2ef8d
Pranav Vashi <neobuddy89@gmail.com>
2019-12-30 22:04:39 +0530

    Separate battery estimate from percent for QS [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4748,18 +4748,29 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
-         * QS Battery mode
+         * QS battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
          * @hide
          */
-        public static final String QS_BATTERY_MODE = "qs_battery_mode";
+        public static final String QS_SHOW_BATTERY_PERCENT = "qs_show_battery_percent";
 
         /**
-          * SStatusbar Battery %
-          * 0: Hide the battery percentage
-          * 1: Display the battery percentage inside the icon
-          * 2: Display the battery percentage next to the icon
-          * @hide
-          */
+         * QS battery estimate
+         * 0: Hide the battery estimate
+         * 1: Display the battery estimate
+         * @hide
+         */
+        public static final String QS_SHOW_BATTERY_ESTIMATE = "qs_show_battery_estimate";
+
+        /**
+         * Statusbar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * @hide
+         */
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
