4e533b45f8d204c22695f6529514668d565460f8
Pranav Vashi <neobuddy89@gmail.com>
2020-01-01 23:30:08 +0530

    Add option to select VoLTE icon [1/2]
    * Inspired by https://github.com/Project-Xtended/frameworks_base/commit/8351ba85af149361d54a70a5d2a5d5a75e494440
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4895,7 +4895,7 @@ public final class Settings {
          * Whether to display VoLTE icon when VoLTE present
          * @hide
          */
-        public static final String SHOW_VOLTE_ICON = "show_volte_icon";
+        public static final String VOLTE_ICON_STYLE = "volte_icon_style";
 
         /**
          * Whether to display roaming indicator for roaming signal connection
