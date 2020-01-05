14c71277b27c162182aa51050720bc7ecaed8e8e
Pranav Vashi <neobuddy89@gmail.com>
2020-01-05 21:43:25 +0530

    Use Emergency Affordance toggle for Emergency Dialer Action as well
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -400,6 +400,9 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             mItems.add(new RestartAction());
         }
 
+        boolean showEmergencyAffordance = Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.POWER_MENU_EMERGENCY_AFFORDANCE, 1) != 0;
+
         ArraySet<String> addedKeys = new ArraySet<String>();
         mHasLogoutButton = false;
         mHasLockdownButton = false;
@@ -470,7 +473,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                     mItems.add(new LogoutAction());
                     mHasLogoutButton = true;
                 }
-            } else if (GLOBAL_ACTION_KEY_EMERGENCY.equals(actionKey)) {
+            } else if (showEmergencyAffordance && GLOBAL_ACTION_KEY_EMERGENCY.equals(actionKey)) {
                 if (!mEmergencyAffordanceManager.needsEmergencyAffordance() && !mIsRestartMenu) {
                     mItems.add(new EmergencyDialerAction());
                 }
@@ -480,9 +483,6 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             // Add here so we don't add more than one.
             addedKeys.add(actionKey);
         }
-
-        boolean showEmergencyAffordance = Settings.Global.getInt(mContext.getContentResolver(),
-                Settings.Global.POWER_MENU_EMERGENCY_AFFORDANCE, 1) != 0;
 
         if (showEmergencyAffordance && mEmergencyAffordanceManager.needsEmergencyAffordance() && !mIsRestartMenu) {
             mItems.add(new EmergencyAffordanceAction());
