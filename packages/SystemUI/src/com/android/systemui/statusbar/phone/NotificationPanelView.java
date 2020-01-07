9b16a68c03490e8de4aebc462dd2aa0a2fe68b8d
00day0 <therandomuser11@gmail.com>
2020-01-07 20:15:27 +0100

    base: Blur behind quick settings [1/2]
    @neobuddy89: Code and logic improvements.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2320,6 +2320,7 @@ public class NotificationPanelView extends PanelView implements
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScroller.setAlpha(alpha);
+        mStatusBar.updateBlurVisibility();
     }
 
     private float getFadeoutAlpha() {
