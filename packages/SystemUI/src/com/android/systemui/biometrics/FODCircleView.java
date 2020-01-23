a649fa300d0e7588a3617cedd1d0608fbe5c80db
TheScarastic <warabhishek@gmail.com>
2020-01-23 14:30:21 -0700

    FODCircleView: Patch screen flickering
    Change-Id: I88b0bea462841b384c172dd9ee8c1ae33b8c7909
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -196,7 +196,8 @@ public class FODCircleView extends ImageView {
         mUpdateMonitor.registerCallback(mMonitorCallback);
 
         mPowerManager = context.getSystemService(PowerManager.class);
-        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "FODCircleView");
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                FODCircleView.class.getSimpleName());
     }
 
     @Override
@@ -205,6 +206,17 @@ public class FODCircleView extends ImageView {
 
         if (mIsCircleShowing) {
             canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (mIsCircleShowing) {
+            dispatchPress();
+        } else {
+            dispatchRelease();
         }
     }
 
@@ -291,9 +303,9 @@ public class FODCircleView extends ImageView {
 
         setKeepScreenOn(true);
 
+        if (mIsDreaming) mWakeLock.acquire(500);
         setDim(true);
         updateAlpha();
-        dispatchPress();
 
         setImageDrawable(null);
         invalidate();
@@ -304,8 +316,6 @@ public class FODCircleView extends ImageView {
 
         setFODIcon();
         invalidate();
-
-        dispatchRelease();
 
         setDim(false);
         updateAlpha();
@@ -438,9 +448,6 @@ public class FODCircleView extends ImageView {
 
     private void updateAlpha() {
         if (mIsCircleShowing) {
-            if (mIsDreaming) {
-                mWakeLock.acquire(300);
-            }
             setAlpha(1.0f);
         } else {
             setAlpha(mIsDreaming ? 0.5f : 1.0f);
