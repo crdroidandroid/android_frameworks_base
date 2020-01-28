bab51467e8253c6a87d588f61eae1838ebdf6483
Marko Man <darkobas@gmail.com>
2020-01-29 00:24:33 +0100

    SystemUI: Pulse light accent color option
    Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5475,6 +5475,12 @@ public final class Settings {
          */
         public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
 
+         /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+        public static final String AMBIENT_NOTIFICATION_LIGHT_ACCENT = "ambient_notification_light_accent";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
