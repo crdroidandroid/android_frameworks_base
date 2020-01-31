4a7748d4be40956b3c3ca475337fe99bb8cd445a
Stallix <team.evolutionx.rom@gmail.com>
2020-01-31 16:00:38 -0700

    FODCircleView: FOD pressed state [1/2]
    * Cyan vs Green war continues
    * Default option set to fod_icon_pressed due to previous FOD impl changes
    * White joins the war (MIUI V11.0.2 Global)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5092,6 +5092,12 @@ public final class Settings {
         public static final String FOD_ICON_WALLPAPER_COLOR = "fod_icon_wallpaper_color";
 
         /**
+         * FOD pressed state
+         * @hide
+         */
+        public static final String FOD_PRESSED_STATE = "fod_pressed_state";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
