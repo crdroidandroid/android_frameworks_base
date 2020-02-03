64a125f31878e89b3dea5a8d00c7f4c5149e4266
rituj <ritujbeniwal@gmail.com>
2020-02-03 09:26:45 +0530

    base: Add vibration patterns from OOS [1/3]
    idoybh edits: Add settings value to backup array
    
    Change-Id: I9622c17a1969598af71bcee573a576b752b96c5e
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6817,6 +6817,18 @@ public final class Settings {
         public static final String DEFAULT_MAX_ALARM_VOLUME = "default_max_alarm_volume";
 
         /**
+         * Which Vibration Pattern to use
+         * 0: dzzz-dzzz
+         * 1: dzzz-da
+         * 2: mm-mm-mm
+         * 3: da-da-dzzz
+         * 4: da-dzzz-da
+         * @hide
+         */
+        @Readable
+        public static final String RINGTONE_VIBRATION_PATTERN = "ringtone_vibration_pattern";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
