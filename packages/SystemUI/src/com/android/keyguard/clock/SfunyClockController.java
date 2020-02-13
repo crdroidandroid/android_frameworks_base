f6f3abe8e7a34846690d1cd34952b68242fe2513
Adhitya Mohan <me@adhityamohan.in>
2020-02-13 17:53:29 +0530

    Add time refresh capabilities for bold, digital and Samsung lockscreen clocks
    -else it causes time to be stuck on ambient display
    
    * Also add it to other clocks
    
    @neobuddy89: Adapted for Android 11
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -166,6 +166,9 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
+        mView.onTimeChanged();
+        mHourClock.refreshTime();
+        mMinuteClock.refreshTime();
     }
 
     @Override
