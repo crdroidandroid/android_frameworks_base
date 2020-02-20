c77e8677c6cd4d7a2f80d868bce9ff9c5d2f969b
DennySPB <dennyspb@gmail.com>
2020-02-20 18:52:57 +0200

    SystemUI: add optional back gesture vibration tick [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5162,6 +5162,9 @@ public final class Settings {
         /** @hide */
         public static final String GESTURE_PILL_TOGGLE = "gesture_pill_toggle";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
