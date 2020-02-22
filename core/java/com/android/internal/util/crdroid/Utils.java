d15dc0428d11ef007c48c71d70b12ece4d792d1e
ezio84 <brabus84@gmail.com>
2020-02-21 22:30:46 -0500

    Add device key action to expand notifications panel [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/Utils.java
+++ core/java/com/android/internal/util/crdroid/Utils.java
@@ -92,6 +92,10 @@ public class Utils {
         FireActions.clearAllNotifications();
     }
 
+    public static void toggleNotifications() {
+        FireActions.toggleNotifications();
+    }
+
     private static final class FireActions {
         private static IStatusBarService mStatusBarService = null;
 
@@ -121,6 +125,17 @@ public class Utils {
             if (service != null) {
                 try {
                     service.onClearAllNotifications(ActivityManager.getCurrentUser());
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
+        }
+
+        public static void toggleNotifications() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.togglePanel();
                 } catch (RemoteException e) {
                     // do nothing.
                 }
