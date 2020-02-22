818f679388e3c01922d112e5769f438f41f41c22
Alex <du.alexcruz@gmail.com>
2020-02-21 22:46:06 -0500

    base: Add more custom actions [1/3]
    - QS panel
    - Ringer modes

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1961,6 +1961,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NOTIFICATIONS:
                 Utils.toggleNotifications();
                 break;
+            case QS_PANEL:
+                Utils.toggleQsPanel();
+                break;
+            case RINGER_MODES:
+                Utils.toggleRingerModes(mContext);
+                break;
             default:
                 break;
         }
