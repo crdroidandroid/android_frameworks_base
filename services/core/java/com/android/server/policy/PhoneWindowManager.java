0d737d9ad5b8b57f33e9542f6a832c6ade7bab1d
ezio84 <brabus84@gmail.com>
2020-02-21 22:30:46 -0500

    base: Add device key action to expand notifications panel [1/3]

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1956,6 +1956,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case CLEAR_ALL_NOTIFICATIONS:
                 Utils.clearAllNotifications();
                 break;
+            case NOTIFICATIONS:
+                Utils.toggleNotifications();
+                break;
             default:
                 break;
         }
