f825d1c62c4a2dbf0a85ae9d1d315ee45a49ac82
Pranav Vashi <neobuddy89@gmail.com>
2020-02-28 00:19:59 +0530

    Alert Slider: Add toggle to disable notifications [1/2]
    Change-Id: If7aca1f3f52b338e74f504652ebf16e26d77a8fc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5721,6 +5721,12 @@ public final class Settings {
         public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
 
         /**
+         * Whether to show or hide alert slider notifications on supported devices
+         * @hide
+         */
+        public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
