cd0b31fe801fea78a1ee09a18117e6d526d6fe2f
Joe Maples <joe@maples.dev>
2020-02-29 16:38:14 -0500

    QS: Add and use Mobile Data panel
    Change-Id: I9f818eea1ab906bb1721e0aab4cbb138187e507b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -17639,6 +17639,18 @@ public final class Settings {
          */
         public static final String ACTION_APP_VOLUME =
                 "android.settings.panel.action.APP_VOLUME";
+
+        /**
+         * Activity Action: Show a settings dialog containing controls for Mobile Data.
+         * <p>
+         * Input: Nothing.
+         * <p>
+         * Output: Nothing.
+         * @hide
+         */
+        @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
+        public static final String ACTION_MOBILE_DATA =
+                "android.settings.panel.action.MOBILE_DATA";
     }
 
     /**
