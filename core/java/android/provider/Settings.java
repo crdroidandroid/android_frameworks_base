6328cdea292d60e625dbc9c5c4e0819e1e2afe6d
SuperDroidBond <superdroidbond@yahoo.com>
2020-03-08 15:09:19 +0530

    SystemUI: KG Charging Animation: Add more animations [1/3]
    Special Thanks to Rodolphe06, Flowdor Theme Dev for the json files
    
    @neobuddy89: Smarter coding. :)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5017,7 +5017,7 @@ public final class Settings {
          * Whether to show charging animation on lock screen
          * @hide
          */
-        public static final String LOCKSCREEN_CHARGING_ANIMATION = "lockscreen_charging_animation";
+        public static final String LOCKSCREEN_CHARGING_ANIMATION_STYLE = "lockscreen_charging_animation_style";
 
         /**
         * List of Apps hidden from recents
