e871af50428615f344cc299271e5f46663f1368c
Lup Gabriel <gwolf2u.website@gmail.com>
2020-03-22 20:21:09 +0200

    Revert "Incall vibration options [1/3]"
    This reverts commit aa56f143d2fda8bd996ea8cfdbaea2d6b76036c4.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5028,24 +5028,6 @@ public final class Settings {
         public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
 
         /**
-         * Whether the phone vibrates on call connect
-         * @hide
-         */
-        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
-
-         /**
-         * Whether the phone vibrates on call waiting
-         * @hide
-         */
-        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
-
-         /**
-         * Whether the phone vibrates on disconnect
-         * @hide
-         */
-        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
-
-        /**
          * Whether to show lock icon on lockscreen
          * @hide
          */
