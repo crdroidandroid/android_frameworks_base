48d1c246e23116f9bd6e45a7f2febe8508d53292
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-04-02 00:19:54 +0300

    Disable QS pulldown on secure lockscreen (1/2)
      Based on commits:
    
      Commit 1
    
      From e5d6c1e3683c1a87b10e7da9323f32c5d0fa3a7e Mon Sep 17 00:00:00 2001
      From: Marko Man <darkobas@gmail.com>
      Date: Sat, 5 Oct 2019 22:44:48 +0200
      Subject: [PATCH] base: allow disabling quick settings on secure lock screens
    
      inspired by
      SlimRoms/frameworks_base@b76f8af
      DirtyUnicorns/android_frameworks_base@3a4d3be
    
      xyyx: adapted to Oreo
    
      Change-Id: I90736961fd8023b06b544e9a96a43be4202707e4
      Signed-off-by: enzoo96 <enzoo.site@gmail.com>
    
      Commit 2
    
      From 1bb7a657b58f3490e5cb02a20525a3a29107bb97 Mon Sep 17 00:00:00 2001
      From: maxwen <max.weninger@gmail.com>
      Date: Tue, 8 Oct 2019 16:18:45 +0200
      Subject: [PATCH] base: fix disabling quick settings on secure lock screens
    
      Change-Id: I50a75c3ddd32423dda1e71ba1d508d8ad6de4bc6
    
      Commit 3
    
      From 143c478174ce407522cf07f772cc86478a0bb39f Mon Sep 17 00:00:00 2001
      From: Marko Man <darkobas@gmail.com>
      Date: Wed, 9 Oct 2019 10:06:25 +0200
      Subject: [PATCH] SystemUI: Fix disable QS pulldown on secure lockscreens
    
      Change-Id: Ide95a72ff7b271476a8b3f8fbf163db8b7187b01
    
      Commit 4
    
      From 3c6f934d0ad57477e95501c0aae9b78d039da1c4 Mon Sep 17 00:00:00 2001
      From: maxwen <max.weninger@gmail.com>
      Date: Sat, 9 Nov 2019 23:53:10 +0100
      Subject: [PATCH] SystemUI: block open doors for disabled secure lock qs expand
    
      Change-Id: I4a4084e5fe8fb2a0989a367f2b4234a9ef60b7f7
    
      Commit 5
    
      From 6dbfaaec7b9a48dd8eda97d4a95f14b3bfc6b32f Mon Sep 17 00:00:00 2001
      From: Pranav Vashi <neobuddy89@gmail.com>
      Date: Sun, 31 Mar 2019 21:05:30 +0530
      Subject: [PATCH] Fix issues with QS access on secured lockscreen
    
      * This should fix issue where QS was still accessible via emergency screen.
    
    @neobuddy89: Adapted for Android 11.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5123,6 +5123,12 @@ public final class Settings {
         public static final String LOCKSCREEN_LOCK_ICON = "lockscreen_lock_icon";
 
         /**
+         * Whether to show quick settings on secure LockScreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_QS = "lockscreen_enable_qs";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
