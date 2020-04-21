cc5b62a0366cf541b85d974f96563d4674da39dc
Pranav Vashi <neobuddy89@gmail.com>
2020-04-21 20:17:41 +0530

    Adaptive Playback: Convert to seconds
    * Why ms?
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -575,7 +575,7 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
             if (adaptivePlaybackEnabled) {
                 int adaptivePlaybackTimeout = Settings.System.getIntForUser(
                         mContext.getContentResolver(), Settings.System.ADAPTIVE_PLAYBACK_TIMEOUT,
-                        30000, UserHandle.USER_CURRENT);
+                        30, UserHandle.USER_CURRENT);
                 mAudio.dispatchMediaKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,
                         KeyEvent.KEYCODE_MEDIA_PAUSE));
                 mAudio.dispatchMediaKeyEvent(new KeyEvent(KeyEvent.ACTION_UP,
@@ -584,7 +584,7 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
                 mMediaStateHandler.removeCallbacksAndMessages(null);
                 mMediaStateHandler.postDelayed(() -> {
                     isResumable = false;
-                }, adaptivePlaybackTimeout);
+                }, adaptivePlaybackTimeout * 1000);
             }
         }
         if (stream == AudioSystem.STREAM_MUSIC && level > 0 && isResumable) {
