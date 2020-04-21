e3de8af23c60ac096555937037a80be69923e65f
Pranav Vashi <neobuddy89@gmail.com>
2020-04-21 20:17:41 +0530

    Adaptive Playback: Convert to seconds
    * Why ms?
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5236,7 +5236,7 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
 
         /**
-         * Adaptive playback's timeout in ms
+         * Adaptive playback's timeout in seconds
          * @hide
          */
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
