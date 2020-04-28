554991bdf9e65abdf2ea9dd10edd0e4660ef1d6e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-28 13:27:25 -0300

    KeyguardUpdateMonitor: Fix race condition before listening for biometrics
    * Fixes popup camera opening after dismissing incoming calls
    
    Change-Id: I10b1d0ce3d1c94128aab9fa7b66e0d9e753b843d

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -286,6 +286,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
     private static final int HAL_ERROR_RETRY_TIMEOUT = 500; // ms
     private static final int HAL_ERROR_RETRY_MAX = 10;
 
+    private boolean mKeyguardReset = false;
+
     private PocketManager mPocketManager;
     private boolean mIsDeviceInPocket;
     private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
@@ -1808,6 +1810,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
      * If face auth is allows to scan on this exact moment.
      */
     public boolean shouldListenForFace() {
+        if (mFaceAuthOnlyOnSecurityView && mKeyguardReset){
+            mKeyguardReset = false;
+            return false;
+        }
+
         boolean awakeKeyguard = mKeyguardIsVisible && mDeviceInteractive && !mGoingToSleep;
         final int user = getCurrentUser();
         final int strongAuth = mStrongAuthTracker.getStrongAuthForUser(user);
@@ -2315,6 +2322,9 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         if (DEBUG) Log.d(TAG, "handleKeyguardReset");
         updateBiometricListeningState();
         mNeedsSlowUnlockTransition = resolveNeedsSlowUnlockTransition();
+        if (mFaceAuthOnlyOnSecurityView){
+            mKeyguardReset = true;
+        }
     }
 
     private boolean resolveNeedsSlowUnlockTransition() {
