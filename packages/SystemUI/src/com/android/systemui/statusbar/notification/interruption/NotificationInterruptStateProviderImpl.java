c7b35dfec02458b2d5e557dea71c35b266a313cd
rituj <ritujbeniwal@gmail.com>
2020-04-30 19:13:56 +0530

    base: Introduce Applock [1/3]
    * Includes applock 1.1 and more
    * Built upon the initial commit by "Anass Karbila <anaskarbila@aospa.co>"
    
    Change-Id: Ida21d22035d708a359eaaa29e2c48ff6f0dd6627
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
@@ -141,6 +141,10 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
 
     @Override
     public boolean shouldBubbleUp(NotificationEntry entry) {
+        if (entry.isAppLocked()) {
+            return false;
+        }
+
         final StatusBarNotification sbn = entry.getSbn();
 
         if (!canAlertCommon(entry)) {
@@ -193,6 +197,11 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
     }
 
     private boolean shouldHeadsUpWhenAwake(NotificationEntry entry) {
+        if (mStatusBarStateController.getState() != StatusBarState.KEYGUARD
+                && entry.secureContent()) {
+            return false;
+        }
+
         StatusBarNotification sbn = entry.getSbn();
 
         if (shouldSkipHeadsUp(sbn)) {
