edc05192ecaf239265c5b662fd0c4e52bd3903b9
rituj <ritujbeniwal@gmail.com>
2020-04-30 19:13:56 +0530

    base: Introduce Applock [1/3]
    * Includes applock 1.1 and more
    * Built upon the initial commit by "Anass Karbila <anaskarbila@aospa.co>"
    
    Change-Id: Ida21d22035d708a359eaaa29e2c48ff6f0dd6627
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -927,6 +927,15 @@ public final class SystemServiceRegistry {
                     }
                 });
 
+        registerService(Context.APPLOCK_SERVICE, AppLockManager.class,
+                new CachedServiceFetcher<AppLockManager>() {
+            @Override
+            public AppLockManager createService(ContextImpl ctx) throws ServiceNotFoundException {
+                IBinder b = ServiceManager.getServiceOrThrow(Context.APPLOCK_SERVICE);
+                IAppLockService service = IAppLockService.Stub.asInterface(b);
+                return new AppLockManager(service);
+            }});
+
         registerService(Context.POCKET_SERVICE, PocketManager.class,
                 new CachedServiceFetcher<PocketManager>() {
                     @Override
