949a62465da4ca77668a35a5d3e8926bdc8fcd72
rohanpurohit <purohit.rohan@gmail.com>
2020-05-03 18:10:07 +0300

    base: Add device key action to toggle volume panel [1/3]

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1950,6 +1950,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case SCREENSHOT:
                 takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN);
                 break;
+            case VOLUME_PANEL:
+                Utils.toggleVolumePanel(mContext);
+                break;
             default:
                 break;
         }
