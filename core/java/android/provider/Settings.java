40086995ab360dfdf417d332294a19b57911b3e4
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-06-08 21:35:58 +0300

    Make sensor block package list configurable [1/2]
    Based on https://github.com/crdroidandroid/android_frameworks_base/commit/2f0ff544cee1de199856958405e08ad074ee443e
    
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6279,6 +6279,17 @@ public final class Settings {
         public static final String SENSOR_BLOCK = "sensor_block";
 
         /**
+         * Sensor blocked packages
+         * @hide
+         */
+        @Readable
+        public static final String SENSOR_BLOCKED_APP = "sensor_blocked_app";
+
+         /** @hide */
+        @Readable
+        public static final String SENSOR_BLOCKED_APP_DUMMY = "sensor_blocked_app_dummy";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
