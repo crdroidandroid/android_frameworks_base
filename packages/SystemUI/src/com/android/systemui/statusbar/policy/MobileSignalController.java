0d675817bf84cbc47418a29b51002746e9756df9
DennySPb <dennyspb@gmail.com>
2020-06-23 14:34:56 +0300

    SystemUI: Add more VoWiFi icon styles [1/2]
    - OnePlus
    - Asus
    - Huawei
    - Motorola
    
    @neobuddy89: Make implementation similar to VoLTE - 0 = disabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -109,6 +109,7 @@ public class MobileSignalController extends SignalController<
     private boolean mRoamingIconAllowed;
     private boolean mShow4gForLte;
     private boolean mDataDisabledIcon;
+    private int mVoWIFIicon = 0;
 
     public static final String VOLTE_ICON_STYLE =
             "system:" + Settings.System.VOLTE_ICON_STYLE;
@@ -120,6 +121,8 @@ public class MobileSignalController extends SignalController<
             "system:" + Settings.System.DATA_DISABLED_ICON;
     public static final String USE_OLD_MOBILETYPE =
             "system:" + Settings.System.USE_OLD_MOBILETYPE;
+    public static final String VOWIFI_ICON_STYLE =
+            "system:" + Settings.System.VOWIFI_ICON_STYLE;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -192,6 +195,7 @@ public class MobileSignalController extends SignalController<
         Dependency.get(TunerService.class).addTunable(this, SHOW_FOURG_ICON);
         Dependency.get(TunerService.class).addTunable(this, DATA_DISABLED_ICON);
         Dependency.get(TunerService.class).addTunable(this, USE_OLD_MOBILETYPE);
+        Dependency.get(TunerService.class).addTunable(this, VOWIFI_ICON_STYLE);
     }
 
     @Override
@@ -218,6 +222,11 @@ public class MobileSignalController extends SignalController<
                 updateTelephony();                
                 break; 
             case USE_OLD_MOBILETYPE:
+                notifyListeners();
+                break;
+            case VOWIFI_ICON_STYLE:
+                mVoWIFIicon =
+                    TunerService.parseInteger(newValue, 0);
                 notifyListeners();
                 break;
             default:
@@ -855,10 +864,28 @@ public class MobileSignalController extends SignalController<
     }
 
     private MobileIconGroup getVowifiIconGroup() {
+        if (mVoWIFIicon == 0) return null;
+
         if ( isVowifiAvailable() && !isCallIdle() ) {
             return TelephonyIcons.VOWIFI_CALLING;
         } else if (isVowifiAvailable()) {
-            return TelephonyIcons.VOWIFI;
+            switch (mVoWIFIicon) {
+                case 1:
+                default:
+                    return TelephonyIcons.VOWIFI;
+                // OOS
+                case 2:
+                    return TelephonyIcons.VOWIFI_ONEPLUS;
+                // Motorola
+                case 3:
+                    return TelephonyIcons.VOWIFI_MOTO;
+                // ASUS
+                case 4:
+                    return TelephonyIcons.VOWIFI_ASUS;
+                // EMUI (Huawei P10)
+                case 5:
+                    return TelephonyIcons.VOWIFI_EMUI;
+            }
         } else {
             return null;
         }
