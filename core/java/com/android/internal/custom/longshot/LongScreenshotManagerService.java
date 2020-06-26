neobuddy89/frameworks_base

base: Port extended screenshot function from OOS
Change-Id: I856597056cb56d479a7a2c47dfdfc199da4302b8

core/java/com/android/internal/longshot/LongScreenshotManagerService.java
@@ -1,0 +1,195 @@
+package com.android.internal.longshot;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongScreenshotManagerService extends ILongScreenshotManager.Stub {
+    public static final String PACKAGENAME_LONGSHOT = "org.pixelexperience.screenshot";
+    public static final ComponentName TAKE_SCREENSHOT_COMPONENT = new ComponentName(PACKAGENAME_LONGSHOT, PACKAGENAME_LONGSHOT + ".TakeScreenshotService");
+    private static final ComponentName COMPONENT_LONGSHOT = new ComponentName(PACKAGENAME_LONGSHOT, PACKAGENAME_LONGSHOT + ".LongshotService");
+    private static final String TAG = "Longshot.ManagerService";
+    private static LongScreenshotManagerService sInstance = null;
+    public Context mContext = null;
+    private LongshotConnection mLongshot = new LongshotConnection();
+
+    private class LongshotConnection extends ILongScreenshotCallback.Stub implements ServiceConnection {
+        private List<ILongScreenshotListener> mListeners;
+        public ILongScreenshot mService;
+
+        private LongshotConnection() {
+            mService = null;
+            mListeners = new ArrayList();
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = ILongScreenshot.Stub.asInterface(service);
+            try {
+                mService.start(this);
+            } catch (NullPointerException ignored) {
+            } catch (RemoteException e) {
+                Log.w(TAG, "Remote exception in onServiceConnected: ", e);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            stop();
+        }
+
+        @Override
+        public void stop() {
+            mContext.unbindService(this);
+            mService = null;
+            try {
+                WindowManagerGlobal.getWindowManagerService().stopLongshotConnection();
+            } catch (RemoteException e) {
+                Log.w(TAG, "Remote exception in stop: ", e);
+            }
+        }
+
+        @Override
+        public void notifyMove() {
+            synchronized (mListeners) {
+                for (ILongScreenshotListener listener : mListeners) {
+                    try {
+                        listener.onMove();
+                    } catch (RemoteException e) {
+                        Log.w(TAG, "Remote exception in notifyMove: ", e);
+                    }
+                }
+            }
+        }
+
+        public void registerListener(ILongScreenshotListener listener) {
+            synchronized (mListeners) {
+                mListeners.add(listener);
+            }
+        }
+
+        public void unregisterListener(ILongScreenshotListener listener) {
+            synchronized (mListeners) {
+                mListeners.remove(listener);
+            }
+        }
+    }
+
+    private LongScreenshotManagerService(Context context) {
+        mContext = context;
+    }
+
+    public static LongScreenshotManagerService getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new LongScreenshotManagerService(context);
+        }
+        return sInstance;
+    }
+
+    @Override
+    public void takeLongshot(boolean statusBarVisible, boolean navBarVisible) {
+        stopLongshot();
+        bindService(createLongshotIntent(statusBarVisible, navBarVisible), mLongshot, 1);
+    }
+
+    @Override
+    public void registerLongshotListener(ILongScreenshotListener listener) {
+        mLongshot.registerListener(listener);
+    }
+
+    @Override
+    public void unregisterLongshotListener(ILongScreenshotListener listener) {
+        mLongshot.unregisterListener(listener);
+    }
+
+    @Override
+    public void notifyLongshotScroll(boolean isOverScroll) {
+        try {
+            mLongshot.mService.notifyScroll(isOverScroll);
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in notifyLongshotScroll: ", e);
+        }
+    }
+
+    @Override
+    public boolean isLongshotMoveState() {
+        try {
+            return mLongshot.mService.isMoveState();
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in isLongshotMoveState: ", e);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isLongshotHandleState() {
+        try {
+            return mLongshot.mService.isHandleState();
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in isHandleState: ", e);
+        }
+        return false;
+    }
+
+    @Override
+    public void notifyScrollViewTop(int viewTop) {
+        try {
+            mLongshot.mService.notifyScrollViewTop(viewTop);
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in notifyScrollViewTop: ", e);
+        }
+    }
+
+    @Override
+    public void onUnscrollableView() {
+        try {
+            mLongshot.mService.onUnscrollableView();
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in onUnscrollableView: ", e);
+        }
+    }
+
+    @Override
+    public boolean isLongshotMode() {
+        return mLongshot.mService != null;
+    }
+
+    @Override
+    public void stopLongshot() {
+        if (mLongshot.mService != null) {
+            try {
+                mLongshot.mService.stopLongshot();
+            } catch (RemoteException e) {
+                Log.w(TAG, "Remote exception in stopLongshot: ", e);
+            }
+        }
+    }
+
+    private Intent createIntent(ComponentName component) {
+        return new Intent().setComponent(component);
+    }
+
+    private Intent createLongshotIntent(boolean statusBarVisible, boolean navBarVisible) {
+        return createIntent(COMPONENT_LONGSHOT).putExtra(LongScreenshotManager.STATUSBAR_VISIBLE, statusBarVisible).putExtra(LongScreenshotManager.NAVIGATIONBAR_VISIBLE, navBarVisible);
+    }
+
+    private boolean bindService(Intent service, ServiceConnection conn, int flags) {
+        if (service != null && conn != null) {
+            return mContext.bindServiceAsUser(service, conn, flags, UserHandle.CURRENT);
+        }
+        return false;
+    }
+}

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -306,6 +306,8 @@ import java.util.List;
 
 import dalvik.system.PathClassLoader;
 
+import com.android.internal.longshot.ILongScreenshotManager;
+
 /**
  * WindowManagerPolicy implementation for the Android phone UI.  This
  * introduces a new method suffix, Lp, for an internal lock of the
@@ -772,6 +774,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mSystemNavigationKeysEnabled;
 
     Display mDisplay;
+    protected int mDisplayRotation;
 
     int mLandscapeRotation = 0;  // default landscape rotation
     int mSeascapeRotation = 0;   // "other" landscape rotation, 180 degrees from mLandscapeRotation
@@ -1907,10 +1910,29 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         public void run() {
+            boolean longshot;
+            boolean inMultiWindow = mFocusedWindow != null ? mFocusedWindow.isInMultiWindowMode() : false;
+            boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
+            if (mScreenshotType == 2 || keyguardOn() || !isUserSetupComplete() ||
+                    !isDeviceProvisioned() || ((inMultiWindow && !dockMinimized) || mDisplayRotation != 0)) {
+                longshot = false;
+            } else {
+                longshot = true;
+            }
+            Bundle screenshotBundle = new Bundle();
+            screenshotBundle.putBoolean("longshot", longshot);
+            if (mFocusedWindow != null) {
+                screenshotBundle.putString("focusWindow", mFocusedWindow.getAttrs().packageName);
+            }
+            if (mFocusedWindow != null &&
+                (mFocusedWindow.getAttrs().flags & WindowManager.LayoutParams.FLAG_SECURE) != 0){
+                    mScreenshotHelper.notifyScreenshotCaptureError();
+                    return;
+            }
             if (!mPocketLockShowing){
                 mScreenshotHelper.takeScreenshot(mScreenshotType,
                         mStatusBar != null && mStatusBar.isVisibleLw(),
-                        mNavigationBar != null && mNavigationBar.isVisibleLw(), mHandler);
+                        mNavigationBar != null && mNavigationBar.isVisibleLw(), mHandler, longshot, screenshotBundle);
             }
         }
     }
@@ -1929,11 +1951,38 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         final boolean keyguardShowing = isKeyguardShowingAndNotOccluded();
         mGlobalActions.showDialog(keyguardShowing, isDeviceProvisioned());
+        stopLongshot();
         if (keyguardShowing) {
             // since it took two seconds of long press to bring this up,
             // poke the wake lock so they have some time to see the dialog.
             mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
         }
+    }
+
+    private void stopLongshot() {
+        ILongScreenshotManager shot = ILongScreenshotManager.Stub.asInterface(ServiceManager.getService(Context.LONGSCREENSHOT_SERVICE));
+        if (shot != null) {
+            try {
+                if (shot.isLongshotMode()) {
+                    shot.stopLongshot();
+                }
+            } catch (RemoteException e) {
+                Slog.d(TAG, e.toString());
+            }
+        }
+    }
+
+    @Override
+    public void stopLongshotConnection() {
+        if (mScreenshotHelper != null) {
+            mScreenshotHelper.stopLongshotConnection();
+        }
+    }
+
+    @Override
+    public void takeOPScreenshot(int type, int reason) {
+        mScreenshotRunnable.setScreenshotType(type);
+        mHandler.post(mScreenshotRunnable);
     }
 
     boolean isDeviceProvisioned() {
@@ -4868,6 +4917,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     @Override
     public void beginLayoutLw(DisplayFrames displayFrames, int uiMode) {
         displayFrames.onBeginLayout();
+        mDisplayRotation = displayFrames.mRotation;
         // TODO(multi-display): This doesn't seem right...Maybe only apply to default display?
         mSystemGestures.screenWidth = displayFrames.mUnrestricted.width();
         mSystemGestures.screenHeight = displayFrames.mUnrestricted.height();

crdroidandroid/android_frameworks_base

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/custom/longshot/LongScreenshotManagerService.java
@@ -1,0 +1,195 @@
+package com.android.internal.custom.longshot;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongScreenshotManagerService extends ILongScreenshotManager.Stub {
+    public static final String PACKAGENAME_LONGSHOT = "org.mokee.screenshot";
+    public static final ComponentName TAKE_SCREENSHOT_COMPONENT = new ComponentName(PACKAGENAME_LONGSHOT, PACKAGENAME_LONGSHOT + ".TakeScreenshotService");
+    private static final ComponentName COMPONENT_LONGSHOT = new ComponentName(PACKAGENAME_LONGSHOT, PACKAGENAME_LONGSHOT + ".LongshotService");
+    private static final String TAG = "Longshot.ManagerService";
+    private static LongScreenshotManagerService sInstance = null;
+    public Context mContext = null;
+    private LongshotConnection mLongshot = new LongshotConnection();
+
+    private class LongshotConnection extends ILongScreenshotCallback.Stub implements ServiceConnection {
+        private List<ILongScreenshotListener> mListeners;
+        public ILongScreenshot mService;
+
+        private LongshotConnection() {
+            mService = null;
+            mListeners = new ArrayList();
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = ILongScreenshot.Stub.asInterface(service);
+            try {
+                mService.start(this);
+            } catch (NullPointerException ignored) {
+            } catch (RemoteException e) {
+                Log.w(TAG, "Remote exception in onServiceConnected: ", e);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            stop();
+        }
+
+        @Override
+        public void stop() {
+            mContext.unbindService(this);
+            mService = null;
+            try {
+                WindowManagerGlobal.getWindowManagerService().stopLongshotConnection();
+            } catch (RemoteException e) {
+                Log.w(TAG, "Remote exception in stop: ", e);
+            }
+        }
+
+        @Override
+        public void notifyMove() {
+            synchronized (mListeners) {
+                for (ILongScreenshotListener listener : mListeners) {
+                    try {
+                        listener.onMove();
+                    } catch (RemoteException e) {
+                        Log.w(TAG, "Remote exception in notifyMove: ", e);
+                    }
+                }
+            }
+        }
+
+        public void registerListener(ILongScreenshotListener listener) {
+            synchronized (mListeners) {
+                mListeners.add(listener);
+            }
+        }
+
+        public void unregisterListener(ILongScreenshotListener listener) {
+            synchronized (mListeners) {
+                mListeners.remove(listener);
+            }
+        }
+    }
+
+    private LongScreenshotManagerService(Context context) {
+        mContext = context;
+    }
+
+    public static LongScreenshotManagerService getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new LongScreenshotManagerService(context);
+        }
+        return sInstance;
+    }
+
+    @Override
+    public void takeLongshot(boolean statusBarVisible, boolean navBarVisible) {
+        stopLongshot();
+        bindService(createLongshotIntent(statusBarVisible, navBarVisible), mLongshot, 1);
+    }
+
+    @Override
+    public void registerLongshotListener(ILongScreenshotListener listener) {
+        mLongshot.registerListener(listener);
+    }
+
+    @Override
+    public void unregisterLongshotListener(ILongScreenshotListener listener) {
+        mLongshot.unregisterListener(listener);
+    }
+
+    @Override
+    public void notifyLongshotScroll(boolean isOverScroll) {
+        try {
+            mLongshot.mService.notifyScroll(isOverScroll);
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in notifyLongshotScroll: ", e);
+        }
+    }
+
+    @Override
+    public boolean isLongshotMoveState() {
+        try {
+            return mLongshot.mService.isMoveState();
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in isLongshotMoveState: ", e);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isLongshotHandleState() {
+        try {
+            return mLongshot.mService.isHandleState();
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in isHandleState: ", e);
+        }
+        return false;
+    }
+
+    @Override
+    public void notifyScrollViewTop(int viewTop) {
+        try {
+            mLongshot.mService.notifyScrollViewTop(viewTop);
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in notifyScrollViewTop: ", e);
+        }
+    }
+
+    @Override
+    public void onUnscrollableView() {
+        try {
+            mLongshot.mService.onUnscrollableView();
+        } catch (NullPointerException ignored) {
+        } catch (RemoteException e) {
+            Log.w(TAG, "Remote exception in onUnscrollableView: ", e);
+        }
+    }
+
+    @Override
+    public boolean isLongshotMode() {
+        return mLongshot.mService != null;
+    }
+
+    @Override
+    public void stopLongshot() {
+        if (mLongshot.mService != null) {
+            try {
+                mLongshot.mService.stopLongshot();
+            } catch (RemoteException e) {
+                Log.w(TAG, "Remote exception in stopLongshot: ", e);
+            }
+        }
+    }
+
+    private Intent createIntent(ComponentName component) {
+        return new Intent().setComponent(component);
+    }
+
+    private Intent createLongshotIntent(boolean statusBarVisible, boolean navBarVisible) {
+        return createIntent(COMPONENT_LONGSHOT).putExtra(LongScreenshotManager.STATUSBAR_VISIBLE, statusBarVisible).putExtra(LongScreenshotManager.NAVIGATIONBAR_VISIBLE, navBarVisible);
+    }
+
+    private boolean bindService(Intent service, ServiceConnection conn, int flags) {
+        if (service != null && conn != null) {
+            return mContext.bindServiceAsUser(service, conn, flags, UserHandle.CURRENT);
+        }
+        return false;
+    }
+}

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -213,6 +213,7 @@ import android.widget.Toast;
 
 import com.android.internal.R;
 import com.android.internal.accessibility.AccessibilityShortcutController;
+import com.android.internal.custom.longshot.ILongScreenshotManager;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.os.DeviceKeyHandler;
@@ -1741,14 +1742,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         @Override
         public void run() {
             if(!mPocketLockShowing) {
-                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType);
+                boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
+                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
             }
         }
     }
 
     private final ScreenshotRunnable mScreenshotRunnable = new ScreenshotRunnable();
 
-    private void takeScreenshot(int screenshotType) {
+    @Override
+    public void takeScreenshot(int screenshotType) {
         takeScreenshotDelayed(screenshotType, 0);
     }
 
@@ -1782,6 +1785,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     void showGlobalActionsInternal() {
+        stopLongshot();
         if (mGlobalActions == null) {
             mGlobalActions = new GlobalActions(mContext, mWindowManagerFuncs);
         }
@@ -1790,6 +1794,24 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // since it took two seconds of long press to bring this up,
         // poke the wake lock so they have some time to see the dialog.
         mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
+    }
+
+    private void stopLongshot() {
+        ILongScreenshotManager shot = ILongScreenshotManager.Stub.asInterface(ServiceManager.getService(Context.LONGSCREENSHOT_SERVICE));
+        if (shot != null) {
+            try {
+                if (shot.isLongshotMode()) {
+                    shot.stopLongshot();
+                }
+            } catch (RemoteException e) {
+                Slog.d(TAG, e.toString());
+            }
+        }
+    }
+
+    @Override
+    public void stopLongshotConnection() {
+        mDefaultDisplayPolicy.stopLongshotConnection();
     }
 
     boolean isDeviceProvisioned() {