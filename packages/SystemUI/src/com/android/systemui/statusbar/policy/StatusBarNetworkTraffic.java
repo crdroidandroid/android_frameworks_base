980ed8748f609da3f3dc0a29ea5102365d1172b8
Gobinda Joy <gobinda.joy@gmail.com>
2020-06-27 22:15:38 +0600

    NetworkTraffic: Improvements and cleanup
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/StatusBarNetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/StatusBarNetworkTraffic.java
@@ -79,7 +79,7 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements DarkRecei
 
     @Override
     public boolean isIconVisible() {
-        return mLocation == 1;
+        return mLocation == LOCATION_STATUSBAR;
     }
 
     @Override
@@ -104,7 +104,6 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements DarkRecei
                 mSystemIconVisible = false;
                 break;
         }
-        updateVisibility();
     }
 
     @Override
