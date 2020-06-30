c4c025a8e81b90d81d1362c7365158ab947286b5
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-06-30 12:33:28 +0300

    Add FOD system overlay types

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -1111,6 +1111,18 @@ public interface WindowManager extends ViewManager {
         public static final int TYPE_APPLICATION_OVERLAY = FIRST_SYSTEM_WINDOW + 38;
 
         /**
+         * Window type: Fingerprint overlay
+         * @hide
+         */
+        public static final int TYPE_SYSTEM_FINGERPRINT = FIRST_SYSTEM_WINDOW + 39;
+
+        /**
+         * Window type: Fingerprint high light overlay
+         * @hide
+         */
+        public static final int TYPE_SYSTEM_FINGERPRINT_HIGH_LIGHT = FIRST_SYSTEM_WINDOW + 40;
+
+        /**
          * End of types of system windows.
          */
         public static final int LAST_SYSTEM_WINDOW      = 2999;
