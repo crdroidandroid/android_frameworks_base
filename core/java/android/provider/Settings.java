91cbacffc96be603f5863653ff63a4c163f4020b
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-07-03 18:03:39 +0300

    A2DP codec priority [1/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5304,6 +5304,12 @@ public final class Settings {
         public static final String BERRY_SWITCH_STYLE = "berry_switch_style";
 
         /**
+         * A2DP codec priority
+         * @hide
+         */
+        public static final String A2DP_SOURCE_CODEC_PRIORITY = "a2dp_source_codec_priority";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
