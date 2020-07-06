dd32ccb318f02533cd2a82c5ff3f7cdc57c35d1a
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-07-06 01:35:10 +0000

    SystemUI: Use app icons for notifications in statusbar
    Squashed:
    
        From: spkal01 <spkal01@users.noreply.github.com>
        Date: Thu, 9 Dec 2021 18:32:21 +0000
        Subject: SystemUI: Make app icons in statusbar optional [1/2]
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sat, 15 Jan 2022 17:09:09 +0530
        Subject: SystemUI: Refactor colored statusbar icons
    
        * Remove painful systemui checks everywhere in StatusBariconView
        * Apply NO_COLOR when colored icons are enabled.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [neobuddy89: Updated for A14 QPR1]
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -22,6 +22,8 @@ import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_WIFI
 import android.annotation.Nullable;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
 import android.view.View;
@@ -357,6 +359,8 @@ public interface StatusBarIconController {
 
         protected ArrayList<String> mBlockList = new ArrayList<>();
 
+        private final boolean mNewIconStyle;
+
         public IconManager(
                 ViewGroup group,
                 StatusBarLocation location,
@@ -368,6 +372,9 @@ public interface StatusBarIconController {
             mMobileContextProvider = mobileContextProvider;
             mContext = group.getContext();
             mLocation = location;
+
+            mNewIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
 
             reloadDimens();
 
@@ -441,6 +448,7 @@ public interface StatusBarIconController {
         protected StatusBarIconView addIcon(int index, String slot, boolean blocked,
                 StatusBarIcon icon) {
             StatusBarIconView view = onCreateStatusBarIconView(slot, blocked);
+            view.setIconStyle(mNewIconStyle);
             view.set(icon);
             mGroup.addView(view, index, onCreateLayoutParams());
             return view;
