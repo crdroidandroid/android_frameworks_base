5d8c9f143a7f71072f3321759cd41ce995478221
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-07-06 01:35:10 +0000

    SystemUI: Use app icons for notifications in statusbar
    Squashed:
    
        From: spkal01 <spkal01@users.noreply.github.com>
        Date: Thu, 9 Dec 2021 18:32:21 +0000
        Subject: SystemUI: Make app icons in statusbar optional [1/2]
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sat, 15 Jan 2022 17:09:09 +0530
        Subject: SystemUI: Refactor colored statusbar icons
    
        * Remove painful systemui checks everywhere in StatusBariconView
        * Apply NO_COLOR when colored icons are enabled.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [neobuddy89: Updated for A14 QPR1, A15, A15 QPR2]
    
    Change-Id: I90cde2d2ae2fdf228ee167e9b24d2316e97a61a7
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
@@ -26,6 +26,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Icon;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Property;
 import android.view.ContextThemeWrapper;
@@ -294,7 +296,7 @@ public class NotificationIconContainer extends ViewGroup {
         mIconSize = size;
     }
 
-    private void updateState() {
+    public void updateState() {
         resetViewStates();
         calculateIconXTranslations();
         applyIconStates();
@@ -832,11 +834,15 @@ public class NotificationIconContainer extends ViewGroup {
                     }
                 }
                 icon.setVisibleState(visibleState, animationsAllowed);
-                if (mOverrideIconColor) {
-                    int overrideIconColor = mUseInverseOverrideIconColor
-                            ? mThemedTextColorPrimaryInverse : mThemedTextColorPrimary;
-                    icon.setIconColor(overrideIconColor,
-                            /* animate= */ needsCannedAnimation && animationsAllowed);
+                boolean newIconStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                            Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
+                if (icon.getStatusBarIcon().pkg.contains("systemui") || !newIconStyle) {
+                    if (mOverrideIconColor) {
+                        int overrideIconColor = mUseInverseOverrideIconColor
+                                ? mThemedTextColorPrimaryInverse : mThemedTextColorPrimary;
+                        icon.setIconColor(overrideIconColor,
+                                /* animate= */ needsCannedAnimation && animationsAllowed);
+                    }
                 }
                 if (animate) {
                     animateTo(icon, animationProperties);
@@ -879,7 +885,14 @@ public class NotificationIconContainer extends ViewGroup {
         public void initFrom(View view) {
             super.initFrom(view);
             if (view instanceof StatusBarIconView) {
-                iconColor = ((StatusBarIconView) view).getStaticDrawableColor();
+                StatusBarIconView icon = (StatusBarIconView) view;
+                boolean newIconStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                            Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
+                if (icon.getStatusBarIcon().pkg.contains("systemui") || !newIconStyle) {
+                    iconColor = ((StatusBarIconView) view).getStaticDrawableColor();
+                } else {
+                    iconColor = StatusBarIconView.NO_COLOR;
+                }
             }
         }
     }
