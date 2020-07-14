9c96a305409b781f89f4219946c54a0bddc39018
Varun Date <date.varun123@gmail.com>
2020-07-14 18:51:34 +1000

    NetworkTraffic: Fix visibility on overlayChange()
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconList.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconList.java
@@ -51,7 +51,7 @@ public class StatusBarIconList {
         }
         // Auto insert new items behind network traffic.
         mSlots.add(1, new Slot(slot, null));
-        return 0;
+        return 1;
     }
 
     protected ArrayList<Slot> getSlots() {
