10e417907e51f80c86eebd801c9abaf7f276ea29
Pranav Vashi <neobuddy89@gmail.com>
2020-07-31 02:33:19 +0530

    base: Nuke Visualizer for lockscreen [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -73,7 +73,6 @@ import com.android.systemui.statusbar.phone.ShadeController;
 import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.statusbar.phone.StatusBarWindowController;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
-import com.android.systemui.statusbar.VisualizerView;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.tuner.TunerService;
 
@@ -540,14 +539,7 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
                     break;
                 }
             }
-
-            if (mStatusBar.getVisualizer() != null) {
-                mStatusBar.getVisualizer().setPlaying(true);
-            }
         } else {
-            if (mStatusBar.getVisualizer() != null) {
-                mStatusBar.getVisualizer().setPlaying(false);
-            }
             mStatusBar.resetTrackInfo();
         }
     }
@@ -666,20 +658,6 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
         mColorExtractor.setHasMediaArtwork(hasMediaArtwork);
         if (mScrimController != null) {
             mScrimController.setHasBackdrop(hasArtwork);
-        }
-
-        if (mStatusBar != null && mStatusBar.getVisualizer() != null) {
-            if (artworkDrawable instanceof BitmapDrawable) {
-                // always use current backdrop to color eq
-                mStatusBar.getVisualizer().setBitmap(((BitmapDrawable) artworkDrawable).getBitmap());
-            } else if (lockWallpaper instanceof Bitmap) {
-                // use lockscreen wallpaper in case user set one
-                mStatusBar.getVisualizer().setBitmap(lockWallpaper.getConfig() == Bitmap.Config.HARDWARE ?
-                        lockWallpaper.copy(Bitmap.Config.ARGB_8888, false) : lockWallpaper);
-            } else {
-                // use regular wallpaper
-                mStatusBar.getVisualizer().setBitmap(mWallpaperManager.getBitmap(false));
-            }
         }
 
         if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
