debb713443888da1ee87308f67857ee5a4f39cda
AndroBuddy <anirudhchimpidi8@gmail.com>
2020-08-15 22:38:44 +0530

    Introduce Fluid lockscreen clock
    AshutoshSundresh:
    -Move string to controller instead of xml as it isn't translatable anyways
    -Port to A11
    -Prevent it from getting stuck on Ambient Display
    
    Change-Id: Ie4212fe580154dcd7e5234f42aaf32a6845d5d9b
    Signed-off-by: AndroBuddy <anirudhchimpidi8@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ packages/SystemUI/src/com/android/keyguard/clock/FluidClockController.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2020 ProjectFluid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.keyguard.clock;
+
+import android.app.WallpaperManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint.Style;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextClock;
+
+import com.android.internal.colorextraction.ColorExtractor;
+import com.android.systemui.R;
+import com.android.systemui.colorextraction.SysuiColorExtractor;
+import com.android.systemui.plugins.ClockPlugin;
+
+import java.util.TimeZone;
+
+import static com.android.systemui.statusbar.phone
+        .KeyguardClockPositionAlgorithm.CLOCK_USE_DEFAULT_Y;
+
+/**
+ * Plugin for the default clock face used only to provide a preview.
+ */
+public class FluidClockController implements ClockPlugin {
+
+    /**
+     * Resources used to get title and thumbnail.
+     */
+    private final Resources mResources;
+
+    /**
+     * LayoutInflater used to inflate custom clock views.
+     */
+    private final LayoutInflater mLayoutInflater;
+
+    /**
+     * Extracts accent color from wallpaper.
+     */
+    private final SysuiColorExtractor mColorExtractor;
+
+    /**
+     * Renders preview from clock view.
+     */
+    private final ViewPreviewer mRenderer = new ViewPreviewer();
+
+    /**
+     * Root view of clock.
+     */
+    private ClockLayout mView;
+
+    /**
+     * Text clock in preview view hierarchy.
+     */
+    private TextClock mTimeClock;
+    private TextClock mSecondsClock;
+    private TextClock mDay;
+    private TextClock mDate;
+    private TextClock mYear;
+
+    /**
+     * Create a DefaultClockController instance.
+     *
+     * @param res Resources contains title and thumbnail.
+     * @param inflater Inflater used to inflate custom clock views.
+     * @param colorExtractor Extracts accent color from wallpaper.
+     */
+    public FluidClockController(Resources res, LayoutInflater inflater,
+            SysuiColorExtractor colorExtractor) {
+        mResources = res;
+        mLayoutInflater = inflater;
+        mColorExtractor = colorExtractor;
+    }
+
+    private void createViews() {
+        mView = (ClockLayout) mLayoutInflater
+                .inflate(R.layout.digital_clock_fluid, null);
+        mTimeClock = mView.findViewById(R.id.time_clock);
+        mSecondsClock = mView.findViewById(R.id.seconds_clock);
+        mDay = mView.findViewById(R.id.clock_day);
+        mDate = mView.findViewById(R.id.clock_date);
+        mYear = mView.findViewById(R.id.clock_year);
+    }
+
+    @Override
+    public void onDestroyView() {
+        mView = null;
+        mTimeClock = null;
+        mSecondsClock = null;
+        mDay = null;
+        mDate = null;
+        mYear = null;
+    }
+
+    @Override
+    public String getName() {
+        return "fluid";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Fluid";
+    }
+
+    @Override
+    public Bitmap getThumbnail() {
+        return BitmapFactory.decodeResource(mResources, R.drawable.fluid_thumbnail);
+    }
+
+    @Override
+    public Bitmap getPreview(int width, int height) {
+
+        View previewView = mLayoutInflater.inflate(R.layout.digital_fluid_preview, null);
+        TextClock previewTime = previewView.findViewById(R.id.time_clock);
+        TextClock previewSeconds = previewView.findViewById(R.id.seconds_clock);
+        TextClock previewDay = previewView.findViewById(R.id.clock_day);
+        TextClock previewDate = previewView.findViewById(R.id.clock_date);
+        TextClock previewYear = previewView.findViewById(R.id.clock_year);
+
+        // Initialize state of plugin before generating preview.
+        previewTime.setTextColor(Color.WHITE);
+        previewDay.setTextColor(Color.WHITE);
+        previewYear.setTextColor(Color.WHITE);
+        ColorExtractor.GradientColors colors = mColorExtractor.getColors(
+                WallpaperManager.FLAG_LOCK);
+        int[] colorPalette = colors.getColorPalette();
+        int accentColor = mResources.getColor(R.color.typeClockAccentColor, null);
+        if (colorPalette != null) {
+            accentColor = colorPalette[Math.max(0, colorPalette.length - 5)];
+        }
+        previewSeconds.setTextColor(accentColor);
+        previewDate.setTextColor(accentColor);
+        onTimeTick();
+
+        return mRenderer.createPreview(previewView, width, height);
+    }
+
+    @Override
+    public View getView() {
+        if (mView == null) {
+            createViews();
+        }
+        return mView;
+    }
+
+    @Override
+    public View getBigClockView() {
+        return null;
+    }
+
+    @Override
+    public int getPreferredY(int totalHeight) {
+        return CLOCK_USE_DEFAULT_Y;
+    }
+
+    @Override
+    public void setStyle(Style style) {}
+
+    @Override
+    public void setTextColor(int color) {
+        mTimeClock.setTextColor(color);
+        mDay.setTextColor(color);
+        mYear.setTextColor(color);
+    }
+
+    @Override
+    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {
+        if (colorPalette == null || colorPalette.length == 0) {
+            return;
+        }
+        final int accentColor = colorPalette[Math.max(0, colorPalette.length - 5)];
+        mSecondsClock.setTextColor(accentColor);
+        mDate.setTextColor(accentColor);
+    }
+
+    @Override
+    public void onTimeTick() {
+        mView.onTimeChanged();
+        mTimeClock.refreshTime();
+        mSecondsClock.refreshTime();
+        mDay.refreshTime();
+        mDate.refreshTime();
+        mYear.refreshTime();
+    }
+
+    @Override
+    public void setDarkAmount(float darkAmount) {
+        mView.setDarkAmount(darkAmount);
+    }
+
+    @Override
+    public void onTimeZoneChanged(TimeZone timeZone) {}
+
+    @Override
+    public boolean shouldShowStatusArea() {
+        return false;
+    }
+}
