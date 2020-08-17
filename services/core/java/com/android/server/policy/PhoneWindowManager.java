e7d4e82ed5801758389827b6637254409ae49948
Pranav Vashi <neobuddy89@gmail.com>
2020-08-17 21:44:31 +0530

    Long screenshot: Use helper function takeScreenshot
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6910,8 +6910,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public void takeAlternativeScreenshot() {
-        mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
-        mHandler.post(mScreenshotRunnable);
+        takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN);
     }
-
 }
