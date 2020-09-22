f838831dd91ed750a7df1e4a3a0cc3e2e81dc4a7
lucaslin <lucaslin@google.com>
2020-09-22 20:52:27 +0800

    Add synchronized to make sure data correctness
    Both mNetworkAgent and mConfig are mutable, add synchronized to
    make sure the data correctness of those two vars when getNetId()
    and getVpnConfig() are called.
    
    Bug: 168576152
    Test: atest FrameworkNetTests
          atest CtsHostsideNetworkTests:HostsideVpnTests
    Change-Id: I4133de84c904009b5fbf4208853773fc79fdd17f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -1129,7 +1129,13 @@ public class Vpn {
         return mNetworkInfo;
     }
 
-    public int getNetId() {
+    /**
+     * Return netId of current running VPN network.
+     *
+     * @return a netId if there is a running VPN network or NETID_UNSET if there is no running VPN
+     *         network or network is null.
+     */
+    public synchronized int getNetId() {
         final NetworkAgent agent = mNetworkAgent;
         if (null == agent) return NETID_UNSET;
         final Network network = agent.getNetwork();
@@ -1707,7 +1713,7 @@ public class Vpn {
     /**
      * Return the configuration of the currently running VPN.
      */
-    public VpnConfig getVpnConfig() {
+    public synchronized VpnConfig getVpnConfig() {
         enforceControlPermission();
         return mConfig;
     }
