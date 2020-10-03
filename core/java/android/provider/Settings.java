f9cbe78b6684724fc95f651e9e7bf4c2772aaccc
jhenrique09 <jhenrique09@users.noreply.github.com>
2020-10-03 14:44:59 +0000

    fwb: Implement cutout force full screen [1/2]
    Inspired by MIUI and Essential
    
    CutoutFullscreenController: Adapted from https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    
    Change-Id: I37a6c0a29e7a5fbd9bded530f5de947cce5c7c25
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7395,6 +7395,12 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Force full screen for devices with cutout
+         * @hide
+         */
+        public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -9789,4 +9789,8 @@ final class ActivityRecord extends WindowToken {
     public boolean shouldForceLongScreen() {
         return mAtmService.shouldForceLongScreen(packageName);
     }
+
+    public boolean shouldForceCutoutFullscreen() {
+        return mAtmService.shouldForceCutoutFullscreen(packageName);
+    }
 }
