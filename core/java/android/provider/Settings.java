3d1cc60797c365a56e6331edb5105971f300a19e
jhonboy121 <alfredmathew05@gmail.com>
2020-10-04 12:49:28 -0700

    Allow changing the length of gesture navbar [1/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7008,6 +7008,13 @@ public final class Settings {
         public static final String LOCKSCREEN_SHOW_CARRIER = "lockscreen_show_carrier";
 
         /**
+         * Gesture navbar length mode.
+         * Supported modes: 0 for short length, 1 for normal and 2 for long.
+         * @hide
+         */
+        public static final String GESTURE_NAVBAR_LENGTH_MODE = "gesture_navbar_length_mode";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
