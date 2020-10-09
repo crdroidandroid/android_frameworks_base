9977b8252caecd8763cbdf7542697aaeb047ac74
00day0 <therandomuser11@gmail.com>
2020-10-09 23:34:40 +0200

    SystemUI: Introduce OOS style notification clear all button [1/2]
    @neobuddy89:
    * Adapt with 13 monet colors.
    * Polished icons from material design icons.
    * Remove unneccessary cruft.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
@@ -579,4 +579,10 @@ public interface CentralSurfaces extends Dumpable, ActivityStarter, LifecycleOwn
             mDeviceId = deviceId;
         }
     }
+
+    View getDismissAllButton();
+
+    void updateDismissAllVisibility(boolean visible);
+
+    void updateDismissAllButton();
 }
