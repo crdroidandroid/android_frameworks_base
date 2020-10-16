39384819ea13576995e905279bf4f7087f77e188
Pranav Vashi <neobuddy89@gmail.com>
2020-10-17 00:15:53 +0530

    base: Disable stock accent overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -17,16 +17,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := frameworks-base-overlays
 LOCAL_REQUIRED_MODULES := \
-	AccentColorBlackOverlay \
-	AccentColorCinnamonOverlay \
-	AccentColorOceanOverlay \
-	AccentColorOrchidOverlay \
-	AccentColorSpaceOverlay \
-	AccentColorGreenOverlay \
-	AccentColorPurpleOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
-        DisplayCutoutEmulationHoleOverlay \
+    DisplayCutoutEmulationHoleOverlay \
 	DisplayCutoutEmulationTallOverlay \
 	DisplayCutoutEmulationWaterfallOverlay \
 	FontNotoSerifSourceOverlay \
