619595efd0dd1d104c51e0e307ccb8a23799155f
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-10-24 22:45:11 +0300

    Make Quick Unlock compatible with long PIN/Password [1/2]
    Idea from OnePlus Oxygen OS.
    
    * Use AsyncTask for PIN/Password check.
    * Restore call on main thread check.
    
    @neobuddy89: Adapted for Android 12
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -423,6 +423,7 @@ public class LockPatternUtils {
     @NonNull
     public VerifyCredentialResponse verifyCredential(@NonNull LockscreenCredential credential,
             int userId, @VerifyFlag int flags) {
+        throwIfCalledOnMainThread();
         try {
             final VerifyCredentialResponse response = getLockSettings().verifyCredential(
                     credential, userId, flags);
@@ -480,6 +481,7 @@ public class LockPatternUtils {
     public boolean checkCredential(@NonNull LockscreenCredential credential, int userId,
             @Nullable CheckCredentialProgressCallback progressCallback)
             throws RequestThrottledException {
+        throwIfCalledOnMainThread();
         try {
             VerifyCredentialResponse response = getLockSettings().checkCredential(
                     credential, userId, wrapCallback(progressCallback));
@@ -513,6 +515,7 @@ public class LockPatternUtils {
     @NonNull
     public VerifyCredentialResponse verifyTiedProfileChallenge(
             @NonNull LockscreenCredential credential, int userId, @VerifyFlag int flags) {
+        throwIfCalledOnMainThread();
         try {
             final VerifyCredentialResponse response = getLockSettings()
                     .verifyTiedProfileChallenge(credential, userId, flags);
@@ -1225,6 +1228,12 @@ public class LockPatternUtils {
         if (isDeviceEncryptionEnabled()){
             Settings.Global.putInt(mContext.getContentResolver(),
                Settings.Global.REQUIRE_PASSWORD_TO_DECRYPT, required ? 1 : 0);
+        }
+    }
+
+    private void throwIfCalledOnMainThread() {
+        if (Looper.getMainLooper().isCurrentThread()) {
+            throw new IllegalStateException("should not be called from the main thread.");
         }
     }
 
