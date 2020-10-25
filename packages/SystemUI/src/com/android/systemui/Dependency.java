237bb48098d34dda4644ccb593add7f4539a9a44
bigrushdog <randall.rushing@gmail.com>
2020-10-24 17:48:33 -0700

    Pulse initial checkin for Android 12 [1/2]
    @neobuddy89:
    * This includes few of NPE fixes and improvements.
    * Android 12 removed colored notifications in ff165814e10cf757981d1e0254bd88473dff126e,
      so retire pulse color based on album art till we find other way.
    
    ----
    ezio:
    
    big squash here to handle Android 11 changes,
    check external pulse tree to know who contributed
    to this epic feature that Randall made back in the days
    (yeah, we're getting old) :D
    
    some mentions: neobuddy89, Alex Cruz, LorDClockaN, varund7726
    
    ps: now when the the new qs media player notification is enabled,
    a different call to media processor is done by the mediadatamanager
    in packages/SystemUI/src/com/android/systemui/media/, that's why the
    need of MediaDataManager.Listener in NotificationMediaManager.
    reminder for myself:  we can acces properties of kotlin "data" classes
    like MediaData.kt just calling a built-in getter (like getBackgroundColor
    for the backgroundColor prop). For reference, it's also possible to call
    straihgt mediaData.background color but only after adding the @JvmField
    flag to the prop declaration
    ----
    
    Change-Id: I0205f8ba6b8883012d1b809de9a33f6391c4cf0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -120,6 +120,7 @@ import com.android.systemui.statusbar.policy.HotspotController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.LocationController;
 import com.android.systemui.statusbar.policy.NextAlarmController;
+import com.android.systemui.statusbar.policy.PulseController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
 import com.android.systemui.statusbar.policy.RotationLockController;
 import com.android.systemui.statusbar.policy.SecurityController;
@@ -370,6 +371,7 @@ public class Dependency {
     @Inject Lazy<AmbientState> mAmbientStateLazy;
     @Inject Lazy<GroupMembershipManager> mGroupMembershipManagerLazy;
     @Inject Lazy<GroupExpansionManager> mGroupExpansionManagerLazy;
+    @Inject Lazy<PulseController> mPulseController;
 
     @Inject
     public Dependency() {
@@ -590,6 +592,7 @@ public class Dependency {
         mProviders.put(AmbientState.class, mAmbientStateLazy::get);
         mProviders.put(GroupMembershipManager.class, mGroupMembershipManagerLazy::get);
         mProviders.put(GroupExpansionManager.class, mGroupExpansionManagerLazy::get);
+        mProviders.put(PulseController.class, mPulseController::get);
 
         Dependency.setInstance(this);
     }
