6e0f8b4cf425e9822b83f9ab2e60793ac2e6e139
bigrushdog <randall.rushing@gmail.com>
2020-10-24 17:48:33 -0700

    Pulse initial checkin for Android 11 [1/2]
    ----
    ezio:
    
    big squash here to handle Android 11 changes,
    check external pulse tree to know who contributed
    to this epic feature that Randall made back in the days
    (yeah, we're getting old) :D
    
    some mentions: neobuddy89, Alex Cruz, LorDClockaN, varund7726
    
    ps: now when the the new qs media player notification is enabled,
    a different call to media processor is done by the mediadatamanager
    in packages/SystemUI/src/com/android/systemui/media/, that's why the
    need of MediaDataManager.Listener in NotificationMediaManager.
    reminder for myself:  we can acces properties of kotlin "data" classes
    like MediaData.kt just calling a built-in getter (like getBackgroundColor
    for the backgroundColor prop). For reference, it's also possible to call
    straihgt mediaData.background color but only after adding the @JvmField
    flag to the prop declaration
    ----
    
    Change-Id: I0205f8ba6b8883012d1b809de9a33f6391c4cf0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -109,6 +109,7 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.LocationController;
 import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NextAlarmController;
+import com.android.systemui.statusbar.policy.PulseController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
 import com.android.systemui.statusbar.policy.RotationLockController;
 import com.android.systemui.statusbar.policy.SecurityController;
@@ -326,6 +327,7 @@ public class Dependency {
     @Inject Lazy<ProtoTracer> mProtoTracer;
     @Inject Lazy<Divider> mDivider;
     @Inject Lazy<MediaOutputDialogFactory> mMediaOutputDialogFactory;
+    @Inject Lazy<PulseController> mPulseController;
 
     @Inject
     public Dependency() {
@@ -525,6 +527,7 @@ public class Dependency {
         mProviders.put(Divider.class, mDivider::get);
 
         mProviders.put(MediaOutputDialogFactory.class, mMediaOutputDialogFactory::get);
+        mProviders.put(PulseController.class, mPulseController::get);
 
         sDependency = this;
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -58,6 +58,7 @@ import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.media.MediaData;
 import com.android.systemui.media.MediaDataManager;
+import com.android.systemui.media.MediaFeatureFlag;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.dagger.StatusBarModule;
 import com.android.systemui.statusbar.notification.NotificationEntryListener;
@@ -94,7 +95,7 @@ import dagger.Lazy;
  * Handles tasks and state related to media notifications. For example, there is a 'current' media
  * notification, which this class keeps track of.
  */
-public class NotificationMediaManager implements Dumpable, TunerService.Tunable {
+public class NotificationMediaManager implements Dumpable, TunerService.Tunable, MediaDataManager.Listener {
     private static final String TAG = "NotificationMediaManager";
     public static final boolean DEBUG_MEDIA = false;
 
@@ -120,6 +121,7 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
 
     private final NotificationEntryManager mEntryManager;
     private final MediaDataManager mMediaDataManager;
+    private final boolean mIsMediaInQS;
 
     @Nullable
     private Lazy<NotificationShadeWindowController> mNotificationShadeWindowController;
@@ -209,7 +211,8 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             KeyguardBypassController keyguardBypassController,
             @Main DelayableExecutor mainExecutor,
             DeviceConfigProxy deviceConfig,
-            MediaDataManager mediaDataManager) {
+            MediaDataManager mediaDataManager,
+            MediaFeatureFlag mediaFeatureFlag) {
         mContext = context;
         mMediaArtworkProcessor = mediaArtworkProcessor;
         mKeyguardBypassController = keyguardBypassController;
@@ -224,6 +227,8 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
         mEntryManager = notificationEntryManager;
         mMainExecutor = mainExecutor;
         mMediaDataManager = mediaDataManager;
+        mMediaDataManager.addListener(this);
+        mIsMediaInQS = mediaFeatureFlag.getEnabled();
 
         notificationEntryManager.addNotificationEntryListener(new NotificationEntryListener() {
 
@@ -245,6 +250,9 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             @Override
             public void onEntryReinflated(NotificationEntry entry) {
                 findAndUpdateMediaNotifications();
+                if (!mIsMediaInQS) {
+                    checkMediaNotificationColor(entry);
+                }
             }
 
             @Override
@@ -254,6 +262,17 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
                     boolean removedByUser,
                     int reason) {
                 removeEntry(entry);
+            }
+
+            // these get called from NotificationEntryManager.onAsyncInflationFinished
+            // so we are sure the final media notification albumart and colors elaboration
+            // has been completed by the system
+            @Override
+            public void onNotificationAdded(
+                    NotificationEntry entry) {
+                if (!mIsMediaInQS) {
+                    checkMediaNotificationColor(entry);
+                }
             }
         });
 
@@ -339,6 +358,37 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             clearCurrentMediaNotification();
             dispatchUpdateMediaMetaData(true /* changed */, true /* allowEnterAnimation */);
         }
+    }
+
+    private void checkMediaNotificationColor(NotificationEntry entry) {
+        if (entry.getSbn().getKey().equals(mMediaNotificationKey)) {
+            ArrayList<MediaListener> callbacks = new ArrayList<>(mMediaListeners);
+            for (int i = 0; i < callbacks.size(); i++) {
+                callbacks.get(i).setMediaNotificationColor(
+                        entry.getSbn().getNotification().isColorizedMedia(),
+                        entry.getRow().getCurrentBackgroundTint());
+            }
+        }
+    }
+
+    @Override
+    public void onMediaDataLoaded(String key, String oldKey, MediaData data) {
+        /* for future reference, now this static call is also available:
+        MediaDataManagerKt.isMediaNotification(sbn)*/
+        // TODO: mIsMediaInQS check should be useless here, if so we can remove it
+        if (mIsMediaInQS && key.equals(mMediaNotificationKey)) {
+            ArrayList<MediaListener> callbacks = new ArrayList<>(mMediaListeners);
+            for (int i = 0; i < callbacks.size(); i++) {
+                callbacks.get(i).setMediaNotificationColor(
+                        true/*colorized*/,
+                        data.getBackgroundColor());
+            }
+        }
+    }
+
+    @Override
+    public void onMediaDataRemoved(String key) {
+        //
     }
 
     public String getMediaNotificationKey() {
@@ -853,5 +903,7 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
          */
         default void onPrimaryMetadataOrStateChanged(MediaMetadata metadata,
                 @PlaybackState.State int state) {}
+
+        default void setMediaNotificationColor(boolean colorizedMedia, int color) {};
     }
 }
