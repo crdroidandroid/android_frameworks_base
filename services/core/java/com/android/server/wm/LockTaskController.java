d7993c32a92bbed356d540dec05bd7912c03a7f3
bigrushdog <randall.rushing@gmail.com>
2020-10-24 17:48:33 -0700

    Pulse initial checkin for Android 11 [1/2]
    ----
    ezio:
    
    big squash here to handle Android 11 changes,
    check external pulse tree to know who contributed
    to this epic feature that Randall made back in the days
    (yeah, we're getting old) :D
    
    some mentions: neobuddy89, Alex Cruz, LorDClockaN, varund7726
    
    ps: now when the the new qs media player notification is enabled,
    a different call to media processor is done by the mediadatamanager
    in packages/SystemUI/src/com/android/systemui/media/, that's why the
    need of MediaDataManager.Listener in NotificationMediaManager.
    reminder for myself:  we can acces properties of kotlin "data" classes
    like MediaData.kt just calling a built-in getter (like getBackgroundColor
    for the backgroundColor prop). For reference, it's also possible to call
    straihgt mediaData.background color but only after adding the @JvmField
    flag to the prop declaration
    ----
    
    Change-Id: I0205f8ba6b8883012d1b809de9a33f6391c4cf0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/LockTaskController.java
+++ services/core/java/com/android/server/wm/LockTaskController.java
@@ -521,6 +521,7 @@ public class LockTaskController {
                 getStatusBarService().showPinningEnterExitToast(false /* entering */);
             }
             mWindowManager.onLockTaskStateChanged(mLockTaskModeState);
+            getStatusBarService().screenPinningStateChanged(false);
         } catch (RemoteException ex) {
             throw new RuntimeException(ex);
         }
@@ -642,6 +643,7 @@ public class LockTaskController {
             if (getDevicePolicyManager() != null) {
                 getDevicePolicyManager().notifyLockTaskModeChanged(true, packageName, userId);
             }
+            getStatusBarService().screenPinningStateChanged(true);
         } catch (RemoteException ex) {
             throw new RuntimeException(ex);
         }
