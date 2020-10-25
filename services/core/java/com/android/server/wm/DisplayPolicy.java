aa3a455060ba6426165ac0c440af3aeaccd26952
bigrushdog <randall.rushing@gmail.com>
2020-10-24 17:48:33 -0700

    Pulse initial checkin for Android 13 [1/2]
    @neobuddy89:
    * Adapted for A13 - removed dagger dependencies, PulseController interfaces, unused listeners.
    * Pulses disabled by default - following AOSP / LOS behavior.
    * This includes few of NPE fixes and improvements.
    * Android 12 removed colored notifications in ff165814e10cf757981d1e0254bd88473dff126e,
      so retire pulse color based on album art till we find other way.
    
    ----
    ezio:
    
    big squash here to handle Android 11 changes,
    check external pulse tree to know who contributed
    to this epic feature that Randall made back in the days
    (yeah, we're getting old) :D
    
    some mentions: neobuddy89, Alex Cruz, LorDClockaN, varund7726
    
    ps: now when the the new qs media player notification is enabled,
    a different call to media processor is done by the mediadatamanager
    in packages/SystemUI/src/com/android/systemui/media/, that's why the
    need of MediaDataManager.Listener in NotificationMediaManager.
    reminder for myself:  we can acces properties of kotlin "data" classes
    like MediaData.kt just calling a built-in getter (like getBackgroundColor
    for the backgroundColor prop). For reference, it's also possible to call
    straihgt mediaData.background color but only after adding the @JvmField
    flag to the prop declaration
    
    Change-Id: I0205f8ba6b8883012d1b809de9a33f6391c4cf0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -1711,7 +1711,13 @@ public class DisplayPolicy {
             // Keep mNavigationBarPosition updated to make sure the transient detection and bar
             // color control is working correctly.
             final DisplayFrames displayFrames = mDisplayContent.mDisplayFrames;
+            final int lastNavbarPosition = mNavigationBarPosition;
             mNavigationBarPosition = navigationBarPosition(displayFrames.mRotation);
+            if (lastNavbarPosition == NAV_BAR_LEFT && mNavigationBarPosition != NAV_BAR_LEFT) {
+                notifyLeftInLandscapeChanged(false);
+            } else if (lastNavbarPosition != NAV_BAR_LEFT && mNavigationBarPosition == NAV_BAR_LEFT) {
+                notifyLeftInLandscapeChanged(true);
+            }
         }
         final boolean affectsSystemUi = win.canAffectSystemUiFlags();
         if (DEBUG_LAYOUT) Slog.i(TAG, "Win " + win + ": affectsSystemUi=" + affectsSystemUi);
@@ -2240,6 +2246,15 @@ public class DisplayPolicy {
 
     DecorInsets.Info getDecorInsetsInfo(int rotation, int w, int h) {
         return mDecorInsets.get(rotation, w, h);
+    }
+
+    private void notifyLeftInLandscapeChanged(boolean isOnLeft) {
+        mHandler.post(() -> {
+            StatusBarManagerInternal statusBar = getStatusBarManagerInternal();
+            if (statusBar != null) {
+                statusBar.leftInLandscapeChanged(isOnLeft);
+            }
+        });
     }
 
     @NavigationBarPosition
