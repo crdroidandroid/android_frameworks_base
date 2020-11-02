0b7cc67a4268d6eb4d72b14be5f692cc36b1a5ba
Joey Huab <joey@evolution-x.org>
2020-11-02 23:01:22 +0900

    SystemUI: Add remaining FOD animations
    Includes:
    
    * Add ASUS ROG FOD animations
    * Add MIUI Aurora FOD animation
    * Add Mi 10 Ultra FOD animations
    * Add OP Energy FOD animation from OnePlus 8 Pro
    * Add OP Ripple animations
    * FOD Animations: convert to webp icons
    * Update OOS FOD animations
    
    Co-authored-by: Raaj52 <mail2raaj52@gmail.com>
    Co-authored-by: Alexandru Scurtu <sasha.scurtu@gmail.com>
    Co-authored-by: Anushek <anushekprasal@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
@@ -44,19 +44,27 @@ public class FODAnimation extends ImageView {
     private final int[] ANIMATION_STYLES = {
         R.drawable.fod_miui_normal_recognizing_anim,
         R.drawable.fod_miui_aod_recognizing_anim,
+        R.drawable.fod_miui_aurora_recognizing_anim,
+        R.drawable.fod_miui_aurora_cas_recognizing_anim,
         R.drawable.fod_miui_light_recognizing_anim,
         R.drawable.fod_miui_pop_recognizing_anim,
         R.drawable.fod_miui_pulse_recognizing_anim,
         R.drawable.fod_miui_pulse_recognizing_white_anim,
         R.drawable.fod_miui_rhythm_recognizing_anim,
+        R.drawable.fod_miui_star_cas_recognizing_anim,
         R.drawable.fod_op_cosmos_recognizing_anim,
+        R.drawable.fod_op_energy_recognizing_anim,
         R.drawable.fod_op_mclaren_recognizing_anim,
+        R.drawable.fod_op_ripple_recognizing_anim,
         R.drawable.fod_op_stripe_recognizing_anim,
         R.drawable.fod_op_wave_recognizing_anim,
         R.drawable.fod_pureview_dna_recognizing_anim,
         R.drawable.fod_pureview_future_recognizing_anim,
         R.drawable.fod_pureview_halo_ring_recognizing_anim,
         R.drawable.fod_pureview_molecular_recognizing_anim,
+        R.drawable.fod_rog_fusion_recognizing_anim,
+        R.drawable.fod_rog_pulsar_recognizing_anim,
+        R.drawable.fod_rog_supernova_recognizing_anim,
     };
 
     public FODAnimation(Context context, int mPositionX, int mPositionY) {
