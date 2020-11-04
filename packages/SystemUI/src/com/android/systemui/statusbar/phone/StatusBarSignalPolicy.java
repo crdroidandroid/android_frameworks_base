5a198d675f1317662ac3ec0963ff79c3e310103e
SagarMakhar <sagarmakhar@gmail.com>
2020-11-04 04:53:38 +0000

    SystemUI: Update mobile data icon instantly when toggled
    Change-Id: Ic3cb0749b778d9caf2493757ac3580a743e53c19
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -18,6 +18,7 @@ package com.android.systemui.statusbar.phone;
 
 import android.content.Context;
 import android.os.Handler;
+import android.provider.Settings;
 import android.telephony.SubscriptionInfo;
 import android.util.ArraySet;
 import android.util.Log;
@@ -48,6 +49,9 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
         SecurityController.SecurityControllerCallback, Tunable {
     private static final String TAG = "StatusBarSignalPolicy";
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    private static final String USE_OLD_MOBILETYPE =
+            "system:" + Settings.System.USE_OLD_MOBILETYPE;
 
     private final String mSlotAirplane;
     private final String mSlotMobile;
@@ -113,8 +117,7 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
                 mContext.getString(com.android.internal.R.string.status_bar_call_strength);
         mActivityEnabled = mContext.getResources().getBoolean(R.bool.config_showActivity);
 
-
-        tunerService.addTunable(this, StatusBarIconController.ICON_HIDE_LIST);
+        tunerService.addTunable(this, StatusBarIconController.ICON_HIDE_LIST, USE_OLD_MOBILETYPE);
         mNetworkController.addCallback(this);
         mSecurityController.addCallback(this);
     }
@@ -148,6 +151,10 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
 
     @Override
     public void onTuningChanged(String key, String newValue) {
+        if (USE_OLD_MOBILETYPE.equals(key)) {
+            mNetworkController.removeCallback(this);
+            mNetworkController.addCallback(this);
+        }
         if (!StatusBarIconController.ICON_HIDE_LIST.equals(key)) {
             return;
         }
