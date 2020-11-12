b76506c4cb240883696f194489d67d2c09599d53
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-12 14:30:29 +0000

    Feature: reticker [1/2]
    [someone5678]
    * Based on crDroid Android's cleaned A12 commit[1]
    * Forward port to Android 13
    * Follow the Material You styles
    
    Original commit:
    https://github.com/Descendant-XI/frameworks_base/commit/6ca0cff0da94d07a14ca8695b7bde5eaea9deef5
    
    Ref:
    [1] https://github.com/crdroidandroid/android_frameworks_base/commit/e44ede735e177d8297cfbfa8b22a21041c229ace
    
    Change-Id: I582d0484977f0e850d158e8645171e638fcc0c4c
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -196,12 +196,13 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
             boolean animateIsolation = false;
             if (newEntry == null) {
                 // no heads up anymore, lets start the disappear animation
-
+                mNotificationPanelViewController.reTickerView(false);
                 setShown(false);
                 animateIsolation = !isExpanded();
             } else if (previousEntry == null) {
                 // We now have a headsUp and didn't have one before. Let's start the disappear
                 // animation
+                mNotificationPanelViewController.reTickerView(true);
                 setShown(true);
                 animateIsolation = !isExpanded();
             }
