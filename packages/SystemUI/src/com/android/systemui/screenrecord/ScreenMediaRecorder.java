3b9c162306ace7c453e76215d23e5986a5ef7605
ezio84 <brabus84@gmail.com>
2020-11-14 22:23:23 +0100

    Screenrecord: add blinking stop dot and low quality options
    We could want to stop the recording without expanding the notification
    panel and show sensitive info.
    Long pressing on the red dot moves it to the opposite side,
    in case the user wants not to cover specific elements on the screen.
    
    Add also a low quality option that still gives a good recording
    reducing a lot the final file size (useful when sharing the screenrecord
    on data connections).
    
    old commits ref:
    https://github.com/ezio84/abc_frameworks_base/commit/6092d9ca8b0949d64c1b87d6cd53d66daf002e14
    https://github.com/ezio84/abc_frameworks_base/commit/b50c7caf8b46c731093b11f5647028a23346f937
    https://github.com/ezio84/abc_frameworks_base/commit/91c00e8fb70cb17b8658678bf476452f96cd44d1
    
    Change-Id: If413241c3fa533650690cf3b7df5c05fb2f8c8ed
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -62,6 +62,8 @@ public class ScreenMediaRecorder {
     private static final int TOTAL_NUM_TRACKS = 1;
     private static final int VIDEO_FRAME_RATE = 30;
     private static final int VIDEO_FRAME_RATE_TO_RESOLUTION_RATIO = 6;
+    private static final int LOW_VIDEO_FRAME_RATE = 25;
+    private static final int LOW_VIDEO_BIT_RATE = 1750000;
     private static final int AUDIO_BIT_RATE = 196000;
     private static final int AUDIO_SAMPLE_RATE = 44100;
     private static final int MAX_DURATION_MS = 60 * 60 * 1000;
@@ -80,6 +82,8 @@ public class ScreenMediaRecorder {
     private ScreenInternalAudioRecorder mAudio;
     private ScreenRecordingAudioSource mAudioSource;
 
+    private boolean mLowQuality;
+
     private Context mContext;
     MediaRecorder.OnInfoListener mListener;
 
@@ -90,6 +94,10 @@ public class ScreenMediaRecorder {
         mUser = user;
         mListener = listener;
         mAudioSource = audioSource;
+    }
+
+    public void setLowQuality(boolean low) {
+        mLowQuality = low;
     }
 
     private void prepare() throws IOException, RemoteException {
@@ -126,9 +134,13 @@ public class ScreenMediaRecorder {
         wm.getDefaultDisplay().getRealMetrics(metrics);
         int screenWidth = metrics.widthPixels;
         int screenHeight = metrics.heightPixels;
-        int refereshRate = (int) wm.getDefaultDisplay().getRefreshRate();
-        int vidBitRate = screenHeight * screenWidth * refereshRate / VIDEO_FRAME_RATE
+        int refereshRate = mLowQuality? LOW_VIDEO_FRAME_RATE : (int) wm.getDefaultDisplay().getRefreshRate();
+        // TODO: make low quality bitrate scalable per device, like the default one
+        int vidBitRate = mLowQuality ? LOW_VIDEO_BIT_RATE :
+                screenHeight * screenWidth * refereshRate / VIDEO_FRAME_RATE
                 * VIDEO_FRAME_RATE_TO_RESOLUTION_RATIO;
+        /* PS: HEVC can be set too, to reduce file size without quality loss (h265 is more efficient than h264),
+        but at the same time the cpu load is 8-10 times higher and some devices don't support it yet */
         mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
         mMediaRecorder.setVideoEncodingProfileLevel(
                 MediaCodecInfo.CodecProfileLevel.AVCProfileMain,
