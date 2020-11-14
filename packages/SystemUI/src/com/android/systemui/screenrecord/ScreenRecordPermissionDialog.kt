3a97ee7d70107e4dccaeb9442182ea0194eb754e
ezio84 <brabus84@gmail.com>
2020-11-14 22:23:23 +0100

    Screenrecord: add blinking stop dot and low quality options
    We could want to stop the recording without expanding the notification
    panel and show sensitive info.
    Long pressing on the red dot moves it to the opposite side,
    in case the user wants not to cover specific elements on the screen.
    
    Add also a low quality option that still gives a good recording
    reducing a lot the final file size (useful when sharing the screenrecord
    on data connections).
    
    old commits ref:
    https://github.com/ezio84/abc_frameworks_base/commit/6092d9ca8b0949d64c1b87d6cd53d66daf002e14
    https://github.com/ezio84/abc_frameworks_base/commit/b50c7caf8b46c731093b11f5647028a23346f937
    https://github.com/ezio84/abc_frameworks_base/commit/91c00e8fb70cb17b8658678bf476452f96cd44d1
    
    Change-Id: If413241c3fa533650690cf3b7df5c05fb2f8c8ed
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialog.kt
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialog.kt
@@ -58,6 +58,8 @@ class ScreenRecordPermissionDialog(
     private lateinit var tapsView: View
     private lateinit var audioSwitch: Switch
     private lateinit var options: Spinner
+    private lateinit var stopDotSwitch: Switch
+    private lateinit var lowQualitySwitch: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setDialogTitle(R.string.screenrecord_start_label)
@@ -96,6 +98,8 @@ class ScreenRecordPermissionDialog(
         tapsView = findViewById(R.id.show_taps)
         updateTapsViewVisibility()
         options = findViewById(R.id.screen_recording_options)
+        stopDotSwitch = findViewById(R.id.screenrecord_stopdot_switch)
+        lowQualitySwitch = findViewById(R.id.screenrecord_lowquality_switch)
         val a: ArrayAdapter<*> =
             ScreenRecordingAdapter(context, android.R.layout.simple_spinner_dropdown_item, MODES)
         a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -125,6 +129,8 @@ class ScreenRecordPermissionDialog(
         val audioMode =
             if (audioSwitch.isChecked) options.selectedItem as ScreenRecordingAudioSource
             else ScreenRecordingAudioSource.NONE
+        val showStopDot = stopDotSwitch.isChecked
+        val lowQuality = lowQualitySwitch.isChecked
         val startIntent =
             PendingIntent.getForegroundService(
                 userContext,
@@ -134,7 +140,9 @@ class ScreenRecordPermissionDialog(
                     Activity.RESULT_OK,
                     audioMode.ordinal,
                     showTaps,
-                    captureTarget
+                    captureTarget,
+                    showStopDot,
+                    lowQuality
                 ),
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
