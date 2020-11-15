8c4ea7b5c0d4dcbbdb395edb0b08fa69ae6227e1
Alex Cruz <du.alexcruz@gmail.com>
2020-11-15 08:14:39 +0100

    Navbar: Introduce navbar styles [1/4]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5353,6 +5353,12 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * Switch between different navbar styles
+         * @hide
+         */
+        public static final String NAVBAR_STYLE = "navbar_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
