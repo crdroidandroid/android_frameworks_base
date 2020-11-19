ebb7419af8de3ad39025010b287c5311487062f6
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 20:46:32 -0300

    Introduce PixelPropsUtils for pixel features [SQUASHED]
    * That will spoof build fingerprints on some g00gle apps
    * Thanks to kdrag0n for the original idea at
      https://github.com/ProtonAOSP/android_frameworks_base/commit/5a54bfd846c7a26ba4820a361a6fec779edf8c5a
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: Alexander Winkowski <dereference23@outlook.com>
    Co-authored-by: Akash Srivastava <akashniki@gmail.com>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: Anay Wadhera <awadhera@berkeley.edu>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Co-authored-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -80,6 +80,8 @@ import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.crdroid.PixelPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1359,6 +1361,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        PixelPropsUtils.setProps(context);
         return app;
     }
     
@@ -1377,6 +1380,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        PixelPropsUtils.setProps(context);
         return app;
     }
 
