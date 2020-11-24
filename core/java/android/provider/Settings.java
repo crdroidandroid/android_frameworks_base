768f4480ecb0dcd53a0e3549bb28c2a8914afdfe
maxwen <maxwen@users.noreply.github.com>
2020-11-24 21:21:35 +0000

    base: Navigation mode settings [1/2]
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5423,6 +5423,9 @@ public final class Settings {
          */
         public static final String FOD_COLOR = "fod_color";
 
+        /** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
