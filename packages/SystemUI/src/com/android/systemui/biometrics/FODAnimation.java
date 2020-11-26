e7010aea6ce6b4075d02ad42e978a716591b3c64
Joey <joey@evolution-x.org>
2020-11-26 15:05:27 +0800

    SystemUI: Add OP8T Cyberpunk 2077 FOD animation [1/2]
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
@@ -57,6 +57,7 @@ public class FODAnimation extends ImageView {
         R.drawable.fod_op_energy_recognizing_anim,
         R.drawable.fod_op_mclaren_recognizing_anim,
         R.drawable.fod_op_ripple_recognizing_anim,
+        R.drawable.fod_op_scanning_recognizing_anim,
         R.drawable.fod_op_stripe_recognizing_anim,
         R.drawable.fod_op_wave_recognizing_anim,
         R.drawable.fod_pureview_dna_recognizing_anim,
