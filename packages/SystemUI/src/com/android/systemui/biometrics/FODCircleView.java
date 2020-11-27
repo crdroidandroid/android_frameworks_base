14acdf005ec53f0822a41b6bb8d0c8c6b47ec43a
AdarshGrewal <adarshgrewal@gmail.com>
2020-11-27 18:28:53 +0300

    Revert "FODCircleView: Hide fod while going to sleep"

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -166,7 +166,7 @@ public class FODCircleView extends ImageView {
         }
 
         @Override
-        public void onStartedGoingToSleep(int why) {
+        public void onScreenTurnedOff() {
             hide();
         }
 
