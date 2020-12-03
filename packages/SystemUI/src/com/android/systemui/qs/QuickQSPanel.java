a4ce134656792b81f4d09a30761d70517b684b94
ezio84 <brabus84@gmail.com>
2020-12-03 22:25:57 +0100

    QS rows&columns: use fixed values if landscape and playing media
    in landscape, the media player notification is moved to the right
    of the panel, so there's not so much space to have a lot of
    columns.
    
    Change-Id: I3368e7e9506b4f216b1953541a1f626257ba9256
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -112,6 +112,11 @@ public class QuickQSPanel extends QSPanel {
     }
 
     @Override
+    protected boolean needsDynamicRowsAndColumns() {
+        return false; // QQS always have the same layout
+    }
+
+    @Override
     protected boolean displayMediaMarginsOnMedia() {
         // Margins should be on the container to visually center the view
         return false;
