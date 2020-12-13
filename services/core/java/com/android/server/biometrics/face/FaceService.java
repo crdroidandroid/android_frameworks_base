0ab295a23ea4359f2f566c39fa23b253619e850b
Pranav Vashi <neobuddy89@gmail.com>
2020-12-13 13:20:03 +0530

    FaceUnlock: Update authentication method
    * Required after Android 11 r19 merge
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/face/FaceService.java
+++ services/core/java/com/android/server/biometrics/face/FaceService.java
@@ -145,11 +145,12 @@ public class FaceService extends BiometricServiceBase {
                 @Override
                 public final void run() {
                     Face face = new Face("", faceId, MOTO_DEVICE_ID);
+                    final boolean authenticated = faceId != 0;
                     ArrayList<Byte> token_AL = new ArrayList<>(token.length);
                     for (byte b : token) {
                         token_AL.add(new Byte(b));
                     }
-                    FaceService.super.handleAuthenticated(face, token_AL);
+                    FaceService.super.handleAuthenticated(authenticated, face, token_AL);
                 }
             });
         }
