db7b21548fb0a3e5ee37bb16c0b50ea12a9d13ef
ezio84 <brabus84@gmail.com>
2020-12-18 05:42:44 +0000

    Restore one-click to add/remove tiles after r19 merge
    Google now added the possibility to double click to move tiles
    for accessibility purposes but i actually prefer the oneclick way
    
    Change-Id: Ia44fc4e2ddc77c9cf918c2b8fea38a4e9a3c8252

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -89,7 +89,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
     private int mAccessibilityFromIndex;
     private QSTileHost mHost;
     private final UiEventLogger mUiEventLogger;
-    private final AccessibilityDelegateCompat mAccessibilityDelegate;
+  //private final AccessibilityDelegateCompat mAccessibilityDelegate;
     private RecyclerView mRecyclerView;
 
     public TileAdapter(Context context, UiEventLogger uiEventLogger) {
@@ -98,7 +98,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         mItemTouchHelper = new ItemTouchHelper(mCallbacks);
         mDecoration = new TileItemDecoration(context);
         mMinNumTiles = context.getResources().getInteger(R.integer.quick_settings_min_num_tiles);
-        mAccessibilityDelegate = new TileAdapterDelegate();
+      //mAccessibilityDelegate = new TileAdapterDelegate();
     }
 
     @Override
@@ -317,13 +317,27 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
 
         holder.mTileView.handleStateChanged(info.state);
         holder.mTileView.setShowAppLabel(position > mEditIndex && !info.isSystem);
-        holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+      /*holder.mTileView.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
         holder.mTileView.setClickable(true);
         holder.mTileView.setOnClickListener(null);
         holder.mTileView.setFocusable(true);
-        holder.mTileView.setFocusableInTouchMode(true);
+        holder.mTileView.setFocusableInTouchMode(true);*/
 
-        if (mAccessibilityAction != ACTION_NONE) {
+	holder.mTileView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getLayoutPosition();
+                if (position < mEditIndex) {
+                    if (canRemoveTiles()) {
+                        move(position, mEditIndex);
+                    }
+                } else {
+                    move(position, mEditIndex);
+                }
+            }
+        });
+
+      /*if (mAccessibilityAction != ACTION_NONE) {
             holder.mTileView.setClickable(selectable);
             holder.mTileView.setFocusable(selectable);
             holder.mTileView.setFocusableInTouchMode(selectable);
@@ -345,7 +359,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         }
         if (position == mFocusIndex) {
             focusOnHolder(holder);
-        }
+        }*/
     }
 
     private void focusOnHolder(Holder holder) {
@@ -502,7 +516,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
                 mTileView.setBackground(null);
                 mTileView.getIcon().disableAnimation();
                 mTileView.setTag(this);
-                ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
+              //ViewCompat.setAccessibilityDelegate(mTileView, mAccessibilityDelegate);
             }
         }
 
