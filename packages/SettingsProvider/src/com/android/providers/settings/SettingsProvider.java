38e8218aa5d0129a4e3d813a86733fdd6a6b000a
Ting Yu <ting.x.yu@sony.com>
2020-12-22 09:55:27 +0200

    Phone ringtone setting for Multi SIM device [1/3]
    Add below changes for Multi SIM device phone ringtone.
    1. Add api setActualDefaultRingtoneUriForPhoneAccountHandle to
    set ringtone for specific PhoneAccountHandle.
    2. Add api getActualDefaultRingtoneUriForPhoneAccountHandle to
    get ringtone for specific PhoneAccountHandle.
    3. Support saving ringtone cache for specific PhoneAccountHandle.
    
    Bug: 118735436
    Test: Manual, CTS Test
    Change-Id: I26c20ef52753256e1148ee9b01492034f55a3bd4

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -48,6 +48,7 @@ import android.compat.annotation.EnabledSince;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
@@ -818,6 +819,16 @@ public class SettingsProvider extends ContentProvider {
         if (Settings.System.RINGTONE_CACHE_URI.equals(uri)) {
             cacheRingtoneSetting = Settings.System.RINGTONE;
             cacheName = Settings.System.RINGTONE_CACHE;
+        } else if (uri != null && ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())
+                && Settings.AUTHORITY.equals(
+                        ContentProvider.getAuthorityWithoutUserId(uri.getAuthority()))
+                && uri.getPathSegments().size() == 2
+                && uri.getPathSegments().get(1).startsWith(Settings.System.RINGTONE_CACHE)) {
+            // Check whether the uri is ringtone cache uri for a specific PhoneAccountHandle,
+            // which should be in the form of "content://settings/system/ringtone_cache_xxxx".
+            cacheRingtoneSetting = uri.getPathSegments().get(1)
+                    .replace(Settings.System.RINGTONE_CACHE, Settings.System.RINGTONE);
+            cacheName = uri.getPathSegments().get(1);
         } else if (Settings.System.NOTIFICATION_SOUND_CACHE_URI.equals(uri)) {
             cacheRingtoneSetting = Settings.System.NOTIFICATION_SOUND;
             cacheName = Settings.System.NOTIFICATION_SOUND_CACHE;
@@ -1880,6 +1891,8 @@ public class SettingsProvider extends ContentProvider {
         String cacheName = null;
         if (Settings.System.RINGTONE.equals(name)) {
             cacheName = Settings.System.RINGTONE_CACHE;
+        } else if (name.startsWith(Settings.System.RINGTONE)) {
+            cacheName = name.replace(Settings.System.RINGTONE, Settings.System.RINGTONE_CACHE);
         } else if (Settings.System.NOTIFICATION_SOUND.equals(name)) {
             cacheName = Settings.System.NOTIFICATION_SOUND_CACHE;
         } else if (Settings.System.ALARM_ALERT.equals(name)) {
@@ -2000,7 +2013,8 @@ public class SettingsProvider extends ContentProvider {
             case MUTATION_OPERATION_INSERT:
                 // Insert updates.
             case MUTATION_OPERATION_UPDATE: {
-                if (Settings.System.PUBLIC_SETTINGS.contains(name)) {
+                if (Settings.System.PUBLIC_SETTINGS.contains(name)
+                        || name.startsWith(Settings.System.RINGTONE)) {
                     return;
                 }
 
@@ -2019,7 +2033,8 @@ public class SettingsProvider extends ContentProvider {
 
             case MUTATION_OPERATION_DELETE: {
                 if (Settings.System.PUBLIC_SETTINGS.contains(name)
-                        || Settings.System.PRIVATE_SETTINGS.contains(name)) {
+                        || Settings.System.PRIVATE_SETTINGS.contains(name)
+                        || name.startsWith(Settings.System.RINGTONE)) {
                     throw new IllegalArgumentException("You cannot delete system defined"
                             + " secure settings.");
                 }
