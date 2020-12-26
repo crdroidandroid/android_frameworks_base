0dd72c9bc27196f0d5650fa3dafcf57e5d7b6127
kondors1995 <normandija1945@gmail.com>
2020-12-26 16:22:16 +0300

    SystemUI: FOD icon picker [1/2]
    Stallix edit: Include SystemUI: Use OP8T Cyberpunk 2077 FOD icon: https://github.com/Evolution-X/frameworks_base/commit/84675bc9681ca186ec62da70e14e71eb99c55697
    
    Squashed commit of the following:
    
    commit 6661dcfaa194bd31c0627cdb917ec714d039850f
    Author: kondors1995 <normandija1945@gmail.com>
    Date:   Fri Nov 27 12:48:29 2020 +0000
    
        FOD: convert fod icons to webp
    
        Change-Id: I791863a995f9d9bd178f9fb3445458a6212883db
    
    commit 88565cd49d1a6bae723dc3aaaa766ba592ee559e
    Author: kondors1995 <normandija1945@gmail.com>
    Date:   Fri Nov 27 12:31:19 2020 +0000
    
        SystemUI: FOD icon picker
    
        Change-Id: I6ea35710bfb083dbf2f0f9551ab9bcb15ca4dce2
    
    commit 40e3e43dad0aad2ca89c74a147fbfa9c992e1768
    Author: AnierinB <anierinb@evolution-x.org>
    Date:   Mon Feb 17 23:10:17 2020 +0000
    
        SystemUI: Add fire_ice_ouroboros style FOD icon [1/2]
    
        Signed-off-by: AnierinB <anierinb@evolution-x.org>
        Change-Id: Idc1f17ead23cc9fd3d4b0135df8e1c20897c9c3c
    
    commit c81549d485a602bc48cd7524dec3bd09f27d18ac
    Author: AnierinB <anierinb@evolution-x.org>
    Date:   Sun Feb 9 01:31:39 2020 +0000
    
        SystemUI: Add scratch style FOD icons [1/2]
    
        Co-authored-by: deadlyghost088 <raag.naidu@gmail.com>
        Signed-off-by: AnierinB <anierinb@evolution-x.org>
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Change-Id: I029f3e0e73dc6d3203f456f331bfc01d0e9c301d
    
    commit 67133da8651d87d05383b18e0150a29f185ccff4
    Author: Joey Huab <team.evolutionx.rom@gmail.com>
    Date:   Wed Jan 8 14:40:03 2020 +0800
    
        SystemUI: Add Evo FOD icons [1/2]
    
        Co-authored-by: ecnivtwelve <ecnivtwelve@users.noreply.github.com>
        Co-authored-by: MartonDev <martondeveloper@gmail.com>
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Change-Id: I9888361aba4e5311bf564c3dd0253137f572d605
    
    commit d171d6a36fe78fd55153216204b9d0d7e825031d
    Author: Joey Huab <team.evolutionx.rom@gmail.com>
    Date:   Sat Jan 4 00:41:41 2020 +0800
    
        SystemUI: Add more FOD icons [1/2]
    
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Change-Id: I3962c5ec7d4d93a49d0d253c15889ec39e88d828
    
    commit a2f9903e095be735e8bd219095899c72b81eb868
    Author: 00day0 <therandomuser11@gmail.com>
    Date:   Mon Dec 16 21:31:27 2019 +0100
    
        SystemUI: FOD icon picker [1/2]
    
        Co-authored-by: Stallix <thestallix@gmail.com>
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Change-Id: I588229074ad07452cfca10dc572f5f919df6a448
    
    Co-authored-by: Stallix <joey@evolution-x.org>
    Change-Id: Ibfc3d72091c83b817eddf8fda750c09deb195783

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -74,7 +74,6 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     private final int mNavigationBarSize;
     private final boolean mShouldBoostBrightness;
     private final Paint mPaintFingerprintBackground = new Paint();
-    private final Paint mPaintFingerprint = new Paint();
     private final WindowManager.LayoutParams mParams = new WindowManager.LayoutParams();
     private final WindowManager.LayoutParams mPressedParams = new WindowManager.LayoutParams();
     private final WindowManager mWindowManager;
@@ -82,6 +81,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     private IFingerprintInscreen mFingerprintInscreenDaemon;
 
     private int mDreamingOffsetX;
+    private int mColorBackground;
     private int mDreamingOffsetY;
 
     private boolean mFading;
@@ -110,6 +110,36 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
 
     private FODAnimation mFODAnimation;
     private boolean mIsRecognizingAnimEnabled;
+
+    private int mSelectedIcon;
+    private final int[] ICON_STYLES = {
+        R.drawable.fod_icon_default,
+        R.drawable.fod_icon_default_0,
+        R.drawable.fod_icon_default_1,
+        R.drawable.fod_icon_default_2,
+        R.drawable.fod_icon_default_3,
+        R.drawable.fod_icon_default_4,
+        R.drawable.fod_icon_default_5,
+        R.drawable.fod_icon_arc_reactor,
+        R.drawable.fod_icon_cpt_america_flat,
+        R.drawable.fod_icon_cpt_america_flat_gray,
+        R.drawable.fod_icon_dragon_black_flat,
+        R.drawable.fod_icon_evo1,
+        R.drawable.fod_icon_glow_circle,
+        R.drawable.fod_icon_neon_arc,
+        R.drawable.fod_icon_neon_arc_gray,
+        R.drawable.fod_icon_neon_circle_pink,
+        R.drawable.fod_icon_neon_triangle,
+        R.drawable.fod_icon_paint_splash_circle,
+        R.drawable.fod_icon_rainbow_horn,
+        R.drawable.fod_icon_shooky,
+        R.drawable.fod_icon_spiral_blue,
+        R.drawable.fod_icon_sun_metro,
+        R.drawable.fod_icon_scratch_pink_blue,
+        R.drawable.fod_icon_scratch_red_blue,
+        R.drawable.fod_icon_fire_ice_ouroboros,
+        R.drawable.fod_icon_transparent
+    };
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =
             new IFingerprintInscreenCallback.Stub() {
@@ -247,8 +277,6 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     public FODCircleView(Context context) {
         super(context);
 
-        setScaleType(ScaleType.CENTER);
-
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         if (daemon == null) {
             throw new RuntimeException("Unable to get IFingerprintInscreen");
@@ -265,10 +293,8 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
 
         Resources res = context.getResources();
 
-        mPaintFingerprint.setColor(res.getColor(R.color.config_fodColor));
-        mPaintFingerprint.setAntiAlias(true);
-
-        mPaintFingerprintBackground.setColor(res.getColor(R.color.config_fodColorBackground));
+        mColorBackground = res.getColor(R.color.config_fodColorBackground);
+        mPaintFingerprintBackground.setColor(mColorBackground);
         mPaintFingerprintBackground.setAntiAlias(true);
 
         mPowerManager = context.getSystemService(PowerManager.class);
@@ -305,7 +331,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
             @Override
             protected void onDraw(Canvas canvas) {
                 if (mIsCircleShowing) {
-                    canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+                    canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprintBackground);
                 }
                 super.onDraw(canvas);
             }
@@ -351,12 +377,16 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.FOD_ANIM),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.FOD_ICON),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.FOD_ANIM))) {
+                    Settings.System.FOD_ANIM)) || uri.equals(Settings.System.getUriFor(
+                Settings.System.FOD_ICON))) {
                 updateStyle();
             }
         }
@@ -479,7 +509,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     public void hideCircle() {
         mIsCircleShowing = false;
 
-        setImageResource(R.drawable.fod_icon_default);
+        setImageResource(ICON_STYLES[mSelectedIcon]);
         invalidate();
 
         dispatchRelease();
@@ -551,6 +581,9 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     private void updateStyle() {
         mIsRecognizingAnimEnabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.FOD_RECOGNIZING_ANIMATION, 0) != 0;
+        mSelectedIcon = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_ICON, 0);
+
         if (mFODAnimation != null) {
             mFODAnimation.update(mIsRecognizingAnimEnabled);
         }
