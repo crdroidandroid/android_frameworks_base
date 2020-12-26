5918de34ac1aedf05042eef177b66671c2380238
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-12-26 15:45:11 +0300

    FODAnimation: Allow to set vertical position offset
    For some reason on OnePlus 7 FOD animation not perfectly aligned with FOD circle.
    This commit allows to set offset for vertical position of FOD animation.
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I88d21447df3ac6cec7ab9256960e030d5e716d84
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODAnimation.java
@@ -35,7 +35,7 @@ public class FODAnimation extends ImageView {
     private boolean mShowing = false;
     private Context mContext;
     private int mAnimationSize;
-    private int mAnimationPositionY;
+    private int mAnimationOffset;
     private AnimationDrawable recognizingAnim;
     private WindowManager mWindowManager;
     private boolean mIsKeyguard;
@@ -74,6 +74,7 @@ public class FODAnimation extends ImageView {
         mWindowManager = mContext.getSystemService(WindowManager.class);
 
         mAnimationSize = mContext.getResources().getDimensionPixelSize(R.dimen.fod_animation_size);
+        mAnimationOffset = mContext.getResources().getDimensionPixelSize(R.dimen.fod_animation_offset);
         mAnimParams.height = mAnimationSize;
         mAnimParams.width = mAnimationSize;
 
@@ -82,7 +83,7 @@ public class FODAnimation extends ImageView {
         mAnimParams.flags =  WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                 | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
         mAnimParams.gravity = Gravity.TOP | Gravity.CENTER;
-        mAnimParams.y = mPositionY - (mAnimationSize / 2);
+        mAnimParams.y = mPositionY - (mAnimationSize / 2) + mAnimationOffset;
 
         setScaleType(ImageView.ScaleType.CENTER_INSIDE);
         update();
@@ -97,7 +98,7 @@ public class FODAnimation extends ImageView {
     }
 
     public void updateParams(int mDreamingOffsetY) {
-        mAnimParams.y = mDreamingOffsetY - (mAnimationSize / 2);
+        mAnimParams.y = mDreamingOffsetY - (mAnimationSize / 2) + mAnimationOffset;
     }
 
     public void setAnimationKeyguard(boolean state) {
