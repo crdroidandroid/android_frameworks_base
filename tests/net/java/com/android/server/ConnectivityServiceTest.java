664cf1f43ba58f9e6affe95c374ad65d69d42114
BL4CKH47H4CK3R <54761717+BL4CKH47H4CK3R@users.noreply.github.com>
2020-12-29 01:17:40 +0000

    Captive Portal URL Google to GrapheneOS
    For a truely vanilla experience one needs to be free from google servers !
    Though you don't use gapps google knows very well where you are !
    You can verify
    curl -I http://connectivitycheck.grapheneos.network/generate_204
    curl -I http://connectivitycheck.gstatic.com/generate_204

--- tests/net/java/com/android/server/ConnectivityServiceTest.java
+++ tests/net/java/com/android/server/ConnectivityServiceTest.java
@@ -4465,7 +4465,7 @@ public class ConnectivityServiceTest {
     @Test
     public void testGetCaptivePortalServerUrl() throws Exception {
         String url = mCm.getCaptivePortalServerUrl();
-        assertEquals("http://connectivitycheck.gstatic.com/generate_204", url);
+        assertEquals("http://connectivitycheck.grapheneos.network/generate_204", url);
     }
 
     private static class TestNetworkPinner extends NetworkPinner {
