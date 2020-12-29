7ca7dc0a4015901459c49be4623517f723e0d59c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-12-29 14:05:01 +0530

    BatteryService: Add Turbo power charging support
    Inspired by oneplus dash charging commit
    
    https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    
    Charlie-117: Port to R
    
    Change-Id: I12784d37062753a306af9fd9c9d5cbba0cc578ea
    Signed-off-by: Charlie-117 <gmoto1179@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -187,6 +187,10 @@ public final class BatteryService extends SystemService {
     private boolean mHasVoocCharger;
     private boolean mLastVoocCharger;
 
+    private boolean mTurboPower;
+    private boolean mHasTurboPower;
+    private boolean mLastTurboPower;
+
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
 
@@ -236,6 +240,8 @@ public final class BatteryService extends SystemService {
                 com.android.internal.R.bool.config_hasWarpCharger);
         mHasVoocCharger = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_hasVoocCharger);
+        mHasTurboPower = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasTurboPowerCharger);
 
         mCriticalBatteryLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
@@ -574,6 +580,7 @@ public final class BatteryService extends SystemService {
         mDashCharger = mHasDashCharger && isDashCharger();
         mWarpCharger = mHasWarpCharger && isWarpCharger();
         mVoocCharger = mHasVoocCharger && isVoocCharger();
+        mTurboPower = mHasTurboPower && isTurboPower();
 
         if (force
                 || (mHealthInfo.batteryStatus != mLastBatteryStatus
@@ -594,7 +601,8 @@ public final class BatteryService extends SystemService {
                         || mInvalidCharger != mLastInvalidCharger
                         || mDashCharger != mLastDashCharger
                         || mWarpCharger != mLastWarpCharger
-                        || mVoocCharger != mLastVoocCharger)) {
+                        || mVoocCharger != mLastVoocCharger
+                        || mTurboPower != mLastTurboPower)) {
 
             if (mPlugType != mLastPlugType) {
                 if (mLastPlugType == BATTERY_PLUGGED_NONE) {
@@ -776,6 +784,7 @@ public final class BatteryService extends SystemService {
             mLastDashCharger = mDashCharger;
             mLastWarpCharger = mWarpCharger;
             mLastVoocCharger = mVoocCharger;
+            mLastTurboPower = mTurboPower;
         }
     }
 
@@ -816,6 +825,7 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_DASH_CHARGER, mDashCharger);
         intent.putExtra(BatteryManager.EXTRA_WARP_CHARGER, mWarpCharger);
         intent.putExtra(BatteryManager.EXTRA_VOOC_CHARGER, mVoocCharger);
+        intent.putExtra(BatteryManager.EXTRA_TURBO_POWER, mTurboPower);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
                     + ", info:" + mHealthInfo.toString());
@@ -906,6 +916,20 @@ public final class BatteryService extends SystemService {
             br.close();
             file.close();
             return "1".equals(state);
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+        return false;
+    }
+
+    private boolean isTurboPower() {
+        try {
+            FileReader file = new FileReader("/sys/class/power_supply/battery/charge_rate");
+            BufferedReader br = new BufferedReader(file);
+            String state = br.readLine();
+            br.close();
+            file.close();
+            return "Turbo".equals(state);
         } catch (FileNotFoundException e) {
         } catch (IOException e) {
         }
