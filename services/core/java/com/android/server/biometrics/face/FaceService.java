c84b5d5fb3b336ac41c5631afabcb241350a0917
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-12-29 01:52:50 +0000

    FaceService: save ourselves from NPE
    This happened after removing every security method and going to lockscreen.
    
    12-29 02:50:31.119  1442  1442 E AndroidRuntime: java.lang.NullPointerException: Attempt to get length of null array
    12-29 02:50:31.119  1442  1442 E AndroidRuntime:        at com.android.server.biometrics.face.FaceService$1$2.run(FaceService.java:146)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/face/FaceService.java
+++ services/core/java/com/android/server/biometrics/face/FaceService.java
@@ -141,6 +141,7 @@ public class FaceService extends BiometricServiceBase {
 
         @Override
         public void onAuthenticated(int faceId, int userId, byte[] token) {
+            if (token == null) return;
             mHandler.post(new Runnable() {
                 @Override
                 public final void run() {
