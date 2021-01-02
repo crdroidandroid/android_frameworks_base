ab7e0c22bf0f9c63cef0650931e80074d59eece1
Arian <arian.kulmer@web.de>
2021-01-02 02:09:49 +0100

    Camera: Replace packageblacklist with packageexcludelist
    * Replace vendor.camera.aux.packageblacklist with vendor.camera.aux.packageexcludelist
      as recommended by coding guidelines https://developers.google.com/style/word-list#blacklist.
    
    Change-Id: I9c1862528ecba06ac612d926757cbc3c313009fa
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -293,7 +293,7 @@ public class Camera {
         List<String> packageList = Arrays.asList(
                 SystemProperties.get("vendor.camera.aux.packagelist", packageName).split(","));
         List<String> packageBlacklist = Arrays.asList(
-                SystemProperties.get("vendor.camera.aux.packageblacklist", "").split(","));
+                SystemProperties.get("vendor.camera.aux.packageexcludelist", "").split(","));
 
         return packageList.contains(packageName) && !packageBlacklist.contains(packageName);
     }
