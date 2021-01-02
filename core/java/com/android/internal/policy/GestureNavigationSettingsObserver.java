735de1359b7b8dec25dcb291899ab539646c74ba
Ali B <abittin@gmail.com>
2021-01-02 21:14:45 +0000

    Allow to hide arrow for back gesture [1/2]

--- core/java/com/android/internal/policy/GestureNavigationSettingsObserver.java
+++ core/java/com/android/internal/policy/GestureNavigationSettingsObserver.java
@@ -67,6 +67,9 @@ public class GestureNavigationSettingsObserver extends ContentObserver {
         r.registerContentObserver(
                 Settings.Secure.getUriFor(Settings.Secure.USER_SETUP_COMPLETE),
                 false, this, UserHandle.USER_ALL);
+        r.registerContentObserver(Settings.Secure.getUriFor(
+                Settings.Secure.SHOW_BACK_ARROW_GESTURE),
+                false, this, UserHandle.USER_ALL);
         DeviceConfig.addOnPropertiesChangedListener(
                 DeviceConfig.NAMESPACE_SYSTEMUI,
                 runnable -> mMainHandler.post(runnable),
@@ -92,6 +95,12 @@ public class GestureNavigationSettingsObserver extends ContentObserver {
 
     public int getRightSensitivity(Resources userRes) {
         return getSensitivity(userRes, Settings.Secure.BACK_GESTURE_INSET_SCALE_RIGHT);
+    }
+
+    public boolean getBackArrowGesture() {
+        return Settings.Secure.getIntForUser(
+              mContext.getContentResolver(), Settings.Secure.SHOW_BACK_ARROW_GESTURE, 1,
+              UserHandle.USER_CURRENT) == 1;
     }
 
     public boolean areNavigationButtonForcedVisible() {
