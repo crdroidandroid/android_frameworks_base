BlissRoms/platform_frameworks_base

base: Add device key action to expand notifications panel [1/3]
Change-Id: I70878ba8815af82edbe856ef39c832e0b8206a8b
Signed-off-by: rohan <purohit.rohan@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1960,6 +1960,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case CLEAR_ALL_NOTIFICATIONS:
                 BlissUtils.clearAllNotifications();
                 break;
+            case NOTIFICATIONS:
+                BlissUtils.toggleNotifications();
+                break;
             default:
                 break;
         }

crdroidandroid/android_frameworks_base

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1956,6 +1956,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case CLEAR_ALL_NOTIFICATIONS:
                 Utils.clearAllNotifications();
                 break;
+            case NOTIFICATIONS:
+                Utils.toggleNotifications();
+                break;
             default:
                 break;
         }