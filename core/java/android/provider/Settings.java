b2b425551d7977f80a85dd2ca9f9be624825f757
ShubhamB <shubhamprince111@gmail.com>
2021-02-13 20:03:52 +0530

    Powermenu: Separate Advanced Reboot actions from Restart
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5257,6 +5257,13 @@ public final class Settings {
         public static final String POWERMENU_RESTART = "powermenu_restart";
 
          /**
+         * Whether to display reboot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_ADVANCED = "powermenu_advanced";
+
+         /**
          * Whether to display power in the power menu
          *
          * @hide
