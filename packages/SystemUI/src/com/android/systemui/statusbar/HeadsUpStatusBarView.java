1180e439d6e8603ca4faebd66850e175701058f1
HELLBOY017 <76108696+HELLBOY017@users.noreply.github.com>
2021-02-22 08:32:22 +0530

    Fix Status Bar Heads Up Padding Start

--- packages/SystemUI/src/com/android/systemui/statusbar/HeadsUpStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/HeadsUpStatusBarView.java
@@ -79,9 +79,7 @@ public class HeadsUpStatusBarView extends AlphaOptimizedLinearLayout {
             int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         Resources res = getResources();
-        mAbsoluteStartPadding = res.getDimensionPixelSize(R.dimen.notification_side_paddings)
-            + res.getDimensionPixelSize(
-                    com.android.internal.R.dimen.notification_content_margin_start);
+        mAbsoluteStartPadding = res.getDimensionPixelSize(R.dimen.heads_up_status_bar_padding_start);
         mEndMargin = res.getDimensionPixelSize(
                 com.android.internal.R.dimen.notification_content_margin_end);
         setPaddingRelative(mAbsoluteStartPadding, 0, mEndMargin, 0);
