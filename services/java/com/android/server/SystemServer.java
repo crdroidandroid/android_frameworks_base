6430b3a2bf6643fb8586fd3fc72e0a2ab3a0b389
Ido Ben-Hur <idoybh2@gmail.com>
2021-02-27 01:52:33 +0200

    base: Allow scheduling always on display [1/2]
    From sunset to sunrise or at a custom time
    
    Squashed:
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Tue, 16 Mar 2021 13:53:27 +0200
        Subject: AutoAODService: Add support for mixed time & sun modes [1/2]
    
        Also refactor some code
    
        Change-Id: I3a78dec88f532766d00e0d1d276c27dc9d3dc68f
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Fri, 26 Mar 2021 08:45:31 +0300
        Subject: AutoAODService: Account for disabled doze
    
        Change-Id: I30f52c4e3db2a27f9cde01662a25eee33e225414
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Fri, 9 Apr 2021 13:33:41 +0300
        Subject: AutoAODService: Slightly improve code and docs
    
        * Correctly link local vars and functions in docs
        * Make vars we can final
        * Don't use String.valueOf() where we don't have to
        * Should be Integer.parseInt() and not Integer.valueOf()
    
        Change-Id: I5892858c7142113ad3c9c87ddf00e58b18508207
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Wed, 2 Mar 2022 19:05:43 +0200
        Subject: AutoAODService: Improve some code
    
        And call using the right handler on settings change
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Thu, 31 Mar 2022 20:59:38 +0300
        Subject: AutoAODService: Use Calendar.add instead of Calendar.roll
    
        Roll doesn't always give the intended result which can cause an alarm to be set to the distance past/future on the end of the month
        Also make sure we cancel previous alarms before setting new ones, just incase
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: cjh1249131356 <cjh1249131356@gmail.com>
        Date: Fri, 20 May 2022 22:41:49 +0800
        Subject: base: Fix scheduled AOD
    
        - Always init state when settings change observed, as we need to update scheduled time, not only auto mode.
        - Fix an edge case which can be reproduced with following steps:
          Set phone to 00:00
          Set an overnight schedule time (23:00 ~ 07:00 etc)
    
          Then three Calendar instances hold following dates:
          current: 2022/05/20 00:00
          since: 2022/05/20 23:00
          till: 2022/05/20 07:00
    
          According to the first if condition, till date will be 2022/05/21 07:00
          Then issue comes, till time can never be reached until you set a till time after since time.
    
        Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Sun, 11 Dec 2022 15:52:01 +0200
        Subject: AutoAODService: Properly handle reboots & fix some logic
    
        Use shared preferences to find whether the user aborted current scheduled session
        In case that happens - don't automatically toggle AOD on boot until we passed the next alarm
    
        In addition:
        * Don't toggle AOD onTwilightStateChanged nor in mTimeChangedReceiver - they should just change the alarms
        * Properly re-init on ALL setting changes, not just mode
        * Never disable AOD on boot just because the setting is disabled
        * Never trigger doze / screen on when interactive
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Mon, 9 Jan 2023 06:55:56 +0200
        Subject: AutoAODService: Better check for doze enablement
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Ib76ecb1855ac215de6d7cd2f2346abfce6cc2214
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -168,6 +168,7 @@ import com.android.server.credentials.CredentialManagerService;
 import com.android.server.criticalevents.CriticalEventLog;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
+import com.android.server.display.AutoAODService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.display.color.ColorDisplayService;
 import com.android.server.dreams.DreamManagerService;
@@ -2785,6 +2786,12 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("AttestationService");
             mSystemServiceManager.startService(AttestationService.class);
             t.traceEnd();
+
+            if (context.getResources().getBoolean(R.bool.config_dozeAlwaysOnDisplayAvailable)) {
+                t.traceBegin("AutoAODService");
+                mSystemServiceManager.startService(AutoAODService.class);
+                t.traceEnd();
+            }
         }
 
         t.traceBegin("StartMediaProjectionManager");
