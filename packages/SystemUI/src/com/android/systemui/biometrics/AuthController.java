d458abc61d55a55c96d6d7f92adee3fbbf723e6e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-03-06 15:27:11 -0300

    Allow using face as auth method for apps
    Change-Id: I008370cefe9584c86e058b718e2ef79518b95eb6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
@@ -165,6 +165,19 @@ public class AuthController extends SystemUI implements CommandQueue.Callbacks,
     }
 
     @Override
+    public void onUseFacePressed() {
+        if (mReceiver == null) {
+            Log.e(TAG, "onUseFacePressed: Receiver is null");
+            return;
+        }
+        try {
+            mReceiver.onUseFacePressed();
+        } catch (RemoteException e) {
+            Log.e(TAG, "RemoteException when handling credential button", e);
+        }
+    }
+
+    @Override
     public void onSystemEvent(int event) {
         if (mReceiver == null) {
             Log.e(TAG, "onSystemEvent(" + event + "): Receiver is null");
