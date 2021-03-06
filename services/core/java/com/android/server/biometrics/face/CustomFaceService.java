cf3a800d8caab78364ebb392ada06417a4d9bc89
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-03-06 15:27:11 -0300

    Allow using face as auth method for apps
    Change-Id: I008370cefe9584c86e058b718e2ef79518b95eb6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/face/CustomFaceService.java
+++ services/core/java/com/android/server/biometrics/face/CustomFaceService.java
@@ -28,6 +28,8 @@ import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.hardware.biometrics.BiometricAuthenticator;
+import android.hardware.biometrics.BiometricManager.Authenticators;
 import android.hardware.face.Face;
 import android.os.Binder;
 import android.os.Handler;
@@ -384,7 +386,11 @@ public class CustomFaceService {
         return info != null && info.serviceInfo.isEnabled();
     }
 
-    public boolean isSupported() {
+    public static String getConfiguration() {
+        return HAL_DEVICE_ID + ":" + BiometricAuthenticator.TYPE_FACE + ":" + Authenticators.BIOMETRIC_STRONG;
+    }
+
+    public static boolean isSupported() {
         return FaceUnlockUtils.isFaceUnlockSupported();
     }
 
