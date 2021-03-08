961cd4bdc060345f269f0219cd6eeb2bcf13c493
Pranav Vashi <neobuddy89@gmail.com>
2021-03-08 17:22:43 +0530

    Pixel Navbar animation toggle [1/2]
    * Forward ported old commit: https://github.com/crdroidandroid/android_frameworks_base/commit/13e6d680003d60c43f1163d41631ff4ae10159ba
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5693,6 +5693,12 @@ public final class Settings {
         public static final String LOCKSCREEN_SMALL_CLOCK = "lockscreen_small_clock";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
