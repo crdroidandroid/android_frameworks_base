7b166687bf113bc0a63525f27a8c3e7692f6a2d0
mukesh22584 <mks22584@gmail.com>
2021-03-12 18:57:38 +0530

    SystemUI: Add crDroid Light FOD icon [1/2]
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -126,7 +126,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         R.drawable.fod_icon_cpt_america_flat,
         R.drawable.fod_icon_cpt_america_flat_gray,
         R.drawable.fod_icon_dragon_black_flat,
-        R.drawable.fod_icon_evo1,
+        R.drawable.fod_icon_cr1,
         R.drawable.fod_icon_glow_circle,
         R.drawable.fod_icon_neon_arc,
         R.drawable.fod_icon_neon_arc_gray,
