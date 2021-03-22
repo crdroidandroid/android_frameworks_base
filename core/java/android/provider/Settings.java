f58490e7ec0b540d488234abc6b5d03671c7dbff
Adam Szorenyi <adam.szorenyi88@gmail.com>
2021-03-22 21:36:01 +0100

    base: Smart Pulldown [1/2]
        Rewritten by Beanstown106 for 6.0/7.0
    
        previously the detection for ongoing/persistent notifications were not working
        im not sure how long this has actually been broken but i cleaned things up a little bit
        also put to use the isOngoing boolean and properly put to use the hasActiveVisibleNotifications boolean
    
        current options now are off, no dismissable, no ongoing, and no notifications
    
        Credits: kufikugel, HardCorePawn, acardinal
        mydongistiny: fixed for oreo
    
    Change-Id: I627295eaa3b1f73503c0952d72f879e4ebe6e407
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Co-authored-by: Lars Greiss <kufikugel@googlemail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5551,6 +5551,12 @@ public final class Settings {
         public static final String QS_DATAUSAGE = "qs_datausage";
 
         /**
+         * Quick Settings Smart Pulldown
+         * @hide
+         */
+        public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
