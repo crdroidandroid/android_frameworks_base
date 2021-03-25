ca23bb3babb93d1aa48d02bab122f22cc8699d27
Peter Collingbourne <pcc@google.com>
2021-03-25 11:46:44 -0700

    Reset PAC keys on thread creation instead of on zygote fork.
    Resetting PAC keys on fork appears to lead to a number of problems. One
    problem is that we are constrained in where we can run C++ code after
    forking, and with ART those places are implementation-defined. For
    example, in app zygotes, ART turns out to insert "interpreter frames"
    in the stack trace. Returning into these interpreter frames may lead
    to crashes due to failing the ROP protection check on return.
    
    It seems better to reset keys on thread creation instead. We only need
    to reset IA because only this key needs to be reset for reverse-edge
    PAC, and resetting the other keys may be incompatible with future ABIs.
    
    Chrome (and potentially other applications) has a sandbox that prevents
    the use of the prctl, so we restrict its use to applications targeting
    S and above.
    
    Bug: 183024045
    Change-Id: I1e6502a7d7df319d424e2b0f653aad9a343ae71b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/jni/com_android_internal_os_Zygote.cpp
+++ core/jni/com_android_internal_os_Zygote.cpp
@@ -1043,21 +1043,6 @@ static void ClearUsapTable() {
   gUsapPoolCount = 0;
 }
 
-NO_PAC_FUNC
-static void PAuthKeyChange(JNIEnv* env) {
-#ifdef __aarch64__
-  unsigned long int hwcaps = getauxval(AT_HWCAP);
-  if (hwcaps & HWCAP_PACA) {
-    const unsigned long key_mask = PR_PAC_APIAKEY | PR_PAC_APIBKEY |
-                                   PR_PAC_APDAKEY | PR_PAC_APDBKEY | PR_PAC_APGAKEY;
-    if (prctl(PR_PAC_RESET_KEYS, key_mask, 0, 0, 0) != 0) {
-      ALOGE("Failed to change the PAC keys: %s", strerror(errno));
-      RuntimeAbort(env, __LINE__, "PAC key change failed.");
-    }
-  }
-#endif
-}
-
 // Create an app data directory over tmpfs overlayed CE / DE storage, and bind mount it
 // from the actual app data directory in data mirror.
 static void createAndMountAppData(std::string_view package_name,
@@ -1957,7 +1942,6 @@ void zygote::ZygoteFailure(JNIEnv* env,
 }
 
 // Utility routine to fork a process from the zygote.
-NO_PAC_FUNC
 pid_t zygote::ForkCommon(JNIEnv* env, bool is_system_server,
                          const std::vector<int>& fds_to_close,
                          const std::vector<int>& fds_to_ignore,
@@ -2012,7 +1996,6 @@ pid_t zygote::ForkCommon(JNIEnv* env, bool is_system_server,
     }
 
     // The child process.
-    PAuthKeyChange(env);
     PreApplicationInit();
 
     // Clean up any descriptors which must be closed immediately
@@ -2044,7 +2027,6 @@ static void com_android_internal_os_Zygote_nativePreApplicationInit(JNIEnv*, jcl
   PreApplicationInit();
 }
 
-NO_PAC_FUNC
 static jint com_android_internal_os_Zygote_nativeForkAndSpecialize(
         JNIEnv* env, jclass, jint uid, jint gid, jintArray gids,
         jint runtime_flags, jobjectArray rlimits,
@@ -2098,7 +2080,6 @@ static jint com_android_internal_os_Zygote_nativeForkAndSpecialize(
     return pid;
 }
 
-NO_PAC_FUNC
 static jint com_android_internal_os_Zygote_nativeForkSystemServer(
         JNIEnv* env, jclass, uid_t uid, gid_t gid, jintArray gids,
         jint runtime_flags, jobjectArray rlimits, jlong permitted_capabilities,
@@ -2171,7 +2152,6 @@ static jint com_android_internal_os_Zygote_nativeForkSystemServer(
  * @param is_priority_fork  Controls the nice level assigned to the newly created process
  * @return child pid in the parent, 0 in the child
  */
-NO_PAC_FUNC
 static jint com_android_internal_os_Zygote_nativeForkApp(JNIEnv* env,
                                                          jclass,
                                                          jint read_pipe_fd,
@@ -2186,7 +2166,6 @@ static jint com_android_internal_os_Zygote_nativeForkApp(JNIEnv* env,
                             args_known == JNI_TRUE, is_priority_fork == JNI_TRUE, true);
 }
 
-NO_PAC_FUNC
 int zygote::forkApp(JNIEnv* env,
                     int read_pipe_fd,
                     int write_pipe_fd,
