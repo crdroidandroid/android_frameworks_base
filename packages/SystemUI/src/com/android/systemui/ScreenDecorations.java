4b94bfd7a3e01ada46aa24fbe1d42a4d50eabcc6
Pranav Vashi <neobuddy89@gmail.com>
2021-04-13 11:48:16 +0530

    ScreenDecorations: Apply cutout on lockscreen
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
+++ packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
@@ -484,7 +484,7 @@ public class ScreenDecorations extends SystemUI implements Tunable,
 
         lp.setTitle(getWindowTitleByPos(pos));
         lp.gravity = getOverlayWindowGravity(pos);
-        if (mImmerseMode && !mFullscreenMode) {
+        if (mImmerseMode && (!mFullscreenMode || mStatusBar.isKeyguardShowing())) {
             lp.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER;
         } else {
             lp.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
@@ -1322,11 +1322,6 @@ public class ScreenDecorations extends SystemUI implements Tunable,
         }
     }
 
-    private boolean inFullscreenMode() {
-        if (mStatusBar == null) mStatusBar = Dependency.get(StatusBar.class);
-        return mStatusBar != null && mStatusBar.inFullscreenMode();
-    }
-
     private void updateAllForCutout() {
         onTuningChanged(SIZE, null);
         updateLayoutParams();
@@ -1399,9 +1394,8 @@ public class ScreenDecorations extends SystemUI implements Tunable,
 
     @Override
     public void onFullscreenStateChanged(boolean isFullscreen, boolean isImmersive) {
-        boolean fullscreenMode = inFullscreenMode();
-        if (fullscreenMode == mFullscreenMode) return;
-        mFullscreenMode = fullscreenMode;
+        if (isFullscreen == mFullscreenMode) return;
+        mFullscreenMode = isFullscreen;
         if (!mHandler.getLooper().isCurrentThread()) {
             mHandler.post(() -> updateLayoutParams());
         } else {
