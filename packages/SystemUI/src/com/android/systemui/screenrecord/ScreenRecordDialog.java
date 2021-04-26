086d12236bf71f5dcbe391ba23798a6fe9b2a72a
Ido Ben-Hur <idoybh2@gmail.com>
2021-04-26 14:49:39 +0300

    Screenrecord: Save and load set preferences
    It's very annoying setting em back for each recording
    Save preferences on start, load back when dialog opens
    
    Change-Id: Ic8318cdb72a64da8eeb7d0966d45314d450d2d39
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
@@ -34,6 +34,7 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.android.systemui.Prefs;
 import com.android.systemui.R;
 import com.android.systemui.settings.UserContextProvider;
 
@@ -49,6 +50,12 @@ public class ScreenRecordDialog extends Activity {
     private static final long DELAY_MS = 3000;
     private static final long INTERVAL_MS = 1000;
     private static final String TAG = "ScreenRecordDialog";
+    private static final String PREFS = "screenrecord_";
+    private static final String PREF_TAPS = "show_taps";
+    private static final String PREF_DOT = "show_dot";
+    private static final String PREF_LOW = "use_low_quality";
+    private static final String PREF_AUDIO = "use_audio";
+    private static final String PREF_AUDIO_SOURCE = "audio_source";
 
     private final RecordingController mController;
     private final UserContextProvider mUserContextProvider;
@@ -109,10 +116,13 @@ public class ScreenRecordDialog extends Activity {
         mOptions.setOnItemClickListenerInt((parent, view, position, id) -> {
             mAudioSwitch.setChecked(true);
         });
+
+        loadPrefs();
     }
 
     private void requestScreenCapture() {
         Context userContext = mUserContextProvider.getUserContext();
+        savePrefs();
         boolean showTaps = mTapsSwitch.isChecked();
         boolean showStopDot = mStopDotSwitch.isChecked();
         boolean lowQuality = mLowQualitySwitch.isChecked();
@@ -130,5 +140,23 @@ public class ScreenRecordDialog extends Activity {
                 RecordingService.getStopIntent(userContext),
                 PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         mController.startCountdown(DELAY_MS, INTERVAL_MS, startIntent, stopIntent);
+    }
+
+    private void savePrefs() {
+        Context userContext = mUserContextProvider.getUserContext();
+        Prefs.putInt(userContext, PREFS + PREF_TAPS, mTapsSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_DOT, mStopDotSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_LOW, mLowQualitySwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_AUDIO, mAudioSwitch.isChecked() ? 1 : 0);
+        Prefs.putInt(userContext, PREFS + PREF_AUDIO_SOURCE, mOptions.getSelectedItemPosition());
+    }
+
+    private void loadPrefs() {
+        Context userContext = mUserContextProvider.getUserContext();
+        mTapsSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_TAPS, 0) == 1);
+        mStopDotSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_DOT, 0) == 1);
+        mLowQualitySwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_LOW, 0) == 1);
+        mAudioSwitch.setChecked(Prefs.getInt(userContext, PREFS + PREF_AUDIO, 0) == 1);
+        mOptions.setSelection(Prefs.getInt(userContext, PREFS + PREF_AUDIO_SOURCE, 0));
     }
 }
