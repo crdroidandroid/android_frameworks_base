5c969282850b6dd4e6172f943bf035fcd3b461dc
Danny Lin <danny@kdrag0n.dev>
2021-05-05 02:01:39 -0700

    PixelPropsUtils: Make build property spoofing more reliable
    Determining the package name from Zygote's post-fork process name is
    unreliable for several reasons:
      - If USAP is enabled, Zygote will fork with the USAP process name, and
        spoofing will never trigger when the forked process specializes into
        a specific app.
      - With the native fork loop backported from AOSP master,
        Zygote.setAppProcessName() is no longer used for most apps because
        the forking is handled by native code.
    
    Instrumentation#newApplication() is responsible for creating the
    Application singleton during each app's initialization, so hook it
    instead for a more reliable and robust trigger.
    
    Co-authored-by: althafvly <althafvly@gmail.com>
    Change-Id: Ic762d8853967bf78db614efaf46166e527cb397a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -64,6 +64,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.crdroid.PixelPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1188,6 +1190,8 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        String packageName = app.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1205,6 +1209,8 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        String packageName = app.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
 
