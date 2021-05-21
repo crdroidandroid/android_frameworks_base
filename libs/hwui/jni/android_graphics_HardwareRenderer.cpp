92d522c82272e9c7cfde73342810997f732110fe
Brett Chabot <brettchabot@google.com>
2021-05-21 08:41:58 -0700

    Backport 'Add ability to disable GL draws'.
    This feature is intended for use in headless test environments,
    where disabling drawing has minimal compatibility impact and can
    save significant resource usage when running UI tests.
    
    This feature is controlled via a new system property
    'debug.hwui.drawing_enabled', and a HardwareRenderer API that can
    override the property value on a per-process basis.
    
    Bug: 201691826
    Test: boot emulator
    Merged-In: I93c21e680382e03342f235dbf58bd7a5c8a6f767
    Change-Id: I391c841e3a0cdcfcc6c7910a316a4dd5e970547d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/hwui/jni/android_graphics_HardwareRenderer.cpp
+++ libs/hwui/jni/android_graphics_HardwareRenderer.cpp
@@ -838,6 +838,14 @@ static void android_view_ThreadedRenderer_initDisplayInfo(JNIEnv*, jclass, jint 
     DeviceInfo::setPresentationDeadlineNanos(presentationDeadlineNanos);
 }
 
+static void android_view_ThreadedRenderer_setDrawingEnabled(JNIEnv*, jclass, jboolean enabled) {
+    Properties::setDrawingEnabled(enabled);
+}
+
+static jboolean android_view_ThreadedRenderer_isDrawingEnabled(JNIEnv*, jclass) {
+    return Properties::isDrawingEnabled();
+}
+
 // ----------------------------------------------------------------------------
 // HardwareRendererObserver
 // ----------------------------------------------------------------------------
@@ -976,6 +984,9 @@ static const JNINativeMethod gMethods[] = {
         {"preload", "()V", (void*)android_view_ThreadedRenderer_preload},
         {"isWebViewOverlaysEnabled", "()Z",
          (void*)android_view_ThreadedRenderer_isWebViewOverlaysEnabled},
+        {"nSetDrawingEnabled", "(Z)V", (void*)android_view_ThreadedRenderer_setDrawingEnabled},
+        {"nIsDrawingEnabled", "()Z", (void*)android_view_ThreadedRenderer_isDrawingEnabled},
+
 };
 
 static JavaVM* mJvm = nullptr;
