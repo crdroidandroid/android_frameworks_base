61f988816183bda923cffbbd203a9a995b6515de
Pranav Vashi <neobuddy89@gmail.com>
2021-05-28 13:58:55 +0530

    Power menu background opacity [1/2]
    * Originally authored by @Laker87, adding separate tunable for it.
    * Commit: https://github.com/crdroidandroid/android_frameworks_base/commit/2c3cbf5592f60b4b62ccd29311d1a2fe8b3983db
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5124,6 +5124,12 @@ public final class Settings {
         public static final String NOTIFICATION_BG_ALPHA = "notification_bg_alpha";
 
         /**
+         * Whether to change the transparency of the power menu
+         * @hide
+         */
+        public static final String POWER_MENU_BG_ALPHA = "power_menu_bg_alpha";
+
+        /**
          * Whether to change the transparency of the qs status bar
          * @hide
          */
