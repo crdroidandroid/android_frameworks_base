ec78c9cff0f3d0d0ac79059d627b70ac28342339
Divyanand Rangu <drangu@codeaurora.org>
2021-06-14 20:13:36 +0530

    appcompaction: Enable system compaction at bootup
    Enable system compaction at bootup finish and
    change cachedappoptimzer thread priority to
    Background by default.
    
    CRs-Fixed: 2942767
    
    Change-Id: Ib4820c579ea2b03f702a7947cb8dff2945ae38e4
    [@neobuddy89: Remove unused part to change priority via deviceconfig]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -2368,7 +2368,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                     Process.THREAD_GROUP_SYSTEM);
             Process.setThreadGroupAndCpuset(
                     mOomAdjuster.mCachedAppOptimizer.mCachedAppOptimizerThread.getThreadId(),
-                    Process.THREAD_GROUP_SYSTEM);
+                    mOomAdjuster.mCachedAppOptimizer.mCompactionPriority);
         } catch (Exception e) {
             Slog.w(TAG, "Setting background thread cpuset failed");
         }
@@ -4801,6 +4801,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                                 String data, Bundle extras, boolean ordered,
                                 boolean sticky, int sendingUser) {
                             synchronized (mProcLock) {
+                                mOomAdjuster.mCachedAppOptimizer.compactAllSystem();
                                 mAppProfiler.requestPssAllProcsLPr(
                                         SystemClock.uptimeMillis(), true, false);
                             }
