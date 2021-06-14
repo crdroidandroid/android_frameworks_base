BlissRoms/platform_frameworks_base

base: Add device key action to Clear all notifications [1/3]
Change-Id: I44e4de691717db2a9277ded64831f3ece9aae687
Signed-off-by: rohan <purohit.rohan@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1957,6 +1957,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case VOLUME_PANEL:
                 BlissUtils.toggleVolumePanel(mContext);
                 break;
+            case CLEAR_ALL_NOTIFICATIONS:
+                BlissUtils.clearAllNotifications();
+                break;
             default:
                 break;
         }

crdroidandroid/android_frameworks_base

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1953,6 +1953,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case VOLUME_PANEL:
                 Utils.toggleVolumePanel(mContext);
                 break;
+            case CLEAR_ALL_NOTIFICATIONS:
+                Utils.clearAllNotifications();
+                break;
             default:
                 break;
         }