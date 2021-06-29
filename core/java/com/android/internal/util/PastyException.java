28707158cb70a5a75be988dec1e318331880d92f
Joey Huab <joey@evolution-x.org>
2021-06-30 00:03:39 +0900

    base: Switch to pasty to upload crash information
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/PastyException.java
+++ core/java/com/android/internal/util/PastyException.java
@@ -16,11 +16,11 @@
 
 package com.android.internal.util;
 
-public class DogbinException extends Exception {
+public class PastyException extends Exception {
 
     private static final long serialVersionUID = 666L;
 
-	public DogbinException(String message) {
+	public PastyException(String message) {
         super(message);
     }
 }
