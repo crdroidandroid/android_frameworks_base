82abd30f0b0273612e2c5cb0d93832336d9ae8ac
cjybyjk <cjybyjk@zjnu.edu.cn>
2021-07-07 22:07:55 +0800

    SystemUI: Export RecordingService for External Usage
    @neobuddy89: Adapted to 13
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingController.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingController.java
@@ -149,7 +149,7 @@ public class RecordingController
             return new ScreenCaptureDisabledDialog(mContext);
         }
 
-        return flags.isEnabled(Flags.WM_ENABLE_PARTIAL_SCREEN_SHARING)
+        return flags != null && flags.isEnabled(Flags.WM_ENABLE_PARTIAL_SCREEN_SHARING)
                 ? new ScreenRecordPermissionDialog(context,  getHostUserHandle(), this,
                     activityStarter, dialogLaunchAnimator, mUserContextProvider,
                     onStartRecordingClicked)
