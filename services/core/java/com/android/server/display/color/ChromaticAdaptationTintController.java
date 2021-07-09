34932cbc6e51401f573433aa09b5e7cf61d5412c
Danny Lin <danny@kdrag0n.dev>
2021-07-08 18:43:03 -0700

    display: ChromaticAdaptation: Fix matrix formatting in logs
    Google specified 16 columns for the 4x4 transformation matrix, causing
    the entire thing to get printed on a single line. Fix it to improve
    readability.
    
    Change-Id: I99a5bb35694d0703db3bf806af2a632dc021e5f6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/display/color/ChromaticAdaptationTintController.java
+++ services/core/java/com/android/server/display/color/ChromaticAdaptationTintController.java
@@ -131,6 +131,7 @@ abstract class ChromaticAdaptationTintController extends TintController {
         // Adapt the display's nominal white point to match the requested CCT value
         mCurrentTargetXYZ = targetXyz;
 
+        // XYZ -> LMS -> [CAT] -> LMS -> XYZ
         mChromaticAdaptationMatrix =
                 ColorSpace.chromaticAdaptation(ColorSpace.Adaptation.CAT16,
                         mDisplayNominalWhiteXYZ, mCurrentTargetXYZ);
@@ -161,7 +162,7 @@ abstract class ChromaticAdaptationTintController extends TintController {
 
         String xyzString = "[" + targetXyz[0] + "," + targetXyz[1] + "," + targetXyz[2] + "]";
         Slog.d(ColorDisplayService.TAG, "setChromaticAdaptationMatrix: xyz = " + xyzString
-                + " matrix = " + matrixToString(mMatrix, 16));
+                + " matrix = " + matrixToString(mMatrix, 4));
     }
 
     @Override
