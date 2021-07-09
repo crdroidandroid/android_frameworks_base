114a434e146291dd42e4dd33314234852953aef0
Wileen Chiu <wileenc@codeaurora.org>
2021-07-09 13:13:52 -0700

    Catch IllegalArgumentException for invalid phone id
    - CarrierConfigLoader#updateConfigForPhoneId throws
      an IllegalArgumentException when receiving an update
      for a phone id that may not no longer be in use
    - Catch the exception instead and log the information
    
    Change-Id: I167a253ae1fc804c408009ba4a6a4bbaa1eba15a
    CRs-Fixed: 2978083
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- telephony/java/android/telephony/CarrierConfigManager.java
+++ telephony/java/android/telephony/CarrierConfigManager.java
@@ -6118,7 +6118,7 @@ public class CarrierConfigManager {
                 return;
             }
             loader.updateConfigForPhoneId(phoneId, simState);
-        } catch (RemoteException ex) {
+        } catch (RemoteException | IllegalArgumentException ex) {
             Rlog.e(TAG, "Error updating config for phoneId=" + phoneId + ": " + ex.toString());
         }
     }
