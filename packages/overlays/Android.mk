675629db93eae7d2ed3748225b14d018b7c50004
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-07-24 17:13:07 +0530

    overlays: Add an OxygenOS icon pack
    @neobuddy89:
    * Clean up unused resources (like dual volte slot, brightness high/low)
    * Do not change code for drawables. Make drawables compatible instead.
    
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -56,6 +56,8 @@ LOCAL_REQUIRED_MODULES := \
     IconPackRoundedSettingsOverlay \
     IconPackRoundedSystemUIOverlay \
     IconPackRoundedThemePickerOverlay \
+    IconPackOOSAndroidOverlay \
+    IconPackOOSSystemUIOverlay \
     IconShapeCylinderOverlay \
     IconShapeHeartOverlay \
     IconShapeHexagonOverlay \
