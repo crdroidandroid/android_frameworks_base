a2994d097cf236efb3c1c88e6e27695db57f37a4
1582130940 <19632674+1582130940@users.noreply.github.com>
2021-08-09 03:35:07 +0200

    fwb: Disable some debug/log/trace
    * ResourceTypes: Don't spam logcat
    fusionjack <dogfight60-fusionjack@yahoo.de>
    
    * GpsNetInitiatedHandler: Disable More Debugging
    Chet Kener <Cl3Kener@gmail.com>
    
    * Ringtone/DeadZone: Turn off some debugs
    AndroidRul3z <ferrettidario@gmail.com>
    
    * BluetoothAdapter: Disable logspew
    Alexander Martinz <eviscerationls@gmail.com>
    
    * base: Kill some log spam
    * SystemUI: Disable KeyguardConstants DEBUG
    * ViewRootImpl: Log spill when dragging
    maxwen <max.weninger@gmail.com>
    
    * BluetoothUtils: Disable useless logspam
    * AlarmManagerService: Reduce log level wtf -> w
    ?UTF-8?q?Sim=C3=A3o=20Gomes=20Viana?= <devel@superboring.dev>
    
    * InterruptionStateProvider: Disable debugging
    jhonboy121 <alfredmathew05@gmail.com>
    
    * DEBUG: Turn off some debugging we don't need.
    mydongistiny <jaysonedson@gmail.com>
    
    * DefaultPermissionGrantPolicy: Silence harmless errors
    Logcat spams with google packages not installed. Kill it.
    
    * InputMethodUtils: Fix system bootloop when no IME found
    In case of faulty gapps, there are no default IME left on the system.
    This shouldn't be situation where user cannot boot at all.
    Handle InputMethodUtils to prevent PackageManagerService crashing system.
    * SystemUI: Remove trace counter from ScrimState
    updateScrimColor is expensive
    Pranav Vashi <neobuddy89@gmail.com>
    
    * services: LightsService: Mute an annoying error message.
    E LightsService: Light requested not available on this device.
    If you google for the error you will find many discussion threads or logcats
    that show this error. Since the error only describes that the device does
    not support this function, the error can be classified as harmless. In order
    not to confuse the user, we will now hide the logging of this error.
    spezi77 <spezi7713@gmx.net>
    
    * StatusIconContainer: Remove duplicate import
    utzcoz <utzcoz@gmail.com>
    
    * Settingslib: Remove a reflect method that can never succeed
    Stops spamming logcat with  java.lang.ClassNotFoundException
    The target class doesn't exist anywhere
    Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
    
    * ActivityThread: Remove Failed to find provider info logspam
    Immanuel Raj <immanuelr44@gmail.com>
    
    Co-authored-by: fusionjack <dogfight60-fusionjack@yahoo.de>
    Co-authored-by: Chet Kener <Cl3Kener@gmail.com>
    Co-authored-by: AndroidRul3z <ferrettidario@gmail.com>
    Co-authored-by: Alexander Martinz <eviscerationls@gmail.com>
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: ?UTF-8?q?Sim=C3=A3o=20Gomes=20Viana?= <devel@superboring.dev>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: mydongistiny <jaysonedson@gmail.com>
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: utzcoz <utzcoz@gmail.com>
    Co-authored-by: Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
    Co-authored-by: Immanuel Raj <immanuelr44@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -7324,7 +7324,6 @@ public final class ViewRootImpl implements ViewParent,
         }
         if (x < 0 || x >= mView.getWidth() || y < 0 || y >= mView.getHeight()) {
             // E.g. when moving window divider with mouse
-            Slog.d(mTag, "updatePointerIcon called with position out of bounds");
             return false;
         }
 
