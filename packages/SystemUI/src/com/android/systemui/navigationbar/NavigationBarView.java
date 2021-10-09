7b043450c3318482720c66621e54fdd4077bf683
Park Ju Hyung <qkrwngud825@gmail.com>
2021-10-09 03:22:39 -0400

    SystemUI: Implement burn-in protection for status/navbar
    Devices with OLED display suffer from
    status-bar's notification items and nagivation bar's software keys
    causing permanent burn-ins when used long-term.
    
    Moving all items in the area
    both horizontally and vertically workarounds this problem.
    
    SystemUI: rework statusbar burn-in protection controller
    
    * Turns out that this controller was instantiated twice resulting in two timers running simultaneously which resulted in views to shift abruptly.
      Since the shift amount was too low it was not noticeable at all. So now we instantiate it once with all final dependencies and inject PhoneStatusBarView in fragment transaction.
    * Finalized many instance variables and a reference to the main handler is kept instead of creating new ones in each cycle
    * simplified / generalised the shift algorithm a bit so that it's easily configurable
    * added a callback to reload shift vars on screen density changes
    * additional changes:
    * use the same controller for navigation handle, saves some cpu time
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    SystemUI: inject BurnInProtectionController
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]: use the scoped SysUISingleton annotation
    
    SystemUI: BurnInProtectionController: rewrite in kotlin and improvements
    * ditched TimerTask in favor of coroutines
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    [jhonboy121]:
    * adapted to A13
    * use BurnInHelper util functions for calculating offset
    
    [ghostrider-reborn]:
    * use NavigationBarView directly instead of CentralSurfaces
    
    Squashed:
    
        From: jhonboy121 <alfredmathew05@gmail.com>
        Date: Sun, 11 Sep 2022 20:36:57 +0530
        Subject: SystemUI: BurnInProtectionController: offset less aggressively
    
        Change-Id: Ib37b0fde6edfc34cad8876d2e01ba4f37f323036
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
        Date: Wed, 2 Aug 2023 20:06:20 +0200
        Subject: SystemUI: Make setNavigationBarView and setPhoneStatusBarView nullable
    
         * this fixes a potential npe on devices without navbar or statusbar
    
        Change-Id: Ia8e0ff844e24f67685ba20ac61a88d3256c9c648
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -74,6 +74,7 @@ import com.android.systemui.navigationbar.buttons.KeyButtonDrawable;
 import com.android.systemui.navigationbar.buttons.NearestTouchFrame;
 import com.android.systemui.navigationbar.buttons.RotationContextButton;
 import com.android.systemui.navigationbar.gestural.EdgeBackGestureHandler;
+import com.android.systemui.navigationbar.gestural.NavigationHandle;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.settings.DisplayTracker;
 import com.android.systemui.shade.ShadeViewController;
@@ -85,6 +86,7 @@ import com.android.systemui.statusbar.phone.AutoHideController;
 import com.android.systemui.statusbar.phone.CentralSurfaces;
 import com.android.systemui.statusbar.phone.LightBarTransitionsController;
 import com.android.systemui.tuner.TunerService;
+import com.android.systemui.statusbar.policy.Offset;
 import com.android.wm.shell.back.BackAnimation;
 import com.android.wm.shell.pip.Pip;
 
@@ -193,6 +195,9 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
     private boolean mImeVisible;
 
     private boolean mBlockedGesturalNavigation;
+
+    @Nullable
+    private ViewGroup mNavigationBarContents = null;
 
     private class NavTransitionListener implements TransitionListener {
         private boolean mBackTransitioning;
@@ -905,11 +910,30 @@ public class NavigationBarView extends FrameLayout implements TunerService.Tunab
         mContextualButtonGroup.setButtonVisibility(R.id.accessibility_button, visible);
     }
 
+    public void offsetNavBar(Offset offset) {
+        if (isGesturalMode(mNavBarMode)) {
+            final NavigationHandle handle = (NavigationHandle) getHomeHandle().getCurrentView();
+            if (handle != null) {
+                handle.setTranslationY(offset.getY());
+                handle.invalidate();
+            }
+            return;
+        }
+        if (mNavigationBarContents == null) {
+            return;
+        }
+        mNavigationBarContents.setTranslationX(offset.getX());
+        mNavigationBarContents.setTranslationY(offset.getY());
+        invalidate();
+    }
+
     @Override
     public void onFinishInflate() {
         super.onFinishInflate();
         mNavigationInflaterView = findViewById(R.id.navigation_inflater);
         mNavigationInflaterView.setButtonDispatchers(mButtonDispatchers);
+
+        mNavigationBarContents = (ViewGroup) findViewById(R.id.nav_buttons);
 
         updateOrientationViews();
         reloadNavIcons();
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -20,6 +20,7 @@ package com.android.systemui.statusbar.phone;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Rect;
 import android.inputmethodservice.InputMethodService;
 import android.os.IBinder;
@@ -53,6 +54,7 @@ import com.android.systemui.shared.rotation.RotationButtonController;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.CommandQueue.Callbacks;
 import com.android.systemui.statusbar.phone.userswitcher.StatusBarUserSwitcherContainer;
+import com.android.systemui.statusbar.policy.Offset;
 import com.android.systemui.user.ui.binder.StatusBarUserChipViewBinder;
 import com.android.systemui.user.ui.viewmodel.StatusBarUserChipViewModel;
 import com.android.systemui.util.leak.RotationUtils;
@@ -77,6 +79,8 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
     private int mStatusBarHeight;
     @Nullable
     private Gefingerpoken mTouchEventHandler;
+    @Nullable
+    private ViewGroup mStatusBarContents = null;
 
     /**
      * Draw this many pixels into the left/right side of the cutout to optimally use the space
@@ -144,12 +148,22 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
         StatusBarUserChipViewBinder.bind(container, viewModel);
     }
 
+    public void offsetStatusBar(Offset offset) {
+        if (mStatusBarContents == null) {
+            return;
+        }
+        mStatusBarContents.setTranslationX(offset.getX());
+        mStatusBarContents.setTranslationY(offset.getY());
+        invalidate();
+    }
+
     @Override
     public void onFinishInflate() {
         super.onFinishInflate();
         mBattery = findViewById(R.id.battery);
         mClockController = new ClockController(getContext(), this);
         mCutoutSpace = findViewById(R.id.cutout_space_view);
+        mStatusBarContents = (ViewGroup) findViewById(R.id.status_bar_contents);
 
         updateResources();
     }
@@ -308,7 +322,7 @@ public class PhoneStatusBarView extends FrameLayout implements Callbacks {
         int statusBarPaddingStart = getResources().getDimensionPixelSize(
                 R.dimen.status_bar_padding_start);
 
-        findViewById(R.id.status_bar_contents).setPaddingRelative(
+        mStatusBarContents.setPaddingRelative(
                 statusBarPaddingStart,
                 getResources().getDimensionPixelSize(R.dimen.status_bar_padding_top),
                 getResources().getDimensionPixelSize(R.dimen.status_bar_padding_end),
