b0efa1063be3c7139d1ddc292a380406a1450c59
Andrzej Perczak <linux@andrzejperczak.com>
2021-10-10 22:01:39 +0200

    Revert "DragSurface: destroy surfacecontrol in view site"
    It is already present with a check, this old patch may cause issues.
    
    This reverts commit ba9b2b57db687969adc41a4f8724aea8e911b217.

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -26413,7 +26413,6 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                 surface.destroy();
             }
             session.kill();
-            surface.destroy();
         }
     }
 
