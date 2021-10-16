3772ffcf5c551d5ed8bba1da6968ccdcb6fc0d05
Danny Lin <danny@kdrag0n.dev>
2021-10-16 05:27:57 -0700

    base: Add support for app signature spoofing
    This is needed by microG GmsCore to pretend to be the official Google
    Play Services package, because client apps check the package signature
    to make sure it matches Google's official certificate.
    
    This was forward-ported from the Android 10 patch by gudenau:
    https://github.com/microg/android_packages_apps_GmsCore/pull/957
    
    Changes made for Android 11:
      - Updated PackageInfo calls
      - Added new permission to public API surface, needed for
        PermissionController which is now an updatable APEX on 11
      - Added a dummy permission group to allow users to manage the
        permission through the PermissionController UI
        (by Vachounet <vachounet@live.fr>)
      - Updated location provider comment for conciseness
    
    Changes made for Android 12:
      - Moved mayFakeSignature into lock-free Computer subclass
      - Always get permissions for packages that request signature spoofing
        (otherwise permissions are usually ommitted and thus the permission
        check doesn't work properly)
      - Optimize mayFakeSignature check order to improve performance
    
    Link: https://github.com/microg/GmsCore/pull/1586
    Change-Id: Ied7d6ce0b83a2d2345c3abba0429998d86494a88
    [@neobuddy89: Adapted for A13]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -1603,6 +1603,29 @@ public class ComputerEngine implements Computer {
         return result;
     }
 
+    private boolean requestsFakeSignature(AndroidPackage p) {
+        return p.getMetaData() != null &&
+                p.getMetaData().getString("fake-signature") != null;
+    }
+
+    private PackageInfo mayFakeSignature(AndroidPackage p, PackageInfo pi,
+            Set<String> permissions) {
+        try {
+            if (p.getMetaData() != null &&
+                    p.getTargetSdkVersion() > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                String sig = p.getMetaData().getString("fake-signature");
+                if (sig != null &&
+                        permissions.contains("android.permission.FAKE_PACKAGE_SIGNATURE")) {
+                    pi.signatures = new Signature[] {new Signature(sig)};
+                }
+            }
+        } catch (Throwable t) {
+            // We should never die because of any failures, this is system code!
+            Log.w("ComputerEngine.FAKE_PACKAGE_SIGNATURE", t);
+        }
+        return pi;
+    }
+
     public final PackageInfo generatePackageInfo(PackageStateInternal ps,
             @PackageManager.PackageInfoFlagsBits long flags, int userId) {
         if (!mUserManager.exists(userId)) return null;
@@ -1632,13 +1655,14 @@ public class ComputerEngine implements Computer {
             final int[] gids = (flags & PackageManager.GET_GIDS) == 0 ? EMPTY_INT_ARRAY
                     : mPermissionManager.getGidsForUid(UserHandle.getUid(userId, ps.getAppId()));
             // Compute granted permissions only if package has requested permissions
-            final Set<String> permissions = ((flags & PackageManager.GET_PERMISSIONS) == 0
+            final Set<String> permissions = (((flags & PackageManager.GET_PERMISSIONS) == 0
+                    && !requestsFakeSignature(p))
                     || ArrayUtils.isEmpty(p.getRequestedPermissions())) ? Collections.emptySet()
                     : mPermissionManager.getGrantedPermissions(ps.getPackageName(), userId);
 
-            PackageInfo packageInfo = PackageInfoUtils.generate(p, gids, flags,
-                    state.getFirstInstallTime(), ps.getLastUpdateTime(), permissions, state, userId,
-                    ps);
+            PackageInfo packageInfo = mayFakeSignature(p, PackageInfoUtils.generate(p, gids, flags,
+                    state.getFirstInstallTime(), ps.getLastUpdateTime(), permissions, state, userId, ps),
+                    permissions);
 
             if (packageInfo == null) {
                 return null;
