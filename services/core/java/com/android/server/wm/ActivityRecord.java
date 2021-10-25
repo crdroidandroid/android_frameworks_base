394155d423b0a3a1546b8babb698b29fdbbbe86d
ot903107 <ot_zhenhua.deng@mediatek.com>
2021-10-25 18:27:30 +0800

    Fix task info null crash
    Timing issue due to SystemUI died and idle state delay, When process
    the finishing activity and prepare destroy the activity, add a judge
    if it’s task is null.
    
    Bug:
    Test: Manual
    
    Change-Id: I513ebcb5257272ce16f8314ffbca2f23d3d66216
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -3755,6 +3755,10 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
             return false;
         }
 
+        if (task == null){
+            return false;
+        }
+
         EventLogTags.writeWmDestroyActivity(mUserId, System.identityHashCode(this),
                 task.mTaskId, shortComponentName, reason);
 
