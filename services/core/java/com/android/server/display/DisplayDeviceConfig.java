c41734f8447877f0e0474602f52c4c90943dbd84
SagarMakhar <sagarmakhar@gmail.com>
2021-11-01 15:00:44 +0000

    Make DPC respect overlaid min/max brightness values
    Change-Id: If60e404a9717bc582323230e75533948027c23a3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/display/DisplayDeviceConfig.java
+++ services/core/java/com/android/server/display/DisplayDeviceConfig.java
@@ -272,6 +272,14 @@ public class DisplayDeviceConfig {
         return mBrightnessDefault;
     }
 
+    public float getBacklightMin() {
+        return mBacklightMinimum;
+    }
+
+    public float getBacklightMax() {
+        return mBacklightMaximum;
+    }
+
     public float getBrightnessRampFastDecrease() {
         return mBrightnessRampFastDecrease;
     }
@@ -653,7 +661,7 @@ public class DisplayDeviceConfig {
         for (int i = 0; i < mBrightness.length; i++) {
             mBrightness[i] = MathUtils.map(mBacklight[0],
                     mBacklight[mBacklight.length - 1],
-                    PowerManager.BRIGHTNESS_MIN, PowerManager.BRIGHTNESS_MAX, mBacklight[i]);
+                    mBacklightMinimum, mBacklightMaximum, mBacklight[i]);
         }
         mBrightnessToBacklightSpline = Spline.createSpline(mBrightness, mBacklight);
         mBacklightToBrightnessSpline = Spline.createSpline(mBacklight, mBrightness);
