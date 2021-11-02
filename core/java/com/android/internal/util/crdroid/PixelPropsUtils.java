c8a7941000aae4aad12dacb934f54d88679bb8ed
Danny Lin <danny@kdrag0n.dev>
2021-11-01 20:06:48 -0700

    PixelPropsUtils: Limit SafetyNet workarounds to unstable GMS process
    The unstable process is where SafetyNet attestation actually runs, so
    we only need to spoof the model in that process.
    
    Change-Id: Idcf663907a6c3d0408dbd45b1ac53c9eb4200df8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -17,6 +17,7 @@
 
 package com.android.internal.util.crdroid;
 
+import android.app.Application;
 import android.os.Build;
 import android.os.SystemProperties;
 import android.util.Log;
@@ -217,7 +218,10 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
             if (packageName.equals("com.google.android.gms")) {
-                sIsGms = true;
+                final String processName = Application.getProcessName();
+                if (processName.equals("com.google.android.gms.unstable")) {
+                    sIsGms = true;
+                }
             }
             // Set proper indexing fingerprint
             if (packageName.equals("com.google.android.settings.intelligence")) {
