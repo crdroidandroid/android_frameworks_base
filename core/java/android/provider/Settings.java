282df09bfa015178f43d42b55c1fa7b7918a2268
maxwen <max.weninger@gmail.com>
2021-11-06 20:40:52 +0100

    SystemUI: add enablement settings for OmniJaws keyguard slice provider
    Change-Id: Ic3a386fae8ca63708628cd0b5d5dfe359743e539
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5619,6 +5619,11 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
