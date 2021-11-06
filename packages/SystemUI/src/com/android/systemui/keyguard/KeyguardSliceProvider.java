a13aca5d8bbaa32a6377596022bd219bd85fda4c
maxwen <max.weninger@gmail.com>
2021-11-06 20:40:52 +0100

    SystemUI: add enablement settings for OmniJaws keyguard slice provider
    Change-Id: Ic3a386fae8ca63708628cd0b5d5dfe359743e539
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -25,6 +25,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
 import android.graphics.Typeface;
 import android.graphics.drawable.Icon;
 import android.icu.text.DateFormat;
@@ -157,6 +158,8 @@ public class KeyguardSliceProvider extends SliceProvider implements
     protected final Uri mWeatherUri;
     private OmniJawsClient mWeatherClient;
     private OmniJawsClient.WeatherInfo mWeatherData;
+    private SettingsObserver mSettingsObserver;
+    private boolean mShowWeatherSlice;
 
     /**
      * Receiver responsible for time ticking and updating the date format.
@@ -195,6 +198,35 @@ public class KeyguardSliceProvider extends SliceProvider implements
                     }
                 }
             };
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_ENABLED), false, this,
+                    UserHandle.USER_ALL);
+            updateShowWeatherSlice();
+        }
+
+        void unobserve() {
+            mContentResolver.unregisterContentObserver(this);
+        }
+
+        void updateShowWeatherSlice() {
+            mShowWeatherSlice = Settings.System.getIntForUser(mContentResolver,
+                    Settings.System.LOCKSCREEN_WEATHER_ENABLED,
+                    0, UserHandle.USER_CURRENT) != 0;
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateShowWeatherSlice();
+            notifyChange();
+        }
+    }
 
     public static KeyguardSliceProvider getAttachedInstance() {
         return KeyguardSliceProvider.sInstance;
@@ -290,7 +322,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
     }
 
     protected void addWeatherLocked(ListBuilder builder) {
-        if (!mWeatherClient.isOmniJawsEnabled() || mWeatherData == null) {
+        if (!mShowWeatherSlice || !mWeatherClient.isOmniJawsEnabled() || mWeatherData == null) {
             return;
         }
         IconCompat weatherIcon = SliceViewUtil.createIconFromDrawable(mWeatherClient.getWeatherConditionImage(mWeatherData.conditionCode));
@@ -345,6 +377,8 @@ public class KeyguardSliceProvider extends SliceProvider implements
             KeyguardSliceProvider.sInstance = this;
             registerClockUpdate();
             updateClockLocked();
+            mSettingsObserver = new SettingsObserver(mHandler);
+            mSettingsObserver.observe();
             enableWeatherUpdates();
         }
         return true;
@@ -363,6 +397,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 getContext().unregisterReceiver(mIntentReceiver);
             }
             disableWeatherUpdates();
+            mSettingsObserver.unobserve();
             KeyguardSliceProvider.sInstance = null;
         }
     }
@@ -585,7 +620,9 @@ public class KeyguardSliceProvider extends SliceProvider implements
     }
 
     private void disableWeatherUpdates() {
-        mWeatherClient.removeObserver(this);
+        if (mWeatherClient != null) {
+            mWeatherClient.removeObserver(this);
+        }
     }
 
     @Override
@@ -612,8 +649,10 @@ public class KeyguardSliceProvider extends SliceProvider implements
     }
 
     private void queryAndUpdateWeather() {
-        mWeatherClient.queryWeather();
-        mWeatherData = mWeatherClient.getWeatherInfo();
-        notifyChange();
+        if (mWeatherClient != null) {
+            mWeatherClient.queryWeather();
+            mWeatherData = mWeatherClient.getWeatherInfo();
+            notifyChange();
+        }
     }
 }
