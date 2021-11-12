5970e453562acb1ddc0469314a13f5515bd2c3ad
acras01 <acras1@gmail.com>
2021-11-12 08:14:16 +0200

    Allow toggling combined signal icons [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5275,6 +5275,12 @@ public final class Settings {
         public static final String DATA_DISABLED_ICON = "data_disabled_icon";
 
         /**
+         * Whether to combine wifi and signal icons
+         * @hide
+         */
+        public static final String COMBINED_STATUS_BAR_SIGNAL_ICONS = "combined_status_bar_signal_icons";
+
+        /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
          */
