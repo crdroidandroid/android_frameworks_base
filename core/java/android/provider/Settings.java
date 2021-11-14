22face8233ce4716e24d05c4283744735f2d93be
jhonboy121 <alfredmathew05@gmail.com>
2021-11-14 13:23:32 +0530

    base: Allow toggling screen off FOD [1/2]
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 17 Sep 2023 12:39:34 +0530
        Subject: udfps: Clean up for screen off FOD
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I1feece846e4dbddb276ad6ed0efac77e5b7986d8
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11930,6 +11930,13 @@ public final class Settings {
         public static final String ENABLE_PROJECTION_PRIVACY_INDICATOR = "enable_projection_privacy_indicator";
 
         /**
+         * Enable udfps detection even when screen is off
+         * Default value is 0
+         * @hide
+         */
+        public static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
