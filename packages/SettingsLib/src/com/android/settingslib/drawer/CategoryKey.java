f46b6a58dc88ef9a268f17e6317f1a89ad0a9521
Pranav Vashi <neobuddy89@gmail.com>
2021-11-25 01:24:47 +0530

    crdroid: Adapt settings for A12
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -64,6 +64,8 @@ public final class CategoryKey {
             "com.android.settings.category.ia.battery_saver_settings";
     public static final String CATEGORY_SMART_BATTERY_SETTINGS =
             "com.android.settings.category.ia.smart_battery_settings";
+    public static final String CATEGORY_CRDROID =
+            "com.android.settings.category.ia.crdroid";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
