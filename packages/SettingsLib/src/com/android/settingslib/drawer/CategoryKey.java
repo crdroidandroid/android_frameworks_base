4d786c1df6bdaa886a917bfa5bf05699b3aeb293
Pranav Vashi <neobuddy89@gmail.com>
2021-11-25 01:24:47 +0530

    crdroid: Adapt settings for A16
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
+++ packages/SettingsLib/src/com/android/settingslib/drawer/CategoryKey.java
@@ -74,6 +74,8 @@ public final class CategoryKey {
             "com.android.settings.category.ia.more_security_privacy_settings";
     public static final String CATEGORY_SUPERVISION =
             "com.android.settings.category.ia.supervision";
+    public static final String CATEGORY_CRDROID =
+            "com.android.settings.category.ia.crdroid";
 
     public static final Map<String, String> KEY_COMPAT_MAP;
 
