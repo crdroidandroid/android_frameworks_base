c6b9418ba8e384e7371df2dca3baf6c0fe4763e8
Pranav Vashi <neobuddy89@gmail.com>
2021-12-01 07:55:24 +0530

    Clean up power menu options [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5269,21 +5269,7 @@ public final class Settings {
          *
          * @hide
          */
-        public static final String POWERMENU_RESTART = "powermenu_restart";
-
-         /**
-         * Whether to display reboot in the power menu
-         *
-         * @hide
-         */
         public static final String POWERMENU_ADVANCED = "powermenu_advanced";
-
-         /**
-         * Whether to display power in the power menu
-         *
-         * @hide
-         */
-        public static final String POWERMENU_POWER = "powermenu_power";
 
          /**
          * Whether to display the users option in the power menu
