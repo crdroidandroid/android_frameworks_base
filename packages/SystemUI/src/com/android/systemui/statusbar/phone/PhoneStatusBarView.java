7d2f99e10b527a8b0df7f47a7c7040daf399346a
Pranav Vashi <neobuddy89@gmail.com>
2021-12-11 16:12:08 +0530

    Properly apply dark theming to clock
    * This is forward port from Android 11.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -83,7 +83,6 @@ public class PhoneStatusBarView extends PanelBar implements Callbacks {
         }
     };
     private DarkReceiver mBattery;
-    private ClockController mClockController;
     private int mRotationOrientation = -1;
     private RotationButtonController mRotationButtonController;
     @Nullable
@@ -161,7 +160,6 @@ public class PhoneStatusBarView extends PanelBar implements Callbacks {
     @Override
     public void onFinishInflate() {
         mBattery = findViewById(R.id.battery);
-        mClockController = new ClockController(getContext(), this);
         mCutoutSpace = findViewById(R.id.cutout_space_view);
         mCenterIconSpace = findViewById(R.id.centered_icon_area);
 
@@ -173,7 +171,6 @@ public class PhoneStatusBarView extends PanelBar implements Callbacks {
         super.onAttachedToWindow();
         // Always have Battery meters in the status bar observe the dark/light modes.
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mBattery);
-        mClockController.addDarkReceiver();
         if (updateOrientationAndCutout()) {
             updateLayoutForCutout();
         }
@@ -188,7 +185,6 @@ public class PhoneStatusBarView extends PanelBar implements Callbacks {
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         Dependency.get(DarkIconDispatcher.class).removeDarkReceiver(mBattery);
-        mClockController.removeDarkReceiver();
         mDisplayCutout = null;
 
         if (mRotationButtonController != null) {
@@ -470,9 +466,5 @@ public class PhoneStatusBarView extends PanelBar implements Callbacks {
     @Override
     protected boolean shouldPanelBeVisible() {
         return mHeadsUpVisible || super.shouldPanelBeVisible();
-    }
-
-    public ClockController getClockController() {
-        return mClockController;
     }
 }
