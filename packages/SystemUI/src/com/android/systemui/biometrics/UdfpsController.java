395c383deac2ea0ffdc5357ab5387a445a118175
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    Bring back UDFPS animation
    neobuddy89: Adapted for Android 12L
    Stallix - Adapted for A13 QPR1
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: kondors1995 <normandija1945@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: nullxception <nullxception@users.noreply.github.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: 00day0 <00day0@users.noreply.github.com>
    
    [Dhina17]
    - Drop Udfps icon customization
    - Use ContentObserver instead of TunerService
    
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Change-Id: I0f0fcde998ad3d80f22f76f7136f141331ca6c7c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -232,6 +232,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private final int mUdfpsVendorCode;
     private boolean mScreenOffFod;
 
+    private UdfpsAnimation mUdfpsAnimation;
+
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
             new VibrationAttributes.Builder()
@@ -342,11 +344,17 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         for (Callback cb : mCallbacks) {
                             cb.onFingerDown();
                         }
+                        if (mUdfpsAnimation != null) {
+                            mUdfpsAnimation.show();
+                        }
                     });
                 } else {
                     mFgExecutor.execute(() -> {
                         for (Callback cb : mCallbacks) {
                             cb.onFingerUp();
+                        }
+                        if (mUdfpsAnimation != null) {
+                            mUdfpsAnimation.hide();
                         }
                     });
                 }
@@ -750,13 +758,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mUnlockedScreenOffAnimationController = unlockedScreenOffAnimationController;
         mLatencyTracker = latencyTracker;
         mActivityTransitionAnimator = activityTransitionAnimator;
-        mSensorProps = new FingerprintSensorPropertiesInternal(
-                -1 /* sensorId */,
-                SensorProperties.STRENGTH_CONVENIENCE,
-                0 /* maxEnrollmentsPerUser */,
-                new ArrayList<>() /* componentInfo */,
-                FingerprintSensorProperties.TYPE_UNKNOWN,
-                false /* resetLockoutRequiresHardwareAuthToken */);
+        mSensorProps = findFirstUdfps();
 
         mBiometricExecutor = biometricsExecutor;
         mPrimaryBouncerInteractor = primaryBouncerInteractor;
@@ -821,6 +823,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 }, UserHandle.USER_ALL
             );
         }
+
+        if (com.android.internal.util.crdroid.Utils.isPackageInstalled(mContext,
+                "com.crdroid.udfps.animations")) {
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps);
+        }
     }
 
     /**
@@ -839,6 +846,17 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         + "vibration. Either the controller overlay is null or has no view");
             }
         }
+    }
+
+    @Nullable
+    private FingerprintSensorPropertiesInternal findFirstUdfps() {
+        for (FingerprintSensorPropertiesInternal props :
+                mFingerprintManager.getSensorPropertiesInternal()) {
+            if (props.isAnyUdfpsType()) {
+                return props;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -864,6 +882,11 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
         mOverlay = overlay;
         final int requestReason = overlay.getRequestReason();
+
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.setIsKeyguard(requestReason == REASON_AUTH_KEYGUARD);
+        }
+
         if (requestReason == REASON_AUTH_KEYGUARD
                 && !mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
             Log.d(TAG, "Attempting to showUdfpsOverlay when fingerprint detection"
@@ -1111,6 +1134,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             for (Callback cb : mCallbacks) {
                 cb.onFingerDown();
             }
+            if (mUdfpsAnimation != null) {
+                mUdfpsAnimation.show();
+            }
         }
     }
 
@@ -1150,6 +1176,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             if (isOptical()) {
                 for (Callback cb : mCallbacks) {
                     cb.onFingerUp();
+                }
+                if (mUdfpsAnimation != null) {
+                    mUdfpsAnimation.hide();
                 }
             }
         }
