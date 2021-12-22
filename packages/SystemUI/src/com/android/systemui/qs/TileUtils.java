58e4b90865b97cadedf998acb5ce225cbb8bbe5f
maxwen <max.weninger@gmail.com>
2021-12-23 00:12:37 +0100

    SystemUI: Add QS tile layout settings [1/2]
    @neobuddy89:
    * Removed unused configs and imports
    * De-omnify with TunerService and TileUtils
    * Updated for A14
    
    Squashed:
        From: minarypenguin <alexfinhart@gmail.com>
        Date: Mon, 20 Jun 2022 04:51:23 +0800
        Subject: SystemUI: Fix qs tile vertical layout scrolling
    
        * singleLine forces scrollHorizontally, which doesnt go along with gravity="center"
    
        From: strwbrry143 <alexfinhart@gmail.com>
        Date: Sun, 7 Aug 2022 21:36:39 +0800
        Subject: SystemUI: Tune QS tile dimensions
    
        From: timjosten <timjosten@gmail.com>
        Date: Sun, 21 Aug 2022 07:40:03 +0000
        Subject: SystemUI: Improve vertical QS tile layout & animation
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 28 Aug 2022 14:25:01 +0530
        Subject: SystemUI: Update and fix QS tile layout settings
    
        * Do not let every tom, dick and harry set max columns.
        * On config changes and settings update, check if dynamic row and columns update.
        * Update resources in tile layout when orientation changes to get proper tiles in landscape.
        * Request layout changes whenever column update happens.
        * Be smart in calculating how many max tiles should Quick QS show depending on no. of columns.
        * Clean up init in QSTileViewImpl
    
    Change-Id: I5900af574681958bba46c20e85ed342c3283cabd
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/qs/TileUtils.java
@@ -0,0 +1,72 @@
+/*
+* Copyright (C) 2022-2024 crDroid Android Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.qs;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.systemui.res.R;
+import com.android.systemui.flags.Flags;
+import com.android.systemui.flags.RefactorFlag;
+
+public class TileUtils {
+
+    private static boolean useSmallLandscapeLockscreenResources(Resources res) {
+        return RefactorFlag.forView(Flags.LOCKSCREEN_ENABLE_LANDSCAPE).isEnabled()
+                && res.getBoolean(R.bool.is_small_screen_landscape);
+    }
+
+    public static int getQSColumnsCount(Context context) {
+        Resources res = context.getResources();
+        int columns = useSmallLandscapeLockscreenResources(res)
+                ? res.getInteger(R.integer.small_land_lockscreen_quick_settings_num_columns)
+                : res.getInteger(R.integer.quick_settings_num_columns);
+        return getQSColumnsCount(context, columns);
+    }
+
+    public static int getQSColumnsCount(Context context, int resourceCount) {
+        final int QS_COLUMNS_MIN = 2;
+        final Resources res = context.getResources();
+        int value = QS_COLUMNS_MIN;
+        if (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            value = Settings.System.getIntForUser(
+                    context.getContentResolver(), Settings.System.QS_LAYOUT_COLUMNS,
+                    resourceCount, UserHandle.USER_CURRENT);
+        } else {
+            value = Settings.System.getIntForUser(
+                    context.getContentResolver(), Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE,
+                    resourceCount, UserHandle.USER_CURRENT);
+        }
+        return Math.max(QS_COLUMNS_MIN, value);
+    }
+
+    public static boolean getQSTileLabelHide(Context context) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QS_TILE_LABEL_HIDE,
+                0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    public static boolean getQSTileVerticalLayout(Context context, int defaultValue) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.QS_TILE_VERTICAL_LAYOUT,
+                defaultValue, UserHandle.USER_CURRENT) != 0;
+    }
+}
