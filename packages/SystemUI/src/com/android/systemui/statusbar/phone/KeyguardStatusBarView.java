8da63a72e7567049e7d332dffc7794dc27d2add3
Pranav Vashi <neobuddy89@gmail.com>
2021-12-23 11:18:17 +0530

    Revert "SystemUI: Bring back good ol' circle battery style"
    This reverts commit 3b0f5b8557a45b08d6d0131755e91bee86be27e2.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -82,6 +82,7 @@ public class KeyguardStatusBarView extends RelativeLayout implements
 
     private final Rect mEmptyRect = new Rect(0, 0, 0, 0);
 
+    private boolean mShowPercentAvailable;
     private boolean mBatteryCharging;
     private boolean mBatteryListening;
 
@@ -193,6 +194,8 @@ public class KeyguardStatusBarView extends RelativeLayout implements
                 R.dimen.ongoing_appops_dot_min_padding);
         mCutoutSideNudge = getResources().getDimensionPixelSize(
                 R.dimen.display_cutout_margin_consumption);
+        mShowPercentAvailable = getContext().getResources().getBoolean(
+                com.android.internal.R.bool.config_battery_percentage_setting_available);
         mRoundedCornerPadding = res.getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding);
     }
@@ -229,7 +232,7 @@ public class KeyguardStatusBarView extends RelativeLayout implements
                 mMultiUserAvatar.setVisibility(View.GONE);
             }
         }
-        mBatteryView.setForceShowPercent(mBatteryCharging);
+        mBatteryView.setForceShowPercent(mBatteryCharging && mShowPercentAvailable);
     }
 
     private void updateSystemIconsLayoutParams() {
