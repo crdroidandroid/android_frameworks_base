d015bd4ad6988ad3495146742186da14d0d5b340
Pranav Vashi <neobuddy89@gmail.com>
2021-12-27 01:59:02 +0530

    overlays: Restore icon shapes from A11
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -56,6 +56,17 @@ LOCAL_REQUIRED_MODULES := \
     IconPackRoundedSettingsOverlay \
     IconPackRoundedSystemUIOverlay \
     IconPackRoundedThemePickerOverlay \
+	IconShapeCylinderOverlay \
+	IconShapeHeartOverlay \
+	IconShapeHexagonOverlay \
+	IconShapeLeafOverlay \
+	IconShapePebbleOverlay \
+	IconShapeRoundedRectOverlay \
+	IconShapeSquareOverlay \
+	IconShapeSquircleOverlay \
+	IconShapeTaperedRectOverlay \
+	IconShapeTeardropOverlay \
+	IconShapeVesselOverlay \
     NavigationBarMode2ButtonOverlay \
     NavigationBarMode3ButtonOverlay \
     NavigationBarModeGesturalOverlay \
