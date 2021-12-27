0fdc0050c406fd4a31f606cec5ff98a06bfffe49
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-27 17:05:57 +0700

    SystemUI: Add support for GameSpace
    This is an addon for GameSpace for broadcasting about game start/stop.
    Additionally, it also has special option for suppressing fullscreen
    intent like incoming call.
    
    This also squash commits:
    - SystemUI: Improve GameSpace lifecycle and broadcast handling
    - GameSpaceManager: Handle various case against "locking screen" scenario
    - GameSpace: Fix issue with multiple users [neobuddy89]
    - SystemUI: Update gamespace for A13 [Genkzsz11]
    - GameSpaceManager: restrict broadcast to MANAGE_GAME_MODE holders only
    - GameSpaceManager: Update TaskStack listener usage
    - GameSpaceManager: Updated for A14 QPR1 [neobuddy89]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
@@ -44,6 +44,7 @@ import com.android.systemui.plugins.ActivityStarter.OnDismissAction;
 import com.android.systemui.qs.QSPanelController;
 import com.android.systemui.shared.statusbar.phone.BarTransitions;
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
+import com.android.systemui.statusbar.policy.GameSpaceManager;
 import com.android.systemui.util.Compile;
 
 import java.io.PrintWriter;
@@ -350,4 +351,6 @@ public interface CentralSurfaces extends Dumpable, LifecycleOwner, CoreStartable
      */
     ActivityTransitionAnimator.Controller getAnimatorControllerFromNotification(
             ExpandableNotificationRow associatedView);
+
+    GameSpaceManager getGameSpaceManager();
 }
