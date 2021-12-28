74abf99ec6f459f62760a3f715ba3212eb9666c9
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: Integrate smartspace and backgesture provider
    Change-Id: Iadaaa3e24b3a30edeff59e0224d063bf71e20d3d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/crdroid/android/systemui/CrDroidSystemUIFactory.java
+++ packages/SystemUI/src/com/crdroid/android/systemui/CrDroidSystemUIFactory.java
@@ -1,12 +1,22 @@
 package com.crdroid.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
 
 import com.crdroid.android.systemui.dagger.CrDroidGlobalRootComponent;
+import com.crdroid.android.systemui.dagger.CrDroidSysUIComponent;
 import com.crdroid.android.systemui.dagger.DaggerCrDroidGlobalRootComponent;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class CrDroidSystemUIFactory extends SystemUIFactory {
     @Override
@@ -14,5 +24,18 @@ public class CrDroidSystemUIFactory extends SystemUIFactory {
         return DaggerCrDroidGlobalRootComponent.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((CrDroidSysUIComponent) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
