6dd84b4ac74a312d3f878210771c5a5b8dbd90a8
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial crDroid SystemUI stub
    Change-Id: I67f36f20cc01dfc82572f2241287c3e739592306
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ packages/SystemUI/src/com/crdroid/android/systemui/dagger/CrDroidSysUISubcomponentModule.java
@@ -0,0 +1,10 @@
+package com.crdroid.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {CrDroidSysUIComponent.class})
+public abstract class CrDroidSysUISubcomponentModule {
+}
