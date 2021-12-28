9d49eee7c9fae2480101de16a42b87aa11cfc8d3
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:57:38 +0530

    SystemUI: Update monet according to new sysui stub
    Change-Id: I6ef16b8afedac661d06b4b7d8467cac21428d3e3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/org/pixelexperience/systemui/theme/CustomThemeOverlayController.kt
+++ packages/SystemUI/src/org/pixelexperience/systemui/theme/CustomThemeOverlayController.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.protonaosp.systemui.theme
+package org.pixelexperience.systemui.theme
 
 import android.annotation.ColorInt
 import android.app.WallpaperColors
