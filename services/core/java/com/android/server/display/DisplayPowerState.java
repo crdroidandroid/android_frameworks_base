6ff0d2edc77e56e544109b3994ab956f36c6d5bb
Kshitij Gupta <kshitijgm@gmail.com>
2022-01-04 16:10:15 +0530

    fwb: Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    *** Changes for Android 10 by bigrushdog ***
    Various updates needed for Surface related API in the ElectronBeam animation class
    
    [AgentFabulous | POSP]
    - Rebase and rewrite ElectronBeam class on top of ColorFade
    - Add usage of class-common Transaction instance
    - Add support for respecting wide-color and protected-content
    - Fixup for new API
    
    @neobuddy89:
    Updated for Android 14.
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28
    Co-Authored-By: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerState.java
+++ services/core/java/com/android/server/display/DisplayPowerState.java
@@ -58,7 +58,6 @@ final class DisplayPowerState {
     private final Handler mHandler;
     private final Choreographer mChoreographer;
     private final DisplayBlanker mBlanker;
-    private final ColorFade mColorFade;
     private final PhotonicModulator mPhotonicModulator;
     private final int mDisplayId;
 
@@ -77,12 +76,14 @@ final class DisplayPowerState {
 
     private volatile boolean mStopped;
 
+    private ScreenStateAnimator mColorFade;
+
     DisplayPowerState(
-            DisplayBlanker blanker, ColorFade colorFade, int displayId, int displayState) {
+            DisplayBlanker blanker, int screenAnimatorMode, int displayId, int displayState) {
         mHandler = new Handler(true /*async*/);
         mChoreographer = Choreographer.getInstance();
         mBlanker = blanker;
-        mColorFade = colorFade;
+        setScreenStateAnimator(screenAnimatorMode);
         mPhotonicModulator = new PhotonicModulator();
         mPhotonicModulator.start();
         mDisplayId = displayId;
@@ -101,6 +102,17 @@ final class DisplayPowerState {
         mColorFadePrepared = false;
         mColorFadeLevel = 1.0f;
         mColorFadeReady = true;
+    }
+
+    public void setScreenStateAnimator(int mode) {
+        if (mColorFade != null) {
+            mColorFade.dismiss();
+        }
+        if (mode == DisplayPowerController.SCREEN_OFF_FADE) {
+            mColorFade = new ColorFade(Display.DEFAULT_DISPLAY);
+        } else {
+            mColorFade = new ElectronBeam(Display.DEFAULT_DISPLAY);
+        }
     }
 
     public static final FloatProperty<DisplayPowerState> COLOR_FADE_LEVEL =
