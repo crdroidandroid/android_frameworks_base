0f8b2fbd024ea1ab92ef4add5295cee4624a006e
jhonboy121 <alfredmathew05@gmail.com>
2022-01-05 13:30:41 +0530

    SystemUI: Rewrite FPSInfoService in kt from scratch
    * We now make use of coroutines for periodically reading fps
    * Added support for controlling fps read interval via overlay
    * Make sure tile is not available if fps info node is not accessible
    * Also keep the overlay below statusbar inset
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/DefaultServiceBinder.java
+++ packages/SystemUI/src/com/android/systemui/dagger/DefaultServiceBinder.java
@@ -18,6 +18,7 @@ package com.android.systemui.dagger;
 
 import android.app.Service;
 
+import com.android.systemui.FPSInfoService;
 import com.android.systemui.SystemUIService;
 import com.android.systemui.doze.DozeService;
 import com.android.systemui.dreams.DreamOverlayService;
@@ -93,4 +94,9 @@ public abstract class DefaultServiceBinder {
     @ClassKey(IssueRecordingService.class)
     public abstract Service bindIssueRecordingService(IssueRecordingService service);
 
+    /** Inject into FPSInfoService */
+    @Binds
+    @IntoMap
+    @ClassKey(FPSInfoService.class)
+    public abstract Service bindFPSInfoService(FPSInfoService service);
 }
