9c6b2ee943c3c132bb50d7b12544549ded7885e2
jhonboy121 <alfredmathew05@gmail.com>
2022-01-09 14:29:01 +0530

    SystemUI: Add edge light customizations [1/2]
    * Drawables and layout is from omni's commit
    * This a completely rewritten (in kt) and a non-hacky version of their work
    
    * (Stallix) - Adapt for A13 QPR1
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I9d51ea356b56a68feb62c303a60508809ceb7ed5
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeServiceHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeServiceHost.java
@@ -44,6 +44,7 @@ import com.android.systemui.doze.DozeReceiver;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.keyguard.domain.interactor.DozeInteractor;
 import com.android.systemui.scene.shared.flag.SceneContainerFlag;
+import com.android.systemui.shade.EdgeLightViewController;
 import com.android.systemui.shade.NotificationShadeWindowViewController;
 import com.android.systemui.shade.domain.interactor.ShadeLockscreenInteractor;
 import com.android.systemui.statusbar.NotificationShadeWindowController;
@@ -105,6 +106,7 @@ public final class DozeServiceHost implements DozeHost {
     private final NotificationWakeUpCoordinator mNotificationWakeUpCoordinator;
     private NotificationShadeWindowViewController mNotificationShadeWindowViewController;
     private final AuthController mAuthController;
+    private final EdgeLightViewController mEdgeLightViewController;
     private StatusBarKeyguardViewManager mStatusBarKeyguardViewManager;
     private final ShadeLockscreenInteractor mShadeLockscreenInteractor;
     private View mAmbientIndicationContainer;
@@ -128,7 +130,8 @@ public final class DozeServiceHost implements DozeHost {
             NotificationWakeUpCoordinator notificationWakeUpCoordinator,
             AuthController authController,
             ShadeLockscreenInteractor shadeLockscreenInteractor,
-            DozeInteractor dozeInteractor) {
+            DozeInteractor dozeInteractor,
+            EdgeLightViewController edgeLightViewController) {
         super();
         mDozeLog = dozeLog;
         mPowerManager = powerManager;
@@ -149,6 +152,7 @@ public final class DozeServiceHost implements DozeHost {
         mShadeLockscreenInteractor = shadeLockscreenInteractor;
         mHeadsUpManager.addListener(mOnHeadsUpChangedListener);
         mDozeInteractor = dozeInteractor;
+        mEdgeLightViewController = edgeLightViewController;
     }
 
     // TODO: we should try to not pass status bar in here if we can avoid it.
@@ -308,6 +312,7 @@ public final class DozeServiceHost implements DozeHost {
                 mCentralSurfaces.updateScrimController();
                 mPulseExpansionHandler.setPulsing(pulsing);
                 mNotificationWakeUpCoordinator.setPulsing(pulsing);
+                mEdgeLightViewController.setPulsing(pulsing, reason);
             }
         }, reason);
         // DozeScrimController is in pulse state, now let's ask ScrimController to start
