ea89207e738574cc428608c99748099b178ed86d
jhonboy121 <alfredmathew05@gmail.com>
2022-01-11 20:43:24 +0530

    Allow disabling qs on secure lockscreen [1/2]
    * This does not prevent pulling down notification shade,
      qs will be entirely hidden even if you do and that's it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6751,6 +6751,12 @@ public final class Settings {
         public static final String STATUSBAR_EXTRA_PADDING_END = "statusbar_extra_padding_end";
 
         /**
+         * Whether to disable qs on secure lockscreen.
+         * @hide
+         */
+        public static final String SECURE_LOCKSCREEN_QS_DISABLED = "secure_lockscreen_qs_disabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
