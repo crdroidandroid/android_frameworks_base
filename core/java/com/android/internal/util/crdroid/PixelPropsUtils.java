e92cb1fb5b0d9d4e0919fb4fb2da7d2de4643670
Pranav Vashi <neobuddy89@gmail.com>
2022-01-11 09:49:00 +0530

    PixelPropUtils: Bail out early when required
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -150,7 +150,10 @@ public class PixelPropsUtils {
         if (packageName == null) {
             return;
         }
-        if ((packageName.startsWith("com.google.") && !Arrays.asList(packagesToKeep).contains(packageName))
+        if (Arrays.asList(packagesToKeep).contains(packageName)) {
+            return;
+        }
+        if (packageName.startsWith("com.google.")
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
             Map<String, Object> propsToChange = propsToChangePixel6;
 
