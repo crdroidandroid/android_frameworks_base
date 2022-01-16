9708e4b5973ea5e97efebe3b4f4675241295118b
Pranav Vashi <neobuddy89@gmail.com>
2022-01-16 16:53:22 +0530

    NavigationBarInflater: Update navigation no hint package overlay
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarInflaterView.java
@@ -94,7 +94,7 @@ public class NavigationBarInflaterView extends FrameLayout {
     private static final String ABSOLUTE_VERTICAL_CENTERED_SUFFIX = "C";
 
     private static final String OVERLAY_NAVIGATION_HIDE_HINT =
-            "org.lineageos.overlay.customization.navbar.nohint";
+            "com.custom.overlay.systemui.gestural.hidden";
 
     private static class Listener implements NavigationModeController.ModeChangedListener {
         private final WeakReference<NavigationBarInflaterView> mSelf;
