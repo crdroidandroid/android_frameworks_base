5bdb1835021e53639dd0e56b3e0c224a58778b38
ezio84 <brabus84@gmail.com>
2022-01-16 13:20:36 +0400

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    Squashed:
    
        From: someone5678 <someone5678@users.noreply.github.com>
        Date: Sun, 30 Jun 2024 18:35:59 +0900
        Subject: fixup! Allow to suppress notifications sound/vibration if screen is ON [1/2]
    
        * Also apply it to NotificationAttentionHelper
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Tue, 23 Jul 2024 01:12:58 +0530
        Subject: fixup! Allow to suppress notifications
         sound/vibration if screen is ON [1/2]
    
        * We forgot to load settings on boot.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -774,6 +774,8 @@ public class NotificationManagerService extends SystemService {
     // Broadcast intent receiver for notification permissions review-related intents
     private ReviewNotificationPermissionsReceiver mReviewNotificationPermissionsReceiver;
 
+    private boolean mSoundVibScreenOn;
+
     static class Archive {
         final SparseArray<Boolean> mEnabled;
         final int mBufferSize;
@@ -2176,6 +2178,8 @@ public class NotificationManagerService extends SystemService {
                 = Settings.Secure.getUriFor(Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS);
         private final Uri SHOW_NOTIFICATION_SNOOZE
                 = Settings.Secure.getUriFor(Settings.Secure.SHOW_NOTIFICATION_SNOOZE);
+        private final Uri NOTIFICATION_SOUND_VIB_SCREEN_ON
+                = Settings.System.getUriFor(Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON);
 
         SettingsObserver(Handler handler) {
             super(handler);
@@ -2204,6 +2208,8 @@ public class NotificationManagerService extends SystemService {
                     false, this, UserHandle.USER_ALL);
 
             resolver.registerContentObserver(SHOW_NOTIFICATION_SNOOZE,
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(NOTIFICATION_SOUND_VIB_SCREEN_ON,
                     false, this, UserHandle.USER_ALL);
 
             update(null);
@@ -2257,6 +2263,11 @@ public class NotificationManagerService extends SystemService {
                 if (!snoozeEnabled) {
                     unsnoozeAll();
                 }
+            }
+            if (uri == null || NOTIFICATION_SOUND_VIB_SCREEN_ON.equals(uri)) {
+                mSoundVibScreenOn = Settings.System.getIntForUser(resolver,
+                        Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, 1,
+                        UserHandle.USER_CURRENT) == 1;
             }
         }
 
@@ -9197,7 +9208,8 @@ public class NotificationManagerService extends SystemService {
         }
 
         if (aboveThreshold && isNotificationForCurrentUser(record)) {
-            if (mSystemReady && mAudioManager != null) {
+            boolean skipSound = mScreenOn && !mSoundVibScreenOn;
+            if (mSystemReady && mAudioManager != null && !skipSound) {
                 Uri soundUri = record.getSound();
                 hasValidSound = soundUri != null && !Uri.EMPTY.equals(soundUri);
                 VibrationEffect vibration = record.getVibration();
