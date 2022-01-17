a971d0c3d58984386b849faf71afe6be3c21c9f4
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 12:17:42 +0530

    Fix chip visibility in split shade header
    * Chip should be visible only if either indicators are enabled.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
+++ packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
@@ -112,7 +112,7 @@ class HeaderPrivacyIconsController @Inject constructor(
             privacyLogger.logChipVisible(false)
         }
 
-        privacyChip.visibility = if (visible) View.VISIBLE else View.GONE
-        chipVisibilityListener?.onChipVisibilityRefreshed(visible)
+        privacyChip.visibility = if (visible && getChipEnabled()) View.VISIBLE else View.GONE
+        chipVisibilityListener?.onChipVisibilityRefreshed(visible && getChipEnabled())
     }
 }
