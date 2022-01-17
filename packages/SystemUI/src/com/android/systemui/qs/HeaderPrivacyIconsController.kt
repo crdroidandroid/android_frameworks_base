aedefe8f263723660354d7e397a613a550dbceac
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:09:53 +0530

    SystemUI: Kill old privacy indicator icons completely
    * It's messy to keep both versions.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
+++ packages/SystemUI/src/com/android/systemui/qs/HeaderPrivacyIconsController.kt
@@ -79,9 +79,6 @@ constructor(
     private var locationIndicatorsEnabled = false
     private var privacyChipLogged = false
     private var safetyCenterEnabled = false
-    private val cameraSlot = privacyChip.resources.getString(R.string.status_bar_camera)
-    private val micSlot = privacyChip.resources.getString(R.string.status_bar_microphone)
-    private val locationSlot = privacyChip.resources.getString(R.string.status_bar_location)
     private val dialogContext: Context
         get() = shadeDialogContextInteractor.context
 
@@ -145,7 +142,6 @@ constructor(
             }
 
             private fun update() {
-                updatePrivacyIconSlots()
                 setChipVisibility(privacyChip.privacyList.isNotEmpty())
             }
         }
@@ -171,9 +167,6 @@ constructor(
         setChipVisibility(privacyChip.visibility == View.VISIBLE)
         micCameraIndicatorsEnabled = privacyItemController.micCameraAvailable
         locationIndicatorsEnabled = privacyItemController.locationAvailable
-
-        // Ignore privacy icons because they show in the space above QQS
-        updatePrivacyIconSlots()
     }
 
     private fun showSafetyCenter() {
@@ -247,26 +240,5 @@ constructor(
 
         privacyChip.visibility = if (visible) View.VISIBLE else View.GONE
         chipVisibilityListener?.onChipVisibilityRefreshed(visible)
-    }
-
-    private fun updatePrivacyIconSlots() {
-        if (getChipEnabled()) {
-            if (micCameraIndicatorsEnabled) {
-                iconContainer.addIgnoredSlot(cameraSlot)
-                iconContainer.addIgnoredSlot(micSlot)
-            } else {
-                iconContainer.removeIgnoredSlot(cameraSlot)
-                iconContainer.removeIgnoredSlot(micSlot)
-            }
-            if (locationIndicatorsEnabled) {
-                iconContainer.addIgnoredSlot(locationSlot)
-            } else {
-                iconContainer.removeIgnoredSlot(locationSlot)
-            }
-        } else {
-            iconContainer.removeIgnoredSlot(cameraSlot)
-            iconContainer.removeIgnoredSlot(micSlot)
-            iconContainer.removeIgnoredSlot(locationSlot)
-        }
     }
 }
