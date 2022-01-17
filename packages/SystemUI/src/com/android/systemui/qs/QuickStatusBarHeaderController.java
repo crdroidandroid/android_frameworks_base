f185e75343a5d014e67b16df0153b350dcfdaabc
Pranav Vashi <neobuddy89@gmail.com>
2022-01-17 21:09:53 +0530

    StatusBar: Kill old privacy indicator icons completely
    * First, it's messy to keep both versions.
    * Second, it seems Google plans to keep chip version - so let's stick to it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeaderController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeaderController.java
@@ -77,9 +77,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
     private boolean mMicCameraIndicatorsEnabled;
     private boolean mLocationIndicatorsEnabled;
     private boolean mPrivacyChipLogged;
-    private final String mCameraSlot;
-    private final String mMicSlot;
-    private final String mLocationSlot;
 
     private PrivacyItemController.Callback mPICCallback = new PrivacyItemController.Callback() {
         @Override
@@ -105,7 +102,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
         }
 
         private void update() {
-            updatePrivacyIconSlots();
             setChipVisibility(!mPrivacyChip.getPrivacyList().isEmpty());
         }
     };
@@ -162,10 +158,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
 
         mIconManager = new StatusBarIconController.TintedIconManager(mIconContainer, featureFlags);
         mDemoModeReceiver = new ClockDemoModeReceiver(mClockView);
-
-        mCameraSlot = getResources().getString(com.android.internal.R.string.status_bar_camera);
-        mMicSlot = getResources().getString(com.android.internal.R.string.status_bar_microphone);
-        mLocationSlot = getResources().getString(com.android.internal.R.string.status_bar_location);
     }
 
     @Override
@@ -176,7 +168,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
         mLocationIndicatorsEnabled = mPrivacyItemController.getLocationAvailable();
 
         // Ignore privacy icons because they show in the space above QQS
-        updatePrivacyIconSlots();
         mIconContainer.addIgnoredSlot(
                 getResources().getString(com.android.internal.R.string.status_bar_managed_profile));
         mIconContainer.setShouldRestrictIcons(false);
@@ -259,27 +250,6 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
             mPrivacyLogger.logChipVisible(false);
         }
         mView.setChipVisibility(chipVisible && getChipEnabled());
-    }
-
-    private void updatePrivacyIconSlots() {
-        if (getChipEnabled()) {
-            if (mMicCameraIndicatorsEnabled) {
-                mIconContainer.addIgnoredSlot(mCameraSlot);
-                mIconContainer.addIgnoredSlot(mMicSlot);
-            } else {
-                mIconContainer.removeIgnoredSlot(mCameraSlot);
-                mIconContainer.removeIgnoredSlot(mMicSlot);
-            }
-            if (mLocationIndicatorsEnabled) {
-                mIconContainer.addIgnoredSlot(mLocationSlot);
-            } else {
-                mIconContainer.removeIgnoredSlot(mLocationSlot);
-            }
-        } else {
-            mIconContainer.removeIgnoredSlot(mCameraSlot);
-            mIconContainer.removeIgnoredSlot(mMicSlot);
-            mIconContainer.removeIgnoredSlot(mLocationSlot);
-        }
     }
 
     private boolean getChipEnabled() {
