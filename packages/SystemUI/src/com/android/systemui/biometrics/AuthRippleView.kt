a1f3b20e2c983bde316722a522bf2eb95cb6c845
Nauval Rizky <enuma.alrizky@gmail.com>
2022-01-19 08:31:56 +0700

    SystemUI: Fix method for disabling unlock ripple animation
    NotificationShadeWindowController's are not gracefully closed, which
    leads to top parts of the screen is being obscured by invisible shade window.
    And also, "ripple" is actually still exists due to lightRevealScrim are
    being started by onKeyguardFadingAwayChanged, which is not what user expect
    when they want to disable the effect.
    
    So in order to handle those cases, lets intercept it from the
    AuthRippleController instead.
    
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleView.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleView.kt
@@ -251,12 +251,7 @@ class AuthRippleView(context: Context?, attrs: AttributeSet?) : View(context, at
      * Ripple that bursts outwards from the position of the sensor to the edges of the screen
      */
     fun startUnlockedRipple(onAnimationEnd: Runnable?) {
-        val enableRipple = Settings.System.getIntForUser(
-            context.contentResolver,
-            Settings.System.ENABLE_RIPPLE_EFFECT, 1,
-            UserHandle.USER_CURRENT) == 1
-
-        if (unlockedRippleInProgress || !enableRipple) {
+        if (unlockedRippleInProgress) {
             return // Ignore if ripple effect is already playing
         }
 
