21b50b160a6b6e35dfa99231148005dc85efc83f
spkal01 <kalligeross@gmail.com>
2022-02-01 21:52:59 +0800

    SystemUI: Allow Configuring Navbar Radius [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationHandle.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/NavigationHandle.java
@@ -24,6 +24,8 @@ import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.FloatProperty;
 import android.view.ContextThemeWrapper;
@@ -40,7 +42,7 @@ public class NavigationHandle extends View implements ButtonInterface {
     protected final Paint mPaint = new Paint();
     private @ColorInt final int mLightColor;
     private @ColorInt final int mDarkColor;
-    protected final float mRadius;
+    protected float mRadius;
     protected final float mBottom;
     private final float mAdditionalWidthForAnimation;
     private final float mAdditionalHeightForAnimation;
@@ -71,7 +73,6 @@ public class NavigationHandle extends View implements ButtonInterface {
     public NavigationHandle(Context context, AttributeSet attr) {
         super(context, attr);
         final Resources res = context.getResources();
-        mRadius = res.getDimension(R.dimen.navigation_handle_radius);
         mBottom = res.getDimension(R.dimen.navigation_handle_bottom);
         mAdditionalWidthForAnimation =
                 res.getDimension(R.dimen.navigation_home_handle_additional_width_for_animation);
@@ -105,6 +106,29 @@ public class NavigationHandle extends View implements ButtonInterface {
 
         // Draw that bar
         int navHeight = getHeight();
+        int radiusType = Settings.System.getIntForUser(getContext().getContentResolver(),
+            Settings.System.GESTURE_NAVBAR_RADIUS, 3, UserHandle.USER_CURRENT);
+        switch (radiusType) {
+            case 0:
+                mRadius = getResources().getDimensionPixelSize(R.dimen.navigation_handle_radius1);
+                break;
+            case 1:
+                mRadius = getResources().getDimensionPixelSize(R.dimen.navigation_handle_radius2);
+                break;
+            case 2:
+                mRadius = getResources().getDimensionPixelSize(R.dimen.navigation_handle_radius3);
+                break;
+            case 3:
+            default:
+                mRadius = getResources().getDimensionPixelSize(R.dimen.navigation_handle_radius);
+                break;
+            case 4:
+                mRadius = getResources().getDimensionPixelSize(R.dimen.navigation_handle_radius4);
+                break;
+            case 5:
+                mRadius = getResources().getDimensionPixelSize(R.dimen.navigation_handle_radius5);
+                break;
+        }
         float additionalHeight;
         float additionalWidth;
         if (mShrink) {
