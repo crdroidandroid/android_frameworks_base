874396d7bdd318900eb958930de27626034ba0ea
ShevT <evgeny.shishkov@gmail.com>
2022-02-12 00:02:04 +0300

    DataSwitchTile: Resolve initial tile state
     After loading the system, we do not see on the tile the telecom
     operator to which we will switch. Instead, we see "On". And so on
     until we switch the network. If you pull out one SIM, then the name
     of the operator to which we could switch to will "freeze" on the tile.
    
     Let's fix this.
    
    @neobuddy89: Improvise logic and rewrite.
    
    Change-Id: I7a4996b48e014825c5452c1259454e9b7c70ce82
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -68,7 +68,6 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
     private final MyCallStateListener mPhoneStateListener;
     private final SubscriptionManager mSubscriptionManager;
     private final TelephonyManager mTelephonyManager;
-    private String mInactiveSlotName;
 
     class MyCallStateListener extends PhoneStateListener {
         MyCallStateListener() {
@@ -189,22 +188,26 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
             case 0:
                 state.icon = ResourceIcon.get(R.drawable.ic_qs_data_switch_0);
                 state.value = false;
+                state.secondaryLabel = mContext.getString(R.string.tile_unavailable);
                 break;
             case 1:
                 state.icon = ResourceIcon.get(activeSIMZero
                         ? R.drawable.ic_qs_data_switch_2
                         : R.drawable.ic_qs_data_switch_1);
                 state.value = false;
+                state.secondaryLabel = mContext.getString(R.string.tile_unavailable);
                 break;
             case 2:
                 state.icon = ResourceIcon.get(activeSIMZero
                         ? R.drawable.ic_qs_data_switch_2
                         : R.drawable.ic_qs_data_switch_1);
                 state.value = true;
+                state.secondaryLabel = getInactiveSlotName();
                 break;
             default:
                 state.icon = ResourceIcon.get(R.drawable.ic_qs_data_switch_1);
                 state.value = false;
+                state.secondaryLabel = mContext.getString(R.string.tile_unavailable);
                 break;
         }
         if (mSimCount < 2) {
@@ -217,9 +220,6 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
         }
 
         state.label = mContext.getString(R.string.qs_data_switch_label);
-        if (mInactiveSlotName != null) {
-            state.secondaryLabel = mInactiveSlotName;
-        }
     }
 
     @Override
@@ -247,11 +247,27 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
                     // Indicate we found sim with active data, disable data on remaining sim.
                     if (!foundActive) foundActive = !dataEnabled;
                 }
-                // Store carrier label of inactive/opposite sim slot.
-                if (!foundActive) mInactiveSlotName = subInfo.getDisplayName().toString();
                 Log.d(TAG, "Changed subID " + subInfo.getSubscriptionId() + " to "
                     + !dataEnabled);
             }
         }
+    }
+
+    private String getInactiveSlotName() {
+        TelephonyManager telephonyManager;
+        String mInitialState = mContext.getString(R.string.tile_unavailable);
+        List<SubscriptionInfo> subInfoList =
+                mSubscriptionManager.getActiveSubscriptionInfoList(true);
+        if (subInfoList != null) {
+            for (SubscriptionInfo subInfo : subInfoList) {
+                telephonyManager =
+                        mTelephonyManager.createForSubscriptionId(subInfo.getSubscriptionId());
+                if (!telephonyManager.getDataEnabled()) {
+                    // Inactive SIM found
+                    return subInfo.getDisplayName().toString();
+                }
+            }
+        }
+        return mInitialState;
     }
 }
