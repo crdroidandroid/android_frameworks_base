597510ed4867af652c7bba85f8b40faeb8553586
jhenrique09 <jhsv09@gmail.com>
2022-02-11 22:34:49 -0300

    SystemUI: Tell user when face unlock detection is running
    Google removed the animation on Android 12, let's use a text instead
    
    Change-Id: I93c4121c57137764edb0b31678d103fd5f7d4dcc

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -499,8 +499,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
     private static int sCurrentUser;
 
     private final boolean mFaceAuthOnlyOnSecurityView;
-    private static final int FACE_UNLOCK_BEHAVIOR_DEFAULT = 0;
-    private static final int FACE_UNLOCK_BEHAVIOR_SWIPE = 1;
+    public static final int FACE_UNLOCK_BEHAVIOR_DEFAULT = 0;
+    public static final int FACE_UNLOCK_BEHAVIOR_SWIPE = 1;
     private int mFaceUnlockBehavior = FACE_UNLOCK_BEHAVIOR_DEFAULT;
 
     public synchronized static void setCurrentUser(int currentUser) {
@@ -3125,6 +3125,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 || (posture == mConfigFaceAuthSupportedPosture);
     }
 
+    public int getFaceUnlockBehavior() {
+        return mFaceUnlockBehavior;
+    }
+
     private void logListenerModelData(@NonNull KeyguardListenModel model) {
         mLogger.logKeyguardListenerModel(model);
         if (model instanceof KeyguardFingerprintListenModel) {
@@ -3291,7 +3295,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
         return mIsUnlockWithFingerprintPossible.getOrDefault(userId, false);
     }
 
-    private boolean isUnlockWithFacePossible(int userId) {
+    public boolean isUnlockWithFacePossible(int userId) {
         return isFaceAuthEnabledForUser(userId) && !isFaceDisabled(userId);
     }
 
