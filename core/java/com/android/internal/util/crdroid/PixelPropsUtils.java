661348f9cbd022303ce56efa0d96201d207af624
Pranav Vashi <neobuddy89@gmail.com>
2022-02-12 22:27:33 +0530

    User toggle for GamesProp [1/2]
    * Disabled by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -191,6 +191,10 @@ public class PixelPropsUtils {
                 setPropValue("FINGERPRINT", Build.DATE);
             }
         } else {
+
+            if (!SystemProperties.getBoolean("persist.sys.pixelprops.games", false))
+                return;
+
             if (Arrays.asList(packagesToChangeROG1).contains(packageName)) {
                 if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
                 for (Map.Entry<String, Object> prop : propsToChangeROG1.entrySet()) {
