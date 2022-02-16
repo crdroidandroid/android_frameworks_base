36f4780c89cf497bde8280b920f1349c59bc4bee
Alfred Mathew <alfredmathew05@gmail.com>
2022-02-16 19:17:25 +0530

    SystemUI: QSTileImpl: bail out early in handleLongClick if getLongClickIntent returns null
    * Instead of override handleLongClick I think this is a better way to prevent sysui crashes
      on tiles that don't support long click and provides a null intent (which makes sense to do so)
    
    Signed-off-by: Alfred Mathew <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -411,6 +411,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
      * @param view The view from which the opening window will be animated.
      */
     protected void handleLongClick(@Nullable View view) {
+        if (getLongClickIntent() == null) return;
         ActivityTransitionAnimator.Controller animationController =
                 view != null ? ActivityTransitionAnimator.Controller.fromView(view,
                         InteractionJankMonitor.CUJ_SHADE_APP_LAUNCH_FROM_QS_TILE) : null;
