2f351375c20de97ececb22ce43bdd37d7f155608
LibXZR <i@xzr.moe>
2022-02-18 23:16:00 +0800

    DisplayPowerController: Don't apply brightness adjustment if NaN
    On some devices, old incompatible brightness configs are causing
    PowerManager to crash randomly although auto brightness seems working
    just all fine.
    
    Change-Id: I09591f56b4d592d1019a43c639e5e9f0ddbad004
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -2176,6 +2176,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     }
 
     private void putAutoBrightnessAdjustmentSetting(float adjustment) {
+        if (Float.isNaN(adjustment))
+            return;
         if (mDisplayId == Display.DEFAULT_DISPLAY) {
             mAutoBrightnessAdjustment = adjustment;
             Settings.System.putFloatForUser(mContext.getContentResolver(),
