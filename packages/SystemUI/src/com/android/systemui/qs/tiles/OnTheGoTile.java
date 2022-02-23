c51a4e6848b9cc9d55d2ba31c8fd0d69c4c5537a
ShevT <evgeny.shishkov@gmail.com>
2022-02-23 22:37:52 +0300

    fwb: OnTheGoTile: Fixed turning off tile
    Track from Project-Xtended
    
    TEST:
    1) Click on the tile. The On-The-Go mode is activated.
       The tile becomes active.
    2) Click on the tile again. On-The-Go mode turns off.
       The tile remains active until we close quick settings and reopen.
    
    Let's fix it...
    
    Change-Id: I39f454ee9027e476e8b1b0f5fd6b6f99fc72fcc2

--- packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java
@@ -20,25 +20,26 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
-import android.service.quicksettings.Tile;
 import android.view.View;
-
-import androidx.annotation.Nullable;
+import android.service.quicksettings.Tile;
 
 import com.android.systemui.R;
 import com.android.systemui.R.drawable;
 import com.android.systemui.crdroid.onthego.OnTheGoService;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.FalsingManager;
-import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
-import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.logging.QSLogger;
-import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import androidx.annotation.Nullable;
 
 import com.android.internal.logging.MetricsLogger;
+
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.util.crdroid.OnTheGoUtils;
 
@@ -48,6 +49,7 @@ import javax.inject.Inject;
 public class OnTheGoTile extends QSTileImpl<BooleanState> {
 
     private final Icon mIcon = ResourceIcon.get(drawable.ic_qs_onthego);
+    private boolean mIsEnabled;
 
     @Inject
     public OnTheGoTile(
@@ -62,13 +64,12 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
     ) {
         super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
+        mIsEnabled = isOnTheGoEnabled();
     }
 
     @Override
     public BooleanState newTileState() {
-        BooleanState state = new BooleanState();
-        state.handlesLongClick = false;
-        return state;
+        return new BooleanState();
     }
 
     @Override
@@ -83,14 +84,20 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleClick(@Nullable View view) {
+        toggleService();
+    }
+
+    protected void toggleService() {
         ComponentName cn = new ComponentName("com.android.systemui",
                 "com.android.systemui.crdroid.onthego.OnTheGoService");
         Intent startIntent = new Intent();
         startIntent.setComponent(cn);
         if (isOnTheGoEnabled()) {
             startIntent.setAction("stop");
+            mIsEnabled = false;
         } else {
             startIntent.setAction("start");
+            mIsEnabled = true;
         }
         mContext.startService(startIntent);
         refreshState();
@@ -108,7 +115,7 @@ public class OnTheGoTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        state.value = isOnTheGoEnabled();
+        state.value = mIsEnabled;
         state.state = state.value ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
         state.label = mContext.getString(R.string.quick_settings_onthego_label);
         state.icon = mIcon;
