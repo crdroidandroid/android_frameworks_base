329da3b2d9743e1fa2b625df0ea76578f54141ba
Joey Huab <joey@evolution-x.org>
2022-02-25 05:13:31 +0900

    SystemUI: AuthBiometricFaceView: Don't spam log
    02-25 04:09:33.935 18313 18313 D BiometricPrompt/AuthBiometricFaceView: onAnimationEnd, mState: 2, deactivated: false
    02-25 04:09:33.935 18313 18313 D BiometricPrompt/AuthBiometricFaceView: animateIcon, state: 2, deactivated: false
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthBiometricFaceView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthBiometricFaceView.java
@@ -39,6 +39,8 @@ public class AuthBiometricFaceView extends AuthBiometricView {
 
     private static final String TAG = "BiometricPrompt/AuthBiometricFaceView";
 
+    private static final boolean DEBUG = false;
+
     // Delay before dismissing after being authenticated/confirmed.
     private static final int HIDE_DELAY_MS = 500;
 
@@ -68,7 +70,9 @@ public class AuthBiometricFaceView extends AuthBiometricView {
         }
 
         protected void animateIcon(int iconRes, boolean repeat) {
-            Log.d(TAG, "animateIcon, state: " + mState + ", deactivated: " + mDeactivated);
+            if (DEBUG) {
+                Log.d(TAG, "animateIcon, state: " + mState + ", deactivated: " + mDeactivated);
+            }
             if (mDeactivated) {
                 return;
             }
@@ -98,7 +102,9 @@ public class AuthBiometricFaceView extends AuthBiometricView {
         @Override
         public void onAnimationEnd(Drawable drawable) {
             super.onAnimationEnd(drawable);
-            Log.d(TAG, "onAnimationEnd, mState: " + mState + ", deactivated: " + mDeactivated);
+            if (DEBUG) {
+                Log.d(TAG, "onAnimationEnd, mState: " + mState + ", deactivated: " + mDeactivated);
+            }
             if (mDeactivated) {
                 return;
             }
