58c2af927f406495b26d1864bfb64dcdd4d70f0b
Dominik Procházka <38187580+ProchyGaming@users.noreply.github.com>
2022-02-26 14:54:02 +0100

    SystemUI: Fix an NPE in KeyguardMessageArea
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardMessageArea.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardMessageArea.java
@@ -149,9 +149,11 @@ public class KeyguardMessageArea extends TextView implements SecurityMessageDisp
                 ? INVISIBLE : VISIBLE);
         setText(status);
         ColorStateList colorState = mDefaultColorState;
-        if (mNextMessageColorState.getDefaultColor() != DEFAULT_COLOR) {
-            colorState = mNextMessageColorState;
-            mNextMessageColorState = ColorStateList.valueOf(DEFAULT_COLOR);
+        if (mNextMessageColorState != null) {
+            if (mNextMessageColorState.getDefaultColor() != DEFAULT_COLOR) {
+                colorState = mNextMessageColorState;
+                mNextMessageColorState = ColorStateList.valueOf(DEFAULT_COLOR);
+            }
         }
         setTextColor(colorState);
     }
