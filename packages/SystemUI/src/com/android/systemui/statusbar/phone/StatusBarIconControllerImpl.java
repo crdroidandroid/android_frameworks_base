c80c948b8174735330cbace581dcdb2a5aafebe5
jhonboy121 <alfredmathew05@gmail.com>
2022-03-03 00:30:45 +0530

    SystemUI: Improve mobile data style switch logic
    * Set style when an icon manager is added and update it whenever settings changes,
      this way we don't update all icons whenever addMobileIcon is called
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconControllerImpl.java
@@ -23,6 +23,7 @@ import android.content.Context;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.ArrayMap;
 import android.util.ArraySet;
 import android.util.Log;
@@ -65,6 +66,10 @@ public class StatusBarIconControllerImpl implements Tunable,
         ConfigurationListener, Dumpable, StatusBarIconController, DemoMode {
 
     private static final String TAG = "StatusBarIconController";
+
+    private static final String USE_OLD_MOBILETYPE =
+        "system:" + Settings.System.USE_OLD_MOBILETYPE;
+
     // Use this suffix to prevent external icon slot names from unintentionally overriding our
     // internal, system-level slot names. See b/255428281.
     @VisibleForTesting
@@ -75,6 +80,8 @@ public class StatusBarIconControllerImpl implements Tunable,
     private final ArraySet<String> mIconHideList = new ArraySet<>();
     private final StatusBarPipelineFlags mStatusBarPipelineFlags;
     private final Context mContext;
+
+    private boolean mIsOldSignalStyle;
 
     /** */
     @Inject
@@ -91,10 +98,13 @@ public class StatusBarIconControllerImpl implements Tunable,
         mStatusBarIconList = statusBarIconList;
         mContext = context;
         mStatusBarPipelineFlags = statusBarPipelineFlags;
+        mIsOldSignalStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.USE_OLD_MOBILETYPE, 0, UserHandle.USER_CURRENT) == 1;
 
         configurationController.addCallback(this);
         commandQueue.addCallback(mCommandQueueCallbacks);
         tunerService.addTunable(this, ICON_HIDE_LIST);
+        tunerService.addTunable(this, USE_OLD_MOBILETYPE);
         demoModeController.addCallback(this);
         dumpManager.registerDumpable(getClass().getSimpleName(), this);
     }
@@ -110,6 +120,7 @@ public class StatusBarIconControllerImpl implements Tunable,
         }
 
         group.setController(this);
+        group.setMobileSignalStyle(mIsOldSignalStyle);
         mIconGroups.add(group);
         List<Slot> allSlots = mStatusBarIconList.getSlots();
         for (int i = 0; i < allSlots.size(); i++) {
@@ -148,6 +159,16 @@ public class StatusBarIconControllerImpl implements Tunable,
     /** */
     @Override
     public void onTuningChanged(String key, String newValue) {
+        if (USE_OLD_MOBILETYPE.equals(key)) {
+            boolean isOldSignalStyle = (Boolean) newValue;
+            if (mIsOldSignalStyle == isOldSignalStyle) return;
+            mIsOldSignalStyle = isOldSignalStyle;
+            mIconGroups.forEach(group -> {
+                group.setMobileSignalStyle(mIsOldSignalStyle);
+                group.updateMobileIconStyle();
+            });
+            return;
+        }
         if (!ICON_HIDE_LIST.equals(key)) {
             return;
         }
