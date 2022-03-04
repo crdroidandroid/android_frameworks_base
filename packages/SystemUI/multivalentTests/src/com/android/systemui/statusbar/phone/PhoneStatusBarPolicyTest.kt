1f472c54b68e096a6e4e1f0a646ea4fdd40f934d
Oliver Scott <olivercscott@gmail.com>
2022-03-05 00:08:35 +0100

    Implement firewall status bar indicator
    * Add API to get the last resumed activity's UID
    * Show blocked status indicator based on the last resumed activity's
      UID blocked status
    * Rely on NetworkCallbacks from ConnectivityManager instead of network
      policy callbacks from INetworkPolicyListener to react to blocked
      status changes rather than policy changes
    
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id2: Ic5ba608ed468ae50dba5e92a4350b2cd7a190221
    Change-Id: I7aa405e7f5a2f70e6cf2b6156a29d220617e4ae5

--- packages/SystemUI/multivalentTests/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicyTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicyTest.kt
@@ -401,6 +401,7 @@ class PhoneStatusBarPolicyTest : SysuiTestCase() {
 
     private fun createStatusBarPolicy(): PhoneStatusBarPolicy {
         return PhoneStatusBarPolicy(
+            context,
             iconController,
             commandQueue,
             broadcastDispatcher,
