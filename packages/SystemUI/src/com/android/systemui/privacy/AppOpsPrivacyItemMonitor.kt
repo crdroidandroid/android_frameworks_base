63be05618ed7a54f42b4ef166ba39636b54e5ccd
LibXZR <i@xzr.moe>
2022-03-06 20:12:18 +0000

    Do not show camera indicator for Face Unlock service
    Based on: https://github.com/PixelExp-Project/frameworks_base/commit/be8d0370e65b156209a14d45e96bd59a3e74b782
    
    Change-Id: I758cfc7346d0038371892d3446c9ffb7c99c577e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/privacy/AppOpsPrivacyItemMonitor.kt
+++ packages/SystemUI/src/com/android/systemui/privacy/AppOpsPrivacyItemMonitor.kt
@@ -53,6 +53,9 @@ class AppOpsPrivacyItemMonitor @Inject constructor(
 
     @VisibleForTesting
     companion object {
+        val CAMERA_WHITELIST_PKG = arrayOf(
+            "com.crdroid.faceunlock",
+        )
         val OPS_MIC_CAMERA = intArrayOf(AppOpsManager.OP_CAMERA,
                 AppOpsManager.OP_PHONE_CALL_CAMERA, AppOpsManager.OP_RECORD_AUDIO,
                 AppOpsManager.OP_PHONE_CALL_MICROPHONE,
@@ -85,7 +88,8 @@ class AppOpsPrivacyItemMonitor @Inject constructor(
         ) {
             synchronized(lock) {
                 // Check if we care about this code right now
-                if (code in OPS_MIC_CAMERA && !micCameraAvailable) {
+                if (code in OPS_MIC_CAMERA && !micCameraAvailable
+                        || packageName in CAMERA_WHITELIST_PKG) {
                     return
                 }
                 if (code in OPS_LOCATION && !locationAvailable) {
@@ -212,6 +216,10 @@ class AppOpsPrivacyItemMonitor @Inject constructor(
             AppOpsManager.OP_RECEIVE_AMBIENT_TRIGGER_AUDIO,
             AppOpsManager.OP_RECORD_AUDIO -> PrivacyType.TYPE_MICROPHONE
             else -> return null
+        }
+        if (type == PrivacyType.TYPE_CAMERA && !micCameraAvailable
+                || appOpItem.packageName in CAMERA_WHITELIST_PKG) {
+            return null
         }
         val app = PrivacyApplication(appOpItem.packageName, appOpItem.uid)
         return PrivacyItem(type, app, appOpItem.timeStartedElapsed, appOpItem.isDisabled)
