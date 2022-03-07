3b9612692f3784fe48539144b070cb97675099f0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-03-07 22:06:27 +0530

    SystemUI: Add roaming indicator to statusbar tuner
    Dhina17: Ported to 14
    
    Change-Id: I326e1d54019179f0505a558b7e57ab6a2479cdf2
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconsInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/domain/interactor/MobileIconsInteractor.kt
@@ -110,6 +110,9 @@ interface MobileIconsInteractor {
     /** True if we're configured to force-hide the mobile icons and false otherwise. */
     val isForceHidden: Flow<Boolean>
 
+    /** True if we're configured to force-hide the roaming and false otherwise. */
+    val isRoamingForceHidden: Flow<Boolean>
+
     /**
      * Vends out a [MobileIconInteractor] tracking the [MobileConnectionRepository] for the given
      * subId.
@@ -369,6 +372,11 @@ constructor(
             .map { it.contains(ConnectivitySlot.MOBILE) }
             .stateIn(scope, SharingStarted.WhileSubscribed(), false)
 
+    override val isRoamingForceHidden: Flow<Boolean> =
+        connectivityRepository.forceHiddenSlots
+            .map { it.contains(ConnectivitySlot.ROAMING) }
+            .stateIn(scope, SharingStarted.WhileSubscribed(), false)
+
     /** Vends out new [MobileIconInteractor] for a particular subId */
     override fun getMobileConnectionInteractorForSubId(subId: Int): MobileIconInteractor =
         reuseCache[subId]?.get() ?: createMobileConnectionInteractorForSubId(subId)
@@ -385,6 +393,7 @@ constructor(
                 defaultMobileIconGroup,
                 isDefaultConnectionFailed,
                 isForceHidden,
+                isRoamingForceHidden,
                 mobileConnectionsRepo.getRepoForSubId(subId),
                 context,
             )
