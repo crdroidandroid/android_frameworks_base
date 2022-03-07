b8f52d8901394f262b4454b576d62641a3bfe932
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-03-07 22:06:27 +0530

    SystemUI: Add roaming indicator to statusbar tuner
    Dhina17: Ported to 14
    
    Change-Id: I326e1d54019179f0505a558b7e57ab6a2479cdf2
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconViewModel.kt
@@ -52,6 +52,7 @@ interface MobileIconViewModelCommon {
     val icon: Flow<SignalIconModel>
     val contentDescription: Flow<ContentDescription?>
     val roaming: Flow<Boolean>
+    val isRoamingVisible: Flow<Boolean>
     /** The RAT icon (LTE, 3G, 5G, etc) to be displayed. Null if we shouldn't show anything */
     val networkTypeIcon: Flow<Icon.Resource?>
     /** The slice attribution. Drawn as a background layer */
@@ -290,6 +291,18 @@ private class CellularIconViewModel(
                 columnName = "roaming",
                 initialValue = false,
             )
+            .stateIn(scope, SharingStarted.WhileSubscribed(), false)
+
+    override val isRoamingVisible: StateFlow<Boolean> =
+        combine(
+                roaming,
+                iconInteractor.isRoamingForceHidden
+            ) { isRoaming, isHidden ->
+                // If it's force hidden, just hide.
+                // Otherwise follow roaming state
+                isRoaming && !isHidden
+            }
+            .distinctUntilChanged()
             .stateIn(scope, SharingStarted.WhileSubscribed(), false)
 
     private val activity: Flow<DataActivityModel?> =
