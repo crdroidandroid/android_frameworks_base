a838702553986c38af9f2f73c5197eaa13d8facd
LibXZR <i@xzr.moe>
2022-03-17 22:58:19 +0800

    base: Add support for application downgrade [1/2]
    Can be useful in some cases.
    
    Change-Id: I999337cc55e9675cedde35191a6438d90744e5aa
    Signed-off-by: LibXZR <i@xzr.moe>
    
    Adapt to Android 13
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- services/core/java/com/android/server/pm/InstallPackageHelper.java
+++ services/core/java/com/android/server/pm/InstallPackageHelper.java
@@ -2908,8 +2908,9 @@ final class InstallPackageHelper {
                 // system apps case like below.
             } else if (dataOwnerPkg != null && !dataOwnerPkg.isSdkLibrary()) {
                 if (!PackageManagerServiceUtils.isDowngradePermitted(installFlags,
-                        dataOwnerPkg.isDebuggable())) {
-                    // Downgrade is not permitted; a lower version of the app will not be allowed
+                        dataOwnerPkg.isDebuggable())
+                        && android.provider.Settings.Global.getInt(mContext.getContentResolver(),
+                        android.provider.Settings.Global.PM_DOWNGRADE_ALLOWED, 0) == 0) {
                     try {
                         PackageManagerServiceUtils.checkDowngrade(dataOwnerPkg, pkgLite);
                     } catch (PackageManagerException e) {
