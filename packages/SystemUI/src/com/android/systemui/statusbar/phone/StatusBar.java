34c133232ae40b1d864cdb4e3fe330d084d767e7
Pranav Vashi <neobuddy89@gmail.com>
2022-03-20 02:40:14 +0530

    Revert "SystemUI: Add visualizer feature"
    This reverts commit 2168ac6889854e6be034670cb1e2e5a82d493c54.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -203,7 +203,6 @@ import com.android.systemui.statusbar.PowerButtonReveal;
 import com.android.systemui.statusbar.PulseExpansionHandler;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.SysuiStatusBarStateController;
-import com.android.systemui.statusbar.VisualizerView;
 import com.android.systemui.statusbar.charging.WiredChargingRippleController;
 import com.android.systemui.statusbar.connectivity.NetworkController;
 import com.android.systemui.statusbar.events.SystemStatusAnimationScheduler;
@@ -489,7 +488,6 @@ public class StatusBar extends SystemUI implements
     protected PhoneStatusBarView mStatusBarView;
     private PhoneStatusBarViewController mPhoneStatusBarViewController;
     private AuthRippleController mAuthRippleController;
-    private VisualizerView mVisualizerView;
     private int mStatusBarWindowState = WINDOW_STATE_SHOWING;
     protected NotificationShadeWindowController mNotificationShadeWindowController;
     private final StatusBarWindowController mStatusBarWindowController;
@@ -1593,7 +1591,6 @@ public class StatusBar extends SystemUI implements
         mFragmentService.addFragmentInstantiationProvider(mStatusBarComponent);
 
         mNotificationShadeWindowView = mStatusBarComponent.getNotificationShadeWindowView();
-        mVisualizerView = mStatusBarComponent.getVisualizerView();
         mNotificationShadeWindowViewController = mStatusBarComponent
                 .getNotificationShadeWindowViewController();
         mNotificationShadeWindowController.setNotificationShadeView(mNotificationShadeWindowView);
@@ -1690,10 +1687,6 @@ public class StatusBar extends SystemUI implements
 
     public ViewGroup getBouncerContainer() {
         return mNotificationShadeWindowViewController.getBouncerContainer();
-    }
-
-    public VisualizerView getVisualizerView() {
-        return mVisualizerView;
     }
 
     public int getStatusBarHeight() {
@@ -3383,7 +3376,6 @@ public class StatusBar extends SystemUI implements
                 && visibleNotOccludedOrWillBe);
 
         mNotificationPanelViewController.setDozing(mDozing, animate, mWakeUpTouchLocation);
-        mVisualizerView.setDozing(mDozing);
         updateQsExpansionEnabled();
         Trace.endSection();
     }
@@ -3817,14 +3809,12 @@ public class StatusBar extends SystemUI implements
         @Override
         public void onScreenTurnedOn() {
             mScrimController.onScreenTurnedOn();
-            mVisualizerView.setVisible(true);
         }
 
         @Override
         public void onScreenTurnedOff() {
             mFalsingCollector.onScreenOff();
             mScrimController.onScreenTurnedOff();
-            mVisualizerView.setVisible(false);
             updateIsKeyguard();
         }
     };
@@ -4580,7 +4570,6 @@ public class StatusBar extends SystemUI implements
                     checkBarModes();
                     updateScrimController();
                     mPresenter.updateMediaMetaData(false, mState != StatusBarState.KEYGUARD);
-                    mVisualizerView.setStatusBarState(newState);
                     updateKeyguardState();
                     Trace.endSection();
                 }
