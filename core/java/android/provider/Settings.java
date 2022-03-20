a072fbb2ea2332c23c58ed370b355120d6be1dae
Pranav Vashi <neobuddy89@gmail.com>
2022-03-20 17:03:36 +0530

    Allow to quickly open Running Services from QS panel [1/2]
    @neobuddy89:
    
    Rewrite based on ezio84 code - https://github.com/crdroidandroid/android_frameworks_base/commit/1917ca4cb9947ed76973755cfdd0cfd26b6d7fe1
    
    Change-Id: I78f78c3e96a3a699bda1a410f68d9fbca468436e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5961,6 +5961,12 @@ public final class Settings {
         public static final String QS_FOOTER_SHOW_EDIT = "qs_footer_show_edit";
 
         /**
+         * Show Running Services icon in QS Footer.
+         * @hide
+         */
+        public static final String QS_FOOTER_SHOW_SERVICES = "qs_footer_show_services";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
