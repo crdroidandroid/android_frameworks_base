d1fcd924dfbc41ba9fe0cb5a45e3f9dd59a88f1f
Anay Wadhera <anay1018@gmail.com>
2022-03-23 01:06:26 +0900

    SystemUI: catch up to 12L SystemUIDefaultModule
    Change-Id: Ib316311e2eef30a7dbc296a30397b143bbf35aab
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/7113
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/crdroid/android/systemui/dagger/CrDroidSystemUIModule.java
+++ packages/SystemUI/src/com/crdroid/android/systemui/dagger/CrDroidSystemUIModule.java
@@ -62,8 +62,7 @@ import com.android.systemui.plugins.qs.QSFactory;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.EnhancedEstimates;
 import com.android.systemui.power.EnhancedEstimatesImpl;
-import com.android.systemui.power.PowerUI;
-import com.android.systemui.power.PowerNotificationWarnings;
+import com.android.systemui.power.dagger.PowerModule;
 import com.android.systemui.qs.dagger.QSModule;
 import com.android.systemui.qs.tileimpl.QSFactoryImpl;
 import com.android.systemui.recents.OverviewProxyService;
@@ -167,6 +166,7 @@ import javax.inject.Named;
 
 @Module(includes = {
         MediaModule.class,
+        PowerModule.class,
         QSModule.class,
         VolumeModule.class  
 })
@@ -258,12 +258,6 @@ public abstract class CrDroidSystemUIModule {
             ConfigurationController configurationController) {
         return new HeadsUpManagerPhone(context, statusBarStateController, bypassController,
                 groupManager, configurationController);
-    }
-
-    @SysUISingleton
-    @Provides
-    static PowerUI.WarningsUI provideWarningsUi(PowerNotificationWarnings controllerImpl) {
-        return controllerImpl;
     }
 
     @Binds
