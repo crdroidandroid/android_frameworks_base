a9fad3e7f991d50f9d8b93a3a51f353e3398332c
maxwen <max.weninger@gmail.com>
2022-03-24 22:43:46 +0100

    SystemUI: fix initial notif shade layout on split mode
    there is no squishiness on split mode. layout is wrong on first open
    cause mSquishinessFraction is undefined (-Infinity)
    
    Change-Id: I17611cb5f52d1bfa884afdcfc094b504fa290bc0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -149,7 +149,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
     }
 
     public void setSquishinessFraction(float squishinessFraction) {
-        mView.setSquishinessFraction(squishinessFraction);
+        mView.setSquishinessFraction(mShouldUseSplitNotificationShade ? 1f : squishinessFraction);
     }
 
     @Override
