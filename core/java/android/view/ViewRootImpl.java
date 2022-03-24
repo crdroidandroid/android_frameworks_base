01bd43db453b9c178fdd24eee9a5814a27d1d670
maxwen <max.weninger@gmail.com>
2022-03-24 16:36:27 +0100

    base: log spill when dragging
    Change-Id: Ibe8cc38baca134ee97ead015b9a37c1430f86527
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -6531,7 +6531,6 @@ public final class ViewRootImpl implements ViewParent,
         }
         if (x < 0 || x >= mView.getWidth() || y < 0 || y >= mView.getHeight()) {
             // E.g. when moving window divider with mouse
-            Slog.d(mTag, "updatePointerIcon called with position out of bounds");
             return false;
         }
         final PointerIcon pointerIcon = mView.onResolvePointerIcon(event, pointerIndex);
