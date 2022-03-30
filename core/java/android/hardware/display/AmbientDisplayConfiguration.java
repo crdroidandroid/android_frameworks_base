a8a8a59751438b827246e06aaad8817a4022b72d
ShevT <evgeny.shishkov@gmail.com>
2022-03-30 12:43:49 +0300

    fwb: Set "Pick up" off by default
     We will follow the desired crDroid settings.
     By default, this setting is disabled.
     See:
     https://github.com/crdroidandroid/android_packages_apps_crDroidSettings/blob/12.1/res/xml/doze_settings.xml#L92
    
     Otherwise, we get inconsistent settings. In the crDroid
     settings it is disabled, but in the system settings it
     is enabled and working.
    
    Change-Id: Id73cd525fd6bf2dce4ef6c5496422f1aaedc49b3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -100,7 +100,7 @@ public class AmbientDisplayConfiguration {
 
     /** @hide */
     public boolean pickupGestureEnabled(int user) {
-        return boolSettingDefaultOn(Settings.Secure.DOZE_PICK_UP_GESTURE, user)
+        return boolSettingDefaultOff(Settings.Secure.DOZE_PICK_UP_GESTURE, user)
                 && dozePickupSensorAvailable();
     }
 
