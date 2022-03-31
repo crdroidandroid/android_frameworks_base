7696ea974c7996089981fd0b32f8a254e01b1ee6
maxwen <max.weninger@gmail.com>
2022-04-01 01:36:02 +0200

    SystemUI: Add missing init for isDeviceProvisioned in PhoneStatusBarPolicy
    Change-Id: Ia376cd89877749baf0baa9d50639d7d4673043bd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -257,6 +257,7 @@ public class PhoneStatusBarPolicy
                 com.android.internal.R.string.status_bar_screen_record);
         mSlotFirewall = resources.getString(R.string.status_bar_firewall_slot);
         mSlotNfc = resources.getString(com.android.internal.R.string.status_bar_nfc);
+        mCurrentUserSetup = mProvisionedController.isDeviceProvisioned();
 
         mDisplayId = displayId;
         mSharedPreferences = sharedPreferences;
