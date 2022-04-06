5214d6976c143c7041a576a4ffc3d88f39ad7a67
Dylanger Daly <dylanger@diagnostix.io>
2022-04-06 11:58:34 +1000

    All packages installed via Google Play Store

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -2623,6 +2623,9 @@ public class ApplicationPackageManager extends PackageManager {
     @Override
     public String getInstallerPackageName(String packageName) {
         try {
+            if (!packageName.equals("com.aurora.store") && packageName != null) {
+                return "com.android.vending";
+            }
             return mPM.getInstallerPackageName(packageName);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
