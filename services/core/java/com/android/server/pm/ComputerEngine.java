db961e0d1a8bcb1279912f2239581529e4552cd4
Dylanger Daly <dylanger@diagnostix.io>
2022-04-06 11:58:34 +1000

    All packages installed via Google Play Store

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -5172,6 +5172,10 @@ public class ComputerEngine implements Computer {
             }
         }
 
+        if (!packageName.equals("com.aurora.store") && installerPackageName != null) {
+            installerPackageName = "com.android.vending";
+        }
+
         if (installSource.isInitiatingPackageUninstalled) {
             // We can't check visibility in the usual way, since the initiating package is no
             // longer present. So we apply simpler rules to whether to expose the info:
