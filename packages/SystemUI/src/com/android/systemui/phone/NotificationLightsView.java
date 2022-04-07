DerpLab/platform_frameworks_base_old

SystemUI: Allow changing duration of ambient edge light [1/2]

packages/SystemUI/src/com/android/systemui/aosip/NotificationLightsView.java
@@ -92,6 +92,9 @@ public class NotificationLightsView extends RelativeLayout {
         int color = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF,
                 UserHandle.USER_CURRENT);
+        int duration = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_DURATION, 2,
+                UserHandle.USER_CURRENT) * 1000;
         if (Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.PULSE_AMBIENT_AUTO_COLOR, 0,
                 UserHandle.USER_CURRENT) == 1) {
@@ -121,7 +124,7 @@ public class NotificationLightsView extends RelativeLayout {
         leftView.setColorFilter(color);
         rightView.setColorFilter(color);
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
-        mLightAnimator.setDuration(2000);
+        mLightAnimator.setDuration(duration);
         mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
                 Log.e("NotificationLightsView", "onAnimationUpdate");

crdroidandroid/android_frameworks_base

Change-Id: Iaa592ea09a5653e76446c8dc6c05bb536c77ec3f
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/phone/NotificationLightsView.java
@@ -83,6 +83,9 @@ public class NotificationLightsView extends RelativeLayout {
         int color = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF,
                 UserHandle.USER_CURRENT);
+        int duration = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.PULSE_AMBIENT_LIGHT_DURATION, 2,
+                UserHandle.USER_CURRENT) * 1000;
         StringBuilder sb = new StringBuilder();
         sb.append("animateNotification color ");
         sb.append(Integer.toHexString(color));
@@ -92,7 +95,7 @@ public class NotificationLightsView extends RelativeLayout {
         leftView.setColorFilter(color);
         rightView.setColorFilter(color);
         mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
-        mLightAnimator.setDuration(2000);
+        mLightAnimator.setDuration(duration);
         mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
             public void onAnimationUpdate(ValueAnimator animation) {
                 Log.e("NotificationLightsView", "onAnimationUpdate");