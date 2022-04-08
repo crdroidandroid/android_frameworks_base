66d6e31da0cacd98d898b373323da9eae81861bf
Albert I <kras@raphielgang.org>
2022-04-08 18:59:52 +0800

    core: Wipe package cache on upgrade even on user build
    As we're spoofing builds by pretending it to be user, let's just do this
    for everyone.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I5d967c25bd9c4890549d5d20f5e66b0c6845528c
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/7726
    Reviewed-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8592,7 +8592,7 @@ public class PackageManagerService extends IPackageManager.Stub
         // NOTE: When no BUILD_NUMBER is set by the build system, it defaults to a build
         // that starts with "eng." to signify that this is an engineering build and not
         // destined for release.
-        if (mIsUserDebugBuild && mIncrementalVersion.startsWith("eng.")) {
+        if (mIncrementalVersion.startsWith("eng.")) {
             Slog.w(TAG, "Wiping cache directory because the system partition changed.");
 
             // Heuristic: If the /system directory has been modified recently due to an "adb sync"
