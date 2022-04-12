e3248982983a065bd8e66b8a43d584ca237c4f53
Chris Phlipot <cphlipot@fb.com>
2022-04-12 11:29:24 -0700

    Fix swapped backup/heavy oom adj labels
    The labels for the "backup" and "heavy" oom adj are swapped relative
    to the ordering in DUMP_MEM_OOM_ADJ. Swap the order of the labels
    to be consistent.
    
    This should resolve an issue where meminfo mixes up heavy and backup
    in the text-based report.
    
    Change-Id: Idbc985c3080eada0945c21825659b031f1f7f2a5
    Signed-off-by: Chris Phlipot <cphlipot@fb.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -10387,7 +10387,7 @@ public class ActivityManagerService extends IActivityManager.Stub
             "Native",
             "System", "Persistent", "Persistent Service", "Foreground",
             "Visible", "Perceptible", "Perceptible Low", "Perceptible Medium",
-            "Heavy Weight", "Backup",
+            "Backup", "Heavy Weight",
             "A Services", "Home",
             "Previous", "B Services", "Cached"
     };
@@ -10395,7 +10395,7 @@ public class ActivityManagerService extends IActivityManager.Stub
             "native",
             "sys", "pers", "persvc", "fore",
             "vis", "percept", "perceptl", "perceptm",
-            "heavy", "backup",
+            "backup", "heavy",
             "servicea", "home",
             "prev", "serviceb", "cached"
     };
