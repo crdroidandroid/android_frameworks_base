805f4faf6e1155fcaae7e24b35f61cab2b6147a7
Pranav Vashi <neobuddy89@gmail.com>
2022-04-16 10:38:36 +0530

    Fixes and improvements for Black Theme
    * Add to SettingsObserver for observing toggle behavior.
    * Apply only with dark theme - looks awful on light mode.
    * Add configuration listener to reapply theme when dark mode changes.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/org/pixelexperience/systemui/theme/CustomThemeOverlayController.kt
+++ packages/SystemUI/src/org/pixelexperience/systemui/theme/CustomThemeOverlayController.kt
@@ -35,6 +35,7 @@ import com.android.systemui.dump.DumpManager
 import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.keyguard.WakefulnessLifecycle
 import com.android.systemui.settings.UserTracker
+import com.android.systemui.statusbar.policy.ConfigurationController
 import com.android.systemui.statusbar.policy.DeviceProvisionedController
 import com.android.systemui.theme.ThemeOverlayApplier
 import com.android.systemui.theme.ThemeOverlayController
@@ -71,6 +72,7 @@ class CustomThemeOverlayController @Inject constructor(
     dumpManager: DumpManager,
     featureFlags: FeatureFlags,
     wakefulnessLifecycle: WakefulnessLifecycle,
+    configurationController: ConfigurationController,
 ) : ThemeOverlayController(
     context,
     broadcastDispatcher,
@@ -86,6 +88,7 @@ class CustomThemeOverlayController @Inject constructor(
     dumpManager,
     featureFlags,
     wakefulnessLifecycle,
+    configurationController,
 ), Tunable {
     private lateinit var cond: Zcam.ViewingConditions
     private lateinit var targets: MaterialYouTargets
