ac52db1ceeacb005e3a258e89b0288698140b20c
Pranav Vashi <neobuddy89@gmail.com>
2022-04-22 20:23:15 +0530

    Optional haptic feedback on back gesture [1/2]
    This is inspired from
    https://github.com/syberia-project/platform_frameworks_base/commit/2a57b330c1f697ef23460047bd62f6dd20f3368a
    
    However, we do not want to ditch AOSP implementation.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -353,6 +353,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
 
     private boolean mBlockedGesturalNavigation;
     private boolean mIsBackGestureArrowEnabled;
+    private boolean mIsEdgeHapticEnabled;
 
     EdgeBackGestureHandler(
             Context context,
@@ -460,6 +461,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         mIsBackGestureAllowed =
                 !mGestureNavigationSettingsObserver.areNavigationButtonForcedVisible();
         mIsBackGestureArrowEnabled = mGestureNavigationSettingsObserver.getBackArrowGesture();
+        mIsEdgeHapticEnabled = mGestureNavigationSettingsObserver.getEdgeHapticEnabled();
 
         final DisplayMetrics dm = res.getDisplayMetrics();
         final float defaultGestureHeight = res.getDimension(
@@ -948,6 +950,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             if (mAllowGesture) {
                 mEdgeBackPlugin.setIsLeftPanel(mIsOnLeftEdge);
                 mEdgeBackPlugin.setBackArrowVisibility(mIsBackGestureArrowEnabled);
+                mEdgeBackPlugin.setEdgeHapticEnabled(mIsEdgeHapticEnabled);
                 mEdgeBackPlugin.onMotionEvent(ev);
                 dispatchToBackAnimation(ev);
             }
