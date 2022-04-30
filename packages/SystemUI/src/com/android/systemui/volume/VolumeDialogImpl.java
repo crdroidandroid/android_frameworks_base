2ac4d6c38921ee0ccdbcc12091fb1dbef4076885
Mesquita <mesquita@aospa.co>
2022-04-30 12:52:17 +0000

    SystemUI: Show notification row when it's unlinked to ringer volume
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Change-Id: Id7a2602153dbc94eb40caf70946d2b31e4a3c19f
    
    SystemUI: Show notification row only when unlinked
    
    ("SystemUI: Show notification row when it's unliked to ringer volume")
    caused notification row to show in expanded volume panel
    even when notification and ring volumes are linked. Fix
    it so that it shows only when they are unlinked.
    
    Change-Id: I1d4768974c07247bae38470260f7917da44bd911
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1661,6 +1661,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean isExpandableRowH(VolumeRow row) {
         return row != null && row != mDefaultRow && !row.defaultStream
                 && (row.stream == STREAM_RING
+                        || (row.stream == STREAM_NOTIFICATION && !mState.linkedNotification)
                         || row.stream == STREAM_ALARM
                         || row.stream == STREAM_MUSIC);
     }
