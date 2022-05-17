a1f905f3c05fa502cf0abe55c890a842fcec7191
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-17 23:20:38 +0800

    reTicker: Do not show when content/icon is null
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -50,6 +50,7 @@ import android.app.PendingIntent;
 import android.app.StatusBarManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -72,6 +73,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.VibrationEffect;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.transition.ChangeBounds;
 import android.transition.TransitionManager;
 import android.util.IndentingPrintWriter;
@@ -5221,10 +5223,14 @@ public final class NotificationPanelViewController extends PanelViewController {
                 } else {
                     icon = mView.getContext().getPackageManager().getApplicationIcon(pkgname);
                 }
-            } catch (android.content.pm.PackageManager.NameNotFoundException e) {
+            } catch (NameNotFoundException e) {
+                return;
             }
-            if (row.getEntry().getSbn().getNotification().extras.getString("android.text") != null) {
-                reTickerContent = row.getEntry().getSbn().getNotification().extras.getString("android.text");
+            String content = row.getEntry().getSbn().getNotification().extras.getString("android.text");
+            if (!TextUtils.isEmpty(content)) {
+                reTickerContent = content;
+            } else {
+                return;
             }
             String reTickerAppName = row.getEntry().getSbn().getNotification().extras.getString("android.title");
             PendingIntent reTickerIntent = row.getEntry().getSbn().getNotification().contentIntent;
@@ -5236,9 +5242,9 @@ public final class NotificationPanelViewController extends PanelViewController {
                 col = row.getEntry().getSbn().getNotification().color;
                 mAppExceptions = mView.getContext().getResources().getStringArray(R.array.app_exceptions);
                 //check if we need to override the color
-                for (int i=0; i < mAppExceptions.length; i++) {
+                for (int i = 0; i < mAppExceptions.length; i++) {
                     if (mAppExceptions[i].contains(pkgname)) {
-                        col = Color.parseColor(mAppExceptions[i+=1]);
+                        col = Color.parseColor(mAppExceptions[i += 1]);
                     }
                 }
                 dw.setTint(col);
