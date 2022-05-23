78f916831f9c6b1340a474f1a5c9689c9383978f
althafvly <althafvly@gmail.com>
2022-05-23 13:53:01 +0530

    SystemUI: Reset QS on UI mode change
    - Fixes QS header position gets messed up
      in UI mode switch.
    
    Change-Id: I3bc24decafc4bf691b29a0261be4637b0126be91
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4409,6 +4409,12 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         }
 
         @Override
+        public void onUiModeChanged() {
+            if (DEBUG_LOGCAT) Log.d(TAG, "onUiModeChanged");
+            resetViews(true);
+        }
+
+        @Override
         public void onSmallestScreenWidthChanged() {
             Trace.beginSection("onSmallestScreenWidthChanged");
             debugLog("onSmallestScreenWidthChanged");
