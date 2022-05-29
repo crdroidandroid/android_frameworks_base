48ef2f32b520dc7206494ce82220d81d9618f90e
Pranav Vashi <neobuddy89@gmail.com>
2022-05-29 19:34:09 +0530

    PixelPropsUtils: Exclude recorder from pixel props
    * Just like YouTube apps, Recorder shows device is Pixel 5.
      There is no special pixel feature associated with this app, exclude it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -77,7 +77,8 @@ public class PixelPropsUtils {
         "com.google.ar.core",
         "com.google.android.youtube",
         "com.google.android.apps.youtube.kids",
-        "com.google.android.apps.youtube.music"
+        "com.google.android.apps.youtube.music",
+        "com.google.android.apps.recorder"
     };
 
     private static final Map<String, Object> propsToChangeROG1;
