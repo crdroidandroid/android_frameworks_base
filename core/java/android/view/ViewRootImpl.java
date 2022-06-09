c7943a12bf073b62594cd0cc636322fecc5dbf79
Cheng Shi <shicheng@xiaomi.corp-partner.google.com>
2022-06-09 09:09:56 +0000

    Fix the problem that the turn_screen_on Activity does not draw.
    A phone call came when the screen was off, displayState may change from Display.STATE_OFF to STATE_ON before registerDisplayListener, which causes ViewRootImpl.mAttachInfo.mDisplayState of InCallActivity to remain Display.STATE_OFF, which causes performDraw to return directly. So we should update ViewRootImpl.mAttachInfo.mDisplayState after registerDisplayListener.
    
    Bug: 235446909
    
    Change-Id: I60b2da453da0ac5c4df6e5a1040defe4bfe726ec
    Merged-In: I60b2da453da0ac5c4df6e5a1040defe4bfe726ec
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -1102,7 +1102,6 @@ public final class ViewRootImpl implements ViewParent,
             if (mView == null) {
                 mView = view;
 
-                mAttachInfo.mDisplayState = mDisplay.getState();
                 mDisplayInstallOrientation = mDisplay.getInstallOrientation();
                 mViewLayoutDirectionInitial = mView.getRawLayoutDirection();
                 mFallbackEventHandler.setView(view);
@@ -1310,6 +1309,9 @@ public final class ViewRootImpl implements ViewParent,
                 }
 
                 registerListeners();
+                // We should update mAttachInfo.mDisplayState after registerDisplayListener
+                // because displayState might be changed before registerDisplayListener.
+                mAttachInfo.mDisplayState = mDisplay.getState();
                 if ((res & WindowManagerGlobal.ADD_FLAG_USE_BLAST) != 0) {
                     mUseBLASTAdapter = true;
                 }
