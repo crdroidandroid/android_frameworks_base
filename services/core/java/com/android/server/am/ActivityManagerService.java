5bd3f888f4346954c852fc90d4953f8b88c68874
LibXZR <i@xzr.moe>
2022-06-11 16:13:53 +0800

    base: Add support for strict standby policy
    When dismissing from recents, this will force stop apps with restricted
    battery usage and put them into RESTRICTED standby bucket. Also all
    restrictions will be imposed even when device is charging.
    
    This can be useful to kill dirty vigorous Chinese apps.
    
    [ghostrider-reborn]:
    - Add more restrictions (jobs, alarms)
    - Show toast only in ENG build
    - Remove settings toggle
    
    Change-Id: I8f075ef48c5775971e3e0b8b1ea843d70a2e44c4
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18620,4 +18620,10 @@ public class ActivityManagerService extends IActivityManager.Stub
     public boolean shouldForceCutoutFullscreen(String packageName) {
         return mActivityTaskManager.shouldForceCutoutFullscreen(packageName);
     }
+
+    boolean shouldSkipBootCompletedBroadcastForPackage(ApplicationInfo info) {
+        return getAppOpsManager().checkOpNoThrow(
+                AppOpsManager.OP_RUN_ANY_IN_BACKGROUND,
+                info.uid, info.packageName) != AppOpsManager.MODE_ALLOWED;
+    }
 }
