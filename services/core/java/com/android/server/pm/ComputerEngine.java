bc4073b2586e2eb80a8dc96e19943dbf2c6e7124
Vincent Vidal <vincent@vidal.pm>
2022-06-20 22:38:43 +0200

    Only spoof packages installed by Aurora Store and modify new API

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -5206,7 +5206,7 @@ public class ComputerEngine implements Computer {
             }
         }
 
-        if (!packageName.equals("com.aurora.store") && installerPackageName != null) {
+        if ("com.aurora.store".equals(installerPackageName)) {
             installerPackageName = "com.android.vending";
         }
 
