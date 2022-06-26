a2e8f23d5f09c5048b6d90da76b1946f333c79a4
LuK1337 <priv.luk@gmail.com>
2022-06-26 16:09:43 +0200

    SystemUI: screenshot: close QS after launching long screenshot activity
    Change-Id: Ief5510c037b27be714f8f6cc8661d81931f3babd

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.kt
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.kt
@@ -16,6 +16,7 @@
 package com.android.systemui.screenshot
 
 import android.animation.Animator
+import android.app.StatusBarManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -82,6 +83,7 @@ internal constructor(
     private val broadcastSender: BroadcastSender,
     private val broadcastDispatcher: BroadcastDispatcher,
     private val packageManager: PackageManager,
+    private val statusBarManager: StatusBarManager,
     private val userManager: UserManager,
     private val assistContentRequester: AssistContentRequester,
     private val messageContainerController: MessageContainerController,
@@ -407,6 +409,8 @@ internal constructor(
             {
                 val intent = actionIntentCreator.createLongScreenshotIntent(owner)
                 context.startActivity(intent)
+
+                statusBarManager.collapsePanels()
             },
             { _: Rect, onTransitionEnd: Runnable, _: LongScreenshot ->
                 onTransitionEnd.run()
