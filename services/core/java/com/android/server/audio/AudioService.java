db7a4661a966aec6e6707d7083bb5239c73b770e
Pranav Vashi <neobuddy89@gmail.com>
2022-06-26 17:53:17 +0530

    AudioService: Fix issue with linked notification volume
    * A nasty bug where ringer mode is internally reset when switching linked notification volume.
    
    Steps to reproduce:
    1. Put device in vibrate mode
    2. Plug in wired headsets
    3. Create notification via Notification Maker app. There will be no sound.
       (Ensure notification is default mode and not silent mode)
    4. Toggle "Linked notification" option under Sound settings
    5. Create notification again. You will hear message alert (and other system sounds)
    
    * Fix this by initializing streams and volumes again on toggle.
      Do not use mSettingsLock for this.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iec8d140f9e86c9c13cd4f49052bf155ed75f9bde

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -8594,14 +8594,13 @@ public class AudioService extends IAudioService.Stub
                 updateEncodedSurroundOutput();
                 sendEnabledSurroundFormats(mContentResolver, mSurroundModeChanged);
                 updateAssistantUIdLocked(/* forceUpdate= */ false);
-
-                boolean linkNotificationWithVolume = Settings.Secure.getInt(mContentResolver,
-                        Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
-                if (linkNotificationWithVolume != mLinkNotificationWithVolume) {
-                    mLinkNotificationWithVolume = linkNotificationWithVolume;
-                    createStreamStates();
-                    updateStreamVolumeAlias(true, TAG);
-                }
+            }
+            boolean linkNotificationWithVolume = Settings.Secure.getInt(mContentResolver,
+                    Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
+            if (linkNotificationWithVolume != mLinkNotificationWithVolume) {
+                mLinkNotificationWithVolume = linkNotificationWithVolume;
+                onInitStreamsAndVolumes();
+                updateStreamVolumeAlias(true, TAG);
             }
         }
 
