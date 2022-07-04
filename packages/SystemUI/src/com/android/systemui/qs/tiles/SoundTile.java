3255f4f5a3239a65fe990e808621f9918a2d216a
DarkJoker360 <simoespo159@gmail.com>
2022-07-04 14:22:05 -0700

    QS: Define state for sound tile
    * Also use active state during ring mode.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -154,18 +154,21 @@ public class SoundTile extends QSTileImpl<BooleanState> {
                 state.label = mContext.getString(R.string.quick_settings_sound_ring);
                 state.contentDescription =  mContext.getString(
                         R.string.quick_settings_sound_ring);
+                state.state = Tile.STATE_ACTIVE;
                 break;
             case AudioManager.RINGER_MODE_VIBRATE:
                 state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_vibrate);
                 state.label = mContext.getString(R.string.quick_settings_sound_vibrate);
                 state.contentDescription =  mContext.getString(
                         R.string.quick_settings_sound_vibrate);
+                state.state = Tile.STATE_INACTIVE;
                 break;
             case AudioManager.RINGER_MODE_SILENT:
                 state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_silent);
                 state.label = mContext.getString(R.string.quick_settings_sound_dnd);
                 state.contentDescription =  mContext.getString(
                         R.string.quick_settings_sound_dnd);
+                state.state = Tile.STATE_INACTIVE;
                 break;
             default:
                 break;
