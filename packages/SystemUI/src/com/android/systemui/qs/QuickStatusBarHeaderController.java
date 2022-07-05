320fdb43ec934a0c4367d2bd41b7e382ea1ffc4e
markakash <akashniki@gmail.com>
2022-07-05 08:06:10 +0200

    base: Prevent NPE for Demo mode
    * https://katb.in/hobawiwofas
    
    Change-Id: I6bafa90a2dd8905e17883bd8898d6dd29165f480
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeaderController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeaderController.java
@@ -204,7 +204,9 @@ class QuickStatusBarHeaderController extends ViewController<QuickStatusBarHeader
 
         @Override
         public void onDemoModeStarted() {
-            mClockView.onDemoModeStarted();
+            if (mClockView != null) {
+                mClockView.onDemoModeStarted();
+            }
         }
 
         @Override
