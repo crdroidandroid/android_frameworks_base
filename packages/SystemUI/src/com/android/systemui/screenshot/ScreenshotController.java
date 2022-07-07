d2e65bd38d16251886f2e5222a5ea4358a911096
maxwen <max.weninger@gmail.com>
2022-07-07 16:48:06 +0400

    base: allow disable of screenshot shutter sound [1/2]
    @neobuddy89: Updated for A13.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -1164,6 +1164,9 @@ public class ScreenshotController {
 
     private void playCameraSound() {
         mCameraSound.addListener(() -> {
+            if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) != 1)
+                return;
             try {
                 MediaPlayer player = mCameraSound.get();
                 if (player != null) {
