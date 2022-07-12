5b9cf1c0bc82f9fefbf59c336f0d382dcff53e0a
Ido Ben-Hur <idoybh2@gmail.com>
2022-07-12 20:47:45 +0300

    SystemUI: CastTile: Open cast settings on long click
    And also Get rid of old and unused detail adapter
    Some other minor improvements

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
@@ -114,7 +114,6 @@ public class CastTile extends QSTileImpl<BooleanState> {
     @Override
     public BooleanState newTileState() {
         BooleanState state = new BooleanState();
-        state.handlesLongClick = false;
         return state;
     }
 
@@ -135,12 +134,7 @@ public class CastTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return new Intent(Settings.ACTION_CAST_SETTINGS);
-    }
-
-    @Override
-    protected void handleLongClick(@Nullable View view) {
-        handleClick(view);
+        return CAST_SETTINGS;
     }
 
     @Override
