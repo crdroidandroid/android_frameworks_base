2cbc3b644a3674ce4c11bdeeda93f95cc8ababa3
Pranav Vashi <neobuddy89@gmail.com>
2022-07-13 21:05:54 +0530

    SystemUI: Update black theme package overlay
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -108,7 +108,7 @@ import javax.inject.Inject;
 public class ThemeOverlayController extends CoreStartable implements Dumpable {
     protected static final String TAG = "ThemeOverlayController";
     protected static final String OVERLAY_BERRY_BLACK_THEME =
-            "org.lineageos.overlay.customization.blacktheme";
+            "com.android.system.theme.black";
     private static final boolean DEBUG = true;
 
     protected static final int NEUTRAL = 0;
