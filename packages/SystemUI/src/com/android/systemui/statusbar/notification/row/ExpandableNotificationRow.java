1cb7384e4496144fdfd0e9357bd70aa9c3068ebb
DillerOFire <niktofe1@gmail.com>
2022-07-27 15:28:16 +1000

    SystemUI: Switch notification background to monet on heads up
    Change-Id: If1822acc3ea604444f2083d7fadec06ffb8eec19
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
@@ -1047,6 +1047,9 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
         if (intrinsicHeight != getIntrinsicHeight()) {
             notifyHeightChanged(false /* needsAnimation */);
         }
+
+        setTranslucentBackground(!pinned);
+
         if (pinned) {
             setAnimationRunning(true);
             mExpandedWhenPinned = false;
