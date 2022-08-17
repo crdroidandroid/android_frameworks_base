af92c8d46ed30051330dacfc786141eb9178b13c
StarkDroid <gamerprince.exp@gmail.com>
2022-08-17 11:14:47 +0300

    Clock: Add ability to resize QS & Statusbar Clock [1/2]
    spkal01: Utilize Tuner Settings and simplify implementation
    
    Co-Authored-By: spkal01 <kalligeross@gmail.com>
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Change-Id: I4ca9d8fe6258b2dfa3e5ae7ab8bde2fb3372aff5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5587,6 +5587,18 @@ public final class Settings {
         public static final String SHOW_QS_DATE = "show_qs_date";
 
         /**
+         * Clock font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
+
+        /**
+         * Clock font size QS
+         * @hide
+         */
+        public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
