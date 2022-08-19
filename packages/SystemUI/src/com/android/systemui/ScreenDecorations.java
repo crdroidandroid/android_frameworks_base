182dff3aadd7d89a3e30cc64db49d79e1e85fb01
xi yu <yuxi1@xiaomi.com>
2022-08-19 08:56:14 +0000

    Fix SystemUI NPE when ScreenDecorations is disabled
    The variables mRoundedCornerFactory and mRoundedCornerResDelegate will be null if we disable ScrrenDecorations by the property debug.disable_screen_decorations. As a result, when we catch a bugreport, a NullPointerException will happen in method hasRoundedCorners() and mRoundedCornerResDelegate.dump(pw, args) because mRoundedCornerFactory and mRoundedCornerResDelegate are null.
    
    Bug: 243111393
    Test: manual
    Change-Id: Id8a45ea369530245cbad5df6dc634998f1904660
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
+++ packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
@@ -1178,7 +1178,12 @@ public class ScreenDecorations implements
                 }
             }
         }
-        mRoundedCornerResDelegate.dump(pw, args);
+        if (!DEBUG_DISABLE_SCREEN_DECORATIONS) {
+            mRoundedCornerResDelegate.dump(pw, args);
+        } else {
+            pw.println("  RoundedCornerResDelegate state:" +
+                       " skipped due to ScreenDecorations disabled");
+        }
         mDebugRoundedCornerDelegate.dump(pw);
     }
 
