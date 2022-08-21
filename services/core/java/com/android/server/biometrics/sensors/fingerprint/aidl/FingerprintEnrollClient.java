177f70aae694dab6f952f0f2d862eed4c3eac4f0
TheScarastic <warabhishek@gmail.com>
2022-08-21 18:23:47 +0700

    udfps: Add support for udfps on aod without dedicated sensor
    [markakash]: See, Google updated the onAcquiredGood to something more generic (onAcquired, with an acquiredInfo parameter)
    so the UdfpsOverlay can turn off HBM for the appropriate acquiredInfo messages. We are bascially adapting vendorCode to this method
    Change-Id: I4aa39efe3714bcc40ab3da862809ca9c8974c6bd
    
    This is squashed with the following commit:
    Author: TheScarastic <warabhishek@gmail.com>
    Date:   Sun Aug 21 18:40:57 2022 +0700
    
        udfps: Make AOD vendor code overlayable.
    
        Change-Id: If1aff1d0abb158487f7c99eba95124a2a9372c4f
    
    Test: test udfps on AOD without dedicated sensor
    
    Change-Id: I6f53efc5230ce48f22a02e6af665efc0d51c7052
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintEnrollClient.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintEnrollClient.java
@@ -169,7 +169,7 @@ public class FingerprintEnrollClient extends EnrollClient<AidlSession> implement
                 vibrateSuccess();
             }
             mSensorOverlays.ifUdfps(
-                    controller -> controller.onAcquired(getSensorId(), acquiredInfo));
+                    controller -> controller.onAcquired(getSensorId(), acquiredInfo, vendorCode));
         }
 
         mSensorOverlays.ifUdfps(controller -> {
