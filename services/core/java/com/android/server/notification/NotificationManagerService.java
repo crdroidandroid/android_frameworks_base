3247253281d35d1306453ea6b582fcc16e93ca09
WoongKi Min <wk0831.min@samsung.com>
2022-08-23 20:04:16 +0900

    Do not assistant delay for media notifications
    Due to delay, pressing the pause/play button quickly several times may cause malfunction.
    In addition, media notifications do not need an assistant service.
    
    Test: Make media notification
    Test: pressing pause/play/Previous/Next button quickly
    Test: Check music player
    Change-Id: I6ad9153506147db2d2d686d6d0bfffd9eef5717a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -8339,7 +8339,7 @@ public class NotificationManagerService extends SystemService {
                 }
 
                 // tell the assistant service about the notification
-                if (mAssistants.isEnabled()) {
+                if (mAssistants.isEnabled() && !notification.isMediaNotification()) {
                     mAssistants.onNotificationEnqueuedLocked(r);
                     mHandler.postDelayed(
                             new PostNotificationRunnable(r.getKey(), r.getSbn().getPackageName(),
