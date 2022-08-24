dd5079dcba299d3593b4b8150bd719f06a4a2eea
Josep del Rio <joseprio@google.com>
2022-08-24 11:00:59 +0000

    Remove unnecessary HashMap instantiation
    Was investigating something else, but noticed something that could be a tiny bit better; return an empty map from `Collections.emptyMap` instead of using a real `HashMap` to send an empty one.
    
    Test: Builds successfully
    
    Change-Id: Id11d39d3ea90e15bd4cb90031d6242d8e1f98819
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -15122,7 +15122,6 @@ public class TelephonyManager {
     @NonNull
     @RequiresFeature(PackageManager.FEATURE_TELEPHONY_CALLING)
     public Map<Integer, List<EmergencyNumber>> getEmergencyNumberList() {
-        Map<Integer, List<EmergencyNumber>> emergencyNumberList = new HashMap<>();
         try {
             ITelephony telephony = getITelephony();
             if (telephony != null) {
@@ -15135,7 +15134,7 @@ public class TelephonyManager {
             Log.e(TAG, "getEmergencyNumberList RemoteException", ex);
             ex.rethrowAsRuntimeException();
         }
-        return emergencyNumberList;
+        return Collections.emptyMap();
     }
 
     /**
