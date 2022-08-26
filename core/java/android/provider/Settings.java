29598799e566faa275ddf4ccd49e53c57d45ab87
t-m-w <tmw-likes-calyx@pm.me>
2022-08-26 15:45:17 +0000

    Move Bluetooth timeout back to AOSP settings
    This is so it can be accessed by the Bluetooth APEX module.
    
    Change-Id: I81941dc4e390b17e4ec8f57bf45327f3b8b525db

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -18037,6 +18037,12 @@ public final class Settings {
         public static final String WIFI_OFF_TIMEOUT = "wifi_off_timeout";
 
         /**
+         * The amount of time in milliseconds before bluetooth is turned off
+         * @hide
+         */
+        public static final String BLUETOOTH_OFF_TIMEOUT = "bluetooth_off_timeout";
+
+        /**
          * Settings migrated from Wear OS settings provider.
          * @hide
          */
--- packages/SettingsProvider/src/android/provider/settings/validators/GlobalSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/GlobalSettingsValidators.java
@@ -212,6 +212,21 @@ public class GlobalSettingsValidators {
                         "14400000",
                         "28800000"
                 }));
+        VALIDATORS.put(Global.BLUETOOTH_OFF_TIMEOUT,
+                new DiscreteValueValidator(new String[] {
+                        "0",
+                        "15000",
+                        "30000",
+                        "60000",
+                        "120000",
+                        "300000",
+                        "600000",
+                        "1800000",
+                        "3600000",
+                        "7200000",
+                        "14400000",
+                        "28800000"
+                }));
 
         VALIDATORS.put(Global.Wearable.HAS_PAY_TOKENS, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Global.Wearable.GMS_CHECKIN_TIMEOUT_MIN, ANY_INTEGER_VALIDATOR);
