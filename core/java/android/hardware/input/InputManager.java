b6845e5edda120388af968b52f4db29bccb09bb4
nift4 <nift4@protonmail.com>
2022-08-28 14:02:08 +0200

    InputManager: allow to force NULL cursor
    If VNC draws cursor on client side, we do not want to have a second
    cursor visible.
    
    Change-Id: Iab1b9477a9142f7ae23484f76285981bddca1880
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/input/InputManager.java
+++ core/java/android/hardware/input/InputManager.java
@@ -1257,6 +1257,15 @@ public final class InputManager {
         }
     }
 
+    /** @hide */
+    public void setForceNullCursor(boolean forceNullCursor) {
+        try {
+            mIm.setForceNullCursor(forceNullCursor);
+        } catch (RemoteException ex) {
+            throw ex.rethrowFromSystemServer();
+        }
+    }
+
     /**
      * Request or release pointer capture.
      * <p>
