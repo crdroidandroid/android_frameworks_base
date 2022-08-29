9fc256cbe9e4d25a49e82d8bb2d721e554ee8642
Pranav Vashi <neobuddy89@gmail.com>
2022-08-30 02:26:39 +0530

    SystemUI: Allow to change QS tile label text size [1/2]
    * Useful when using more than default columns.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -58,7 +58,6 @@ import androidx.core.graphics.drawable.updateBounds
 import com.android.app.tracing.traceSection
 import com.android.settingslib.Utils
 import com.android.systemui.Flags
-import com.android.systemui.FontSizeUtils
 import com.android.systemui.animation.Expandable
 import com.android.systemui.animation.LaunchableView
 import com.android.systemui.animation.LaunchableViewDelegate
@@ -200,6 +199,7 @@ constructor(
     private var vertical = false
     private var labelHide = false
     private var forceHideCheveron = false
+    private var labelSize = 14f
 
     /** Visuo-haptic long-press effects */
     private var longPressEffectAnimator: ValueAnimator? = null
@@ -228,6 +228,7 @@ constructor(
         vertical = TileUtils.getQSTileVerticalLayout(context)
         labelHide = TileUtils.getQSTileLabelHide(context)
         forceHideCheveron = vertical || labelHide
+        labelSize = TileUtils.getQSTileLabelSize(context)
         importantForAccessibility = IMPORTANT_FOR_ACCESSIBILITY_YES
         clipChildren = false
         clipToPadding = false
@@ -264,8 +265,8 @@ constructor(
     }
 
     fun updateResources() {
-        FontSizeUtils.updateFontSize(label, R.dimen.qs_tile_text_size)
-        FontSizeUtils.updateFontSize(secondaryLabel, R.dimen.qs_tile_text_size)
+        label.setTextSize(TypedValue.COMPLEX_UNIT_SP, labelSize)
+        secondaryLabel.setTextSize(TypedValue.COMPLEX_UNIT_SP, labelSize)
 
         val iconSize = context.resources.getDimensionPixelSize(R.dimen.qs_icon_size)
         icon.layoutParams.apply {
