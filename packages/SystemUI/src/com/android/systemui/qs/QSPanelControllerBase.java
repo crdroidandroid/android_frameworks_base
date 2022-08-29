a8cb4432f7c22e6ad9d50b354244c3e716942e2c
Pranav Vashi <neobuddy89@gmail.com>
2022-08-30 02:26:39 +0530

    SystemUI: Allow to change QS tile label text size [1/2]
    * Useful when using more than default columns.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -216,6 +216,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
 
         mTunerService.addTunable(this, QSPanel.QS_TILE_VERTICAL_LAYOUT);
         mTunerService.addTunable(this, QSPanel.QS_TILE_LABEL_HIDE);
+        mTunerService.addTunable(this, QSPanel.QS_TILE_LABEL_SIZE);
         mTunerService.addTunable(this, QSPanel.QS_LAYOUT_COLUMNS);
         mTunerService.addTunable(this, QSPanel.QS_LAYOUT_COLUMNS_LANDSCAPE);
         mTunerService.addTunable(this, QSPanel.QQS_LAYOUT_ROWS);
@@ -532,6 +533,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         switch (key) {
             case QSPanel.QS_TILE_VERTICAL_LAYOUT:
             case QSPanel.QS_TILE_LABEL_HIDE:
+            case QSPanel.QS_TILE_LABEL_SIZE:
             case QSPanel.QS_LAYOUT_COLUMNS:
             case QSPanel.QS_LAYOUT_COLUMNS_LANDSCAPE:
             case QSPanel.QQS_LAYOUT_ROWS:
