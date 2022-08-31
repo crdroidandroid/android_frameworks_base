3865838c8e6996c0ed739850fbc80e362bdf976e
Mayur <ultramayur123@gmail.com>
2022-08-31 12:02:23 +0530

    Revert "Remove QQS brightness controller"
    This reverts commit 32b7c35f24d29a4e87e386267c725549cc1b6259.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/tests/src/com/android/systemui/qs/QuickQSPanelControllerTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/qs/QuickQSPanelControllerTest.kt
@@ -58,6 +58,7 @@ class QuickQSPanelControllerTest : SysuiTestCase() {
     @Mock private lateinit var tile: QSTile
     @Mock private lateinit var tileLayout: TileLayout
     @Mock private lateinit var tileView: QSTileView
+    @Mock private lateinit var quickQsBrightnessController: QuickQSBrightnessController
     @Captor private lateinit var captor: ArgumentCaptor<QSPanel.OnConfigurationChangedListener>
 
     private val uiEventLogger = UiEventLoggerFake()
@@ -88,7 +89,8 @@ class QuickQSPanelControllerTest : SysuiTestCase() {
                 metricsLogger,
                 uiEventLogger,
                 qsLogger,
-                dumpManager)
+                dumpManager,
+                quickQsBrightnessController)
 
         controller.init()
     }
@@ -143,6 +145,10 @@ class QuickQSPanelControllerTest : SysuiTestCase() {
         captor.allValues.forEach { it.onConfigurationChange(Configuration.EMPTY) }
 
         verify(mediaHost).expansion = MediaHostState.EXPANDED
+    }
+
+    fun testBrightnessVisibilityRefreshedWhenConfigurationChanged() {
+        verify(quickQsBrightnessController).refreshVisibility(anyBoolean())
     }
 
     class TestQuickQSPanelController(
