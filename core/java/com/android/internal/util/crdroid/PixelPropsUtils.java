38c7c7da72a130b1db17d60d55a40dc7ccc16221
Pranav Vashi <neobuddy89@gmail.com>
2022-09-10 21:23:33 +0530

    PixelPropsUtils: Use ro.build.incremental for settings intelligence
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -270,7 +270,7 @@ public class PixelPropsUtils {
             }
             // Set proper indexing fingerprint
             if (packageName.equals("com.google.android.settings.intelligence")) {
-                setPropValue("FINGERPRINT", Build.DATE);
+                setPropValue("FINGERPRINT", Build.VERSION.INCREMENTAL);
             }
         } else {
 
