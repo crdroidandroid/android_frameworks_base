632eb015f00ee1656a85a83c9f430fcbe758a265
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-18 02:05:07 +0530

    InternetDialog: Launch mobile network settings on long press
    Change-Id: Ibd918c62db65fe42e4028e6cc2c029987f013b59
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
@@ -307,6 +307,7 @@ public class InternetDialog extends SystemUIDialog implements
         mHandler.removeCallbacks(mHideProgressBarRunnable);
         mHandler.removeCallbacks(mHideSearchingRunnable);
         mMobileNetworkLayout.setOnClickListener(null);
+        mMobileNetworkLayout.setOnLongClickListener(null);
         mMobileDataToggle.setOnCheckedChangeListener(null);
         mHotspotLayout.setOnClickListener(null);
         mHotspotToggle.setOnCheckedChangeListener(null);
@@ -388,6 +389,10 @@ public class InternetDialog extends SystemUIDialog implements
                     mInternetDialogController.connectCarrierNetwork();
                 }
             }
+        });
+        mMobileNetworkLayout.setOnLongClickListener(v -> {
+                mInternetDialogController.launchMobileNetworkSetting(v);
+                return true;
         });
         mMobileDataToggle.setOnCheckedChangeListener(
                 (buttonView, isChecked) -> {
