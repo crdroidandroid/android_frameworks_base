62c97532dbab3bc524245d28ef95464677761361
Richard Raya <rdxzv.dev@gmail.com>
2022-09-18 13:58:22 -0300

    ExternalStorageProvider: Remove SAF restrictions
    * Inspired by https://github.com/DanGLES3/NoStorageRestrict
    
    Change-Id: I073d9bb8edd49224e17c76a43683365a64686768
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- packages/ExternalStorageProvider/src/com/android/externalstorage/ExternalStorageProvider.java
+++ packages/ExternalStorageProvider/src/com/android/externalstorage/ExternalStorageProvider.java
@@ -382,40 +382,7 @@ public class ExternalStorageProvider extends FileSystemProvider {
      * @see Document#FLAG_DIR_BLOCKS_OPEN_DOCUMENT_TREE
      */
     @Override
-    protected boolean shouldBlockDirectoryFromTree(@NonNull String documentId)
-            throws FileNotFoundException {
-        final File dir = getFileForDocId(documentId, false);
-        // The file is null or it is not a directory
-        if (dir == null || !dir.isDirectory()) {
-            return false;
-        }
-
-        // Allow all directories on USB, including the root.
-        if (isOnRemovableUsbStorage(documentId)) {
-            return false;
-        }
-
-        // Get canonical(!) path. Note that this path will have neither leading nor training "/".
-        // This the root's path will be just an empty string.
-        final String path = getPathFromDocId(documentId);
-
-        // Block the root of the storage
-        if (path.isEmpty()) {
-            return true;
-        }
-
-        // Block /Download/ and /Android/ folders from the tree.
-        if (equalIgnoringCase(path, Environment.DIRECTORY_DOWNLOADS) ||
-                equalIgnoringCase(path, Environment.DIRECTORY_ANDROID)) {
-            return true;
-        }
-
-        // This shouldn't really make a difference, but just in case - let's block hidden
-        // directories as well.
-        if (shouldHideDocument(documentId)) {
-            return true;
-        }
-
+    protected boolean shouldBlockDirectoryFromTree(@NonNull String documentId) throws FileNotFoundException {
         return false;
     }
 
