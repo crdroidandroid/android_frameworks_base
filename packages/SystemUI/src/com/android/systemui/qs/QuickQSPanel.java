0b06f572baf4fb03c01e0d0233ce322857a30227
str-br <alexfinhart@gmail.com>
2022-09-22 06:37:37 +0800

    SystemUI: QSPanel: Display 2 rows when using 3 columns
    Signed-off-by: str-br <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -157,6 +157,7 @@ public class QuickQSPanel extends QSPanel implements TunerService.Tunable {
         mColumns = TileUtils.getQSColumnsCount(mContext,
             getResources().getInteger(NUM_COLUMNS_ID));
         if (mColumns == 2) maxTiles = getResources().getInteger(R.integer.quick_qs_panel_max_tiles);
+        if (mColumns == 3) maxTiles = TUNER_MAX_TILES_FALLBACK;
         if (maxTiles > mColumns && (maxTiles % mColumns != 0)) {
             maxTiles--;
             setMaxTiles(maxTiles);
