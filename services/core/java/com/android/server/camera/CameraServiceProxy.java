48ba8a27439e8e11dc5fe9c988d86a9100c2404b
LibXZR <i@xzr.moe>
2022-09-23 14:56:13 +0800

    services: camera: Whitelist parallel space users
    Fixes unable to use camera in parallel spaces.
    
    Change-Id: I42f412cfb3a803182fc98d10c2a496c5aa3116bc
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/camera/CameraServiceProxy.java
+++ services/core/java/com/android/server/camera/CameraServiceProxy.java
@@ -81,6 +81,8 @@ import com.android.server.ServiceThread;
 import com.android.server.SystemService;
 import com.android.server.wm.WindowManagerInternal;
 
+import ink.kaleidoscope.server.ParallelSpaceManagerService;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
@@ -346,6 +348,7 @@ public class CameraServiceProxy extends SystemService
                 case Intent.ACTION_USER_INFO_CHANGED:
                 case Intent.ACTION_MANAGED_PROFILE_ADDED:
                 case Intent.ACTION_MANAGED_PROFILE_REMOVED:
+                case Intent.ACTION_PARALLEL_SPACE_CHANGED:
                     synchronized(mLock) {
                         // Return immediately if we haven't seen any users start yet
                         if (mEnabledCameraUsers == null) return;
@@ -694,6 +697,7 @@ public class CameraServiceProxy extends SystemService
         filter.addAction(Intent.ACTION_MANAGED_PROFILE_REMOVED);
         filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
         filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        filter.addAction(Intent.ACTION_PARALLEL_SPACE_CHANGED);
         mContext.registerReceiver(mIntentReceiver, filter);
 
         publishBinderService(CAMERA_SERVICE_PROXY_BINDER_NAME, mCameraServiceProxy);
@@ -937,6 +941,7 @@ public class CameraServiceProxy extends SystemService
         for (int id : userProfiles) {
             handles.add(id);
         }
+        handles.addAll(ParallelSpaceManagerService.getCurrentParallelUserIds());
 
         return handles;
     }
