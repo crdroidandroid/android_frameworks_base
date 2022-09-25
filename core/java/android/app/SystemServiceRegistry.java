b72ba0a71ffcf838f9c9130a0b450c12eaf72c48
cjh1249131356 <cjh1249131356@gmail.com>
2022-09-25 14:50:31 +0800

    base: Add mock oplus LinearmotorVibratorService
    * To get haptic feedback in OnePlus / Oplus Camera.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    [@neobuddy89: Remove unnecessary bits.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -266,6 +266,8 @@ import com.android.internal.os.IBinaryTransparencyService;
 import com.android.internal.os.IDropBoxManagerService;
 import com.android.internal.policy.PhoneLayoutInflater;
 import com.android.internal.util.Preconditions;
+import com.oplus.os.ILinearmotorVibratorService;
+import com.oplus.os.LinearmotorVibrator;
 
 import java.util.Map;
 import java.util.Objects;
@@ -1630,6 +1632,15 @@ public final class SystemServiceRegistry {
                         }
                         return new E2eeContactKeysManager(ctx);
                     }});
+
+        registerService(Context.LINEARMOTOR_VIBRATOR_SERVICE, LinearmotorVibrator.class,
+                new CachedServiceFetcher<LinearmotorVibrator>() {
+            @Override
+            public LinearmotorVibrator createService(ContextImpl ctx) {
+                IBinder binder = ServiceManager.getService(Context.LINEARMOTOR_VIBRATOR_SERVICE);
+                ILinearmotorVibratorService service = ILinearmotorVibratorService.Stub.asInterface(binder);
+                return new LinearmotorVibrator(ctx.getOuterContext(), service);
+            }});
 
         // DO NOT do a flag check like this unless the flag is read-only.
         // (because this code is executed during preload in zygote.)
