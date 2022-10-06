c7d33450939795912fb96eafeeb257c603780495
AnierinB <anierin@evolution-x.org>
2022-10-06 06:04:20 +0000

    SystemUI: UdfpsAnimation: Don't run scaleFactor on mAnimationSize
    * This wasn't needed to begin with as it causes udfps animation
    resources to vary in size depending on resolution.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
@@ -95,7 +95,7 @@ public class UdfpsAnimation extends ImageView {
 
         mUdfpsAnimationPackage = "com.crdroid.udfps.animations";
 
-        mAnimationSize = (int) (mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_size) * scaleFactor);
+        mAnimationSize = mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_size);
         mAnimationOffset = (int) (mContext.getResources().getDimensionPixelSize(R.dimen.udfps_animation_offset) * scaleFactor);
 
         mAnimParams.height = mAnimationSize;
