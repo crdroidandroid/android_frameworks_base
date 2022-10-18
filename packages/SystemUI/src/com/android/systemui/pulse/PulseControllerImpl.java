df1a66565e85f27fe0e5ed2da730522f6f3efa7d
Pranav Vashi <neobuddy89@gmail.com>
2022-10-19 00:41:10 +0530

    Pulse: Implement pulse color based album art [1/2]
    * Little hacky but works.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/pulse/PulseControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/pulse/PulseControllerImpl.java
@@ -512,6 +512,11 @@ public class PulseControllerImpl implements
     }
 
     @Override
+    public void setMediaNotificationColor(int color) {
+        mColorController.setMediaNotificationColor(color);
+    }
+
+    @Override
     public String toString() {
         return TAG + " " + getState();
     }
