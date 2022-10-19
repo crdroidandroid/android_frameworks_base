b4b41d8ba4bd296f93826b020e4c5dbda1a8680b
chuan.zhang1 <chuan.zhang1@unisoc.com>
2022-10-19 17:05:16 +0800

    ShortcutPackageItem: Fix deadlock problem
     Require Holding the service-level lock
     before require holding the package-level lock in
     ShortcutPackageItem.waitForBitmapSaves() The service-level lock and the
     package-level lock are held in different order.
    
    Change-Id: I7140f285ce9033decbc0a4c129dcb44c92185ed0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -278,7 +278,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     final Context mContext;
 
-    private final Object mLock = new Object();
+    final Object mLock = new Object();
     private final Object mNonPersistentUsersLock = new Object();
 
     private static List<ResolveInfo> EMPTY_RESOLVE_INFO = new ArrayList<>(0);
