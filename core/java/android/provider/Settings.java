cb77787153c831b7f5a745145e4f85cc80ee4fd5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-24 00:24:27 +0530

    SystemUI: Allow disabling clipboard overlay [1/2]
    Change-Id: Ia755ca9021468a53c8f17413ca3faa787b00ceea

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12821,6 +12821,13 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
+         * Whether to show an overlay in the bottom corner of the screen on copying stuff
+         * into the clipboard.
+         * @hide
+         */
+        public static final String SHOW_CLIPBOARD_OVERLAY = "show_clipboard_overlay";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
