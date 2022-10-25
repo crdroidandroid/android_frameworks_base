da86fc9e7bbcfb4deac437d912308953407e8f94
EnesSastim <sastimenes@gmail.com>
2022-10-25 14:04:49 +0000

    PixelPropsUtils: Don't spoof euicc

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -80,6 +80,7 @@ public class PixelPropsUtils {
             "com.google.android.UltraCVM",
             "com.google.android.apps.cameralite",
             "com.google.android.dialer",
+            "com.google.android.euicc",
             "com.google.ar.core",
             "com.google.android.youtube",
             "com.google.android.apps.youtube.kids",
