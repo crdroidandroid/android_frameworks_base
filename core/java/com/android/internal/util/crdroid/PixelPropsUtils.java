9e1db4211d8a65cbe45c176b2ffe43286b1448ba
Pranav Vashi <neobuddy89@gmail.com>
2022-10-25 23:20:10 +0530

    PixelPropsUtils: Spoof Gboard with PixelXL
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -49,11 +49,11 @@ public class PixelPropsUtils {
     private static final String[] packagesToChangePixel7Pro = {
             "com.google.android.apps.wallpaper",
             "com.google.android.apps.privacy.wildlife",
-            "com.google.android.apps.subscriptions.red",
-            "com.google.android.inputmethod.latin"
+            "com.google.android.apps.subscriptions.red"
     };
 
     private static final String[] packagesToChangePixelXL = {
+            "com.google.android.inputmethod.latin",
             "com.samsung.accessory",
             "com.samsung.accessory.fridaymgr",
             "com.samsung.accessory.berrymgr",
