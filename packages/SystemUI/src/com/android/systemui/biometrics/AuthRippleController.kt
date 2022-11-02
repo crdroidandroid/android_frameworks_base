edf31bdccfa121486362c67f4803923641e4235a
Chenyang Zhong <zhongcy95@gmail.com>
2022-11-02 13:37:49 +0800

    AuthRippleController: Always finish the LightRevealScrim
    The unlock ripple animation triggers a CircleReveal effect of
    the LightRevealScrim. The CircleReveal uses a ValueAnimator that
    gives in when something else takes over the reveal. If the taking
    over happens till the end of the animator, the revealAmount of the
    CircleReveal will not be updated, thus leaving a black-white
    radial gradient patten in the notification panel background.
    
    Make sure to always finish the reveal in such case, by setting
    the revealAmount when the animator is approaching the end.
    
    Test: put an app in landscape, lock the screen, unlock with a
          physical fingerprint sensor, and check the notification panel
    
    Change-Id: Id96404de524b32146bb776210b006b75fd035017
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
@@ -218,6 +218,10 @@ class AuthRippleController @Inject constructor(
                     addUpdateListener { animator ->
                         if (lightRevealScrim.revealEffect != circleReveal) {
                             // if something else took over the reveal, let's cancel ourselves
+                            // When the animator is almost done, fully reveal the scrim.
+                            if (animator.animatedValue as Float >= 0.9999f) {
+                                lightRevealScrim.revealAmount = 1f
+                            }
                             cancel()
                             return@addUpdateListener
                         }
