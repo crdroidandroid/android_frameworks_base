d71ac81fb7b65d7ae575727ad0359fd33dc337ac
minaripenguin37 <alexfinhart@gmail.com>
2022-11-08 12:53:09 +0800

    services: Implement scroll gesture boosting
    * like code linaro's gesture fling boosts
    
    @neobuddy89: onHorizontalFling and onVerticalFling not required, since we are
    boosting onFling. Add onScroll which seems useful.
    
    Co-authored-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/SystemGesturesPointerEventListener.java
+++ services/core/java/com/android/server/wm/SystemGesturesPointerEventListener.java
@@ -91,6 +91,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
     private boolean mMouseHoveringAtRight;
     private boolean mMouseHoveringAtBottom;
     private long mLastFlingTime;
+    private boolean mScrollFired;
 
     SystemGesturesPointerEventListener(Context context, Handler handler, Callbacks callbacks) {
         mContext = checkNull("context", context);
@@ -184,6 +185,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             case MotionEvent.ACTION_DOWN:
                 mSwipeFireable = true;
                 mDebugFireable = true;
+                mScrollFired = false;
                 mDownPointers = 0;
                 captureDown(event, 0);
                 if (mMouseHoveringAtLeft) {
@@ -290,6 +292,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             case MotionEvent.ACTION_CANCEL:
                 mSwipeFireable = false;
                 mDebugFireable = false;
+                mScrollFired = false;
                 mCallbacks.onUpOrCancel();
                 break;
             default:
@@ -450,6 +453,16 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             mCallbacks.onFling(duration);
             return true;
         }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                   float distanceX, float distanceY) {
+           if (!mScrollFired) {
+               mCallbacks.onScroll();
+               mScrollFired = true;
+           }
+           return true;
+        }
     }
 
     interface Callbacks {
@@ -458,6 +471,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
         void onSwipeFromRight();
         void onSwipeFromLeft();
         void onFling(int durationMs);
+        void onScroll();
         void onDown();
         void onUpOrCancel();
         void onMouseHoverAtLeft();
