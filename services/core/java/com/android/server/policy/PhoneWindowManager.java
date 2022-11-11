f7cee7e3ad757e2364e2d340f635017e53fd518d
xi yu <yuxi1@xiaomi.com>
2022-11-11 03:17:11 +0000

    Fix the problem that the home key can not work in some cases.
    When closeSystemDialogs of ActivityTaskManagerService is called from the one way binder callback KeyguardExitResult, the getCallingPid of Binder will return 0, resulting in a NullPointerException in proc.isPerceptible().
    
    Google: 2299757
    Change-Id: I76b653ae08d47fe232677413382d60475a2cbc47

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4259,7 +4259,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     @Override
                     public void onKeyguardExitResult(boolean success) {
                         if (success) {
-                            startDockOrHome(displayId, true /*fromHomeKey*/, awakenFromDreams);
+                            final long origId = Binder.clearCallingIdentity();
+                            try {
+                                startDockOrHome(displayId, true /*fromHomeKey*/, awakenFromDreams);
+                            } finally {
+                                Binder.restoreCallingIdentity(origId);
+                            }
                         }
                     }
                 });
