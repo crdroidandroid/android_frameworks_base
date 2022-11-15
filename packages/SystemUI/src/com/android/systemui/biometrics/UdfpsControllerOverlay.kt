a13a4b8746595a4cc1c5a107e693724dcaf9d079
Nishant Kumar <www.rajsonu13@gmail.com>
2022-11-15 12:13:09 +0530

    udfps: framework_dimming: No need to return newDimAmount
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -137,10 +137,9 @@ class UdfpsControllerOverlay @JvmOverloads constructor(
         }
     }
 
-    fun updateDimAmount(newDimAmount: Float): Float {
+    fun updateDimAmount(newDimAmount: Float) {
         coreLayoutParams.dimAmount = newDimAmount
         windowManager.updateViewLayout(overlayView, coreLayoutParams)
-        return newDimAmount
     }
 
     /** If the overlay is currently showing. */
