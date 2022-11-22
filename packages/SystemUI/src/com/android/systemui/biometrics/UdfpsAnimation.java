f45cfea89741fefa064503f06f0c6c2ef17bcd50
AnierinB <anierin@evolution-x.org>
2022-11-22 23:44:12 +0000

    UdfpsAnimations: Migrate to DisplayUtils.getScaleFactor
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsAnimation.java
@@ -30,8 +30,6 @@ import android.util.AttributeSet;
 import android.util.DisplayUtils;
 import android.util.Log;
 import android.util.MathUtils;
-import android.view.Display;
-import android.view.DisplayInfo;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
@@ -78,15 +76,10 @@ public class UdfpsAnimation extends ImageView {
            FingerprintSensorPropertiesInternal props) {
         super(context);
         mContext = context;
-            DisplayInfo displayInfo = new DisplayInfo();
-            mContext.getDisplay().getDisplayInfo(displayInfo);
-            final Display.Mode maxDisplayMode =
-                    DisplayUtils.getMaximumResolutionDisplayMode(displayInfo.supportedModes);
-            final float scaleFactor = DisplayUtils.getPhysicalPixelDisplaySizeRatio(
-                    maxDisplayMode.getPhysicalWidth(), maxDisplayMode.getPhysicalHeight(),
-                    displayInfo.getNaturalWidth(), displayInfo.getNaturalHeight());
 
         mWindowManager = windowManager;
+
+        final float scaleFactor = DisplayUtils.getScaleFactor(mContext);
 
         mMaxBurnInOffsetX = (int) (context.getResources()
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x) * scaleFactor);
