fed547a9cd8091f159575063eb5143412ab39ba6
Pranav Vashi <neobuddy89@gmail.com>
2022-12-08 15:05:50 +0530

    services: Fix parallel space crash with third party launcher
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/UserManagerService.java
+++ services/core/java/com/android/server/pm/UserManagerService.java
@@ -6280,6 +6280,9 @@ public class UserManagerService extends IUserManager.Stub {
             if (targetUserId == callingUserId) {
                 return true;
             }
+            if (ParallelSpaceManagerService.canInteract(callingUserId, targetUserId)) {
+                return true;
+            }
             synchronized (mUsersLock) {
                 UserInfo callingUserInfo = getUserInfoLU(callingUserId);
                 if (callingUserInfo == null || callingUserInfo.isProfile()) {
