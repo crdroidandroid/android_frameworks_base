0f3f22d4087750bdcaf478d21cfc97aa44389ed8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-22 00:04:37 +0530

    StatusBarIconController: Avoid crash due to incorrect casting
    Example:
    E AndroidRuntime: java.lang.ClassCastException: com.android.systemui.statusbar.StatusBarBluetoothView cannot be cast to com.android.systemui.statusbar.StatusBarIconView
    
    Change-Id: I984bb7d74b4110ce43cac2b98267b55ae37f5f04
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -562,8 +562,10 @@ public interface StatusBarIconController {
         }
 
         public void onSetIcon(int viewIndex, StatusBarIcon icon) {
-            StatusBarIconView view = (StatusBarIconView) mGroup.getChildAt(viewIndex);
-            view.set(icon);
+            View view = mGroup.getChildAt(viewIndex);
+            if (view instanceof StatusBarIconView) {
+                ((StatusBarIconView) view).set(icon);
+            }
         }
 
         public void onSetIconHolder(int viewIndex, StatusBarIconHolder holder) {
@@ -596,8 +598,7 @@ public interface StatusBarIconController {
                 // ModernStatusBarWifiView will automatically apply state based on its callbacks, so
                 // we don't need to call applyWifiState.
             } else {
-                throw new IllegalStateException("View at " + viewIndex + " must be of type "
-                        + "StatusBarWifiView or ModernStatusBarWifiView");
+                return;
             }
 
             if (mIsInDemoMode) {
