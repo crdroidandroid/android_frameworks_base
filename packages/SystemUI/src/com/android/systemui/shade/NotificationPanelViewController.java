651f8d7142f28af552bc1448abbb1b308646e145
Trishiraj <gamerprince.exp@gmail.com>
2022-12-25 13:00:41 +0800

    SystemUI: Refactor notification reticker
    * add interpolators to reveal animation and increase duration
    * reinflate theme on value change
    * add toggle for animation
    * add small reticker style back
    
    original commit message:
    Animation based on iPhone's new Dynamic Island feature.
    
    Author: minaripenguin <minaripenguin@users.noreply.github.com>
    Date:   Tue Jan 3 17:20:04 2023 +0800
    
        SystemUI: Fix a race condition on reticker's visibility
    
        Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -5281,7 +5281,7 @@ public final class NotificationPanelViewController implements Dumpable {
             mReTickerContentTV.setText(mergedContentText);
             mReTickerContentTV.setTextAppearance(mView.getContext(), R.style.TextAppearance_Notifications_reTicker);
             mReTickerContentTV.setSelected(true);
-            RetickerAnimations.doBounceAnimationIn(mReTickerComeback);
+            RetickerAnimations.revealAnimation(mReTickerComeback);
             if (reTickerIntent != null) {
                 mReTickerComeback.setOnClickListener(v -> {
                     final GameSpaceManager gameSpace = mCentralSurfaces.getGameSpaceManager();
@@ -5291,7 +5291,7 @@ public final class NotificationPanelViewController implements Dumpable {
                         } catch (PendingIntent.CanceledException e) {
                         }
                     }
-                    RetickerAnimations.doBounceAnimationOut(mReTickerComeback, mNotificationStackScroller);
+                    RetickerAnimations.revealAnimationHide(mReTickerComeback, mNotificationStackScroller);
                     reTickerViewVisibility();
                 });
             }
@@ -5315,7 +5315,7 @@ public final class NotificationPanelViewController implements Dumpable {
     }
 
     public void reTickerDismissal() {
-        RetickerAnimations.doBounceAnimationOut(mReTickerComeback, mNotificationStackScroller);
+        RetickerAnimations.revealAnimationHide(mReTickerComeback, mNotificationStackScroller);
         mReTickerComeback.getViewTreeObserver().removeOnComputeInternalInsetsListener(mInsetsListener);
     }
 
