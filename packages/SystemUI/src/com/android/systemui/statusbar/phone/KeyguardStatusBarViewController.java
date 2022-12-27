2d95bd6b0c334b7bfb3682c8ad7d97576a3ec7d6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-27 21:31:45 +0530

    SystemUI: Ensure keyguard statusbar is gone when not showing
    Fixes keyguard statusbar overlapping on QS when keyguard is
    bypassed by face unlock.
    
    While we're at it, fixup a derp in aa6b671d
    ("SystemUI: Re-inflate keyguard statusbar on theme change").
    
    Change-Id: I181e785d0d4d5149c1b212cff15b67db32062ab8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarViewController.java
@@ -469,6 +469,7 @@ public class KeyguardStatusBarViewController extends ViewController<KeyguardStat
      */
     public void updateViewState() {
         if (!isKeyguardShowing()) {
+            mView.setVisibility(View.GONE);
             return;
         }
 
