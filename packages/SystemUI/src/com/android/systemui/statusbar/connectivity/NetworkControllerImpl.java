e59d09ffbb9daa9acae6920df2c39dc045a41119
Pranav Vashi <neobuddy89@gmail.com>
2022-12-27 20:09:20 +0530

    Improvements for dynamic VoLTE & VoWiFi icons
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/NetworkControllerImpl.java
@@ -389,7 +389,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
                         .toString();
                 recordLastNetworkCallback(callback);
                 updateConnectivity();
-                updateImsIcon();
             }
 
             @Override
@@ -426,7 +425,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
                         .toString();
                 recordLastNetworkCallback(callback);
                 updateConnectivity();
-                updateImsIcon();
             }
         };
         // Even though this callback runs on the receiver handler thread which also processes the
@@ -452,7 +450,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 }
                 mActiveMobileDataSubscription = subId;
                 doUpdateMobileControllers();
-                updateImsIcon();
             });
         };
 
@@ -465,7 +462,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
         if (DEBUG) Log.d(TAG, ": mClearForceValidated");
         mForceCellularValidated = false;
         updateConnectivity();
-        updateImsIcon();
     };
 
     boolean isInGroupDataSwitch(int subId1, int subId2) {
@@ -837,7 +833,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
         mCurrentUserId = newUserId;
         mAccessPoints.onUserSwitched(newUserId);
         updateConnectivity();
-        updateImsIcon();
     }
 
     @Override
@@ -860,7 +855,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
         switch (action) {
             case ConnectivityManager.CONNECTIVITY_ACTION:
                 updateConnectivity();
-                updateImsIcon();
                 break;
             case Intent.ACTION_AIRPLANE_MODE_CHANGED:
                 refreshLocale();
@@ -938,7 +932,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
             return;
         }
         doUpdateMobileControllers();
-        updateImsIcon();
     }
 
     private void filterMobileSubscriptionInSameGroup(List<SubscriptionInfo> subscriptions) {
@@ -987,6 +980,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
         }
         updateNoSims();
         recalculateEmergency();
+        updateImsIcon();
     }
 
     @VisibleForTesting
@@ -1214,6 +1208,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 && !mConnectedTransports.get(NetworkCapabilities.TRANSPORT_ETHERNET);
         mCallbackHandler.setConnectivityStatus(mNoDefaultNetwork, !mInetCondition,
                 mNoNetworksAvailable);
+        updateImsIcon();
     }
 
     /**
