3237c187ff33a3892c0544ae1c8e244237085aab
Oliver Scott <olivercscott@gmail.com>
2023-01-05 22:37:42 -0500

    Set FakeStore/PlayStore as Aurora Store installer package name
    Issue: calyxos#240
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Tad <tad@spotco.us>
    Change-Id: I883bc16231fa7417b71e273a1909f3178bb8e0b0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -81,6 +81,7 @@ import android.content.pm.InstantAppResolveInfo;
 import android.content.pm.InstrumentationInfo;
 import android.content.pm.KeySet;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageInstaller;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManagerInternal;
 import android.content.pm.ParceledListSlice;
@@ -416,6 +417,10 @@ public class ComputerEngine implements Computer {
     private final BackgroundDexOptService mBackgroundDexOptService;
     private final PackageManagerInternal.ExternalSourcesPolicy mExternalSourcesPolicy;
     private final CrossProfileIntentResolverEngine mCrossProfileIntentResolverEngine;
+
+    private static final String AURORA_SERVICES = "com.aurora.services";
+    private static final String AURORA_STORE = "com.aurora.store";
+    private static final String PLAY_STORE = "com.android.vending";
 
     // PackageManagerService attributes that are primitives are referenced through the
     // pms object directly.  Primitives are the only attributes so referenced.
@@ -5012,6 +5017,22 @@ public class ComputerEngine implements Computer {
 
         if (ps == null || shouldFilterApplicationIncludingUninstalled(ps, callingUid, userId)) {
             return null;
+        }
+
+        InstallSource installSource = ps.getInstallSource();
+        final String installerPackageName = installSource.mInstallerPackageName;
+        if (installSource != null && installerPackageName != null
+                && mSettings.getPackage(PLAY_STORE) != null
+                && callingUid != Process.SYSTEM_UID
+                && (AURORA_STORE.equals(installerPackageName)
+                || AURORA_SERVICES.equals(installerPackageName))) {
+            return InstallSource.create(PLAY_STORE, PLAY_STORE, PLAY_STORE,
+                            installSource.mInstallerPackageUid, // FIXME: likely wrong
+            		    installSource.mUpdateOwnerPackageName,
+            		    installSource.mInstallerAttributionTag,
+                            PackageInstaller.PACKAGE_SOURCE_STORE)
+                    .setInitiatingPackageSignatures(new PackageSignatures(
+                            mSettings.getPackage(PLAY_STORE).getSigningDetails()));
         }
 
         return ps.getInstallSource();
