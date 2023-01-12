d286857ed490a09ded88f7407d629d9b7fa077f8
ShevT <evgeny.shishkov@gmail.com>
2023-01-12 14:24:12 +0300

    Add config overlay for battery info update
     It is not necessary to use this for all devices, but only for those
     that have a problem with charging display (for example OnePlus 7 series)
    
    Change-Id: I98441087ec450ec3a2699a1a79b84bc32a1fe340
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardIndicationRotateTextViewController.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardIndicationRotateTextViewController.java
@@ -142,12 +142,8 @@ public class KeyguardIndicationRotateTextViewController extends
             return;
         }
         long minShowDuration = getMinVisibilityMillis(mIndicationMessages.get(mCurrIndicationType));
-        final boolean hasPreviousIndication = mIndicationMessages.get(type) != null;
         final boolean hasNewIndication = newIndication != null
                 && !TextUtils.isEmpty(newIndication.getMessage());
-
-        mView.setAnimationsEnabled(!hasPreviousIndication);
-
         if (!hasNewIndication) {
             mIndicationMessages.remove(type);
             mIndicationQueue.removeIf(x -> x == type);
