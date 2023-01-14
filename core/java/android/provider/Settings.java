d400cb127ac6d80c21153fc53865b14e65d97e96
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-14 19:56:01 +0200

    base: Allow setting bottom keyguard shortcuts [1/2]
    Squashed:
        From: Adithya R <gh0strider.2k18.reborn@gmail.com>
        Date: Tue, 3 Jan 2023 21:07:49 +0000
        Subject: SystemUI: Introduce camera & flashlight keyguard affordance
    
        This adds buttons for launching camera and toggling flashlight
        at the bottom of the lockscreen for quick access. These will be
        shown only when device controls/wallet/qr scanner are disabled.
    
        Change-Id: I69920b4831938ea6268494e09d4a1926430f3e1c
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Thu, 16 Mar 2023 19:41:00 +0200
        Subject: base: Adapt custom lockscreen shortcuts to 13-QPR2 [1/2]
    
        And add shortcuts they added and made no use of
        Do note a small part of the adaptation was done in merge
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Sun, 19 Mar 2023 13:28:36 +0200
        Subject: SystemUI: keyguard: quickaffordance: Add DND to the defaults [1/2]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6249,6 +6249,17 @@ public final class Settings {
         public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
 
         /**
+         * Bottom screen shortcuts on keyguard
+         * Two lists of strings delimeted by ;
+         * Each list of string is delimited by ,
+         * for valid string see BuiltInKeyguardQuickAffordanceKeys under SystemUI
+         * The order in each list decides the priority for each shortcut
+         * @hide
+         */
+        @Readable
+        public static final String KEYGUARD_QUICK_TOGGLES_NEW = "keyguard_quick_toggles_new";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -315,6 +315,8 @@ public final class NotificationPanelViewController implements Dumpable {
             "system:" + Settings.System.RETICKER_STATUS;
     private static final String RETICKER_COLORED =
             "system:" + Settings.System.RETICKER_COLORED;
+    private static final String KEYGUARD_QUICK_TOGGLES_NEW =
+            "system:" + Settings.System.KEYGUARD_QUICK_TOGGLES_NEW;
 
     private static final Rect M_DUMMY_DIRTY_RECT = new Rect(0, 0, 1, 1);
     private static final Rect EMPTY_RECT = new Rect();
@@ -5865,6 +5867,7 @@ public final class NotificationPanelViewController implements Dumpable {
             mTunerService.addTunable(this, DOUBLE_TAP_SLEEP_LOCKSCREEN);
             mTunerService.addTunable(this, RETICKER_STATUS);
             mTunerService.addTunable(this, RETICKER_COLORED);
+            mTunerService.addTunable(this, KEYGUARD_QUICK_TOGGLES_NEW);
             // Theme might have changed between inflating this view and attaching it to the
             // window, so
             // force a call to onThemeChanged
@@ -5907,6 +5910,9 @@ public final class NotificationPanelViewController implements Dumpable {
                 case RETICKER_COLORED:
                     mReTickerColored =
                             TunerService.parseIntegerSwitch(newValue, false);
+                    break;
+                case KEYGUARD_QUICK_TOGGLES_NEW:
+                    mKeyguardBottomAreaViewModel.updateSettings();
                     break;
                 default:
                     break;
