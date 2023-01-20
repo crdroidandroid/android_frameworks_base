434b22ecd087201cfab5897bb405c089a7c231f8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-01-20 11:42:11 +0200

    bugfix: isServiceTokenValidLocked() was called without holding the lock
    This method iterates through a list that is modified by multiple threads, which is not safe to do
    without locking.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/notification/ManagedServices.java
+++ services/core/java/com/android/server/notification/ManagedServices.java
@@ -1042,6 +1042,12 @@ abstract public class ManagedServices {
         return null;
     }
 
+    protected boolean isServiceTokenValid(IInterface service) {
+        synchronized (mMutex) {
+            return isServiceTokenValidLocked(service);
+        }
+    }
+
     protected boolean isServiceTokenValidLocked(IInterface service) {
         if (service == null) {
             return false;
