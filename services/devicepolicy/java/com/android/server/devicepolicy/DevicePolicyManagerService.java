9a8651445d53a64c108a3eeea3cd8997d25eb6fa
Tommy Webb <tommy@calyxinstitute.org>
2023-01-20 17:25:08 -0500

    Fix profile provisioning check in secondary users
    This resolves a bug preventing a profile from being created for a
    user when the Owner had already reached its max number of profiles.
    
    Prior to this change, DevicePolicyManagerService cleared the calling
    identity before checking whether a profile could be provisioned for a
    user. This meant that even for secondary users, checks were based on
    the system/owner user (0). If user 0 had already reached its max number
    of profiles, a secondary user could not add any either, but this is
    not intended. For example, with a max of 3 managed profiles, you could
    still have multiple secondary users, each with 3 managed profiles and
    no problems, until user 0 reached 3 profiles. This wasn't a result of
    any hard maximum for the profile type, which is checked separately.
    
    Issue: calyxos#972
    Related: I633902a2e4e66c44efbecce5866a43b469cbbd8b
    Change-Id: I735c6bf877d01207cdddc279f8b232292a3b4662
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -14053,7 +14053,8 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
             mInjector.binderRestoreCallingIdentity(ident);
         }
 
-        return checkProvisioningPreconditionSkipPermission(action, packageName) == STATUS_OK;
+        return checkProvisioningPreconditionSkipPermission(action, packageName, caller.getUserId())
+                == STATUS_OK;
     }
 
     @Override
@@ -14063,11 +14064,12 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         Preconditions.checkCallAuthorization(
                 hasCallingOrSelfPermission(permission.MANAGE_PROFILE_AND_DEVICE_OWNERS));
 
-        return checkProvisioningPreconditionSkipPermission(action, packageName);
+        return checkProvisioningPreconditionSkipPermission(action, packageName,
+                mInjector.userHandleGetCallingUserId());
     }
 
     private int checkProvisioningPreconditionSkipPermission(String action,
-            String packageName) {
+            String packageName, @UserIdInt int callingUserId) {
         if (!mHasFeature) {
             logMissingFeatureAction("Cannot check provisioning for action " + action);
             return STATUS_DEVICE_ADMIN_NOT_SUPPORTED;
@@ -14075,11 +14077,12 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         if (!isProvisioningAllowed()) {
             return STATUS_PROVISIONING_NOT_ALLOWED_FOR_NON_DEVELOPER_USERS;
         }
-        final int code = checkProvisioningPreConditionSkipPermissionNoLog(action, packageName);
+        final int code = checkProvisioningPreConditionSkipPermissionNoLog(action, packageName,
+                callingUserId);
         if (code != STATUS_OK) {
             Slogf.d(LOG_TAG, "checkProvisioningPreCondition(" + action + ", " + packageName
                     + ") failed: "
-                    + computeProvisioningErrorString(code, mInjector.userHandleGetCallingUserId()));
+                    + computeProvisioningErrorString(code, callingUserId));
         }
         return code;
     }
@@ -14102,8 +14105,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
     }
 
     private int checkProvisioningPreConditionSkipPermissionNoLog(String action,
-            String packageName) {
-        final int callingUserId = mInjector.userHandleGetCallingUserId();
+            String packageName, @UserIdInt int callingUserId) {
         if (action != null) {
             switch (action) {
                 case DevicePolicyManager.ACTION_PROVISION_MANAGED_PROFILE:
@@ -17675,7 +17677,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         final long identity = Binder.clearCallingIdentity();
         try {
             final int result = checkProvisioningPreconditionSkipPermission(
-                    ACTION_PROVISION_MANAGED_PROFILE, admin.getPackageName());
+                    ACTION_PROVISION_MANAGED_PROFILE, admin.getPackageName(), caller.getUserId());
             if (result != STATUS_OK) {
                 throw new ServiceSpecificException(
                         ERROR_PRE_CONDITION_FAILED,
@@ -18096,7 +18098,8 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         final long identity = Binder.clearCallingIdentity();
         try {
             int result = checkProvisioningPreconditionSkipPermission(
-                    ACTION_PROVISION_MANAGED_DEVICE, deviceAdmin.getPackageName());
+                    ACTION_PROVISION_MANAGED_DEVICE, deviceAdmin.getPackageName(),
+                    caller.getUserId());
             if (result != STATUS_OK) {
                 throw new ServiceSpecificException(
                         ERROR_PRE_CONDITION_FAILED,
