841e1b81cf090d9deb4118476ac917bc86a78b81
Tommy Webb <tommy@calyxinstitute.org>
2023-01-25 17:43:40 -0500

    Support logging out secondary users
    Show an "End session" button in the power menu for secondary users.
    An "End session" button also appears on the lockscreen.
    
    Change-Id: Ic60eabeaa2dcd340e19e8920a62e0d2b0e85cf00

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -12672,7 +12672,11 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
 
     private @UserIdInt int getLogoutUserIdUnchecked() {
         synchronized (getLockObject()) {
-            return mLogoutUserId;
+            final int logoutUserId = mLogoutUserId;
+            if (logoutUserId == UserHandle.USER_NULL) {
+                return UserHandle.USER_SYSTEM;
+            }
+            return logoutUserId;
         }
     }
 
