a6bb00ec4c0ca389ad3509f7d3ff50aa17db7cbf
Pranav Vashi <neobuddy89@gmail.com>
2023-02-03 21:34:13 +0530

    Revert "Revert "SystemUI: Hide oos clear all if reticker enable""
    This reverts commit 1c4d7c5fb0a2404d3eef79dfee29c98968f72056.

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -5257,6 +5257,7 @@ public final class NotificationPanelViewController extends PanelViewController {
             StatusBarNotification sbn = mHeadsUpManager.getTopEntry().getRow().getEntry().getSbn();
             Notification notification = sbn.getNotification();
             String pkgname = sbn.getPackageName();
+            mCentralSurfaces.updateDismissAllVisibility(false);
             Drawable icon = null;
             try {
                 if (pkgname.equals("com.android.systemui")) {
