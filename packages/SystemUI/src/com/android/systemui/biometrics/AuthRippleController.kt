f1f154762370ef774ce8c5dceff06ab24aaebf8a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-24 06:13:58 +0800

    SystemUI: Fix optional ripple animation issue
    * prev implementation was causing global reference table overflow [1] due revealanimator not being able to release its objects
    * with patch 0671bf432c31ace8f81a5a794437cd37644d7344 as reference, which is exact of what we are doing, rely on centralsurfacesimpl instead of manually resetting the the animation.
    * follow strong authentication method in skipping ripple animation
    
    [1]:
    02-12 20:41:07.982 16324 16324 F DEBUG   : Process uptime: 1610s
    02-12 20:41:07.982 16324 16324 F DEBUG   : Cmdline: com.android.systemui
    02-12 20:41:07.982 16324 16324 F DEBUG   : pid: 2376, tid: 2376, name: ndroid.systemui  >>> com.android.systemui <<<
    02-12 20:41:07.982 16324 16324 F DEBUG   : uid: 10356
    02-12 20:41:07.982 16324 16324 F DEBUG   : signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr --------
    02-12 20:41:07.982 16324 16324 F DEBUG   : Abort message: 'JNI ERROR (app bug): global reference table overflow (max=51200)global reference table dump:
    02-12 20:41:07.982 16324 16324 F DEBUG   :   Last 10 entries (of 51200):
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51199: 0x13bba878 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51198: 0x13bb82d0 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51197: 0x13bb8088 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51196: 0x13bb68e8 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51195: 0x13bb66a0 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51194: 0x13bb40f8 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51193: 0x13bb3eb0 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51192: 0x13bb2710 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51191: 0x13bb24c8 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :     51190: 0x13baff20 android.animation.RevealAnimator
    02-12 20:41:07.982 16324 16324 F DEBUG   :   Summary:
    02-12 20:41:07.982 16324 16324 F DEBUG   :     50215 of android.animation.RevealAnimator (50215 unique instances)
    
    qpr2 patch:
    * fix ripple for non-biometric unlock as well
    
    fixes: "leftover" particles appearing on devices like surya
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
@@ -126,7 +126,7 @@ class AuthRippleController @Inject constructor(
     }
 
     fun showUnlockRipple(biometricSourceType: BiometricSourceType) {
-        if (!keyguardStateController.isShowing ||
+        if (!keyguardStateController.isShowing || !isRippleEnabled ||
                 !keyguardUpdateMonitor.isUnlockingWithBiometricAllowed(biometricSourceType)) {
             return
         }
@@ -194,10 +194,7 @@ class AuthRippleController @Inject constructor(
 
     override fun onKeyguardFadingAwayChanged() {
         if (!isRippleEnabled) {
-            // reset and hide the scrim so it doesn't appears on
-            // the next notification shade usage
-            centralSurfaces.lightRevealScrim?.revealAmount = 1f
-            startLightRevealScrimOnKeyguardFadingAway = false
+            // let centralsurfacesimpl reset the lightscrim animation
             return
         }
 
@@ -244,7 +241,7 @@ class AuthRippleController @Inject constructor(
      * Whether we're animating the light reveal scrim from a call to [onKeyguardFadingAwayChanged].
      */
     fun isAnimatingLightRevealScrim(): Boolean {
-        return lightRevealScrimAnimator?.isRunning ?: false
+        return if (!isRippleEnabled) false else (lightRevealScrimAnimator?.isRunning ?: false)
     }
 
     override fun onStartedGoingToSleep() {
