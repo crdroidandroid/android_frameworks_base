74e273cf14358294a61731e8f285d56555318c39
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-27 10:18:10 +0800

    SystemUI: Prevent systemui crash when reinflating QS
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -205,7 +205,9 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         mMediaHost.addVisibilityChangeListener(mMediaHostVisibilityListener);
         mView.addOnConfigurationChangedListener(mOnConfigurationChangedListener);
         mHost.addCallback(mQSHostCallback);
-        setTiles();
+        if (mView.getTileLayout() != null) {
+           setTiles();
+        }
         mLastOrientation = getResources().getConfiguration().orientation;
         mQSLogger.logOnViewAttached(mLastOrientation, mView.getDumpableTag());
         switchTileLayout(true);
@@ -247,6 +249,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
 
     /** */
     public void setTiles() {
+        if (mHost.getTiles() == null) return;
         setTiles(mHost.getTiles(), false);
     }
 
