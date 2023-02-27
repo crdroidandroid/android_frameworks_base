b58c9b7c867481b4677129a1e281b5bc5c83a344
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-27 09:06:02 +0800

    InputMethodManager: Prevent unnecessary invocation of IME
    * attempt to fix random short appearance of IME when creating/switching views
    
    fixes:
    
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: FATAL EXCEPTION: main
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: Process: com.android.launcher3, PID: 2437
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'android.view.ViewRootImpl android.view.View.getViewRootImpl()' on a null object reference
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at android.view.inputmethod.InputMethodManager.startInputInner(InputMethodManager.java:2274)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at android.view.inputmethod.InputMethodManager$H.handleMessage(InputMethodManager.java:963)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:201)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:288)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:7921)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    02-25 07:22:30.059  2437  2437 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:855)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/inputmethod/InputMethodManager.java
+++ core/java/android/view/inputmethod/InputMethodManager.java
@@ -2248,6 +2248,7 @@ public final class InputMethodManager {
             @Nullable IBinder windowGainingFocus, @StartInputFlags int startInputFlags,
             @SoftInputModeFlags int softInputMode, int windowFlags) {
         final View view;
+        final ViewRootImpl viewRoot;
         synchronized (mH) {
             view = getServedViewLocked();
 
@@ -2264,13 +2265,14 @@ public final class InputMethodManager {
 
         if (windowGainingFocus == null) {
             windowGainingFocus = view.getWindowToken();
-            if (windowGainingFocus == null) {
+            viewRoot = view.getViewRootImpl();
+            if (windowGainingFocus == null || viewRoot == null) {
                 Log.e(TAG, "ABORT input: ServedView must be attached to a Window");
                 return false;
             }
             startInputFlags = getStartInputFlags(view, startInputFlags);
-            softInputMode = view.getViewRootImpl().mWindowAttributes.softInputMode;
-            windowFlags = view.getViewRootImpl().mWindowAttributes.flags;
+            softInputMode = viewRoot.mWindowAttributes.softInputMode;
+            windowFlags = viewRoot.mWindowAttributes.flags;
         }
 
         // Now we need to get an input connection from the served view.
