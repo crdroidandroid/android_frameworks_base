4ff3c45bd9d97decd3581ab86f8a88507bc74048
Aaron Kling <webgeek1234@gmail.com>
2023-03-02 16:39:04 -0600

    Don't fail setDeviceConnectionState if not implemented in hal
    Some audio hals do not implement get/set audio_port, even though the
    device hal version should have it. Function not implemented gets mapped
    to invalid operation, so just ignore invalid operation failures.
    
    Change-Id: I1ceaa7aa4467d48f8e492ea6c7de87ed0a9570fa

--- core/jni/android_media_AudioSystem.cpp
+++ core/jni/android_media_AudioSystem.cpp
@@ -674,7 +674,7 @@ static jint android_media_AudioSystem_setDeviceConnectionState(JNIEnv *env, jobj
                     AudioSystem::setDeviceConnectionState(static_cast<audio_policy_dev_state_t>(
                                                                   state),
                                                           port, static_cast<audio_format_t>(codec),
-                                                          deviceSwitch));
+                                                          deviceSwitch), {INVALID_OPERATION});
         } else {
             ALOGE("Failed to read from parcel: %s", statusToString(statusOfParcel).c_str());
             status = kAudioStatusError;
