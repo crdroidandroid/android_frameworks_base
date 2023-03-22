491b4196b559b787c6837977342d3d6537b87258
HELLBOY017 <abhaygill017@gmail.com>
2023-03-22 18:35:31 +0530

    SystemUI: Fix heads up notification timeout on ambient display
    Thanks to prochy-exe for pointing it out.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/headsup/HeadsUpManagerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/headsup/HeadsUpManagerImpl.java
@@ -127,6 +127,7 @@ public class HeadsUpManagerImpl
     private int mAutoDismissTime;
     private final DelayableExecutor mExecutor;
     private final int mDecayDefault;
+    private boolean mDozing;
 
     private final int mExtensionTime;
 
@@ -1269,6 +1270,7 @@ public class HeadsUpManagerImpl
 
         @Override
         public void onDozingChanged(boolean isDozing) {
+            mDozing = isDozing;
             if (!isDozing) {
                 // Let's make sure all huns we got while dozing time out within the normal timeout
                 // duration. Otherwise they could get stuck for a very long time
@@ -1445,7 +1447,8 @@ public class HeadsUpManagerImpl
 
             FinishTimeUpdater finishTimeCalculator = () -> {
                 RemainingDuration remainingDuration =
-                        mAvalancheController.getDuration(this, mAutoDismissTime);
+                        mAvalancheController.getDuration(this,
+                        mDozing ? mDecayDefault * 1000 : mAutoDismissTime);
 
                 if (remainingDuration instanceof RemainingDuration.HideImmediately) {
                     /* Check if */ StatusBarNotifChips.isUnexpectedlyInLegacyMode();
