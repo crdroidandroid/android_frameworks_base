752192e887d342dc12aa9c230f3d92c41089a3d6
Dezhi Huang <huangdezhi@hihonor.com>
2023-03-24 15:33:04 +0800

    Fixed process can't start because of mPendingStart is true problem
    the ProcessRecord.mSuccessorStartRunnable incorrectly nullified by other processe.
    causes new processes to start silently,at the same time ProcessRecord.mPendingStart is true.
    so the new processRecord can no longer start.
    
    Bug: 274190516
    Change-Id: I819769a20403f6858e449f2d52754dd673d71186
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -13111,8 +13111,10 @@ public class ActivityManagerService extends IActivityManager.Stub
         // (probably it's "killed" before starting for real), reset the bookkeeping.
         final ProcessRecord predecessor = app.mPredecessor;
         if (predecessor != null) {
-            predecessor.mSuccessor = null;
-            predecessor.mSuccessorStartRunnable = null;
+            if (predecessor.mSuccessor == app) {
+                predecessor.mSuccessor = null;
+                predecessor.mSuccessorStartRunnable = null;
+            }
             app.mPredecessor = null;
         }
 
