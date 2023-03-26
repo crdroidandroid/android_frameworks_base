2f26a426bd09d8b1ffb20d764228504cf34c6623
Pranav Vashi <neobuddy89@gmail.com>
2023-03-26 11:49:54 +0530

    SmartPixels: Use CoreStartable interface for receiver
    * Also clean up and add check whether smart pixels is supported.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -45,6 +45,7 @@ import com.android.systemui.reardisplay.RearDisplayDialogController
 import com.android.systemui.recents.Recents
 import com.android.systemui.settings.dagger.MultiUserUtilsModule
 import com.android.systemui.shortcut.ShortcutKeyDispatcher
+import com.android.systemui.smartpixels.SmartPixelsReceiver
 import com.android.systemui.statusbar.notification.fsi.FsiChromeRepo
 import com.android.systemui.statusbar.notification.InstantAppNotifier
 import com.android.systemui.statusbar.notification.fsi.FsiChromeViewModelFactory
@@ -305,4 +306,10 @@ abstract class SystemUICoreStartableModule {
     @IntoMap
     @ClassKey(DreamMonitor::class)
     abstract fun bindDreamMonitor(sysui: DreamMonitor): CoreStartable
+
+    /** Inject into SmartPixelsReceiver.  */
+    @Binds
+    @IntoMap
+    @ClassKey(SmartPixelsReceiver::class)
+    abstract fun bindSmartPixelsReceiver(sysui: SmartPixelsReceiver): CoreStartable
 }
