1b1773e5d16239a282ab493b7b14bec203e08066
Pranav Vashi <neobuddy89@gmail.com>
2023-04-01 02:10:16 +0530

    SystemUI: Follow status bar battery style is QS
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/LargeScreenShadeHeaderController.kt
@@ -280,10 +280,6 @@ class LargeScreenShadeHeaderController @Inject constructor(
         }
         batteryMeterViewController.init()
 
-        // battery settings same as in QS icons
-        batteryMeterViewController.ignoreTunerUpdates()
-        batteryIcon.setPercentShowMode(BatteryMeterView.MODE_ESTIMATE)
-
         iconManager = tintedIconManagerFactory.create(iconContainer, StatusBarLocation.QS)
         iconManager.setTint(
             Utils.getColorAttrDefaultColor(header.context, android.R.attr.textColorPrimary)
