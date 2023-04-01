730bd30bf4edaafca3f7d1544315816e5d91d5a6
Trishiraj <gamerprince.exp@gmail.com>
2023-04-02 00:22:46 +0800

    SystemUI: Add QS Panel Style [1/3]
    QSPanel: Design changes
    
    * Switch to rounded rectangular tiles
    * Slim designed brightness bar
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    Add QS Panel Styles [1/3]
    
    Note for other roms:
    This is the jankiest possible implementation that DOES WORK but is more of an extra work.
    Ideally i'd like to have this implemented within in-line ThemeOverLayApplier and ThemeOverlayController but rather
    its being handled through settings with a custom observer at the moment to trigger the overlays.
    
    So why this impl?
    I was just curious to test this out, but once i saw that its implemented and is working, i decided to push it and work on the refinement later
    
    Feel free to do the refinement on your end if you are able to :D
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    Partial Revert "QSPanel: Design changes"
    
    Prepping this for QS Panel Styles
    
    This reverts commit ef0c7cabca565e8ff4b2f24d5023c8ce9d51f030.
    
    QSTile: More slower & Gradual QS Fade-in/out animation
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Tile: Set ripple color to accent by default
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Tile: Make QS Ripple animation more prominent
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    
    QS Panel: Add Two-Tone Accented QS Style [1/3]
    
    Inspired by Fluid-UI's QS Panel changes
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    
    QS Styles: Add QS tiles color pop [1/2]
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    
    QSPanel Styles: Add key into backup and validator
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    QS Style: Make Outline QS Style truly outline
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    QS Styles: two tone: QS improvements
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    QS Styles: Sync with new QS Styles
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    themes: Translucent Two tone theme
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    mnri: forward port to QPR1, squashed modifications
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: rdx420 <padraramesh420@gmail.com>
    Co-authored-by: elluzion <dyrex2004@gmail.com>
    Change-Id: I21a0c38de2f17c0e58f926fa72aafc47dd702567
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5946,6 +5946,11 @@ public final class Settings {
         public static final String QS_TILE_UI_STYLE = "qs_tile_ui_style";
 
         /**
+         * @hide
+         */
+        public static final String QS_PANEL_STYLE = "qs_panel_style";
+
+        /**
          * Switch between filters for lockscreen album art
          * @hide
          */
