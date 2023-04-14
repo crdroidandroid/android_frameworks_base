0f79b7fc219e7114f6a86d32ee3b6adf0b326ba6
Pranav Vashi <neobuddy89@gmail.com>
2023-04-14 14:52:41 +0530

    SystemUI: Switch to TunerService for CollapsedStatusBar
    * Current implementation of ContentObserver is messy.
    * TunerService covers more cases.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/dagger/StatusBarViewModule.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/dagger/StatusBarViewModule.java
@@ -71,7 +71,6 @@ import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.CarrierConfigTracker;
-import com.android.systemui.util.settings.SecureSettings;
 
 import java.util.concurrent.Executor;
 
@@ -300,9 +299,9 @@ public abstract class StatusBarViewModule {
             CarrierConfigTracker carrierConfigTracker,
             CollapsedStatusBarFragmentLogger collapsedStatusBarFragmentLogger,
             OperatorNameViewController.Factory operatorNameViewControllerFactory,
-            SecureSettings secureSettings,
             @Main Executor mainExecutor,
-            DumpManager dumpManager
+            DumpManager dumpManager,
+            TunerService tunerService
     ) {
         return new CollapsedStatusBarFragment(statusBarFragmentComponentFactory,
                 ongoingCallController,
@@ -322,9 +321,9 @@ public abstract class StatusBarViewModule {
                 carrierConfigTracker,
                 collapsedStatusBarFragmentLogger,
                 operatorNameViewControllerFactory,
-                secureSettings,
                 mainExecutor,
-                dumpManager);
+                dumpManager,
+                tunerService);
     }
 
     /**
