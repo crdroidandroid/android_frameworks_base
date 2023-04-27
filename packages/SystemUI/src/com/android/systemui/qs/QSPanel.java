bc99beb398a7019364b8f8c5a11353f8bd17c3ef
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-27 09:20:16 +0800

    QSPanel: Prevent systemui crash when adjusting tiles height
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -335,7 +335,7 @@ public class QSPanel extends LinearLayout implements Tunable {
     }
 
     private void updateViewPositions() {
-        if (mChildrenLayoutTop == null || mChildrenLayoutTop.isEmpty()) {
+        if (mChildrenLayoutTop == null || mChildrenLayoutTop.isEmpty() || mTileLayout == null) {
             return;
         }
         // Adjust view positions based on tile squishing
