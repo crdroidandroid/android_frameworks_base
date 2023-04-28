a60f8b8eb905855497d763fdae33e8136d6e2f7b
Pranav Vashi <neobuddy89@gmail.com>
2023-04-28 11:01:16 +0530

    SystemUI: Add pixel style lockscreen weather based OmniJaws
    * Similar to SmartSpace but more consistent with keyguard status area.
    * Complete rewrite of omni implementation on Android 11.
    
    Ref:
    https://github.com/crdroidandroid/android_frameworks_base/commit/97734230d0c1f92f5bc87ff8a515b198d92cdd15
    https://github.com/crdroidandroid/android_frameworks_base/commit/05fae49a27afdc68ff9747cbe03de7da0d26bb52
    https://github.com/crdroidandroid/android_frameworks_base/commit/8a923c24fb633b7d9f28e4d78145abc44dbb1350
    
    Change-Id: I138c0dc94f08142f6614659037a501d6ae8909b1
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
@@ -201,6 +201,7 @@ object KeyguardRootViewBinder {
                             view.alpha = alpha
                             childViews[burnInLayerId]?.alpha = alpha
                             childViews[sliceViewId]?.alpha = alpha
+                            childViews[weatherAreaId]?.alpha = alpha
                         }
                     }
 
@@ -230,6 +231,7 @@ object KeyguardRootViewBinder {
                         viewModel.translationY.collect { y ->
                             childViews[burnInLayerId]?.translationY = y
                             childViews[sliceViewId]?.translationY = y
+                            childViews[weatherAreaId]?.translationY = y
                             childViews[largeClockId]?.translationY = y
                             if (com.android.systemui.shared.Flags.clockReactiveSmartspaceLayout()) {
                                 childViews[largeClockDateId]?.translationY = y
@@ -247,6 +249,7 @@ object KeyguardRootViewBinder {
                                     // Large Clock is not translated in the x direction
                                     childViews[burnInLayerId]?.translationX = px
                                     childViews[sliceViewId]?.translationX = px
+                                    childViews[weatherAreaId]?.translationX = px
                                     childViews[aodPromotedNotificationId]?.translationX = px
                                     childViews[aodNotificationIconContainerId]?.translationX = px
                                 }
@@ -296,6 +299,7 @@ object KeyguardRootViewBinder {
                         viewModel.burnInLayerVisibility.collect { visibility ->
                             childViews[burnInLayerId]?.visibility = visibility
                             childViews[sliceViewId]?.visibility = visibility
+                            childViews[weatherAreaId]?.visibility = visibility
                         }
                     }
 
@@ -583,6 +587,7 @@ object KeyguardRootViewBinder {
 
     private val burnInLayerId = R.id.burn_in_layer
     private val sliceViewId = R.id.keyguard_slice_view
+    private val weatherAreaId = R.id.keyguard_weather_area
     private val aodPromotedNotificationId = AodPromotedNotificationSection.viewId
     private val aodNotificationIconContainerId = R.id.aod_notification_icon_container
     private val largeClockId = ClockViewIds.LOCKSCREEN_CLOCK_VIEW_LARGE
