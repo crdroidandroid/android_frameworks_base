a84a41d5d61b1a506b923334248165b030c07008
Pranav Vashi <neobuddy89@gmail.com>
2023-04-28 11:01:16 +0530

    SystemUI: Add pixel style lockscreen weather based OmniJaws
    * Similar to SmartSpace but more consistent with keyguard status area.
    * Complete rewrite of omni implementation on Android 11.
    
    Ref:
    https://github.com/crdroidandroid/android_frameworks_base/commit/97734230d0c1f92f5bc87ff8a515b198d92cdd15
    https://github.com/crdroidandroid/android_frameworks_base/commit/05fae49a27afdc68ff9747cbe03de7da0d26bb52
    https://github.com/crdroidandroid/android_frameworks_base/commit/8a923c24fb633b7d9f28e4d78145abc44dbb1350
    
    Change-Id: I138c0dc94f08142f6614659037a501d6ae8909b1
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardWeatherViewSection.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2024-2026 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.android.systemui.keyguard.ui.view.layout.sections
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.Barrier
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.android.systemui.customization.clocks.R as clocksR
+import com.android.systemui.keyguard.shared.model.KeyguardSection
+import com.android.systemui.res.R
+import com.android.systemui.statusbar.lockscreen.LockscreenSmartspaceController
+import javax.inject.Inject
+
+import com.android.systemui.weather.WeatherInfoView
+
+class KeyguardWeatherViewSection
+@Inject
+constructor(
+    private val context: Context,
+    val layoutInflater: LayoutInflater,
+    val smartspaceController: LockscreenSmartspaceController,
+) : KeyguardSection() {
+    private lateinit var weatherView: WeatherInfoView
+
+    override fun addViews(constraintLayout: ConstraintLayout) {
+        if (!smartspaceController.isOmniWeatherEnabled || smartspaceController.isEnabled) return
+
+        weatherView =
+            layoutInflater.inflate(R.layout.keyguard_weather_area, null, false) as WeatherInfoView
+        constraintLayout.addView(weatherView)
+    }
+
+    override fun bindData(constraintLayout: ConstraintLayout) {
+        if (!smartspaceController.isOmniWeatherEnabled || smartspaceController.isEnabled) return
+
+        weatherView.init()
+    }
+
+    override fun applyConstraints(constraintSet: ConstraintSet) {
+        if (!smartspaceController.isOmniWeatherEnabled || smartspaceController.isEnabled) return
+
+        constraintSet.apply {
+            connect(
+                R.id.keyguard_weather_area,
+                ConstraintSet.START,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.START,
+                context.resources.getDimensionPixelSize(clocksR.dimen.clock_padding_start) +
+                    context.resources.getDimensionPixelSize(clocksR.dimen.status_view_margin_horizontal),
+            )
+            connect(
+                R.id.keyguard_weather_area,
+                ConstraintSet.END,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.END
+            )
+            constrainHeight(R.id.keyguard_weather_area, ConstraintSet.WRAP_CONTENT)
+
+            connect(
+                R.id.keyguard_weather_area,
+                ConstraintSet.TOP,
+                R.id.keyguard_slice_view,
+                ConstraintSet.BOTTOM
+            )
+
+            createBarrier(
+                R.id.smart_space_barrier_bottom,
+                Barrier.BOTTOM,
+                0,
+                *intArrayOf(R.id.keyguard_weather_area)
+            )
+        }
+    }
+
+    override fun removeViews(constraintLayout: ConstraintLayout) {
+        if (!smartspaceController.isOmniWeatherEnabled || smartspaceController.isEnabled) return
+
+        constraintLayout.findViewById<WeatherInfoView?>(R.id.keyguard_weather_area)?.let { weatherArea ->
+            weatherArea.cleanup()
+            constraintLayout.removeView(weatherArea)
+        }
+    }
+}
