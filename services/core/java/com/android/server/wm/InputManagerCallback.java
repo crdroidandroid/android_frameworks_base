ff391de4312af6d5136205aa2461442f0b390ff3
nift4 <nift4@protonmail.com>
2023-04-29 14:53:56 +0200

    WindowManager: finally proper desktop mode handling
    * The previous approach turned out to break Miracast screen mirroring,
      because of the windowing mode.
    * Instead of hardcoding desktop, use FLAG_SHOULD_SHOW_SYSTEM_DECORATIONS
      to detect our custom display, as it's impossible for that flag to be
      set on a non-default display on AOSP.
    * Make InputManagerCallback check desktop mode per-display, to match
      new behaviour.
    
    Change-Id: I4a701a2cb219e49d3f89aac35720a3ea5e37ec52
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/wm/InputManagerCallback.java
+++ services/core/java/com/android/server/wm/InputManagerCallback.java
@@ -257,16 +257,13 @@ final class InputManagerCallback implements InputManagerService.WindowManagerCal
     @Override
     public int getPointerDisplayId() {
         synchronized (mService.mGlobalLock) {
-            // If desktop mode is not enabled, show on the default display.
-            if (!mService.mForceDesktopModeOnExternalDisplays) {
-                return DEFAULT_DISPLAY;
-            }
-
             // Look for the topmost freeform display.
             int firstExternalDisplayId = DEFAULT_DISPLAY;
             for (int i = mService.mRoot.mChildren.size() - 1; i >= 0; --i) {
                 final DisplayContent displayContent = mService.mRoot.mChildren.get(i);
-                if (displayContent.getDisplayInfo().state == Display.STATE_OFF) {
+                if (displayContent.getDisplayInfo().state == Display.STATE_OFF
+                            || !displayContent.forceDesktopMode()) {
+                    // If desktop mode is not enabled, show on the default display.
                     continue;
                 }
                 // Heuristic solution here. Currently when "Freeform windows" developer option is
