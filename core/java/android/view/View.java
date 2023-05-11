75c34307bbf96869819e3e0fd190eca942ec6523
Jia Zekun <jia.zekun@zte.com.cn>
2023-05-11 18:06:21 +0800

    View: Fix not refreshDrawableState when set hovered in focused view
    Fix the focused view flicker when hover enter and exit.
    
    STEPS TO REPRODUCE:
    1. Find an app's activity using the default focus highlight drawable.
       (like WPS office's pdf reader activity)
    2. Open this activity, press tab to focus on a view.
    3. Move the hover in and out from the view.
    
    OBSERVED RESULTS:
    the focused view flicker
    
    EXPECTED RESULTS:
    the focused view not flicker
    
    Bug: https://issuetracker.google.com/281928916
    
    Change-Id: I434c6184ebbce01e51c655c8587016d5959161d9
    Signed-off-by: Jia Jia <jia.jia@zte.com.cn>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -16267,13 +16267,17 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
         if (hovered) {
             if ((mPrivateFlags & PFLAG_HOVERED) == 0) {
                 mPrivateFlags |= PFLAG_HOVERED;
-                refreshDrawableState();
+                if (!isFocused()) {
+                    refreshDrawableState();
+                }
                 onHoverChanged(true);
             }
         } else {
             if ((mPrivateFlags & PFLAG_HOVERED) != 0) {
                 mPrivateFlags &= ~PFLAG_HOVERED;
-                refreshDrawableState();
+                if (!isFocused()) {
+                    refreshDrawableState();
+                }
                 onHoverChanged(false);
             }
         }
