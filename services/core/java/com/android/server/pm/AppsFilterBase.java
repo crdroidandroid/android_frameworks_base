08293af4a6c97c5d740133980ff68a8b7fb08746
Oliver Scott <olivercscott@gmail.com>
2023-05-17 15:42:52 -0400

    Filter Google Translate queries for GMS
    Bit of a hack to pretend that microG is not available,
    to make apps work
    
    Issue: calyxos#1144
    Change-Id: Ic5ddb78b1014ce567d1a5c57fc79f79edd1154c0

--- services/core/java/com/android/server/pm/AppsFilterBase.java
+++ services/core/java/com/android/server/pm/AppsFilterBase.java
@@ -37,6 +37,7 @@ import android.util.Slog;
 import android.util.SparseArray;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.function.QuadFunction;
 import com.android.server.om.OverlayReferenceMapper;
 import com.android.server.pm.pkg.AndroidPackage;
@@ -62,6 +63,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public abstract class AppsFilterBase implements AppsFilterSnapshot {
     protected static final String TAG = "AppsFilter";
+
+    private static final String GMS = "com.google.android.gms";
+    private static final String[] GMS_HIDDEN_PACKAGES = {"com.google.android.apps.translate"};
 
     // Logs all filtering instead of enforcing
     protected static final boolean DEBUG_ALLOW_ALL = false;
@@ -508,6 +512,15 @@ public abstract class AppsFilterBase implements AppsFilterSnapshot {
                 if (isForceQueryable(targetAppId)) {
                     if (DEBUG_LOGGING) {
                         log(callingSetting, targetPkgSetting, "force queryable");
+                    }
+                    if (GMS.equals(targetPkgSetting.getPackageName())
+                            && callingPkgSetting != null) {
+                        // HACK: Hide GMS from these packages
+                        // Breaks login but makes them work
+                        if (ArrayUtils.contains(GMS_HIDDEN_PACKAGES,
+                                callingPkgSetting.getPackageName())) {
+                            return true;
+                        }
                     }
                     return false;
                 }
