8e48b0f7028c6190c8e0c08a54a397a44476b7b1
Karthik Gopalan <quic_gkarth@quicinc.com>
2023-05-19 13:08:43 +0530

    DSR: Fix broken DSR
    Fix broken DSR (delayed service restart).
    
    CRs-Fixed: 3506395
    
    Change-Id: I26538d58b2c550f5a63020e8a0d19f5f660544ab
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -1402,6 +1402,9 @@ class TaskFragment extends WindowContainer<WindowContainer> {
         // appropriate for it.
         mTaskSupervisor.mStoppingActivities.remove(next);
 
+        if (!next.translucentWindowLaunch)
+            next.launching = true;
+
         if (DEBUG_SWITCH) Slog.v(TAG_SWITCH, "Resuming " + next);
 
         mTaskSupervisor.setLaunchSource(next.info.applicationInfo.uid);
