544434c3ed02d15b0605c8052a6ee967e98235b2
Pranav Vashi <neobuddy89@gmail.com>
2023-05-26 00:00:42 +0530

    base: Add overlay to mock oplus LinearmotorVibratorService
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -1636,14 +1636,19 @@ public final class SystemServiceRegistry {
                         return new E2eeContactKeysManager(ctx);
                     }});
 
-        registerService(Context.LINEARMOTOR_VIBRATOR_SERVICE, LinearmotorVibrator.class,
-                new CachedServiceFetcher<LinearmotorVibrator>() {
-            @Override
-            public LinearmotorVibrator createService(ContextImpl ctx) {
-                IBinder binder = ServiceManager.getService(Context.LINEARMOTOR_VIBRATOR_SERVICE);
-                ILinearmotorVibratorService service = ILinearmotorVibratorService.Stub.asInterface(binder);
-                return new LinearmotorVibrator(ctx.getOuterContext(), service);
-            }});
+        boolean mockOplus = Resources.getSystem()
+            .getBoolean(com.android.internal.R.bool.config_mockOplusLinearmotorVibratorService);
+
+        if (mockOplus) {
+            registerService(Context.LINEARMOTOR_VIBRATOR_SERVICE, LinearmotorVibrator.class,
+                    new CachedServiceFetcher<LinearmotorVibrator>() {
+                @Override
+                public LinearmotorVibrator createService(ContextImpl ctx) {
+                    IBinder binder = ServiceManager.getService(Context.LINEARMOTOR_VIBRATOR_SERVICE);
+                    ILinearmotorVibratorService service = ILinearmotorVibratorService.Stub.asInterface(binder);
+                    return new LinearmotorVibrator(ctx.getOuterContext(), service);
+                }});
+        }
 
         // DO NOT do a flag check like this unless the flag is read-only.
         // (because this code is executed during preload in zygote.)
