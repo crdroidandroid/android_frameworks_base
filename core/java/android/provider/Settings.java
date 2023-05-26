8ef6187abefefb896dd37dbda99760d756c29f94
Ghosuto <clash.raja10@gmail.com>
2023-05-26 12:39:30 +0200

    SystemUI: Allow toggle dual tone QS background [1/2]
    neobuddy89: Keep dual tone QS as default.
    
    Change-Id: I885fc0a052374af31a55d6dc6e0fcec9e94d48ff
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6118,6 +6118,12 @@ public final class Settings {
         public static final String QQS_LAYOUT_ROWS = "qqs_layout_rows";
 
         /**
+         * Enable and Disable Dual Tone Colors QsPanel
+         * @hide
+         */
+        public static final String QS_DUAL_TONE = "qs_dual_tone";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
