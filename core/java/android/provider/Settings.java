4d58d0e7307edafd4e33b6f06a010b881b526520
Pranav Vashi <neobuddy89@gmail.com>
2023-06-30 17:03:36 +0530

    Add toggle to enable brightness slider haptic feedback [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6228,6 +6228,12 @@ public final class Settings {
         public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
 
         /**
+         * Haptic feedback on brightness slider
+         * @hide
+         */
+        public static final String QS_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
