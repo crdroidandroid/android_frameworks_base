ae46cb492d396fc831f6a885f020cdbfc4b84406
Pranav Vashi <neobuddy89@gmail.com>
2023-06-30 17:03:36 +0530

    Add toggle to enable brightness slider haptic feedback [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessController.java
@@ -77,6 +77,8 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
 
     private static final String SCREEN_BRIGHTNESS_MODE =
             "system:" + Settings.System.SCREEN_BRIGHTNESS_MODE;
+    private static final String QS_BRIGHTNESS_SLIDER_HAPTIC =
+            "system:" + Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC;
 
     private final ImageView mIcon;
     private final int mDisplayId;
@@ -113,6 +115,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
     private Vibrator mVibrator;
     private static final VibrationEffect BRIGHTNESS_SLIDER_HAPTIC =
             VibrationEffect.get(VibrationEffect.EFFECT_TICK);
+    private boolean mBrightnessSliderHaptic;
 
     @Override
     public void setMirror(BrightnessMirrorController controller) {
@@ -127,6 +130,10 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                     mBackgroundHandler.post(mUpdateModeRunnable);
                     mBackgroundHandler.post(mUpdateSliderRunnable);
                 }
+                break;
+            case QS_BRIGHTNESS_SLIDER_HAPTIC:
+                mBrightnessSliderHaptic =
+                        TunerService.parseIntegerSwitch(newValue, false);
                 break;
             default:
                 break;
@@ -312,6 +319,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
                     Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC,
                 UserHandle.USER_CURRENT));
         tunerService.addTunable(this, SCREEN_BRIGHTNESS_MODE);
+        tunerService.addTunable(this, QS_BRIGHTNESS_SLIDER_HAPTIC);
     }
 
     public void registerCallbacks() {
@@ -357,7 +365,7 @@ public class BrightnessController implements ToggleSlider.Listener, MirroredBrig
         setBrightness(valFloat);
 
         // Give haptic feedback only if brightness is changed manually
-        if (mVibrator != null && tracking)
+        if (mBrightnessSliderHaptic && mVibrator != null && tracking)
             mVibrator.vibrate(BRIGHTNESS_SLIDER_HAPTIC);
 
         if (!tracking) {
