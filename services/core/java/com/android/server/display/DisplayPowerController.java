e6afa747fce5f82f04adadd4a4e3ea6cdc937c6e
Jurijs Oniscuks <jurijs.oniscuks@sony.com>
2023-07-13 19:29:11 +0200

    Save AutomaticBrightnessStrategy state before applying changes
    When deciding whether brightness change should be slow or fast we use
    "hasAppliedAutoBrightness()". However, this value is always reset to
    true by "setAutoBrightnessState()". The correct way is to save
    "hasAppliedAutoBrightness()" before "setAutoBrightnessState()" and use
    the saved value.
    
    Bug: 304313764
    Test: manual
    Change-Id: I0ace4523813099af266a37a984cbd785b462403e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -1481,6 +1481,9 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         // request changes.
         final boolean wasShortTermModelActive =
                 mAutomaticBrightnessStrategy.isShortTermModelActive();
+        // Save hasAppliedAutoBrightness state before applying the current request changes.
+        final boolean oldAppliedAutoBrightness =
+                mAutomaticBrightnessStrategy.hasAppliedAutoBrightness();
         mAutomaticBrightnessStrategy.setAutoBrightnessState(state,
                 mDisplayBrightnessController.isAllowAutoBrightnessWhileDozingConfig(),
                 mBrightnessReasonTemp.getReason(), mPowerRequest.policy,
@@ -1515,8 +1518,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                             .getRawAutomaticScreenBrightness();
                     brightnessState = clampScreenBrightness(brightnessState);
                     // slowly adapt to auto-brightness
-                    // TODO(b/253226419): slowChange should be decided by strategy.updateBrightness
-                    slowChange = mAutomaticBrightnessStrategy.hasAppliedAutoBrightness()
+                    slowChange = oldAppliedAutoBrightness
                             && !mAutomaticBrightnessStrategy.getAutoBrightnessAdjustmentChanged();
                     brightnessAdjustmentFlags =
                             mAutomaticBrightnessStrategy.getAutoBrightnessAdjustmentReasonsFlags();
