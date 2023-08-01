2fb727ded5175e1413b50bfe016cd985a3e79761
someone5678 <nemui3353@gmail.com>
2023-08-01 13:23:09 +0900

    Cancel long swipe for cancelBack in navigation gesture
    * When MotionEvent.ACTION_CANCEL, back gesture is cancelled
      Let's follow those behavior in long swipe too
    
    Change-Id: I04439f0cd5a3be3a8a07863f8e1be3264fd7bea8
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -380,6 +380,7 @@ public class EdgeBackGestureHandler implements TunerService.Tunable {
                 public void cancelBack() {
                     if (mBackAnimation != null) {
                         mBackAnimation.setTriggerBack(false);
+                        mBackAnimation.setTriggerLongSwipe(false);
                     }
                     logGesture(SysUiStatsLog.BACK_GESTURE__TYPE__INCOMPLETE);
                 }
