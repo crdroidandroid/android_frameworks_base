674cec468f2b125682717bd8f74cd57a8572e168
Yong Li <yongl0722@fb.com>
2023-08-09 16:56:40 -0700

    Fix race condition in process observer
    There is a race condition in dispatching the changes to the process
    observer. When a `ProcessChangeItem` is initialized and retrieved during
    `enqueueProcessChangeItemLocked()`, the change information will be set
    with the handle retrieved. The change item will be processed in the UI
    thread via a message.
    
    However, there could be race condition. When the change item is
    processed in the UI thread, its actual change information is not yet
    populated and hence the dispatch will find no changes and do nothing.
    
    This patch makes creating the `ProcessChangeItem` to be atomic so that
    when the message to process is sent, it will have all the information.
    
    Test: unit test
      $ atest MockingOomAdjusterTests
      x86_64 FrameworksMockingServicesTests: Passed: 83, Failed: 0, Ignored: 0, Assumption Failed: 0,
      All tests passed!
    
    Change-Id: If19210c06a02f7c11bc2ab78c121adb602f42ff1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/tests/mockingservicestests/src/com/android/server/am/MockingOomAdjusterTests.java
+++ services/tests/mockingservicestests/src/com/android/server/am/MockingOomAdjusterTests.java
@@ -204,7 +204,8 @@ public class MockingOomAdjusterTests {
         doCallRealMethod().when(sService).updateOomAdjPendingTargetsLocked(OOM_ADJ_REASON_ACTIVITY);
         setFieldValue(AppProfiler.class, profiler, "mProfilerLock", new Object());
         doReturn(new ActivityManagerService.ProcessChangeItem()).when(pr)
-                .enqueueProcessChangeItemLocked(anyInt(), anyInt());
+                .enqueueProcessChangeItemLocked(anyInt(), anyInt(), anyInt(), anyBoolean(),
+                    anyInt());
         sService.mOomAdjuster = new OomAdjuster(sService, sService.mProcessList,
                 new ActiveUids(sService, false));
         sService.mOomAdjuster.mAdjSeq = 10000;
