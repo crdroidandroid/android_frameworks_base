4c4bc2e23e092a98332ad37ab3ed7e395e971075
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-03 09:42:39 +0800

    BluetoothControllerImpl: Optimize connected battery level scanning
    * replace streaming with for loop to reduce overheads
    * as soon as we find a device with a known battery level, perform an early return. This avoids scanning all devices when not necessary.
    
    Change-Id: I9cc9e8672908cc5e19939c6b37223b862cb9e5bc
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BluetoothControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BluetoothControllerImpl.java
@@ -18,6 +18,7 @@ package com.android.systemui.statusbar.policy;
 
 import android.annotation.Nullable;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.os.Handler;
@@ -342,11 +343,13 @@ public class BluetoothControllerImpl implements BluetoothController, BluetoothCa
             if (mConnectedDevices.isEmpty()) {
                 return BluetoothDevice.BATTERY_LEVEL_UNKNOWN;
             }
-            return mConnectedDevices.stream()
-                .mapToInt(device -> device.getBatteryLevel())
-                .filter(level -> level != BluetoothDevice.BATTERY_LEVEL_UNKNOWN)
-                .findFirst()
-                .orElse(BluetoothDevice.BATTERY_LEVEL_UNKNOWN);
+            for (CachedBluetoothDevice device : mConnectedDevices) {
+                int level = device.getBatteryLevel();
+                if (level != BluetoothDevice.BATTERY_LEVEL_UNKNOWN) {
+                    return level;
+                }
+            }
+            return BluetoothDevice.BATTERY_LEVEL_UNKNOWN;
         }
     }
 
