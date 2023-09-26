93c2dae30bc2e9c7bb3af66675a4b16b7aa3ab9d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-26 10:01:36 +0800

    SystemUI: Fix exception when retrieving signal strength drawable
    09-26 09:40:48.669  1674  1674 W LayerDrawable: java.lang.RuntimeException
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.graphics.drawable.LayerDrawable$ChildDrawable.<init>(LayerDrawable.java:1857)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.graphics.drawable.LayerDrawable$LayerState.<init>(LayerDrawable.java:1977)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.graphics.drawable.LayerDrawable.createConstantState(LayerDrawable.java:174)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.graphics.drawable.LayerDrawable.mutate(LayerDrawable.java:1782)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.widget.ImageView.applyImageTint(ImageView.java:735)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.widget.ImageView.updateDrawable(ImageView.java:1079)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.widget.ImageView.setImageDrawable(ImageView.java:599)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at com.android.systemui.qs.tiles.dialog.InternetDialog.lambda$setMobileDataLayout$9(InternetDialog.java:464)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at com.android.systemui.qs.tiles.dialog.InternetDialog.$r8$lambda$aZr1ClSzTwrXPDkqWy6PlA9znDs(Unknown Source:0)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at com.android.systemui.qs.tiles.dialog.InternetDialog$$ExternalSyntheticLambda34.run(Unknown Source:4)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.os.Handler.handleCallback(Handler.java:942)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.os.Handler.dispatchMessage(Handler.java:99)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.os.Looper.loopOnce(Looper.java:201)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.os.Looper.loop(Looper.java:288)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at android.app.ActivityThread.main(ActivityThread.java:7942)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at java.lang.reflect.Method.invoke(Native Method)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
    09-26 09:40:48.669  1674  1674 W LayerDrawable: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:848)
    
    Change-Id: Ifff6b832872deb4d91fbe92b71508f882e5fcd45
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
@@ -447,7 +447,7 @@ public class InternetDialogDelegateLegacy implements
         internetContent.mCurrentSatelliteState =
                 mInternetDetailsContentController.getCurrentSatelliteState();
         internetContent.mDefaultSubSignalStrengthIcon =
-                mInternetDetailsContentController.getSignalStrengthDrawable(mDefaultDataSubId);
+                mInternetDetailsContentController.getSignalStrengthDrawable(mDefaultDataSubId).mutate();
 
         return internetContent;
     }
