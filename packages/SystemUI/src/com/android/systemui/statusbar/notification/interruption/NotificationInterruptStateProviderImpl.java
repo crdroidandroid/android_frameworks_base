3987074f202ccd011f7902579ebe26e241842439
Pranav Vashi <neobuddy89@gmail.com>
2023-10-04 00:01:40 +0530

    Rework "SystemUI: Turn less boring headsup whitelist into an overlay"
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderImpl.java
@@ -31,7 +31,9 @@ import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.provider.Settings;
+import android.provider.Telephony.Sms;
 import android.service.notification.StatusBarNotification;
+import android.telecom.TelecomManager;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.UiEvent;
@@ -83,6 +85,8 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
     private boolean mLessBoringHeadsUp = false;
     private boolean mReTicker = false;
     private Context mContext;
+
+    private List<String> mHeadsUpAllowList;
 
     public enum NotificationInterruptEvent implements UiEventLogger.UiEventEnum {
         @UiEvent(doc = "FSI suppressed for suppressive GroupAlertBehavior")
@@ -169,6 +173,18 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
         };
 
         if (ENABLE_HEADS_UP) {
+            String defaultSmsPackage = getDefaultSmsPackage(context);
+            String defaultDialerPackage = getDefaultDialerPackage(context);
+
+            mHeadsUpAllowList = Arrays.asList(
+                    mContext.getResources().getStringArray(R.array.config_boringHeadsUpPackageAllowList));
+
+            if (!defaultSmsPackage.isEmpty() && !mHeadsUpAllowList.contains(defaultSmsPackage))
+                mHeadsUpAllowList.add(defaultSmsPackage);
+
+            if (!defaultDialerPackage.isEmpty() && !mHeadsUpAllowList.contains(defaultDialerPackage))
+                mHeadsUpAllowList.add(defaultDialerPackage);
+
             mContentResolver.registerContentObserver(
                     Settings.Global.getUriFor(Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED),
                     true,
@@ -184,8 +200,9 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
                     Settings.System.getUriFor(Settings.System.RETICKER_STATUS),
                     true,
                     headsUpObserver);
+
+            headsUpObserver.onChange(true); // set up
         }
-        headsUpObserver.onChange(true); // set up
     }
 
     @Override
@@ -568,14 +585,17 @@ public class NotificationInterruptStateProviderImpl implements NotificationInter
             return false;
         }
 
-        String notificationPackageName = entry.getSbn().getPackageName();
+        return !mHeadsUpAllowList.contains(entry.getSbn().getPackageName().toLowerCase());
+    }
 
-        List<String> headsUpWhitelist =
-                    Arrays.asList(mContext.getResources().getStringArray(R.array.heads_up_whitelist_packages));
+    private static String getDefaultSmsPackage(Context ctx) {
+        // for reference, there's also a new RoleManager api with getDefaultSmsPackage(context, userid)
+        return Sms.getDefaultSmsPackage(ctx);
+    }
 
-        boolean shouldSkip = !headsUpWhitelist.contains(notificationPackageName.toLowerCase());
-
-        return shouldSkip;
+    private static String getDefaultDialerPackage(Context ctx) {
+        TelecomManager tm = (TelecomManager) ctx.getSystemService(Context.TELECOM_SERVICE);
+        return tm != null ? tm.getDefaultDialerPackage() : "";
     }
 
     /**
