67f7d9b21da309a181e90bcc14490c68902f8a34
Pranav Vashi <neobuddy89@gmail.com>
2023-10-22 21:05:25 +0530

    Battery Styles: Update plus color for battery
    * A14 changed this in 7a8592e923714632a805412080f0af207aa662e3
    
    Change-Id: I870d8a312ae6304a8984de87b72376731fd4c18a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/graph/LandscapeBatteryDrawable.kt
+++ packages/SettingsLib/src/com/android/settingslib/graph/LandscapeBatteryDrawable.kt
@@ -142,7 +142,7 @@ open class LandscapeBatteryDrawable(private val context: Context, frameColor: In
     }
 
     private val errorPaint = Paint(Paint.ANTI_ALIAS_FLAG).also { p ->
-        p.color = Utils.getColorStateListDefaultColor(context, R.color.batterymeter_plus_color)
+        p.color = Utils.getColorStateListDefaultColor(context, R.color.batterymeter_saver_color)
         p.alpha = 255
         p.isDither = true
         p.strokeWidth = 0f
