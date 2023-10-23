84222f0f80165ef4fd89dc2a8cae72b3d2da5248
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-23 15:00:09 +0300

    add notification-based UI for the background dexopt job
    - report job progress when it's running
    - prompt the user to restart optimized apps after job completion
    - schedule automatic restart of optimized apps via ActivityManager.killProcessesWhenImperceptible()
    in case they aren't restarted manually
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerLocal.java
+++ services/core/java/com/android/server/pm/PackageManagerLocal.java
@@ -16,6 +16,7 @@
 
 package com.android.server.pm;
 
+import android.annotation.ElapsedRealtimeLong;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -191,4 +192,11 @@ public interface PackageManagerLocal {
     }
 
     void showDexoptProgressBootMessage(int percentage, int current, int total);
+
+    void onBgDexoptProgressUpdate(@ElapsedRealtimeLong long start, int percentage, int current, int total);
+
+    void onBgDexoptCompleted(
+            // com.android.server.art.model.DexoptResult
+            @Nullable Object dexOptResult,
+            long durationMs);
 }
