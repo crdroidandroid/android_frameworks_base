982588f5756fb7698c49994533e6148e46cbdfe9
Dhina17 <dhinalogu@gmail.com>
2023-10-28 11:11:51 +0000

    SystemUI: Simplify udfps animation style selection
    if you want to disable the animation, add none or any string as (which don't have drawables)
    item in the index (which you consider as disabled value)
    of the arrays in the animation package.
    
    Example:
    I added <item>None</item> to the arrays in the index 0
    since I want to use 0 as disabled value.
    
    I don't have any drawables in the name of "None" in the
    udfps animation package.
    It's important to make sure.
    
    Change-Id: I4fe1e34fb3402ed2168fc7d8f15a69fb4084faf3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6636,11 +6636,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String UDFPS_ANIM = "udfps_anim";
-
-        /**
-         * @hide
-         */
         public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
 
         /**
