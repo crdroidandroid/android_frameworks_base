e9b7fa22babc6f160d1a44d73edeefda600b2f59
Dhina17 <dhinalogu@gmail.com>
2023-10-28 11:11:51 +0000

    SystemUI: Simplify udfps animation style selection
    if you want to disable the animation, add none or any string as (which don't have drawables)
    item in the index (which you consider as disabled value)
    of the arrays in the animation package.
    
    Example:
    I added <item>None</item> to the arrays in the index 0
    since I want to use 0 as disabled value.
    
    I don't have any drawables in the name of "None" in the
    udfps animation package.
    It's important to make sure.
    
    Change-Id: I4fe1e34fb3402ed2168fc7d8f15a69fb4084faf3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
@@ -344,15 +344,13 @@ class AuthRippleController @Inject constructor(
     private val udfpsControllerCallback =
         object : UdfpsController.Callback {
             override fun onFingerDown() {
-                if (Settings.System.getInt(sysuiContext.contentResolver,
-                       Settings.System.UDFPS_ANIM, 0) == 0) {
+                if (udfpsController?.isAnimationEnabled() == false) {
                     showDwellRipple()
                 }
             }
 
             override fun onFingerUp() {
-                if (Settings.System.getInt(sysuiContext.contentResolver,
-                        Settings.System.UDFPS_ANIM, 0) == 0) {
+                if (udfpsController?.isAnimationEnabled() == false) {
                     mView.retractDwellRipple()
                 }
             }
