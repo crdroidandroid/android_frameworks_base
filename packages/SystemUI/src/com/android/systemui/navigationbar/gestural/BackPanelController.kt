dbcd440122540e5c6d865c1bb79e1751cd585a63
Pranav Vashi <neobuddy89@gmail.com>
2023-11-01 23:22:02 +0530

    Allow to hide gesture on new back gesture affordance
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/BackPanelController.kt
@@ -180,6 +180,8 @@ internal constructor(
     private var triggerLongSwipe = false
     private var isLongSwipeEnabled = false
 
+    private var backArrowVisibility = false
+
     internal enum class GestureState {
         /* Arrow is off the screen and invisible */
         GONE,
@@ -680,6 +682,10 @@ internal constructor(
         setTriggerLongSwipe(isLongSwipeEnabled && triggerLongSwipe)
     }
 
+    override fun setBackArrowVisibility(enabled: Boolean) {
+        backArrowVisibility = enabled
+    }
+
     private fun setTriggerLongSwipe(enabled: Boolean) {
         if (triggerLongSwipe != enabled) {
             triggerLongSwipe = enabled
@@ -946,7 +952,7 @@ internal constructor(
                 mView.isVisible = false
             }
             GestureState.ENTRY -> {
-                mView.isVisible = true
+                mView.isVisible = if (backArrowVisibility) true else false
 
                 updateRestingArrowDimens()
                 gestureEntryTime = SystemClock.uptimeMillis()
