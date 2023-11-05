2ebca224b68db3499ea16bea6992b800c5e0f6b8
Pranav Vashi <neobuddy89@gmail.com>
2023-11-05 08:31:41 +0530

    Revert "SystemUI: Avoid using null Context in onTuningChanged()"
    This reverts commit 7fa5fc34fdff2560d297aaf0f5f80dd2ad37d6e8.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -148,7 +148,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
 
     private ClockController mClockController;
-    private Context mContext;
     private boolean mIsClockBlacklisted;
 
     private List<String> mBlockedIcons = new ArrayList<>();
@@ -316,8 +315,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         showClock(false);
         initOperatorName();
         initNotificationIconArea();
-
-        mContext = getContext();
         Dependency.get(TunerService.class).addTunable(this, StatusBarIconController.ICON_HIDE_LIST);
 
         mSystemEventAnimator = getSystemEventAnimator();
@@ -418,7 +415,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     public void onTuningChanged(String key, String newValue) {
         boolean wasClockBlacklisted = mIsClockBlacklisted;
         mIsClockBlacklisted = StatusBarIconController.getIconHideList(
-                mContext, newValue).contains("clock");
+                getContext(), newValue).contains("clock");
         if (wasClockBlacklisted && !mIsClockBlacklisted) {
             showClock(false);
         }
