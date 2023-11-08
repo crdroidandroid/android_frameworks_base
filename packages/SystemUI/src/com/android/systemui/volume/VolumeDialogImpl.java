09cf8f00905b7da057731452606b0f1f75b90992
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-08 14:27:01 +0800

    SystemUI: VolumeDialog: Ensure proper resource release
    [neobuddy89: Do not nullify mDialog - it results in NPE in onStateChanged]
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -503,6 +503,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     @Override
     public void onUiModeChanged() {
         mContext.getTheme().applyStyle(mContext.getThemeResId(), true);
+        mConfigChanged = true;
     }
 
     public void init(int windowType, Callback callback) {
@@ -636,6 +637,13 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private void initDialog(int lockTaskModeState) {
         Log.d(TAG, "initDialog: called!");
+        if (mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
+        if (mConfigurableTexts != null) {
+            mConfigurableTexts = null;
+        }
         mDialog = new CustomDialog(mContext);
         initDimens();
         adjustPositionOnScreen();
