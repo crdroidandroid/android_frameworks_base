ab3026ca6f55e8ad7b019af297acbd1920099fd5
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-12 08:40:17 +0800

    services: Implement transition boosting
    Squash:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 30 Jun 2024 18:23:49 +0530
        Subject: services: Simplify transition boosting
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/Transition.java
+++ services/core/java/com/android/server/wm/Transition.java
@@ -85,11 +85,13 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.HardwareBuffer;
+import android.hardware.power.Boost;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.IRemoteCallback;
 import android.os.Looper;
+import android.os.PowerManagerInternal;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.Trace;
@@ -112,6 +114,7 @@ import com.android.internal.protolog.ProtoLog;
 import com.android.internal.protolog.ProtoLogGroup;
 import com.android.internal.util.function.pooled.PooledLambda;
 import com.android.server.inputmethod.InputMethodManagerInternal;
+import com.android.server.LocalServices;
 import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.window.flags.Flags;
 
@@ -185,6 +188,8 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
     final TransitionController mController;
     private final BLASTSyncEngine mSyncEngine;
     private final Token mToken;
+
+    private final PowerManagerInternal mPowerManagerInternal;
 
     private @Nullable ActivityRecord mPipActivity;
 
@@ -351,6 +356,8 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
 
         mLogger.mCreateWallTimeMs = System.currentTimeMillis();
         mLogger.mCreateTimeNs = SystemClock.elapsedRealtimeNanos();
+
+        mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
     }
 
     @Nullable
@@ -699,6 +706,9 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
             return;
         }
         mState = STATE_STARTED;
+        if (mPowerManagerInternal != null && mType == TRANSIT_CHANGE) {
+            mPowerManagerInternal.setPowerBoost(Boost.DISPLAY_UPDATE_IMMINENT, 80);
+        }
         ProtoLog.v(ProtoLogGroup.WM_DEBUG_WINDOW_TRANSITIONS, "Starting Transition %d",
                 mSyncId);
         applyReady();
