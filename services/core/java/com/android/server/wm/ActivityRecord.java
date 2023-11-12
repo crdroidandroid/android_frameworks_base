44fc7ab339d50270a8d9dad53ed6720752748c43
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-12 08:15:47 +0800

    services: Implement activity boost optimization
    @neobuddy89:
    * Declare services in constructor.
    * Add mIsBoosted to ensure we dont cancel boost not enabled by us.
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -314,6 +314,7 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.gui.DropInputMode;
 import android.hardware.HardwareBuffer;
+import android.hardware.power.Mode;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
@@ -322,6 +323,7 @@ import android.os.Debug;
 import android.os.IBinder;
 import android.os.IRemoteCallback;
 import android.os.PersistableBundle;
+import android.os.PowerManagerInternal;
 import android.os.Process;
 import android.os.RemoteCallbackList;
 import android.os.RemoteException;
@@ -1009,6 +1011,9 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
     /** Non-zero to pause dispatching configuration changes to the client. */
     int mPauseConfigurationDispatchCount = 0;
+
+    private final PowerManagerInternal mPowerManagerInternal;
+    private boolean mIsBoosted;
 
     private final Runnable mPauseTimeoutRunnable = new Runnable() {
         @Override
@@ -2298,6 +2303,8 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                             return appContext;
                         });
         mCallerState = new ActivityCallerState(mAtmService);
+
+        mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
     }
 
     /**
@@ -6314,12 +6321,13 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 Slog.v(TAG_VISIBILITY, "Start visible activity, " + this);
             }
             setState(STARTED, "makeActiveIfNeeded");
-
+            setActivityBoost(true);
             try {
                 mAtmService.getLifecycleManager().scheduleTransactionItem(app.getThread(),
                         StartActivityItem.obtain(token, takeSceneTransitionInfo()));
             } catch (Exception e) {
                 Slog.w(TAG, "Exception thrown sending start: " + intent.getComponent(), e);
+                setActivityBoost(false);
             }
             // The activity may be waiting for stop, but that is no longer appropriate if we are
             // starting the activity again
@@ -6948,8 +6956,16 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         }
     }
 
+    protected void setActivityBoost(boolean enable) {
+        if (mIsBoosted != enable && mPowerManagerInternal != null) {
+            mIsBoosted = enable;
+            mPowerManagerInternal.setPowerMode(Mode.LAUNCH, enable);
+        }
+    }
+
     /** Called when the windows associated app window container are drawn. */
     private void onWindowsDrawn() {
+        setActivityBoost(false);
         final TransitionInfoSnapshot info = mTaskSupervisor
                 .getActivityMetricsLogger().notifyWindowsDrawn(this);
         final boolean validInfo = info != null;
