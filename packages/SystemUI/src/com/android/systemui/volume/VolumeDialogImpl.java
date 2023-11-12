baba42566bc1924bcf3681ef7fd355fa7b031dff
Pranav Vashi <neobuddy89@gmail.com>
2023-11-12 21:24:42 +0530

    SystemUI: VolumeDialog: Open volume panel on expand long click
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1544,6 +1544,16 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                 updateRowsH(mDefaultRow, true);
                 mExpandRows.setExpanded(mExpansionState == ExpansionState.EXPANDED);
             });
+            mExpandRows.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Events.writeEvent(Events.EVENT_SETTINGS_CLICK);
+                    dismissH(DISMISS_REASON_SETTINGS_CLICKED);
+                    mVolumeNavigator.openVolumePanel(
+                            mVolumePanelNavigationInteractor.getVolumePanelRoute());
+                    return true;
+                }
+            });
         }
     }
 
