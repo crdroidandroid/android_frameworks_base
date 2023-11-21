d4316ae74b3f96b89b4884556fb5a375dbcb5578
Pranav Vashi <neobuddy89@gmail.com>
2023-11-21 21:36:56 +0530

    ActiveServices: Add delayed service restart (DSR) due to app launch
    * Pulled from CAF
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
+++ services/core/java/com/android/server/wm/ActivityTaskSupervisor.java
@@ -2069,6 +2069,10 @@ public class ActivityTaskSupervisor implements RecentTasks.Callbacks {
         return timedout;
     }
 
+    public ActivityRecord getTopResumedActivity() {
+        return mTopResumedActivity;
+    }
+
     void comeOutOfSleepIfNeededLocked() {
         removeSleepTimeouts();
         if (mGoingToSleepWakeLock.isHeld()) {
