2c02dda3ee93d7f4c2a51a7f363ffe642f8bf570
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-21 21:30:51 +0530

    SystemUI: Filter out duplicate notification icons on statusbar
    Wonder why it wasn't done already smh...
    
    Change-Id: I9296fa8ed0f7d95a8024af23dfa53cf6ad9aa5be
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -428,7 +428,11 @@ public class NotificationIconAreaController implements
                         hideRepliedMessages, hideCurrentMedia, hidePulsing)) {
                     StatusBarIconView iconView = function.apply(entry);
                     if (iconView != null) {
-                        toShow.add(iconView);
+                        // Filter out duplicate icons.
+                        if (!toShow.stream().anyMatch(
+                                v -> v.equalIcons(v.getSourceIcon(), iconView.getSourceIcon()))) {
+                            toShow.add(iconView);
+                        }
                     }
                 }
             }
