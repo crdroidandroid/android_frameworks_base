8a6cf3ccaa3a6a78e0a39f199b038540429a6b13
Pranav Vashi <neobuddy89@gmail.com>
2023-11-26 12:28:14 +0530

    Add optional weather condition text on lockscreen
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/crdroid/CurrentWeatherView.java
+++ packages/SystemUI/src/com/android/systemui/crdroid/CurrentWeatherView.java
@@ -39,10 +39,14 @@ public class CurrentWeatherView extends FrameLayout implements OmniJawsClient.Om
     private OmniJawsClient.WeatherInfo mWeatherInfo;
     private TextView mLeftText;
     private TextView mRightText;
+    private TextView mWeatherText;
 
     private SettingsObserver mSettingsObserver;
 
     private boolean mShowWeatherLocation;
+    private boolean mShowWeatherText;
+
+    private Context mContext;
 
     public CurrentWeatherView(Context context) {
         this(context, null);
@@ -54,6 +58,7 @@ public class CurrentWeatherView extends FrameLayout implements OmniJawsClient.Om
 
     public CurrentWeatherView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mContext = context;
         if (mWeatherClient == null) {
             mWeatherClient = new OmniJawsClient(context);
         }
@@ -78,6 +83,7 @@ public class CurrentWeatherView extends FrameLayout implements OmniJawsClient.Om
         mCurrentImage  = (ImageView) findViewById(R.id.current_image);
         mLeftText = (TextView) findViewById(R.id.left_text);
         mRightText = (TextView) findViewById(R.id.right_text);
+        mWeatherText = (TextView) findViewById(R.id.weather_text);
         if (mSettingsObserver == null) {
             mSettingsObserver = new SettingsObserver(new Handler());
             mSettingsObserver.observe();
@@ -88,6 +94,7 @@ public class CurrentWeatherView extends FrameLayout implements OmniJawsClient.Om
         mCurrentImage.setImageDrawable(null);
         mLeftText.setText("");
         mRightText.setText("");
+        mWeatherText.setText("");
     }
 
     @Override
@@ -121,11 +128,29 @@ public class CurrentWeatherView extends FrameLayout implements OmniJawsClient.Om
             mWeatherClient.queryWeather();
             mWeatherInfo = mWeatherClient.getWeatherInfo();
             if (mWeatherInfo != null) {
+                String formattedCondition = mWeatherInfo.condition;
+                if (formattedCondition.toLowerCase().contains("clouds")) {
+                    formattedCondition = mContext.getResources().getString(R.string.weather_condition_clouds);
+                } else if (formattedCondition.toLowerCase().contains("rain")) {
+                    formattedCondition = mContext.getResources().getString(R.string.weather_condition_rain);
+                } else if (formattedCondition.toLowerCase().contains("clear")) {
+                    formattedCondition = mContext.getResources().getString(R.string.weather_condition_clear);
+                } else if (formattedCondition.toLowerCase().contains("storm")) {
+                    formattedCondition = mContext.getResources().getString(R.string.weather_condition_storm);
+                } else if (formattedCondition.toLowerCase().contains("snow")) {
+                    formattedCondition = mContext.getResources().getString(R.string.weather_condition_snow);
+                } else if (formattedCondition.toLowerCase().contains("wind")) {
+                    formattedCondition = mContext.getResources().getString(R.string.weather_condition_wind);
+                } else if (formattedCondition.toLowerCase().contains("mist")) {
+                    formattedCondition = mContext.getResources().getString(R.string.weather_condition_mist);
+                }
                 Drawable d = mWeatherClient.getWeatherConditionImage(mWeatherInfo.conditionCode);
                 mCurrentImage.setImageDrawable(d);
                 mRightText.setText(mWeatherInfo.temp + " " + mWeatherInfo.tempUnits);
                 mLeftText.setText(mWeatherInfo.city);
                 mLeftText.setVisibility(mShowWeatherLocation ? View.VISIBLE : View.GONE);
+                mWeatherText.setText(" · "  + formattedCondition);
+                mWeatherText.setVisibility(mShowWeatherText ? View.VISIBLE : View.GONE);
             }
         } catch(Exception e) {
             // Do nothing
@@ -138,21 +163,28 @@ public class CurrentWeatherView extends FrameLayout implements OmniJawsClient.Om
         }
 
         void observe() {
-            getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_WEATHER_LOCATION), false, this,
+                    UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_WEATHER_TEXT), false, this,
                     UserHandle.USER_ALL);
             updateWeatherSettings();
         }
 
         void unobserve() {
-            getContext().getContentResolver().unregisterContentObserver(this);
+            mContext.getContentResolver().unregisterContentObserver(this);
         }
 
         void updateWeatherSettings() {
-            mShowWeatherLocation = Settings.System.getIntForUser(getContext().getContentResolver(),
+            mShowWeatherLocation = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.LOCKSCREEN_WEATHER_LOCATION,
                     0, UserHandle.USER_CURRENT) != 0;
+            mShowWeatherText = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_WEATHER_TEXT,
+                    1, UserHandle.USER_CURRENT) != 0;
             mLeftText.setVisibility(mShowWeatherLocation ? View.VISIBLE : View.GONE);
+            mWeatherText.setVisibility(mShowWeatherText ? View.VISIBLE : View.GONE);
         }
 
         @Override
