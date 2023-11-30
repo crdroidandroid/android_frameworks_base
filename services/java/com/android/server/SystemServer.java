13f7ff60400793f2dd0841de7f4da2b388237cd2
SamarV-121 <samar@samarv121.dev>
2023-11-30 15:41:12 +0530

    PixelPropsUtils: Dynamically spoof props for GMS
    @neobuddy89: Adapted for PixelPropsUtils. Use local props as fallback.
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 8 Dec 2024 16:20:34 +0530
        Subject: AttestationService: Fixes and tune up
    
        * Increase interval from 5 minutes to 8 hours. For 10k+ active users, having
          small interval may load server with too many requests.
        * When internet is not available, defer the update instead of cancelling.
        * Fix SPOOF_PIXEL_PI being checked only boot. User may toggle it while using ROM.
          We should check it during scheduling instead.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Fri, 29 Aug 2025 20:17:02 +0530
        Subject: AttestationService: Make it atomic
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -163,6 +163,7 @@ import com.android.server.contextualsearch.ContextualSearchManagerService;
 import com.android.server.coverage.CoverageService;
 import com.android.server.cpu.CpuMonitorService;
 import com.android.server.crashrecovery.CrashRecoveryAdaptor;
+import com.android.server.crdroid.AttestationService;
 import com.android.server.credentials.CredentialManagerService;
 import com.android.server.criticalevents.CriticalEventLog;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
@@ -2757,6 +2758,11 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(BackgroundInstallControlService.class);
                 t.traceEnd();
             }
+
+            // AttestationService
+            t.traceBegin("AttestationService");
+            mSystemServiceManager.startService(AttestationService.class);
+            t.traceEnd();
         }
 
         t.traceBegin("StartMediaProjectionManager");
