ce9e7be77ff8e0093309cb30e1cd0589ac405089
Pranav Vashi <neobuddy89@gmail.com>
2023-12-10 08:35:49 +0530

    Allow setting quick QS rows [1/2]
    * Default to 2 for landscape since we use split shade by default
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -82,6 +82,10 @@ public class QSPanel extends LinearLayout implements Tunable {
             "system:" + Settings.System.QS_LAYOUT_COLUMNS;
     public static final String QS_LAYOUT_COLUMNS_LANDSCAPE =
             "system:" + Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE;
+    public static final String QQS_LAYOUT_ROWS =
+            "system:" + Settings.System.QQS_LAYOUT_ROWS;
+    public static final String QQS_LAYOUT_ROWS_LANDSCAPE =
+            "system:" + Settings.System.QQS_LAYOUT_ROWS_LANDSCAPE;
 
     private static final String TAG = "QSPanel";
 
@@ -408,6 +412,8 @@ public class QSPanel extends LinearLayout implements Tunable {
                 break;
             case QS_LAYOUT_COLUMNS:
             case QS_LAYOUT_COLUMNS_LANDSCAPE:
+            case QQS_LAYOUT_ROWS:
+            case QQS_LAYOUT_ROWS_LANDSCAPE:
                 needsDynamicRowsAndColumns();
                 break;
             default:
@@ -518,9 +524,8 @@ public class QSPanel extends LinearLayout implements Tunable {
 
     public void needsDynamicRowsAndColumns() {
         if (mTileLayout != null) {
-            int columns = mTileLayout.getResourceColumns();
-            mTileLayout.setMinRows(mUsingHorizontalLayout ? 2 : 1);
-            mTileLayout.setMaxColumns(columns > 3 && mUsingHorizontalLayout ? columns / 2 : columns);
+            mTileLayout.setMinRows(mTileLayout.getResourceRows());
+            mTileLayout.setMaxColumns(mTileLayout.getResourceColumns());
         }
     }
 
@@ -848,6 +853,8 @@ public class QSPanel extends LinearLayout implements Tunable {
         default void setLogger(QSLogger qsLogger) { }
 
         int getResourceColumns();
+
+        int getResourceRows();
 
         void updateSettings();
     }
