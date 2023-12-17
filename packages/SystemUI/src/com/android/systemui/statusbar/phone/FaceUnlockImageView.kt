895ed2520c54ce2134a39efa0cf8a9702c7e4d9e
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 08:36:37 +0800

    SystemUI: Implement bouncer face unlock animation
    mnri: move face indicator to top, rewrite code and fix inconsistencies
    
    niv: Update for U QPR3
    
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/FaceUnlockImageView.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/FaceUnlockImageView.kt
@@ -15,88 +15,213 @@
  */
 package com.android.systemui.statusbar.phone
 
-import android.animation.ObjectAnimator
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.AsyncTask
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.AttributeSet
+import android.view.View
 import android.widget.ImageView
-import android.view.animation.LinearInterpolator
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.view.animation.OvershootInterpolator
 
-import com.android.systemui.R
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnCancel
+
+import com.android.systemui.Dependency
+import com.android.systemui.plugins.statusbar.StatusBarStateController
+
+import com.android.systemui.res.R
 
 class FaceUnlockImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ImageView(context, attrs, defStyleAttr) {
 
     enum class State {
-        SCANNING, NOT_VERIFIED, SUCCESS
+        SCANNING, NOT_VERIFIED, SUCCESS, HIDDEN
     }
 
-    private var currentState: State = State.SCANNING
+    private val DELAY_HIDE_DURATION = 1500
+    private var currentState: State = State.HIDDEN
+    private var colorState: ColorStateList? = null
+    private val startAnimation: ObjectAnimator = createScaleAnimation(start = true)
+    private val dismissAnimation: ObjectAnimator = createScaleAnimation(start = false)
     private val scanningAnimation: ObjectAnimator = createScanningAnimation()
-    private val successShakeAnimation: AnimatorSet = createShakeAnimation(5f)
-    private val failureShakeAnimation: AnimatorSet = createShakeAnimation(10f)
+    private val successAnimation: ObjectAnimator = createSuccessRotationAnimation()
+    private val failureShakeAnimation: ObjectAnimator = createShakeAnimation(10f)
+    private val vibrator: Vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    private val animations = listOf(failureShakeAnimation, dismissAnimation, scanningAnimation, successAnimation, startAnimation)
+    private var mDozing = false
+    private val statusBarStateController: StatusBarStateController = Dependency.get(StatusBarStateController::class.java)
+
+    companion object {
+        private var instance: FaceUnlockImageView? = null
+
+        @JvmStatic
+        fun setBouncerState(state: State) {
+            instance?.postDelayed({
+                instance?.setState(state)
+            }, 100)
+        }
+
+        @JvmStatic
+        fun setInstance(instance: FaceUnlockImageView) {
+            this.instance = instance
+        }
+
+        @JvmStatic
+        fun getInstance(): FaceUnlockImageView? {
+            return instance
+        }
+    }
+
+    private val statusBarStateListener = object : StatusBarStateController.StateListener {
+        override fun onStateChanged(newState: Int) {}
+
+        override fun onDozingChanged(dozing: Boolean) {
+            if (mDozing == dozing) {
+                return
+            }
+            mDozing = dozing
+            if (mDozing) {
+                visibility = View.GONE
+            }
+        }
+    }
 
     init {
-        updateDrawable(animate = false)
+        statusBarStateController.addCallback(statusBarStateListener)
+        statusBarStateListener.onDozingChanged(statusBarStateController.isDozing())
+        visibility = View.GONE
+        updateFaceIconState()
+    }
+
+    public override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        setInstance(this)
+        updateColor()
+    }
+
+    fun updateColor() {
+        imageTintList = ColorStateList.valueOf(Color.WHITE)
+        backgroundTintList = ColorStateList.valueOf(Color.parseColor("#99000000"))
     }
 
     fun setState(state: State) {
         if (currentState != state) {
             currentState = state
-            updateDrawable(animate = true)
+            updateFaceIconState()
             handleAnimationForState(state)
         }
     }
 
-    private fun updateDrawable(animate: Boolean) {
+    fun updateFaceIconState() {
+        if (mDozing) {
+            visibility = View.GONE
+            return
+        }
         setImageResource(when (currentState) {
             State.SCANNING -> R.drawable.face_scanning
             State.NOT_VERIFIED -> R.drawable.face_not_verified
             State.SUCCESS -> R.drawable.face_success
+            State.HIDDEN -> R.drawable.face_scanning
         })
     }
 
     private fun createScanningAnimation(): ObjectAnimator {
-        val scaleX = PropertyValuesHolder.ofFloat("scaleX", 1f, 1.2f, 1f)
-        val scaleY = PropertyValuesHolder.ofFloat("scaleY", 1f, 1.2f, 1f)
-        val scanningAnimator = ObjectAnimator.ofPropertyValuesHolder(this, scaleX, scaleY)
-        scanningAnimator.duration = 1000
-        scanningAnimator.repeatCount = ObjectAnimator.INFINITE
-        scanningAnimator.interpolator = LinearInterpolator()
-        return scanningAnimator
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, 1.2f, 1f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, 1.2f, 1f)
+        return ObjectAnimator.ofPropertyValuesHolder(this, scaleX, scaleY).apply {
+            duration = 1000
+            repeatCount = ObjectAnimator.INFINITE
+            interpolator = LinearInterpolator()
+        }
     }
 
-    private fun createShakeAnimation(amplitude: Float): AnimatorSet {
-        val animatorSet = AnimatorSet()
-        val translationX = PropertyValuesHolder.ofFloat("translationX", 0f, amplitude, -amplitude, amplitude, -amplitude, 0f)
-        val shakeAnimator = ObjectAnimator.ofPropertyValuesHolder(this, translationX)
-        shakeAnimator.duration = 500
-        shakeAnimator.interpolator = AccelerateDecelerateInterpolator()
-        animatorSet.play(shakeAnimator)
-        return animatorSet
+    private fun createSuccessRotationAnimation(): ObjectAnimator {
+        return ObjectAnimator.ofFloat(this, View.ROTATION_Y, 0f, 360f).apply {
+            duration = 800
+            interpolator = AccelerateDecelerateInterpolator()
+        }
+    }
+
+    private fun createShakeAnimation(amplitude: Float): ObjectAnimator {
+        return ObjectAnimator.ofFloat(this, View.TRANSLATION_X, 0f, amplitude, -amplitude, amplitude, -amplitude, 0f).apply {
+            duration = 500
+            interpolator = AccelerateDecelerateInterpolator()
+        }
+    }
+
+    private fun createScaleAnimation(start: Boolean): ObjectAnimator {
+        val startScale = if (start) 0f else 1f
+        val endScale = if (start) 1f else 0f
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, startScale, endScale)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, startScale, endScale)
+        return ObjectAnimator.ofPropertyValuesHolder(this, scaleX, scaleY).apply {
+            duration = 500
+            interpolator = AccelerateDecelerateInterpolator()
+            if (!start) {
+                doOnEnd { visibility = View.GONE }
+            }
+        }
+    }
+
+    private fun vibrate(effect: Int) {
+        if (mDozing) {
+            return
+        }
+        post {
+            val vibrationEffect = VibrationEffect.createPredefined(effect)
+            vibrator.vibrate(vibrationEffect)
+        }
+    }
+
+    private fun cancelAllAnimations() {
+        animations.forEach { it.cancel() }
     }
 
     private fun handleAnimationForState(state: State) {
+        if (mDozing) {
+            visibility = View.GONE
+            return
+        }
+        cancelAllAnimations()
+        if (state == State.SCANNING) {
+            visibility = View.VISIBLE
+        }
         when (state) {
-            State.SCANNING -> {
-                failureShakeAnimation.cancel()
-                successShakeAnimation.cancel()
-                scanningAnimation.start()
-            }
+            State.SCANNING -> startAnimationChain(startAnimation, scanningAnimation)
             State.NOT_VERIFIED -> {
-                scanningAnimation.cancel()
-                successShakeAnimation.cancel()
-                failureShakeAnimation.start()
+                startAnimationChain(failureShakeAnimation, dismissAnimation)
+                vibrate(VibrationEffect.EFFECT_DOUBLE_CLICK)
             }
             State.SUCCESS -> {
-                scanningAnimation.cancel()
-                failureShakeAnimation.cancel()
-                successShakeAnimation.start()
+                startAnimationChain(successAnimation, dismissAnimation)
+                vibrate(VibrationEffect.EFFECT_CLICK)
             }
+            State.HIDDEN -> dismissAnimation.start()
         }
+    }
+
+    private fun startAnimationChain(first: ObjectAnimator, next: ObjectAnimator) {
+        first.startWithEndAction { next.start() }
+    }
+
+    private fun ObjectAnimator.startWithEndAction(endAction: () -> Unit) {
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                endAction()
+            }
+        })
+        start()
     }
 }
