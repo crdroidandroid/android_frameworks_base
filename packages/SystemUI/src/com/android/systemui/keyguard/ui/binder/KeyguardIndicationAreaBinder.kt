7e0180edcafb856845309daabfbee62110ab09b0
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 08:36:37 +0800

    SystemUI: Implement bouncer face unlock animation
    mnri: move face indicator to top, rewrite code and fix inconsistencies
    
    niv: Update for U QPR3
    
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardIndicationAreaBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardIndicationAreaBinder.kt
@@ -54,6 +54,7 @@ object KeyguardIndicationAreaBinder {
         indicationController: KeyguardIndicationController,
     ): DisposableHandle {
         indicationController.setIndicationArea(view)
+        indicationController.setIndicationAreaTop(view)
 
         val indicationText: TextView = view.requireViewById(R.id.keyguard_indication_text)
         val indicationTextBottom: TextView =
