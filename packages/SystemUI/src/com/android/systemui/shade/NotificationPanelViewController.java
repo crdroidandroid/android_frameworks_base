6d4aa9f079e249bf1a029285c1c5e06aa58bc35f
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 20:39:23 +0900

    Integrate Pixel framework hooks and wrappers
    [someone5678]
    * Add collapseShade, animateCollapseShade, and animateExpandNotificationsPanel
      for Quick Tap gesture
    
    Change-Id: I6f4203391d544205ea77107303fe2a5b1468e3c7
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -390,8 +390,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private boolean mTracking;
     private boolean mIsTrackingExpansionFromStatusBar;
     private boolean mHintAnimationRunning;
+    // Need public mKeyguardBottomArea for use by elmyra
     @Deprecated
-    private KeyguardBottomAreaView mKeyguardBottomArea;
+    public KeyguardBottomAreaView mKeyguardBottomArea;
     private boolean mExpanding;
     private boolean mSplitShadeEnabled;
     /** The bottom padding reserved for elements of the keyguard measuring notifications. */
@@ -1554,6 +1555,10 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(-1f);
         }
+    }
+
+    public void updateMaxDisplayedNotificationsWrapper(boolean recompute) {
+        updateMaxDisplayedNotifications(recompute);
     }
 
     private boolean shouldAvoidChangingNotificationsCount() {
@@ -3237,6 +3242,14 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mNotificationStackScrollLayoutController.setPulsing(pulsing, animatePulse);
 
         updateKeyguardStatusViewAlignment(/* animate= */ true);
+    }
+
+    public void setAmbientIndicationBottomPadding(int bottomPadding) {
+        mAmbientIndicationBottomPadding = bottomPadding;
+    }
+
+    public int getAmbientIndicationBottomPadding() {
+        return mAmbientIndicationBottomPadding;
     }
 
     @Override
@@ -5449,5 +5462,15 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (mUseIslandNotification && mUseHeadsUp) {
             mNotifIsland.updateIslandVisibility(getExpandedFraction());
         }
+    }
+
+    @Override
+    public NotificationStackScrollLayoutController getScrollerLayoutController() {
+        return mNotificationStackScrollLayoutController;
+    }
+
+    @Override
+    public KeyguardBottomAreaView getKeyguardBottomAreaView() {
+        return mKeyguardBottomArea;
     }
 }
--- packages/SystemUI/src/com/android/systemui/shade/ShadeViewController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeViewController.kt
@@ -20,7 +20,9 @@ import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import com.android.systemui.keyguard.shared.model.WakefulnessModel
 import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow
+import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayoutController
 import com.android.systemui.statusbar.phone.HeadsUpAppearanceController
+import com.android.systemui.statusbar.phone.KeyguardBottomAreaView
 import com.android.systemui.statusbar.phone.KeyguardStatusBarView
 import com.android.systemui.statusbar.phone.KeyguardStatusBarViewController
 import java.util.function.Consumer
@@ -231,6 +233,10 @@ interface ShadeViewController {
 
     /** island visibility  */
     fun showIsland(show: Boolean)
+    
+    fun getScrollerLayoutController(): NotificationStackScrollLayoutController?
+    
+    fun getKeyguardBottomAreaView(): KeyguardBottomAreaView?
 
     /**
      * This method should not be used anymore, you should probably use [.isShadeFullyOpen] instead.
