52963db31a19340a9d3ad7370b01e6206d614007
Pranav Vashi <neobuddy89@gmail.com>
2023-12-25 00:24:45 +0530

    SystemUI: Ensure updating margins and paddings on brightness slider
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -718,8 +718,8 @@ public class QSPanel extends LinearLayout implements Tunable {
             mUsingHorizontalLayout = horizontal;
             ViewGroup newParent = horizontal ? mHorizontalContentContainer : this;
             switchAllContentToParent(newParent, mTileLayout);
+            updateResources();
             if (mBrightnessRunnable != null) {
-                updateResources();
                 mBrightnessRunnable.run();
             }
             reAttachMediaHost(mediaHostView, horizontal);
@@ -740,8 +740,8 @@ public class QSPanel extends LinearLayout implements Tunable {
         if (mBrightnessView == null) return;
         ViewGroup newParent = mUsingHorizontalLayout ? mHorizontalContentContainer : this;
         switchAllContentToParent(newParent, mTileLayout);
+        updateResources();
         if (mBrightnessRunnable != null) {
-            updateResources();
             mBrightnessRunnable.run();
         }
     }
