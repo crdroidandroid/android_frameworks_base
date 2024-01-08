587ca2883c1126926b1a15044d13c7d7a819f1b9
Ashok Mutyala <quic_amutyala@quicinc.com>
2024-01-08 06:33:03 +0000

    Fix User Switch Timeout Issue
    While Switching between Users, UserTracker Callbacks are taking more time which is more than 5 secs (set by AOSP) due to some usertracker callbacks are getting executed on Main Thread which is not guaranteed to execute on time. As we have a synchronization mechanism with CountDownLatch, we can safely execute on Bg Executors
    
    Bug: 319352292
    Test: Boot and Check User Switch timeout
    Google: 2898333
    Change-Id: I0d83bb51b63d49e0a69a143c834572c4192c9f24
    Merged-In: I0d83bb51b63d49e0a69a143c834572c4192c9f24

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -26,6 +26,7 @@ import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.HandlerExecutor;
+import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.UserHandle;
 import android.os.UserManager;
@@ -103,6 +104,7 @@ public class TunerServiceImpl extends TunerService {
     private UserTracker.Callback mCurrentUserTracker;
     private UserTracker mUserTracker;
     private final ComponentName mTunerComponent;
+    private HandlerThread mHandlerThread;
 
     /**
      */
@@ -120,7 +122,8 @@ public class TunerServiceImpl extends TunerService {
         mDemoModeController = demoModeController;
         mUserTracker = userTracker;
         mTunerComponent = new ComponentName(mContext, TunerActivity.class);
-
+        mHandlerThread = new HandlerThread("TunerServiceImpl");
+        mHandlerThread.start();
         for (UserInfo user : UserManager.get(mContext).getUsers()) {
             mCurrentUser = user.getUserHandle().getIdentifier();
             if (getValue(TUNER_VERSION, 0) != CURRENT_TUNER_VERSION) {
@@ -138,7 +141,7 @@ public class TunerServiceImpl extends TunerService {
             }
         };
         mUserTracker.addCallback(mCurrentUserTracker,
-                new HandlerExecutor(mainHandler));
+                new HandlerExecutor(mHandlerThread.getThreadHandler()));
     }
 
     @Override
