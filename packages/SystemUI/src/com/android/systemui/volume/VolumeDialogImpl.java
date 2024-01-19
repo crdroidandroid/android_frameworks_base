bf77474c8d15dd3279c30dc2bb1746840f6993b8
Ido Ben-Hur <idoybh2@gmail.com>
2024-01-19 05:41:48 +0200

    base: Allow customizing volume stream steps [1/2]
    only media, calls and alarms seem fit
    the rest are pretty much set and forget and don't need vol button granularity
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -2629,7 +2629,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         final int vlevel = row.ss.muted && (!isRingStream && !zenMuted) ? 0
                 : row.ss.level;
         Trace.beginSection("VolumeDialogImpl#updateVolumeRowSliderH");
-        updateVolumeRowSliderH(row, enableSlider, vlevel);
+        updateVolumeRowSliderH(row, enableSlider, vlevel, max != row.slider.getMax());
         Trace.endSection();
         if (row.number != null) row.number.setText(Integer.toString(vlevel));
     }
@@ -2703,6 +2703,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     }
 
     private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel) {
+        updateVolumeRowSliderH(row, enable, vlevel, false);
+    }
+
+    private void updateVolumeRowSliderH(VolumeRow row, boolean enable, int vlevel, boolean force) {
         row.slider.setEnabled(enable);
         updateVolumeRowTintH(row, row.stream == mActiveStream);
         if (row.tracking) {
@@ -2726,7 +2730,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             }
         }
         final int newProgress = getProgressFromVolume(row.ss, row.slider, vlevel);
-        if (progress != newProgress) {
+        if (progress != newProgress || force) {
             if (mIsTv) {
                 // don't animate slider on TVs
                 row.slider.setProgress(newProgress, false);
