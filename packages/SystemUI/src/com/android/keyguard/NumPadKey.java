ba77c7142186a8b78318d2d574be81dc9d736834
Pranav Vashi <neobuddy89@gmail.com>
2024-01-21 15:18:55 +0530

    Revert "Keyguard: Add option to scramble pin layout when unlocking (2/2)."
    This reverts commit 78e834d58f8958c24ba37f0e7a56a47cf7cf2b37.

--- packages/SystemUI/src/com/android/keyguard/NumPadKey.java
+++ packages/SystemUI/src/com/android/keyguard/NumPadKey.java
@@ -38,8 +38,6 @@ import androidx.annotation.Nullable;
 import com.android.settingslib.Utils;
 import com.android.systemui.R;
 
-import lineageos.providers.LineageSettings;
-
 /**
  * Viewgroup for the bouncer numpad button, specifically for digits.
  */
@@ -119,7 +117,21 @@ public class NumPadKey extends ViewGroup implements NumPadAnimationListener {
         mDigitText.setText(Integer.toString(mDigit));
         mKlondikeText = (TextView) findViewById(R.id.klondike_text);
 
-        updateText();
+        if (mDigit >= 0) {
+            if (sKlondike == null) {
+                sKlondike = getResources().getStringArray(R.array.lockscreen_num_pad_klondike);
+            }
+            if (sKlondike != null && sKlondike.length > mDigit) {
+                String klondike = sKlondike[mDigit];
+                final int len = klondike.length();
+                if (len > 0) {
+                    mKlondikeText.setText(klondike);
+                } else if (mKlondikeText.getVisibility() != View.GONE) {
+                    mKlondikeText.setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+
         setContentDescription(mDigitText.getText().toString());
 
         Drawable background = getBackground();
@@ -128,31 +140,6 @@ public class NumPadKey extends ViewGroup implements NumPadAnimationListener {
                     R.style.NumPadKey, mDigitText, null);
         } else {
             mAnimator = null;
-        }
-    }
-
-    public void setDigit(int digit) {
-        mDigit = digit;
-        updateText();
-    }
-
-    private void updateText() {
-        boolean scramblePin = (LineageSettings.System.getInt(getContext().getContentResolver(),
-                LineageSettings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0) == 1);
-        if (mDigit >= 0) {
-            mDigitText.setText(Integer.toString(mDigit));
-            if (sKlondike == null) {
-                sKlondike = getResources().getStringArray(R.array.lockscreen_num_pad_klondike);
-            }
-            if (sKlondike != null && sKlondike.length > mDigit) {
-                String klondike = sKlondike[mDigit];
-                final int len = klondike.length();
-                if (len > 0 || scramblePin) {
-                    mKlondikeText.setText(klondike);
-                } else if (mKlondikeText.getVisibility() != View.GONE) {
-                    mKlondikeText.setVisibility(View.INVISIBLE);
-                }
-            }
         }
     }
 
