8198999f9f83ef26b8847325d66981e4f5f6c887
Wen Zhang <zhangwen8@xiaomi.corp-partner.google.com>
2024-01-29 11:57:30 +0000

    Avoid device reboot caused by SecurityException.
    The user manually installed an old version of the GMS core app on a device without the GMS framework. When unlocking the device, the lack of INTERACT_ACROSS_USERS permission caused the system server to crash.
    
    Bug:322749757
    
    Change-Id: If68206b4ebe223d94b91dbbcc3246a2ed41563aa
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ContentProviderHelper.java
+++ services/core/java/com/android/server/am/ContentProviderHelper.java
@@ -1399,7 +1399,7 @@ public class ContentProviderHelper {
                                     }
                                 }
                             }
-                        } catch (RemoteException ignored) {
+                        } catch (RemoteException|SecurityException ignored) {
                         }
                     });
                 }
