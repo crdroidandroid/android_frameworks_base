25c28f95a177c5b73a5be4ede864246fd3b477d1
Dhina17 <dhinalogu@gmail.com>
2024-01-30 05:30:14 +0000

    SystemUI: Make sure the udfps animation is hidden on the bouncer
    When pressing the udfps icon repetively for some time, it will enter primary bouncer.
    While launching bouncer, Somehow touch event ACTION_DOWN passes the checks in newOnTouch()
    and invokes onFingerDown(). So it shows udfps animation.
    on next event, checks in newOnTouch() stop it and not able to reach onFingerUp().
    So unable to hide udfps animation until you exit bouncer and tap the screen once.
    
    To avoid this case, hide the udfps animation in all checks of newOnTouch().
    
    [@neobuddy89: Add showUdfpsAnimation() similarly.]
    
    Change-Id: I629a1b4e0a1c45814cc8470315c29a44cb46a0c9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -337,18 +337,14 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         for (Callback cb : mCallbacks) {
                             cb.onFingerDown();
                         }
-                        if (mUdfpsAnimation != null) {
-                            mUdfpsAnimation.show();
-                        }
+                        showUdfpsAnimation();
                     });
                 } else {
                     mFgExecutor.execute(() -> {
                         for (Callback cb : mCallbacks) {
                             cb.onFingerUp();
                         }
-                        if (mUdfpsAnimation != null) {
-                            mUdfpsAnimation.hide();
-                        }
+                        hideUdfpsAnimation();
                     });
                 }
             }
@@ -569,11 +565,13 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean onTouch(long requestId, @NonNull MotionEvent event) {
         if (mOverlay == null) {
             Log.w(TAG, "ignoring onTouch with null overlay");
+            hideUdfpsAnimation();
             return false;
         }
         if (!mOverlay.matchesRequestId(requestId)) {
             Log.w(TAG, "ignoring stale touch event: " + requestId + " current: "
                     + mOverlay.getRequestId());
+            hideUdfpsAnimation();
             return false;
         }
         if (event.getAction() == MotionEvent.ACTION_DOWN
@@ -596,6 +594,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 mOverlayParams);
         if (result instanceof TouchProcessorResult.Failure) {
             Log.w(TAG, ((TouchProcessorResult.Failure) result).getReason());
+            hideUdfpsAnimation();
             return false;
         }
 
@@ -1143,9 +1142,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             for (Callback cb : mCallbacks) {
                 cb.onFingerDown();
             }
-            if (mUdfpsAnimation != null) {
-                mUdfpsAnimation.show();
-            }
+            showUdfpsAnimation();
         }
     }
 
@@ -1186,9 +1183,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                 for (Callback cb : mCallbacks) {
                     cb.onFingerUp();
                 }
-                if (mUdfpsAnimation != null) {
-                    mUdfpsAnimation.hide();
-                }
+                hideUdfpsAnimation();
             }
         }
         mOnFingerDown = false;
@@ -1198,6 +1193,18 @@ public class UdfpsController implements DozeReceiver, Dumpable {
 
     public boolean isAnimationEnabled() {
         return mUdfpsAnimation != null && mUdfpsAnimation.isAnimationEnabled();
+    }
+
+    private void showUdfpsAnimation() {
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.show();
+        }
+    }
+
+    private void hideUdfpsAnimation() {
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
+        }
     }
 
     /**
