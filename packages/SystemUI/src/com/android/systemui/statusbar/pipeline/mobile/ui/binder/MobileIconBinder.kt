18b3439c9402d8ee764ce57cf6d7fa890044bad7
Dhina17 <dhinalogu@gmail.com>
2024-01-31 16:47:49 +0000

    SystemUI: Forward port 'HD & wifi calling statusbar icons'
    Squashed:
    * SystemUI: Fix HD calling icon for RTL layout
    * SystemUI: sb: Fix VoWifi icon size
    * SystemUI: sb: Hide wifi airplane spacer when vowifi is available
    * SignalDrawable: Add support for Cut + Roaming back
    
    Adithya R <gh0strider.2k18.reborn@gmail.com>:
        HD icon shows for VoLTE/VoNR, and wifi calling for VoWiFi.
        Based on CLO's volte/vowifi icon implementation, reworked manually.
        Drawables taken from Nothing OS and manually tweaked.
    
    Dhina17 <dhinalogu@gmail.com>:
        Rewrite it for new status bar impl in Android 14.
        Move VoWifi icon to Wifi space.
        New VoWifi icon with SIM identification.
    
    neobuddy89: Updated for A16 QPR1
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Weijie Wang <quic_weijiew@quicinc.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: nift4 <nift4@protonmail.com>
    Original-Change-Id: I0190394690e9b38e0575ffb099be8a5ed2ae9d90
    Change-Id: Ib48b1295d060acf462b2f91e81cbb570fd8df42e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/binder/MobileIconBinder.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/binder/MobileIconBinder.kt
@@ -70,8 +70,8 @@ object MobileIconBinder {
         val networkTypeContainer = view.requireViewById<FrameLayout>(R.id.mobile_type_container)
         val iconView = view.requireViewById<ImageView>(R.id.mobile_signal)
         val mobileDrawable = SignalDrawable(view.context)
-        val roamingView = view.requireViewById<ImageView>(R.id.mobile_roaming)
-        val roamingSpace = view.requireViewById<Space>(R.id.mobile_roaming_space)
+        val mobileHdView = view.requireViewById<ImageView>(R.id.mobile_hd)
+        val mobileHdSpace = view.requireViewById<Space>(R.id.mobile_hd_space)
         val endSideRoamingView = view.requireViewById<ImageView>(R.id.mobile_roaming_updated)
         val dotView = view.requireViewById<StatusBarIconView>(R.id.status_bar_dot)
 
@@ -210,14 +210,19 @@ object MobileIconBinder {
                         }
                     }
 
+                    // Set the mobile HD indicator (VoLTE/VoNR)
+                    launch {
+                        viewModel.showHd.distinctUntilChanged().collect { isHd ->
+                            mobileHdView.isVisible = isHd
+                            mobileHdSpace.isVisible = isHd
+                        }
+                    }
+
                     // Set the roaming indicator
                     launch {
                         viewModel.isRoamingVisible.distinctUntilChanged().collect { isRoaming ->
                             if (NewStatusBarIcons.isEnabled) {
                                 endSideRoamingView.isVisible = isRoaming
-                            } else {
-                                roamingView.isVisible = isRoaming
-                                roamingSpace.isVisible = isRoaming
                             }
                         }
                     }
@@ -268,7 +273,7 @@ object MobileIconBinder {
                                 networkTypeView.imageTintList = tint
                             }
 
-                            roamingView.imageTintList = tint
+                            mobileHdView.imageTintList = tint
                             endSideRoamingView.imageTintList = tint
                             activityIn.imageTintList = tint
                             activityOut.imageTintList = tint
