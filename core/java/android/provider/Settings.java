e154ac90f3757206579eaac71dd70b2c58ce469d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-02 12:33:16 +0900

    SystemUI: IslandView: Implement now playing for Island notification
    neobuddy89: Added few clean ups.
    
    Change-Id: Ic56f44fded8ee3f521738da36968e8aecfde7a95
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6758,6 +6758,11 @@ public final class Settings {
         public static final String ISLAND_NOTIFICATION = "island_notification";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
