5bc3b51d3325586b34ae53f0c913cdba90cc0907
someone5678 <someone5678@users.noreply.github.com>
2024-02-17 09:29:24 +0900

    SystemUI: Enable more flags
    Change-Id: If054f270d80d0302310f467a5fa9a2d89a00fec9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -511,7 +511,7 @@ object Flags {
     // TODO(b/279405451): Tracking Bug
     @JvmField
     val CLIPBOARD_SHARED_TRANSITIONS =
-            unreleasedFlag("clipboard_shared_transitions", teamfood = true)
+            releasedFlag("clipboard_shared_transitions")
 
     /**
      * Whether the scene container (Flexiglass) is enabled. Note that SceneContainerFlags#isEnabled
@@ -601,7 +601,7 @@ object Flags {
     val LOCKSCREEN_WALLPAPER_DREAM_ENABLED = unreleasedFlag("enable_lockscreen_wallpaper_dream")
 
     // TODO(b/283084712): Tracking Bug
-    @JvmField val IMPROVED_HUN_ANIMATIONS = unreleasedFlag("improved_hun_animations")
+    @JvmField val IMPROVED_HUN_ANIMATIONS = releasedFlag("improved_hun_animations")
 
     // TODO(b/283447257): Tracking bug
     @JvmField
