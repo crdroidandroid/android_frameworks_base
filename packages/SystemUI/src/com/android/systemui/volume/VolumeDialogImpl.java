5cc773bf71711b1200265eb4233f79ece1c8225b
Alvin Francis <nivlafx@gmail.com>
2024-02-23 05:17:12 -0400

    VolumeDialog: Fix systemui crash nullpointerexception while casting
     * Was causing crashes when casting to devices, see below log
    
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: Process: com.android.systemui, PID: 11690
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'android.view.LayoutInflater android.app.Dialog.getLayoutInflater()' on a null object reference
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at com.android.systemui.volume.VolumeDialogImpl.initRow(VolumeDialogImpl.java:1026)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at com.android.systemui.volume.VolumeDialogImpl.addRow(VolumeDialogImpl.java:957)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at com.android.systemui.volume.VolumeDialogImpl.onStateChangedH(VolumeDialogImpl.java:2372)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at com.android.systemui.volume.VolumeDialogImpl$10.onStateChanged(VolumeDialogImpl.java:2865)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at com.android.systemui.volume.VolumeDialogControllerImpl$C$3.run(VolumeDialogControllerImpl.java:965)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:958)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:99)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:205)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:294)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8295)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:554)
    02-22 22:30:12.834 11690 11690 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:878)
    
    test: install update, cast from youtube to tv, systemui no longer crashes
    
    Change-Id: I455adca9119dc2360c29ad1241e299de051a0875
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -512,6 +512,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     }
 
     public void init(int windowType, Callback callback) {
+        if (mDialog != null) {
+            mDialog.dismiss();
+        }
         initDialog(mActivityManager.getLockTaskModeState());
 
         mController.addCallback(mControllerCallbackH, mHandler);
@@ -644,7 +647,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         Log.d(TAG, "initDialog: called!");
         if (mDialog != null) {
             mDialog.dismiss();
-            mDialog = null;
         }
         if (mConfigurableTexts != null) {
             mConfigurableTexts = null;
