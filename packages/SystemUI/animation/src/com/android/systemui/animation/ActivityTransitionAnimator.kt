6a7df307ea7bfcd614da46af47b69af625b5314c
Tim Zimmermann <tim@linux4.de>
2024-02-24 05:54:52 +0100

    SystemUI: Don't animate QS tile text when opening activity/dialog from A11QS ui
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 6 Oct 2024 01:37:57 +0530
        Subject: fixup! SystemUI: Don't animate QS tile text when
         opening activity/dialog from A11QS ui
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I6a1cb0ae2c71904ad99be1c140c2bc36ea859a29
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/animation/src/com/android/systemui/animation/ActivityTransitionAnimator.kt
+++ packages/SystemUI/animation/src/com/android/systemui/animation/ActivityTransitionAnimator.kt
@@ -391,7 +391,12 @@ class ActivityTransitionAnimator(
                     )
                 }
 
-                if (view.parent !is ViewGroup) {
+                var animatedView = view
+                if (view.getAnimatedView() is View) {
+                    animatedView = view.getAnimatedView() as View
+                }
+
+                if (animatedView.parent !is ViewGroup) {
                     Log.e(
                         TAG,
                         "Skipping animation as view $view is not attached to a ViewGroup",
@@ -400,7 +405,7 @@ class ActivityTransitionAnimator(
                     return null
                 }
 
-                return GhostedViewTransitionAnimatorController(view, cujType)
+                return GhostedViewTransitionAnimatorController(animatedView, cujType)
             }
         }
 
