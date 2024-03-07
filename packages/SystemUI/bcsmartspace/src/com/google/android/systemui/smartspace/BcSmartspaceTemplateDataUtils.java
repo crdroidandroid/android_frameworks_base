8eda49b86a479d7eea7e5d41d93741ec7b2c9a1f
cjh1249131356 <cjh1249131356@gmail.com>
2024-03-08 00:41:48 +0800

    Smartspace: Return if text is null
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/bcsmartspace/src/com/google/android/systemui/smartspace/BcSmartspaceTemplateDataUtils.java
+++ packages/SystemUI/bcsmartspace/src/com/google/android/systemui/smartspace/BcSmartspaceTemplateDataUtils.java
@@ -80,6 +80,7 @@ public final class BcSmartspaceTemplateDataUtils {
         if (SmartspaceUtils.isEmpty(text)) {
             Log.w("BcSmartspaceTemplateDataUtils", "Cannot set. The given text is empty");
             updateVisibility(textView, 8);
+            return;
         }
         textView.setText(text.getText());
         textView.setEllipsize(text.getTruncateAtType());
