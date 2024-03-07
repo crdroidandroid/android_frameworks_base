df3f0098f44326960e6b565260cec251e867889c
zhengchuanlong <zhengchuanlong@xiaomi.com>
2024-03-07 20:43:54 +0800

    Fix SCREEN_BRIGHTNESS inconsistency issue before and after reboot.
    When we turn off adaptive brightness mode and adjust the brightness bar
    to the lowest, then first turn on adaptive brightness mode and then turn
    off adaptive brightness mode on the settings page.Note that to turn on
    and off adaptive brightness mode, just click the adaptive brightness
    switch and do not click on other areas of the screen. Execute `adb
    shell settings get system screen_brightness` to get the system
    brightness before reboot, and then execute `adb reboot` to reboot the phone. After the reboot is complete, execute `adb shell settings get system screen_brightness` again to get the system brightness after reboot. It is found that the brightness after reboot is inconsistent with the brightness before reboot. That is, the brightness of automatic brightness adjustment is not persisted to the brightness database.
    Therefore, we need to determine whether it needs to be persisted to the
    brightness database when setting the brightness instead of waiting
    for other click events.
    
    Bug: 328547549
    
    Test: Manually operations:
    1. Turn off adaptive brightness mode and adjust the brightness bar to
       the lowest;
    2. Turn on adaptive brightness mode and turn off screen brightness (do
       not touch other areas of the screen;
    3. Run `adb shell settings get system screen_brightness`
    4. Run `adb reboot`
    5. Run `adb shell settings get system screen_brightness`.
    
    Change-Id: I78605bc2262b1a284de8f62d435bf5c6cf171ff8
    Signed-off-by: zhengchuanlong <zhengchuanlong@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/display/PersistentDataStore.java
+++ services/core/java/com/android/server/display/PersistentDataStore.java
@@ -317,6 +317,7 @@ final class PersistentDataStore {
         final DisplayState state = getDisplayState(displayDeviceUniqueId, true);
         if (state.setBrightness(brightness, userSerial)) {
             setDirty();
+            saveIfNeeded();
             return true;
         }
         return false;
