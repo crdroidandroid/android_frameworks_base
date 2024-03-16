ed32b2e7188de36c944ab2fa51da926af78f845e
Pranav Vashi <neobuddy89@gmail.com>
2024-03-16 20:01:22 +0530

    Allow signature spoofing on user builds
    * This is custom rom breaking google's integrity check.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -1493,13 +1493,7 @@ public class ComputerEngine implements Computer {
         return result;
     }
 
-    private static native boolean isDebuggable();
-
     public static boolean isMicrogSigned(AndroidPackage p) {
-        if (!isDebuggable()) {
-            return false;
-        }
-
         // Allowlist the following apps:
         // * com.android.vending - microG Companion
         // * com.google.android.gms - microG Services
