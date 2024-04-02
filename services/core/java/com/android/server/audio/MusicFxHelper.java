36da269b7d8de49c519f5df66c1b9f562ee12589
ot_chenqi.c <ot_chenqi.c@mediatek.com>
2024-04-02 10:46:23 +0800

    Fix unbind service error for MusicFxHelper
    Unbinding an unbound service will cause an exception,
    for example: ervice not registered: com.android.server.audio.MusicFxHelper.
    
    Bug: 332454067
    Test: run MTBF test and no crash
    
    Change-Id: Ic519221b2bb7d28e5378a995519f8b3269d03c70
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/audio/MusicFxHelper.java
+++ services/core/java/com/android/server/audio/MusicFxHelper.java
@@ -70,6 +70,8 @@ public class MusicFxHelper {
     // The binder token identifying the UidObserver registration.
     private IBinder mUidObserverToken = null;
 
+    private boolean mIsBinded;
+
     // Package name and list of open audio sessions for this package
     private static class PackageSessions {
         String mPackageName;
@@ -110,7 +112,7 @@ public class MusicFxHelper {
                 if (procState > ActivityManager.PROCESS_STATE_IMPORTANT_FOREGROUND) {
                     Intent bindIntent = new Intent().setClassName(mMusicFxPackageName,
                             "com.android.musicfx.KeepAliveService");
-                    mContext.bindServiceAsUser(
+                    mIsBinded = mContext.bindServiceAsUser(
                             bindIntent, mMusicFxBindConnection, Context.BIND_AUTO_CREATE,
                             UserHandle.of(getCurrentUserId()));
                     Log.i(TAG, "bindService to " + mMusicFxPackageName);
@@ -158,9 +160,12 @@ public class MusicFxHelper {
                     Log.e(TAG, "RemoteException with unregisterUidObserver: " + e);
                 }
                 mUidObserverToken = null;
-                mContext.unbindService(mMusicFxBindConnection);
-                Log.i(TAG, "last session closed, unregister UID observer, and unbind "
+                if (mIsBinded) {
+                    mContext.unbindService(mMusicFxBindConnection);
+                    mIsBinded = false;
+                    Log.i(TAG, "last session closed, unregister UID observer, and unbind "
                         + mMusicFxPackageName);
+                }
             }
         }
     }
