f8fa93167ba6454b1eb057af065b5fc72a850b5d
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-03 03:15:48 +0300

    SystemUI: Show big keyguard clock when media controls are hidden
    Was hiding even when media notification isn't showing on keyguard.
    Playing media alone shouldn't cause this unexpected behavior
    Preserves old behavior when media is shown on keyguard
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1810,7 +1810,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     @ClockSize
     private int computeDesiredClockSizeForSingleShade() {
-        if (hasVisibleNotifications()) {
+        if (hasVisibleNotifications(true)) {
             return SMALL;
         }
         return LARGE;
@@ -1909,14 +1909,22 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     }
 
     private boolean hasVisibleNotifications() {
-        final boolean mediaVisible = mMediaDataManager.hasActiveMediaOrRecommendation()
+        return hasVisibleNotifications(false);
+    }
+
+    private boolean hasVisibleNotifications(boolean onKeyguard) {
+        final boolean mediaOnKeyguard = !isOnAod()
                 && mMediaHierarchyManager.getShouldShowOnLockScreen();
+        final boolean isMediaVisibleToUser =
+                mMediaDataManager.hasActiveMediaOrRecommendation()
+                && (mediaOnKeyguard || !onKeyguard);
         if (FooterViewRefactor.isEnabled()) {
             return mActiveNotificationsInteractor.getAreAnyNotificationsPresentValue()
-                    || mediaVisible;
+                    || isMediaVisibleToUser;
         } else {
             return mNotificationStackScrollLayoutController
-                    .getVisibleNotificationCount() != 0 || mediaVisible;
+                    .getVisibleNotificationCount() != 0
+                    || isMediaVisibleToUser;
         }
     }
 
