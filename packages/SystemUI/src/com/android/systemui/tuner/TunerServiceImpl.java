f46413ebff5b81f7c6036c105989f381bf1fd9d3
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-06 08:22:07 +0800

    SystemUI: Fix thread safety issue when adding tunables
    04-05 20:55:11.791  1923  1923 E AndroidRuntime: Caused by: java.util.ConcurrentModificationException
    04-05 20:55:11.791  1923  1923 E AndroidRuntime: 	at android.util.ArrayMap.put(ArrayMap.java:626)
    04-05 20:55:11.791  1923  1923 E AndroidRuntime: 	at com.android.systemui.tuner.TunerServiceImpl.addTunable(TunerServiceImpl.java:342)
    04-05 20:55:11.791  1923  1923 E AndroidRuntime: 	at com.android.systemui.tuner.TunerServiceImpl.addTunable(TunerServiceImpl.java:314)
    04-05 20:55:11.791  1923  1923 E AndroidRuntime: 	at com.android.systemui.volume.VolumeDialogComponent.<init>(VolumeDialogComponent.java:137)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -334,10 +334,12 @@ public class TunerServiceImpl extends TunerService {
         } else {
             uri = Settings.Secure.getUriFor(key);
         }
-        if (!mListeningUris.containsKey(uri)) {
-            mListeningUris.put(uri, key);
-            mContentResolver.registerContentObserver(uri, false, mObserver,
-                    isLineageGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
+        synchronized (this) {
+            if (!mListeningUris.containsKey(uri)) {
+                mListeningUris.put(uri, key);
+                mContentResolver.registerContentObserver(uri, false, mObserver,
+                        isLineageGlobal(key) ? UserHandle.USER_ALL : mCurrentUser);
+            }
         }
         // Send the first state.
         String value = DejankUtils.whitelistIpcs(() -> getValue(key));
