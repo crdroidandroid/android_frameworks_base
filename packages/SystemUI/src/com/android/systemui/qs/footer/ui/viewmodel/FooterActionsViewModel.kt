d377e23b7fc850192fa77c7410d52b952ba40177
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-09 12:27:46 +0300

    SystemUI: Adapt long press on QS settings to launch crDroid Settings
    Add support for adding an onLongPress Unit to the Expandable for view
    AOSP now uses it by default for the settings button
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -189,16 +189,13 @@ fun FooterActionsViewModel(
         footerActionsInteractor.showSettings(expandable)
     }
 
-    fun onSettingsButtonLongClicked(expandable: Expandable): Boolean {
+    fun onSettingsButtonLongClicked(expandable: Expandable) {
         if (falsingManager.isFalseTap(FalsingManager.LOW_PENALTY)) {
-            return false
+            return
         }
 
-        return footerActionsInteractor.showCustomSettings(expandable)
-    }
-
-    fun doNothingLongClick(expandable: Expandable): Boolean {
-        return false
+        footerActionsInteractor.showCustomSettings(expandable)
+        return
     }
 
     fun onPowerButtonClicked(expandable: Expandable) {
@@ -257,7 +254,6 @@ fun FooterActionsViewModel(
                             qsThemedContext,
                             userSwitcherStatus,
                             ::onUserSwitcherClicked,
-                            ::doNothingLongClick
                         )
                     }
                 }
@@ -267,7 +263,7 @@ fun FooterActionsViewModel(
     val settings = settingsButtonViewModel(qsThemedContext, ::onSettingsButtonClicked, ::onSettingsButtonLongClicked)
     val power =
         if (showPowerButton) {
-            powerButtonViewModel(qsThemedContext, ::onPowerButtonClicked, ::doNothingLongClick)
+            powerButtonViewModel(qsThemedContext, ::onPowerButtonClicked)
         } else {
             null
         }
@@ -321,7 +317,6 @@ fun userSwitcherButtonViewModel(
     qsThemedContext: Context,
     status: UserSwitcherStatusModel.Enabled,
     onUserSwitcherClicked: (Expandable) -> Unit,
-    doNothingLongClick: (Expandable) -> Boolean,
 ): FooterActionsButtonViewModel {
     val icon = status.currentUserImage!!
     return FooterActionsButtonViewModel(
@@ -336,7 +331,6 @@ fun userSwitcherButtonViewModel(
         iconTint = null,
         backgroundColor = R.attr.shadeInactive,
         onClick = onUserSwitcherClicked,
-        onLongClick = doNothingLongClick,
     )
 }
 
@@ -352,7 +346,7 @@ private fun userSwitcherContentDescription(
 fun settingsButtonViewModel(
     qsThemedContext: Context,
     onSettingsButtonClicked: (Expandable) -> Unit,
-    onSettingsButtonLongClicked: (Expandable) -> Boolean,
+    onSettingsButtonLongClicked: (Expandable) -> Unit,
 ): FooterActionsButtonViewModel {
     return FooterActionsButtonViewModel(
         id = R.id.settings_button_container,
@@ -374,7 +368,6 @@ fun settingsButtonViewModel(
 fun powerButtonViewModel(
     qsThemedContext: Context,
     onPowerButtonClicked: (Expandable) -> Unit,
-    doNothingLongClick: (Expandable) -> Boolean,
 ): FooterActionsButtonViewModel {
     return FooterActionsButtonViewModel(
         id = R.id.pm_lite,
@@ -389,6 +382,5 @@ fun powerButtonViewModel(
             ),
         backgroundColor = R.attr.shadeActive,
         onPowerButtonClicked,
-        doNothingLongClick,
     )
 }
