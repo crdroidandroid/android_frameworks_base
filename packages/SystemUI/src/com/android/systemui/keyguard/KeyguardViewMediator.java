db38a2985ac0f55bed995c580915ad7623b08f02
Pranav Vashi <neobuddy89@gmail.com>
2024-04-11 10:46:45 +0530

    SystemUI: Add null check for wakelock in KeyguardViewMediator
    * When SystemUI crashes, below crash appears making lockscreen blank.
    
    Log:
    
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: FATAL EXCEPTION: main
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: Process: com.android.systemui, PID: 24478
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.os.PowerManager$WakeLock.acquire()' on a null object reference
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at com.android.systemui.keyguard.KeyguardViewMediator.showLocked(go/retraceme c8e5bd84b42ae4e4ba71c46a73b244063826e864a9442c6ee58b8289e448cdd9:19)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at com.android.systemui.keyguard.KeyguardViewMediator.doKeyguardLocked(go/retraceme c8e5bd84b42ae4e4ba71c46a73b244063826e864a9442c6ee58b8289e448cdd9:195)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at com.android.systemui.keyguard.KeyguardViewMediator$12.handleMessage(go/retraceme c8e5bd84b42ae4e4ba71c46a73b244063826e864a9442c6ee58b8289e448cdd9:77)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:107)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:232)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:317)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8498)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
    04-11 10:42:00.060 24478 24478 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:878
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2371,7 +2371,8 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         Trace.beginSection("KeyguardViewMediator#showKeyguard acquiring mShowKeyguardWakeLock");
         if (DEBUG) Log.d(TAG, "showKeyguard");
         // ensure we stay awake until we are finished displaying the keyguard
-        mShowKeyguardWakeLock.acquire();
+        if (mShowKeyguardWakeLock != null)
+            mShowKeyguardWakeLock.acquire();
         Message msg = mHandler.obtainMessage(SHOW, options);
         // Treat these messages with priority - This call can originate from #doKeyguardTimeout,
         // meaning the device may lock, so it shouldn't wait for other messages on the thread to
@@ -2823,7 +2824,8 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             userActivity();
             mUpdateMonitor.setKeyguardGoingAway(false);
             mKeyguardViewControllerLazy.get().setKeyguardGoingAwayState(false);
-            mShowKeyguardWakeLock.release();
+            if (mShowKeyguardWakeLock != null)
+                mShowKeyguardWakeLock.release();
         }
         mKeyguardDisplayManager.show();
 
