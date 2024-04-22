78f5789062f452007993e41923525eb1b8cad455
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-22 15:35:28 +0300

    base: Allow turning on Bluetooth when pressing the tile [1/2]
    Respectively
    Could save some taps for some users
    
    Change-Id: I1c3a2e9b37d7ea07d8d1ba2bee26998bd3c8ac02
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6589,6 +6589,12 @@ public final class Settings {
         public static final String NOTIFICATION_MATERIAL_DISMISS = "notification_material_dismiss";
 
         /**
+         * Whether to turn on Bluetooth automatically when showing the Bluetooth dialog
+         * @hide
+         */
+        public static final String QS_BT_AUTO_ON = "qs_bt_auto_on";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
