e41fc7e8f8b171ae00110d0758b3e6ffa33e934f
lgjint <lgjint@protonmail.com>
2024-04-30 22:32:37 +0800

    SystemUI: Fix the bluetooth icon cannot be toggled by status tuner
    The implementation of bluetooth icon in commit 2071f631752953f5d61457d8a760651109176fde didn't consider the status tuner, fix it by adding mBlocked variable.
    
    Change-Id: I011c40c34f44a17a005bb95b619dce4d3e0cdb96
    Signed-off-by: lgjint <lgjint@protonmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -447,7 +447,7 @@ public interface StatusBarIconController {
                     return addBindableIcon((BindableIconHolder) holder, index);
 
                 case TYPE_BLUETOOTH:
-                    return addBluetoothIcon(index, slot, holder.getBluetoothState());
+                    return addBluetoothIcon(index, slot, blocked, holder.getBluetoothState());
             }
 
             return null;
@@ -507,8 +507,8 @@ public interface StatusBarIconController {
         }
 
         protected StatusBarBluetoothView addBluetoothIcon(
-                int index, String slot, BluetoothIconState state) {
-            StatusBarBluetoothView view = onCreateStatusBarBluetoothView(slot);
+                int index, String slot, boolean blocked, BluetoothIconState state) {
+            StatusBarBluetoothView view = onCreateStatusBarBluetoothView(slot, blocked);
             view.applyBluetoothState(state);
             mGroup.addView(view, index, onCreateLayoutParams());
             return view;
@@ -534,8 +534,10 @@ public interface StatusBarIconController {
                         );
         }
 
-        private StatusBarBluetoothView onCreateStatusBarBluetoothView(String slot) {
-            StatusBarBluetoothView view = StatusBarBluetoothView.fromContext(mContext, slot);
+        private StatusBarBluetoothView onCreateStatusBarBluetoothView(
+                String slot, boolean blocked) {
+            StatusBarBluetoothView view =
+                    StatusBarBluetoothView.fromContext(mContext, slot, blocked);
             return view;
         }
 
