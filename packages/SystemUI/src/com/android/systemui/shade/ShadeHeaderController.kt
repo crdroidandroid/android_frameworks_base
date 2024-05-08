853ee5becdd11b7ef38a2de30cd1e329d8fe15f8
Pranav Vashi <neobuddy89@gmail.com>
2024-05-08 22:25:07 +0530

    SystemUI: Drop unused rounded corner padding
    * Wtf google?
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -163,7 +163,6 @@ constructor(
     private val systemIconsHoverContainer: View =
         header.requireViewById(R.id.hover_system_icons_container)
 
-    private var roundedCorners = 0
     private var cutout: DisplayCutout? = null
     private var lastInsets: WindowInsets? = null
     private var nextAlarmIntent: PendingIntent? = null
@@ -611,7 +610,6 @@ constructor(
     }
 
     private fun updateResources() {
-        roundedCorners = resources.getDimensionPixelSize(R.dimen.rounded_corner_content_padding)
         val padding = resources.getDimensionPixelSize(R.dimen.qs_panel_padding)
         header.setPadding(padding, header.paddingTop, padding, header.paddingBottom)
         updateQQSPaddings()
