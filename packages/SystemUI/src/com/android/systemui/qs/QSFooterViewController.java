f0896287ff9ed470ac9e1d6128a6f4803e47773e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-20 08:17:07 +0800

    SystemUI: Improve QS data usage info
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic415ff1b32f69b925b8bf603ef6430ac8ad707bb
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterViewController.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterViewController.java
@@ -65,6 +65,7 @@ public class QSFooterViewController extends ViewController<QSFooterView>
 
     private final UserTracker mUserTracker;
     private final QSPanelController mQsPanelController;
+    private final TextView mUsageText;
     private final PageIndicator mPageIndicator;
     private final View mEditButton;
     private final FalsingManager mFalsingManager;
@@ -127,6 +128,7 @@ public class QSFooterViewController extends ViewController<QSFooterView>
         mWifiTracker = trackerFactory.createTracker(this::onWifiStatusUpdated, null);
         mPageIndicator = mView.findViewById(R.id.footer_page_indicator);
         mEditButton = mView.findViewById(android.R.id.edit);
+        mUsageText = mView.findViewById(R.id.build);
     }
 
     @Override
@@ -137,6 +139,12 @@ public class QSFooterViewController extends ViewController<QSFooterView>
             }
             mActivityStarter
                     .postQSRunnableDismissingKeyguard(() -> mQsPanelController.showEdit(view));
+        });
+        mUsageText.setOnClickListener(view -> {
+            Intent nIntent = new Intent(Intent.ACTION_MAIN);
+            nIntent.setClassName("com.android.settings",
+                    "com.android.settings.Settings$DataUsageSummaryActivity");
+            mActivityStarter.startActivity(nIntent, true /* dismissShade */);
         });
         mQsPanelController.setFooterPageIndicator(mPageIndicator);
         final IntentFilter filter = new IntentFilter();
