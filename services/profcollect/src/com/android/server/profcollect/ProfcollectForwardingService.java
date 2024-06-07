0509dae0a598ec25f543e081f08bf8d65d71f634
Yi Kong <yikong@google.com>
2024-06-07 02:45:43 +0000

    profcollect: Use postDelayed instead of starting a thread and sleep
    Addresses follow-up comments from https://r.android.com/3115077.
    
    Change-Id: I6abaee35a4797c4e0d43daffaf2ab49d650cd9cc
    Test: presubmit
    Bug: 319394981
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/profcollect/src/com/android/server/profcollect/ProfcollectForwardingService.java
+++ services/profcollect/src/com/android/server/profcollect/ProfcollectForwardingService.java
@@ -321,16 +321,14 @@ public final class ProfcollectForwardingService extends SystemService {
                 "dex2oat_trace_freq", 25);
         int randomNum = ThreadLocalRandom.current().nextInt(100);
         if (randomNum < traceFrequency) {
-            BackgroundThread.get().getThreadHandler().post(() -> {
+            // Dex2oat could take a while before it starts. Add a short delay before start tracing.
+            BackgroundThread.get().getThreadHandler().postDelayed(() -> {
                 try {
-                    // Dex2oat could take a while before it starts. Add a short delay before start
-                    // tracing.
-                    Thread.sleep(1000);
                     mIProfcollect.trace_once("dex2oat");
-                } catch (RemoteException | InterruptedException e) {
+                } catch (RemoteException e) {
                     Log.e(LOG_TAG, "Failed to initiate trace: " + e.getMessage());
                 }
-            });
+            }, 1000);
         }
     }
 
