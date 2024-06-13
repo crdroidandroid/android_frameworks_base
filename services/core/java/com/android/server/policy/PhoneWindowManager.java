de503410b14c380ddeb31f5c7b579602cc49605d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-13 18:04:13 +0300

    fix crash of boot-time info dialog due to delayed font init
    com.android.text.flags.use_optimized_boottime_font_loading flag is now enabled, which delays font
    initialization.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7073,6 +7073,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     lp.setFitInsetsTypes(0 /* types */);
                     mBootMsgDialog.getWindow().setAttributes(lp);
                     mBootMsgDialog.setCancelable(false);
+                    android.graphics.Typeface.loadPreinstalledSystemFontMap();
                     mBootMsgDialog.show();
                 }
                 mBootMsgDialog.setMessage(msg);
