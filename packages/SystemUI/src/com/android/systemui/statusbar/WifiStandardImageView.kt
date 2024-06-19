b17b2f637bbf595558a8c66787bdb990a8150b75
minaripenguin <minaripenguin@users.noreply.github.com>
2024-06-19 09:58:32 +0800

    Wifi Standard: Rework handling logic
    * attempt to address crashes caused by flooding requests and fa245e385b14d4776f611014e31845105139029e
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/WifiStandardImageView.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/WifiStandardImageView.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The risingOS Android Project
+ * Copyright (C) 2023-2024 The risingOS Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,8 +21,9 @@ import android.provider.Settings
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import android.net.wifi.WifiManager
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.widget.ImageView
 import com.android.systemui.Dependency
@@ -38,24 +39,34 @@ class WifiStandardImageView @JvmOverloads constructor(
     private val tunerService: TunerService by lazy { Dependency.get(TunerService::class.java) }
     private val connectivityManager: ConnectivityManager by lazy { context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
     private val wifiManager: WifiManager by lazy { context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager }
-    private var networkCallback: ConnectivityManager.NetworkCallback? = null
     private var wifiStandardEnabled = false
     private var isTunerRegistered = false
-    private var isRegistered = false
+    private var currWifiStandard: Int = -1
+    private val handler = Handler(Looper.getMainLooper())
+    private val updateRunnable = object : Runnable {
+        override fun run() {
+            if (wifiStandardEnabled) {
+                val wifiStandard = getWifiStandard()
+                if (currWifiStandard != wifiStandard) {
+                    updateWifiStandard(wifiStandard)
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+    }
 
     init {
         registerTunerService()
-        showWifiStandard()
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        showWifiStandard()
+        handler.post(updateRunnable)
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        unregisterNetworkCallback()
+        handler.removeCallbacks(updateRunnable)
     }
 
     private fun registerTunerService() {
@@ -63,66 +74,45 @@ class WifiStandardImageView @JvmOverloads constructor(
         tunerService.addTunable({ key, value ->
             wifiStandardEnabled = TunerService.parseIntegerSwitch(value, false)
             if (wifiStandardEnabled) {
-                showWifiStandard()
+                handler.post(updateRunnable)
             } else {
-                unregisterNetworkCallback()
+                handler.removeCallbacks(updateRunnable)
+                visibility = GONE
             }
         }, "system:" + Settings.System.WIFI_STANDARD_ICON)
         isTunerRegistered = true
     }
 
-    private fun showWifiStandard() {
-        if (!wifiStandardEnabled || networkCallback != null) return
-        networkCallback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                updateWifiStandard(network)
-            }
-
-            override fun onUnavailable() {
-                updateWifiStandard(null)
-            }
-
-            override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
-                if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    updateWifiStandard(network)
-                }
-            }
-        }
-        registerNetworkCallback()
-    }
-
-    private fun updateWifiStandard(network: Network?) {
-        val wifiStandard = if (network != null) getWifiStandard(network) else -1
-        updateIcon(wifiStandard)
-    }
-
-    private fun getWifiStandard(network: Network): Int {
-        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+    private fun getWifiStandard(): Int {
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         return if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
             val wifiInfo = wifiManager.connectionInfo
             wifiInfo.wifiStandard
         } else {
-            -1 
+            -1
         }
     }
 
+    private fun updateWifiStandard(wifiStandard: Int) {
+        currWifiStandard = wifiStandard
+        updateIcon(wifiStandard)
+    }
+
     private fun updateIcon(wifiStandard: Int) {
-        if (!wifiStandardEnabled || wifiStandard < 4) {
-            post {
+        post {
+            if (!wifiStandardEnabled || wifiStandard < 4) {
                 visibility = GONE
                 layoutParams = (layoutParams as MarginLayoutParams).apply {
                     marginEnd = 0
                 }
-            }
-            return
-        }
-        val drawableId = getDrawableForWifiStandard(wifiStandard)
-        if (drawableId > 0) {
-            post {
-                setImageResource(drawableId)
-                visibility = VISIBLE
-                layoutParams = (layoutParams as MarginLayoutParams).apply {
-                    marginEnd = resources.getDimensionPixelSize(R.dimen.status_bar_airplane_spacer_width)
+            } else {
+                val drawableId = getDrawableForWifiStandard(wifiStandard)
+                if (drawableId > 0) {
+                    setImageResource(drawableId)
+                    visibility = VISIBLE
+                    layoutParams = (layoutParams as MarginLayoutParams).apply {
+                        marginEnd = resources.getDimensionPixelSize(R.dimen.status_bar_airplane_spacer_width)
+                    }
                 }
             }
         }
@@ -135,29 +125,6 @@ class WifiStandardImageView @JvmOverloads constructor(
             6 -> R.drawable.ic_wifi_standard_6
             7 -> R.drawable.ic_wifi_standard_7
             else -> 0
-        }
-    }
-
-    private fun registerNetworkCallback() {
-        if (isRegistered || networkCallback == null) return
-        val networkRequest = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .build()
-        connectivityManager.registerNetworkCallback(networkRequest, networkCallback!!)
-        isRegistered = true
-    }
-
-    private fun unregisterNetworkCallback() {
-        if (!isRegistered || networkCallback == null) return
-        try {
-            networkCallback?.let { connectivityManager.unregisterNetworkCallback(it) }
-            post {
-                visibility = GONE
-            }
-        } catch (e: IllegalArgumentException) {
-        } finally {
-            networkCallback = null
-            isRegistered = false
         }
     }
 }
