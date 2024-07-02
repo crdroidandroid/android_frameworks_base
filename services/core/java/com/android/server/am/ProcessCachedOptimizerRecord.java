be268ba30b8c3ff1ab38810350138c4d986ba28c
ayushigupta <quic_ayugup@quicinc.com>
2024-07-02 14:56:58 +0530

    CachedAppOptimizer: Initialize compactProfile and compactTime
    CRs-Fixed: 3858822
    Change-Id: I6f06176d8e5e40a5fd2afb53b9417005f2e26b2a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ProcessCachedOptimizerRecord.java
+++ services/core/java/com/android/server/am/ProcessCachedOptimizerRecord.java
@@ -178,7 +178,7 @@ final class ProcessCachedOptimizerRecord {
     CachedAppOptimizer.CompactProfile getLastCompactProfile() {
         if (mLastCompactProfile == null) {
             // The first compaction won't have a previous one, so assign one to avoid crashing.
-            mLastCompactProfile = CachedAppOptimizer.CompactProfile.SOME;
+            mLastCompactProfile = CachedAppOptimizer.CompactProfile.FULL;
         }
 
         return mLastCompactProfile;
@@ -337,6 +337,7 @@ final class ProcessCachedOptimizerRecord {
 
     void init(long nowUptime) {
         mFreezeUnfreezeTime = nowUptime;
+        mLastCompactTime = nowUptime;
     }
 
     @GuardedBy("mProcLock")
