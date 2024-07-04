ca1414a44a698a4722c50a02d13b63242b5ab2be
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-04 11:28:19 +0800

    SystemUI: Fix AOD and pulsing scrim color
    * it follows background color resulting to white AOD on light mode theme
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -188,11 +188,11 @@ public enum ScrimState {
             final boolean isDocked = mDockManager.isDocked();
             mBlankScreen = mDisplayRequiresBlanking;
 
-            mFrontTint = mBackgroundColor;
+            mFrontTint = Color.BLACK;
             mFrontAlpha = (alwaysOnEnabled || isDocked || quickPickupEnabled)
                     ? mAodFrontScrimAlpha : 1f;
 
-            mBehindTint = mBackgroundColor;
+            mBehindTint = Color.BLACK;
             mBehindAlpha = ScrimController.TRANSPARENT;
 
             mAnimationDuration = ScrimController.ANIMATION_DURATION_LONG;
@@ -226,8 +226,8 @@ public enum ScrimState {
         @Override
         public void prepare(ScrimState previousState) {
             mFrontAlpha = mAodFrontScrimAlpha;
-            mBehindTint = mBackgroundColor;
-            mFrontTint = mBackgroundColor;
+            mBehindTint = Color.BLACK;
+            mFrontTint = Color.BLACK;
             mBlankScreen = mDisplayRequiresBlanking;
             mAnimationDuration = mWakeLockScreenSensorActive
                     ? ScrimController.ANIMATION_DURATION_LONG : ScrimController.ANIMATION_DURATION;
@@ -267,12 +267,12 @@ public enum ScrimState {
 
             if (mDisplayRequiresBlanking && previousState == ScrimState.AOD) {
                 // Set all scrims black, before they fade transparent.
-                updateScrimColor(mScrimInFront, 1f /* alpha */, mBackgroundColor /* tint */);
-                updateScrimColor(mScrimBehind, 1f /* alpha */, mBackgroundColor /* tint */);
+                updateScrimColor(mScrimInFront, 1f /* alpha */, Color.BLACK /* tint */);
+                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.BLACK /* tint */);
 
                 // Scrims should still be black at the end of the transition.
-                mFrontTint = mBackgroundColor;
-                mBehindTint = mBackgroundColor;
+                mFrontTint = Color.BLACK;
+                mBehindTint = Color.BLACK;
                 mBlankScreen = true;
             }
 
