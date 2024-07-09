a98af2641a409f3de8dd97dad7bc66a433467abf
wumin3 <wumin3@xiaomi.com>
2024-07-09 15:00:43 +0800

    AudioService: do not block focus request from applications compiled with lower version sdks
    The applications compiled with lower version sdks can not request audio focus properly in some scenes, which will cause many anomalies.
    
    Change-Id: I8875922efdfbcd08a7872e090c3c4b51446b2637
    Signed-off-by: wumin3 <wumin3@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -11433,6 +11433,8 @@ public class AudioService extends IAudioService.Stub
             permissionOverridesCheck = true;
         } else if (uid < UserHandle.AID_APP_START) {
             permissionOverridesCheck = true;
+        } else if (sdk <= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            permissionOverridesCheck = true;
         }
 
         final long token = Binder.clearCallingIdentity();
