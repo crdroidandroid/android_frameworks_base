87e0fab8a0f1b66635ede073bc0d27e9899fabd5
Pranav Vashi <neobuddy89@gmail.com>
2024-07-13 12:24:05 +0530

    Battery Styles: Apply battery style to battery status chip
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BatteryStatusChip.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/BatteryStatusChip.kt
@@ -18,6 +18,8 @@ import android.annotation.IntRange
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Configuration
+import android.os.UserHandle
+import android.provider.Settings
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
@@ -36,11 +38,25 @@ class BatteryStatusChip @JvmOverloads constructor(context: Context, attrs: Attri
     override val contentView: View
         get() = batteryMeterView
 
+    private val BATTERY_STYLE_TEXT = 4
+    private val BATTERY_STYLE_HIDDEN = 5
+
+    private var batteryStyle = Settings.System.getIntForUser(
+             context.contentResolver, Settings.System.STATUS_BAR_BATTERY_STYLE, 0, UserHandle.USER_CURRENT)
+    private var showBatteryPercent = Settings.System.getIntForUser(
+             context.contentResolver, Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT)
+
     init {
         inflate(context, R.layout.battery_status_chip, this)
         roundedContainer = requireViewById(R.id.rounded_container)
         batteryMeterView = requireViewById(R.id.battery_meter_view)
         batteryMeterView.setStaticColor(true)
+        if (batteryStyle == BATTERY_STYLE_HIDDEN) {
+            batteryMeterView.setBatteryStyle(BATTERY_STYLE_TEXT)
+        } else {
+            batteryMeterView.setBatteryStyle(batteryStyle)
+        }
+        batteryMeterView.setBatteryPercent(showBatteryPercent);
         if (newStatusBarIcons()) {
             batteryMeterView.setUnifiedBatteryColors(BatteryColors.LightThemeColors)
         } else {
