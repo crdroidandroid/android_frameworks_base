f7c048a838597a148af0f32d4dfd8254184a8b5f
Oleg Lyovin <ovlevin@salutedevices.com>
2024-07-25 13:20:22 +0300

    Mark ProcessRecord as persistent for child processes of persistent apps
    Service can be declared in an Androidmanifest to run in a separate
    process. If an application is marked as persistent, it is expected that
    all the ProcessRecords created by this application will also be
    persistent.
    
    However, processes that run services are currently not marked as
    persistent. This patch fixes this issue.
    
    Bug: 355249252
    Test: adb shell dumpsys activity processes
    Change-Id: I447e7337bead8ed81cd67492484189f0a97faa2b
    Signed-off-by: Oleg Lyovin <ovlevin@salutedevices.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -5712,6 +5712,10 @@ public final class ActiveServices {
                 bringDownServiceLocked(r, enqueueOomAdj);
                 return msg;
             }
+            if ((r.appInfo.flags & ApplicationInfo.FLAG_PERSISTENT) != 0
+                    && !TextUtils.equals(procName, r.appInfo.processName)) {
+                app.setPersistent(true);
+            }
             mAm.mProcessList.getAppStartInfoTracker().handleProcessServiceStart(startTimeNs, app, r,
                     true);
             if (isolated) {
