b2767801b36b1cb586ee0f8008d5475579aa7e4f
Pranav Vashi <neobuddy89@gmail.com>
2024-07-27 02:56:26 +0530

    NetworkTraffic: Prevent SystemUI crash on user change
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NetworkTraffic.java
@@ -109,7 +109,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
 
     private int mRefreshInterval = 2;
 
-    private boolean mAttached;
+    protected boolean mAttached;
     private boolean mHideArrows;
 
     protected boolean mVisible = true;
