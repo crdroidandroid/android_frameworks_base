608e37e8a7a4871f8020e9dbfc071eb036be558d
Pranav Vashi <neobuddy89@gmail.com>
2024-07-27 02:56:26 +0530

    NetworkTraffic: Prevent SystemUI crash on user change
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarNetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarNetworkTraffic.java
@@ -44,7 +44,7 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements DarkRecei
     private int mVisibleState = -1;
     private boolean mColorIsStatic;
 
-    private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
+    private KeyguardUpdateMonitor mKeyguardUpdateMonitor;
     private boolean mKeyguardShowing;
 
     private String mSlot;
@@ -59,8 +59,6 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements DarkRecei
 
     public StatusBarNetworkTraffic(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mKeyguardUpdateMonitor = Dependency.get(KeyguardUpdateMonitor.class);
-        mKeyguardUpdateMonitor.registerCallback(mUpdateCallback);
     }
 
     public static StatusBarNetworkTraffic fromContext(Context context, String slot) {
@@ -113,6 +111,15 @@ public class StatusBarNetworkTraffic extends NetworkTraffic implements DarkRecei
     public void setVisibleState(int state, boolean animate) {
         mVisibleState = state;
         updateVisibility();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mAttached && mKeyguardUpdateMonitor == null) {
+            mKeyguardUpdateMonitor = Dependency.get(KeyguardUpdateMonitor.class);
+            mKeyguardUpdateMonitor.registerCallback(mUpdateCallback);
+        }
     }
 
     public void applyNetworkTrafficState(NetworkTrafficState state) {
