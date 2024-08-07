c34a5a26634a4d3b09ca2b76b6ad692c11e55424
Matt Filetto <matt.filetto@gmail.com>
2024-08-07 05:56:21 -0700

    ScrimController: Ensure alpha is fully transparent during AOD mode
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1203,6 +1203,12 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
      * device is dozing when the light sensor is on.
      */
     public void setAodFrontScrimAlpha(float alpha) {
+    
+         // Ensure alpha is fully opaque during AOD mode
+        if (mState == ScrimState.AOD) {
+            alpha = 0.0f; // Fully transparent
+        }
+
         if (mInFrontAlpha != alpha && shouldUpdateFrontScrimAlpha()) {
             mInFrontAlpha = alpha;
             updateScrims();
