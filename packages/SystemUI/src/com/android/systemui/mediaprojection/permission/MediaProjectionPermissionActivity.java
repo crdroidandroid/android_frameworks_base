e0d12ca6be9538543818c8932563900357751916
Shao Mengchuan <shao.mengchuan@zte.com.cn>
2024-08-14 15:49:13 +0800

    Fix memory leak on MediaProjectionPermissionActivity
    It will cause a memory leak when calling dialog.setOnDismissListener()
    after calling dialog.registerDismissListener(). It should be replaced
    with an extra Runnable as a parameter.
    
    Google: 3216578
    Change-Id: Ic02f03107c9930087b743ca70f1fca3ad72436d5
    Signed-off-by: Jia Jia <jia.jia@zte.com.cn>

--- packages/SystemUI/src/com/android/systemui/mediaprojection/permission/MediaProjectionPermissionActivity.java
+++ packages/SystemUI/src/com/android/systemui/mediaprojection/permission/MediaProjectionPermissionActivity.java
@@ -313,7 +313,7 @@ public class MediaProjectionPermissionActivity extends Activity
     }
 
     private void setUpDialog(AlertDialog dialog) {
-        SystemUIDialog.registerDismissListener(dialog);
+        SystemUIDialog.registerDismissListener(dialog, this::onDialogDismissed);
         SystemUIDialog.applyFlags(dialog);
         SystemUIDialog.setDialogSize(dialog);
 
@@ -324,6 +324,12 @@ public class MediaProjectionPermissionActivity extends Activity
 
         final Window w = dialog.getWindow();
         w.addSystemFlags(SYSTEM_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
+    }
+
+    private void onDialogDismissed() {
+        if (!isFinishing()) {
+            finish();
+        }
     }
 
     private boolean showScreenCaptureDisabledDialogIfNeeded() {
