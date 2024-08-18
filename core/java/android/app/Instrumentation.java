f254fced84e1a90258766abf58c29bd0575c6b32
Lup Gabriel <gwolf2u.website@gmail.com>
2024-08-18 14:55:03 +0300

    PixelPropUtils: fixed call for setProps (#1134)
    fixes https://github.com/crdroidandroid/issue_tracker/issues/488

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1244,8 +1244,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        String packageName = context.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        PixelPropsUtils.setProps(context);
         return app;
     }
     
@@ -1263,8 +1262,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        String packageName = context.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        PixelPropsUtils.setProps(context);
         return app;
     }
 
