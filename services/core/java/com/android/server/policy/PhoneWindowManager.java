6db0f01256dbb42270d1ae4d6d71e1fde35136fe
Pranav Vashi <neobuddy89@gmail.com>
2024-08-21 09:49:41 +0530

    PhoneWindowManager: Prevent NPE with voice search action
    Log:
    
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: android.ui
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: java.lang.RuntimeException: WakeLock under-locked PhoneWindowManager.mBroadcastWakeLock
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.os.PowerManager$WakeLock.release(PowerManager.java:3949)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.os.PowerManager$WakeLock.release(PowerManager.java:3910)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.policy.PhoneWindowManager.launchVoiceAssistWithWakeLock(PhoneWindowManager.java:6288)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.policy.PhoneWindowManager.performKeyAction(PhoneWindowManager.java:2294)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.policy.PhoneWindowManager.performKeyAction(PhoneWindowManager.java:2274)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.policy.PhoneWindowManager.-$$Nest$mperformKeyAction(PhoneWindowManager.java:0)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.policy.PhoneWindowManager$17.onSwipeThreeFinger(PhoneWindowManager.java:6971)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.policy.ThreeFingerSwipeListener.onPointerEvent(ThreeFingerSwipeListener.java:90)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.wm.PointerEventDispatcher.onInputEvent(PointerEventDispatcher.java:53)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.view.InputEventReceiver.dispatchInputEvent(InputEventReceiver.java:295)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.view.InputEventReceiver.nativeConsumeBatchedInputEvents(Native Method)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.view.InputEventReceiver.consumeBatchedInputEvents(InputEventReceiver.java:259)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.view.InputEventReceiver.onBatchedInputEventPending(InputEventReceiver.java:203)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.os.MessageQueue.nativePollOnce(Native Method)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.os.MessageQueue.next(MessageQueue.java:349)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:189)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:317)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at android.os.HandlerThread.run(HandlerThread.java:85)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.ServiceThread.run(ServiceThread.java:46)
    08-21 09:37:42.335  6607  6630 E AndroidRuntime: 	at com.android.server.UiThread.run(UiThread.java:45)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2284,7 +2284,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 logKeyboardSystemsEvent(event, KeyboardLogEvent.LAUNCH_ASSISTANT);
                 break;
             case VOICE_SEARCH:
-                launchVoiceAssistWithWakeLock();
+                launchVoiceAssist(mAllowStartActivityForLongPressOnPowerDuringSetup);
                 break;
             case IN_APP_SEARCH:
                 triggerVirtualKeypress(KeyEvent.KEYCODE_SEARCH);
