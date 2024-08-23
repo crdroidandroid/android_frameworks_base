a4607dd1718d2fe70adcaa770b188d73f5931dfb
Pranav Vashi <neobuddy89@gmail.com>
2024-08-24 02:07:37 +0530

    ThemeUtils: Use current user for THEME_CUSTOMIZATION_OVERLAY_PACKAGES
    * Follow ThemePicker and SystemUI.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/ThemeUtils.java
+++ core/java/com/android/internal/util/crdroid/ThemeUtils.java
@@ -40,6 +40,7 @@ import android.graphics.drawable.shapes.PathShape;
 import android.net.Uri;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -98,7 +99,7 @@ public class ThemeUtils {
     public void writeSettings(String category, String packageName, boolean disable) {
         final String overlayPackageJson = Settings.Secure.getStringForUser(
                 mContext.getContentResolver(),
-                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES, USER_SYSTEM);
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES, UserHandle.USER_CURRENT);
         JSONObject object;
         try {
             if (overlayPackageJson == null) {
@@ -113,7 +114,7 @@ public class ThemeUtils {
             }
             Settings.Secure.putStringForUser(mContext.getContentResolver(),
                     Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
-                    object.toString(), USER_SYSTEM);
+                    object.toString(), UserHandle.USER_CURRENT);
         } catch (JSONException e) {
             Log.e(TAG, "Failed to parse THEME_CUSTOMIZATION_OVERLAY_PACKAGES.", e);
         }
