72c76f12931420416d911d74ab3158d036632d2d
Pranav Vashi <neobuddy89@gmail.com>
2024-08-27 22:23:49 +0530

    SystemUI: Properly inject TunerService for PhoneStatusBarPolicy
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -192,6 +192,8 @@ public class PhoneStatusBarPolicy
 
     private boolean mShowNetworkTraffic;
 
+    private TunerService mTunerService;
+
     @Inject
     public PhoneStatusBarPolicy(Context context, StatusBarIconController iconController,
             CommandQueue commandQueue, BroadcastDispatcher broadcastDispatcher,
@@ -210,7 +212,8 @@ public class PhoneStatusBarPolicy
             @Main SharedPreferences sharedPreferences, DateFormatUtil dateFormatUtil,
             RingerModeTracker ringerModeTracker,
             ConnectedDisplayInteractor connectedDisplayInteractor,
-            JavaAdapter javaAdapter
+            JavaAdapter javaAdapter,
+            TunerService tunerService
     ) {
         mContext = context;
         mIconController = iconController;
@@ -270,9 +273,7 @@ public class PhoneStatusBarPolicy
         mSharedPreferences = sharedPreferences;
         mDateFormatUtil = dateFormatUtil;
 
-        Dependency.get(TunerService.class).addTunable(this,
-                BLUETOOTH_SHOW_BATTERY,
-                NETWORK_TRAFFIC_LOCATION);
+        mTunerService = tunerService;
     }
 
     /** Initialize the object after construction. */
@@ -304,6 +305,8 @@ public class PhoneStatusBarPolicy
         updateTTY();
 
         // bluetooth status
+        mShowBluetoothBattery = Settings.System.getIntForUser(mContext.getContentResolver(),
+            BLUETOOTH_SHOW_BATTERY, 1, UserHandle.USER_CURRENT) != 0;
         updateBluetooth();
 
         // Alarm clock
@@ -388,6 +391,9 @@ public class PhoneStatusBarPolicy
                 this::onConnectedDisplayAvailabilityChanged);
 
         mCommandQueue.addCallback(this);
+
+        mTunerService.addTunable(this, BLUETOOTH_SHOW_BATTERY);
+        mTunerService.addTunable(this, NETWORK_TRAFFIC_LOCATION);
 
         // Get initial user setup state
         onUserSetupChanged();
