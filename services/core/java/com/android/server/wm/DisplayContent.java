77e798e4a8f0f23d8a59c2ac1fe222cf4d355279
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-31 06:18:29 +0530

    wm: Add API to listen for secure content in display
    Required by LMOFreeform.
    
    Change-Id: I97143a8b35af1432b9b12319bab5c1dfec9f9b34
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -791,6 +791,8 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
     /** Last window to hold the screen locked. */
     private WindowState mLastWakeLockHoldingWindow;
 
+    private boolean mHasSecureContent;
+
     /**
      * Whether display is allowed to ignore all activity size restrictions.
      * @see #isDisplayIgnoreActivitySizeRestrictions
@@ -5190,6 +5192,13 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         mInputMonitor.setUpdateInputWindowsNeededLw();
         if (updateInputWindows) {
             mInputMonitor.updateInputWindowsLw(false /*force*/);
+        }
+
+        // Notify if display added or removed a secure window
+        final boolean hasSecureContent = hasSecureWindowOnScreen();
+        if (hasSecureContent != mHasSecureContent) {
+            mHasSecureContent = hasSecureContent;
+            mWmService.notifyDisplaySecureContentChange(mDisplayId, hasSecureContent);
         }
     }
 
