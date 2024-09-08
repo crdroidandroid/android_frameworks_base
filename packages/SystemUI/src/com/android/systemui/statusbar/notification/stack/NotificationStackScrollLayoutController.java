8e43ebbdda2effd4db188f0fba19ed538efe8f09
minaripenguin <minaripenguin@users.noreply.github.com>
2024-09-08 19:42:16 +0800

    NSSLC: Prevent possible memory leak
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayoutController.java
@@ -239,6 +239,7 @@ public class NotificationStackScrollLayoutController implements Dumpable {
                     mColorUpdateLogger.logTriggerEvent("NSSLC.onViewDetachedFromWindow()");
                     mConfigurationController.removeCallback(mConfigurationListener);
                     mStatusBarStateController.removeCallback(mStateListener);
+                    mTunerService.removeTunable(mTunable);
                 }
             };
 
@@ -891,16 +892,7 @@ public class NotificationStackScrollLayoutController implements Dumpable {
         mVisibilityLocationProviderDelegator.setDelegate(this::isInVisibleLocation);
 
         mTunerService.addTunable(
-                (key, newValue) -> {
-                    switch (key) {
-                        case Settings.Secure.NOTIFICATION_HISTORY_ENABLED:
-                            mHistoryEnabled = null;  // invalidate
-                            break;
-                        case HIGH_PRIORITY:
-                            mView.setHighPriorityBeforeSpeedBump("1".equals(newValue));
-                            break;
-                    }
-                },
+                mTunable,
                 HIGH_PRIORITY,
                 Settings.Secure.NOTIFICATION_HISTORY_ENABLED);
 
@@ -933,6 +925,17 @@ public class NotificationStackScrollLayoutController implements Dumpable {
 
         mView.setWallpaperInteractor(mWallpaperInteractor);
     }
+
+    private TunerService.Tunable mTunable = (key, newValue) -> {
+        switch (key) {
+            case Settings.Secure.NOTIFICATION_HISTORY_ENABLED:
+                mHistoryEnabled = null;  // Invalidate
+                break;
+            case HIGH_PRIORITY:
+                mView.setHighPriorityBeforeSpeedBump("1".equals(newValue));
+                break;
+        }
+    };
 
     private boolean isInVisibleLocation(NotificationEntry entry) {
         ExpandableNotificationRow row = entry.getRow();
