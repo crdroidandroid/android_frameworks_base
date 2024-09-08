166fcfe4933f156c92d7d2803d62a92e78b2ffeb
minaripenguin <minaripenguin@users.noreply.github.com>
2024-09-08 19:48:05 +0800

    Udfps Icons: Prevent possible memory leak
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/LockIconView.java
+++ packages/SystemUI/src/com/android/keyguard/LockIconView.java
@@ -258,6 +258,14 @@ public class LockIconView extends FrameLayout implements Dumpable {
 
         return "invalid";
     }
+    
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mFingerprintDrawable != null) {
+            mFingerprintDrawable.destroy();
+        }
+    }
 
     @Override
     public void dump(@NonNull PrintWriter pw, @NonNull String[] args) {
