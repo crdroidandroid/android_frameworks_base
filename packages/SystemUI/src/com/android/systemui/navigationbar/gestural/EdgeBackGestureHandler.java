a4cb7b4b6c39533d6f57bfb44020a0d13c9a7f20
minaripenguin <minaripenguin@users.noreply.github.com>
2024-09-09 15:51:54 +0800

    EdgeBackGestureHandler: Prevent possible memory leak
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -313,6 +313,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
     private boolean mBlockedGesturalNavigation;
     private boolean mIsBackGestureArrowEnabled;
     private boolean mIsEdgeHapticEnabled;
+    
+    private TunerService mTunerService;
 
     private final NavigationEdgeBackPlugin.BackCallback mBackCallback =
             new NavigationEdgeBackPlugin.BackCallback() {
@@ -569,9 +571,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         if (mMLEnableWidth > mEdgeWidthRight) mMLEnableWidth = mEdgeWidthRight;
         if (mMLEnableWidth > mEdgeWidthLeft) mMLEnableWidth = mEdgeWidthLeft;
 
-        final TunerService tunerService = Dependency.get(TunerService.class);
-        tunerService.addTunable(this, KEY_EDGE_LONG_SWIPE_ACTION);
-        tunerService.addTunable(this, BACK_GESTURE_HEIGHT);
+        mTunerService = Dependency.get(TunerService.class);
 
         // Reduce the default touch slop to ensure that we can intercept the gesture
         // before the app starts to react to it.
@@ -620,6 +620,8 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         }
         updateIsEnabled();
         mUserTracker.addCallback(mUserChangedCallback, mMainExecutor);
+        mTunerService.addTunable(this, KEY_EDGE_LONG_SWIPE_ACTION);
+        mTunerService.addTunable(this, BACK_GESTURE_HEIGHT);
     }
 
     /**
@@ -632,6 +634,7 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
         mInputManager.unregisterInputDeviceListener(mInputDeviceListener);
         updateIsEnabled();
         mUserTracker.removeCallback(mUserChangedCallback);
+        mTunerService.removeTunable(this);
     }
 
     /**
