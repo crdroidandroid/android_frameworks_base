594e9ddef22b00e2dd5fb3426b4987b4e2c7204a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-15 09:56:17 +0530

    SystemUI: Allow setting end section in keyguard slices
    Move to a new row (line) when we encounter end of section.
    
    Change-Id: I895416773e7eb9ba98dcfd62182ebe657d17d112
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -265,6 +265,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
             if (mediaIconCompat != null) {
                 albumBuilder.addEndItem(mediaIconCompat, ListBuilder.ICON_IMAGE);
             }
+            albumBuilder.setEndOfSection(true);
 
             listBuilder.addRow(albumBuilder);
         }
@@ -290,7 +291,8 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 R.drawable.ic_access_alarms_big);
         RowBuilder alarmRowBuilder = new RowBuilder(mAlarmUri)
                 .setTitle(mNextAlarm)
-                .addEndItem(alarmIcon, ListBuilder.ICON_IMAGE);
+                .addEndItem(alarmIcon, ListBuilder.ICON_IMAGE)
+                .setEndOfSection(true);
         builder.addRow(alarmRowBuilder);
     }
 
@@ -307,7 +309,8 @@ public class KeyguardSliceProvider extends SliceProvider implements
                         .getString(R.string.accessibility_quick_settings_dnd))
                 .addEndItem(
                     IconCompat.createWithResource(getContext(), R.drawable.stat_sys_dnd),
-                    ListBuilder.ICON_IMAGE);
+                    ListBuilder.ICON_IMAGE)
+                .setEndOfSection(true);
         builder.addRow(dndBuilder);
     }
 
