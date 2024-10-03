6e71e16e5bbfee7496493dc8d245a74f7dcabe41
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-03 23:34:10 +0300

    base: Allow disabling turbulence noise after clicking the play button
    Having the turbulence noise play causes frame drops on mid to lower-end devices.
    To work around this, allow disabling it through an overlay.
    
    [basamaryan: Only disable the turbulence noise and kill the toggle]
    
    Change-Id: If03f9ce6e4cb11805ef1edd484702367859933fe
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
@@ -1180,7 +1180,9 @@ public class MediaControlPanel {
     }
 
     private boolean shouldPlayTurbulenceNoise() {
-        return mButtonClicked && !mWasPlaying && isPlaying();
+        boolean isTurbulenceNoiseEnabled = mContext.getResources().getBoolean(
+                com.android.systemui.res.R.bool.config_turbulenceNoise);
+        return mButtonClicked && !mWasPlaying && isPlaying() && isTurbulenceNoiseEnabled;
     }
 
     private TurbulenceNoiseAnimationConfig createTurbulenceNoiseConfig() {
