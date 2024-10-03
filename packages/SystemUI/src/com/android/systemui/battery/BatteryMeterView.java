c5a4e25a65dc40c52d56354460f27d1bfab3ed03
Pranav Vashi <neobuddy89@gmail.com>
2024-10-03 14:54:35 +0530

    BatteryStyles: Handle visibility from controller only
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -178,7 +178,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         }
 
         updatePercentView();
-        updateVisibility();
         mDualToneHandler = new DualToneHandler(context);
         // Init to not dark at all.
         if (isNightMode()) {
@@ -236,7 +235,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         updateDrawable();
         scaleBatteryMeterViews();
         updatePercentView();
-        updateVisibility();
     }
 
     public void setBatteryPercent(int showBatteryPercent) {
@@ -659,16 +657,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         }
     }
 
-    private void updateVisibility() {
-        if (mBatteryStyle == BATTERY_STYLE_HIDDEN || mBatteryStyle == BATTERY_STYLE_TEXT) {
-            mBatteryIconView.setVisibility(View.GONE);
-            mBatteryIconView.setImageDrawable(null);
-        } else {
-            mBatteryIconView.setVisibility(View.VISIBLE);
-            scaleBatteryMeterViews();
-        }
-    }
-
     private Drawable getUnknownStateDrawable() {
         if (mUnknownStateDrawable == null) {
             mUnknownStateDrawable = mContext.getDrawable(R.drawable.ic_battery_unknown);
@@ -809,6 +797,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
                 break;
             case BATTERY_STYLE_HIDDEN:
             case BATTERY_STYLE_TEXT:
+                mBatteryIconView.setImageDrawable(null);
                 break;
         }
     }
