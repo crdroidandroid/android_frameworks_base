4b575dcebfbef8d2362938d4d210b50fd71cdc53
Pranav Vashi <neobuddy89@gmail.com>
2024-10-06 01:24:30 +0530

    SystemUI: Allow to toggle BT directly with Bluetooth tile [1/2]
    * We do not want to lose default behavior that matches with the internet tile
      The default behavior is to show dialog.
    * However, disabling dialog - will match the behavior with the wifi tile.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -29,6 +29,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.HandlerExecutor;
 import android.os.Looper;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
@@ -159,7 +160,9 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
     }
 
     private void handleClickEvent(@Nullable Expandable expandable) {
-        if (mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG)) {
+        boolean showDialog = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_BT_SHOW_DIALOG, 1, UserHandle.USER_CURRENT) != 0;
+        if (showDialog && mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG)) {
             mDetailsContentViewModel.get().showDialog(expandable);
         } else {
             // Secondary clicks are header clicks, just toggle.
@@ -257,7 +260,9 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
         }
 
         state.expandedAccessibilityClassName = Button.class.getName();
-        state.forceExpandIcon = mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG);
+        boolean showDialog = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_BT_SHOW_DIALOG, 1, UserHandle.USER_CURRENT) != 0;
+        state.forceExpandIcon = showDialog && mFeatureFlags.isEnabled(Flags.BLUETOOTH_QS_TILE_DIALOG);
     }
 
     private void toggleBluetooth() {
