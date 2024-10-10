0954748a71451ce336596df9266840eff848b6ea
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-10 21:46:17 +0300

    KeyguardClockSwitchController: Prevent an NPE on early boot
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
@@ -443,7 +443,7 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
     }
 
     private void addWeatherView() {
-        if (MigrateClocksToBlueprint.isEnabled()) {
+        if (MigrateClocksToBlueprint.isEnabled() || mDateWeatherView == null) {
             return;
         }
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
@@ -757,6 +757,9 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
     }
 
     private void removeViewsFromStatusArea() {
+        if (mStatusArea == null) {
+            return;
+        }
         for  (int i = mStatusArea.getChildCount() - 1; i >= 0; i--) {
             final View childView = mStatusArea.getChildAt(i);
             if (childView.getTag(R.id.tag_smartspace_view) != null) {
