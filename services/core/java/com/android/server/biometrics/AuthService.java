d39fed5f68717afdfc4687ec6884a6bd7677a130
Pranav Vashi <neobuddy89@gmail.com>
2024-10-14 17:04:40 +0530

    services: Force load FaceSense providers
    * Current AOSP implementation checks if HIDL/AIDL configs available and
      skips registering providers if configs not available.
    * Fix NPE case in BiometricScheduler by replicating old path.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/AuthService.java
+++ services/core/java/com/android/server/biometrics/AuthService.java
@@ -72,6 +72,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.ArrayUtils;
 import com.android.server.SystemService;
 import com.android.server.biometrics.sensors.face.FaceService;
+import com.android.server.biometrics.sensors.face.sense.SenseUtils;
 import com.android.server.biometrics.sensors.fingerprint.FingerprintService;
 import com.android.server.companion.virtual.VirtualDeviceManagerInternal;
 
@@ -838,7 +839,8 @@ public class AuthService extends SystemService {
             final String[] hidlConfigStrings, final Context context,
             final IFaceService faceService, final BiometricHandlerProvider handlerProvider) {
         if ((hidlConfigStrings == null || hidlConfigStrings.length == 0)
-                && (faceAidlInstances == null || faceAidlInstances.length == 0)) {
+                && (faceAidlInstances == null || faceAidlInstances.length == 0)
+                && !SenseUtils.canUseProvider()) {
             Slog.d(TAG, "No face sensors.");
             return;
         }
