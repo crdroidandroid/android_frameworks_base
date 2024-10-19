a6d12d2a70f5fd7bd9728ecf0fad807dd16c0219
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-19 13:01:37 +0800

    AppLock: Fix service initialization
    test: boot, app lock works, no ClassNotFound exceptions during bootup
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -121,6 +121,7 @@ import com.android.server.adb.AdbService;
 import com.android.server.alarm.AlarmManagerService;
 import com.android.server.am.ActivityManagerService;
 import com.android.server.ambientcontext.AmbientContextManagerService;
+import com.android.server.app.AppLockManagerService;
 import com.android.server.app.GameManagerService;
 import com.android.server.appbinding.AppBindingService;
 import com.android.server.appfunctions.AppFunctionManagerService;
@@ -450,9 +451,6 @@ public final class SystemServer implements Dumpable {
     private static final String RANGING_SERVICE_CLASS = "com.android.server.ranging.RangingService";
 
     private static final String TETHERING_CONNECTOR_CLASS = "android.net.ITetheringConnector";
-
-    private static final String APP_LOCK_SERVICE_CLASS =
-            "com.android.server.app.AppLockManagerService$Lifecycle";
 
     private static final String PERSISTENT_DATA_BLOCK_PROP = "ro.frp.pst";
 
@@ -2656,7 +2654,7 @@ public final class SystemServer implements Dumpable {
             }
 
             t.traceBegin("AppLockManagerService");
-            mSystemServiceManager.startService(APP_LOCK_SERVICE_CLASS);
+            mSystemServiceManager.startService(AppLockManagerService.Lifecycle.class);
             t.traceEnd();
 
             if (!isWatch) {
