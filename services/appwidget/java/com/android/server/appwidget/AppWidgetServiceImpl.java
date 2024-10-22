e9e17d12b8d2e9c838c082ee2eaca1e381a2242e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-22 20:25:38 +0300

    don't remove app widgets from user's home screen when the user stops
    onUserStopped() method name was misleading, it's called from onUserStopping(), while the user is
    still running.
    
    Initial research was done by maade93791 <70593890+maade69@users.noreply.github.com>
    
    Co-authored-by: maade93791 <70593890+maade69@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/appwidget/java/com/android/server/appwidget/AppWidgetServiceImpl.java
+++ services/appwidget/java/com/android/server/appwidget/AppWidgetServiceImpl.java
@@ -4498,7 +4498,7 @@ class AppWidgetServiceImpl extends IAppWidgetService.Stub implements WidgetBacku
         return new AtomicFile(new File(Environment.getUserSystemDirectory(userId), STATE_FILENAME));
     }
 
-    void onUserStopped(int userId) {
+    void onUserStopping(int userId) {
         if (DEBUG) {
             Slog.i(TAG, "onUserStopped() " + userId);
         }
@@ -4518,6 +4518,10 @@ class AppWidgetServiceImpl extends IAppWidgetService.Stub implements WidgetBacku
                 // as we do not want to make host callbacks and provider broadcasts
                 // as the host and the provider will be killed.
                 if (hostInUser && (!hasProvider || providerInUser)) {
+                    // The user's app widget host (i.e. the launcher) is usually still running at
+                    // this point. Don't notify it that the widget is removed to avoid affecting
+                    // homescreen configuration
+                    widget.host.callbacks = null;
                     removeWidgetLocked(widget);
                     widget.host.widgets.remove(widget);
                     widget.host = null;
