4a62f11bfe66f132cea21b4a28441531dfe1e5ed
yangjing24 <yangjing24@xiaomi.com>
2024-10-24 09:50:03 +0800

    remove ScrollCache when Activity destroyed
    Test: Manual test
    Bug:374925259
    
    Change-Id: Ifbb20a003863b8e0f472c9fc6af69c419aa29253
    Signed-off-by: yangjing24 <yangjing24@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -22906,6 +22906,11 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
         cleanupDraw();
         mCurrentAnimation = null;
 
+        if (mScrollCache != null && mScrollCache.state == ScrollabilityCache.ON
+                && mAttachInfo != null) {
+            mAttachInfo.mHandler.removeCallbacks(mScrollCache);
+        }
+
         if ((mViewFlags & TOOLTIP) == TOOLTIP) {
             removeCallbacks(mTooltipInfo.mShowTooltipRunnable);
             removeCallbacks(mTooltipInfo.mHideTooltipRunnable);
