63ddd8b34a6b1cc134b933e673dc9744bb3a5786
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-30 16:36:01 +0200

    SystemUI: biometrics: Add missing calls to parent onFinishInflate
    you should always call that.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/ui/view/UdfpsTouchOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/ui/view/UdfpsTouchOverlay.kt
@@ -42,6 +42,7 @@ class UdfpsTouchOverlay(context: Context, attrs: AttributeSet?) : FrameLayout(co
         private set
 
     override fun onFinishInflate() {
+        super.onFinishInflate()
         ghbmView = findViewById(R.id.hbm_view)
     }
 
