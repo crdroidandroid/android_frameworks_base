9ee2b860bbf52c21f0d6835bb0884bad8e4d376b
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-01 13:08:48 +0800

    Media: Limit Max album artwork bitmap size
    * AOSP allows up to 320dp max width/height for media bitmaps, which can result in bitmaps growing up to 1280px on high-resolution devices.
      According to our tests, setting album art for a song to 1000 x 1000 significantly increases bitmap memory and CPU usage,
      resulting in performance degradation after media with high-quality artwork is played.
      By setting the album art size to 500 x 500 manually, QS expansion becomes smoother.
      To avoid this regression, we limit the media panel artwork size and MediaSession bitmap size to 500 x 500 instead of relying
      on config_mediaMetadataBitmapMaxSize, as it may be either too low for low-resolution devices or too high for high-resolution
      devices if defined in dp. Therefore, we hardcode the max bitmap size to 500px, regardless of density or resolution.
    
    * Fixes the well-known QS panel lag/jank issue on AOSP-based ROMs.
    
    test:
    
    1. Set song album art resolution to 1000 x 1000 using poweramp.
    2. Play the song and expand QS.
    
    Result:
    Before this patch:
    Qs fling animation is smooth after boot but after a media playback,
    QS fling animation becomes janky, unless the media art work is low-resolution.
    QS fling animation remains janky even after pausing/stopping media playback and hiding QS media panel.
    
    After this patch:
    No jank or lag observed while playing/pausing media.
    QS media panel and lock screen media artwork quality is not pixelated/low/poor.
    
    This change is based on RemoteController's legacy behaviour in limiting bitmap size:
    https://cs.android.com/android/platform/superproject/main/+/main:frameworks/base/media/java/android/media/RemoteController.java;l=60?q=MAX_BITMAP_DIMENSION
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/domain/pipeline/LegacyMediaDataManagerImpl.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/domain/pipeline/LegacyMediaDataManagerImpl.kt
@@ -240,12 +240,10 @@ class LegacyMediaDataManagerImpl(
     @Keep private var smartspaceSession: SmartspaceSession? = null
     private var allowMediaRecommendations = allowMediaRecommendations(context)
 
-    private val artworkWidth =
-        context.resources.getDimensionPixelSize(
-            com.android.internal.R.dimen.config_mediaMetadataBitmapMaxSize
-        )
-    private val artworkHeight =
-        context.resources.getDimensionPixelSize(R.dimen.qs_media_session_height_expanded)
+    private val artworkWidth = context.getResources().getInteger(
+                        com.android.internal.R.integer.config_maxBitmapSizePx)
+    private val artworkHeight = context.getResources().getInteger(
+                        com.android.internal.R.integer.config_maxBitmapSizePx)
 
     @SuppressLint("WrongConstant") // sysui allowed to call STATUS_BAR_SERVICE
     private val statusBarManager =
