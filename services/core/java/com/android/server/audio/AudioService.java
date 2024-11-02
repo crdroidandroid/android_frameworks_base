b26cf323b681a03fab9cdfdd480918f724ad8f81
gang.huang <gang.huang@unisoc.com>
2024-11-02 12:16:55 +0800

    Ignore null action in AudioService.
    Test: Manual.
    Google: 3327900
    Change-Id: I50bef0d76bf85d8032a129acddd278ed92c6ee9b

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -12722,6 +12722,9 @@ public class AudioService extends IAudioService.Stub
             @Override
             public void onReceive(Context context, Intent intent) {
                 String action = intent.getAction();
+                if (action == null) {
+                    return;
+                }
                 String pkgName = intent.getData().getEncodedSchemeSpecificPart();
                 int uid = intent.getIntExtra(Intent.EXTRA_UID, Process.INVALID_UID);
                 if (intent.getBooleanExtra(EXTRA_REPLACING, false) ||
