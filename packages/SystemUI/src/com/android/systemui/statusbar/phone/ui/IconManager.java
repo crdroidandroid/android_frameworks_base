d8c3f0328c02537e805e50f2693a5b07806b89eb
Pranav Vashi <neobuddy89@gmail.com>
2024-11-09 12:15:55 +0530

    NetworkTraffic: Clean up and move settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
@@ -36,8 +36,8 @@ import com.android.systemui.demomode.DemoModeCommandReceiver;
 import com.android.systemui.modes.shared.ModesUiIcons;
 import com.android.systemui.res.R;
 import com.android.systemui.statusbar.BaseStatusBarFrameLayout;
+import com.android.systemui.statusbar.NetworkTraffic;
 import com.android.systemui.statusbar.StatusBarIconView;
-import com.android.systemui.statusbar.StatusBarNetworkTraffic;
 import com.android.systemui.statusbar.StatusIconDisplayable;
 import com.android.systemui.statusbar.connectivity.ui.MobileContextProvider;
 import com.android.systemui.statusbar.phone.DemoStatusIcons;
@@ -230,8 +230,8 @@ public class IconManager implements DemoModeCommandReceiver {
         return view;
     }
 
-    private StatusBarNetworkTraffic addNetworkTraffic(int index, String slot, NetworkTrafficState state) {
-        StatusBarNetworkTraffic view = onCreateStatusBarNetworkTraffic(slot);
+    private NetworkTraffic addNetworkTraffic(int index, String slot, NetworkTrafficState state) {
+        NetworkTraffic view = onCreateNetworkTraffic(slot);
         view.applyNetworkTrafficState(state);
         mGroup.addView(view, index, onCreateLayoutParams(Shape.WRAP_CONTENT));
         return view;
@@ -257,8 +257,8 @@ public class IconManager implements DemoModeCommandReceiver {
                 );
     }
 
-    private StatusBarNetworkTraffic onCreateStatusBarNetworkTraffic(String slot) {
-        StatusBarNetworkTraffic view = StatusBarNetworkTraffic.fromContext(mContext, slot);
+    private NetworkTraffic onCreateNetworkTraffic(String slot) {
+        NetworkTraffic view = NetworkTraffic.fromContext(mContext, slot);
         return view;
     }
 
@@ -319,7 +319,7 @@ public class IconManager implements DemoModeCommandReceiver {
     }
 
     public void onSetNetworkTraffic(int viewIndex, NetworkTrafficState state) {
-        StatusBarNetworkTraffic view = (StatusBarNetworkTraffic) mGroup.getChildAt(viewIndex);
+        NetworkTraffic view = (NetworkTraffic) mGroup.getChildAt(viewIndex);
         if (view != null) {
             view.applyNetworkTrafficState(state);
         }
