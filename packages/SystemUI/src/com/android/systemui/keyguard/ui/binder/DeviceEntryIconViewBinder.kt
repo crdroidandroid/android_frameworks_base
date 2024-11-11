038a5bcc46e476299cd490126a83ce904c80670b
Pranav Vashi <neobuddy89@gmail.com>
2024-11-11 05:49:04 +0530

    Add support for UDFPS icons
    * This is complete rewrite basis UDFPS refactor. Cleaner and without hacks.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/DeviceEntryIconViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/DeviceEntryIconViewBinder.kt
@@ -29,7 +29,9 @@ import androidx.core.view.isInvisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.repeatOnLifecycle
 import com.android.app.tracing.coroutines.launchTraced as launch
+import com.android.systemui.Dependency
 import com.android.systemui.Flags
+import com.android.systemui.biometrics.UdfpsIconDrawable
 import com.android.systemui.common.ui.view.TouchHandlingView
 import com.android.systemui.keyguard.ui.view.DeviceEntryIconView
 import com.android.systemui.keyguard.ui.viewmodel.DeviceEntryBackgroundViewModel
@@ -39,15 +41,27 @@ import com.android.systemui.lifecycle.repeatWhenAttached
 import com.android.systemui.plugins.FalsingManager
 import com.android.systemui.res.R
 import com.android.systemui.statusbar.VibratorHelper
+import com.android.systemui.tuner.TunerService
 import com.android.systemui.util.kotlin.DisposableHandles
 import com.google.android.msdl.data.model.MSDLToken
 import com.google.android.msdl.domain.MSDLPlayer
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DisposableHandle
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.SharingStarted
+import android.provider.Settings
+import android.os.UserHandle
+import com.android.internal.util.crdroid.Utils
 
 object DeviceEntryIconViewBinder {
     private const val TAG = "DeviceEntryIconViewBinder"
+
+    private final val UDFPS_ICON: String =
+            "system:" + Settings.System.UDFPS_ICON
 
     /**
      * Updates UI for:
@@ -70,6 +84,27 @@ object DeviceEntryIconViewBinder {
         msdlPlayer: MSDLPlayer,
         overrideColor: Color? = null,
     ): DisposableHandle {
+        val packageInstalled = Utils.isPackageInstalled(
+            view.context, "com.crdroid.udfps.icons"
+        )
+
+        val shouldUseCustomUdfpsIcon: StateFlow<Boolean> = callbackFlow {
+            val callback = object : TunerService.Tunable {
+                override fun onTuningChanged(key: String, newValue: String?) {
+                    if (key == UDFPS_ICON) {
+                        trySend(TunerService.parseIntegerSwitch(newValue, false)).isSuccess
+                    }
+                }
+            }
+            Dependency.get(TunerService::class.java).addTunable(callback, UDFPS_ICON)
+
+            awaitClose { Dependency.get(TunerService::class.java).removeTunable(callback) }
+        }.stateIn(
+            scope = applicationScope,
+            started = SharingStarted.Eagerly,
+            initialValue = false
+        )
+
         val disposables = DisposableHandles()
         val touchHandlingView = view.touchHandlingView
         val fgIconView = view.iconView
@@ -108,10 +143,19 @@ object DeviceEntryIconViewBinder {
                 repeatOnLifecycle(Lifecycle.State.CREATED) {
                     launch("$TAG#viewModel.useBackgroundProtection") {
                         viewModel.useBackgroundProtection.collect { useBackgroundProtection ->
-                            if (useBackgroundProtection) {
-                                bgView.visibility = View.VISIBLE
-                            } else {
+                            if (shouldUseCustomUdfpsIcon.value && packageInstalled) {
                                 bgView.visibility = View.GONE
+                            } else {
+                                bgView.visibility = if (useBackgroundProtection) View.VISIBLE else View.GONE
+                            }
+                        }
+                    }
+                    launch("$TAG#shouldUseCustomUdfpsIcon") {
+                        shouldUseCustomUdfpsIcon.collect { useCustomIcon ->
+                            if (useCustomIcon && packageInstalled) {
+                                bgView.visibility = View.GONE
+                            } else {
+                                bgView.visibility = if (viewModel.useBackgroundProtection.value) View.VISIBLE else View.GONE
                             }
                         }
                     }
@@ -229,12 +273,16 @@ object DeviceEntryIconViewBinder {
                             }
                             fgIconView.imageTintList =
                                 ColorStateList.valueOf(overrideColor?.toArgb() ?: viewModel.tint)
-                            fgIconView.setPadding(
-                                viewModel.padding,
-                                viewModel.padding,
-                                viewModel.padding,
-                                viewModel.padding,
-                            )
+                            if (fgIconView.drawable.current !is UdfpsIconDrawable) {
+                                fgIconView.setPadding(
+                                    viewModel.padding,
+                                    viewModel.padding,
+                                    viewModel.padding,
+                                    viewModel.padding
+                                )
+                            } else {
+                                fgIconView.setPadding(0, 0, 0, 0)
+                            }
                             // Set image state at the end after updating other view state. This
                             // method forces the ImageView to recompute the bounds of the drawable.
                             fgIconView.setImageState(
@@ -256,7 +304,11 @@ object DeviceEntryIconViewBinder {
                     }
                     launch("$TAG#bgViewModel.color") {
                         bgViewModel.color.collect { color ->
+                            if (!shouldUseCustomUdfpsIcon.value || !packageInstalled) {
                             bgView.imageTintList = ColorStateList.valueOf(color)
+                            } else {
+                                bgView.imageTintList = null
+                            }
                         }
                     }
                 }
