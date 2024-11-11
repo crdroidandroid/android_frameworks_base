84d6552952a83bdfb88ed83453ce8483d07ae8f3
Pranav Vashi <neobuddy89@gmail.com>
2024-11-11 05:49:04 +0530

    Add support for UDFPS icons
    * This is complete rewrite basis UDFPS refactor. Cleaner and without hacks.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/AlternateBouncerViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/AlternateBouncerViewBinder.kt
@@ -44,6 +44,7 @@ import com.android.systemui.log.TouchHandlingViewLogger
 import com.android.systemui.res.R
 import com.android.systemui.scene.shared.flag.SceneContainerFlag
 import com.android.systemui.scrim.ScrimView
+import com.android.systemui.tuner.TunerService
 import dagger.Lazy
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
@@ -64,6 +65,7 @@ constructor(
     private val alternateBouncerDependencies: Lazy<AlternateBouncerDependencies>,
     private val windowManager: Lazy<WindowManager>,
     private val layoutInflater: Lazy<LayoutInflater>,
+    private val tunerService: TunerService,
 ) : CoreStartable {
 
     private var alternateBouncerView: ConstraintLayout? = null
@@ -248,7 +250,7 @@ constructor(
                         var udfpsView = view.getViewById(udfpsViewId)
                         if (udfpsView == null) {
                             udfpsView =
-                                DeviceEntryIconView(view.context, null, logger = logger).apply {
+                                DeviceEntryIconView(view.context, null, 0, logger, tunerService).apply {
                                     id = udfpsViewId
                                     contentDescription =
                                         context.resources.getString(
@@ -256,7 +258,7 @@ constructor(
                                         )
                                 }
                             view.addView(udfpsView)
-                            AlternateBouncerUdfpsViewBinder.bind(udfpsView, udfpsIconViewModel)
+                            AlternateBouncerUdfpsViewBinder.bind(applicationScope, udfpsView, udfpsIconViewModel)
                         }
 
                         val constraintSet = ConstraintSet().apply { clone(view) }
