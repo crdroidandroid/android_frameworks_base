2cb7c944a584f1288a5864c88d22f22e7b686d49
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-18 21:24:57 +0100

    Make default permissions updates tied to version incremental
    Otherwise adding new rules won't have any effect for us.
    
    Change-Id: Ib81082a184956ea550bdccdbdf53b2b4cc48f487

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4308,7 +4308,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             final int userId = livingUsers.get(i).id;
             final boolean isPermissionUpgradeNeeded = !Objects.equals(
                     mPermissionManager.getDefaultPermissionGrantFingerprint(userId),
-                    Build.FINGERPRINT);
+                    Build.VERSION.INCREMENTAL);
             if (isPermissionUpgradeNeeded) {
                 grantPermissionsUserIds = ArrayUtils.appendInt(
                         grantPermissionsUserIds, userId);
@@ -4317,7 +4317,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         // If we upgraded grant all default permissions before kicking off.
         for (int userId : grantPermissionsUserIds) {
             mLegacyPermissionManager.grantDefaultPermissions(userId);
-            mPermissionManager.setDefaultPermissionGrantFingerprint(Build.FINGERPRINT, userId);
+            mPermissionManager.setDefaultPermissionGrantFingerprint(
+                    Build.VERSION.INCREMENTAL, userId);
         }
         if (grantPermissionsUserIds == EMPTY_INT_ARRAY) {
             // If we did not grant default permissions, we preload from this the
@@ -4481,7 +4482,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         if (!convertedFromPreCreated || !readPermissionStateForUser(userId)) {
             mPermissionManager.onUserCreated(userId);
             mLegacyPermissionManager.grantDefaultPermissions(userId);
-            mPermissionManager.setDefaultPermissionGrantFingerprint(Build.FINGERPRINT, userId);
+            mPermissionManager.setDefaultPermissionGrantFingerprint(
+                    Build.VERSION.INCREMENTAL, userId);
             mDomainVerificationManager.clearUser(userId);
         }
     }
@@ -4493,7 +4495,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             mPermissionManager.readLegacyPermissionStateTEMP();
             final boolean isPermissionUpgradeNeeded = !Objects.equals(
                     mPermissionManager.getDefaultPermissionGrantFingerprint(userId),
-                    Build.FINGERPRINT);
+                    Build.VERSION.INCREMENTAL);
             return isPermissionUpgradeNeeded;
         }
     }
