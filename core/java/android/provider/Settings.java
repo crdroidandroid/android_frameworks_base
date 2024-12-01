ee77ecaa6398b25ddab8bd7f71435f4971baf0f4
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-01 15:15:35 +0900

    Settings: Expose clipboard auto clear setting [1/3]
    [someone5678]
    * Adapt to current project
    * Use Settings instead of DeviceConfig as GMS don't likes it
    
    Ref:
    https://gitlab.com/CalyxOS/platform_packages_apps_Settings/-/commit/72db57c966dd996cb7a9106e52a6416c589df92d
    https://github.com/crdroidandroid/android_packages_apps_Settings/commit/48e00e2b812db5ea85253edaced9b1fc48db0efd
    https://github.com/crdroidandroid/android_packages_apps_crDroidSettings/commit/33c49aa70c9250d64b284af6ee46d5dad618497c
    https://github.com/crdroidandroid/android_packages_apps_crDroidSettings/commit/bc81eea9cce7d964b2a3f64ae318cd8d0c840adb
    
    Issue: calyxos#2208
    Change-Id: Ib6b62b5bc97473e192ed3dbdb4feadd868e3bdf7
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14254,6 +14254,18 @@ public final class Settings {
         public static final String SMART_PIXEL_FILTER_PERCENT = "smart_pixel_filter_percent";
 
         /**
+         * Whether to enable clipboard auto clear
+         * @hide
+         */
+        public static final String CLIPBOARD_AUTO_CLEAR_ENABLED = "clipboard_auto_clear_enabled";
+
+        /**
+         * Timeout length for clipboard auto clear
+         * @hide
+         */
+        public static final String CLIPBOARD_AUTO_CLEAR_TIMEOUT = "clipboard_auto_clear_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
