1878b3875c8ab6e9c4655bced9b03fc070e7cb9d
John Galt <johngaltfirstrun@gmail.com>
2024-12-11 09:24:05 -0500

    UdfpsController: add LAUNCH boost
    Based on logic from
    https://github.com/AOSPA/android_frameworks_base/commit/94cb933df8fa0cf2eb55c78e72f14b30f690e066
    which used qcom perf framework opposed to cleaner libperfmgr.
    
    Massively improves performance on OP12, should improve performance on
    virtually any device with libperfmgr and a powerhint that has
    significant LAUNCH boosts.

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -55,6 +55,7 @@ import android.os.CancellationSignal;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
+import android.os.PowerManagerInternal;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.VibrationAttributes;
@@ -78,6 +79,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.util.LatencyTracker;
 import com.android.keyguard.KeyguardUpdateMonitor;
+import com.android.server.LocalServices;
 import com.android.systemui.Dumpable;
 import com.android.systemui.animation.ActivityTransitionAnimator;
 import com.android.systemui.biometrics.AuthController;
@@ -244,6 +246,8 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     private boolean mSmartPixelsFlag;
     private boolean mSmartPixelsEnabled;
     private boolean mSmartPixelsOnPowerSave;
+
+    PowerManagerInternal mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
 
     @VisibleForTesting
     public static final VibrationAttributes UDFPS_VIBRATION_ATTRIBUTES =
@@ -1250,6 +1254,9 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                     + " current: " + mOverlay.getRequestId());
             return;
         }
+        if (mPowerManagerInternal != null) {
+            mPowerManagerInternal.setPowerMode(PowerManagerInternal.MODE_LAUNCH, true);
+        }
         if (mDisableSmartPixels) {
             if (!mSmartPixelsFlag && (mSmartPixelsEnabled || mSmartPixelsOnPowerSave)) {
                 disableSmartPixels();
@@ -1345,6 +1352,10 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mOnFingerDown = false;
         unconfigureDisplay(view);
         cancelAodSendFingerUpAction();
+        if (mPowerManagerInternal != null) {
+            mPowerManagerInternal.setPowerMode(PowerManagerInternal.MODE_LAUNCH, false);
+        }
+
     }
 
     public boolean isAnimationEnabled() {
