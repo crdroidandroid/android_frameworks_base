ceed8fd586c6436f041869b25b3ba8e9cd80ab73
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-15 11:35:46 +0900

    SystemUI: ConfigurationControllerImpl: Avoid NullPointerException
    * As listener could be null, apply filterNotNull to avoid it
    
    Log:
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: FATAL EXCEPTION: main
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: Process: com.android.systemui, PID: 3663
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke interface method 'void com.android.systemui.statusbar.policy.ConfigurationController$ConfigurationListener.onConfigChanged(android.content.res.Configuration)' on a null object reference
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at com.android.systemui.statusbar.phone.ConfigurationControllerImpl.onConfigurationChanged(ConfigurationControllerImpl.kt:74)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at com.android.systemui.SystemUIApplication.onConfigurationChanged(SystemUIApplication.java:449)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.ConfigurationController.performConfigurationChanged(ConfigurationController.java:242)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.ConfigurationController.handleConfigurationChanged(ConfigurationController.java:216)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.ConfigurationController.handleConfigurationChanged(ConfigurationController.java:128)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.ActivityThread.handleConfigurationChanged(ActivityThread.java:6520)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.servertransaction.ConfigurationChangeItem.execute(ConfigurationChangeItem.java:48)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.servertransaction.TransactionExecutor.executeNonLifecycleItem(TransactionExecutor.java:231)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.servertransaction.TransactionExecutor.executeTransactionItems(TransactionExecutor.java:152)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.servertransaction.TransactionExecutor.execute(TransactionExecutor.java:93)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2595)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:107)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:232)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:317)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8597)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:583)
    12-14 17:58:13.733  3663  3663 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:878)
    
    Change-Id: I1b5197667f7837dcabd67204ae6b764113dbbcce
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ConfigurationControllerImpl.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ConfigurationControllerImpl.kt
@@ -62,19 +62,19 @@ constructor(@Assisted private val context: Context) :
         // Avoid concurrent modification exception
         val listeners = synchronized(this.listeners) { ArrayList(this.listeners) }
 
-        listeners.filterForEach({ this.listeners.contains(it) }) { it.onThemeChanged() }
+        listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) { it.onThemeChanged() }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         // Avoid concurrent modification exception
         val listeners = synchronized(this.listeners) { ArrayList(this.listeners) }
-        listeners.filterForEach({ this.listeners.contains(it) }) { it.onConfigChanged(newConfig) }
+        listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) { it.onConfigChanged(newConfig) }
         val fontScale = newConfig.fontScale
         val density = newConfig.densityDpi
         val uiMode = newConfig.uiMode and Configuration.UI_MODE_NIGHT_MASK
         val uiModeChanged = uiMode != this.uiMode
         if (density != this.density || fontScale != this.fontScale || inCarMode && uiModeChanged) {
-            listeners.filterForEach({ this.listeners.contains(it) }) {
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) {
                 it.onDensityOrFontScaleChanged()
             }
             this.density = density
@@ -84,7 +84,7 @@ constructor(@Assisted private val context: Context) :
         val smallestScreenWidth = newConfig.smallestScreenWidthDp
         if (smallestScreenWidth != this.smallestScreenWidth) {
             this.smallestScreenWidth = smallestScreenWidth
-            listeners.filterForEach({ this.listeners.contains(it) }) {
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) {
                 it.onSmallestScreenWidthChanged()
             }
         }
@@ -96,13 +96,13 @@ constructor(@Assisted private val context: Context) :
             // would be a direct reference to windowConfiguration.maxBounds, so the if statement
             // above would always fail. See b/245799099 for more information.
             this.maxBounds.set(maxBounds)
-            listeners.filterForEach({ this.listeners.contains(it) }) { it.onMaxBoundsChanged() }
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) { it.onMaxBoundsChanged() }
         }
 
         val localeList = newConfig.locales
         if (localeList != this.localeList) {
             this.localeList = localeList
-            listeners.filterForEach({ this.listeners.contains(it) }) { it.onLocaleListChanged() }
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) { it.onLocaleListChanged() }
         }
 
         if (uiModeChanged) {
@@ -111,24 +111,24 @@ constructor(@Assisted private val context: Context) :
             context.theme.applyStyle(context.themeResId, true)
 
             this.uiMode = uiMode
-            listeners.filterForEach({ this.listeners.contains(it) }) { it.onUiModeChanged() }
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) { it.onUiModeChanged() }
         }
 
         if (layoutDirection != newConfig.layoutDirection) {
             layoutDirection = newConfig.layoutDirection
-            listeners.filterForEach({ this.listeners.contains(it) }) {
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) {
                 it.onLayoutDirectionChanged(layoutDirection == LAYOUT_DIRECTION_RTL)
             }
         }
 
         if (lastConfig.updateFrom(newConfig) and ActivityInfo.CONFIG_ASSETS_PATHS != 0) {
-            listeners.filterForEach({ this.listeners.contains(it) }) { it.onThemeChanged() }
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) { it.onThemeChanged() }
         }
 
         val newOrientation = newConfig.orientation
         if (orientation != newOrientation) {
             orientation = newOrientation
-            listeners.filterForEach({ this.listeners.contains(it) }) {
+            listeners.filterForEach({ this.listeners.filterNotNull().contains(it) }) {
                 it.onOrientationChanged(orientation)
             }
         }
