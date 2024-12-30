5b3040605bf1c033e59d63bdec7cf3621d987a92
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-30 09:51:07 +0800

    Fix SystemUI crash when loading album art color
    -log:
    
    --------- beginning of crash
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: FATAL EXCEPTION: SysUiBg
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: Process: com.android.systemui, PID: 2094
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: java.lang.ArrayIndexOutOfBoundsException: length=0; index=8984
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.internal.graphics.palette.WuQuantizer.constructHistogram(WuQuantizer.java:135)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.internal.graphics.palette.WuQuantizer.quantize(WuQuantizer.java:87)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.internal.graphics.palette.CelebiQuantizer.quantize(CelebiQuantizer.java:45)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.internal.graphics.palette.Palette$Builder.generate(Palette.java:344)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at android.app.WallpaperColors.fromBitmap(WallpaperColors.java:231)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at android.app.WallpaperColors.fromBitmap(WallpaperColors.java:185)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.systemui.media.controls.ui.controller.MediaControlPanel.getWallpaperColor(MediaControlPanel.java:1065)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.systemui.media.controls.ui.controller.MediaControlPanel.lambda$bindArtworkAndColors$11(MediaControlPanel.java:909)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.systemui.media.controls.ui.controller.MediaControlPanel.$r8$lambda$1OvqKy4NhfYH72C2YMNL5X53se0(MediaControlPanel.java:0)
    12-30 09:41:20.864  2094  2159 E AndroidRuntime: 	at com.android.systemui.media.controls.ui.controller.MediaControlPanel$$ExternalSyntheticLambda15.run(R8$$SyntheticClass:0)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
@@ -992,7 +992,11 @@ public class MediaControlPanel {
                     Log.d(TAG, "Cannot load wallpaper color from a recycled bitmap");
                     return null;
                 }
-                return WallpaperColors.fromBitmap(artworkBitmap);
+                try {
+                    return WallpaperColors.fromBitmap(artworkBitmap);
+                } catch (Exception e) {
+                    return null;
+                }
             } else {
                 Drawable artworkDrawable = artworkIcon.loadDrawable(mContext);
                 if (artworkDrawable != null) {
