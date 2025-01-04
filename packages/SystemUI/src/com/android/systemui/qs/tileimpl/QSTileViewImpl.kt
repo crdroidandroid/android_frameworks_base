5955e2aebd1b72cce68f0fe3f517312a414249ed
Pranav Vashi <neobuddy89@gmail.com>
2025-01-04 21:37:05 +0530

    SystemUI: Disable longpress effect for A11 QS tile
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -573,7 +573,7 @@ constructor(
     }
 
     override fun init(tile: QSTile) {
-        if (longPressEffect != null) {
+        if (!isA11Style && longPressEffect != null) {
             isHapticFeedbackEnabled = false
             longPressEffect.qsTile = tile
             longPressEffect.createExpandableFromView(this)
@@ -809,7 +809,7 @@ constructor(
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         // let the View run the onTouch logic for click and long-click detection
         val result = super.onTouchEvent(event)
-        if (longPressEffect != null) {
+        if (!isA11Style && longPressEffect != null) {
             when (event?.actionMasked) {
                 MotionEvent.ACTION_DOWN -> {
                     longPressEffect.handleActionDown()
@@ -833,7 +833,7 @@ constructor(
         val allowAnimations = animationsEnabled()
         isClickable = state.state != Tile.STATE_UNAVAILABLE
         isLongClickable = state.handlesLongClick
-        hasLongClickEffect = (state.handlesLongClick && state.hasLongClickEffect)
+        hasLongClickEffect = !isA11Style && state.handlesLongClick && state.hasLongClickEffect
         icon.setIcon(state, allowAnimations)
         contentDescription = state.contentDescription
 
@@ -1147,7 +1147,7 @@ constructor(
 
     override fun onActivityLaunchAnimationEnd() {
         longPressEffect?.resetState()
-        if (longPressEffect != null && !haveLongPressPropertiesBeenReset) {
+        if (!isA11Style && longPressEffect != null && !haveLongPressPropertiesBeenReset) {
             resetLongPressEffectProperties()
         }
     }
