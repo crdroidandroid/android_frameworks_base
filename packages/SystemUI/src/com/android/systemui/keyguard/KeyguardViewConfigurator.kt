71f5ea8ce667d1287a5cbddec9f35c4d18bddcf0
Pranav Vashi <neobuddy89@gmail.com>
2025-01-04 19:53:36 +0530

    SystemUI: Add face unlock icon to keyguard blueprint
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewConfigurator.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewConfigurator.kt
@@ -31,6 +31,7 @@ import com.android.systemui.keyguard.ui.binder.KeyguardJankBinder
 import com.android.systemui.keyguard.ui.binder.KeyguardRootViewBinder
 import com.android.systemui.keyguard.ui.binder.LightRevealScrimViewBinder
 import com.android.systemui.keyguard.ui.view.KeyguardIndicationArea
+import com.android.systemui.keyguard.ui.view.KeyguardIndicationAreaTop
 import com.android.systemui.keyguard.ui.view.KeyguardRootView
 import com.android.systemui.keyguard.ui.viewmodel.KeyguardBlueprintViewModel
 import com.android.systemui.keyguard.ui.viewmodel.KeyguardClockViewModel
@@ -127,6 +128,9 @@ constructor(
     private fun initializeViews() {
         val indicationArea = KeyguardIndicationArea(context, null)
         keyguardIndicationController.setIndicationArea(indicationArea)
+
+        val indicationAreaTop = KeyguardIndicationAreaTop(context, null)
+        keyguardIndicationController.setIndicationAreaTop(indicationAreaTop)
     }
 
     private fun bindKeyguardRootView() {
