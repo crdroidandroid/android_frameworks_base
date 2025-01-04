00ac86f7d906262dc02a286d9107deee6c58dd03
Pranav Vashi <neobuddy89@gmail.com>
2025-01-04 21:09:21 +0530

    SystemUI: Disable secondary click on bluetooth tile conditionally
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -115,8 +115,10 @@ public class BluetoothTile extends QSTileImpl<BooleanState> {
 
     @Override
     public BooleanState newTileState() {
+        boolean showDialog = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_BT_SHOW_DIALOG, 1, UserHandle.USER_CURRENT) != 0;
         BooleanState s = new BooleanState();
-        s.handlesSecondaryClick = true;
+        s.handlesSecondaryClick = showDialog;
         return s;
     }
 
