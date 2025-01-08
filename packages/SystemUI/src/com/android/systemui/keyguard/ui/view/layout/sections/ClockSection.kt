6a80c225a07042169ca3b37733170dd63b7f610b
Pranav Vashi <neobuddy89@gmail.com>
2025-01-09 00:21:31 +0530

    SystemUI: Fix smartspace layout
    Change-Id: If3a3bd1908c566b8a2d9195fbac75b6ecbdf9a9b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/ClockSection.kt
@@ -120,27 +120,7 @@ constructor(
             setAlpha(getTargetClockFace(clock).views, 1F)
             setAlpha(getNonTargetClockFace(clock).views, 0F)
 
-            if (!keyguardClockViewModel.isLargeClockVisible.value) {
-                if (
-                    KeyguardSmartspaceViewModel.dateWeatherBelowSmallClock(
-                        context.resources.configuration
-                    )
-                ) {
-                    connect(
-                        sharedR.id.bc_smartspace_view,
-                        TOP,
-                        sharedR.id.date_smartspace_view,
-                        BOTTOM,
-                    )
-                } else {
-                    connect(
-                        sharedR.id.bc_smartspace_view,
-                        TOP,
-                        customR.id.lockscreen_clock_view,
-                        BOTTOM,
-                    )
-                }
-            } else {
+            if (keyguardClockViewModel.isLargeClockVisible.value) {
                 setScaleX(getTargetClockFace(clock).views, aodBurnInViewModel.movement.value.scale)
                 setScaleY(getTargetClockFace(clock).views, aodBurnInViewModel.movement.value.scale)
             }
