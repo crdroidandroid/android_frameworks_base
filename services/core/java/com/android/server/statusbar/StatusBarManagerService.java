4dd5bcca07cb89ec3ad0cd7633fd04bd36cbcffd
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-14 16:57:21 +0900

    core: Expose method to toggle recent apps through Binder
    This is necessary in order to toggle recent apps from other
    system apps using platform APIs, which we need for implementing gestures
    in a modular way.
    
    Ref:
    "core: Expose method to start assistant through Binder"
    
    Change-Id: I1664c0618251a525697e73a2de0c8247a71dd27e

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -2716,6 +2716,18 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
         }
     }
 
+    @Override
+    public void toggleRecentApps() {
+        enforceStatusBarService();
+        if (mBar != null) {
+            try {
+                mBar.toggleRecentApps();
+            } catch (RemoteException e) {
+                Slog.e(TAG, "toggleRecentApps", e);
+            }
+        }
+    }
+
     /** @hide */
     public void passThroughShellCommand(String[] args, FileDescriptor fd) {
         enforceStatusBarOrShell();
