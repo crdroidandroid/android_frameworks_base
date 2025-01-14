f5f9fa6758c435259a91dbddb90d994048ca9542
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-01-14 09:20:06 +0100

    SystemUI: Implement pocket lock check for faceunlock
    Change-Id: I36007db4e07ffb927192871edcf5bd6a3f99ed5a
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/deviceentry/domain/interactor/SystemUIDeviceEntryFaceAuthInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/deviceentry/domain/interactor/SystemUIDeviceEntryFaceAuthInteractor.kt
@@ -20,6 +20,8 @@ import android.app.trust.TrustManager
 import android.content.Context
 import android.hardware.biometrics.BiometricFaceConstants
 import android.hardware.biometrics.BiometricSourceType
+import android.pocket.IPocketCallback
+import android.pocket.PocketManager
 import android.service.dreams.Flags.dreamsV2
 import com.android.keyguard.KeyguardUpdateMonitor
 import com.android.systemui.biometrics.data.repository.FacePropertyRepository
@@ -114,7 +116,22 @@ constructor(
 
     private val listeners: MutableList<FaceAuthenticationListener> = mutableListOf()
 
+    private var isDeviceInPocket: Boolean = false
+    private var pocketManager: PocketManager? = context.getSystemService(Context.POCKET_SERVICE) as? PocketManager
+    private val pocketCallback = object : IPocketCallback.Stub() {
+        override fun onStateChanged(state: Boolean, reason: Int) {
+            if (reason == PocketManager.REASON_SENSOR) {
+                isDeviceInPocket = state
+            } else {
+                isDeviceInPocket = false
+            }
+        }
+    }
+
     override fun start() {
+        // Register pocket callback to detect pocket state changes
+        pocketManager?.addCallback(pocketCallback)
+
         // Todo(b/310594096): there is a dependency cycle introduced by the repository depending on
         //  KeyguardBypassController, which in turn depends on KeyguardUpdateMonitor through
         //  its other dependencies. Once bypassEnabled state is available through a repository, we
@@ -351,7 +368,7 @@ constructor(
 
     fun isAuthOrDetectRunning(): Boolean = isAuthRunning() || isDetectRunning()
 
-    override fun canFaceAuthRun(): Boolean = repository.canRunFaceAuth.value
+    override fun canFaceAuthRun(): Boolean = repository.canRunFaceAuth.value && !isDeviceInPocket
 
     override fun isFaceAuthStrong(): Boolean =
         facePropertyRepository.sensorInfo.value?.strength == SensorStrength.STRONG
@@ -390,6 +407,7 @@ constructor(
     override val isBypassEnabled: StateFlow<Boolean> = repository.isBypassEnabled
 
     private fun runFaceAuth(uiEvent: FaceAuthUiEvent, fallbackToDetect: Boolean) {
+        if (isDeviceInPocket) return
         faceAuthenticationStatusOverride.value = null
         faceAuthenticationLogger.authRequested(uiEvent)
         repository.requestAuthenticate(uiEvent, fallbackToDetection = fallbackToDetect)
