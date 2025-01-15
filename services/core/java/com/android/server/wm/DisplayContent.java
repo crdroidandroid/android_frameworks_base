c4bf6b47a5dcf4f0f76a399e68eb383f1ecbae70
xingyun.wang <xingyun.wang@unisoc.com>
2025-01-15 15:41:14 +0800

    Fixed screen flickering when launching horizontal apps from vertical apps
    The reason for the flashing screen is that when starting a horizontal application from a vertical application, the application relaunch
    
    Bug: 390049399
    
    Test:
    1.turn on Auto_rotate
    2.enter the vertical application
    3.launching a horizontal application from a vertical application
    
    Google: 3452389
    Change-Id: If1654352818686e6b6fe4310425652099aff5573

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -2069,7 +2069,9 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
             return;
         }
         // The orientation of display is not changed.
-        clearFixedRotationLaunchingApp();
+        if (!mTransitionController.isCollecting(this)) {
+            clearFixedRotationLaunchingApp();
+        }
     }
 
     /**
