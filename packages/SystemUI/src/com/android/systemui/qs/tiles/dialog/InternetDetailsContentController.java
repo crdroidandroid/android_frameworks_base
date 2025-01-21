175a17c1659843d254f434c1ee812f9c40159448
Pranav Vashi <neobuddy89@gmail.com>
2025-01-21 23:07:07 +0530

    SystemUI: Allow devices to disable 5G toggle
    Change-Id: I424f5aca2b3c0799b9e89a10ced3b40ae6dfd57e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDetailsContentController.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDetailsContentController.java
@@ -1086,6 +1086,9 @@ public class InternetDetailsContentController implements AccessPointController.A
     }
 
     boolean isFivegSupported() {
+        if (!mContext.getResources().getBoolean(R.bool.config_supportsVONR))
+            return false;
+
         List<Integer> list = TelephonyProperties.default_network();
         for (int type : list) {
             if (type > 22)
