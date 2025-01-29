bce5f593871a4eded33348c0a02146d34ea52844
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2025-01-28 22:54:05 -0500

    screenrecord: Set entire screenrecord as the default option
    I don't wanna record a single app and it's so annoying to open the dropdown menu
    Every. Single. Time.
    
    Change-Id: Idef4ec7a7ea76d3fb648dbc4089a9141439bfa2e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialogDelegate.kt
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordPermissionDialogDelegate.kt
@@ -100,7 +100,7 @@ class ScreenRecordPermissionDialogDelegate(
         onStartRecordingClicked,
         mediaProjectionMetricsLogger,
         systemUIDialogFactory,
-        defaultSelectedMode = SINGLE_APP,
+        defaultSelectedMode = ENTIRE_SCREEN,
         theme = SystemUIDialog.DEFAULT_THEME,
         context,
         displayManager,
