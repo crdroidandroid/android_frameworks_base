02c3f46b5c1c48c1736e739c4586e5d559aa4e92
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-02-01 16:33:26 +0200

    don't delay setting observer callbacks for background system packages
    462062c91cc2bad6d289ded27d3d5d7b53e224fb added an unconditional 10 second delay to ContentProvider
    change callbacks for all background apps as part of MediaProvider optimizations.
    
    This led to unexpected delays when changing SettingsProvider settings (android.provider.Settings)
    that are observed by background system packages.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/content/ContentService.java
+++ services/core/java/com/android/server/content/ContentService.java
@@ -49,6 +49,7 @@ import android.content.SyncInfo;
 import android.content.SyncRequest;
 import android.content.SyncStatusInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManagerInternal;
 import android.content.pm.ProviderInfo;
 import android.database.IContentObserver;
 import android.net.Uri;
@@ -63,6 +64,7 @@ import android.os.RemoteException;
 import android.os.ResultReceiver;
 import android.os.ShellCallback;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.util.ArrayMap;
@@ -84,6 +86,8 @@ import com.android.internal.util.IndentingPrintWriter;
 import com.android.server.LocalServices;
 import com.android.server.SystemService;
 import com.android.server.pm.permission.LegacyPermissionManagerInternal;
+import com.android.server.pm.pkg.AndroidPackage;
+import com.android.server.pm.pkg.PackageState;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -580,6 +584,14 @@ public final class ContentService extends IContentService.Stub {
                 final Key key = collected.keyAt(i);
                 final List<Uri> value = collected.valueAt(i);
 
+                boolean hasSettingsUri = false;
+                for (Uri uri : value) {
+                    if (Settings.AUTHORITY.equals(uri.getAuthority())) {
+                        hasSettingsUri = true;
+                        break;
+                    }
+                }
+
                 final Runnable task = () -> {
                     try {
                         key.observer.onChangeEtc(key.selfChange,
@@ -589,12 +601,27 @@ public final class ContentService extends IContentService.Stub {
                 };
 
                 // Immediately dispatch notifications to foreground apps that
-                // are important to the user; all other background observers are
-                // delayed to avoid stampeding
+                // are important to the user and to all system apps if there's at least one changed
+                // settings uri; all other background observers are delayed to avoid stampeding
                 final boolean noDelay = (key.flags & ContentResolver.NOTIFY_NO_DELAY) != 0;
-                final int procState = LocalServices.getService(ActivityManagerInternal.class)
+                boolean isImmediate = noDelay;
+                if (!isImmediate) {
+                    final int procState = LocalServices.getService(ActivityManagerInternal.class)
                         .getUidProcessState(key.uid);
-                if (procState <= ActivityManager.PROCESS_STATE_IMPORTANT_FOREGROUND || noDelay) {
+                    isImmediate = procState <= ActivityManager.PROCESS_STATE_IMPORTANT_FOREGROUND;
+                }
+                if (hasSettingsUri && !isImmediate) {
+                    var pm = LocalServices.getService(PackageManagerInternal.class);
+                    AndroidPackage pkg = pm.getPackage(key.uid);
+                    if (pkg != null) {
+                        PackageState state = pm.getPackageStateInternal(pkg.getPackageName());
+                        if (state != null && state.isSystem()) {
+                            isImmediate = true;
+                        }
+                    }
+                }
+
+                if (isImmediate) {
                     task.run();
                 } else {
                     BackgroundThread.getHandler().postDelayed(task, BACKGROUND_OBSERVER_DELAY);
