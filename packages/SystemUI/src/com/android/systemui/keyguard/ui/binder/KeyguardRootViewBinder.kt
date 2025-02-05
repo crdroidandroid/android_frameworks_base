4cd87588c3a9359fd1069badad12bdf66b9aba3b
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2025-02-05 13:53:22 +0530

    SystemUI: Apply burn-in translations to weather view as well
    Ref : https://github.com/SuperiorOS/frameworks_base/commit/472a48741ac22537b3a0d1c0b87dbff2c1c2af8f
    
    ^ with the above commit there is an overlap of keyyguard slice view on weather view on AOD.
    
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
@@ -195,6 +195,7 @@ object KeyguardRootViewBinder {
                                     childViews[statusViewId]?.alpha = alpha
                                     childViews[burnInLayerId]?.alpha = alpha
                                     childViews[sliceViewId]?.alpha = alpha
+                                    childViews[weatherContainerId]?.alpha = alpha
                                 }
                             }
                         }
@@ -209,6 +210,7 @@ object KeyguardRootViewBinder {
                             viewModel.translationY.collect { y ->
                                 childViews[burnInLayerId]?.translationY = y
                                 childViews[sliceViewId]?.translationY = y
+                                childViews[weatherContainerId]?.translationY = y
                                 childViews[largeClockId]?.translationY = y
                                 childViews[aodNotificationIconContainerId]?.translationY = y
                             }
@@ -222,6 +224,7 @@ object KeyguardRootViewBinder {
                                         // Large Clock is not translated in the x direction
                                         childViews[burnInLayerId]?.translationX = px
                                         childViews[sliceViewId]?.translationX = px
+                                        childViews[weatherContainerId]?.translationX = px
                                         childViews[aodNotificationIconContainerId]?.translationX =
                                             px
                                     }
@@ -281,6 +284,7 @@ object KeyguardRootViewBinder {
                             viewModel.burnInLayerVisibility.collect { visibility ->
                                 childViews[burnInLayerId]?.visibility = visibility
                                 childViews[sliceViewId]?.visibility = visibility
+                                childViews[weatherContainerId]?.visibility = visibility
                             }
                         }
 
@@ -684,6 +688,7 @@ object KeyguardRootViewBinder {
     private val statusViewId = R.id.keyguard_status_view
     private val burnInLayerId = R.id.burn_in_layer
     private val sliceViewId = R.id.keyguard_slice_view
+    private val weatherContainerId = R.id.weather_container
     private val aodNotificationIconContainerId = R.id.aod_notification_icon_container
     private val largeClockId = R.id.lockscreen_clock_view_large
     private val smallClockId = R.id.lockscreen_clock_view
