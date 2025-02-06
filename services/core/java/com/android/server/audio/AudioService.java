d9d5fe9b1e0e810db0ee92635b70cc9e0a027ad1
Stefan Wysocki <stefan@haleytek.com>
2025-02-06 09:41:20 +0100

    Check if getParameters reply contains the required key
    For getAdditionalOutputDeviceDelay and getMaxAdditionalOutputDeviceDelay
    the method expects that the AudioSystem.getParameters() returns a reply
    in a form: `<key>=<value>`. If the optional parameter is unsupported
    it expects that reply string is constructed as `<key>=""` but instead,
    it might receive empty string that gives
    StringIndexOutOfBoundsException.
    
    Bug: 393338993
    Test: run cts -m CtsMediaTestCases -t android.media.cts.AudioManagerTest
    Google: 3482893
    Change-Id: I6bc6043f60092e278cc00e3c7c5b5f4974e8e9a5

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -15279,11 +15279,13 @@ public class AudioService extends IAudioService.Stub
         final String key = "additional_output_device_delay";
         final String reply = AudioSystem.getParameters(
                 key + "=" + device.getInternalType() + "," + device.getAddress());
-        long delayMillis;
-        try {
-            delayMillis = Long.parseLong(reply.substring(key.length() + 1));
-        } catch (NullPointerException e) {
-            delayMillis = 0;
+        long delayMillis = 0;
+        if (reply.contains(key)) {
+            try {
+                delayMillis = Long.parseLong(reply.substring(key.length() + 1));
+            } catch (NullPointerException e) {
+                delayMillis = 0;
+            }
         }
         return delayMillis;
     }
@@ -15309,11 +15311,13 @@ public class AudioService extends IAudioService.Stub
         final String key = "max_additional_output_device_delay";
         final String reply = AudioSystem.getParameters(
                 key + "=" + device.getInternalType() + "," + device.getAddress());
-        long delayMillis;
-        try {
-            delayMillis = Long.parseLong(reply.substring(key.length() + 1));
-        } catch (NullPointerException e) {
-            delayMillis = 0;
+        long delayMillis = 0;
+        if (reply.contains(key)) {
+            try {
+                delayMillis = Long.parseLong(reply.substring(key.length() + 1));
+            } catch (NullPointerException e) {
+                delayMillis = 0;
+            }
         }
         return delayMillis;
     }
