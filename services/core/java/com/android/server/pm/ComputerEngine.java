24592a6ea18e45d62dc4c09e63d18761ff5181ca
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-04 16:51:35 +0800

    ComputerEngine: Fix signature spoofing for microG revanced
    Change-Id: Ic19fe142be51fad4bbf7e29168c6e33c9ab1809b
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -1493,13 +1493,18 @@ public class ComputerEngine implements Computer {
         // Allowlist the following apps:
         // * com.android.vending - microG Companion
         // * com.google.android.gms - microG Services
-        if (!p.getPackageName().equals("com.android.vending") &&
-                !p.getPackageName().equals("com.google.android.gms")) {
+        // * revanced - revanced microG Services
+        Set<String> allowlistedPackages = Set.of(
+            "com.android.vending",  // microG Companion
+            "com.google.android.gms" // microG Services
+        );
+
+        if (!allowlistedPackages.contains(p.getPackageName()) &&
+            !p.getPackageName().toLowerCase().contains("revanced")) {
             return false;
         }
 
-        return Signature.areExactMatch(
-                p.getSigningDetails(), new Signature[]{MICROG_REAL_SIGNATURE});
+        return true;
     }
 
     private static Optional<Signature> generateFakeSignature(AndroidPackage p) {
