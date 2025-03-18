3a7906c25e4d6effa235bc7a57480a94b4db1942
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-18 17:04:45 +0200

    SystemUI: improve alignment between keyguard clock and date

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -18,6 +18,7 @@
 package com.android.systemui.keyguard.ui.view.layout.sections
 
 import android.content.Context
+import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.Barrier
@@ -59,7 +60,8 @@ constructor(
                 ConstraintSet.PARENT_ID,
                 ConstraintSet.START,
                 context.resources.getDimensionPixelSize(customR.dimen.clock_padding_start) +
-                    context.resources.getDimensionPixelSize(customR.dimen.status_view_margin_horizontal),
+                    context.resources.getDimensionPixelSize(customR.dimen.status_view_margin_horizontal) +
+                    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4f, context.resources.displayMetrics).toInt(),
             )
             connect(
                 R.id.keyguard_slice_view,
