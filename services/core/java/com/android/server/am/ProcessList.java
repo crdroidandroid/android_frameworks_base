a26146de74253389cb222ed8ec501d1b3d1411c2
Andrei Kulakov <Andrei.Kulakov@partner.bmwgroup.com>
2025-03-20 13:34:04 +0100

    Fix persistent apps OomAdj score setup
    Persistent apps running under the foreground user (i.e. not
    USER_SYSTEM), the OomAdj score is not taking the "persistent" flag
    into account, so those apps get a high score and are good candidates
    for being killed in low memory situations.
    
    This seems contradictory with the meaning of the "persistent" flag.
    
    Bug: 406278057
    Test: Manual Tests
    
    Change-Id: Ie0c8bcfe4bb1642bab81941535e029bf6857f1c9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -3442,7 +3442,6 @@ public final class ProcessList {
         }
 
         if (!isolated && !isSdkSandbox
-                && userId == UserHandle.USER_SYSTEM
                 && (info.flags & PERSISTENT_MASK) == PERSISTENT_MASK
                 && (TextUtils.equals(proc, info.processName))) {
             // The system process is initialized to SCHED_GROUP_DEFAULT in init.rc.
