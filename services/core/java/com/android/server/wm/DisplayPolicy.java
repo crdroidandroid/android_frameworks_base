69607e84ebae7fd1a7fcd6a52c4c1ba150aefe50
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-25 06:54:39 +0800

    DisplayPolicy: Do not attach navbar during top app transition when tiny task bar is enabled
    Change-Id: Ie18eb94201b580141c3d1ee92bc353ecdb893b41
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -241,6 +241,7 @@ public class DisplayPolicy {
     private volatile boolean mHasStatusBar;
     private volatile boolean mHasNavigationBar;
     private volatile boolean mForceNavbar;
+    private volatile boolean mTaskBarEnabled;
     // Can the navigation bar ever move to the side?
     private volatile boolean mNavigationBarCanMove;
     private volatile boolean mNavigationBarAlwaysShowOnSideGesture;
@@ -407,6 +408,9 @@ public class DisplayPolicy {
             ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.FORCE_SHOW_NAVBAR), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(LineageSettings.System.getUriFor(
+                    LineageSettings.System.ENABLE_TASKBAR), false, this,
                     UserHandle.USER_ALL);
 
             updateSettings();
@@ -737,6 +741,13 @@ public class DisplayPolicy {
         mForceNavbar = LineageSettings.System.getIntForUser(resolver,
                 LineageSettings.System.FORCE_SHOW_NAVBAR, 0,
                 UserHandle.USER_CURRENT) == 1;
+        mTaskBarEnabled = LineageSettings.System.getIntForUser(resolver,
+                LineageSettings.System.ENABLE_TASKBAR, isTablet() ? 1 : 0,
+                UserHandle.USER_CURRENT) != 0;
+    }
+    
+    private boolean isTablet() {
+        return getCurrentUserResources().getConfiguration().smallestScreenWidthDp >= 600;
     }
 
     private int getDisplayId() {
@@ -1839,10 +1850,12 @@ public class DisplayPolicy {
                 R.bool.config_remoteInsetsControllerControlsSystemBars);
 
         updateConfigurationAndScreenSizeDependentBehaviors();
+        
+        final boolean isMobileTaskbarEnabled = !isTablet() && mTaskBarEnabled;
 
         final boolean shouldAttach =
                 res.getBoolean(R.bool.config_attachNavBarToAppDuringTransition)
-                        && !Flags.enableTinyTaskbar();
+                        && !isMobileTaskbarEnabled;
         if (mShouldAttachNavBarToAppDuringTransition != shouldAttach) {
             mShouldAttachNavBarToAppDuringTransition = shouldAttach;
         }
