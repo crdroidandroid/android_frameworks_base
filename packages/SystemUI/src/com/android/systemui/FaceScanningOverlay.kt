8fda3f65503b79463b85856f70cfcc6cf1b798a2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-26 18:11:15 +0800

    SystemUI: Fix face unlock indication static calls
    Change-Id: Id216d9b6675e05d929d1ef61e5d76a8f43a4c27e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/FaceScanningOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/FaceScanningOverlay.kt
@@ -36,6 +36,7 @@ import com.android.systemui.biometrics.AuthController
 import com.android.systemui.log.ScreenDecorationsLogger
 import com.android.systemui.plugins.statusbar.StatusBarStateController
 import com.android.systemui.statusbar.phone.FaceUnlockImageView
+import com.android.systemui.statusbar.phone.FaceUnlockProxy
 import com.android.systemui.util.asIndenting
 import java.io.PrintWriter
 import java.util.concurrent.Executor
@@ -106,7 +107,7 @@ class FaceScanningOverlay(
         // Delay the relayout until the end of the animation when hiding,
         // otherwise we'd clip it.
         if (showScanningAnim) {
-            FaceUnlockImageView.setBouncerState(FaceUnlockImageView.State.SCANNING)
+            FaceUnlockProxy.INSTANCE().setBouncerState(FaceUnlockImageView.State.SCANNING)
             visibility = View.VISIBLE
             requestLayout()
         }
@@ -127,7 +128,7 @@ class FaceScanningOverlay(
         )
         rimAnimator?.start()
         if (faceAuthSucceeded) {
-            FaceUnlockImageView.setBouncerState(FaceUnlockImageView.State.SUCCESS)
+            FaceUnlockProxy.INSTANCE().setBouncerState(FaceUnlockImageView.State.SUCCESS)
         }
     }
 
