575cb5e3b2512af62318f75435630200749140dd
Alexander Martinz <amartinz@shiftphones.com>
2025-03-28 07:44:24 +0100

    fixup! Add support to configure double line clock in lockscreen [1/2]
    Fetch the default value from overlay config which was originally
    introduced in aosp/2527300 but was missed out in commit 9282e2c.
    
    Bug: 277244307
    Test: atest KeyguardClockSwitchControllerTest
    Change-Id: Ie8cb62e04b9fbc7f8d42fbefeceb0f7b11c342b1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/data/repository/KeyguardClockRepository.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/data/repository/KeyguardClockRepository.kt
@@ -173,7 +173,9 @@ constructor(
         return ClockSizeSetting.fromSettingValue(
             secureSettings.getIntForUser(
                 Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK,
-                /* defaultValue= */ 1,
+                context.resources
+                    .getInteger(com.android.internal.R.integer
+                        .config_doublelineClockDefault),
                 UserHandle.USER_CURRENT,
             )
         )
