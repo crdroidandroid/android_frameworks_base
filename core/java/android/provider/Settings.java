12b8fde36b2437c417e2a10dfcb2b3c8c19839c4
Ghosuto <clash.raja10@gmail.com>
2025-04-10 16:37:44 +0000

    SystemUI: Lock Screen Clock Accent Color Option
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14444,6 +14444,18 @@ public final class Settings {
         public static final String NOTIFICATION_ROW_TRANSPARENCY_LOCKSCREEN = "notification_row_transparency_lockscreen";
 
         /**
+         * Whether to use system accent color for lock screen clock text
+         * @hide
+         */
+        public static final String CLOCK_TEXT_ACCENT_COLOR = "clock_text_accent_color";
+
+        /**
+         * Lock screen clock text opacity (0-100)
+         * @hide
+         */
+        public static final String CLOCK_TEXT_OPACITY = "clock_text_opacity";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
