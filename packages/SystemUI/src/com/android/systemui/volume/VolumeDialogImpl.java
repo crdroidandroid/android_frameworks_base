cae729abcda69cec33ba518ce10060983b1e16a2
Pranav Vashi <neobuddy89@gmail.com>
2025-04-12 04:10:17 +0530

    SystemUI: VolumeDialog: Do not check previous state when updating rows
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1621,7 +1621,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     mPrevExpansionState = mExpansionState;
                     mExpansionState = ExpansionState.COLLAPSED;
                     updateRowsH(mDefaultRow, false);
-                    mExpandRows.setExpanded(false);
+                    if (mExpandRows != null) {
+                        mExpandRows.setExpanded(false);
+                    }
                 }
                 mPrevExpansionState = mExpansionState;
                 mExpansionState = mPrevExpansionState == ExpansionState.APPS_EXPANDED
@@ -2138,10 +2140,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             trimObsoleteH();
         }
 
-        final boolean expanded = mExpansionState == ExpansionState.EXPANDED
-                || mPrevExpansionState == ExpansionState.EXPANDED;
-        final boolean appsExpanded = mExpansionState == ExpansionState.APPS_EXPANDED
-                || mPrevExpansionState == ExpansionState.APPS_EXPANDED;
+        final boolean expanded = mExpansionState == ExpansionState.EXPANDED;
+        final boolean appsExpanded = mExpansionState == ExpansionState.APPS_EXPANDED;
         final boolean collapsed = mExpansionState == ExpansionState.COLLAPSED;
 
         final boolean isOutmostIndexMax = isWindowGravityLeft() ? isRtl() : !isRtl();
