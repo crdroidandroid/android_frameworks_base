774f94ee7218f1c010e42f623aa14f14492b04c3
PK Guo <pk.guo@mediatek.com>
2025-04-22 14:23:59 +0800

    audio: fix SAD and SAM inconsistent after audioserver crash
    When restoring setting after audioserver crash
    1. The setHdmiSystemAudioSupported doesn't setForceUse with SAM
    because the setting value is equal to mHdmiSystemAudioSupported.
    2. The SAD is stored in the AudioDescriptor, but AudioDescriptor
    isn't pass to native layer.
    
    Adjust logic to ensure that SAM and AudioDescriptor can set to
    native layer when restoring setting after the crash.
    
    Bug: 412816152
    Test: manual
    Change-Id: I3a473dddfd4e69a4dbfe0905c2ba0aa849c8cfae

--- services/core/java/com/android/server/audio/AudioDeviceInventory.java
+++ services/core/java/com/android/server/audio/AudioDeviceInventory.java
@@ -795,9 +795,14 @@ public class AudioDeviceInventory {
             //TODO iterate on mApmConnectedDevices instead once it handles all device types
             for (DeviceInfo di : mConnectedDevices.values()) {
                 res = mAudioSystem.setDeviceConnectionState(new AudioDeviceAttributes(
-                        di.mDeviceType,
+                        AudioSystem.isInputDevice(di.mDeviceType)
+                            ? AudioDeviceAttributes.ROLE_INPUT
+                            : AudioDeviceAttributes.ROLE_OUTPUT,
+                        AudioDeviceInfo.convertInternalDeviceToDeviceType(di.mDeviceType),
                         di.mDeviceAddress,
-                        di.mDeviceName),
+                        di.mDeviceName,
+                        di.mAudioProfiles,
+                        di.mAudioDescriptors),
                         AudioSystem.DEVICE_STATE_AVAILABLE,
                         di.mDeviceCodecFormat, false /*deviceSwitch*/);
                 if (asDeviceConnectionFailure() && res != AudioSystem.AUDIO_STATUS_OK) {
