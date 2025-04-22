100a35476ec09b6a4e15197e1bf3302b2029974e
lijilou <lijilou@xiaomi.corp-partner.google.com>
2025-04-22 21:53:59 +0800

    Add global lock for getTargetWindowTokenFromInputToken method.
    Bug: none
    Flag: EXEMPT bugfix
    Google: 3599451
    Change-Id: I10b2813175156e26a919a4b8c292d48465a910e5

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -8772,8 +8772,11 @@ public class WindowManagerService extends IWindowManager.Stub
 
         @Override
         public @Nullable IBinder getTargetWindowTokenFromInputToken(IBinder inputToken) {
-            InputTarget inputTarget = WindowManagerService.this.getInputTargetFromToken(inputToken);
-            return inputTarget == null ? null : inputTarget.getWindowToken();
+            synchronized (mGlobalLock) {
+                InputTarget inputTarget =
+                        WindowManagerService.this.getInputTargetFromToken(inputToken);
+                return inputTarget == null ? null : inputTarget.getWindowToken();
+            }
         }
 
         @Override
