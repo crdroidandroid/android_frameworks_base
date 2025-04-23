03f82ffc64a3fa76bd808f73fc0c184eefa2c346
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-23 12:55:38 +0800

    services: optimize memory on device wake
    [neobuddy89: Drop experimental stuff.]
    
    Change-Id: I67d917630fbbba8c72bb94d0cd96ee2de1d71165
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -432,6 +432,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final String ACTION_TORCH_OFF =
             "com.android.server.policy.PhoneWindowManager.ACTION_TORCH_OFF";
 
+    private static final long MEMORY_RELEASE_INTERVAL_MS = 10 * 60 * 1000L; // 10 minutes
+    private long lastMemoryReleaseTime = 0L;
+
     /**
      * Keyguard stuff
      */
@@ -6411,6 +6414,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mPocketManager != null) {
             mPocketManager.onInteractiveChanged(false);
         }
+
+        mHandler.removeCallbacks(mMemoryOpt);
     }
 
     // Called on the PowerManager's Notifier thread.
@@ -6471,6 +6476,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         EventLogTags.writeScreenToggled(1);
 
+        mHandler.removeCallbacks(mMemoryOpt);
+        mHandler.postDelayed(mMemoryOpt, 1250 /* allowance time */);
+
         mIsGoingToSleep = false;
         setPendingWakingUpGroup(displayGroupId);
         mDefaultDisplayPolicy.setAwake(true);
@@ -6518,6 +6526,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mDisplayFoldController.finishedWakingUp();
         }
     }
+
+    private final Runnable mMemoryOpt = new Runnable() {
+        @Override
+        public void run() {
+            releaseMemoryAtScreenOn();
+        }
+    };
 
     private boolean shouldWakeUpWithHomeIntent() {
         if (mWakeUpToLastStateTimeout <= 0) {
@@ -8201,6 +8216,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case AudioManager.RINGER_MODE_SILENT:
                 am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
                 break;
+        }
+    }
+
+    private void releaseMemoryAtScreenOn() {
+        long currentTime = System.currentTimeMillis();
+        if (lastMemoryReleaseTime == 0L || currentTime - lastMemoryReleaseTime > MEMORY_RELEASE_INTERVAL_MS) {
+            try {
+                ActivityManager.getService().releaseMemory(900, 20, false, false);
+                lastMemoryReleaseTime = currentTime;
+            } catch (RemoteException e) {
+            }
         }
     }
 }
