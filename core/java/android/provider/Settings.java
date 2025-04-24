907ac7863a944151abb19e817a76760c4af64c18
Pranav Vashi <neobuddy89@gmail.com>
2025-04-25 00:37:55 +0530

    services: Clean up unused settings for three finger swipe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7068,14 +7068,6 @@ public final class Settings {
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
         /**
-         * Whether three fingers swipe is active
-         * 0 = Inactive, 1 = Active
-         * @hide
-         */
-        @Readable
-        public static final String THREE_FINGER_GESTURE_ACTIVE = "three_fingers_swipe_active";
-
-        /**
          * Whether to show Bluetooth dialog or toggle bluetooth using Bluetooth tile
          * @hide
          */
