96b36eab8637e215c728e0e30a33de130d45b87d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-25 13:04:54 +0800

    services: disable compaction on screen wake
    nothing os was using CompactProfile.POPULATE which is a kind of compaction that uses madv_populate, which is not yet supported on aosp
    
    Change-Id: I3b3f6600ffedc1312278a2bca9b4ce4ac04ec43b
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19677,6 +19677,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @Override
     public void loadProcessMemory(String packageName) {
+        /*
         synchronized (mProcLock) {
             ProcessRecord proc = getProcessRecord(packageName);
             if (proc != null) {
@@ -19687,6 +19688,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                         true);
             }
         }
+        */
     }
 
     public ProcessRecord getProcessRecord(String str) {
