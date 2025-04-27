31d043dc502a4209dfa3575046768dfbf9bb9a20
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-27 20:22:51 +0800

    GameManagerService: use verbose logging for game affinity logs
    Change-Id: Ia9dbaaca6dfc140f8618d52d58865a1415495ca9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/app/GameManagerService.java
+++ services/core/java/com/android/server/app/GameManagerService.java
@@ -2379,7 +2379,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
         private void setGameAffinity(int uid, boolean boosted) {
             Map<String, Integer> packagePidMap = getRunningGamePidsPerPackage(uid);
             if (packagePidMap.isEmpty()) {
-                Slog.w(TAG, "No running game process found for UID=" + uid);
+                Slog.v(TAG, "No running game process found for UID=" + uid);
                 return;
             }
 
@@ -2388,7 +2388,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
                 String packageName = entry.getKey();
 
                 Process.setThreadAffinity(pid, boosted ? 0 : 2);
-                Slog.d(TAG, "Set affinity for PID=" + pid + " (Package: " + packageName + 
+                Slog.v(TAG, "Set affinity for PID=" + pid + " (Package: " + packageName + 
                         ", UID=" + uid + ") to " + (boosted ? "big cores (boosted)" : "all cores (normal)"));
             }
         }
