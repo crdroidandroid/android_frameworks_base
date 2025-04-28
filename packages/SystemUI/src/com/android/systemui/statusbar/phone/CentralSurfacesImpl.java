e6575423774ca074226e63895ee5d9b29b9753ab
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-29 07:16:32 +0800

    SystemUI: clean heap memory on screen off
    Change-Id: If1ed8a9a02c190ba9fb056c093c21b8b91c85161
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -65,6 +65,7 @@ import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -456,6 +457,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final StatusBarSignalPolicy mStatusBarSignalPolicy;
     private final StatusBarHideIconsForBouncerManager mStatusBarHideIconsForBouncerManager;
 
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+
     /** Controller for the Shade. */
     private final ShadeSurface mShadeSurface;
     private final ShadeLogger mShadeLogger;
@@ -486,6 +489,9 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private GameSpaceManager mGameSpaceManager;
 
     private final DisplayMetrics mDisplayMetrics;
+
+    private static final long GC_INTERVAL_MS = 10 * 60 * 1000L; // 10 minutes
+    private long lastGcTime = 0L;
 
     // XXX: gesture research
     private final GestureRecorder mGestureRec = DEBUG_GESTURES
@@ -2731,6 +2737,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                         () -> mCommandQueueCallbacks.onEmergencyActionLaunchGestureDetected());
             }
             updateIsKeyguard();
+            // make sure we do garbage collection at screen off but delay it to avoid black wallpaper
+            mHandler.postDelayed(mSystemUiGcOpt, 5000);
         }
 
         @Override
@@ -2779,6 +2787,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                 }
             });
             DejankUtils.stopDetectingBlockingIpcs(tag);
+            mHandler.removeCallbacks(mSystemUiGcOpt);
         }
 
         /**
@@ -2844,6 +2853,20 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             }
             updateScrimController();
             mBurnInProtectionController.startShiftTimer();
+        }
+    };
+
+    private final Runnable mSystemUiGcOpt = new Runnable() {
+        @Override
+        public void run() {
+            long currentTime = System.currentTimeMillis();
+            if (lastGcTime == 0L || currentTime - lastGcTime > GC_INTERVAL_MS) {
+                Log.v("GcOpt", "performing garbage collection for SystemUI");
+                System.gc();
+                System.runFinalization();
+                System.gc();
+                lastGcTime = currentTime;
+            }
         }
     };
 
