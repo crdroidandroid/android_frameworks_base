4d6e807b7951b6a19237f507ed1cd968cfb1da00
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-29 07:09:17 +0800

    services: clean system_server heap memory on screen off
    Change-Id: I27277ae924f3563ee56a323804ba41e64e317731
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -433,6 +433,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final long MEMORY_RELEASE_INTERVAL_MS = 10 * 60 * 1000L; // 10 minutes
     private long lastMemoryReleaseTime = 0L;
 
+    private static final long GC_INTERVAL_MS = 10 * 60 * 1000L; // 10 minutes
+    private long lastGcTime = 0L;
+
     /**
      * Keyguard stuff
      */
@@ -6452,6 +6455,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         mPowerButtonLaunchGestureTriggeredDuringGoingToSleep = false;
         mPowerButtonLaunchGestureTriggered = false;
+
+        // make sure we do garbage collection at screen off but delay it to avoid black wallpaper
+        mHandler.postDelayed(mSystemServerGcOpt, 5000);
     }
 
     // Called on the PowerManager's Notifier thread.
@@ -6472,6 +6478,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mHandler.removeCallbacks(mMemoryOpt);
         mHandler.postDelayed(mMemoryOpt, 1250 /* allowance time */);
+
+        // remove pending system server gc for frequent screen state changes
+        mHandler.removeCallbacks(mSystemServerGcOpt);
 
         mIsGoingToSleep = false;
         setPendingWakingUpGroup(displayGroupId);
@@ -6525,6 +6534,20 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         @Override
         public void run() {
             releaseMemoryAtScreenOn();
+        }
+    };
+
+    private final Runnable mSystemServerGcOpt = new Runnable() {
+        @Override
+        public void run() {
+            long currentTime = System.currentTimeMillis();
+            if (lastGcTime == 0L || currentTime - lastGcTime > GC_INTERVAL_MS) {
+                System.gc();
+                System.runFinalization();
+                System.gc();
+                lastGcTime = currentTime;
+                Log.v("GcOpt", "performing garbage collection for system_server");
+            }
         }
     };
 
