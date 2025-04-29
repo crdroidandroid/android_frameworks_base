6faf69dbda75b97ded14263a5ef05a53c841120a
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-29 22:59:57 +0300

    DisplayCutout: fully animate camera protection
    Animating from / until 0.5 scale makes the animation seem choppy,
    do a full animation to make it look nicer.
    
    Change-Id: I89001fc8baf480c9d16989a19174cf3bd8734966

--- packages/SystemUI/src/com/android/systemui/DisplayCutoutBaseView.kt
+++ packages/SystemUI/src/com/android/systemui/DisplayCutoutBaseView.kt
@@ -300,7 +300,7 @@ open class DisplayCutoutBaseView : View, RegionInterceptableView {
     }
 
     companion object {
-        const val HIDDEN_CAMERA_PROTECTION_SCALE = 0.5f
+        const val HIDDEN_CAMERA_PROTECTION_SCALE = 0.0f
 
         @JvmStatic protected fun transformPhysicalToLogicalCoordinates(
             @Surface.Rotation rotation: Int,
