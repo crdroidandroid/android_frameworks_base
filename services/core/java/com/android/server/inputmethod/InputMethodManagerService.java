47db147c02f2ae8f2ef2a1dee5545d0453c479ac
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-20 08:45:49 +0800

    inputmethod: allow hiding navigation ime bar [1/4]
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iaa15dda08d614d01fae64ae2b5aa72f50513af99

--- services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
+++ services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -507,6 +507,9 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
                         LineageSettings.Secure.FEATURE_TOUCH_HOVERING),
                         false, this, UserHandle.ALL);
             }
+            resolver.registerContentObserverAsUser(Settings.Secure.getUriFor(
+                    "sysui_show_nav_bar_ime"),
+                    false, this, UserHandle.ALL);
         }
 
         @Override
@@ -522,6 +525,8 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
                     LineageSettings.System.HIGH_TOUCH_SENSITIVITY_ENABLE);
             final Uri touchHoveringUri = LineageSettings.Secure.getUriFor(
                     LineageSettings.Secure.FEATURE_TOUCH_HOVERING);
+            final Uri navBarUri = Settings.Secure.getUriFor(
+                    "sysui_show_nav_bar_ime");
             synchronized (ImfLock.class) {
                 if (!mConcurrentMultiUserModeEnabled && mCurrentImeUserId != userId) {
                     return;
@@ -532,6 +537,8 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
                     updateTouchSensitivity();
                 } else if (touchHoveringUri.equals(uri)) {
                     updateTouchHovering();
+                } else if (navBarUri.equals(uri)) {
+                    onUpdateResourceOverlay(userId);
                 }
             }
         }
@@ -1236,7 +1243,9 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
                     final boolean value =
                             InputMethodDrawsNavBarResourceMonitor.evaluate(context,
                                     profileParentId);
-                    userData.mImeDrawsNavBar.set(value);
+                    final boolean showNavBarIme = Settings.Secure.getIntForUser(
+                        context.getContentResolver(), "sysui_show_nav_bar_ime", 1, userId) == 1;
+                    userData.mImeDrawsNavBar.set(value && showNavBarIme);
 
                     userData.mBackgroundLoadLatch.countDown();
                     Slog.d(TAG, "Complete initialization for user=" + userId);
@@ -5483,10 +5492,12 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         final boolean value =
                 InputMethodDrawsNavBarResourceMonitor.evaluate(mContext, profileParentId);
         final var profileUserIds = mUserManagerInternal.getProfileIds(profileParentId, false);
+        final boolean showNavBarIme = Settings.Secure.getIntForUser(
+            mContext.getContentResolver(), "sysui_show_nav_bar_ime", 1, userId) == 1;
         final ArrayList<UserData> updatedUsers = new ArrayList<>();
         for (int profileUserId : profileUserIds) {
             final var userData = getUserData(profileUserId);
-            userData.mImeDrawsNavBar.set(value);
+            userData.mImeDrawsNavBar.set(value && showNavBarIme);
             updatedUsers.add(userData);
         }
         synchronized (ImfLock.class) {
