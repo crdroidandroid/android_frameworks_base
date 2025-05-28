0aa34d96b87c762b8114bb08bc475ff05380f45c
Abhay Singh Gill <abhaygill017@gmail.com>
2025-05-29 03:59:29 +0530

    base: Add key generation for keybox spoofing [1/2]
    The code is used from :-
    https://github.com/5ec1cff/TrickyStore
    
    This allows us to enjoy spoofing keyboxes the right way even with broken TEE.
    Allow users to provide keybox XML as well.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13431,6 +13431,13 @@ public final class Settings {
                 "com.android.server.display.HBM_SETTING_KEY";
 
         /**
+         * User selectable keybox data.
+         * @hide
+         */
+        @Readable
+        public static final String KEYBOX_DATA = "keybox_data";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
