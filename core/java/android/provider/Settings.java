4e1a597684611d684dc2dc336190466999be1deb
Abhay Singh Gill <abhaygill017@gmail.com>
2025-05-29 03:59:29 +0530

    base: Add key generation for keybox spoofing [1/2]
    The code is used from :-
    https://github.com/5ec1cff/TrickyStore
    
    This allows us to enjoy spoofing keyboxes the right way even with broken TEE.
    Allow users to provide keybox XML as well.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13876,6 +13876,13 @@ public final class Settings {
         public static final String VPN_ENFORCE_DNS_STORE = "vpn_enforce_dns_store";
 
         /**
+         * User selectable keybox data.
+         * @hide
+         */
+        @Readable
+        public static final String KEYBOX_DATA = "keybox_data";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
