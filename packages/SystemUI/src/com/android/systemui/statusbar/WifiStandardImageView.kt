da004379e34f39dad88c6904b4ff4db209b07f76
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-30 06:52:05 +0800

    Wifi Standard: apply MVC structure
    Change-Id: I92c504deda6baad1a41aa294447637fef35fa02a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/WifiStandardImageView.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/WifiStandardImageView.kt
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2023-2024 The risingOS Android Project
+ * Copyright (C) 2025 The AxionAOSP Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,82 +17,40 @@
 package com.android.systemui.statusbar
 
 import android.content.Context
-import android.content.res.Resources
-import android.provider.Settings
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.net.wifi.WifiManager
-import android.os.UserHandle
 import android.util.AttributeSet
-import android.widget.ImageView
-import com.android.systemui.res.R
 import android.view.ViewGroup.MarginLayoutParams
+import android.widget.ImageView
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.*
+import com.android.systemui.res.R
 
 class WifiStandardImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ImageView(context, attrs, defStyleAttr) {
 
-    private val connectivityManager: ConnectivityManager by lazy { 
-        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager 
-    }
-    private val wifiManager: WifiManager by lazy { 
-        context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager 
-    }
-
-    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-
-    private val wifiStandardFlow = flow {
-        while (true) {
-            emit(getWifiStandard())
-            delay(1000)
-        }
-    }.distinctUntilChanged()
-
-    private val wifiStandardEnabledFlow = callbackFlow {
-        val settingObserver = object : android.database.ContentObserver(null) {
-            override fun onChange(selfChange: Boolean) {
-                trySend(getWifiStandardEnabled())
-            }
-        }
-
-        context.contentResolver.registerContentObserver(
-            Settings.System.getUriFor(Settings.System.WIFI_STANDARD_ICON),
-            false, settingObserver
-        )
-
-        send(getWifiStandardEnabled())
-
-        awaitClose { context.contentResolver.unregisterContentObserver(settingObserver) }
-    }.distinctUntilChanged()
-
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-
-        coroutineScope.launch {
-            combine(wifiStandardFlow, wifiStandardEnabledFlow) { wifiStandard, wifiStandardEnabled ->
-                Pair(wifiStandard, wifiStandardEnabled)
-            }.collect { (wifiStandard, wifiStandardEnabled) ->
-                updateWifiStatus(wifiStandard, wifiStandardEnabled)
-            }
-        }
+        WifiStandardController.INSTANCE(context).attachView(this)
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        coroutineScope.cancel()
+        WifiStandardController.INSTANCE(context).detachView()
     }
 
-    private fun updateWifiStatus(wifiStandard: Int, wifiStandardEnabled: Boolean) {
+    fun updateWifiStatus(wifiStandard: Int, wifiStandardEnabled: Boolean) {
         post {
             if (!wifiStandardEnabled || wifiStandard < 4) {
                 visibility = GONE
                 layoutParams = (layoutParams as MarginLayoutParams).apply { marginEnd = 0 }
             } else {
-                val drawableId = getDrawableForWifiStandard(wifiStandard)
+                val drawableId = when (wifiStandard) {
+                    4 -> R.drawable.ic_wifi_standard_4
+                    5 -> R.drawable.ic_wifi_standard_5
+                    6 -> R.drawable.ic_wifi_standard_6
+                    7 -> R.drawable.ic_wifi_standard_7
+                    else -> 0
+                }
+
                 if (drawableId > 0) {
                     setImageResource(drawableId)
                     visibility = VISIBLE
@@ -100,34 +59,6 @@ class WifiStandardImageView @JvmOverloads constructor(
                     }
                 }
             }
-        }
-    }
-
-    private fun getWifiStandard(): Int {
-        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        return if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-            wifiManager.connectionInfo.wifiStandard
-        } else {
-            -1
-        }
-    }
-
-    private fun getWifiStandardEnabled(): Boolean {
-        return Settings.System.getIntForUser(
-            context.contentResolver,
-            Settings.System.WIFI_STANDARD_ICON,
-            0,
-            UserHandle.USER_CURRENT
-        ) == 1
-    }
-
-    private fun getDrawableForWifiStandard(wifiStandard: Int): Int {
-        return when (wifiStandard) {
-            4 -> R.drawable.ic_wifi_standard_4
-            5 -> R.drawable.ic_wifi_standard_5
-            6 -> R.drawable.ic_wifi_standard_6
-            7 -> R.drawable.ic_wifi_standard_7
-            else -> 0
         }
     }
 }
