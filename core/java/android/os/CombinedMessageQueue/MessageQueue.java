a49052559313e2f24c9d630a6425cd72454b52ff
Mark Fasheh <mfasheh@google.com>
2025-06-04 09:40:39 -0700

    Avoid races between nextMessage() and stackHasMessages()
    Always check if the stack is draining before we exit stackHasMessages().
     - This will ensure that next has put any pending messages into the
       priority queues before we search them.
    
    Bug: 396569494
    Flag: EXEMPT - bugfix
    Test: atest MessageQueueTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:19f88e9f24ef7bc899aa3410bf8d5c756c6155ff)
    Merged-In: If3259e13060baae4125a4977519a7f08390f79cb
    Google: 3652816
    Change-Id: If3259e13060baae4125a4977519a7f08390f79cb

--- core/java/android/os/CombinedMessageQueue/MessageQueue.java
+++ core/java/android/os/CombinedMessageQueue/MessageQueue.java
@@ -2349,10 +2349,6 @@ public final class MessageQueue {
             return mMessage.when;
         }
 
-        boolean isRemovedFromStack() {
-            return mRemovedFromStackValue;
-        }
-
         boolean removeFromStack() {
             return sRemovedFromStack.compareAndSet(this, false, true);
         }
@@ -2666,16 +2662,10 @@ public final class MessageQueue {
         StateNode bottom = getStateNode(top);
 
         /*
-         * If the top node is a state node, there are no reachable messages.
-         * If it's anything other than Active, we can quit as we know that next() is not
-         * consuming items.
-         * If the top node is Active then we know that next() is currently consuming items.
-         * In that case we should wait next() has drained the stack.
+         * If the top node is a state node, there are no reachable messages. We should still
+         * wait for next to complete draining the stack.
          */
         if (top == bottom) {
-            if (bottom != sStackStateActive) {
-                return false;
-            }
             waitForDrainCompleted();
             return false;
         }
@@ -2703,7 +2693,7 @@ public final class MessageQueue {
                         }
                     }
                 } else {
-                    return true;
+                    break;
                 }
             }
 
@@ -2713,15 +2703,16 @@ public final class MessageQueue {
                 if (DEBUG) {
                     Log.d(TAG_C, "stackHasMessages next() is walking the stack, we must re-sample");
                 }
-                waitForDrainCompleted();
                 break;
             }
             if (!n.isMessageNode()) {
                 /* We reached the end of the stack */
-                return found;
+                break;
             }
             p = (MessageNode) n;
         }
+
+        waitForDrainCompleted();
 
         return found;
     }
