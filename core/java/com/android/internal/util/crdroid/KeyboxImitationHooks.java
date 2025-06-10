d1eba2e1ed7bdfe7aaf81443f28452d273d05c3f
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-10 10:25:22 +0530

    base: Use keybox spoofing only when attest key descriptor is null
    Change-Id: Ie4135665ebe23a5cd7e7a0a80a96a0e98ed0450e
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/crdroid/KeyboxImitationHooks.java
+++ core/java/com/android/internal/util/crdroid/KeyboxImitationHooks.java
@@ -34,6 +34,7 @@ public class KeyboxImitationHooks {
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
     private static boolean mFailed = false;
     private static boolean mIsAttestation = false;
+    private static boolean hasAttestKeyDescriptor = false;
     private static Integer keyAlgo;
 
     public static KeyEntryResponse onGetKeyEntry(KeyDescriptor descriptor) {
@@ -50,6 +51,10 @@ public class KeyboxImitationHooks {
         }
 
         if (!mIsAttestation) {
+            return null;
+        }
+
+        if (hasAttestKeyDescriptor) {
             return null;
         }
 
@@ -87,7 +92,6 @@ public class KeyboxImitationHooks {
             KeyboxUtils.append(uid, descriptor.alias, response);
             mFailed = false;
             putAlgo(params.algorithm);
-            mIsAttestation = true;
             return response.metadata;
         } catch (Exception e) {
             Log.e(TAG, "Failed to generate key", e);
@@ -185,6 +189,10 @@ public class KeyboxImitationHooks {
 
     public static void setAttestationFlag(boolean flag) {
         mIsAttestation = flag;
+    }
+
+    public static void setAttestKeyFlag(boolean flag) {
+        hasAttestKeyDescriptor = flag;
     }
 
     private static void dlog(String msg) {
