4f6792ea8d3583f079217edd041f466c2896cd32
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-10 10:25:22 +0530

    base: Use keybox spoofing only when attest key descriptor is null
    Change-Id: Ie4135665ebe23a5cd7e7a0a80a96a0e98ed0450e
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreKeyPairGeneratorSpi.java
@@ -699,20 +699,34 @@ public abstract class AndroidKeyStoreKeyPairGeneratorSpi extends KeyPairGenerato
             KeyStoreSecurityLevel iSecurityLevel = mKeyStore.getSecurityLevel(securityLevel);
 
             KeyMetadata metadata;
-            if (mSpec.getAttestationChallenge() != null &&
-                (KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
-                 KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
-                metadata = KeyboxImitationHooks.generateKey(
-                    iSecurityLevel.getBinderInterface(),
-                    descriptor,
-                    constructKeyGenerationArguments()
-                );
-                if (metadata == null) {
-                    KeyboxImitationHooks.setFailFlag(true);
+            if (mSpec.getAttestationChallenge() != null) {
+                KeyboxImitationHooks.setAttestationFlag(true);
+                if (mAttestKeyDescriptor == null) {
+                    KeyboxImitationHooks.setAttestKeyFlag(false);
+                    if ((KeyProperties.KEY_ALGORITHM_EC.equals(mJcaKeyAlgorithm) ||
+                      KeyProperties.KEY_ALGORITHM_RSA.equals(mJcaKeyAlgorithm))) {
+                        metadata = KeyboxImitationHooks.generateKey(
+                            iSecurityLevel.getBinderInterface(),
+                            descriptor,
+                            constructKeyGenerationArguments()
+                        );
+                        if (metadata == null) {
+                            KeyboxImitationHooks.setFailFlag(true);
+                            metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                                    constructKeyGenerationArguments(), flags, additionalEntropy);
+                        }
+                    } else {
+                        metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
+                                constructKeyGenerationArguments(), flags, additionalEntropy);
+                    }
+                } else {
+                    KeyboxImitationHooks.setAttestKeyFlag(true);
                     metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
-                        constructKeyGenerationArguments(), flags, additionalEntropy);
+                            constructKeyGenerationArguments(), flags, additionalEntropy);
                 }
             } else {
+                KeyboxImitationHooks.setAttestationFlag(false);
+                KeyboxImitationHooks.setAttestKeyFlag(false);
                 metadata = iSecurityLevel.generateKey(descriptor, mAttestKeyDescriptor,
                         constructKeyGenerationArguments(), flags, additionalEntropy);
             }
