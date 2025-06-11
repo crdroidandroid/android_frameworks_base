d207415d0ee1fca61639f697c96d42cce6c8f327
Cosmin Tanislav <demonsingur@gmail.com>
2025-06-11 21:54:06 +0300

    PhoneWindowManager: implement search button play/pause
    Change-Id: I27d15b6863913d4b51d1026eba52b1ef731aebcb

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2371,6 +2371,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KILL_APP:
                 ActionUtils.killForegroundApp(mContext, mCurrentUserId);
                 break;
+            case PLAY_PAUSE_MUSIC:
+                triggerVirtualKeypress(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+                break;
             default:
                 break;
         }
