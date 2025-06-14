e09fab897830e841298a9d2021b04b815ba6bbb3
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-14 21:11:45 +0800

    SystemUI: fix doze/keyguard issues
    Change-Id: Id610e500e0f7a0ffdb4978a222501e78d98ab917
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSImpl.java
@@ -403,6 +403,17 @@ public class QSImpl implements QS, CommandQueue.Callbacks, StatusBarStateControl
             }
         }
         updateQsState();
+        resetOverScrollAmountIfNeeded();
+    }
+
+    public final void resetOverScrollAmountIfNeeded() {
+        View qs = getView();
+        if (qs == null || qs.getTranslationY() == 0.0f 
+                || mBypassController.getBouncerShowing() 
+                || mQsVisible || mStatusBarState == SHADE_LOCKED) {
+            return;
+        }
+        setOverScrollAmount(0);
     }
 
     @Override
