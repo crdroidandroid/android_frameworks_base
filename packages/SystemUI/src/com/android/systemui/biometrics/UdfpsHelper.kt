491f6d4c33d2a98206289190b7d1e7a883eaefd1
pjgowtham <pjgowtham@gmail.com>
2025-06-22 11:00:08 +0530

    UdfpsHelper: Track brightness mirror to hide/show Udfps dim layer
    This provides a much deeper integration with Udfps dim layer and
    the new brightness slider such that we don't need to look for APIs
    that can track temporary brightness. That is because when the
    brightness mirror is shown, there is no necessity for the Udfps
    dim layer to be present. This change also isn't functional with the
    legacy slider.
    
    It also fixes the issue where the dim layer is hidden with
    QS expansion -> Device Control where the alternate bouncer is brought up.
    
    Tests : Ensure that appropriate transparency of the dim layer is set
    when dragging or selecting the preferred brightness with the slider.
    
    Change-Id: I53f78cbcfd3b42a3b5dbe6110424687ebfac102d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsHelper.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsHelper.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright (C) 2024 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.android.systemui.biometrics
@@ -32,6 +21,7 @@ import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.repeatOnLifecycle
 import com.android.systemui.lifecycle.repeatWhenAttached
+import com.android.systemui.settings.brightness.domain.interactor.BrightnessMirrorShowingInteractor
 import com.android.systemui.shade.domain.interactor.ShadeInteractor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
@@ -49,6 +39,7 @@ class UdfpsHelper(
     private val windowManager: WindowManager,
     private val shadeInteractor: ShadeInteractor,
     @RequestReason val requestReason: Int,
+    private val brightnessMirrorShowingInteractor: BrightnessMirrorShowingInteractor,
     private var view: View = View(context).apply {
         setBackgroundColor(Color.BLACK)
         visibility = View.GONE
@@ -56,7 +47,6 @@ class UdfpsHelper(
 ) {
     private val displayManager = context.getSystemService(DisplayManager::class.java)!!
     private val isKeyguard = requestReason == REASON_AUTH_KEYGUARD
-    private var newIsQsExpanded = false
 
     private val currentBrightness: Float get() =
         displayManager.getBrightness(Display.DEFAULT_DISPLAY)
@@ -165,16 +155,23 @@ class UdfpsHelper(
     fun addDimLayer() {
         brightnessToAlpha()
         windowManager.addView(view, dimLayoutParams)
+        displayManager.registerDisplayListener(
+            displayListener,
+            null,
+            /* eventFlags */ 0,
+            DisplayManager.PRIVATE_EVENT_TYPE_DISPLAY_BRIGHTNESS,
+        )
     }
 
     fun removeDimLayer() {
         windowManager.removeView(view)
+        displayManager.unregisterDisplayListener(displayListener)
     }
 
     init {
         view.repeatWhenAttached {
             repeatOnLifecycle(Lifecycle.State.CREATED) {
-                listenForQsExpansion(this)
+                listenForBrightnessMirror(this)
 
                 if (isKeyguard) {
                     listenForShadeTouchability(this)
@@ -187,25 +184,10 @@ class UdfpsHelper(
         }
     }
 
-    // We don't have ways to get temporary brightness when operating the brightness slider.
-    // Therefore, the dim layer is hidden when the slider is expected to be utilized.
-    private suspend fun listenForQsExpansion(scope: CoroutineScope): Job {
+    private suspend fun listenForBrightnessMirror(scope: CoroutineScope): Job {
         return scope.launch {
-            shadeInteractor.qsExpansion.collect { qsExpansion ->
-                if (qsExpansion == 1f && !newIsQsExpanded) {
-                    newIsQsExpanded = true
-                    displayManager.registerDisplayListener(
-                        displayListener,
-                        null,
-                        /* eventFlags */ 0,
-                        DisplayManager.PRIVATE_EVENT_TYPE_DISPLAY_BRIGHTNESS,
-                    )
-                    view.isVisible = false
-                } else if (qsExpansion == 0f && newIsQsExpanded) {
-                    newIsQsExpanded = false
-                    displayManager.unregisterDisplayListener(displayListener)
-                    view.isVisible = true
-                }
+            brightnessMirrorShowingInteractor.isShowing.collect {
+                view.isVisible = !it
             }
         }
     }
