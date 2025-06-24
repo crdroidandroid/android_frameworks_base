53d2bd1ebaa1e5b5bdcbd213648e8539bd314cfb
Dmitrii <bankersenator@gmail.com>
2025-06-24 00:20:35 +0000

    SystemUI: port volte/vowifi icons to A16 kairos impl
    neobuddy89: Updated for A16 QPR1
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/data/repository/demo/DemoMobileConnectionRepositoryKairos.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/data/repository/demo/DemoMobileConnectionRepositoryKairos.kt
@@ -36,6 +36,7 @@ import com.android.systemui.kairos.util.nameTag
 import com.android.systemui.kairosBuilder
 import com.android.systemui.log.table.TableLogBuffer
 import com.android.systemui.log.table.logDiffsForTable
+import com.android.systemui.statusbar.pipeline.ims.data.model.ImsStateModel
 import com.android.systemui.statusbar.pipeline.mobile.data.model.DataConnectionState
 import com.android.systemui.statusbar.pipeline.mobile.data.model.NetworkNameModel
 import com.android.systemui.statusbar.pipeline.mobile.data.model.ResolvedNetworkType
@@ -402,6 +403,8 @@ class DemoMobileConnectionRepositoryKairos(
         lastEvent.map { it.firstOrNull()?.slice ?: false }
 
     override val isInEcmMode: State<Boolean> = stateOf(false)
+
+    override val imsState: State<ImsStateModel> = stateOf(ImsStateModel())
 
     private fun TransactionScope.resolvedNetworkTypeForIconGroup(
         iconGroup: SignalIcon.MobileIconGroup?
