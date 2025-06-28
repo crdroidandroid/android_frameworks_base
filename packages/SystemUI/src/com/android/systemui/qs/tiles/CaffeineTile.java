3e7dac098ecd7fb7a3c760871504ca42b1e25ae4
Pranav Vashi <neobuddy89@gmail.com>
2025-06-29 01:48:05 +0530

    SystemUI: tiles: Preload all icon drawable for custom tiles
    Ref: ce8bee1b93af62a57097ca60c36265488a1d9371
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -53,7 +53,8 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     public static final String TILE_SPEC = "caffeine";
 
-    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_caffeine);
+    @Nullable
+    private Icon mIcon = null;
 
     private final PowerManager.WakeLock mWakeLock;
     private int mSecondsRemaining;
@@ -216,8 +217,12 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
         state.value = mWakeLock.isHeld();
-        state.icon = mIcon;
         state.label = mContext.getString(R.string.quick_settings_caffeine_label);
+        if (mIcon == null) {
+            mIcon = maybeLoadResourceIcon(
+                    R.drawable.ic_qs_caffeine);
+        }
+        state.icon = mIcon;
         state.hasLongClickEffect = false;
         if (state.value) {
             state.secondaryLabel = formatValueWithRemainingTime();
