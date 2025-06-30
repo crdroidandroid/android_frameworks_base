5c448b1ea57103b8d1ae2e78c91cc6692e70783c
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-30 17:43:55 +0800

    SystemUI: blur rendering enhancements
    Change-Id: I024055b5ce6e0dc080e74b4d8b5598c05a2a56ba

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -6080,7 +6080,7 @@ public class NotificationStackScrollLayout
     private void updateBlurEffect() {
         if (mBlurRadius > 0) {
             mBlurEffect =
-                    RenderEffect.createBlurEffect(mBlurRadius, mBlurRadius, Shader.TileMode.CLAMP);
+                    RenderEffect.createBlurEffect(mBlurRadius, mBlurRadius, Shader.TileMode.MIRROR);
             spewLog("Setting up blur RenderEffect for NotificationStackScrollLayout");
         } else {
             spewLog("Clearing the blur RenderEffect setup for NotificationStackScrollLayout");
