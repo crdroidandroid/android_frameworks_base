06794a5fa41aa33605f8739586214c83b191c2d6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-05 06:43:05 +0800

    SystemUI: Add lockscreen media art
    Change-Id: I4db66c3e1e98c1ac04a315e8e76ec3799e8f4cbd
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -25,6 +25,7 @@ import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dagger.qualifiers.PerUser;
 import com.android.systemui.dump.DumpManager;
 import com.android.systemui.keyguard.KeyguardSliceProvider;
+import com.android.systemui.media.MediaViewController;
 import com.android.systemui.people.PeopleProvider;
 import com.android.systemui.startable.Dependencies;
 import com.android.systemui.statusbar.NotificationInsetsModule;
@@ -205,4 +206,7 @@ public interface SysUIComponent {
 
     @SysUISingleton
     NTForbiddenSwipeDownQSController forbiddenSwipeDownQSController();
+
+    @SysUISingleton
+    MediaViewController mediaViewController();
 }
