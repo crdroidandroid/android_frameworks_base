fec114dcb2483fddc814dd24bc4ecd9992e4d28d
Fred Underwood <underwoodfred@proton.me>
2025-07-05 07:30:30 +1000

    SystemUI: Fix broken keyguard slice view
    Upstream refactor in commit 1f73b12d5dd4db6ebcad5d444c79ac18cc59e3c3
    removed too much and as a result the legacy Keyguard slice code was
    broken. This adds back the needed elements.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -29,8 +29,10 @@ import com.android.systemui.customization.R as customR
 import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.dump.DumpManager
+import com.android.systemui.keyguard.domain.interactor.KeyguardInteractor
 import com.android.systemui.keyguard.shared.model.KeyguardSection
 import com.android.systemui.plugins.ActivityStarter
+import com.android.systemui.power.domain.interactor.PowerInteractor
 import com.android.systemui.res.R
 import com.android.systemui.settings.DisplayTracker
 import com.android.systemui.statusbar.lockscreen.LockscreenSmartspaceController
@@ -49,6 +51,8 @@ constructor(
     val configurationController: ConfigurationController,
     val dumpManager: DumpManager,
     val displayTracker: DisplayTracker,
+    val keyguardInteractor: KeyguardInteractor,
+    val powerInteractor: PowerInteractor,
 ) : KeyguardSection() {
     private lateinit var sliceView: KeyguardSliceView
 
@@ -72,6 +76,8 @@ constructor(
                 configurationController,
                 dumpManager,
                 displayTracker,
+                keyguardInteractor,
+                powerInteractor,
             )
         controller.init()
     }
