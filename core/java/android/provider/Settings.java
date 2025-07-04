f0ddc3842e83792e2696e0289150b3b7283ad521
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-05 06:43:05 +0800

    SystemUI: Add lockscreen media art
    Squashed:
    
        From: rmp22 <195054967+rmp22@users.noreply.github.com>
        Date: Thu, 30 Oct 2025 19:58:37 +0800
        Subject: [PATCH] [media_art] removing retry mechanism
    
        Change-Id: Id59bd09fd112caf7f6e3d3aef06066f94ee41faf
    
    Change-Id: I4db66c3e1e98c1ac04a315e8e76ec3799e8f4cbd
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7277,6 +7277,12 @@ public final class Settings {
         public static final String QQS_TILES_ROWS = "qqs_tiles_rows";
 
         /**
+         * Lockscreen Media Art
+         * @hide
+         */
+        public static final String LS_MEDIA_ART_ENABLED = "ls_media_art_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
