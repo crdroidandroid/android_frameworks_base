0ad099827ce1d7499d92b0f71cc5f51aedcb7697
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-18 20:44:16 +0800

    SystemUI: Add pulse visualizer
    neobuddy89:
    * Add Settings keys in Settings.java
    * Fix for multi-user usage
    * Disable rounded corners by default
    
    Change-Id: Ic7b2353d9214c3ecd3b9438e0be0222e509b52de
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -168,6 +168,7 @@ import com.android.systemui.plugins.PluginManager;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.domain.interactor.PowerInteractor;
+import com.android.systemui.pulse.PulseViewController;
 import com.android.systemui.qs.QSFragmentLegacy;
 import com.android.systemui.qs.QSPanelController;
 import com.android.systemui.qs.composefragment.QSFragmentCompose;
@@ -1160,6 +1161,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                         mNotificationShadeWindowController.setRequestTopUi(
                                 requestTopUi, componentTag))));
         getNotifContainerParentView().addView(MediaViewController.get(mContext).getMediaArtScrim(), 0);
+        getNotifContainerParentView().addView(PulseViewController.get(mContext).getPulseView(), 1);
     }
 
     private ViewGroup getNotifContainerParentView() {
