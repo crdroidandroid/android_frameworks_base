4b899c24e5bc1ebbe4dd2ea2d4334e88182f44d1
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-18 20:44:16 +0800

    SystemUI: Add pulse visualizer
    neobuddy89:
    * Add Settings keys in Settings.java
    * Fix for multi-user usage
    * Disable rounded corners by default
    
    Change-Id: Ic7b2353d9214c3ecd3b9438e0be0222e509b52de
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -166,6 +166,7 @@ import com.android.systemui.plugins.PluginManager;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.domain.interactor.PowerInteractor;
+import com.android.systemui.pulse.PulseViewController;
 import com.android.systemui.qs.QSFragmentLegacy;
 import com.android.systemui.qs.QSPanelController;
 import com.android.systemui.qs.composefragment.QSFragmentCompose;
@@ -484,6 +485,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     private final TunerService mTunerService;
     private final ActivityStarter mActivityStarter;
     private final MediaViewController mMediaViewController;
+    private final PulseViewController mPulseViewController;
 
     private final DisplayMetrics mDisplayMetrics;
 
@@ -758,7 +760,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             WindowManager windowManager,
             WindowManagerProvider windowManagerProvider,
             TunerService tunerService,
-            MediaViewController mediaViewController
+            MediaViewController mediaViewController,
+            PulseViewController pulseViewController
     ) {
         mContext = context;
         mNotificationsController = notificationsController;
@@ -907,6 +910,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mWindowManager = windowManager;
         mWindowManagerProvider = windowManagerProvider;
         mMediaViewController = mediaViewController;
+        mPulseViewController = pulseViewController;
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1149,6 +1153,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                         () -> mTopUiController.setRequestTopUi(requestTopUi, componentTag)
                 )));
         getNotifContainerParentView().addView(mMediaViewController.getMediaArtScrim(), 0);
+        getNotifContainerParentView().addView(mPulseViewController.getPulseView(), 1);
     }
 
     private ViewGroup getNotifContainerParentView() {
