94c6bb57f999a3722a07836bac1f9a6cf22080c6
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-18 20:44:16 +0800

    SystemUI: Add pulse visualizer
    neobuddy89:
    * Add Settings keys in Settings.java
    * Fix for multi-user usage
    * Disable rounded corners by default
    
    Change-Id: Ic7b2353d9214c3ecd3b9438e0be0222e509b52de
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13925,6 +13925,27 @@ public final class Settings {
         public static final String ENABLE_LOCKSCREEN_QUICK_SETTINGS = "enable_lockscreen_quick_settings";
 
         /**
+         * Pulse lockscreen music visualizer
+         * @hide
+         */
+        public static final String LOCKSCREEN_PULSE_ENABLED = "lockscreen_pulse_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String PULSE_BAR_COUNT = "pulse_bar_count";
+
+        /**
+         * @hide
+         */
+        public static final String PULSE_ROUNDED_BARS = "pulse_rounded_bars";
+
+        /**
+         * @hide
+         */
+        public static final String PULSE_COLOR = "pulse_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
