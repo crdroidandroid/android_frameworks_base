a90a9e8f14da9b086885aa4acfb757d60c241878
Joey Huab <jhuab@edukasyon.ph>
2025-07-24 10:28:44 +0900

    Revert "Biometrics: Hook up support for halHandlesDisplayTouches prop"
    * Breaks UDFPS for some tensor devices.
    
    This reverts commit 20ad479f7e02990a497f41c2270b7c15eb57c48f.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/AuthService.java
+++ services/core/java/com/android/server/biometrics/AuthService.java
@@ -1013,7 +1013,6 @@ public class AuthService extends SystemService {
             return new FingerprintSensorPropertiesInternal(sensorId,
                     Utils.authenticatorStrengthToPropertyStrength(strength), maxEnrollmentsPerUser,
                     componentInfo, sensorType, true /* halControlsIllumination */,
-                    false /* halHandlesDisplayTouches */,
                     resetLockoutRequiresHardwareAuthToken,
                     List.of(new SensorLocationInternal("" /* display */, udfpsProps[0],
                             udfpsProps[1], udfpsProps[2])));
--- services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
@@ -758,9 +758,6 @@ public class FingerprintProvider implements IBinder.DeathRecipient, ServiceProvi
 
     @Override
     public void onPointerDown(long requestId, int sensorId, PointerContext pc) {
-        if (mFingerprintSensors.get(sensorId).getSensorProperties().halHandlesDisplayTouches) {
-            return;
-        }
         mFingerprintSensors.get(sensorId).getScheduler().getCurrentClientIfMatches(
                 requestId, (client) -> {
                     if (!(client instanceof Udfps)) {
@@ -773,9 +770,6 @@ public class FingerprintProvider implements IBinder.DeathRecipient, ServiceProvi
 
     @Override
     public void onPointerUp(long requestId, int sensorId, PointerContext pc) {
-        if (mFingerprintSensors.get(sensorId).getSensorProperties().halHandlesDisplayTouches) {
-            return;
-        }
         mFingerprintSensors.get(sensorId).getScheduler().getCurrentClientIfMatches(
                 requestId, (client) -> {
                     if (!(client instanceof Udfps)) {
