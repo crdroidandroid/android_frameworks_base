7d54ef823ec8fbb62d9d558b67ce958b531a5691
Pranav Vashi <neobuddy89@gmail.com>
2025-07-26 20:49:53 +0530

    services: Implement exit app animation boost
    * Re-write version for A16.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -86,7 +86,9 @@ import android.content.res.Configuration;
 import android.graphics.Insets;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.hardware.power.Boost;
 import android.os.IBinder;
+import android.os.PowerManagerInternal;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.util.ArraySet;
@@ -103,6 +105,7 @@ import android.window.TaskFragmentOrganizerToken;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.protolog.ProtoLog;
 import com.android.internal.util.ToBooleanFunction;
+import com.android.server.LocalServices;
 import com.android.server.am.HostingRecord;
 import com.android.server.pm.pkg.AndroidPackage;
 import com.android.window.flags.Flags;
@@ -420,6 +423,8 @@ class TaskFragment extends WindowContainer<WindowContainer> {
     private final EnsureActivitiesVisibleHelper mEnsureActivitiesVisibleHelper =
             new EnsureActivitiesVisibleHelper(this);
 
+     private final PowerManagerInternal mPowerManagerInternal;
+
     /** Creates an embedded task fragment. */
     TaskFragment(ActivityTaskManagerService atmService, IBinder fragmentToken,
             boolean createdByOrganizer) {
@@ -440,6 +445,7 @@ class TaskFragment extends WindowContainer<WindowContainer> {
                 mAtmService.mWindowOrganizerController.mTaskFragmentOrganizerController;
         mFragmentToken = fragmentToken;
         mRemoteToken = new RemoteToken(this);
+        mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
     }
 
     @NonNull
@@ -1603,6 +1609,7 @@ class TaskFragment extends WindowContainer<WindowContainer> {
         }
 
         if (anim) {
+            setActivityBoost();
             next.applyOptionsAnimation();
         } else {
             next.abortAndClearOptionsAnimation();
@@ -1755,6 +1762,13 @@ class TaskFragment extends WindowContainer<WindowContainer> {
         }
 
         return true;
+    }
+
+    private void setActivityBoost() {
+        if (mPowerManagerInternal != null) {
+            mPowerManagerInternal.setPowerBoost(Boost.INTERACTION, 80);
+            mPowerManagerInternal.setPowerBoost(Boost.DISPLAY_UPDATE_IMMINENT, 80);
+        }
     }
 
     /** Likely app process has been killed. Needs to restart this activity. */
