e073a1218d55f0da9caea2e83e8b123085de1dd1
Pranav Vashi <neobuddy89@gmail.com>
2025-08-18 18:14:10 +0530

    SystemUI: Fix up OmniJaws weather view on lockscreen
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardRootViewBinder.kt
@@ -175,6 +175,7 @@ object KeyguardRootViewBinder {
                             view.alpha = alpha
                             childViews[burnInLayerId]?.alpha = alpha
                             childViews[sliceViewId]?.alpha = alpha
+                            childViews[weatherAreaId]?.alpha = alpha
                         }
                     }
 
@@ -192,6 +193,7 @@ object KeyguardRootViewBinder {
                         viewModel.translationY.collect { y ->
                             childViews[burnInLayerId]?.translationY = y
                             childViews[sliceViewId]?.translationY = y
+                            childViews[weatherAreaId]?.translationY = y
                             childViews[largeClockId]?.translationY = y
                             if (com.android.systemui.shared.Flags.clockReactiveSmartspaceLayout()) {
                                 childViews[largeClockDateId]?.translationY = y
@@ -209,6 +211,7 @@ object KeyguardRootViewBinder {
                                     // Large Clock is not translated in the x direction
                                     childViews[burnInLayerId]?.translationX = px
                                     childViews[sliceViewId]?.translationX = px
+                                    childViews[weatherAreaId]?.translationX = px
                                     childViews[aodPromotedNotificationId]?.translationX = px
                                     childViews[aodNotificationIconContainerId]?.translationX = px
                                 }
@@ -267,6 +270,7 @@ object KeyguardRootViewBinder {
                         viewModel.burnInLayerVisibility.collect { visibility ->
                             childViews[burnInLayerId]?.visibility = visibility
                             childViews[sliceViewId]?.visibility = visibility
+                            childViews[weatherAreaId]?.visibility = visibility
                         }
                     }
 
@@ -598,6 +602,7 @@ object KeyguardRootViewBinder {
 
     private val burnInLayerId = R.id.burn_in_layer
     private val sliceViewId = R.id.keyguard_slice_view
+    private val weatherAreaId = R.id.keyguard_weather_area
     private val aodPromotedNotificationId = AodPromotedNotificationSection.viewId
     private val aodNotificationIconContainerId = R.id.aod_notification_icon_container
     private val largeClockId = customR.id.lockscreen_clock_view_large
