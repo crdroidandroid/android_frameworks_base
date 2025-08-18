53e2bff1ab95356ab3fb43880b036b1a885598db
Abhay Singh Gill <abhaygill017@gmail.com>
2025-08-18 19:37:37 +0000

    base: Add the ability to generate key pairs for keybox spoofing
    The code is used from :- https://github.com/5ec1cff/TrickyStore
    
    Needs :-
    HELLBOY017/external_bouncycastle@f70ea01
    
    This allows us to enjoy spoofing keyboxes the right way even with broken TEE.
    
    Change-Id: Id196dd58c21ca303966187466af0f82b875968a8
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- keystore/java/android/security/KeyStoreSecurityLevel.java
+++ keystore/java/android/security/KeyStoreSecurityLevel.java
@@ -19,6 +19,7 @@ package android.security;
 import android.annotation.NonNull;
 import android.app.compat.CompatChanges;
 import android.hardware.security.keymint.KeyParameter;
+import android.hardware.security.keymint.Tag;
 import android.os.Binder;
 import android.os.RemoteException;
 import android.os.ServiceSpecificException;
@@ -32,6 +33,8 @@ import android.system.keystore2.KeyDescriptor;
 import android.system.keystore2.KeyMetadata;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
+
+import com.android.internal.util.crdroid.KeyboxImitationHooks;
 
 import java.util.Calendar;
 import java.util.Collection;
@@ -146,6 +149,19 @@ public class KeyStoreSecurityLevel {
             throws KeyStoreException {
         StrictMode.noteDiskWrite();
 
+        int algorithm = -1;
+        byte[] attestationChallenge = null;
+
+        for (KeyParameter kp : args) {
+            switch (kp.tag) {
+                case Tag.ALGORITHM -> algorithm = kp.value.getAlgorithm();
+                case Tag.ATTESTATION_CHALLENGE -> attestationChallenge = kp.value.getBlob();
+            }
+        }
+
+        KeyboxImitationHooks.putAlgo(algorithm);
+        KeyboxImitationHooks.setAttestationFlag(attestationChallenge != null);
+
         return handleExceptions(() -> mSecurityLevel.generateKey(
                 descriptor, attestationKey, args.toArray(new KeyParameter[args.size()]),
                 flags, entropy));
@@ -223,5 +239,9 @@ public class KeyStoreSecurityLevel {
         if (wasInterrupted) {
             Thread.currentThread().interrupt();
         }
+    }
+
+    public IKeystoreSecurityLevel getBinderInterface() {
+        return mSecurityLevel;
     }
 }
