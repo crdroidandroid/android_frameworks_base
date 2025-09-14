2ee2dcb3e27c36487fb2ba1f41f06e68cb5e66cc
Pranav Vashi <neobuddy89@gmail.com>
2025-09-14 20:45:48 +0530

    SystemUI: Add toggle for QS tile haptic feedback
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7241,6 +7241,12 @@ public final class Settings {
         public static final String QS_BRIGHTNESS_SLIDER_SHAPE = "qs_brightness_slider_shape";
 
         /**
+         * Haptic feedback on QS tiles
+         * @hide
+         */
+        public static final String QS_TILE_HAPTIC = "qs_tile_haptic";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
