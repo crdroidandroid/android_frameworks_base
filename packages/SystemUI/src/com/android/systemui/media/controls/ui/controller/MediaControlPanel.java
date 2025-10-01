b540e66d82be7a06d59ab65753d589a02fbd113c
Pranav Vashi <neobuddy89@gmail.com>
2025-10-01 10:44:58 +0530

    MediaArt: Utilities: Update for media control
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/MediaControlPanel.java
@@ -899,7 +899,6 @@ public class MediaControlPanel {
             int screenWidth = bounds.width();
             int screenHeight = bounds.height();
             Drawable albumArt = getScaledBackground(artworkIcon, screenWidth, screenHeight);
-            MediaSessionManager.Companion.get().onAlbumArtChanged(albumArt);
             WallpaperColors wallpaperColors = getWallpaperColor(artworkIcon);
             boolean darkTheme = false;
             if (wallpaperColors != null) {
@@ -922,8 +921,6 @@ public class MediaControlPanel {
                 }
             }
 
-            MediaSessionManager.Companion.get().onMediaColorsChanged(mutableColorScheme.getAccent1().getS100());
-
             final ColorScheme colorScheme = mutableColorScheme;
             mMainExecutor.execute(() -> {
                 // Cancel the request if a later one arrived first
@@ -932,6 +929,9 @@ public class MediaControlPanel {
                     return;
                 }
                 mArtworkBoundId = reqId;
+
+                MediaSessionManager.Companion.get().onAlbumArtChanged(albumArt);
+                MediaSessionManager.Companion.get().onMediaColorsChanged(colorScheme.getAccent1().getS100());
 
                 // Transition Colors to current color scheme
                 boolean colorSchemeChanged;
