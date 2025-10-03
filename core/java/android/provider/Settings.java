51d6458c3bc47e53736e755a0e3b0bc124176169
Pranav Vashi <neobuddy89@gmail.com>
2025-10-04 01:57:10 +0530

    Pulse: Add fading block renderer
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13946,6 +13946,11 @@ public final class Settings {
         public static final String PULSE_COLOR = "pulse_color";
 
         /**
+         * @hide
+         */
+        public static final String PULSE_RENDERER = "pulse_renderer";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
