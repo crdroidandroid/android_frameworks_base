caef2f1064c6449d40d47b8a0b82ced7b64130e5
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-04 01:50:21 +0530

    SystemUI: Revamp Network Traffic
    * Remove arrows (They waste space and by using wrap content they overlap other icons)
    * Don't add network traffic slot at the start, add it right before the wifi slot so that it no longer gets cut by cutout space
    * Fix network traffic being visible even after turning it off (Use removeIcon instead of setIconVisbility)
    * Make sure NetworkTraffic's content tint updates in accordance with the other icons
    * Give it a fixed width
    
    neobuddy89:
    * Drop removal of arrows, update tint for drawable as well.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -375,6 +375,8 @@ constructor(
             Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimaryInverse)
 
         iconManager.setTint(fgColor, bgColor)
+        iconContainer.setIsUsingQs(true)
+        iconContainer.setNetworkTrafficColor(fgColor)
 
         if (!NewStatusBarIcons.isEnabled) {
             batteryIcon.isVisible = true
@@ -471,6 +473,8 @@ constructor(
 
         updateQsBatteryStyle()
 
+        iconContainer.setIsUsingQs(true)
+
         tunerService.addTunable(this, QS_BATTERY_STYLE)
         tunerService.addTunable(this, STATUS_BAR_BATTERY_STYLE)
         tunerService.addTunable(this, QS_SHOW_BATTERY_PERCENT)
@@ -485,6 +489,7 @@ constructor(
         statusBarIconController.removeIconGroup(iconManager)
         nextAlarmController.removeCallback(nextAlarmCallback)
         systemIconsHoverContainer.setOnHoverListener(null)
+        iconContainer.setIsUsingQs(false)
         tunerService.removeTunable(this)
     }
 
