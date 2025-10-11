b856b059b2a7d0350770df351f3c15bc172436ab
Pranav Vashi <neobuddy89@gmail.com>
2025-10-11 23:21:05 +0530

    EdgeLight: Allow customizing pulse count
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/edgelight/EdgeLightViewController.kt
+++ packages/SystemUI/src/com/android/systemui/edgelight/EdgeLightViewController.kt
@@ -89,6 +89,7 @@ constructor(
                 edgeLightView.visible = false
             } else {
                 edgeLightView.paintColor = getColor()
+                edgeLightView.userPulseCount = currentSettings.pulseCount
             }
         }
     }
