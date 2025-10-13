08bdb43b1f5b0b08c1eb62f3e6e199a86eaadbe3
Pranav Vashi <neobuddy89@gmail.com>
2025-10-13 15:42:01 +0530

    SystemUI: Improve guarding header image
    * No functionality change but slight perf improvement.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -540,6 +540,7 @@ public final class NotificationPanelViewController implements
     private FadingEdgeLayout mQsHeaderLayout;
     private ImageView mQsHeaderImageView;
     private boolean mHeaderImageEnabled;
+    private int mOrientation = -1;
     private int mHeaderImageHeight;
     private int mBottomFadeHeight;
     private int mHeaderImageShadow;
@@ -3598,6 +3599,7 @@ public final class NotificationPanelViewController implements
                 updateResources();
             }
             if (mHeaderImageEnabled) {
+                mOrientation = newConfig.orientation;
                 mView.post(() -> updateHeaderImage());
             }
         }
@@ -3798,18 +3800,27 @@ public final class NotificationPanelViewController implements
                 case STATUS_BAR_CUSTOM_HEADER:
                     mHeaderImageEnabled =
                             TunerService.parseIntegerSwitch(newValue, false);
-                    mView.post(() -> updateHeaderImage());
+                    if (!mHeaderImageEnabled) {
+                        mQsHeaderLayout.setVisibility(View.GONE);
+                    } else {
+                        mOrientation = mView.getResources().getConfiguration().orientation;
+                        mView.post(() -> updateHeaderImage());
+                    }
                     break;
                 case STATUS_BAR_CUSTOM_HEADER_HEIGHT:
                     mHeaderImageHeight =
                             TunerService.parseInteger(newValue, 142);
                     mBottomFadeHeight = (int) Math.round(mHeaderImageHeight * 0.555);
-                    mView.post(() -> updateHeaderImage());
+                    if (mHeaderImageEnabled) {
+                        mView.post(() -> updateHeaderImage());
+                    }
                     break;
                 case STATUS_BAR_CUSTOM_HEADER_SHADOW:
                     mHeaderImageShadow =
                             TunerService.parseInteger(newValue, 0);
-                    mView.post(() -> updateHeaderImage());
+                    if (mHeaderImageEnabled) {
+                        mView.post(() -> updateHeaderImage());
+                    }
                     break;
                 default:
                     break;
@@ -4487,14 +4498,15 @@ public final class NotificationPanelViewController implements
             mCurrentBackground = null;
             mQsHeaderImageView.setVisibility(View.GONE);
         }
-        updateHeaderImage();
+        if (mHeaderImageEnabled) {
+            updateHeaderImage();
+        }
     }
 
     private void updateHeaderImage() {
-        Configuration config = mView.getResources().getConfiguration();
         float shadeHeaderExpansion = mShadeHeaderController.getShadeExpandedFraction();
-        if (mHeaderImageEnabled && shadeHeaderExpansion > 0f &&
-            config.orientation != Configuration.ORIENTATION_LANDSCAPE &&
+        if (shadeHeaderExpansion > 0f &&
+            mOrientation != Configuration.ORIENTATION_LANDSCAPE &&
             mCurrentBackground != null && mQsHeaderImageView.getDrawable() != null) {
 
             if (mQsHeaderLayout.getVisibility() != View.VISIBLE) {
