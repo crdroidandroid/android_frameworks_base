b51dd34b81e94a7c5dc936e90d5bc138a54ebcfa
Ghosuto <clash.raja10@gmail.com>
2025-10-17 14:30:41 +0000

    EdgeLight: Add 4-side rounded style option
    Change-Id: Ie2a8efc57d1a28308bceee35ec62b818190988cd
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7328,6 +7328,12 @@ public final class Settings {
         public static final String EDGE_LIGHT_STROKE_WIDTH = "edge_light_stroke_width";
 
         /**
+         * Edge light style
+         * @hide
+         */
+        public static final String EDGE_LIGHT_STYLE = "edge_light_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
