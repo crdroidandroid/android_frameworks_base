6aa845d0e0be8f406498b18612a9f85c95a857de
Ghosuto <clash.raja10@gmail.com>
2025-10-17 14:30:41 +0000

    EdgeLight: Add 4-side rounded style option
    Change-Id: Ie2a8efc57d1a28308bceee35ec62b818190988cd
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/edgelight/EdgeLightViewController.kt
+++ packages/SystemUI/src/com/android/systemui/edgelight/EdgeLightViewController.kt
@@ -91,6 +91,7 @@ constructor(
                 edgeLightView.paintColor = getColor()
                 edgeLightView.userPulseCount = currentSettings.pulseCount
                 edgeLightView.userStrokeWidth = currentSettings.strokeWidth
+                edgeLightView.edgeStyle = currentSettings.edgeStyle
             }
         }
     }
