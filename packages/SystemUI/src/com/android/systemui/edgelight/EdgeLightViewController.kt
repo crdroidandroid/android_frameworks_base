56cabd8de8b233a76f239513da08eecc36927198
Ghosuto <clash.raja10@gmail.com>
2025-10-19 08:36:04 +0000

    EdgeLight: Introduce EdgeLight animation style
    Change-Id: I133b28619d00254488815660e70e86c8f754aebc
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/edgelight/EdgeLightViewController.kt
+++ packages/SystemUI/src/com/android/systemui/edgelight/EdgeLightViewController.kt
@@ -93,6 +93,7 @@ constructor(
                 edgeLightView.userPulseCount = currentSettings.pulseCount
                 edgeLightView.userStrokeWidth = currentSettings.strokeWidth
                 edgeLightView.edgeStyle = currentSettings.edgeStyle
+                edgeLightView.animationEffect = currentSettings.animationEffect
             }
         }
     }
