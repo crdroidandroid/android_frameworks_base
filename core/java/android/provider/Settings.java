8e8d6fa92d6542ad16ee45dfe16e515f6cfdb3ef
Ghosuto <clash.raja10@gmail.com>
2025-10-19 08:36:04 +0000

    EdgeLight: Introduce EdgeLight animation style
    Change-Id: I133b28619d00254488815660e70e86c8f754aebc
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7334,6 +7334,12 @@ public final class Settings {
         public static final String EDGE_LIGHT_STYLE = "edge_light_style";
 
         /**
+         * Edge light animation effect type
+         * @hide
+         */
+        public static final String EDGE_LIGHT_ANIMATION_EFFECT = "edge_light_animation_effect";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
