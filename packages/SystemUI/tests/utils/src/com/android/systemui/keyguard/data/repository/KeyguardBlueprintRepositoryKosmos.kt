55b704df0caebfdd8f14baffe350f1a8ccbf6093
LuK1337 <priv.luk@gmail.com>
2025-10-21 10:22:45 +0200

    SystemUI: Add KeyguardSliceViewSection to split shade blueprint
    Change-Id: I829c3c88814a50ff7b562baba022898c43a44db5

--- packages/SystemUI/tests/utils/src/com/android/systemui/keyguard/data/repository/KeyguardBlueprintRepositoryKosmos.kt
+++ packages/SystemUI/tests/utils/src/com/android/systemui/keyguard/data/repository/KeyguardBlueprintRepositoryKosmos.kt
@@ -90,6 +90,7 @@ val Kosmos.splitShadeBlueprint by
             clockSection = keyguardClockSection,
             smartspaceSection = keyguardSmartspaceSection,
             mediaSection = mock(),
+            keyguardSliceViewSection = mock(),
             accessibilityActionsSection = mock(),
         )
     }
