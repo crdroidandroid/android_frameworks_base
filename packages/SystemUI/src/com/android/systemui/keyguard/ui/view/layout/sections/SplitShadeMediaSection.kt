a7624de619587dc9aa15d329011010331baec03c
LuK1337 <priv.luk@gmail.com>
2025-10-21 10:06:49 +0200

    SystemUI: Fix media section constraints with Smartspace disabled
    Change-Id: Iffdaa512298ebb9c0e674d9b8ee2ed9e62955501

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/SplitShadeMediaSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/SplitShadeMediaSection.kt
@@ -29,6 +29,7 @@ import androidx.constraintlayout.widget.ConstraintSet.START
 import androidx.constraintlayout.widget.ConstraintSet.TOP
 import com.android.systemui.customization.R as customR
 import com.android.systemui.keyguard.shared.model.KeyguardSection
+import com.android.systemui.keyguard.ui.viewmodel.KeyguardSmartspaceViewModel
 import com.android.systemui.media.controls.ui.controller.KeyguardMediaController
 import com.android.systemui.res.R
 import com.android.systemui.shade.ShadeDisplayAware
@@ -40,6 +41,7 @@ class SplitShadeMediaSection
 constructor(
     @ShadeDisplayAware private val context: Context,
     private val keyguardMediaController: KeyguardMediaController,
+    private val smartspaceViewModel: KeyguardSmartspaceViewModel,
 ) : KeyguardSection() {
     private val mediaContainerId = R.id.status_view_media_container
 
@@ -66,7 +68,11 @@ constructor(
         constraintSet.apply {
             constrainWidth(mediaContainerId, MATCH_CONSTRAINT)
             constrainHeight(mediaContainerId, WRAP_CONTENT)
-            connect(mediaContainerId, TOP, R.id.smart_space_barrier_bottom, BOTTOM)
+            if (smartspaceViewModel.isSmartspaceEnabled) {
+                connect(mediaContainerId, TOP, R.id.smart_space_barrier_bottom, BOTTOM)
+            } else {
+                connect(mediaContainerId, TOP, R.id.keyguard_slice_view, BOTTOM)
+            }
             connect(mediaContainerId, START, PARENT_ID, START)
             connect(mediaContainerId, END, R.id.split_shade_guideline, END)
         }
