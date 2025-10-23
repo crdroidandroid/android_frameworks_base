456e9d1c550ac30c4a526fdc6ae0b211bc64f2de
Aaron Okano <aaronokano@google.com>
2025-10-23 16:37:56 +0000

    Catch uncaught IllegalStateException
    MediaProvider may throw an IllegalStateException during some race
    conditions on the Screenshots folder. The exception is then passed over
    the binder and rethrown in the call to ContentResolver.insert. There's
    not a whole lot that can be done in this case, and error handling
    already seems to be in place in this function, so I think the best we
    can do is catch the exception and eat it with a warning log.
    
    Bug: 441090735
    Bug: 393608784
    Flag: EXEMPT CVE_FIX
    Test: Take a few screenshots
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:4e5e9b3725dc86b2e0acac9d3f652f45339a0c5a
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:ff11d3e1237516b82cb68cd737214eb26e787666
    Merged-In: I787ea2f2e449b1a88e8f0c33051bd2a5959970ac
    Change-Id: I787ea2f2e449b1a88e8f0c33051bd2a5959970ac

--- packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ImageExporter.java
@@ -395,8 +395,16 @@ public class ImageExporter {
             if (resultUri == null) {
                 // If file overwriting is disabled or there is no existing file to overwrite, create
                 // and insert a new entry.
-                resultUri = resolver.insert(uriWithUserId, values);
-                Log.d(TAG, "Inserted new URI: " + resultUri);
+                try {
+                    resultUri = resolver.insert(uriWithUserId, values);
+                    Log.d(TAG, "Inserted new URI: " + resultUri);
+                } catch (IllegalStateException e) {
+                    // A race on the screenshots directory may cause
+                    // this transaction to fail. Catch and eat the
+                    // exception and defer to the error handling
+                    // below.
+                    Log.w(TAG, "Failed to create new entry", e);
+                }
             }
 
             if (resultUri == null) {
