c5123254e83b7a7ff5611fba15b711679641d2ad
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-24 12:25:13 +0530

    SystemUI: Inflate QS on orientation change
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/fragments/FragmentHostManager.java
+++ packages/SystemUI/src/com/android/systemui/fragments/FragmentHostManager.java
@@ -62,7 +62,7 @@ public class FragmentHostManager {
     private final LeakDetector mLeakDetector;
     private final InterestingConfigChanges mConfigChanges = new InterestingConfigChanges(
             ActivityInfo.CONFIG_FONT_SCALE | ActivityInfo.CONFIG_LOCALE
-                    | ActivityInfo.CONFIG_ASSETS_PATHS);
+                    | ActivityInfo.CONFIG_ASSETS_PATHS | ActivityInfo.CONFIG_ORIENTATION);
     private final FragmentService mManager;
     private final ExtensionFragmentManager mPlugins = new ExtensionFragmentManager();
 
