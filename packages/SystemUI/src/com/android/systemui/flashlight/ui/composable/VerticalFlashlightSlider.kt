9acabe5b0247cc920c8136be2e4b49edd7cba5d0
Behnam Heydarshahi <bhnm@google.com>
2025-10-27 22:01:06 +0000

    Remove extra clip from flashlight slider
    Flag: com.android.systemui.flashlight_strength
    Bug: b/445908979
    Test: manual. open flashlight dialog and verify there is no extra cutout below the handle.
    Change-Id: I7ba4d7490efc81a6d6e893afe72b3f4f097ece8f
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/flashlight/ui/composable/VerticalFlashlightSlider.kt
+++ packages/SystemUI/src/com/android/systemui/flashlight/ui/composable/VerticalFlashlightSlider.kt
@@ -274,8 +274,7 @@ private fun TrapezoidTrack(
 
         Canvas(
             modifier =
-                modifier
-                    .width { (TRACK_LENGTH * (sliderState.value / maxSliderRange)).roundToPx() }
+                Modifier.width { (TRACK_LENGTH * (sliderState.value / maxSliderRange)).roundToPx() }
                     .height { canvasHeight.roundToPx() }
                     .testTag(TRACK_TEST_TAG)
                     .motionTestValues {
