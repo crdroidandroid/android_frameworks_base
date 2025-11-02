514607e147c1a1e153014de178c162a946dd3ec9
Pranav Vashi <neobuddy89@gmail.com>
2025-11-02 21:55:02 +0530

    SystemUI: Add defensive checks in ClockRegistry
    Log:
    
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): Failed to parse clock settings
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): java.lang.NullPointerException: Attempt to invoke virtual method 'int java.lang.String.length()' on a null object reference
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at org.json.JSONTokener.nextCleanInternal(JSONTokener.java:121)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at org.json.JSONTokener.nextValue(JSONTokener.java:98)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at org.json.JSONObject.<init>(JSONObject.java:168)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at org.json.JSONObject.<init>(JSONObject.java:185)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at com.android.systemui.shared.clocks.ClockRegistry.querySettings(go/retraceme 58953af1c47628b0bd6e0edf694cb559a1e6d114937272e2ebf36a10b1a129c4:24)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at com.android.systemui.shared.clocks.ClockRegistry$registerListeners$1.invokeSuspend(go/retraceme 58953af1c47628b0bd6e0edf694cb559a1e6d114937272e2ebf36a10b1a129c4:12)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith(go/retraceme 58953af1c47628b0bd6e0edf694cb559a1e6d114937272e2ebf36a10b1a129c4:8)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at kotlinx.coroutines.DispatchedTask.run(go/retraceme 58953af1c47628b0bd6e0edf694cb559a1e6d114937272e2ebf36a10b1a129c4:110)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:524)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at java.util.concurrent.FutureTask.run(FutureTask.java:317)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(ScheduledThreadPoolExecutor.java:348)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1156)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:651)
    10-04 22:58:17.482  7192  7230 E ClockRegistry (System): 	at java.lang.Thread.run(Thread.java:1119)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/customization/src/com/android/systemui/shared/clocks/ClockRegistry.kt
+++ packages/SystemUI/customization/src/com/android/systemui/shared/clocks/ClockRegistry.kt
@@ -288,6 +288,17 @@ open class ClockRegistry(
     var isRegistered: Boolean = false
         private set
 
+    private fun parseClockSettingsOrNull(raw: String?): ClockSettings? {
+        if (raw.isNullOrBlank()) return null
+        if (raw.equals("null", ignoreCase = true)) return null
+
+        return try {
+            ClockSettings.fromJson(JSONObject(raw))
+        } catch (_: Exception) {
+            null
+        }
+    }
+
     @OpenForTesting
     open fun querySettings() {
         assert.isNotMainThread()
@@ -306,7 +317,7 @@ open class ClockRegistry(
                             Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE,
                         )
                     }
-                json?.let { ClockSettings.fromJson(JSONObject(it)) }
+                parseClockSettingsOrNull(json)
             } catch (ex: Exception) {
                 logger.e("Failed to parse clock settings", ex)
                 null
