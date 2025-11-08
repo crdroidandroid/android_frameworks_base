dd134427a0222d508d8b8c5c059d4c8c1c1ea0b2
Pranav Vashi <neobuddy89@gmail.com>
2025-11-08 13:37:21 +0530

    SystemUI: Update carrier text and icons for modern views
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernShadeCarrierGroupMobileView.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernShadeCarrierGroupMobileView.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.statusbar.pipeline.mobile.ui.view
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
@@ -51,6 +52,14 @@ class ModernShadeCarrierGroupMobileView(context: Context, attrs: AttributeSet?) 
         return "ModernShadeCarrierGroupMobileView(" +
             "subId=$subId, " +
             "viewString=${super.toString()}"
+    }
+
+    fun setCarrierTextColor(color: Int) {
+        requireViewById<AutoMarqueeTextView>(R.id.mobile_carrier_text).setTextColor(color)
+    }
+
+    fun setImageTintList(colorStateList: ColorStateList) {
+        requireViewById<ModernStatusBarMobileView>(R.id.mobile_combo).setImageTintList(colorStateList)
     }
 
     fun updateTextAppearance(resId: Int) {
