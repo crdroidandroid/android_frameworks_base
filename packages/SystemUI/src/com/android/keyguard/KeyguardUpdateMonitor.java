24378b8b259a8136d6d9c928224339175dec17af
Pranav Vashi <neobuddy89@gmail.com>
2025-11-17 12:51:38 +0530

    ScrimUtils: Add notifier for user change
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2716,6 +2716,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
         public void onUserChanged(int newUser, Context userContext) {
             mHandler.sendMessage(mHandler.obtainMessage(MSG_USER_SWITCH_COMPLETE,
                     newUser, 0));
+            ScrimUtils.get().onUserChanged();
         }
     };
 
