e2bbc93e21c25addeb67c69e4b8c21cd566ddab7
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-11-19 19:20:20 +0000

    Introduce `Force LTE_CA` override on per subscription basis [1/3]
    Patches required in:
    - frameworks/base [1/3]
    - frameworks/opt/telephony [2/3]
    - packages/apps/settings [3/3]
    
    Workflow after this patch:
    ---------------------------------------------------------------------------------------------
    Hardware CA | Force LTE_CA Toggle | Code Path                                  | Icon Shows |
    ------------|---------------------|--------------------------------------------|------------|
    No          | Unset (null)        | Returns false                              | LTE        |
    No          | ON (true)           | Software-based Forced LTE-CA (mForceLteCA) | LTE+       |
    No          | OFF (false)         | Returns mForceLteCA (false)                | LTE        |
    Yes         | Unset (null)        | Returns false                              | LTE        |
    Yes         | ON (true)           | Returns mForceLteCA (true)                 | LTE+       |
    Yes         | OFF (false)         | Returns mForceLteCA (false)                | LTE        |
    ---------------------------------------------------------------------------------------------
    
    Change-Id: Ib8bdf4207fb55b80f829187328ec24ae2bbcdcf0
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -18548,6 +18548,30 @@ public final class Settings {
                 "preferred_network_mode";
 
         /**
+         * Force LTE Carrier Aggregation setting per SIM slot.
+         * Use FORCE_LTE_CA_0 for SIM slot 0 (first SIM)
+         * Use FORCE_LTE_CA_1 for SIM slot 1 (second SIM)
+         * 
+         * Type: int (0 = disabled, 1 = enabled)
+         * @hide
+         */
+        public static final String FORCE_LTE_CA = "force_lte_ca";
+
+        /**
+         * Force LTE Carrier Aggregation for SIM slot 0
+         * Type: int (0 = disabled, 1 = enabled)
+         * @hide
+         */
+        public static final String FORCE_LTE_CA_0 = "force_lte_ca_0";
+
+        /**
+         * Force LTE Carrier Aggregation for SIM slot 1
+         * Type: int (0 = disabled, 1 = enabled)
+         * @hide
+         */
+        public static final String FORCE_LTE_CA_1 = "force_lte_ca_1";
+
+        /**
          * Name of an application package to be debugged.
          */
         @Readable
