8cb02938e273dcd6ee629232601b73260cdc91c6
Pranav Vashi <neobuddy89@gmail.com>
2025-11-27 06:24:14 +0530

    Revert "SystemUI: Don't remove keyguard slice view"
    This reverts commit 088bfdbae8f876d25587de5b213f28ee23e5141a.

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -121,5 +121,9 @@ constructor(
         }
     }
 
-    override fun removeViews(constraintLayout: ConstraintLayout) {}
+    override fun removeViews(constraintLayout: ConstraintLayout) {
+        if (smartspaceController.isEnabled) return
+
+        constraintLayout.removeView(R.id.keyguard_slice_view)
+    }
 }
