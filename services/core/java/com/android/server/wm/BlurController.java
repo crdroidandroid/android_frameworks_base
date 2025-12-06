e9c30fe9cecb702a2a20c4da6c90849b72ac7404
John Galt <johngaltfirstrun@gmail.com>
2025-12-06 11:12:06 -0500

    Revert "Adjust the threshold for disabling blur on thermal status"
    This reverts commit a22611171fd334ea153dded3f310bffaa38e56ed.
    
    Just no.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/BlurController.java
+++ services/core/java/com/android/server/wm/BlurController.java
@@ -16,7 +16,7 @@
 
 package com.android.server.wm;
 
-import static android.os.PowerManager.THERMAL_STATUS_SEVERE;
+import static android.os.PowerManager.THERMAL_STATUS_CRITICAL;
 import static android.view.CrossWindowBlurListeners.CROSS_WINDOW_BLUR_SUPPORTED;
 
 import android.content.BroadcastReceiver;
@@ -46,7 +46,7 @@ final class BlurController {
     private final Object mLock = new Object();
     private volatile boolean mBlurEnabled;
     private boolean mInPowerSaveMode;
-    private boolean mDisabledByThermal;
+    private boolean mCriticalThermalStatus;
     private boolean mBlurDisabledSetting;
     private boolean mTunnelModeEnabled = false;
 
@@ -92,10 +92,10 @@ final class BlurController {
         mBlurDisabledSetting = getBlurDisabledSetting();
 
         powerManager.addThermalStatusListener((status) -> {
-            mDisabledByThermal = status >= THERMAL_STATUS_SEVERE;
+            mCriticalThermalStatus = status >= THERMAL_STATUS_CRITICAL;
             updateBlurEnabled();
         });
-        mDisabledByThermal = powerManager.getCurrentThermalStatus() >= THERMAL_STATUS_SEVERE;
+        mCriticalThermalStatus = powerManager.getCurrentThermalStatus() >= THERMAL_STATUS_CRITICAL;
 
         TunnelModeEnabledListener.register(mTunnelModeListener);
 
@@ -120,7 +120,7 @@ final class BlurController {
     private void updateBlurEnabled() {
         synchronized (mLock) {
             final boolean newEnabled = CROSS_WINDOW_BLUR_SUPPORTED && !mBlurDisabledSetting
-                    && !mInPowerSaveMode && !mTunnelModeEnabled && !mDisabledByThermal;
+                    && !mInPowerSaveMode && !mTunnelModeEnabled && !mCriticalThermalStatus;
             if (mBlurEnabled == newEnabled) {
                 return;
             }
