f7da0afc13079d7ecca658371794e99e6a6d7403
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-30 08:22:06 +0800

    fixing broken tee cert generation
    Change-Id: Ie45cd7460ca8e6c3fa75e284e9727dc5e791782b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/security/trickystore/CertificateGenerator.java
+++ core/java/android/security/trickystore/CertificateGenerator.java
@@ -1,5 +1,10 @@
 package android.security.trickystore;
 
+import android.app.ActivityThread;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.util.Log;
 
 import com.android.internal.org.bouncycastle.asn1.ASN1Boolean;
@@ -8,6 +13,7 @@ import com.android.internal.org.bouncycastle.asn1.ASN1EncodableVector;
 import com.android.internal.org.bouncycastle.asn1.ASN1Enumerated;
 import com.android.internal.org.bouncycastle.asn1.ASN1Integer;
 import com.android.internal.org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import com.android.internal.org.bouncycastle.asn1.ASN1OctetString;
 import com.android.internal.org.bouncycastle.asn1.DEROctetString;
 import com.android.internal.org.bouncycastle.asn1.DERSequence;
 import com.android.internal.org.bouncycastle.asn1.DERSet;
@@ -19,21 +25,30 @@ import com.android.internal.org.bouncycastle.asn1.x509.KeyUsage;
 import com.android.internal.org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import com.android.internal.org.bouncycastle.cert.X509CertificateHolder;
 import com.android.internal.org.bouncycastle.cert.X509v3CertificateBuilder;
+import com.android.internal.org.bouncycastle.jce.provider.BouncyCastleProvider;
 import com.android.internal.org.bouncycastle.operator.ContentSigner;
 import com.android.internal.org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.ECGenParameterSpec;
 import java.security.spec.RSAKeyGenParameterSpec;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.security.auth.x500.X500Principal;
 
 /**
  * @hide
@@ -53,7 +68,7 @@ public final class CertificateGenerator {
         public BigInteger certificateSerial;
         public Date certificateNotBefore;
         public Date certificateNotAfter;
-        public X500Name certificateSubject;
+        public X500Principal certificateSubject;
         public BigInteger rsaPublicExponent;
         public int ecCurve;
         public String ecCurveName;
@@ -104,7 +119,8 @@ public final class CertificateGenerator {
     public static List<Certificate> generateCertificateChain(
             KeyPair keyPair,
             KeyGenParameters params,
-            int securityLevel) {
+            int securityLevel,
+            int uid) {
         
         KeyBoxManager keyboxManager = TrickyStoreService.getInstance().getKeyBoxManager();
         String algorithm = params.algorithm == 3 ? "EC" : "RSA";
@@ -120,7 +136,7 @@ public final class CertificateGenerator {
                 keybox.certificates.get(0).getEncoded());
             X500Name issuer = issuerHolder.getSubject();
 
-            X509Certificate leaf = buildCertificate(keyPair, keybox, params, issuer, securityLevel);
+            X509Certificate leaf = buildCertificate(keyPair, keybox, params, issuer, securityLevel, uid);
 
             List<Certificate> chain = new ArrayList<>();
             chain.add(leaf);
@@ -137,7 +153,8 @@ public final class CertificateGenerator {
             KeyBoxManager.KeyBox keybox,
             KeyGenParameters params,
             X500Name issuer,
-            int securityLevel) throws Exception {
+            int securityLevel,
+            int uid) throws Exception {
 
         BigInteger serial = params.certificateSerial != null ? 
             params.certificateSerial : BigInteger.ONE;
@@ -147,7 +164,7 @@ public final class CertificateGenerator {
             params.certificateNotAfter : 
             ((X509Certificate) keybox.certificates.get(0)).getNotAfter();
         X500Name subject = params.certificateSubject != null ? 
-            params.certificateSubject : new X500Name("CN=Android Keystore Key");
+            X500Name.getInstance(params.certificateSubject.getEncoded()) : new X500Name("CN=Android Keystore Key");
 
         SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfo.getInstance(
             keyPair.getPublic().getEncoded());
@@ -162,11 +179,14 @@ public final class CertificateGenerator {
         );
 
         builder.addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.keyCertSign));
-        builder.addExtension(buildAttestExtension(params, securityLevel));
+        builder.addExtension(buildAttestExtension(params, securityLevel, uid));
 
         String sigAlg = params.algorithm == 3 ? "SHA256withECDSA" : "SHA256withRSA";
-        ContentSigner signer = new JcaContentSignerBuilder(sigAlg)
-            .build(keybox.keyPair.getPrivate());
+        JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(sigAlg);
+        if (params.algorithm == 3) {
+            signerBuilder.setProvider(new BouncyCastleProvider());
+        }
+        ContentSigner signer = signerBuilder.build(keybox.keyPair.getPrivate());
 
         X509CertificateHolder holder = builder.build(signer);
         CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
@@ -174,7 +194,7 @@ public final class CertificateGenerator {
             new ByteArrayInputStream(holder.getEncoded()));
     }
 
-    private static Extension buildAttestExtension(KeyGenParameters params, int securityLevel) {
+    private static Extension buildAttestExtension(KeyGenParameters params, int securityLevel, int uid) {
         try {
             byte[] bootKey = AttestationUtils.getBootKey();
             byte[] bootHash = AttestationUtils.getBootHash();
@@ -229,6 +249,12 @@ public final class CertificateGenerator {
             }
 
             ASN1EncodableVector softwareEnforced = new ASN1EncodableVector();
+            try {
+                ASN1OctetString applicationId = createApplicationId(uid);
+                softwareEnforced.add(new DERTaggedObject(true, 709, applicationId));
+            } catch (Throwable e) {
+                 Log.w(TAG, "Failed to create application ID", e);
+            }
             softwareEnforced.add(new DERTaggedObject(true, 701, new ASN1Integer(System.currentTimeMillis())));
 
             ASN1Encodable[] keyDescriptionElements = new ASN1Encodable[] {
@@ -250,5 +276,54 @@ public final class CertificateGenerator {
             Log.e(TAG, "Failed to build attestation extension", e);
             throw new RuntimeException(e);
         }
+    }
+
+    private static DEROctetString createApplicationId(int uid) throws Throwable {
+        Context context = ActivityThread.currentApplication();
+        if (context == null) {
+            throw new IllegalStateException("createApplicationId: context not available from ActivityThread!");
+        }
+
+        PackageManager pm = context.getPackageManager();
+        if (pm == null) {
+            throw new IllegalStateException("createApplicationId: PackageManager not found!");
+        }
+
+        String[] packages = pm.getPackagesForUid(uid);
+        if (packages == null || packages.length == 0) {
+            throw new IllegalStateException("No packages found for UID: " + uid);
+        }
+
+        int size = packages.length;
+        ASN1Encodable[] packageInfoAA = new ASN1Encodable[size];
+        Set<ByteBuffer> signatures = new HashSet<>();
+        MessageDigest dg = MessageDigest.getInstance("SHA-256");
+
+        for (int i = 0; i < size; i++) {
+            String name = packages[i];
+            PackageInfo info = pm.getPackageInfo(name, PackageManager.GET_SIGNATURES);
+            ASN1Encodable[] arr = new ASN1Encodable[2];
+            arr[0] = new DEROctetString(name.getBytes(StandardCharsets.UTF_8));
+            arr[1] = new ASN1Integer(info.getLongVersionCode());
+            packageInfoAA[i] = new DERSequence(arr);
+
+            if (info.signatures != null) {
+                for (Signature s : info.signatures) {
+                    signatures.add(ByteBuffer.wrap(dg.digest(s.toByteArray())));
+                }
+            }
+        }
+
+        ASN1Encodable[] signaturesAA = new ASN1Encodable[signatures.size()];
+        int i = 0;
+        for (ByteBuffer d : signatures) {
+            signaturesAA[i++] = new DEROctetString(d.array());
+        }
+
+        ASN1Encodable[] applicationIdAA = new ASN1Encodable[2];
+        applicationIdAA[0] = new DERSet(packageInfoAA);
+        applicationIdAA[1] = new DERSet(signaturesAA);
+
+        return new DEROctetString(new DERSequence(applicationIdAA).getEncoded());
     }
 }
