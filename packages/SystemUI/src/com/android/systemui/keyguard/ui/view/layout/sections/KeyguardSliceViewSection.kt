c936a12ec4f2fa1ab74b6322940fe3c749e934db
Pranav Vashi <neobuddy89@gmail.com>
2025-12-31 18:30:58 +0530

    Revert "SystemUI: Fix constraints for keyguard slice view"
    This reverts commit 5e6adeebde035eed840d29c6bd0679649bc4fcd5.

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -17,7 +17,6 @@
 
 package com.android.systemui.keyguard.ui.view.layout.sections
 
-import android.content.Context
 import android.os.Handler
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.Barrier
@@ -25,7 +24,6 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.android.keyguard.KeyguardSliceView
 import com.android.keyguard.KeyguardSliceViewController
-import com.android.systemui.customization.clocks.R as clocksR
 import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.dump.DumpManager
@@ -43,7 +41,6 @@ import javax.inject.Inject
 class KeyguardSliceViewSection
 @Inject
 constructor(
-    private val context: Context,
     val smartspaceController: LockscreenSmartspaceController,
     val layoutInflater: LayoutInflater,
     @Main val handler: Handler,
@@ -92,10 +89,6 @@ constructor(
                 ConstraintSet.START,
                 ConstraintSet.PARENT_ID,
                 ConstraintSet.START,
-                context.resources.getDimensionPixelSize(clocksR.dimen.clock_padding_start) +
-                    context.resources.getDimensionPixelSize(
-                        clocksR.dimen.status_view_margin_horizontal
-                    ),
             )
             connect(
                 R.id.keyguard_slice_view,
