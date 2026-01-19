fc8c733c3bb98764c2b4806647291551df651138
Rve27 <rve27github@gmail.com>
2026-01-19 14:18:22 +0000

    base: Migrate to MaterialExpressiveTheme
    Change-Id: I7aaf57be5d62337f49a5dbbbfdb5ed0a4f187727
    Signed-off-by: Rve27 <rve27github@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/compose/core/src/com/android/compose/theme/PlatformTheme.kt
+++ packages/SystemUI/compose/core/src/com/android/compose/theme/PlatformTheme.kt
@@ -22,6 +22,7 @@ import android.content.Context
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -74,7 +75,7 @@ fun PlatformTheme(isDarkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
         }
     val windowSizeClass = calculateWindowSizeClass()
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = typography,
         motionScheme = ExpressiveMotionScheme,
