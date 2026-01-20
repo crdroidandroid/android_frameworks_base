7a24128002d30531e010e7877f32d2858c7937c3
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-20 21:24:25 +0800

    MediaFocusControl: fixing media focus issues
    Change-Id: Iede8487e4e9c66269bd64b96dff49b78da26ff82
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/audio/MediaFocusControl.java
+++ services/core/java/com/android/server/audio/MediaFocusControl.java
@@ -423,7 +423,7 @@ public class MediaFocusControl implements PlayerFocusEnforcer {
                 }
             } else {
                 for (FocusRequester multifr : mMultiAudioFocusList) {
-                    if (isLockedFocusOwner(multifr)) {
+                    if (isLockedFocusOwner(multifr) || mFocusStack.empty()) {
                         multifr.handleFocusGain(AudioManager.AUDIOFOCUS_GAIN);
                     }
                 }
@@ -1163,6 +1163,9 @@ public class MediaFocusControl implements PlayerFocusEnforcer {
             IAudioFocusDispatcher fd, @NonNull String clientId, @NonNull String callingPackageName,
             int flags, int sdk, boolean forceDuck, int testUid,
             boolean permissionOverridesCheck) {
+        if (mMultiAudioFocusEnabled) {
+            return AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
+        }
         new MediaMetrics.Item(mMetricsId)
                 .setUid(Binder.getCallingUid())
                 .set(MediaMetrics.Property.CALLING_PACKAGE, callingPackageName)
