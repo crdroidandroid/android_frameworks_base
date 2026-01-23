0cbd3a53e4a28982767280506819ea3258f68992
Peng Dong <pengdong@google.com>
2026-01-22 22:04:14 -0800

    Monitor Settings.Global.DEVICE_PROVISIONED, if changed from 0 to 1 and the autofill provider is not available, reset to default.
    Change-Id: I2f52e85915582d08a586fffc4f61367bc6721c71

--- services/tests/servicestests/src/com/android/server/autofill/AutofillManagerServiceTest.java
+++ services/tests/servicestests/src/com/android/server/autofill/AutofillManagerServiceTest.java
@@ -18,15 +18,101 @@ package com.android.server.autofill;
 import static com.android.server.autofill.AutofillManagerService.getAllowedCompatModePackages;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.UserInfo;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.os.Build;
+import android.os.UserHandle;
+import android.provider.DeviceConfig;
+import android.provider.Settings;
+import android.test.mock.MockContentResolver;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.android.internal.util.test.FakeSettingsProvider;
+import com.android.server.LocalServices;
+import com.android.server.SystemService;
+import com.android.server.pm.UserManagerInternal;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RunWith(JUnit4.class)
 public class AutofillManagerServiceTest {
+
+    private static final int USER_ID = 42;
+    private static final String RESET_INVALID_SERVICE_ON_DEVICE_PROVISIONED_FLAG =
+            "autofill_reset_invalid_service_on_device_provisioned";
+
+    @Mock private Context mContext;
+    private MockContentResolver mContentResolver;
+    private @Mock PackageManager mPackageManager;
+    private @Mock Resources mResources;
+    @Mock private UserManagerInternal mMockUserManagerInternal;
+    private String mOriginalResetInvalidServiceOnDeviceProvisioned;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        LocalServices.addService(UserManagerInternal.class, mMockUserManagerInternal);
+        final ApplicationInfo applicationInfo = new ApplicationInfo();
+        applicationInfo.targetSdkVersion = Build.VERSION_CODES.CUR_DEVELOPMENT;
+        when(mContext.getApplicationInfo()).thenReturn(applicationInfo);
+        when(mContext.getResources()).thenReturn(mResources);
+        when(mContext.getPackageManager()).thenReturn(mPackageManager);
+        mContentResolver = new MockContentResolver(mContext);
+        mContentResolver.addProvider(Settings.AUTHORITY, new FakeSettingsProvider());
+        when(mContext.getContentResolver()).thenReturn(mContentResolver);
+        when(mResources.getString(anyInt())).thenReturn("autofill");
+        List<UserInfo> users = new ArrayList<>();
+        users.add(new UserInfo(10, "user10", UserInfo.FLAG_FULL));
+        users.add(new UserInfo(12, "user12", UserInfo.FLAG_FULL));
+        when(mMockUserManagerInternal.getUserInfos()).thenReturn(users.toArray(new UserInfo[0]));
+        mOriginalResetInvalidServiceOnDeviceProvisioned = DeviceConfig.getProperty(
+                DeviceConfig.NAMESPACE_AUTOFILL,
+                RESET_INVALID_SERVICE_ON_DEVICE_PROVISIONED_FLAG);
+    }
+
+    @After
+    public void teardown() {
+        LocalServices.removeServiceForTest(UserManagerInternal.class);
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+            .adoptShellPermissionIdentity();
+        try {
+            DeviceConfig.setProperty(DeviceConfig.NAMESPACE_AUTOFILL,
+                    RESET_INVALID_SERVICE_ON_DEVICE_PROVISIONED_FLAG,
+                    mOriginalResetInvalidServiceOnDeviceProvisioned, false);
+        } finally {
+            InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                    .dropShellPermissionIdentity();
+        }
+    }
 
     @Test
     public void testGetAllowedCompatModePackages_null() {
@@ -90,5 +176,75 @@ public class AutofillManagerServiceTest {
         assertThat(result).hasSize(2);
         assertThat(result.get("p1")).asList().containsExactly("p1u1");
         assertThat(result.get("p3")).asList().containsExactly("p3u1", "p3u2");
+    }
+
+    @Test
+    public void testHandleDeviceProvisioned() throws Exception {
+        // Arrange
+        setResetInvalidServiceOnDeviceProvisioned("true");
+        Settings.Global.putInt(mContentResolver, Settings.Global.DEVICE_PROVISIONED, 0);
+        final String invalidService = "com.invalid.service/.Service";
+        Settings.Secure.putStringForUser(
+                mContentResolver, Settings.Secure.AUTOFILL_SERVICE, invalidService, 10);
+        final String defaultService = "default/service";
+        when(mResources.getString(com.android.internal.R.string.config_defaultAutofillService))
+                .thenReturn(defaultService);
+        ContentResolver spiedResolver = spy(mContentResolver);
+        when(mContext.getContentResolver()).thenReturn(spiedResolver);
+        AutofillManagerService service = new AutofillManagerService(mContext);
+
+        // Act
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+            .adoptShellPermissionIdentity();
+        try {
+            Settings.Global.putInt(mContentResolver, Settings.Global.DEVICE_PROVISIONED, 1);
+            service.handleDeviceProvisioned();
+        } finally {
+            InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                    .dropShellPermissionIdentity();
+        }
+
+        // Assert
+        assertThat(Settings.Secure.getStringForUser(mContentResolver,
+                Settings.Secure.AUTOFILL_SERVICE, 10)).isEqualTo(defaultService);
+    }
+
+    @Test
+    public void testHandleDeviceProvisioned_whenAlreadyProvisioned() throws Exception {
+        // Arrange
+        setResetInvalidServiceOnDeviceProvisioned("true");
+        final String defaultService = "default/service";
+        Settings.Global.putInt(mContentResolver, Settings.Global.DEVICE_PROVISIONED, 1);
+        Settings.Secure.putStringForUser(
+                mContentResolver, Settings.Secure.AUTOFILL_SERVICE, defaultService, 10);
+        when(mResources.getString(com.android.internal.R.string.config_defaultAutofillService))
+                .thenReturn(defaultService);
+        AutofillManagerService service = new AutofillManagerService(mContext);
+
+        // Act
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+            .adoptShellPermissionIdentity();
+        try {
+            service.handleDeviceProvisioned();
+        } finally {
+            InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                    .dropShellPermissionIdentity();
+        }
+
+        // Assert
+        assertThat(Settings.Secure.getStringForUser(mContentResolver,
+                Settings.Secure.AUTOFILL_SERVICE, 10)).isEqualTo(defaultService);
+    }
+
+    private void setResetInvalidServiceOnDeviceProvisioned(String value) {
+        InstrumentationRegistry.getInstrumentation().getUiAutomation()
+            .adoptShellPermissionIdentity();
+        try {
+            DeviceConfig.setProperty(DeviceConfig.NAMESPACE_AUTOFILL,
+                    RESET_INVALID_SERVICE_ON_DEVICE_PROVISIONED_FLAG, value, false);
+        } finally {
+            InstrumentationRegistry.getInstrumentation().getUiAutomation()
+                    .dropShellPermissionIdentity();
+        }
     }
 }
