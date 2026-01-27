32c057ecf73fa8cb709bcd5cab94b88bdb422f4b
Pranav Vashi <neobuddy89@gmail.com>
2026-01-28 01:26:00 +0530

    SystemUI: Add status bar battery disable toggle
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/870
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/domain/interactor/HomeStatusBarIconBlockListInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/domain/interactor/HomeStatusBarIconBlockListInteractor.kt
@@ -16,21 +16,29 @@
 
 package com.android.systemui.statusbar.pipeline.shared.domain.interactor
 
+import android.content.Context
 import android.content.res.Resources
 import android.provider.Settings
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.res.R
 import com.android.systemui.shared.settings.data.repository.SecureSettingsRepository
+import com.android.systemui.statusbar.phone.ui.StatusBarIconController
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 
 /** A place to define the blocklist/allowlist for home status bar icons */
 @SysUISingleton
 class HomeStatusBarIconBlockListInteractor
 @Inject
-constructor(@Main res: Resources, secureSettingsRepository: SecureSettingsRepository) {
+constructor(
+    @Main private val context: Context,
+    @Main res: Resources,
+    secureSettingsRepository: SecureSettingsRepository,
+) {
     private val defaultBlockedIcons =
         res.getStringArray(R.array.config_collapsed_statusbar_icon_blocklist)
 
@@ -40,17 +48,29 @@ constructor(@Main res: Resources, secureSettingsRepository: SecureSettingsReposi
     private val shouldShowVibrateIcon: Flow<Boolean> =
         secureSettingsRepository.boolSetting(Settings.Secure.STATUS_BAR_SHOW_VIBRATE_ICON, false)
 
+    private val tunerBlockedIcons: Flow<Set<String>> =
+        secureSettingsRepository
+            .stringSetting(StatusBarIconController.ICON_HIDE_LIST)
+            .map { hideListStr ->
+                StatusBarIconController.getIconHideList(context, hideListStr).toSet()
+            }
+            .distinctUntilChanged()
+
     val iconBlockList: Flow<List<String>> =
-        shouldShowVibrateIcon.map {
-            val defaultSet = defaultBlockedIcons.toMutableSet()
+        combine(tunerBlockedIcons, shouldShowVibrateIcon) { tunerSet, showVibrate ->
+            val set = defaultBlockedIcons.toMutableSet()
+
+            // Merge tuner blacklist (this is what hides “clock”, “battery”, etc)
+            set.addAll(tunerSet)
+
             // It's possible that the vibrate icon was in the default blocklist, so we manually
             // merge the setting and list
-            if (it) {
-                defaultSet.remove(vibrateIconSlot)
+            if (showVibrate) {
+                set.remove(vibrateIconSlot)
             } else {
-                defaultSet.add(vibrateIconSlot)
+                set.add(vibrateIconSlot)
             }
 
-            defaultSet.toList()
+            set.toList()
         }
 }
