b4badf4251c0581fbe4f0c43b5b0f5b7e12940d8
LuK1337 <priv.luk@gmail.com>
2026-01-31 17:43:19 +0100

    fixup! webkit: SystemImpl: Filter out unavailable providers
    Allows non-factory packages to be used.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8764
    Change-Id: I318ae9402c7d55ecf2d400a22050d0cb4053ed2b

--- services/core/java/com/android/server/webkit/SystemImpl.java
+++ services/core/java/com/android/server/webkit/SystemImpl.java
@@ -144,7 +144,7 @@ public class SystemImpl implements SystemInterface {
     @Override
     public WebViewProviderInfo[] getWebViewPackages() {
         return Arrays.stream(mWebViewProviderPackages)
-                .filter(x -> isProviderAvailable(x.packageName))
+                .filter(this::isProviderAvailable)
                 .toArray(WebViewProviderInfo[]::new);
     }
 
@@ -154,9 +154,9 @@ public class SystemImpl implements SystemInterface {
                 .getLongVersionCode();
     }
 
-    private boolean isProviderAvailable(String packageName) {
+    private boolean isProviderAvailable(WebViewProviderInfo configInfo) {
         try {
-            getFactoryPackageVersion(packageName);
+            getPackageInfoForProvider(configInfo);
             return true;
         } catch (NameNotFoundException e) {
             return false;
