366418018bbb414fd269d6af5932f5e16c1eb774
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-02-17 06:40:15 +0800

    SystemUI: Adding platform hooks
    adding hooks to reduce ax platform apps boilerplate
    
    [neobuddy89: Hook in SystemUICoreStartableModule.]
    
    Change-Id: Ibdbd59a11ea8375c8698bef65a971a6c522fbe3e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/WifiSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/WifiSignalController.java
@@ -158,7 +158,9 @@ public class WifiSignalController extends SignalController<WifiState, IconGroup>
                 statusIcon, qsIcon, typeIcon, qsTypeIcon,
                 mCurrentState.activityIn, mCurrentState.activityOut, dataContentDescription,
                 dataContentDescriptionHtml, description,
-                mCurrentState.subId, /* roaming= */ false, /* showTriangle= */ true
+                mCurrentState.subId, /* roaming= */ false, /* showTriangle= */ true,
+                /* isDefault= */ qsIcon != null,
+                mCurrentState.level
         );
         callback.setMobileDataIndicators(mobileDataIndicators);
     }
