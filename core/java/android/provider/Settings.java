b20c0729f535184a6752093f45658e1458757c5f
Pranav Vashi <neobuddy89@gmail.com>
2026-02-25 21:16:11 +0530

    SystemUI: Add QS tile gradient customization
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7510,6 +7510,12 @@ public final class Settings {
         public static final String QS_FOOTER_SHOW_POWER_MENU = "qs_footer_show_power_menu";
 
         /**
+         * Gradient on QS tiles
+         * @hide
+         */
+        public static final String QS_TILE_GRADIENT = "qs_tile_gradient";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
