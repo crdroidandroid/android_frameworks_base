39fb9ecca4bf7a91b49437b91c000f59c3421756
Pranav Vashi <neobuddy89@gmail.com>
2026-03-01 22:20:07 +0530

    SystemUI: Clean up legacy code in ongoing action progress chip
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -208,18 +208,15 @@ import com.android.systemui.statusbar.KeyguardIndicationController;
 import com.android.systemui.statusbar.LiftReveal;
 import com.android.systemui.statusbar.LightRevealScrim;
 import com.android.systemui.statusbar.LockscreenShadeTransitionController;
-import com.android.systemui.statusbar.NotificationListener;
 import com.android.systemui.statusbar.NotificationLockscreenUserManager;
 import com.android.systemui.statusbar.NotificationPresenter;
 import com.android.systemui.statusbar.NotificationRemoteInputManager;
 import com.android.systemui.statusbar.NotificationShadeDepthController;
 import com.android.systemui.statusbar.NotificationShadeWindowController;
-import com.android.systemui.statusbar.OnGoingActionProgressController;
 import com.android.systemui.statusbar.PowerButtonReveal;
 import com.android.systemui.statusbar.PulseExpansionHandler;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.SysuiStatusBarStateController;
-import com.android.systemui.statusbar.VibratorHelper;
 import com.android.systemui.statusbar.core.StatusBarConnectedDisplays;
 import com.android.systemui.statusbar.core.StatusBarInitializer;
 import com.android.systemui.statusbar.core.StatusBarRootModernization;
@@ -431,11 +428,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     DozeServiceHost mDozeServiceHost;
     private final LightRevealScrim mLightRevealScrim;
     private PowerButtonReveal mPowerButtonReveal;
-
-    private OnGoingActionProgressController mOnGoingActionProgressController = null;
-    private final VibratorHelper mVibratorHelper;
-
-    @Inject public NotificationListener mNotificationListener;
 
     /**
      * Whether we should delay the AOD->Lockscreen animation.
@@ -790,8 +782,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             MediaViewController mediaViewController,
             PulseViewController pulseViewController,
             EdgeLightViewController edgeLightViewController,
-            BurnInProtectionController burnInProtectionController,
-            VibratorHelper vibrator
+            BurnInProtectionController burnInProtectionController
     ) {
         mContext = context;
         mNotificationsController = notificationsController;
@@ -945,7 +936,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mMediaViewController = mediaViewController;
         mPulseViewController = pulseViewController;
         mEdgeLightViewController = edgeLightViewController;
-        mVibratorHelper = vibrator;
     }
 
     private void initBubbles(Bubbles bubbles) {
@@ -1377,13 +1367,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                         checkBarModes();
                         mPhoneStatusBarViewController.setBrightnessControlEnabled(mBrightnessControl);
                         mBurnInProtectionController.setPhoneStatusBarView(mPhoneStatusBarViewController.getPhoneStatusBarView());
-                        if (!StatusBarRootModernization.isEnabled()) {
-                            mOnGoingActionProgressController =
-                                 new OnGoingActionProgressController(
-                                         mContext,
-                                         statusBarViewController.getOngoingActionProgressGroup(), mNotificationListener,
-                                         mKeyguardStateController, mHeadsUpManager, mVibratorHelper);
-                        }
                     });
         }
         if (!StatusBarRootModernization.isEnabled() && !StatusBarConnectedDisplays.isEnabled()) {
