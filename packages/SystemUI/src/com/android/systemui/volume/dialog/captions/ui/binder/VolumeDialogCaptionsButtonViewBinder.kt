1cb7e24dede08dc3dc8510ecba8e8be6e97f3320
Pranav Vashi <neobuddy89@gmail.com>
2026-03-03 07:19:16 +0530

    SystemUI: Add gradient for volume buttons along with slider
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/dialog/captions/ui/binder/VolumeDialogCaptionsButtonViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/volume/dialog/captions/ui/binder/VolumeDialogCaptionsButtonViewBinder.kt
@@ -16,11 +16,19 @@
 
 package com.android.systemui.volume.dialog.captions.ui.binder
 
+import android.content.Context
+import android.database.ContentObserver
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.InsetDrawable
 import android.graphics.drawable.TransitionDrawable
 import android.os.Handler
+import android.os.UserHandle
+import android.provider.Settings
 import android.view.View
 import com.android.app.tracing.coroutines.launchInTraced
 import com.android.app.tracing.coroutines.launchTraced
+import com.android.internal.R as internalR
 import com.android.systemui.Flags
 import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.res.R
@@ -34,6 +42,7 @@ import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.withIndex
+import kotlinx.coroutines.job
 
 /** Binds the captions button view. */
 @VolumeDialogScope
@@ -53,6 +62,39 @@ constructor(
 
         launchTraced("VDCBVB#addTouchableBounds") {
             dialogViewModel.addTouchableBounds(captionsButton)
+        }
+
+        val contentResolver = captionsButton.context.contentResolver
+        var gradientEnabled = isVolumeGradientEnabled(captionsButton.context)
+        var gradientColorsForRinger = getGradientColorsForRinger(captionsButton.context)
+
+        val gradientObserver =
+            object : ContentObserver(null) {
+                override fun onChange(selfChange: Boolean) {
+                    gradientEnabled = isVolumeGradientEnabled(view.context)
+                    gradientColorsForRinger = getGradientColorsForRinger(view.context)
+                }
+            }
+
+        contentResolver.registerContentObserver(
+            Settings.System.getUriFor(Settings.System.VOLUME_SLIDER_GRADIENT),
+            false, gradientObserver, UserHandle.USER_ALL,
+        )
+        contentResolver.registerContentObserver(
+            Settings.System.getUriFor(Settings.System.CUSTOM_GRADIENT_COLOR_MODE),
+            false, gradientObserver, UserHandle.USER_ALL,
+        )
+        contentResolver.registerContentObserver(
+            Settings.System.getUriFor(Settings.System.CUSTOM_GRADIENT_START_COLOR),
+            false, gradientObserver, UserHandle.USER_ALL,
+        )
+        contentResolver.registerContentObserver(
+            Settings.System.getUriFor(Settings.System.CUSTOM_GRADIENT_END_COLOR),
+            false, gradientObserver, UserHandle.USER_ALL,
+        )
+
+        coroutineContext.job.invokeOnCompletion {
+            contentResolver.unregisterContentObserver(gradientObserver)
         }
 
         viewModel.isVisible
@@ -88,6 +130,10 @@ constructor(
                         )
                     )
 
+                    if (isEnabled && gradientEnabled) {
+                        applyGradientSelectionBackground(this, gradientColorsForRinger)
+                    }
+
                     val transition = background as TransitionDrawable
                     transition.isCrossFadeEnabled = true
                     if (index == 0) {
@@ -116,7 +162,70 @@ constructor(
         )
     }
 
+    private fun isVolumeGradientEnabled(context: Context): Boolean {
+        return Settings.System.getIntForUser(
+            context.contentResolver, Settings.System.VOLUME_SLIDER_GRADIENT, 0,
+            UserHandle.USER_CURRENT) != 0
+    }
+
+    private fun getGradientColorsForRinger(context: Context): Pair<Int, Int> {
+        val resolver = context.contentResolver
+
+        val mode = Settings.System.getIntForUser(
+                resolver, Settings.System.CUSTOM_GRADIENT_COLOR_MODE, 0,
+                UserHandle.USER_CURRENT,
+            )
+
+        val primary = context.getColor(internalR.color.materialColorPrimary)
+        val secondary = context.getColor(internalR.color.materialColorSecondary)
+
+        if (mode == 1) {
+            val start = Settings.System.getIntForUser(
+                    resolver, Settings.System.CUSTOM_GRADIENT_START_COLOR, 0,
+                    UserHandle.USER_CURRENT,
+                )
+            val end = Settings.System.getIntForUser(
+                    resolver, Settings.System.CUSTOM_GRADIENT_END_COLOR, 0,
+                    UserHandle.USER_CURRENT,
+                )
+
+            val startColor = if (start != 0) start else primary
+            val endColor = if (end != 0) end else secondary
+
+            return startColor to endColor
+        }
+
+        return primary to secondary
+    }
+
+    private fun applyGradientSelectionBackground(
+        button: CaptionsToggleImageButton,
+        gradientColorsForRinger: Pair<Int, Int>,
+    ) {
+        val (startColor, endColor) = gradientColorsForRinger
+        val shape = button.backgroundShape()
+        shape.orientation = GradientDrawable.Orientation.TOP_BOTTOM
+        shape.colors = intArrayOf(startColor, endColor)
+        button.background.invalidateSelf()
+    }
+
     private companion object {
         const val DURATION_MILLIS = 500
     }
+}
+
+private fun CaptionsToggleImageButton.backgroundShape(): GradientDrawable {
+    val td = background as TransitionDrawable
+
+    fun unwrap(d: Drawable): GradientDrawable? {
+        return when (d) {
+            is GradientDrawable -> d
+            is InsetDrawable -> d.drawable as? GradientDrawable
+            else -> null
+        }
+    }
+
+    return unwrap(td.getDrawable(1))
+        ?: unwrap(td.getDrawable(0))
+        ?: throw IllegalStateException("Captions button background is not a GradientDrawable")
 }
