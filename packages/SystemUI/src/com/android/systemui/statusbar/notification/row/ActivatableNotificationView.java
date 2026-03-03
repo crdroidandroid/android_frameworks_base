5f88ba0106c226e0a98a1bfd44f31afd423f747d
Pranav Vashi <neobuddy89@gmail.com>
2026-03-03 08:47:16 +0530

    SystemUI: Fix notification flicker with transluency enabled
    Test: Screen record notification will flicker on expansion.
    
    Reason: Override tint color was reset on expansion.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ActivatableNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ActivatableNotificationView.java
@@ -166,10 +166,6 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
                 R.color.notification_ripple_tinted_color);
         mNormalRippleColor = mContext.getColor(
                 R.color.notification_ripple_untinted_color);
-        // Reset background color tint and override tint, as they are from an old theme
-        mBgTint = NO_COLOR;
-        mOverrideTint = NO_COLOR;
-        mOverrideAmount = 0.0f;
     }
 
     /**
@@ -306,8 +302,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     public void setOverrideTintColor(int color, float overrideAmount) {
         mOverrideTint = color;
         mOverrideAmount = overrideAmount;
-        int newColor = calculateBgColor();
-        setBackgroundTintColor(newColor);
+        updateBackgroundTint(false /* animated */);
     }
 
     protected void updateBackgroundTint() {
@@ -777,11 +772,11 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     }
 
     private int getRippleColor() {
-        if (mBgTint != 0) {
-            return mTintedRippleColor;
-        } else {
-            return mNormalRippleColor;
-        }
+        final boolean hasExplicitTint =
+                mBgTint != NO_COLOR
+                || (mOverrideTint != NO_COLOR && mOverrideAmount > 0f);
+
+        return hasExplicitTint ? mTintedRippleColor : mNormalRippleColor;
     }
 
     /**
