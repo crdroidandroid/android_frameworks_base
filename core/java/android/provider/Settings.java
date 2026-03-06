b4ce18d462e849e14fa69714892c8aa2d7c49b55
Ghosuto <clash.raja10@gmail.com>
2026-03-06 07:32:39 +0000

    SystemUI: Add color modes for music chip bg [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7561,6 +7561,11 @@ public final class Settings {
         public static final String ONGOING_COMPACT_MODE = "ongoing_compact_mode";
 
         /**
+         * @hide
+         */
+        public static final String ONGOING_CHIP_COLOR_MODE = "ongoing_chip_color_mode";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
