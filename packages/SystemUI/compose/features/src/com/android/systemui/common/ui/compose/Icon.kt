bdde02271b34d0a7951fd47c77423b990754f308
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-21 19:58:04 +0800

    SystemUI: Fixing unsupported qs tile icon crash
    Change-Id: Ide5149ab21a867b06647f9bd1306a0a21f716115
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/compose/features/src/com/android/systemui/common/ui/compose/Icon.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/common/ui/compose/Icon.kt
@@ -16,13 +16,18 @@
 
 package com.android.systemui.common.ui.compose
 
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.android.compose.ui.graphics.painter.rememberDrawablePainter
 import com.android.systemui.common.shared.model.Icon
 
@@ -36,15 +41,7 @@ import com.android.systemui.common.shared.model.Icon
  */
 @Composable
 fun Icon(icon: Icon, modifier: Modifier = Modifier, tint: Color = LocalContentColor.current) {
-    val contentDescription = icon.contentDescription?.load()
-    when (icon) {
-        is Icon.Loaded -> {
-            Icon(rememberDrawablePainter(icon.drawable), contentDescription, modifier, tint)
-        }
-        is Icon.Resource -> {
-            Icon(painterResource(icon.resId), contentDescription, modifier, tint)
-        }
-    }
+    IconInternal(icon, modifier, tint)
 }
 
 /**
@@ -55,27 +52,39 @@ fun Icon(icon: Icon, modifier: Modifier = Modifier, tint: Color = LocalContentCo
  * Note: Some drawables aren't compatible with [rememberDrawablePainter], used here for
  * [Icon.Loaded] icons, and won't be resized from their intrinsic size (b/394738023).
  */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Icon(icon: Icon, tint: (() -> Color)?, modifier: Modifier = Modifier) {
-    val localContentColor = LocalContentColor.current
+    IconInternal(icon, modifier, tint?.invoke() ?: LocalContentColor.current)
+}
+
+@Composable
+private fun IconInternal(icon: Icon, modifier: Modifier, tint: Color) {
+    val context = LocalContext.current
     val contentDescription = icon.contentDescription?.load()
+
     when (icon) {
         is Icon.Loaded -> {
             Icon(
-                rememberDrawablePainter(icon.drawable),
-                tint ?: { localContentColor },
-                contentDescription,
-                modifier,
+                bitmap = remember(icon.drawable) {
+                    icon.drawable.toBitmap().asImageBitmap()
+                },
+                contentDescription = contentDescription,
+                modifier = modifier,
+                tint = tint,
             )
         }
         is Icon.Resource -> {
-            Icon(
-                painterResource(icon.resId),
-                tint ?: { localContentColor },
-                contentDescription,
-                modifier,
-            )
+            val drawable = remember(icon.resId) {
+                ContextCompat.getDrawable(context, icon.resId)
+            }
+            if (drawable != null) {
+                Image(
+                    painter = rememberDrawablePainter(drawable),
+                    contentDescription = contentDescription,
+                    colorFilter = if (tint == Color.Unspecified) null else ColorFilter.tint(tint),
+                    modifier = modifier,
+                )
+            }
         }
     }
 }
