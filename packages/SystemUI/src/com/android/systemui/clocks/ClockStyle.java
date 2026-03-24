bf7e7df7e19308efa699b8d73a375356798e0e92
Ghosuto <clash.raja10@gmail.com>
2026-03-24 13:05:21 +0000

    SystemUI: Added new clock styles
    Co-authored-by: Idc <59904122+IDontCare-05@users.noreply.github.com>
    Co-authored-by: spkal01 <kalligeross@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
+++ packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
@@ -47,39 +47,52 @@ public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
     private static final int[] CLOCK_LAYOUTS = {
             0,
             R.layout.keyguard_clock_oos, // 1
-            R.layout.keyguard_clock_ios, // 2
-            R.layout.keyguard_clock_simple, // 3
-            R.layout.keyguard_clock_miui, // 4
-            R.layout.keyguard_clock_ide,  // 5
-            R.layout.keyguard_clock_moto, // 6
-            R.layout.keyguard_clock_stylish, // 7
-            R.layout.keyguard_clock_stylish2, //8
-            R.layout.keyguard_clock_stylish3, // 9
-            R.layout.keyguard_clock_stylish4, // 10
-            R.layout.keyguard_clock_stylish5, // 11
-            R.layout.keyguard_clock_stylish6, // 12
-            R.layout.keyguard_clock_stylish7, // 13
-            R.layout.keyguard_clock_stylish8, // 14
-            R.layout.keyguard_clock_stylish9, // 15
-            R.layout.keyguard_clock_stylish10, // 16
-            R.layout.keyguard_clock_word, // 17
-            R.layout.keyguard_clock_life, // 18
-            R.layout.keyguard_clock_a9, // 19
-            R.layout.keyguard_clock_nos1, // 20
-            R.layout.keyguard_clock_nos2, // 21
-            R.layout.keyguard_clock_num, // 22
-            R.layout.keyguard_clock_accent, // 23
-            R.layout.keyguard_clock_analog, // 24
-            R.layout.keyguard_clock_block, // 25
-            R.layout.keyguard_clock_bubble, // 26
-            R.layout.keyguard_clock_label, // 27
-            R.layout.keyguard_clock_taden, // 28
-            R.layout.keyguard_clock_mont, // 29
-            R.layout.keyguard_clock_encode, // 30
-            R.layout.keyguard_clock_nos3 // 31
+            R.layout.keyguard_clock_oos2, // 2
+            R.layout.keyguard_clock_center, // 3
+            R.layout.keyguard_clock_simple, // 4
+            R.layout.keyguard_clock_miui, // 5
+            R.layout.keyguard_clock_ide, // 6
+            R.layout.keyguard_clock_moto, // 7
+            R.layout.keyguard_clock_stylish, // 8
+            R.layout.keyguard_clock_stylish2, // 9
+            R.layout.keyguard_clock_stylish3, // 10
+            R.layout.keyguard_clock_stylish4, // 11
+            R.layout.keyguard_clock_stylish5, // 12
+            R.layout.keyguard_clock_stylish6, // 13
+            R.layout.keyguard_clock_stylish7, // 14
+            R.layout.keyguard_clock_stylish8, // 15
+            R.layout.keyguard_clock_stylish9, // 16
+            R.layout.keyguard_clock_stylish10, // 17
+            R.layout.keyguard_clock_word, // 18
+            R.layout.keyguard_clock_life, // 19
+            R.layout.keyguard_clock_a9, // 20
+            R.layout.keyguard_clock_nos1, // 21
+            R.layout.keyguard_clock_nos2, // 22
+            R.layout.keyguard_clock_num, // 23
+            R.layout.keyguard_clock_accent, // 24
+            R.layout.keyguard_clock_analog, // 25
+            R.layout.keyguard_clock_block, // 26
+            R.layout.keyguard_clock_bubble, // 27
+            R.layout.keyguard_clock_label, // 28
+            R.layout.keyguard_clock_ios, // 29
+            R.layout.keyguard_clock_taden, // 30
+            R.layout.keyguard_clock_mont, // 31
+            R.layout.keyguard_clock_encode, // 32
+            R.layout.keyguard_clock_nos3, // 33
+            R.layout.keyguard_anci_clock_outline, // 34
+            R.layout.keyguard_anci_clock_ovalium, // 35
+            R.layout.keyguard_anci_clock_rectangle, // 36
+            R.layout.keyguard_anci_clock_wallet, // 37
+            R.layout.keyguard_anci_clockdate_clavicula, // 38
+            R.layout.keyguard_anci_clockdate_kln, // 39
+            R.layout.keyguard_anci_clockdate_miring, // 40
+            R.layout.keyguard_anci_clockdate_scapula, // 41
+            R.layout.keyguard_anci_clockdate_sternum, // 42
+            R.layout.keyguard_sparkCircle, // 43
+            R.layout.keyguard_sparkList, // 44
     };
 
-    private static final int[] mNoColorClocks = {25, 26};
+    private static final int[] mNoColorClocks = {1, 2, 25, 26};
 
     public static final String CLOCK_STYLE_KEY = Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_STYLE;
     public static final String CLOCK_COLOR_MODE_KEY = Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_COLOR_MODE;
