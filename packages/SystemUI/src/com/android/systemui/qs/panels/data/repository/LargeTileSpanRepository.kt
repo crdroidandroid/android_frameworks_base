5efa95d71c9af54cc1e642701cafc8dd86a67121
Pranav Vashi <neobuddy89@gmail.com>
2026-03-26 09:01:22 +0530

    SystemUI: Add classic QS panel style for tiles
    Design Concept attribution:
    https://github.com/BlissRoms/platform_frameworks_base/commit/d7be9f37ae7e1c1187970adacf96ec1bfd7c2f22
    https://github.com/BlissRoms/platform_frameworks_base/commit/7a8a0263a60e1b76b47e42011ef8c161f36528d2
    https://github.com/BlissRoms/platform_frameworks_base/commit/a50e10ce08d79e577f5feb8a1f2f2005e4a4ca91
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/panels/data/repository/LargeTileSpanRepository.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/data/repository/LargeTileSpanRepository.kt
@@ -16,15 +16,23 @@
 
 package com.android.systemui.qs.panels.data.repository
 
+import android.content.Context
 import android.content.res.Resources
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.UserHandle
+import android.provider.Settings
 import com.android.systemui.common.ui.data.repository.ConfigurationRepository
 import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.dagger.qualifiers.Application
 import com.android.systemui.res.R
 import com.android.systemui.shade.ShadeDisplayAware
 import com.android.systemui.util.kotlin.emitOnStart
 import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.mapLatest
 
@@ -33,6 +41,7 @@ import kotlinx.coroutines.flow.mapLatest
 class LargeTileSpanRepository
 @Inject
 constructor(
+    @Application private val context: Context,
     @ShadeDisplayAware private val resources: Resources,
     @ShadeDisplayAware configurationRepository: ConfigurationRepository,
 ) {
@@ -60,4 +69,33 @@ constructor(
         const val FONT_SCALE_THRESHOLD = 1.8f
         const val DEFAULT_LARGE_TILE_WIDTH = 2
     }
+
+    private fun settingsChanges(): Flow<Unit> = callbackFlow {
+        val observer = object : ContentObserver(/* handler */ null) {
+            override fun onChange(selfChange: Boolean, uri: Uri?) {
+                trySend(Unit)
+            }
+        }
+        val cr = context.contentResolver
+        cr.registerContentObserver(Settings.System.getUriFor(Settings.System.QS_PANEL_STYLE),
+            /* notifyForDescendants */ false, observer, UserHandle.USER_ALL)
+        awaitClose { cr.unregisterContentObserver(observer) }
+    }
+
+    private fun readQSPanelStyleEnabled(): Boolean {
+        return try {
+            Settings.System.getIntForUser(
+                context.contentResolver, Settings.System.QS_PANEL_STYLE, 0,
+                UserHandle.USER_CURRENT
+            ) != 0
+        } catch (_: Throwable) {
+            false
+        }
+    }
+
+    val classicStyle: Flow<Boolean> =
+        settingsChanges()
+            .emitOnStart()
+            .mapLatest { readQSPanelStyleEnabled() }
+            .distinctUntilChanged()
 }
