cdc4fb620d432959bc57b6693e3f19f28fd9c84f
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-01 06:32:29 +0800

    services: Optimizing redundant updateLockscreenTimeout updates
    Change-Id: I128b8442ed1b1e2be900c0f15820a9e8aaabe2b0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7283,12 +7283,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && mUserManagerInternal.getUserAssignedToDisplay(displayId) != mCurrentUserId) {
             return;
         }
-        if (allow) {
-            mAllowLockscreenWhenOnDisplays.add(displayId);
-        } else {
-            mAllowLockscreenWhenOnDisplays.remove(displayId);
+        final boolean changed = allow
+                ? mAllowLockscreenWhenOnDisplays.add(displayId)
+                : mAllowLockscreenWhenOnDisplays.remove(displayId);
+        if (changed) {
+            updateLockScreenTimeout();
         }
-        updateLockScreenTimeout();
     }
 
     private void updateLockScreenTimeout() {
