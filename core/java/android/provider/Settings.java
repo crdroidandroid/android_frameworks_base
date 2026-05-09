43f51b6da361808d4c5db4f91b023a01127490b4
Pranav Vashi <neobuddy89@gmail.com>
2026-05-10 01:57:32 +0530

    core: pif: Add separate tunable for gphotos spoof
    * This is dead-end. This change is only to clean up code.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14950,6 +14950,11 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String SPOOF_PIF_PHOTOS = "spoof_pif_photos";
+
+        /**
+         * @hide
+         */
         public static final String SPOOF_GAMEPROPS_CONFIG = "spoof_gameprops_config";
 
         /**
