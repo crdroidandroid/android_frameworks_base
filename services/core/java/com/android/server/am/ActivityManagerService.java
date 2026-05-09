16fb36f46876483f1e6166d44700eb747e9ad435
Pranav Vashi <neobuddy89@gmail.com>
2026-05-10 01:57:32 +0530

    core: pif: Add separate tunable for gphotos spoof
    * This is dead-end. This change is only to clean up code.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19725,6 +19725,11 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
 
     @Override
+    public String getSpoofPifSpoofPhotos() {
+        return AxExtServiceFactory.getSpoofManager().getPifSpoofPhotos();
+    }
+
+    @Override
     public String getSpoofGamePropsConfig() {
         return AxExtServiceFactory.getSpoofManager().getGamePropsConfig();
     }
