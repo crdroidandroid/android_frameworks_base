82d3515c188a0124f676ce4b9ef4f1509303305e
Pranav Vashi <neobuddy89@gmail.com>
2026-05-23 19:33:32 +0530

    SystemUI: Fix up and improve clock styles
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
+++ packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
@@ -121,8 +121,7 @@ public class ClockStyle extends RelativeLayout implements TunerService.Tunable {
 
     private static final int[] mCenterClocks = {
         3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-        20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-        34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44
+        20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33
     };
 
     private static final int[] mNoColorClocks = {1, 2, 25, 26, 45};
