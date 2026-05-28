873db70175cd0d47357f3b4cd460206851177061
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-28 19:38:06 +0800

    core: sandbox: Fix app lock issue
    fixed bypass issue - https://github.com/AxionAOSP/issue_tracker/issues/252
    fixed notification dim stuck issue - use goToShade instead. TODO: fix activity prompt on keyguard
    
    Change-Id: If181c38ac9048213495bd35de17978350839e380
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ExpandableNotificationRow.java
@@ -415,7 +415,7 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
     }
 
     private void toggleExpansionState(View v, boolean shouldLogExpandClickMetric) {
-        if (doesNotificationNeedAuth()) {
+        if (doesNotificationNeedAuth() && !isOnKeyguard()) {
             promptAppUnlock();
             return;
         }
