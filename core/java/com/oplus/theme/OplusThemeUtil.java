f0fc0d947daf806648fa29707fd54d31140e4d25
Pranav Vashi <neobuddy89@gmail.com>
2026-06-12 20:30:52 +0530

    Revert "Add some fwb stubs from Oplus"
    This reverts commit c3a7ec56fd31a779451bd65db681d55410ffe2ea.

--- core/java/com/oplus/theme/OplusThemeUtil.java
+++
@@ -1,9 +0,0 @@
-package com.oplus.theme;
-
-import android.compat.annotation.UnsupportedAppUsage;
-
-public class OplusThemeUtil {
-    /** @hide */
-    @UnsupportedAppUsage
-    public static String CUSTOM_THEME_PATH = "/data/theme/com.oplus.camera";
-}
