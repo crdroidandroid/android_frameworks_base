fbfeb729668dad320df6cc0a5b14b02ad222631e
Cobra_Fast <c@ezl.re>
2026-07-03 15:53:51 +0200

    services: Add screen-on memory reclaim toggle
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/1059
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -859,6 +859,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private ThreeFingersSwipeListener mThreeFingersSwipe;
     private boolean mThreeFingersSwipeHasAction;
 
+    private boolean mScreenOnReclaim;
+
     private PocketManager mPocketManager;
     private PocketLock mPocketLock;
     private boolean mPocketLockShowing;
@@ -1147,6 +1149,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ANBI_ENABLED), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREEN_ON_MEMORY_RECLAIM), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -3700,6 +3705,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mGlobalActionsOnLockEnable = Settings.System.getIntForUser(resolver,
                     Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1,
                     UserHandle.USER_CURRENT) != 0;
+
+            mScreenOnReclaim = Settings.System.getIntForUser(resolver,
+                    Settings.System.SCREEN_ON_MEMORY_RECLAIM, 1, UserHandle.USER_CURRENT) == 1;
 
             kidsModeEnabled = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.NAV_BAR_KIDS_MODE, 0, UserHandle.USER_CURRENT) == 1;
@@ -6482,8 +6490,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         EventLogTags.writeScreenToggled(1);
 
-        mHandler.removeCallbacks(mMemoryOpt);
-        mHandler.postDelayed(mMemoryOpt, 1250 /* allowance time */);
+        if (mScreenOnReclaim) {
+            mHandler.removeCallbacks(mMemoryOpt);
+            mHandler.postDelayed(mMemoryOpt, 1250 /* allowance time */);
+        }
 
         // remove pending system server gc for frequent screen state changes
         mHandler.removeCallbacks(mSystemServerGcOpt);
