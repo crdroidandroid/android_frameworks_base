e006566046a0a9d96bb23777f75756db9ff01410
LuK1337 <priv.luk@gmail.com>
2026-07-10 18:37:34 +0200

    webkit: WebViewUpdateServiceImpl2: Use compile time debuggable check
    Since Google WebView has the highest priority out of all configured
    packages in config_webview_packages.xml, if one happens to have it
    installed and its version is older than AOSP WebView, we'll fallback to
    no WebView, breaking all apps that use it.
    
    Since we'd rather just let user do whatever they wish, let's just skip
    this check on debuggable builds regardless of `ro.debuggable` value.
    
    Test: Install Google WebView older than AOSP WebView, then reboot and
          open Jelly
    Change-Id: If1263ae92ab1a720758c7e149c7b11f6e23ac2fd

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -1490,7 +1490,7 @@ public class ComputerEngine implements Computer {
         return result;
     }
 
-    private static native boolean isDebuggable();
+    public static native boolean isDebuggable();
 
     public static boolean isMicrogSigned(AndroidPackage p) {
         if (!isDebuggable()) {
--- services/core/java/com/android/server/webkit/WebViewUpdateServiceImpl2.java
+++ services/core/java/com/android/server/webkit/WebViewUpdateServiceImpl2.java
@@ -15,6 +15,8 @@
  */
 package com.android.server.webkit;
 
+import static com.android.server.pm.ComputerEngine.isDebuggable;
+
 import android.annotation.Nullable;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -593,7 +595,7 @@ class WebViewUpdateServiceImpl2 {
             return VALIDITY_OS_INCOMPATIBLE;
         }
         if (!versionCodeGE(packageInfo.getLongVersionCode(), getMinimumVersionCode())
-                && !mSystemInterface.systemIsDebuggable()) {
+                && !isDebuggable()) {
             // Webview providers may be downgraded arbitrarily low, prevent that by enforcing
             // minimum version code. This check is only enforced for user builds.
             return VALIDITY_INCORRECT_VERSION_CODE;
