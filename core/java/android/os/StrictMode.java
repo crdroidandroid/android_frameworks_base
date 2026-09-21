bed6484a3893adc35d54dd050d44673ccc96d085
Pranav Vashi <neobuddy89@gmail.com>
2026-09-21 23:24:25 +0530

    Revert "core: Preventing memory leaks from bloating os memory that leads to OOM"
    This reverts commit c01e57f2b46febb8a739ae1ab4f525c29a887fdf.
    
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/1150

--- core/java/android/os/StrictMode.java
+++ core/java/android/os/StrictMode.java
@@ -1559,8 +1559,6 @@ public final class StrictMode {
         if (targetSdkVersion >= Build.VERSION_CODES.N) {
             builder.detectFileUriExposure();
             builder.penaltyDeathOnFileUriExposure();
-            builder.detectActivityLeaks();
-            builder.detectLeakedRegistrationObjects();
         }
 
         if (Build.IS_USER || Build.IS_USERDEBUG || DISABLE || SystemProperties.getBoolean(DISABLE_PROPERTY, false)) {
@@ -2663,9 +2661,7 @@ public final class StrictMode {
 
     /** @hide */
     public static void onVmPolicyViolation(Violation originStack) {
-        boolean forceDeath = originStack instanceof IntentReceiverLeakedViolation 
-            || originStack instanceof ServiceConnectionLeakedViolation;
-        onVmPolicyViolation(originStack, forceDeath);
+        onVmPolicyViolation(originStack, false);
     }
 
     /** @hide */
